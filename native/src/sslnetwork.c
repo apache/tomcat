97c9698942fa3cd7da5405a2a4e06dec033a2326
Mladen Turk <mturk@apache.org>
2005-05-24 10:53:20 +0000

    Initial implementation.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk/jni@300673 13f79535-47bb-0310-9956-ffa450edef68

---
+++ native/src/sslnetwork.c
@@ -0,0 +1,39 @@
+/* Copyright 2000-2004 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** SSL network wrapper
+ *
+ * @author Mladen Turk
+ * @version $Revision$, $Date$
+ */
+
+#include "apr.h"
+#include "apr_pools.h"
+#include "apr_file_io.h"
+#include "apr_portable.h"
+#include "apr_thread_mutex.h"
+
+#include "tcn.h"
+
+#ifdef HAVE_OPENSSL
+#include "ssl_private.h"
+
+#else
+/* OpenSSL is not supported
+ * If someday we make OpenSSL optional
+ * APR_ENOTIMPL will go here
+ */
+#error "No OpenSSL Toolkit defined."
+#endif
