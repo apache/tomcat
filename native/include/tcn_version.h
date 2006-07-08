4cfaa0d9b9787c9c7091b7bdeb38f43f8833a881
Mladen Turk <mturk@apache.org>
2006-07-08 07:16:58 +0000

    Bump to the next version (1.1.4)
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk/jni@420067 13f79535-47bb-0310-9956-ffa450edef68

--- native/include/tcn_version.h
+++ native/include/tcn_version.h
@@ -68,7 +68,7 @@ extern "C" {
 #define TCN_MINOR_VERSION       1
 
 /** patch level */
-#define TCN_PATCH_VERSION       3
+#define TCN_PATCH_VERSION       4
 
 /**
  *  This symbol is defined for internal, "development" copies of TCN. This
