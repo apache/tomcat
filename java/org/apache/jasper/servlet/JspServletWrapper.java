d79e61ef57cc3f664f99c8cd2d9f771c0e3f0d39
Remy Maucherat <remm@apache.org>
2006-09-01 22:37:38 +0000

    - Reset the elContext field after use. - Submitted by Scott Johnson and Stan Silvert.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc6.0.x/trunk@439494 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/jasper/servlet/JspServletWrapper.java
+++ java/org/apache/jasper/servlet/JspServletWrapper.java
@@ -17,12 +17,11 @@
 package org.apache.jasper.servlet;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
