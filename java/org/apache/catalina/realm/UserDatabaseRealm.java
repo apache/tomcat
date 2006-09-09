6f561e7ed27c70c1b2c33126748d06dfe40c7e9f
Mark Thomas <markt@apache.org>
2006-09-09 21:10:42 +0000

    Port logic error fix from TC5.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc6.0.x/trunk@441856 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/realm/UserDatabaseRealm.java
+++ java/org/apache/catalina/realm/UserDatabaseRealm.java
@@ -223,7 +223,7 @@ public class UserDatabaseRealm
         Iterator groups = user.getGroups();
         while(groups.hasNext()) {
             Group group = (Group)groups.next();
-            uroles = user.getRoles();
+            uroles = group.getRoles();
             while(uroles.hasNext()) {
                 Role role = (Role)uroles.next();
                 roles.add(role.getName());
