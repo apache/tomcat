3b12cfe5e5c0c990e7cd3faaa08fbb040d0292b7
Remy Maucherat <remm@apache.org>
2006-09-19 14:36:34 +0000

    - Fix type.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc6.0.x/trunk@447863 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/jasper/el/ELResolverImpl.java
+++ java/org/apache/jasper/el/ELResolverImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1999,2004 The Apache Software Foundation.
+ * Copyright 2006 The Apache Software Foundation.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.jasper.el;
 
 import java.util.Iterator;
@@ -130,7 +131,7 @@ public final class ELResolverImpl extends ELResolver {
 		return DefaultResolver.isReadOnly(context, base, property);
 	}
 
-	public Iterator getFeatureDescriptors(ELContext context, Object base) {
+	public Iterator<java.beans.FeatureDescriptor> getFeatureDescriptors(ELContext context, Object base) {
 		return DefaultResolver.getFeatureDescriptors(context, base);
 	}
 
