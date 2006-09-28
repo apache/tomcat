6669cb60b84b78b7247130fcdb88bed87e4eecd3
Mladen Turk <mturk@apache.org>
2006-09-28 09:47:44 +0000

    Fix release process. The sources will from now have the -src in the path like APR has.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk/jni@450777 13f79535-47bb-0310-9956-ffa450edef68

--- jnirelease.sh
+++ jnirelease.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 #
-# Copyright 1999-2005 The Apache Software Foundation
+# Copyright 1999-2006 The Apache Software Foundation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,15 +14,46 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# APRDIR have to be the location of the APR sources
-APRDIR=$HOME/apr
-#
-# Replace JKJNIEXT with branche/or tag
-#  and JKJNIVER by the version like -1.1.0
-JKJNIEXT="trunk"
-JKJNIVER="-dev"
+# Default place to look for apr source.  Can be overridden with 
+#   --with-apr=[directory]
+apr_src_dir=../apr
+
+while test $# -gt 0 
+do
+  # Normalize
+  case "$1" in
+  -*=*) optarg=`echo "$1" | sed 's/[-_a-zA-Z0-9]*=//'` ;;
+  *) optarg= ;;
+  esac
+
+  case "$1" in
+  --with-apr=*)
+  apr_src_dir=$optarg
+  ;;
+  esac
+
+  shift
+done
+
+if test -d "$apr_src_dir"
+then
+  echo ""
+  echo "Looking for apr source in $apr_src_dir"
+else
+  echo ""
+  echo "Problem finding apr source in $apr_src_dir."
+  echo "Use:"
+  echo "  --with-apr=[directory]" 
+  exit 1
+fi
+
+# Replace JKJNIEXT with branch/or tag
+# and JKJNIVER by the version like 1.1.0
+JKJNIEXT=trunk
+JKJNIVER=current
+# JKJNIVER="1.1.5"
 SVNBASE=https://svn.apache.org/repos/asf/tomcat/connectors/
-JKJNIDIST=tomcat-connectors${JKJNIVER}
+JKJNIDIST=tomcat-connectors-${JKJNIVER}-src
 rm -rf ${JKJNIDIST}
 mkdir -p ${JKJNIDIST}/jni
 svn export $SVNBASE/${JKJNIEXT}/jni/native ${JKJNIDIST}/jni/native
@@ -31,15 +62,22 @@ svn cat $SVNBASE/${JKJNIEXT}/LICENSE > ${JKJNIDIST}/LICENSE
 svn cat $SVNBASE/${JKJNIEXT}/NOTICE > ${JKJNIDIST}/NOTICE
 svn cat $SVNBASE/${JKJNIEXT}/jni/NOTICE.txt > ${JKJNIDIST}/NOTICE.txt
 svn cat $SVNBASE/${JKJNIEXT}/jni/README.txt > ${JKJNIDIST}/README.txt
-
+#
 # Prebuild
 cd ${JKJNIDIST}/jni/native
-# Adjust the location of APR sources
-./buildconf --with-apr=$APRDIR
+./buildconf --with-apr=$apr_src_dir
 cd ../../../
-# Create distribution
-tar cvf ${JKJNIDIST}.tar ${JKJNIDIST}
-gzip ${JKJNIDIST}.tar
-# Convert lineends to DOS
-perl $APRDIR/build/lineends.pl --cr ${JKJNIDIST}
-zip -9 -r  ${JKJNIDIST}.zip ${JKJNIDIST}
+# Create source distribution
+tar cfz ${JKJNIDIST}.tar.gz ${JKJNIDIST}
+#
+# Create Win32 source distribution
+JKJNIDIST=tomcat-connectors-${JKJNIVER}-win32-src
+rm -rf ${JKJNIDIST}
+mkdir -p ${JKJNIDIST}/jni
+svn export --native-eol CRLF $SVNBASE/${JKJNIEXT}/jni/native ${JKJNIDIST}/jni/native
+svn cat $SVNBASE/${JKJNIEXT}/KEYS > ${JKJNIDIST}/KEYS
+svn cat $SVNBASE/${JKJNIEXT}/LICENSE > ${JKJNIDIST}/LICENSE
+svn cat $SVNBASE/${JKJNIEXT}/NOTICE > ${JKJNIDIST}/NOTICE
+svn cat $SVNBASE/${JKJNIEXT}/jni/NOTICE.txt > ${JKJNIDIST}/NOTICE.txt
+svn cat $SVNBASE/${JKJNIEXT}/jni/README.txt > ${JKJNIDIST}/README.txt
+zip -9rqo ${JKJNIDIST}.zip ${JKJNIDIST}
