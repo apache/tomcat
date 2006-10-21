8211e64f46a606b5d5e35f3b497400926ee613ce
Mark Thomas <markt@apache.org>
2006-10-21 23:30:39 +0000

    Update license header in TC6 trunk to comply with: http://www.apache.org/legal/src-headers.html#headers
    It does compile but I need to check the commit message carefully to ensure the script hasn't changed anything it shouldn't have done.
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc6.0.x/trunk@466609 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/digester/SetPropertyRule.java
+++ java/org/apache/tomcat/util/digester/SetPropertyRule.java
@@ -1,10 +1,11 @@
 /* $Id: SetPropertyRule.java 299475 2004-06-26 17:41:32Z remm $
  *
- * Copyright 2001-2004 The Apache Software Foundation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  * 
  *      http://www.apache.org/licenses/LICENSE-2.0
  * 
