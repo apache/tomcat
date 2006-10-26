403688863a9500788e2ccca110355a6e3d27e40a
Filip Hanik <fhanik@apache.org>
2006-10-26 22:04:24 +0000

    Cleaned up imports
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc6.0.x/trunk@468166 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http11/InternalNioInputBuffer.java
+++ java/org/apache/coyote/http11/InternalNioInputBuffer.java
@@ -20,21 +20,16 @@ package org.apache.coyote.http11;
 
 import java.io.EOFException;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.CancelledKeyException;
-import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
 
 import org.apache.coyote.InputBuffer;
 import org.apache.coyote.Request;
 import org.apache.tomcat.util.buf.ByteChunk;
 import org.apache.tomcat.util.buf.MessageBytes;
 import org.apache.tomcat.util.http.MimeHeaders;
-import org.apache.tomcat.util.net.NioEndpoint.KeyAttachment;
-import org.apache.tomcat.util.net.NioEndpoint.Poller;
-import org.apache.tomcat.util.res.StringManager;
 import org.apache.tomcat.util.net.NioChannel;
 import org.apache.tomcat.util.net.NioSelectorPool;
-import java.nio.channels.Selector;
+import org.apache.tomcat.util.res.StringManager;
 
 /**
  * Implementation of InputBuffer which provides HTTP request header parsing as
