bbb317a2625913ecd8f650bb5b7bb18f856c50c9
Timothy A. Funk <funkman@apache.org>
2006-10-29 19:28:37 +0000

    There is no org.apache.catalina.Logger or setLogger anymore
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc6.0.x/trunk@468957 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/startup/EngineRuleSet.java
+++ java/org/apache/catalina/startup/EngineRuleSet.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -120,13 +120,6 @@ public class EngineRuleSet extends RuleSetBase {
                             "addLifecycleListener",
                             "org.apache.catalina.LifecycleListener");
 
-        digester.addObjectCreate(prefix + "Engine/Logger",
-                                 null, // MUST be specified in the element
-                                 "className");
-        digester.addSetProperties(prefix + "Engine/Logger");
-        digester.addSetNext(prefix + "Engine/Logger",
-                            "setLogger",
-                            "org.apache.catalina.Logger");
 
         digester.addObjectCreate(prefix + "Engine/Realm",
                                  null, // MUST be specified in the element
