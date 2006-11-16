0d52fc64a7f69c99d28f26e65afd0efc3777da1e
Mladen Turk <mturk@apache.org>
2006-11-16 18:05:18 +0000

    Increment the version number.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk/jni@475841 13f79535-47bb-0310-9956-ffa450edef68

--- native/include/tcn_version.h
+++ native/include/tcn_version.h
@@ -69,7 +69,7 @@ extern "C" {
 #define TCN_MINOR_VERSION       1
 
 /** patch level */
-#define TCN_PATCH_VERSION       7
+#define TCN_PATCH_VERSION       8
 
 /**
  *  This symbol is defined for internal, "development" copies of TCN. This
