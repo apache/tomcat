3287c3afcc81d75d08782615de2072c38e663d91
Remy Maucherat <remm@apache.org>
2007-03-12 14:34:39 +0000

    - Cleanup of the message that is displayed.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc6.0.x/trunk@517231 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/jasper/compiler/DefaultErrorHandler.java
+++ java/org/apache/jasper/compiler/DefaultErrorHandler.java
@@ -67,17 +67,25 @@ class DefaultErrorHandler implements ErrorHandler {
         StringBuffer buf = new StringBuffer();
         
         for (int i=0; i < details.length; i++) {
-            args = new Object[] {
-                    new Integer(details[i].getJspBeginLineNumber()), 
-                    details[i].getJspFileName() };
-            buf.append("\n\n");
-            buf.append(Localizer.getMessage("jsp.error.single.line.number",
-                    args));
-            buf.append("\n");
-            buf.append(details[i].getErrorMessage());
             if (details[i].getJspBeginLineNumber() >= 0) {
+                args = new Object[] {
+                        new Integer(details[i].getJspBeginLineNumber()), 
+                        details[i].getJspFileName() };
+                buf.append("\n\n");
+                buf.append(Localizer.getMessage("jsp.error.single.line.number",
+                        args));
+                buf.append("\n");
+                buf.append(details[i].getErrorMessage());
                 buf.append("\n");
                 buf.append(details[i].getJspExtract());
+            } else {
+                args = new Object[] {
+                        new Integer(details[i].getJavaLineNumber()) };
+                buf.append("\n\n");
+                buf.append(Localizer.getMessage("jsp.error.java.line.number",
+                        args));
+                buf.append("\n");
+                buf.append(details[i].getErrorMessage());
             }
         }
         buf.append("\n\nStacktrace:");
