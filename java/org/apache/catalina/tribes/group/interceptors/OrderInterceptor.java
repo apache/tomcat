e8fbf5b95404860884093826586fcadc66aed0fe
Filip Hanik <fhanik@apache.org>
2007-05-21 09:08:02 +0000

    minor change
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc6.0.x/trunk@540076 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/tribes/group/interceptors/OrderInterceptor.java
+++ java/org/apache/catalina/tribes/group/interceptors/OrderInterceptor.java
@@ -51,7 +51,7 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
  * 
  * 
  * @author Filip Hanik
- * @version 1.0
+ * @version 1.1
  */
 public class OrderInterceptor extends ChannelInterceptorBase {
     private HashMap outcounter = new HashMap();
