d957848b7bd22037f0689f9059ed4e3f6815e44b
Remy Maucherat <remm@apache.org>
2007-06-18 15:54:54 +0000

    - Fix API. Submitted by Bill Burke.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc6.0.x/trunk@548385 13f79535-47bb-0310-9956-ffa450edef68

--- java/javax/persistence/PersistenceContextType.java
+++ java/javax/persistence/PersistenceContextType.java
@@ -18,16 +18,7 @@
 
 package javax.persistence;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
-@Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD})
-@Retention(RetentionPolicy.RUNTIME)
-
-public @interface PersistenceContext {
-   String name() default "";
-   String unitName() default "";
-   PersistenceContextType type() default PersistenceContextType.TRANSACTION;
+public enum PersistenceContextType {
+    TRANSACTION,
+    EXTENDED
 }
