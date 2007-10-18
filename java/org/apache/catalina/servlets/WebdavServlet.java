901292cf9d7d8225f8a3b96c7583e2bd8b41772d
Mark Emlyn David Thomas <markt@apache.org>
2007-10-18 11:44:53 +0000

    Fix CVE-2007-5461.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc6.0.x/trunk@585934 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/servlets/WebdavServlet.java
+++ java/org/apache/catalina/servlets/WebdavServlet.java
@@ -243,6 +243,7 @@ public class WebdavServlet
         try {
             documentBuilderFactory = DocumentBuilderFactory.newInstance();
             documentBuilderFactory.setNamespaceAware(true);
+            documentBuilderFactory.setExpandEntityReferences(false);
             documentBuilder = documentBuilderFactory.newDocumentBuilder();
         } catch(ParserConfigurationException e) {
             throw new ServletException
