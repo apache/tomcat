c1d2bd3bfb89fbd3e5250a5e5b6be2364fc2be4c
Mark Thomas <markt@apache.org>
2008-01-07 23:15:59 +0000

    Fix bug 43743. Correctly handle nest tag files packaged in a jar.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@609803 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/jasper/compiler/Parser.java
+++ java/org/apache/jasper/compiler/Parser.java
@@ -424,6 +424,13 @@ class Parser implements TagConstants {
                         if (ctxt.getOptions().isCaching()) {
                             ctxt.getOptions().getCache().put(uri, impl);
                         }
+                    } else {
+                        // Current compilation context needs location of cached
+                        // tag files
+                        for (TagFileInfo info : impl.getTagFiles()) {
+                            ctxt.setTagFileJarUrl(info.getPath(),
+                                    ctxt.getTagFileJarUrl());
+                        }
                     }
                     pageInfo.addTaglib(uri, impl);
                 }
