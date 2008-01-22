a549bc92a1d9f8527f42c30979f72604304352cd
Filip Hanik <fhanik@apache.org>
2008-01-22 17:33:32 +0000

    add the ability to set timeout during any EVENT, not just begin
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@614268 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http11/Http11NioProcessor.java
+++ java/org/apache/coyote/http11/Http11NioProcessor.java
@@ -751,7 +751,10 @@ public class Http11NioProcessor implements ActionHook {
                 NioEndpoint.KeyAttachment attach = (NioEndpoint.KeyAttachment)socket.getAttachment(false);
                 if (attach != null) {
                     attach.setComet(comet);
-                    if (!comet) {
+                    if (comet) {
+                        Integer comettimeout = (Integer) request.getAttribute("org.apache.tomcat.comet.timeout");
+                        if (comettimeout != null) attach.setTimeout(comettimeout.longValue());
+                    } else {
                         //reset the timeout
                         attach.setTimeout(endpoint.getSocketProperties().getSoTimeout());
                     }
