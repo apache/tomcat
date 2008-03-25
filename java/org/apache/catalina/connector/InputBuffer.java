148814be312c9410ccfdb4a74d0b10db55e309ab
Mark Thomas <markt@apache.org>
2008-03-25 22:21:57 +0000

    Apply the patch for BZ44494 that was applied to 6.0.x
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@641051 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/connector/InputBuffer.java
+++ java/org/apache/catalina/connector/InputBuffer.java
@@ -355,7 +355,7 @@ public class InputBuffer extends Reader
         }
 
         state = CHAR_STATE;
-        conv.convert(bb, cb, len);
+        conv.convert(bb, cb, bb.getLength());
         bb.setOffset(bb.getEnd());
 
         return cb.getLength();
