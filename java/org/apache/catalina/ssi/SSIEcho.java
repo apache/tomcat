9fb48082f5066ac1fb50d19db9e58da436cf694a
Mark Thomas <markt@apache.org>
2008-04-11 20:07:26 +0000

    Fix bug 44392. HTML entities not resolved in SSI.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@647289 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/ssi/SSIEcho.java
+++ java/org/apache/catalina/ssi/SSIEcho.java
@@ -37,20 +37,14 @@ public class SSIEcho implements SSICommand {
      */
     public long process(SSIMediator ssiMediator, String commandName,
             String[] paramNames, String[] paramValues, PrintWriter writer) {
-    	long lastModified = 0;
         String encoding = DEFAULT_ENCODING;
+        String originalValue = null;
         String errorMessage = ssiMediator.getConfigErrMsg();
         for (int i = 0; i < paramNames.length; i++) {
             String paramName = paramNames[i];
             String paramValue = paramValues[i];
             if (paramName.equalsIgnoreCase("var")) {
-                String variableValue = ssiMediator.getVariableValue(
-                        paramValue, encoding);
-                if (variableValue == null) {
-                    variableValue = MISSING_VARIABLE_VALUE;
-                }
-                writer.write(variableValue);
-                lastModified = System.currentTimeMillis();
+                originalValue = paramValue;
             } else if (paramName.equalsIgnoreCase("encoding")) {
                 if (isValidEncoding(paramValue)) {
                     encoding = paramValue;
@@ -63,7 +57,13 @@ public class SSIEcho implements SSICommand {
                 writer.write(errorMessage);
             }
         }
-        return lastModified;
+        String variableValue = ssiMediator.getVariableValue(
+                originalValue, encoding);
+        if (variableValue == null) {
+            variableValue = MISSING_VARIABLE_VALUE;
+        }
+        writer.write(variableValue);
+        return System.currentTimeMillis();
     }
 
 
