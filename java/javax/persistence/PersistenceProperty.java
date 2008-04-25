1faa0f4c9e34919eb2f7244bd54e4f5367207cc0
Mark Emlyn David Thomas <markt@apache.org>
2008-04-25 22:55:38 +0000

    Fix bug https://issues.apache.org/bugzilla/show_bug.cgi?id=43425 Annotations not spec compliant. Patch provided by Dain Sundstrom.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@651732 13f79535-47bb-0310-9956-ffa450edef68

--- java/javax/persistence/PersistenceProperty.java
+++ java/javax/persistence/PersistenceProperty.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,21 +15,16 @@
  * limitations under the License.
  */
 
+package javax.persistence;
 
-package javax.ejb;
-
-import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 
-@Target({ElementType.METHOD, ElementType.TYPE, ElementType.FIELD})
+@Target({})
 @Retention(RetentionPolicy.RUNTIME)
 
-public @interface EJB {
-   String name() default "";
-   String description() default "";
-   Class beanInterface() default java.lang.Object.class;
-   String beanName() default "";
-   String mappedName() default "";
+public @interface PersistenceProperty {
+   String name();
+   String value();
 }
