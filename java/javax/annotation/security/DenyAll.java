c1f4be0adac12038f8693eb5a94c31bc228ba0ec
Mark Thomas <markt@apache.org>
2008-04-25 22:55:38 +0000

    Fix bug https://issues.apache.org/bugzilla/show_bug.cgi?id=43425 Annotations not spec compliant. Patch provided by Dain Sundstrom.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@651732 13f79535-47bb-0310-9956-ffa450edef68

--- java/javax/annotation/security/DenyAll.java
+++ java/javax/annotation/security/DenyAll.java
@@ -23,7 +23,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.TYPE, ElementType.METHOD})
+@Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 
 public @interface DenyAll {
