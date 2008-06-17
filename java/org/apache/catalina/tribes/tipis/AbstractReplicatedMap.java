c1e2cd12a1b9dcc2f33238c38f9351a1888a8204
Mark Emlyn David Thomas <markt@apache.org>
2008-06-17 22:04:50 +0000

    Apply Filip's better patch for https://issues.apache.org/bugzilla/show_bug.cgi?id=45212 Map.entrySet() should return entries, not values
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@668849 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/tribes/tipis/AbstractReplicatedMap.java
+++ java/org/apache/catalina/tribes/tipis/AbstractReplicatedMap.java
@@ -1017,7 +1017,9 @@ public abstract class AbstractReplicatedMap extends ConcurrentHashMap implements
                 Map.Entry e = (Map.Entry)i.next();
                 Object key = e.getKey();
                 MapEntry entry = (MapEntry)super.get(key);
-                if ( entry != null && entry.isPrimary() ) set.add(entry.getValue());
+                if ( entry != null && entry.isPrimary() ) {
+                    set.add(new MapEntry(key, entry.getValue()));
+                }
             }
             return Collections.unmodifiableSet(set);
         }
