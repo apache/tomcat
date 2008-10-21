7283b710c5215a8150a8553cfd3931f50dea9749
Filip Hanik <fhanik@apache.org>
2008-10-21 18:07:04 +0000

    work on the bayeux samples
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@706696 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/bayeux/BayeuxServlet.java
+++ java/org/apache/tomcat/bayeux/BayeuxServlet.java
@@ -92,7 +92,7 @@ public class BayeuxServlet implements CometProcessor {
     
     protected int getReconnectInterval() {
         String rs = servletConfig.getInitParameter("reconnectInterval");
-        int rct = 5000; //5 seconds
+        int rct = 1000; //1 seconds
         try {
             rct = Integer.parseInt(rs);
         }catch (NumberFormatException nfe) {
