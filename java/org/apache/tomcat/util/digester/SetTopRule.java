c46da90a3a12ad9046c910c8ddf98500c1557ec5
Mark Emlyn David Thomas <markt@apache.org>
2008-11-19 00:39:04 +0000

    Remove deprecated methods. (In a few places public methods become private) In the few places they are used, replace the call to the deprecated method with an appropriate alternative. Should be no functional change.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@718819 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/digester/SetTopRule.java
+++ java/org/apache/tomcat/util/digester/SetTopRule.java
@@ -44,44 +44,6 @@ public class SetTopRule extends Rule {
      * "set parent" method's argument type is assumed to be the class of the
      * parent object.
      *
-     * @param digester The associated Digester
-     * @param methodName Method name of the "set parent" method to call
-     *
-     * @deprecated The digester instance is now set in the {@link Digester#addRule} method. 
-     * Use {@link #SetTopRule(String methodName)} instead.
-     */
-    public SetTopRule(Digester digester, String methodName) {
-
-        this(methodName);
-
-    }
-
-
-    /**
-     * Construct a "set parent" rule with the specified method name.
-     *
-     * @param digester The associated Digester
-     * @param methodName Method name of the "set parent" method to call
-     * @param paramType Java class of the "set parent" method's argument
-     *  (if you wish to use a primitive type, specify the corresonding
-     *  Java wrapper class instead, such as <code>java.lang.Boolean</code>
-     *  for a <code>boolean</code> parameter)
-     *
-     * @deprecated The digester instance is now set in the {@link Digester#addRule} method. 
-     * Use {@link #SetTopRule(String methodName, String paramType)} instead.
-     */
-    public SetTopRule(Digester digester, String methodName,
-                      String paramType) {
-
-        this(methodName, paramType);
-
-    }
-
-    /**
-     * Construct a "set parent" rule with the specified method name.  The
-     * "set parent" method's argument type is assumed to be the class of the
-     * parent object.
-     *
      * @param methodName Method name of the "set parent" method to call
      */
     public SetTopRule(String methodName) {
