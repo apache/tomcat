0c3319af068a88c79a4cd8e41afa2acb94bbed0a
Mark Emlyn David Thomas <markt@apache.org>
2008-11-21 15:46:12 +0000

    Make size of all threads pools dynamically configurable
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@719602 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/net/JIoEndpoint.java
+++ java/org/apache/tomcat/util/net/JIoEndpoint.java
@@ -174,7 +174,14 @@ public class JIoEndpoint {
      * Maximum amount of worker threads.
      */
     protected int maxThreads = 200;
-    public void setMaxThreads(int maxThreads) { this.maxThreads = maxThreads; }
+    public void setMaxThreads(int maxThreads) {
+        this.maxThreads = maxThreads;
+        if (running) {
+            synchronized(workers) {
+                workers.resize(maxThreads);
+            }
+        }
+    }
     public int getMaxThreads() { return maxThreads; }
 
 
@@ -777,12 +784,18 @@ public class JIoEndpoint {
         }
         
         /** 
-         * Put the object into the queue.
+         * Put the object into the queue. If the queue is full (for example if
+         * the queue has been reduced in size) the object will be dropped.
          * 
-         * @param   object      the object to be appended to the queue (first element). 
+         * @param   object  the object to be appended to the queue (first
+         *                  element).
          */
         public void push(Worker worker) {
-            workers[end++] = worker;
+            if (end < workers.length) {
+                workers[end++] = worker;
+            } else {
+                curThreads--;
+            }
         }
         
         /**
@@ -816,6 +829,22 @@ public class JIoEndpoint {
          */
         public int size() {
             return (end);
+        }
+        
+        /**
+         * Resize the queue. If there are too many objects in the queue for the
+         * new size, drop the excess.
+         * 
+         * @param newSize
+         */
+        public void resize(int newSize) {
+            Worker[] newWorkers = new Worker[newSize];
+            int len = workers.length;
+            if (newSize < len) {
+                len = newSize;
+            }
+            System.arraycopy(workers, 0, newWorkers, 0, len);
+            workers = newWorkers;
         }
     }
 
