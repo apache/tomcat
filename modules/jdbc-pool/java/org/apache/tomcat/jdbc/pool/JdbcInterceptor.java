868d19c8dbddce5709e0572f51e086241ddee585
Filip Hanik <fhanik@apache.org>
2008-11-24 20:43:15 +0000

    fix bug
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@720284 13f79535-47bb-0310-9956-ffa450edef68

--- modules/jdbc-pool/java/org/apache/tomcat/jdbc/pool/JdbcInterceptor.java
+++ modules/jdbc-pool/java/org/apache/tomcat/jdbc/pool/JdbcInterceptor.java
@@ -88,7 +88,7 @@ public abstract class JdbcInterceptor implements InvocationHandler {
     }
     
     public boolean useEquals() {
-        return getUseEquals();
+        return useEquals();
     }
     
     public void setUseEquals(boolean useEquals) {
