c5200318b7f030d658f22b6c2f2ab6fd565d26a3
Mark Thomas <markt@apache.org>
2009-01-15 15:34:52 +0000

    Remove 3 of the essentially 4 duplicate normalise implementations. Minor tweaks where required to use the new single implementation. Remove unnecessary normalisation calls. This includes the fix for CVE-2008-5515.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@734734 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/core/ApplicationHttpRequest.java
+++ java/org/apache/catalina/core/ApplicationHttpRequest.java
@@ -318,10 +318,9 @@ class ApplicationHttpRequest extends HttpServletRequestWrapper {
         int pos = requestPath.lastIndexOf('/');
         String relative = null;
         if (pos >= 0) {
-            relative = RequestUtil.normalize
-                (requestPath.substring(0, pos + 1) + path);
+            relative = requestPath.substring(0, pos + 1) + path;
         } else {
-            relative = RequestUtil.normalize(requestPath + path);
+            relative = requestPath + path;
         }
 
         return (context.getServletContext().getRequestDispatcher(relative));
--- java/org/apache/catalina/util/RequestUtil.java
+++ java/org/apache/catalina/util/RequestUtil.java
@@ -93,12 +93,28 @@ public final class RequestUtil {
      * @param path Relative path to be normalized
      */
     public static String normalize(String path) {
+        return normalize(path, true);
+    }
+
+    /**
+     * Normalize a relative URI path that may have relative values ("/./",
+     * "/../", and so on ) it it.  <strong>WARNING</strong> - This method is
+     * useful only for normalizing application-generated paths.  It does not
+     * try to perform security checks for malicious input.
+     *
+     * @param path Relative path to be normalized
+     * @param replaceBackSlash Should '\\' be replaced with '/'
+     */
+    public static String normalize(String path, boolean replaceBackSlash) {
 
         if (path == null)
             return null;
 
         // Create a place for the normalized path
         String normalized = path;
+
+        if (replaceBackSlash && normalized.indexOf('\\') >= 0)
+            normalized = normalized.replace('\\', '/');
 
         if (normalized.equals("/."))
             return "/";
