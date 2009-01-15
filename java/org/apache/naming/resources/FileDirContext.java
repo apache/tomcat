bbd861485981ab0732f7746a4e173e8b524bb691
Mark Emlyn David Thomas <markt@apache.org>
2009-01-15 15:34:52 +0000

    Remove 3 of the essentially 4 duplicate normalise implementations. Minor tweaks where required to use the new single implementation. Remove unnecessary normalisation calls. This includes the fix for CVE-2008-5515.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@734734 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/naming/resources/FileDirContext.java
+++ java/org/apache/naming/resources/FileDirContext.java
@@ -40,6 +40,7 @@ import javax.naming.directory.ModificationItem;
 import javax.naming.directory.SearchControls;
 import javax.naming.directory.SearchResult;
 
+import org.apache.catalina.util.RequestUtil;
 import org.apache.naming.NamingContextBindingsEnumeration;
 import org.apache.naming.NamingContextEnumeration;
 import org.apache.naming.NamingEntry;
@@ -768,49 +769,9 @@ public class FileDirContext extends BaseDirContext {
      */
     protected String normalize(String path) {
 
-    String normalized = path;
-
-    // Normalize the slashes and add leading slash if necessary
-    if (File.separatorChar == '\\' && normalized.indexOf('\\') >= 0)
-        normalized = normalized.replace('\\', '/');
-    if (!normalized.startsWith("/"))
-        normalized = "/" + normalized;
-
-    // Resolve occurrences of "//" in the normalized path
-    while (true) {
-        int index = normalized.indexOf("//");
-        if (index < 0)
-        break;
-        normalized = normalized.substring(0, index) +
-        normalized.substring(index + 1);
-    }
-
-    // Resolve occurrences of "/./" in the normalized path
-    while (true) {
-        int index = normalized.indexOf("/./");
-        if (index < 0)
-        break;
-        normalized = normalized.substring(0, index) +
-        normalized.substring(index + 2);
-    }
-
-    // Resolve occurrences of "/../" in the normalized path
-    while (true) {
-        int index = normalized.indexOf("/../");
-        if (index < 0)
-        break;
-        if (index == 0)
-        return (null);  // Trying to go outside our context
-        int index2 = normalized.lastIndexOf('/', index - 1);
-        normalized = normalized.substring(0, index2) +
-        normalized.substring(index + 3);
-    }
-
-    // Return the normalized path that we have completed
-    return (normalized);
+        return RequestUtil.normalize(path, File.separatorChar == '\\');
 
     }
-
 
     /**
      * Return a File object representing the specified normalized
