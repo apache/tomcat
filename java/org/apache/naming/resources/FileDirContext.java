af77b54d2e6a9174eca2a969e41112e81931e07e
Mark Emlyn David Thomas <markt@apache.org>
2009-02-09 20:00:01 +0000

    Only perform validation test when allowLinking is enabled
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@742711 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/naming/resources/FileDirContext.java
+++ java/org/apache/naming/resources/FileDirContext.java
@@ -846,7 +846,7 @@ public class FileDirContext extends BaseDirContext {
      * List the resources which are members of a collection.
      *
      * @param file Collection
-     * @return Vector containg NamingEntry objects
+     * @return Vector containing NamingEntry objects
      */
     protected ArrayList<NamingEntry> list(File file) {
 
@@ -867,8 +867,11 @@ public class FileDirContext extends BaseDirContext {
 
         for (int i = 0; i < names.length; i++) {
 
-            File currentFile = validate(new File(file, names[i]));
-            if (currentFile == null) continue;
+            File currentFile = new File(file, names[i]);
+            if (allowLinking) {
+                currentFile = validate(currentFile);
+                if (currentFile == null) continue;
+            }
             
             Object object = null;
             if (currentFile.isDirectory()) {
