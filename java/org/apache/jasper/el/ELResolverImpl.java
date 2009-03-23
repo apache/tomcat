3c0a44fbd5b9c7e421e0bd1118f88017d6014590
Mark Emlyn David Thomas <markt@apache.org>
2009-03-23 07:36:38 +0000

    Remove the Catalina dependency from Jasper.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@757335 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/jasper/el/ELResolverImpl.java
+++ java/org/apache/jasper/el/ELResolverImpl.java
@@ -32,7 +32,7 @@ import javax.el.PropertyNotWritableException;
 import javax.el.ResourceBundleELResolver;
 import javax.servlet.jsp.el.VariableResolver;
 
-import org.apache.catalina.Globals;
+import org.apache.jasper.Constants;
 
 public final class ELResolverImpl extends ELResolver {
 	/** @deprecated - Use getDefaultResolver(). Needs to be made private */
@@ -146,7 +146,7 @@ public final class ELResolverImpl extends ELResolver {
 	}
 
 	public static ELResolver getDefaultResolver() {
-	    if (Globals.IS_SECURITY_ENABLED) {
+	    if (Constants.IS_SECURITY_ENABLED) {
 	        ELResolver defaultResolver = new CompositeELResolver();
 	        ((CompositeELResolver) defaultResolver).add(new MapELResolver());
 	        ((CompositeELResolver) defaultResolver).add(new ResourceBundleELResolver());
