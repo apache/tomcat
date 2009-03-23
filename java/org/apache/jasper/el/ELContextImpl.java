0796e062a3b9989364d945f2249b31e95833a933
Mark Emlyn David Thomas <markt@apache.org>
2009-03-23 07:36:38 +0000

    Remove the Catalina dependency from Jasper.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@757335 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/jasper/el/ELContextImpl.java
+++ java/org/apache/jasper/el/ELContextImpl.java
@@ -26,7 +26,7 @@ import javax.el.FunctionMapper;
 import javax.el.ValueExpression;
 import javax.el.VariableMapper;
 
-import org.apache.catalina.Globals;
+import org.apache.jasper.Constants;
 
 /**
  * Implementation of ELContext
@@ -69,7 +69,7 @@ public final class ELContextImpl extends ELContext {
 
     public ELContextImpl() {
         this(ELResolverImpl.getDefaultResolver());
-        if (Globals.IS_SECURITY_ENABLED) {
+        if (Constants.IS_SECURITY_ENABLED) {
             functionMapper = new FunctionMapper() {
                 public Method resolveFunction(String prefix, String localName) {
                     return null;
