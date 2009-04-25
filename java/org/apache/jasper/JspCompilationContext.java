1860022ddb6665c11b621a974018f4a5c168a231
Mark Thomas <markt@apache.org>
2009-04-25 23:25:36 +0000

    Reduce dependency on URLClassloader. Allow custom Loader elements to use ClassLoader rather than URLClassLoader.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@768624 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/jasper/JspCompilationContext.java
+++ java/org/apache/jasper/JspCompilationContext.java
@@ -77,7 +77,7 @@ public class JspCompilationContext {
     protected String baseURI;
     protected String outputDir;
     protected ServletContext context;
-    protected URLClassLoader loader;
+    protected ClassLoader loader;
 
     protected JspRuntimeContext rctxt;
 
@@ -170,7 +170,7 @@ public class JspCompilationContext {
         return rctxt.getParentClassLoader();
     }
 
-    public void setClassLoader(URLClassLoader loader) {
+    public void setClassLoader(ClassLoader loader) {
         this.loader = loader;
     }
 
