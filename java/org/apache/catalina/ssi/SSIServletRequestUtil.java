ed41ca64cc52a4b9206954c75696e5b705a0da63
Mark Emlyn David Thomas <markt@apache.org>
2009-06-08 20:25:34 +0000

    Remove a deprecated method
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@782770 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/ssi/SSIServletRequestUtil.java
+++ java/org/apache/catalina/ssi/SSIServletRequestUtil.java
@@ -50,22 +50,4 @@ public class SSIServletRequestUtil {
         return RequestUtil.normalize(result);
     }
 
-
-    /**
-     * Return a context-relative path, beginning with a "/", that represents
-     * the canonical version of the specified path after ".." and "." elements
-     * are resolved out. If the specified path attempts to go outside the
-     * boundaries of the current context (i.e. too many ".." path elements are
-     * present), return <code>null</code> instead. This normalize should be
-     * the same as DefaultServlet.normalize, which is almost the same ( see
-     * source code below ) as RequestUtil.normalize. Do we need all this
-     * duplication?
-     * 
-     * @param path
-     *            Path to be normalized
-     * @deprecated
-     */
-    public static String normalize(String path) {
-        return RequestUtil.normalize(path);
-    }
 }
