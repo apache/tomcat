adefbc7a34d3bc43562a5bf75f64df3e65a57dd2
Mark Emlyn David Thomas <markt@apache.org>
2009-07-01 22:13:33 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=37984 Strip {MD5} as well as {SHA} from digested passwords
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@790405 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/realm/JNDIRealm.java
+++ java/org/apache/catalina/realm/JNDIRealm.java
@@ -1419,10 +1419,10 @@ public class JNDIRealm extends RealmBase {
 
         boolean validated = false;
         if (hasMessageDigest()) {
-            // iPlanet support if the values starts with {SHA1}
+            // Some directories prefix the password with the hash type
             // The string is in a format compatible with Base64.encode not
             // the Hex encoding of the parent class.
-            if (password.startsWith("{SHA}")) {
+            if (password.startsWith("{MD5}") || password.startsWith("{SHA}")) {
                 /* sync since super.digest() does this same thing */
                 synchronized (this) {
                     password = password.substring(5);
