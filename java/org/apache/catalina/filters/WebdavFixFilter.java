17a352b572f21ae08e44df32a497fea3ad16b8b2
Mark Emlyn David Thomas <markt@apache.org>
2009-07-14 17:18:21 +0000

    Implements, doesn't override
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@793979 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/filters/WebdavFixFilter.java
+++ java/org/apache/catalina/filters/WebdavFixFilter.java
@@ -74,19 +74,18 @@ public class WebdavFixFilter implements Filter {
     private static final String UA_MINIDIR_5_2_3790 =
         "Microsoft-WebDAV-MiniRedir/5.2.3790";
 
-    @Override
     public void init(FilterConfig filterConfig) throws ServletException {
+        // NOOP
     }
 
-	@Override
 	public void destroy() {
+	    // NOOP
 	}
 
     /**
      * Check for the broken MS WebDAV client and if detected issue a re-direct
      * that hopefully will cause the non-broken client to be used.
      */
-	@Override
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
 	    if (!(request instanceof HttpServletRequest) ||
