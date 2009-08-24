428ba8e8693647e401ca6ea7806f006798f89757
Mark Emlyn David Thomas <markt@apache.org>
2009-08-24 08:22:21 +0000

    Extract the SimpleHttpClient in to a separate class and move it to a more logical package since it will be reused. Don't override method unnecessarily. Add test case for bug 46243
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@807113 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/catalina/connector/TestRequest.java
+++ test/org/apache/catalina/connector/TestRequest.java
@@ -17,20 +17,9 @@
 
 package org.apache.catalina.connector;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.io.Reader;
-import java.io.Writer;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
 import java.util.Enumeration;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -38,6 +27,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.SimpleHttpClient;
 import org.apache.catalina.startup.TestTomcatBase;
 import org.apache.catalina.startup.Tomcat;
 
@@ -45,15 +35,6 @@ import org.apache.catalina.startup.Tomcat;
  * Test case for {@link Request}. 
  */
 public class TestRequest extends TestTomcatBase {
-    @Override
-    public void setUp() throws Exception {
-        super.setUp();
-    }
-
-    @Override
-    public void tearDown() throws Exception {
-        super.tearDown();
-    }
 
     /**
      * Test case for https://issues.apache.org/bugzilla/show_bug.cgi?id=37794
@@ -209,132 +190,5 @@ public class TestRequest extends TestTomcatBase {
             return true;
         }
         
-    }
-
-    /**
-     * Simple client for unit testing. It isn't robust, it isn't secure and
-     * should not be used as the basis for production code. Its only purpose
-     * is to do the bare minimum for the unit tests. 
-     */
-    private abstract static class SimpleHttpClient {
-        public static final String TEMP_DIR =
-            System.getProperty("java.io.tmpdir");
-        
-        public static final String CRLF = "\r\n";
-
-        public static final String OK_200 = "HTTP/1.1 200";
-        public static final String FAIL_500 = "HTTP/1.1 500";
-        
-        private Socket socket;
-        private Writer writer;
-        private BufferedReader reader;
-        private int port = 8080;
-        
-        private String[] request;
-        private int requestPause = 1000;
-        
-        private String responseLine;
-        private List<String> responseHeaders = new ArrayList<String>();
-        private String responseBody;
-
-        public void setPort(int thePort) {
-            port = thePort;
-        }
-
-        public void setRequest(String[] theRequest) {
-            request = theRequest;
-        }
-        
-        public void setRequestPause(int theRequestPause) {
-            requestPause = theRequestPause;
-        }
-
-        public String getResponseLine() {
-            return responseLine;
-        }
-
-        public List<String> getResponseHeaders() {
-            return responseHeaders;
-        }
-
-        public String getResponseBody() {
-            return responseBody;
-        }
-
-        public void connect() throws UnknownHostException, IOException {
-            socket = new Socket("localhost", port);
-            OutputStream os = socket.getOutputStream();
-            writer = new OutputStreamWriter(os);
-            InputStream is = socket.getInputStream();
-            Reader r = new InputStreamReader(is);
-            reader = new BufferedReader(r);
-        }
-        
-        public void processRequest() throws IOException, InterruptedException {
-            // Send the request
-            boolean first = true;
-            for (String requestPart : request) {
-                if (first) {
-                    first = false;
-                } else {
-                    Thread.sleep(requestPause);
-                }
-                writer.write(requestPart);
-                writer.flush();
-            }
-
-            // Read the response
-            responseLine = readLine();
-            
-            // Put the headers into the map
-            String line = readLine();
-            while (line.length() > 0) {
-                responseHeaders.add(line);
-                line = readLine();
-            }
-            
-            // Read the body, if any
-            StringBuilder builder = new StringBuilder();
-            line = readLine();
-            while (line != null && line.length() > 0) {
-                builder.append(line);
-                line = readLine();
-            }
-            responseBody = builder.toString();
-
-        }
-
-        public String readLine() throws IOException {
-            return reader.readLine();
-        }
-        
-        public void disconnect() throws IOException {
-            writer.close();
-            reader.close();
-            socket.close();
-        }
-        
-        public void reset() {
-            socket = null;
-            writer = null;
-            reader = null;
-            
-            request = null;
-            requestPause = 1000;
-            
-            responseLine = null;
-            responseHeaders = new ArrayList<String>();
-            responseBody = null;
-        }
-        
-        public boolean isResponse200() {
-            return getResponseLine().startsWith(OK_200);
-        }
-        
-        public boolean isResponse500() {
-            return getResponseLine().startsWith(FAIL_500);
-        }
-
-        public abstract boolean isResponseBodyOK();
     }
 }
