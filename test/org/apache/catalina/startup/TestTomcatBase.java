67b2ae5126571f3762ec04bbd9c9d8625cf092fd
Mark Emlyn David Thomas <markt@apache.org>
2009-08-24 08:20:12 +0000

    Add a simple Hello World Servlet that can be used for testing.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@807112 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/catalina/startup/TestTomcatBase.java
+++ test/org/apache/catalina/startup/TestTomcatBase.java
@@ -17,6 +17,13 @@
 package org.apache.catalina.startup;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import junit.framework.TestCase;
 
@@ -62,4 +69,18 @@ public class TestTomcatBase extends TestCase {
         ExpandWar.delete(tempDir);
     }
     
+    /**
+     * Simple Hello World servlet for use by test cases
+     */
+    public static final class HelloWorldServlet extends HttpServlet {
+
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+                throws ServletException, IOException {
+            PrintWriter out = resp.getWriter();
+            out.print("<html><body><p>Hello World</p></body></html>");
+        }
+    }
 }
