f98665aa0acb234376eaf623887cfb795b31c07d
Mark Thomas <markt@apache.org>
2009-10-21 20:53:47 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=48009 Add a check in case the file is in the middle of being renamed. It isn't perfect (it can't be) but this should cover most cases
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@828210 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/startup/HostConfig.java
+++ java/org/apache/catalina/startup/HostConfig.java
@@ -1103,6 +1103,17 @@ public class HostConfig
                     return;
                 }
             } else {
+                // There is a chance the the resource was only missing
+                // temporarily eg renamed during a text editor save
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e1) {
+                    // Ignore
+                }
+                // Recheck the resource to see if it was really deleted
+                if (resource.exists()) {
+                    continue;
+                }
                 long lastModified =
                     app.redeployResources.get(resources[i]).longValue();
                 if (lastModified == 0L) {
