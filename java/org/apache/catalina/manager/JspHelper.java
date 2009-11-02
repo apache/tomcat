a9e2e8d42abd76fcabcb1a1d553f8d5aab51783a
Mark Emlyn David Thomas <markt@apache.org>
2009-11-02 18:32:07 +0000

    StringBuffer -> StringBuilder for o.a.c.manager
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@832013 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/manager/JspHelper.java
+++ java/org/apache/catalina/manager/JspHelper.java
@@ -104,7 +104,7 @@ public class JspHelper {
     }
 
     public static String secondsToTimeString(long in_seconds) {
-        StringBuffer buff = new StringBuffer(9);
+        StringBuilder buff = new StringBuilder(9);
         long rest = in_seconds;
         long hour = rest / 3600;
         rest = rest % 3600;
@@ -172,16 +172,16 @@ public class JspHelper {
         int start = 0;
         int length = buffer.length();
         char[] arrayBuffer = buffer.toCharArray();
-        StringBuffer escapedBuffer = null;
+        StringBuilder escapedBuffer = null;
 
         for (int i = 0; i < length; i++) {
             char c = arrayBuffer[i];
             if (c <= HIGHEST_SPECIAL) {
                 char[] escaped = specialCharactersRepresentation[c];
                 if (escaped != null) {
-                    // create StringBuffer to hold escaped xml string
+                    // create StringBuilder to hold escaped xml string
                     if (start == 0) {
-                        escapedBuffer = new StringBuffer(length + 5);
+                        escapedBuffer = new StringBuilder(length + 5);
                     }
                     // add unescaped portion
                     if (start < i) {
