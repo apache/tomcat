df29e6b39ff34d695c1ce7fcaada14c75d2cb0c4
Timothy A. Funk <funkman@apache.org>
2009-11-05 18:46:43 +0000

    https://issues.apache.org/bugzilla/show_bug.cgi?id=47495
    If server.xml doesnt have read permission - let the user know
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@833121 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/startup/Catalina.java
+++ java/org/apache/catalina/startup/Catalina.java
@@ -516,6 +516,9 @@ public class Catalina extends Embedded {
 
         if ((inputStream == null) && (file != null)) {
             log.warn("Can't load server.xml from " + file.getAbsolutePath());
+            if (file.exists() && !file.canRead()) {
+                log.warn("Permissions incorrect, read permission is not allowed on the file.");
+            }
             return;
         }
 
