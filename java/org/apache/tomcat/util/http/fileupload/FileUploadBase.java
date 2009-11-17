89acf3d820ca1fedbca8d195f777a5c2d76af498
Mark Emlyn David Thomas <markt@apache.org>
2009-11-17 21:19:44 +0000

    Remove unused imports
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@881532 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/http/fileupload/FileUploadBase.java
+++ java/org/apache/tomcat/util/http/fileupload/FileUploadBase.java
@@ -20,8 +20,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
