654c30ccc71622707f6e8cff2e1dc8a0b9cd4f89
Mark Emlyn David Thomas <markt@apache.org>
2009-11-17 22:47:39 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=48174 Use final
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@881591 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/jni/Address.java
+++ java/org/apache/tomcat/jni/Address.java
@@ -25,7 +25,7 @@ package org.apache.tomcat.jni;
 
 public class Address {
 
-    static public String APR_ANYADDR = "0.0.0.0";
+    static public final String APR_ANYADDR = "0.0.0.0";
     /**
      * Fill the Sockaddr class from apr_sockaddr_t
      * @param info Sockaddr class to fill
