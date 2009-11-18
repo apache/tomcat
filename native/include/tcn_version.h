ba6f61b1042b619608c359dd98971ab8b9b30d9c
Jean-Frederic Clere <jfclere@apache.org>
2009-11-18 13:15:52 +0000

    Preparing 1.1.18 release.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/native/branches/1.1.x@881760 13f79535-47bb-0310-9956-ffa450edef68

--- native/include/tcn_version.h
+++ native/include/tcn_version.h
@@ -75,7 +75,7 @@ extern "C" {
  *  This symbol is defined for internal, "development" copies of TCN. This
  *  symbol will be #undef'd for releases.
  */
-#define TCN_IS_DEV_VERSION      1
+#define TCN_IS_DEV_VERSION      0
 
 
 /** The formatted string of APU's version */
