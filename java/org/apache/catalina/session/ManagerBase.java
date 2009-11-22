10838ff9291c202940e61049bdd25ff368e651e3
Mark Emlyn David Thomas <markt@apache.org>
2009-11-22 19:39:33 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=47718 Correct file descriptor leak on context reload/stop Patch provided by George Sexton
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@883130 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/session/ManagerBase.java
+++ java/org/apache/catalina/session/ManagerBase.java
@@ -709,6 +709,15 @@ public abstract class ManagerBase implements Manager, MBeanRegistration {
     public void destroy() {
         if( oname != null )
             Registry.getRegistry(null, null).unregisterComponent(oname);
+        if (randomIS!=null) {
+            try {
+                randomIS.close();
+            } catch (IOException ioe) {
+                log.warn("Failed to close randomIS.");
+            }
+            randomIS=null;
+        }
+
         initialized=false;
         oname = null;
     }
