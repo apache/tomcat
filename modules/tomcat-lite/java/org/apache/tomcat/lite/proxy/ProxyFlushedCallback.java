c16ef7887894add1daefa2d274fb60698dbc4417
Costin Manolache <costin@apache.org>
2009-11-26 06:42:49 +0000

    Proxy service - CONNECT doesn't seem to work ( probably because it didn't have a test ), the rest works pretty well. Also includes a small socks server I used while testing. Almost all work is non-blocking and done in the selector thread.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@884413 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/tomcat-lite/java/org/apache/tomcat/lite/proxy/ProxyFlushedCallback.java
@@ -0,0 +1,25 @@
+/*
+ */
+package org.apache.tomcat.lite.proxy;
+
+import java.io.IOException;
+
+import org.apache.tomcat.lite.io.IOChannel;
+import org.apache.tomcat.lite.io.IOConnector;
+
+public final class ProxyFlushedCallback implements IOConnector.DataFlushedCallback {
+    IOChannel peerCh;
+    
+    public ProxyFlushedCallback(IOChannel ch2, IOChannel clientChannel2) {
+        peerCh = ch2;
+    }
+
+    @Override
+    public void handleFlushed(IOChannel ch) throws IOException {
+        if (ch.getOut().isClosedAndEmpty()) {
+            if (!peerCh.getOut().isAppendClosed()) {
+                peerCh.close();
+            }
+        }
+    }
+}
