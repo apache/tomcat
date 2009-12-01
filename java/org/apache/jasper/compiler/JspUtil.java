7a14f93281ed296510ef3d870cf6b2ffa398a867
Mark Emlyn David Thomas <markt@apache.org>
2009-12-01 22:52:55 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=47453 Handle void return types. Patch by Tim Funk.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@885991 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/jasper/compiler/JspUtil.java
+++ java/org/apache/jasper/compiler/JspUtil.java
@@ -338,6 +338,8 @@ public class JspUtil {
             c = float.class;
         else if ("double".equals(type))
             c = double.class;
+        else if ("void".equals(type))
+            c = void.class;
         else if (type.indexOf('[') < 0)
             c = loader.loadClass(type);
 
