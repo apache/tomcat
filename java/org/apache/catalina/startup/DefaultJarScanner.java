d6d1a22235208ff28e37465fe74ced6a0fb3a65c
Mark Emlyn David Thomas <markt@apache.org>
2009-12-04 10:14:00 +0000

    Fix typo
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@887143 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/startup/DefaultJarScanner.java
+++ java/org/apache/catalina/startup/DefaultJarScanner.java
@@ -37,7 +37,7 @@ import org.apache.tomcat.util.res.StringManager;
  * The default {@link JarScanner} implementation scans the WEB-INF/lib directory
  * followed by the provided classloader and then works up the classloader
  * hierarchy. This implementation is sufficient to meet the requirements of the
- * Servlet 3.0 specification as well is to provide a number of Tomcat specific
+ * Servlet 3.0 specification as well as to provide a number of Tomcat specific
  * extensions. The extensions are:
  * <ul>
  *   <li>Scanning the classloader hierarchy (enabled by default)</li>
