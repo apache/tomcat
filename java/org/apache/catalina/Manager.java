4ee1db9dde693e388a4a00b2570d9be6829b8f59
Mark Emlyn David Thomas <markt@apache.org>
2009-12-11 17:30:59 +0000

    Address session fixation by changing the session ID on authentication. This is enabled by default. This should be safe since this also happens when sessions migrate between nodes in a cluster. If an app can't handle a changing ID, then the feature can be disabled in the authenticator.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@889716 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/Manager.java
+++ java/org/apache/catalina/Manager.java
@@ -260,6 +260,15 @@ public interface Manager {
 
 
     /**
+     * Change the session ID of the current session to a new randomly generated
+     * session ID.
+     * 
+     * @param session   The session to change the session ID for
+     */
+    public void changeSessionId(Session session);
+    
+    
+    /**
      * Get a session from the recycled ones or create a new empty one.
      * The PersistentManager manager does not need to create session data
      * because it reads it from the Store.
