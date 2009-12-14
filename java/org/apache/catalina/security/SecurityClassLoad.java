8d1822a5330a07aa15fe319439361e57c995ca8e
Mark Emlyn David Thomas <markt@apache.org>
2009-12-14 14:51:12 +0000

    Pre-load a class required for apps to obtain the SSL key size when running under a security manager
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@890349 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/security/SecurityClassLoad.java
+++ java/org/apache/catalina/security/SecurityClassLoad.java
@@ -44,6 +44,7 @@ public final class SecurityClassLoad {
         loadJavaxPackage(loader);
         loadCoyotePackage(loader);        
         loadHttp11Package(loader);        
+        loadTomcatPackage(loader);
     }
     
     
@@ -209,5 +210,10 @@ public final class SecurityClassLoad {
              "Response$3");
     }
 
+    private final static void loadTomcatPackage(ClassLoader loader)
+        throws Exception {
+        String basePackage = "org.apache.tomcat.";
+        loader.loadClass(basePackage + "util.net.SSLSupport$CipherData");
+    }
 }
 
