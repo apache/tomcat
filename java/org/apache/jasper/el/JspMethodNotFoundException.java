95408d6469fb202eeacf9ed481a0e665847a202a
Mark Thomas <markt@apache.org>
2010-01-01 18:31:17 +0000

    Fix some trivial Eclipse warnings
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@895048 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/jasper/el/JspMethodNotFoundException.java
+++ java/org/apache/jasper/el/JspMethodNotFoundException.java
@@ -20,6 +20,8 @@ import javax.el.MethodNotFoundException;
 
 public class JspMethodNotFoundException extends MethodNotFoundException {
 
+    private static final long serialVersionUID = 1L;
+
     public JspMethodNotFoundException(String mark, MethodNotFoundException e) {
         super(mark + " " + e.getMessage(), e.getCause());
     }
