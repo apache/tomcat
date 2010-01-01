1c45f1c54cbb1493aaf87fd953c82341bbf15733
Mark Emlyn David Thomas <markt@apache.org>
2010-01-01 18:31:17 +0000

    Fix some trivial Eclipse warnings
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@895048 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/jasper/el/JspPropertyNotFoundException.java
+++ java/org/apache/jasper/el/JspPropertyNotFoundException.java
@@ -21,6 +21,8 @@ import javax.el.PropertyNotFoundException;
 public final class JspPropertyNotFoundException extends
         PropertyNotFoundException {
 
+    private static final long serialVersionUID = 1L;
+
     public JspPropertyNotFoundException(String mark, PropertyNotFoundException e) {
         super(mark + " " + e.getMessage(), e.getCause());
     }
