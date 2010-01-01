969cfd79f702efd1f4791f9c8332147e577886bf
Mark Emlyn David Thomas <markt@apache.org>
2010-01-01 18:31:17 +0000

    Fix some trivial Eclipse warnings
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@895048 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/jasper/el/JspPropertyNotWritableException.java
+++ java/org/apache/jasper/el/JspPropertyNotWritableException.java
@@ -21,6 +21,8 @@ import javax.el.PropertyNotWritableException;
 public class JspPropertyNotWritableException extends
         PropertyNotWritableException {
 
+    private static final long serialVersionUID = 1L;
+
     public JspPropertyNotWritableException(String mark, PropertyNotWritableException e) {
         super(mark + " " + e.getMessage(), e.getCause());
     }
