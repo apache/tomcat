0776e42c8b2d585d731e214c56b16016858aa01d
Mark Emlyn David Thomas <markt@apache.org>
2010-01-01 18:31:17 +0000

    Fix some trivial Eclipse warnings
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@895048 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/jasper/el/JspELException.java
+++ java/org/apache/jasper/el/JspELException.java
@@ -20,6 +20,8 @@ import javax.el.ELException;
 
 public class JspELException extends ELException {
 
+    private static final long serialVersionUID = 1L;
+
     public JspELException(String mark, ELException e) {
         super(mark + " " + e.getMessage(), e.getCause());
     }
