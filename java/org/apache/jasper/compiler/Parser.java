ea683cb28f19cf5895a7a2c2e459d2bc32a7503e
Mark Thomas <markt@apache.org>
2010-01-10 11:10:36 +0000

    Re-work EL attribute parsing. The underlying issue was complete independence of attribute and EL parsing. The attribute parser would generate the same result - ${1+1} - after parsing ${1+1} and \${+1} and the EL had no way to differentiate between the first (that should be treated as an expression) and the second (that should be treated as a literal). The attribute parser has been modified to output any literals that would be mi-interpreted by the EL parser as EL literals. ie \ is output as ${'\\'} or #{'\\'}, $ as ${'$'} or #{'$'} and # as ${'#'} or #{'#'}.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@897614 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/jasper/compiler/Parser.java
+++ java/org/apache/jasper/compiler/Parser.java
@@ -75,11 +75,6 @@ class Parser implements TagConstants {
     private static final String JAVAX_BODY_CONTENT_TEMPLATE_TEXT =
         "JAVAX_BODY_CONTENT_TEMPLATE_TEXT";
 
-    private static final boolean STRICT_QUOTE_ESCAPING = Boolean.valueOf(
-            System.getProperty(
-                    "org.apache.jasper.compiler.Parser.STRICT_QUOTE_ESCAPING",
-                    "true")).booleanValue();
-
     /**
      * The constructor
      */
@@ -247,64 +242,23 @@ class Parser implements TagConstants {
             err.jspError(start, "jsp.error.attribute.unterminated", watch);
         }
 
-        String ret = parseQuoted(start, reader.getText(start, stop),
-                watch.charAt(watch.length() - 1));
+        String ret = null;
+        try {
+            char quote = 0;
+            if (watch.length() == 1) {
+                quote = watch.charAt(0);
+            }
+            ret = AttributeParser.getUnquoted(reader.getText(start, stop),
+                    quote, pageInfo.isELIgnored());
+        } catch (IllegalArgumentException iae) {
+            err.jspError(start, iae.getMessage());
+        }
         if (watch.length() == 1) // quote
             return ret;
 
-        // putback delimiter '<%=' and '%>', since they are needed if the
+        // Put back delimiter '<%=' and '%>', since they are needed if the
         // attribute does not allow RTexpression.
         return "<%=" + ret + "%>";
-    }
-
-    /**
-     * QuotedChar ::= '&apos;' | '&quot;' | '\\' | '\"' | "\'" | '\>' | '\$' |
-     * Char
-     */
-    private String parseQuoted(Mark start, String tx, char quote)
-            throws JasperException {
-        StringBuilder buf = new StringBuilder();
-        int size = tx.length();
-        int i = 0;
-        while (i < size) {
-            char ch = tx.charAt(i);
-            if (ch == '&') {
-                if (i + 5 < size && tx.charAt(i + 1) == 'a'
-                        && tx.charAt(i + 2) == 'p' && tx.charAt(i + 3) == 'o'
-                        && tx.charAt(i + 4) == 's' && tx.charAt(i + 5) == ';') {
-                    buf.append('\'');
-                    i += 6;
-                } else if (i + 5 < size && tx.charAt(i + 1) == 'q'
-                        && tx.charAt(i + 2) == 'u' && tx.charAt(i + 3) == 'o'
-                        && tx.charAt(i + 4) == 't' && tx.charAt(i + 5) == ';') {
-                    buf.append('"');
-                    i += 6;
-                } else {
-                    buf.append(ch);
-                    ++i;
-                }
-            } else if (ch == '\\' && i + 1 < size) {
-                ch = tx.charAt(i + 1);
-                if (ch == '\\' || ch == '\"' || ch == '\'' || ch == '>') {
-                    // \ " and ' are always unescaped regardless of if they are
-                    // inside or outside of an EL expression. JSP.1.6 takes
-                    // precedence over JSP.1.3.10 (confirmed with EG).
-                    buf.append(ch);
-                    i += 2;
-                } else {
-                    buf.append('\\');
-                    ++i;
-                }
-            } else if (ch == quote && STRICT_QUOTE_ESCAPING) {
-                // Unescaped quote character
-                err.jspError(start, "jsp.error.attribute.noescape", tx,
-                        "" + quote);
-            } else {
-                buf.append(ch);
-                ++i;
-            }
-        }
-        return buf.toString();
     }
 
     private String parseScriptText(String tx) {
