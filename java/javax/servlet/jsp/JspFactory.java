d5a6d54e5fbea72607dc98606f00d1b03c92f404
Mark Thomas <markt@apache.org>
2010-02-08 22:20:55 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=48170 Remove unnecessary syncs that cause issues under heavy load
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@907825 13f79535-47bb-0310-9956-ffa450edef68

--- java/javax/servlet/jsp/JspFactory.java
+++ java/javax/servlet/jsp/JspFactory.java
@@ -43,7 +43,7 @@ import javax.servlet.jsp.PageContext;
 
 public abstract class JspFactory {
 
-    private static JspFactory deflt = null;
+    private static volatile JspFactory deflt = null;
     
     /**
      * Sole constructor. (For invocation by subclass constructors, 
@@ -62,7 +62,7 @@ public abstract class JspFactory {
      * @param deflt        The default factory implementation
      */
 
-    public static synchronized void setDefaultFactory(JspFactory deflt) {
+    public static void setDefaultFactory(JspFactory deflt) {
         JspFactory.deflt = deflt;
     }
 
@@ -72,7 +72,7 @@ public abstract class JspFactory {
      * @return the default factory for this implementation
      */
 
-    public static synchronized JspFactory getDefaultFactory() {
+    public static JspFactory getDefaultFactory() {
         return deflt;
     }
 
