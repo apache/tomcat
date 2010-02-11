a7dfdfce2217f6c6ef53f92c57b28242ea0ff840
Mark Thomas <markt@apache.org>
2010-02-11 16:55:26 +0000

    Revert simple ResourceBundle fix On reflection (no pun intended) a reflection based will be better since - it can be implemented at a single point - it can easily be ported to Tomcat 6 - it will be able to handle any exotic class loader structures created by apps
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@909072 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/loader/WebappClassLoader.java
+++ java/org/apache/catalina/loader/WebappClassLoader.java
@@ -47,7 +47,6 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.ResourceBundle;
 import java.util.Vector;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.jar.Attributes;
@@ -1774,9 +1773,6 @@ public class WebappClassLoader
             org.apache.juli.logging.LogFactory.release(this);
         }
         
-        // Clear any cached resource bundles
-        ResourceBundle.clearCache(this);
-
         // Clear the classloader reference in the VM's bean introspector
         java.beans.Introspector.flushCaches();
 
