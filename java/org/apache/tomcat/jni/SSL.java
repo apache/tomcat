7b3bf519c2bcacb01def8a2b9b8aaaaa18cdf452
Mladen Turk <mturk@apache.org>
2010-03-02 09:08:26 +0000

    Marge typo fixes from tomcat trunk
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/native/branches/1.1.x@917926 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/jni/SSL.java
+++ java/org/apache/tomcat/jni/SSL.java
@@ -224,7 +224,7 @@ public final class SSL {
      * lifetime of JVM. Library.init() has to be called before.
      * @param engine Support for external a Crypto Device ("engine"),
      *                usually
-     * a hardware accellerator card for crypto operations.
+     * a hardware accelerator card for crypto operations.
      * @return APR status code
      */
     public static native int initialize(String engine);
@@ -275,7 +275,7 @@ public final class SSL {
             throws Exception;
 
     /**
-     * Close BIO and derefrence callback object
+     * Close BIO and dereference callback object
      * @param bio BIO to close and destroy.
      * @return APR Status code
      */
@@ -320,7 +320,7 @@ public final class SSL {
      * SSL_TMP_KEY_DH_4096
      * </PRE>
      * @param idx temporary key index.
-     * @param file File contatining DH params.
+     * @param file File containing DH params.
      */
     public static native boolean loadDSATempKey(int idx, String file);
 
