9ea27dd343d5253afb92fdfc0c6463e03b4ddf0b
Mark Thomas <markt@apache.org>
2010-03-04 18:06:21 +0000

    Lifecycle refactoring. Part 1 of many. Better define the start and stop methods as well as the overall component lifecycle and the relationship between start/stop, component state and events. Add a getState() method but it is commented out in this checkin to prevent compilation errors. It will be uncommented once the method has been added to the components that implement Lifecycle.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@919100 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/org/apache/catalina/LifecycleState.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.catalina;
+
+/**
+ * The list of valid states for components that implement {@link Lifecycle}.
+ * See {@link Lifecycle} for the state transition diagram.
+ */
+public enum LifecycleState {
+    NEW(false, null),
+    STARTING_PREP(false, Lifecycle.BEFORE_START_EVENT),
+    STARTING(true, Lifecycle.START_EVENT),
+    STARTED(true, Lifecycle.AFTER_START_EVENT),
+    STOPPING_PREP(true, Lifecycle.BEFORE_STOP_EVENT),
+    STOPPING(false, Lifecycle.STOP_EVENT),
+    STOPPED(false, Lifecycle.AFTER_STOP_EVENT),
+    FAILED(false, null),
+    MUST_STOP(true, null);
+    
+    private final boolean available;
+    private final String lifecycleEvent;
+    
+    private LifecycleState(boolean available, String lifecycleEvent) {
+        this.available = available;
+        this.lifecycleEvent = lifecycleEvent;
+    }
+    
+    /**
+     * Is a component in this state available for use?
+     */
+    public boolean isAvailable() {
+        return available;
+    }
+    
+    /**
+     * 
+     */
+    public String getLifecycleEvent() {
+        return lifecycleEvent;
+    }
+}
