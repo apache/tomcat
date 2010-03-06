4a4caf73d19df8f41ee4bdc711e2ffa266854db4
Mark Emlyn David Thomas <markt@apache.org>
2010-03-06 12:02:19 +0000

    Lifecycle refactoring - Store Added toString() for use in Lifecycle error messages
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@919750 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/session/StoreBase.java
+++ java/org/apache/catalina/session/StoreBase.java
@@ -21,12 +21,11 @@ import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.io.IOException;
 
-import org.apache.catalina.Lifecycle;
 import org.apache.catalina.LifecycleException;
-import org.apache.catalina.LifecycleListener;
+import org.apache.catalina.LifecycleState;
 import org.apache.catalina.Manager;
 import org.apache.catalina.Store;
-import org.apache.catalina.util.LifecycleSupport;
+import org.apache.catalina.util.LifecycleBase;
 import org.apache.tomcat.util.res.StringManager;
 
 /**
@@ -37,8 +36,7 @@ import org.apache.tomcat.util.res.StringManager;
  * @version $Revision$, $Date$
  */
 
-public abstract class StoreBase
-    implements Lifecycle, Store {
+public abstract class StoreBase extends LifecycleBase implements Store {
 
     // ----------------------------------------------------- Instance Variables
 
@@ -51,16 +49,6 @@ public abstract class StoreBase
      * Name to register for this Store, used for logging.
      */
     protected static String storeName = "StoreBase";
-
-    /**
-     * Has this component been started yet?
-     */
-    protected boolean started = false;
-
-    /**
-     * The lifecycle event support for this component.
-     */
-    protected LifecycleSupport lifecycle = new LifecycleSupport(this);
 
     /**
      * The property change support for this component.
@@ -117,36 +105,6 @@ public abstract class StoreBase
     // --------------------------------------------------------- Public Methods
 
     /**
-     * Add a lifecycle event listener to this component.
-     *
-     * @param listener The listener to add
-     */
-    public void addLifecycleListener(LifecycleListener listener) {
-        lifecycle.addLifecycleListener(listener);
-    }
-
-
-    /**
-     * Get the lifecycle listeners associated with this lifecycle. If this 
-     * Lifecycle has no listeners registered, a zero-length array is returned.
-     */
-    public LifecycleListener[] findLifecycleListeners() {
-
-        return lifecycle.findLifecycleListeners();
-
-    }
-
-
-    /**
-     * Remove a lifecycle event listener from this component.
-     *
-     * @param listener The listener to add
-     */
-    public void removeLifecycleListener(LifecycleListener listener) {
-        lifecycle.removeLifecycleListener(listener);
-    }
-
-    /**
      * Add a property change listener to this component.
      *
      * @param listener a value of type 'PropertyChangeListener'
@@ -175,7 +133,7 @@ public abstract class StoreBase
     public void processExpires() {
         String[] keys = null;
 
-        if(!started) {
+        if(!getState().isAvailable()) {
             return;
         }
 
@@ -224,45 +182,47 @@ public abstract class StoreBase
     }
 
 
-    // --------------------------------------------------------- Thread Methods
-
-
     /**
-     * Prepare for the beginning of active use of the public methods of this
-     * component.  This method should be called after <code>configure()</code>,
-     * and before any of the public methods of the component are utilized.
+     * Start this component and implement the requirements
+     * of {@link LifecycleBase#startInternal()}.
      *
      * @exception LifecycleException if this component detects a fatal error
      *  that prevents this component from being used
      */
-    public void start() throws LifecycleException {
-        // Validate and update our current component state
-        if (started)
-            throw new LifecycleException
-                (sm.getString(getStoreName()+".alreadyStarted"));
-        lifecycle.fireLifecycleEvent(START_EVENT, null);
-        started = true;
-
+    @Override
+    protected synchronized void startInternal() throws LifecycleException {
+        
+        setState(LifecycleState.STARTING);
     }
 
 
     /**
-     * Gracefully terminate the active use of the public methods of this
-     * component.  This method should be the last one called on a given
-     * instance of this component.
+     * Stop this component and implement the requirements
+     * of {@link LifecycleBase#stopInternal()}.
      *
      * @exception LifecycleException if this component detects a fatal error
-     *  that needs to be reported
+     *  that prevents this component from being used
      */
-    public void stop() throws LifecycleException {
-        // Validate and update our current component state
-        if (!started)
-            throw new LifecycleException
-                (sm.getString(getStoreName()+".notStarted"));
-        lifecycle.fireLifecycleEvent(STOP_EVENT, null);
-        started = false;
+    @Override
+    protected synchronized void stopInternal() throws LifecycleException {
 
+        setState(LifecycleState.STOPPING);
     }
-
-
+    
+    
+    /**
+     * Return a String rendering of this object.
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder(this.getClass().getName());
+        sb.append('[');
+        if (manager == null) {
+            sb.append("Manager is null");
+        } else {
+            sb.append(manager);
+        }
+        sb.append(']');
+        return sb.toString();
+    }
 }
