3d318808075f9d451a554bec55248f12bf60bc6f
Mark Emlyn David Thomas <markt@apache.org>
2010-03-08 21:53:29 +0000

    Revisit https://issues.apache.org/bugzilla/show_bug.cgi?id=48701 Allow TagVariableInfo as well as VariableInfo to introduce objects later used by <jsp:getProperty> - JSP.5.3
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@920532 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/jasper/compiler/Generator.java
+++ java/org/apache/jasper/compiler/Generator.java
@@ -1711,6 +1711,20 @@ class Generator {
                         pageInfo.getVarInfoNames().add(info.getVarName());
                 }
             }
+            TagVariableInfo[] tagInfos = n.getTagVariableInfos();
+            if (tagInfos != null && tagInfos.length > 0) {
+                for (int i = 0; i < tagInfos.length; i++) {
+                    TagVariableInfo tagInfo = tagInfos[i];
+                    if (tagInfo != null) {
+                        String name = tagInfo.getNameFromAttribute();
+                        if (name == null) {
+                            name = tagInfo.getNameGiven();
+                        }
+                        pageInfo.getVarInfoNames().add(name);
+                    }
+                }
+            }
+            
             
             if (n.implementsSimpleTag()) {
                 generateCustomDoTag(n, handlerInfo, tagHandlerVar);
