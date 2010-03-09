f629775d758481fcdc4e171ccc018b8e0385475c
Mark Thomas <markt@apache.org>
2010-03-09 21:51:46 +0000

    Bug 48701 test cases
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@921145 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/jasper/compiler/TestGenerator.java
+++ test/org/apache/jasper/compiler/TestGenerator.java
@@ -19,7 +19,15 @@ package org.apache.jasper.compiler;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagData;
+import javax.servlet.jsp.tagext.TagExtraInfo;
+import javax.servlet.jsp.tagext.TagSupport;
+import javax.servlet.jsp.tagext.VariableInfo;
+
+import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.catalina.startup.TomcatBaseTest;
 import org.apache.tomcat.util.buf.ByteChunk;
@@ -75,6 +83,114 @@ public class TestGenerator extends TomcatBaseTest {
         assertNotNull(e);
     }
 
+    public void testBug48701Fail() throws Exception {
+        Tomcat tomcat = getTomcatInstance();
+
+        File appDir = 
+            new File("test/webapp-3.0");
+        // app dir is relative to server home
+        StandardContext ctxt = (StandardContext) tomcat.addWebapp(null,
+                "/test", appDir.getAbsolutePath());
+        
+        // This test needs the JSTL libraries
+        File lib = new File("webapps/examples/WEB-INF/lib");
+        ctxt.setAliases("/WEB-INF/lib=" + lib.getCanonicalPath());
+        
+        tomcat.start();
+
+        Exception e = null;
+        try {
+            getUrl("http://localhost:" + getPort() + "/test/bug48701-fail.jsp");
+        } catch (IOException ioe) {
+            e = ioe;
+        }
+
+        // Failure is expected
+        assertNotNull(e);
+    }
+
+    public void testBug48701UseBean() throws Exception {
+        testBug48701("bug48701-UseBean.jsp");
+    }
+    
+    public void testBug48701VariableInfo() throws Exception {
+        testBug48701("bug48701-VI.jsp");
+    }
+    
+    public void testBug48701TagVariableInfoNameGiven() throws Exception {
+        testBug48701("bug48701-TVI-NG.jsp");
+    }
+    
+    public void testBug48701TagVariableInfoNameFromAttribute() throws Exception {
+        testBug48701("bug48701-TVI-NFA.jsp");
+    }
+    
+    private void testBug48701(String jsp) throws Exception {
+        Tomcat tomcat = getTomcatInstance();
+
+        File appDir = 
+            new File("test/webapp-3.0");
+        // app dir is relative to server home
+        tomcat.addWebapp(null, "/test", appDir.getAbsolutePath());
+        
+        tomcat.start();
+
+        ByteChunk res = getUrl("http://localhost:" + getPort() +
+                "/test/" + jsp);
+        
+        String result = res.toString();
+        assertEcho(result, "00-PASS");
+    }
+
+    public static class Bug48701 extends TagSupport {
+
+        private static final long serialVersionUID = 1L;
+
+        private String beanName = null;
+
+        public void setBeanName(String beanName) {
+            this.beanName = beanName;
+        }
+
+        public String getBeanName() {
+            return beanName;
+        }
+
+        @Override
+        public int doStartTag() throws JspException {
+            Bean bean = new Bean();
+            bean.setTime((new Date()).toString());
+            pageContext.setAttribute("now", bean);
+            return super.doStartTag();
+        }
+
+        
+    }
+    
+    public static class Bug48701TEI extends TagExtraInfo {
+
+        @Override
+        public VariableInfo[] getVariableInfo(TagData data) {
+            return new VariableInfo[] {
+                    new VariableInfo("now", Bean.class.getCanonicalName(),
+                            true, VariableInfo.AT_END)
+                };
+        }
+
+    }
+    
+    public static class Bean {
+        private String time;
+
+        public void setTime(String time) {
+            this.time = time;
+        }
+
+        public String getTime() {
+            return time;
+        }
+    }
+    
     /** Assertion for text printed by tags:echo */
     private static void assertEcho(String result, String expected) {
         assertTrue(result.indexOf("<p>" + expected + "</p>") > 0);
