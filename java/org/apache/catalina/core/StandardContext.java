37eb137e90c89899371cdc56e8e292ba507c6c47
Mark Thomas <markt@apache.org>
2010-03-21 21:22:54 +0000

    Fix various exceptions that prevented one of the TCK web applications from starting
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@925901 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/core/StandardContext.java
+++ java/org/apache/catalina/core/StandardContext.java
@@ -2550,12 +2550,11 @@ public class StandardContext
             System.arraycopy(filterMaps, 0, results, 0, filterMapInsertPoint);
             results[filterMapInsertPoint] = filterMap;
             System.arraycopy(filterMaps, filterMapInsertPoint, results,
-                    filterMaps.length - filterMapInsertPoint+1,
-                    filterMapInsertPoint);
+                    filterMaps.length - filterMapInsertPoint + 1,
+                    filterMaps.length - filterMapInsertPoint);
             
             filterMapInsertPoint++;
-            
-            results[filterMaps.length] = filterMap;
+
             filterMaps = results;
         }
         fireContainerEvent("addFilterMap", filterMap);
