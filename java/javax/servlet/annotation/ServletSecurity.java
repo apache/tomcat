efffa6e45750291c6966dbe3fdea3998b79ad0f6
Mark Emlyn David Thomas <markt@apache.org>
2010-04-12 10:34:09 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=49086 Add Javadocs to javax.servlet.annotation Patch provided by Pid.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@933183 13f79535-47bb-0310-9956-ffa450edef68

--- java/javax/servlet/annotation/ServletSecurity.java
+++ java/javax/servlet/annotation/ServletSecurity.java
@@ -24,22 +24,67 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * Declare this annotation on a {@link javax.servlet.Servlet} implementation
+ * class to enforce security constraints on HTTP protocol requests.<br />
+ * The container applies constraints to the URL patterns mapped to each Servlet
+ * which declares this annotation.<br />
+ * <br />
+ * 
  * @since Servlet 3.0
- * TODO SERVLET3 - Add comments
  */
 @Inherited
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface ServletSecurity {
+
+    /**
+     * Represents the two possible values of the empty role semantic, active
+     * when a list of role names is empty.
+     */
     enum EmptyRoleSemantic {
+
+        /**
+         * Access MUST be permitted, regardless of authentication state or
+         * identity
+         */
         PERMIT,
+
+        /**
+         * Access MUST be denied, regardless of authentication state or identity
+         */
         DENY
     }
+
+    /**
+     * Represents the two possible values of data transport, encrypted or not.
+     */
     enum TransportGuarantee {
+
+        /**
+         * User data must not be encrypted by the container during transport
+         */
         NONE,
+
+        /**
+         * The container MUST encrypt user data during transport
+         */
         CONFIDENTIAL
     }
+
+    /**
+     * The default constraint to apply to requests not handled by specific
+     * method constraints
+     * 
+     * @return http constraint
+     */
     HttpConstraint value() default @HttpConstraint;
+
+    /**
+     * An array of HttpMethodContraint objects to which the security constraint
+     * will be applied
+     * 
+     * @return array of http method constraint
+     */
     HttpMethodConstraint[] httpMethodConstraints() default {};
 }
