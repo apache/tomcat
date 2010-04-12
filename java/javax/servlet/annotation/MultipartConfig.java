abb7d0303ff736f2704d6d171ad56f48ca70be84
Mark Emlyn David Thomas <markt@apache.org>
2010-04-12 10:34:09 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=49086 Add Javadocs to javax.servlet.annotation Patch provided by Pid.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@933183 13f79535-47bb-0310-9956-ffa450edef68

--- java/javax/servlet/annotation/MultipartConfig.java
+++ java/javax/servlet/annotation/MultipartConfig.java
@@ -22,14 +22,47 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * This annotation is used to indicate that the {@link javax.servlet.Servlet} on
+ * which it is declared expects requests to made using the {@code
+ * multipart/form-data} MIME type. <br />
+ * <br />
+ * 
+ * {@link javax.servlet.http.Part} components of a given {@code
+ * multipart/form-data} request are retrieved by a Servlet annotated with
+ * {@code MultipartConfig} by calling
+ * {@link javax.servlet.http.HttpServletRequest#getPart} or
+ * {@link javax.servlet.http.HttpServletRequest#getParts}.<br />
+ * <br />
+ * 
+ * E.g. <code>@WebServlet("/upload")}</code><br />
+ * 
+ * <code>@MultipartConfig()</code> <code>public class UploadServlet extends
+ * HttpServlet ... } </code><br />
+ * 
  * @since Servlet 3.0
- * TODO SERVLET3 - Add comments
  */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface MultipartConfig {
+
+    /**
+     * @return location in which the Container stores temporary files
+     */
     String location() default "";
+
+    /**
+     * @return the maximum size allowed for uploaded files (in bytes)
+     */
     long maxFileSize() default -1L;
+
+    /**
+     * @return the maximum size of the request allowed for {@code
+     *         multipart/form-data}
+     */
     long maxRequestSize() default -1L;
+
+    /**
+     * @return the size threshold at which the file will be written to the disk
+     */
     int fileSizeThreshold() default 0;
 }
--- java/javax/servlet/annotation/WebFilter.java
+++ java/javax/servlet/annotation/WebFilter.java
@@ -16,31 +16,107 @@
  */
 package javax.servlet.annotation;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.lang.annotation.Documented;
 
 import javax.servlet.DispatcherType;
 
 /**
- * @since Servlet 3.0
- * TODO SERVLET3 - Add comments
+ * The annotation used to declare a Servlet {@link javax.servlet.Filter}. <br />
+ * <br />
+ * 
+ * This annotation will be processed by the container during deployment, the
+ * Filter class in which it is found will be created as per the configuration
+ * and applied to the URL patterns, {@link javax.servlet.Servlet}s and
+ * {@link javax.servlet.DispatcherType}s.<br />
+ * <br/>
+ * 
+ * If the name attribute is not defined, the fully qualified name of the class
+ * is used.<br/>
+ * <br/>
+ * 
+ * At least one URL pattern MUST be declared in either the {@code value} or
+ * {@code urlPattern} attribute of the annotation, but not both.<br/>
+ * <br/>
+ * 
+ * The {@code value} attribute is recommended for use when the URL pattern is
+ * the only attribute being set, otherwise the {@code urlPattern} attribute
+ * should be used.<br />
+ * <br />
+ * 
+ * The annotated class MUST implement {@link javax.servlet.Filter}.
+ * 
+ * E.g.
+ * 
+ * <code>@WebFilter("/path/*")</code><br />
+ * <code>public class AnExampleFilter implements Filter { ... </code><br />
+ * 
+ * @since Servlet 3.0 (Section 8.1.2)
+ * 
  */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface WebFilter {
+
+    /**
+     * @return description of the Filter, if present
+     */
     String description() default "";
+
+    /**
+     * @return display name of the Filter, if present
+     */
     String displayName() default "";
+
+    /**
+     * @return array of initialization params for this Filter
+     */
     WebInitParam[] initParams() default {};
+
+    /**
+     * @return name of the Filter, if present
+     */
     String filterName() default "";
+
+    /**
+     * @return small icon for this Filter, if present
+     */
     String smallIcon() default "";
+
+    /**
+     * @return the large icon for this Filter, if present
+     */
     String largeIcon() default "";
+
+    /**
+     * @return array of Servlet names to which this Filter applies
+     */
     String[] servletNames() default {};
+
+    /**
+     * A convenience method, to allow extremely simple annotation of a class.
+     * 
+     * @return array of URL patterns
+     * @see #urlPatterns()
+     */
     String[] value() default {};
+
+    /**
+     * @return array of URL patterns to which this Filter applies
+     */
     String[] urlPatterns() default {};
+
+    /**
+     * @return array of DispatcherTypes to which this filter applies
+     */
     DispatcherType[] dispatcherTypes() default {DispatcherType.REQUEST};
+
+    /**
+     * @return asynchronous operation supported by this Filter
+     */
     boolean asyncSupported() default false;
 }
