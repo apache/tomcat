db32c25e06e1b0a5a274cad4cfc1ad35ff7c0e9a
Mark Emlyn David Thomas <markt@apache.org>
2010-04-21 22:11:29 +0000

    Fix CVE-2010-1157. Prevent possible disclosure of host name or IP address via the HTTP WWW-Authenticate header when using BASIC or DIGEST authentication.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@936539 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/authenticator/AuthenticatorBase.java
+++ java/org/apache/catalina/authenticator/AuthenticatorBase.java
@@ -107,6 +107,11 @@ public abstract class AuthenticatorBase extends ValveBase
     protected static final String AUTH_HEADER_NAME = "WWW-Authenticate";
 
     /**
+     * Default authentication realm name.
+     */
+    protected static final String REALM_NAME = "Authentication required";
+
+    /**
      * The message digest algorithm to be used when generating session
      * identifiers.  This must be an algorithm supported by the
      * <code>java.security.MessageDigest</code> class on your platform.
