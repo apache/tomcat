6372b7b4d969521586cdbd677c9109bbafc3c000
Mark Emlyn David Thomas <markt@apache.org>
2010-04-21 22:11:29 +0000

    Fix CVE-2010-1157. Prevent possible disclosure of host name or IP address via the HTTP WWW-Authenticate header when using BASIC or DIGEST authentication.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@936539 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/authenticator/BasicAuthenticator.java
+++ java/org/apache/catalina/authenticator/BasicAuthenticator.java
@@ -165,9 +165,7 @@ public class BasicAuthenticator
         StringBuilder value = new StringBuilder(16);
         value.append("Basic realm=\"");
         if (config.getRealmName() == null) {
-            value.append(request.getServerName());
-            value.append(':');
-            value.append(Integer.toString(request.getServerPort()));
+            value.append(REALM_NAME);
         } else {
             value.append(config.getRealmName());
         }
