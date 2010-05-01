616bd684e7ec46d58d994d19992010236364d621
Mark Emlyn David Thomas <markt@apache.org>
2010-05-01 15:20:31 +0000

    Another fix for https://issues.apache.org/bugzilla/show_bug.cgi?id=49092 Make constant final
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@940055 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/catalina/tribes/demos/IntrospectionUtils.java
+++ test/org/apache/catalina/tribes/demos/IntrospectionUtils.java
@@ -231,7 +231,8 @@ public final class IntrospectionUtils {
         }
     }
 
-    public static String PATH_SEPARATOR = System.getProperty("path.separator");
+    public static final String PATH_SEPARATOR =
+        System.getProperty("path.separator");
 
     /**
      * Adds classpath entries from a vector of URL's to the "tc_path_add" System
