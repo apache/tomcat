b5de39e22b07f1a4a15bde2b47cefee260684468
Mark Thomas <markt@apache.org>
2010-05-06 22:19:17 +0000

    IBM has agreed to re-license the affected files. A patch was provided by an IBM employee with a CLA on file under https://issues.apache.org/bugzilla/show_bug.cgi?id=49259 The files can now therefore be treated the same way as the other BCEL files. The updated files have been applied to BCEL and I have copied them across to Tomcat 7 and made the same set of changes as I made previously. The various LICENSE and NOTICE changes made in r934220 have been reverted. Thanks to Kevan Miller and his IBM colleagues for working to resolve this so quickly.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@941955 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/bcel/classfile/EnclosingMethod.java
+++ java/org/apache/tomcat/util/bcel/classfile/EnclosingMethod.java
@@ -1,25 +1,22 @@
-/* *******************************************************************
- * Copyright (c) 2004 IBM Corporation
- * 
- * All rights reserved. 
- * This program and the accompanying materials are made available 
- * under the terms of the Common Public License v1.0 
- * which accompanies this distribution and is available at 
- * http://www.eclipse.org/legal/cpl-v10.html 
- * 
- * Contributors: 
- *    Andy Clement     initial implementation 
- * ******************************************************************/
-
-/*
- * Under the terms of the CPL v1.0, the ASF has elected to distribute this
- * file under the Eclipse Public License (EPL) 1.0 which has been designated
- * as the follow-on version of the CPL by the Agreement Steward.
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
-
 package org.apache.tomcat.util.bcel.classfile;
 
-import java.io.DataInput;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
@@ -46,7 +43,7 @@ public class EnclosingMethod extends Attribute {
 	private int methodIndex;
 
 	// Ctors - and code to read an attribute in.
-	public EnclosingMethod(int nameIndex, int len, DataInput dis, ConstantPool cpool) throws IOException {
+	public EnclosingMethod(int nameIndex, int len, DataInputStream dis, ConstantPool cpool) throws IOException {
 		this(nameIndex, len, dis.readUnsignedShort(), dis.readUnsignedShort(), cpool);
 	}
 
@@ -62,16 +59,6 @@ public class EnclosingMethod extends Attribute {
 		// return (EnclosingMethod)clone();
 	}
 	
-	  
-	
-	
-	
-	
-
-	
-	
-	
-
     public final void dump(DataOutputStream file) throws IOException {
 	    super.dump(file);
 	    file.writeShort(classIndex);
