334ca2b7a3c568d3432cfc0f83fc58a63e5676bc
Mark Emlyn David Thomas <markt@apache.org>
2010-05-19 14:52:42 +0000

    Typo
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@946205 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/catalina/startup/TomcatBaseTest.java
+++ test/org/apache/catalina/startup/TomcatBaseTest.java
@@ -105,7 +105,7 @@ public abstract class TomcatBaseTest extends TestCase {
     @Override
     public void tearDown() throws Exception {
         // Some tests may call tomcat.destroy(), some tests may just call
-        // tomcat.stop(), smoe not call either method. Make sure that stop() &
+        // tomcat.stop(), some not call either method. Make sure that stop() &
         // destroy() are called as necessary.
         if (tomcat.server != null &&
                 tomcat.server.getState() != LifecycleState.DESTROYED) {
