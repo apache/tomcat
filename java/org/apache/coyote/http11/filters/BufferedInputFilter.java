40e5880dfc51517334acda5f12beacdec52ca283
Jean-Frederic Clere <jfclere@apache.org>
2010-06-29 10:13:54 +0000

    Arrange filter logic. This is the fix for CVE-2010-2227.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@958911 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http11/filters/BufferedInputFilter.java
+++ java/org/apache/coyote/http11/filters/BufferedInputFilter.java
@@ -102,10 +102,12 @@ public class BufferedInputFilter implements InputFilter {
     }
 
     public void recycle() {
-        if (buffered.getBuffer().length > 65536) {
-            buffered = null;
-        } else {
-            buffered.recycle();
+        if (buffered != null) {
+            if (buffered.getBuffer().length > 65536) {
+                buffered = null;
+            } else {
+                buffered.recycle();
+            }
         }
         tempRead.recycle();
         hasRead = false;
