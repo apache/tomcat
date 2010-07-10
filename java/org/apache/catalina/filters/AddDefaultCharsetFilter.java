6132d2d8a1e04ddbaef443b9ea864c92e7ecd7a9
Mark Emlyn David Thomas <markt@apache.org>
2010-07-10 21:13:23 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=49478 Add support for user specified character sets to the AddDefaultCharsetFilter. Based on a patch by Felix Schumacher.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@962917 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/filters/AddDefaultCharsetFilter.java
+++ java/org/apache/catalina/filters/AddDefaultCharsetFilter.java
@@ -17,10 +17,9 @@
 
 package org.apache.catalina.filters;
 
-
 import java.io.IOException;
+import java.nio.charset.Charset;
 
-import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
@@ -29,24 +28,54 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletResponseWrapper;
 
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+
 
 /**
  * Filter that explicitly sets the default character set for media subtypes of
- * the "text" type to ISO-8859-1. RFC2616 explicitly states that browsers must
- * use ISO-8859-1 in these circumstances. However, browsers may attempt to
+ * the "text" type to ISO-8859-1, or another user defined character set. RFC2616
+ * explicitly states that browsers must use ISO-8859-1 if no character set is
+ * defined for media with subtype "text". However, browsers may attempt to
  * auto-detect the character set. This may be exploited by an attacker to
  * perform an XSS attack. Internet Explorer has this behaviour by default. Other
- * browsers have an option to enable it.
+ * browsers have an option to enable it.<br/>
  * 
  * This filter prevents the attack by explicitly setting a character set. Unless
  * the provided character set is explicitly overridden by the user - in which
  * case they deserve everything they get - the browser will adhere to an
  * explicitly set character set, thus preventing the XSS attack.
  */
-public class AddDefaultCharsetFilter implements Filter {
+public class AddDefaultCharsetFilter extends FilterBase {
 
-    public void destroy() {
-        // NOOP
+    private static final Log log =
+        LogFactory.getLog(AddDefaultCharsetFilter.class);
+
+    private static final String DEFAULT_ENCODING = "ISO-8859-1";
+    
+    private String encoding;
+
+    public void setEncoding(String encoding) {
+        this.encoding = encoding;
+    }
+
+    @Override
+    protected Log getLogger() {
+        return log;
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        super.init(filterConfig);
+        if (encoding == null || encoding.length() == 0 ||
+                encoding.equalsIgnoreCase("default")) {
+            encoding = DEFAULT_ENCODING;
+        } else if (encoding.equalsIgnoreCase("system")) {
+            encoding = Charset.defaultCharset().name();
+        } else if (!Charset.isSupported(encoding)) {
+            throw new IllegalArgumentException(sm.getString(
+                    "addDefaultCharset.unsupportedCharset", encoding));
+        }
     }
 
     public void doFilter(ServletRequest request, ServletResponse response,
@@ -55,40 +84,46 @@ public class AddDefaultCharsetFilter implements Filter {
         // Wrap the response
         if (response instanceof HttpServletResponse) {
             ResponseWrapper wrapped =
-                new ResponseWrapper((HttpServletResponse)response);
+                new ResponseWrapper((HttpServletResponse)response, encoding);
             chain.doFilter(request, wrapped);
         } else {
             chain.doFilter(request, response);
         }
     }
 
-    public void init(FilterConfig filterConfig) throws ServletException {
-        // NOOP
-    }
-
     /**
-     * Wrapper that adds the default character set for text media types if no
-     * character set is specified.
+     * Wrapper that adds a character set for text media types if no character
+     * set is specified.
      */
-    public class ResponseWrapper extends HttpServletResponseWrapper {
+    public static class ResponseWrapper extends HttpServletResponseWrapper {
+
+        private String encoding;
+        
+        public ResponseWrapper(HttpServletResponse response, String encoding) {
+            super(response);
+            this.encoding = encoding;
+        }
 
         @Override
         public void setContentType(String ct) {
             
-            if (ct != null && ct.startsWith("text/") &&
-                    ct.indexOf("charset=") < 0) {
-                // Use getCharacterEncoding() in case the charset has already
-                // been set by a separate call.
-                super.setContentType(ct + ";charset=" + getCharacterEncoding());
+            if (ct != null && ct.startsWith("text/")) {
+                if (ct.indexOf("charset=") < 0) {
+                    super.setContentType(ct + ";charset=" + encoding);
+                } else {
+                    super.setContentType(ct);
+                    encoding = getCharacterEncoding();
+                }
             } else {
                 super.setContentType(ct);
             }
 
         }
 
-        public ResponseWrapper(HttpServletResponse response) {
-            super(response);
+        @Override
+        public void setCharacterEncoding(String charset) {
+            super.setCharacterEncoding(charset);
+            encoding = charset;
         }
-        
     }
 }
