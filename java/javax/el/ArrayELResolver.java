8834f0a0d73fa08edbf38f6c5701d1d08ad7aa4c
Mark Emlyn David Thomas <markt@apache.org>
2010-07-22 22:35:32 +0000

    Tab police: javax.el Thanks to Checkstyle
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@966883 13f79535-47bb-0310-9956-ffa450edef68

--- java/javax/el/ArrayELResolver.java
+++ java/javax/el/ArrayELResolver.java
@@ -24,149 +24,149 @@ import java.util.Iterator;
 
 public class ArrayELResolver extends ELResolver {
 
-	private final boolean readOnly;
+    private final boolean readOnly;
 
-	public ArrayELResolver() {
-		this.readOnly = false;
-	}
+    public ArrayELResolver() {
+        this.readOnly = false;
+    }
 
-	public ArrayELResolver(boolean readOnly) {
-		this.readOnly = readOnly;
-	}
+    public ArrayELResolver(boolean readOnly) {
+        this.readOnly = readOnly;
+    }
 
-	@Override
+    @Override
     public Object getValue(ELContext context, Object base, Object property)
-			throws NullPointerException, PropertyNotFoundException, ELException {
-		if (context == null) {
-			throw new NullPointerException();
-		}
+            throws NullPointerException, PropertyNotFoundException, ELException {
+        if (context == null) {
+            throw new NullPointerException();
+        }
 
-		if (base != null && base.getClass().isArray()) {
-			context.setPropertyResolved(true);
-			int idx = coerce(property);
+        if (base != null && base.getClass().isArray()) {
+            context.setPropertyResolved(true);
+            int idx = coerce(property);
             if (idx < 0 || idx >= Array.getLength(base)) {
                 return null;
             }
             return Array.get(base, idx);
         }
 
-		return null;
-	}
+        return null;
+    }
 
-	@Override
+    @Override
     public Class<?> getType(ELContext context, Object base, Object property)
-			throws NullPointerException, PropertyNotFoundException, ELException {
-		if (context == null) {
-			throw new NullPointerException();
-		}
+            throws NullPointerException, PropertyNotFoundException, ELException {
+        if (context == null) {
+            throw new NullPointerException();
+        }
 
-		if (base != null && base.getClass().isArray()) {
-			context.setPropertyResolved(true);
-			int idx = coerce(property);
-			checkBounds(base, idx);
-			return base.getClass().getComponentType();
-		}
+        if (base != null && base.getClass().isArray()) {
+            context.setPropertyResolved(true);
+            int idx = coerce(property);
+            checkBounds(base, idx);
+            return base.getClass().getComponentType();
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	@Override
+    @Override
     public void setValue(ELContext context, Object base, Object property,
-			Object value) throws NullPointerException,
-			PropertyNotFoundException, PropertyNotWritableException,
-			ELException {
-		if (context == null) {
-			throw new NullPointerException();
-		}
+            Object value) throws NullPointerException,
+            PropertyNotFoundException, PropertyNotWritableException,
+            ELException {
+        if (context == null) {
+            throw new NullPointerException();
+        }
 
-		if (base != null && base.getClass().isArray()) {
-			context.setPropertyResolved(true);
+        if (base != null && base.getClass().isArray()) {
+            context.setPropertyResolved(true);
 
-			if (this.readOnly) {
-				throw new PropertyNotWritableException(message(context,
-						"resolverNotWriteable", new Object[] { base.getClass()
-								.getName() }));
-			}
+            if (this.readOnly) {
+                throw new PropertyNotWritableException(message(context,
+                        "resolverNotWriteable", new Object[] { base.getClass()
+                                .getName() }));
+            }
 
-			int idx = coerce(property);
-			checkBounds(base, idx);
-			if (value != null &&
-			        !base.getClass().getComponentType().isAssignableFrom(
-			                value.getClass())) {
-			    throw new ClassCastException(message(context,
-			            "objectNotAssignable",
-			            new Object[] {value.getClass().getName(),
-			            base.getClass().getComponentType().getName()}));
-			}
-			Array.set(base, idx, value);
-		}
-	}
+            int idx = coerce(property);
+            checkBounds(base, idx);
+            if (value != null &&
+                    !base.getClass().getComponentType().isAssignableFrom(
+                            value.getClass())) {
+                throw new ClassCastException(message(context,
+                        "objectNotAssignable",
+                        new Object[] {value.getClass().getName(),
+                        base.getClass().getComponentType().getName()}));
+            }
+            Array.set(base, idx, value);
+        }
+    }
 
-	@Override
+    @Override
     public boolean isReadOnly(ELContext context, Object base, Object property)
-			throws NullPointerException, PropertyNotFoundException, ELException {
-		if (context == null) {
-			throw new NullPointerException();
-		}
+            throws NullPointerException, PropertyNotFoundException, ELException {
+        if (context == null) {
+            throw new NullPointerException();
+        }
 
-		if (base != null && base.getClass().isArray()) {
-			context.setPropertyResolved(true);
-			int idx = coerce(property);
-			checkBounds(base, idx);
-		}
+        if (base != null && base.getClass().isArray()) {
+            context.setPropertyResolved(true);
+            int idx = coerce(property);
+            checkBounds(base, idx);
+        }
 
-		return this.readOnly;
-	}
+        return this.readOnly;
+    }
 
-	@Override
+    @Override
     public Iterator<FeatureDescriptor> getFeatureDescriptors(ELContext context, Object base) {
-		if (base != null && base.getClass().isArray()) {
-			FeatureDescriptor[] descs = new FeatureDescriptor[Array.getLength(base)];
-			for (int i = 0; i < descs.length; i++) {
-				descs[i] = new FeatureDescriptor();
-				descs[i].setDisplayName("["+i+"]");
-				descs[i].setExpert(false);
-				descs[i].setHidden(false);
-				descs[i].setName(""+i);
-				descs[i].setPreferred(true);
-				descs[i].setValue(RESOLVABLE_AT_DESIGN_TIME, Boolean.FALSE);
-				descs[i].setValue(TYPE, Integer.class);
-			}
-			return Arrays.asList(descs).iterator();
-		}
-		return null;
-	}
+        if (base != null && base.getClass().isArray()) {
+            FeatureDescriptor[] descs = new FeatureDescriptor[Array.getLength(base)];
+            for (int i = 0; i < descs.length; i++) {
+                descs[i] = new FeatureDescriptor();
+                descs[i].setDisplayName("["+i+"]");
+                descs[i].setExpert(false);
+                descs[i].setHidden(false);
+                descs[i].setName(""+i);
+                descs[i].setPreferred(true);
+                descs[i].setValue(RESOLVABLE_AT_DESIGN_TIME, Boolean.FALSE);
+                descs[i].setValue(TYPE, Integer.class);
+            }
+            return Arrays.asList(descs).iterator();
+        }
+        return null;
+    }
 
-	@Override
+    @Override
     public Class<?> getCommonPropertyType(ELContext context, Object base) {
-		if (base != null && base.getClass().isArray()) {
-			return Integer.class;
-		}
-		return null;
-	}
+        if (base != null && base.getClass().isArray()) {
+            return Integer.class;
+        }
+        return null;
+    }
 
-	private final static void checkBounds(Object base, int idx) {
-		if (idx < 0 || idx >= Array.getLength(base)) {
-			throw new PropertyNotFoundException(
-					new ArrayIndexOutOfBoundsException(idx).getMessage());
-		}
-	}
+    private final static void checkBounds(Object base, int idx) {
+        if (idx < 0 || idx >= Array.getLength(base)) {
+            throw new PropertyNotFoundException(
+                    new ArrayIndexOutOfBoundsException(idx).getMessage());
+        }
+    }
 
-	private final static int coerce(Object property) {
-		if (property instanceof Number) {
-			return ((Number) property).intValue();
-		}
-		if (property instanceof Character) {
-			return ((Character) property).charValue();
-		}
-		if (property instanceof Boolean) {
-			return (((Boolean) property).booleanValue() ? 1 : 0);
-		}
+    private final static int coerce(Object property) {
+        if (property instanceof Number) {
+            return ((Number) property).intValue();
+        }
+        if (property instanceof Character) {
+            return ((Character) property).charValue();
+        }
+        if (property instanceof Boolean) {
+            return (((Boolean) property).booleanValue() ? 1 : 0);
+        }
         if (property instanceof String) {
             return Integer.parseInt((String) property);
         }
-		throw new IllegalArgumentException(property != null ? property
-				.toString() : "null");
-	}
+        throw new IllegalArgumentException(property != null ?
+                property.toString() : "null");
+    }
 
 }
