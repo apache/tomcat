6cec0f2f4607ba86d5d37bc36369be66aca52bb0
Mark Emlyn David Thomas <markt@apache.org>
2010-07-23 16:03:25 +0000

    Tab police: javax.servlet.http Thanks to Checkstyle and Eclipse source formatting
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@967146 13f79535-47bb-0310-9956-ffa450edef68

--- java/javax/servlet/http/HttpServletResponseWrapper.java
+++ java/javax/servlet/http/HttpServletResponseWrapper.java
@@ -1,19 +1,19 @@
 /*
-* Licensed to the Apache Software Foundation (ASF) under one or more
-* contributor license agreements.  See the NOTICE file distributed with
-* this work for additional information regarding copyright ownership.
-* The ASF licenses this file to You under the Apache License, Version 2.0
-* (the "License"); you may not use this file except in compliance with
-* the License.  You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package javax.servlet.http;
 
 import java.io.IOException;
@@ -22,216 +22,206 @@ import java.util.Collection;
 import javax.servlet.ServletResponseWrapper;
 
 /**
+ * Provides a convenient implementation of the HttpServletResponse interface
+ * that can be subclassed by developers wishing to adapt the response from a
+ * Servlet. This class implements the Wrapper or Decorator pattern. Methods
+ * default to calling through to the wrapped response object.
  * 
- * Provides a convenient implementation of the HttpServletResponse interface that
- * can be subclassed by developers wishing to adapt the response from a Servlet.
- * This class implements the Wrapper or Decorator pattern. Methods default to
- * calling through to the wrapped response object.
- * 
- * @author 	Various
- * @version 	$Version$
-  * @since	v 2.3
- *
- * @see 	javax.servlet.http.HttpServletResponse
- *
+ * @author Various
+ * @version $Version$
+ * @since v 2.3
+ * @see javax.servlet.http.HttpServletResponse
  */
+public class HttpServletResponseWrapper extends ServletResponseWrapper
+        implements HttpServletResponse {
 
-public class HttpServletResponseWrapper extends ServletResponseWrapper implements HttpServletResponse {
-
-
-    /** 
-    * Constructs a response adaptor wrapping the given response.
-    * @throws java.lang.IllegalArgumentException if the response is null
-    */
+    /**
+     * Constructs a response adaptor wrapping the given response.
+     * 
+     * @throws java.lang.IllegalArgumentException
+     *             if the response is null
+     */
     public HttpServletResponseWrapper(HttpServletResponse response) {
-	    super(response);
+        super(response);
     }
-    
+
     private HttpServletResponse _getHttpServletResponse() {
-	return (HttpServletResponse) super.getResponse();
+        return (HttpServletResponse) super.getResponse();
     }
-    
+
     /**
      * The default behavior of this method is to call addCookie(Cookie cookie)
      * on the wrapped response object.
      */
     public void addCookie(Cookie cookie) {
-	this._getHttpServletResponse().addCookie(cookie);
+        this._getHttpServletResponse().addCookie(cookie);
     }
 
     /**
-     * The default behavior of this method is to call containsHeader(String name)
-     * on the wrapped response object.
+     * The default behavior of this method is to call containsHeader(String
+     * name) on the wrapped response object.
      */
-
- 
     public boolean containsHeader(String name) {
-	return this._getHttpServletResponse().containsHeader(name);
+        return this._getHttpServletResponse().containsHeader(name);
     }
-    
+
     /**
-     * The default behavior of this method is to call encodeURL(String url)
-     * on the wrapped response object.
+     * The default behavior of this method is to call encodeURL(String url) on
+     * the wrapped response object.
      */
     public String encodeURL(String url) {
-	return this._getHttpServletResponse().encodeURL(url);
+        return this._getHttpServletResponse().encodeURL(url);
     }
 
     /**
-     * The default behavior of this method is to return encodeRedirectURL(String url)
-     * on the wrapped response object.
+     * The default behavior of this method is to return encodeRedirectURL(String
+     * url) on the wrapped response object.
      */
     public String encodeRedirectURL(String url) {
-	return this._getHttpServletResponse().encodeRedirectURL(url);
+        return this._getHttpServletResponse().encodeRedirectURL(url);
     }
 
     /**
-     * The default behavior of this method is to call encodeUrl(String url)
-     * on the wrapped response object.
+     * The default behavior of this method is to call encodeUrl(String url) on
+     * the wrapped response object.
+     * 
      * @deprecated As of Version 3.0 of the Java Servlet API
      */
-    @SuppressWarnings("dep-ann") // Spec API does not use @Deprecated
+    @SuppressWarnings("dep-ann")
+    // Spec API does not use @Deprecated
     public String encodeUrl(String url) {
-	return this._getHttpServletResponse().encodeUrl(url);
+        return this._getHttpServletResponse().encodeUrl(url);
     }
-    
+
     /**
-     * The default behavior of this method is to return encodeRedirectUrl(String url)
-     * on the wrapped response object.
+     * The default behavior of this method is to return encodeRedirectUrl(String
+     * url) on the wrapped response object.
+     * 
      * @deprecated As of Version 3.0 of the Java Servlet API
      */
-    @SuppressWarnings("dep-ann") // Spec API does not use @Deprecated
+    @SuppressWarnings("dep-ann")
+    // Spec API does not use @Deprecated
     public String encodeRedirectUrl(String url) {
-	return this._getHttpServletResponse().encodeRedirectUrl(url);
+        return this._getHttpServletResponse().encodeRedirectUrl(url);
     }
-    
+
     /**
-     * The default behavior of this method is to call sendError(int sc, String msg)
-     * on the wrapped response object.
+     * The default behavior of this method is to call sendError(int sc, String
+     * msg) on the wrapped response object.
      */
     public void sendError(int sc, String msg) throws IOException {
-	this._getHttpServletResponse().sendError(sc, msg);
+        this._getHttpServletResponse().sendError(sc, msg);
     }
 
     /**
-     * The default behavior of this method is to call sendError(int sc)
-     * on the wrapped response object.
+     * The default behavior of this method is to call sendError(int sc) on the
+     * wrapped response object.
      */
-
-
     public void sendError(int sc) throws IOException {
-	this._getHttpServletResponse().sendError(sc);
+        this._getHttpServletResponse().sendError(sc);
     }
 
     /**
-     * The default behavior of this method is to return sendRedirect(String location)
-     * on the wrapped response object.
+     * The default behavior of this method is to return sendRedirect(String
+     * location) on the wrapped response object.
      */
     public void sendRedirect(String location) throws IOException {
-	this._getHttpServletResponse().sendRedirect(location);
+        this._getHttpServletResponse().sendRedirect(location);
     }
-    
+
     /**
-     * The default behavior of this method is to call setDateHeader(String name, long date)
-     * on the wrapped response object.
+     * The default behavior of this method is to call setDateHeader(String name,
+     * long date) on the wrapped response object.
      */
     public void setDateHeader(String name, long date) {
-	this._getHttpServletResponse().setDateHeader(name, date);
+        this._getHttpServletResponse().setDateHeader(name, date);
     }
-    
+
     /**
-     * The default behavior of this method is to call addDateHeader(String name, long date)
-     * on the wrapped response object.
+     * The default behavior of this method is to call addDateHeader(String name,
+     * long date) on the wrapped response object.
      */
-   public void addDateHeader(String name, long date) {
-	this._getHttpServletResponse().addDateHeader(name, date);
+    public void addDateHeader(String name, long date) {
+        this._getHttpServletResponse().addDateHeader(name, date);
     }
-    
+
     /**
-     * The default behavior of this method is to return setHeader(String name, String value)
-     * on the wrapped response object.
+     * The default behavior of this method is to return setHeader(String name,
+     * String value) on the wrapped response object.
      */
     public void setHeader(String name, String value) {
-	this._getHttpServletResponse().setHeader(name, value);
+        this._getHttpServletResponse().setHeader(name, value);
     }
-    
+
     /**
-     * The default behavior of this method is to return addHeader(String name, String value)
-     * on the wrapped response object.
+     * The default behavior of this method is to return addHeader(String name,
+     * String value) on the wrapped response object.
      */
-     public void addHeader(String name, String value) {
-	this._getHttpServletResponse().addHeader(name, value);
+    public void addHeader(String name, String value) {
+        this._getHttpServletResponse().addHeader(name, value);
     }
-    
+
     /**
-     * The default behavior of this method is to call setIntHeader(String name, int value)
-     * on the wrapped response object.
+     * The default behavior of this method is to call setIntHeader(String name,
+     * int value) on the wrapped response object.
      */
     public void setIntHeader(String name, int value) {
-	this._getHttpServletResponse().setIntHeader(name, value);
+        this._getHttpServletResponse().setIntHeader(name, value);
     }
-    
+
     /**
-     * The default behavior of this method is to call addIntHeader(String name, int value)
-     * on the wrapped response object.
+     * The default behavior of this method is to call addIntHeader(String name,
+     * int value) on the wrapped response object.
      */
     public void addIntHeader(String name, int value) {
-	this._getHttpServletResponse().addIntHeader(name, value);
+        this._getHttpServletResponse().addIntHeader(name, value);
     }
 
     /**
-     * The default behavior of this method is to call setStatus(int sc)
-     * on the wrapped response object.
+     * The default behavior of this method is to call setStatus(int sc) on the
+     * wrapped response object.
      */
-
-
     public void setStatus(int sc) {
-	this._getHttpServletResponse().setStatus(sc);
+        this._getHttpServletResponse().setStatus(sc);
     }
-    
+
     /**
-     * The default behavior of this method is to call setStatus(int sc, String sm)
-     * on the wrapped response object.
+     * The default behavior of this method is to call setStatus(int sc, String
+     * sm) on the wrapped response object.
+     * 
      * @deprecated As of Version 3.0 of the Java Servlet API
      */
-    @SuppressWarnings("dep-ann") // Spec API does not use @Deprecated
+    @SuppressWarnings("dep-ann")
+    // Spec API does not use @Deprecated
     public void setStatus(int sc, String sm) {
-	this._getHttpServletResponse().setStatus(sc, sm);
+        this._getHttpServletResponse().setStatus(sc, sm);
     }
 
-
     /**
-     * @since Servlet 3.0
-     * TODO SERVLET3 - Add comments
+     * @since Servlet 3.0 TODO SERVLET3 - Add comments
      */
     public int getStatus() {
         return this._getHttpServletResponse().getStatus();
     }
 
-     
     /**
-     * @since Servlet 3.0
-     * TODO SERVLET3 - Add comments
+     * @since Servlet 3.0 TODO SERVLET3 - Add comments
      */
     public String getHeader(String name) {
         return this._getHttpServletResponse().getHeader(name);
     }
 
-    
     /**
-     * @since Servlet 3.0
-     * TODO SERVLET3 - Add comments
+     * @since Servlet 3.0 TODO SERVLET3 - Add comments
      */
     public Collection<String> getHeaders(String name) {
         return this._getHttpServletResponse().getHeaders(name);
     }
 
     /**
-     * @since Servlet 3.0
-     * TODO SERVLET3 - Add comments
+     * @since Servlet 3.0 TODO SERVLET3 - Add comments
      */
     public Collection<String> getHeaderNames() {
         return this._getHttpServletResponse().getHeaderNames();
     }
-
 }
