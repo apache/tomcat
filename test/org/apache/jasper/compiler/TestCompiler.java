2ba20ed8f3c81c7103f09f40464035ec293768b2
Mark Thomas <markt@apache.org>
2010-08-23 19:23:28 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=49726 Specifying a default content type via a JSP property group should not prevent a page from setting some other content type Includes test cases
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@988262 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test/org/apache/jasper/compiler/TestCompiler.java
@@ -0,0 +1,77 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.jasper.compiler;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.startup.TomcatBaseTest;
+import org.apache.tomcat.util.buf.ByteChunk;
+
+public class TestCompiler extends TomcatBaseTest {
+    
+    public void testBug49726a() throws Exception {
+        Tomcat tomcat = getTomcatInstance();
+
+        File appDir = new File("test/webapp-3.0");
+        tomcat.addWebapp(null, "/test", appDir.getAbsolutePath());
+        tomcat.start();
+
+        ByteChunk res = new ByteChunk();
+        Map<String,List<String>> headers = new HashMap<String,List<String>>();
+        
+        getUrl("http://localhost:" + getPort() + "/test/bug49726a.jsp", res,
+                headers);
+
+        // Check request completed
+        String result = res.toString();
+        assertEcho(result, "OK");
+        
+        // Check content type
+        assertTrue(headers.get("Content-Type").get(0).startsWith("text/html"));
+    }
+
+    public void testBug49726b() throws Exception {
+        Tomcat tomcat = getTomcatInstance();
+
+        File appDir = new File("test/webapp-3.0");
+        tomcat.addWebapp(null, "/test", appDir.getAbsolutePath());
+        tomcat.start();
+
+        ByteChunk res = new ByteChunk();
+        Map<String,List<String>> headers = new HashMap<String,List<String>>();
+        
+        getUrl("http://localhost:" + getPort() + "/test/bug49726b.jsp", res,
+                headers);
+
+        // Check request completed
+        String result = res.toString();
+        assertEcho(result, "OK");
+        
+        // Check content type
+        assertTrue(headers.get("Content-Type").get(0).startsWith("text/plain"));
+    }
+
+    /** Assertion for text printed by tags:echo */
+    private static void assertEcho(String result, String expected) {
+        assertTrue(result.indexOf("<p>" + expected + "</p>") > 0);
+    }
+}
