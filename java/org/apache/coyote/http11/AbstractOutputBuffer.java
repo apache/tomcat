aad632dce54ad4f274ac58cd3449209260067c2e
Mark Emlyn David Thomas <markt@apache.org>
2010-08-24 10:49:33 +0000

    Fix logger visibility
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@988474 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http11/AbstractOutputBuffer.java
+++ java/org/apache/coyote/http11/AbstractOutputBuffer.java
@@ -108,7 +108,7 @@ public abstract class AbstractOutputBuffer implements OutputBuffer{
     /**
      * Logger.
      */
-    protected static org.apache.juli.logging.Log log
+    private static final org.apache.juli.logging.Log log
         = org.apache.juli.logging.LogFactory.getLog(AbstractOutputBuffer.class);
 
     // ------------------------------------------------------------- Properties
