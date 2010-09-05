d921e11ca70834e99d532401760fc13617ab1665
Mark Emlyn David Thomas <markt@apache.org>
2010-09-05 19:47:36 +0000

    Make sure acceptor threads are stopped when the connector is stopped. Also fix @Override warnings for the method
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@992881 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/net/AprEndpoint.java
+++ java/org/apache/tomcat/util/net/AprEndpoint.java
@@ -918,6 +918,7 @@ public class AprEndpoint extends AbstractEndpoint {
          * The background thread that listens for incoming TCP/IP connections and
          * hands them off to an appropriate processor.
          */
+        @Override
         public void run() {
 
             // Loop until we receive a shutdown command
