542a01909da225146e3917760003e215e617eb1f
Mark Thomas <markt@apache.org>
2010-10-06 21:46:43 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=49952 Allow ServletContainerInitializers to add listeners to a web application. Patch provided by David Jencks.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1005265 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test/org/apache/catalina/startup/TestListener.java
@@ -0,0 +1,123 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+package org.apache.catalina.startup;
+
+import java.util.Set;
+
+import javax.servlet.ServletContainerInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.ServletException;
+
+import org.apache.catalina.Context;
+
+public class TestListener extends TomcatBaseTest {
+
+    /**
+     * Check that a ServletContainerInitializer can install a
+     * {@link ServletContextListener} and that it gets initialized.
+     * @throws Exception
+     */
+    public void testServletContainerInitializer() throws Exception {
+        Tomcat tomcat = getTomcatInstance();
+
+        Context context = tomcat.addContext("/",
+                System.getProperty("java.io.tmpdir"));
+
+        context.addServletContainerInitializer(new SCI(), null);
+        tomcat.start();
+        assertTrue(SCL.initialized);
+    }
+
+    /**
+     * Check that a {@link ServletContextListener} cannot install a
+     * {@link ServletContextInitializer}.
+     * @throws Exception
+     */
+    public void testServletContextListener() throws Exception {
+        Tomcat tomcat = getTomcatInstance();
+
+        Context context = tomcat.addContext("/",
+                System.getProperty("java.io.tmpdir"));
+
+        // SCL2 pretends to be in web.xml, and tries to install a
+        // ServletContextInitializer.
+        context.addApplicationListener(SCL2.class.getName());
+        tomcat.start();
+
+        //check that the ServletContextInitializer wasn't initialized.
+        assertFalse(SCL3.initialized);
+    }
+
+    public static class SCI implements ServletContainerInitializer {
+
+        @Override
+        public void onStartup(Set<Class<?>> c, ServletContext ctx)
+                throws ServletException {
+            ctx.addListener(new SCL());
+        }
+    }
+
+    public static class SCL implements ServletContextListener {
+
+        static boolean initialized = false;
+
+        @Override
+        public void contextInitialized(ServletContextEvent sce) {
+            initialized = true;
+        }
+
+        @Override
+        public void contextDestroyed(ServletContextEvent sce) {
+            // NOOP
+        }
+    }
+    
+    public static class SCL2 implements ServletContextListener {
+
+        @Override
+        public void contextInitialized(ServletContextEvent sce) {
+            ServletContext sc = sce.getServletContext();
+            sc.addListener(SCL3.class.getName());
+        }
+
+        @Override
+        public void contextDestroyed(ServletContextEvent sce) {
+            // NOOP
+        }
+    }
+
+    public static class SCL3 implements ServletContextListener {
+
+        static boolean initialized = false;
+
+        @Override
+        public void contextInitialized(ServletContextEvent sce) {
+            initialized = true;
+        }
+
+        @Override
+        public void contextDestroyed(ServletContextEvent sce) {
+            // NOOP
+        }
+    }
+}
