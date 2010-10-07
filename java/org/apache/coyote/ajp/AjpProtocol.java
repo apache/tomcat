21521e80fabf06c7337e5811b350448691cf193f
Mark Thomas <markt@apache.org>
2010-10-07 14:21:11 +0000

    Fix import order
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1005458 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/ajp/AjpProtocol.java
+++ java/org/apache/coyote/ajp/AjpProtocol.java
@@ -31,11 +31,11 @@ import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
 import org.apache.tomcat.util.ExceptionUtils;
 import org.apache.tomcat.util.modeler.Registry;
+import org.apache.tomcat.util.net.AbstractEndpoint.Handler.SocketState;
 import org.apache.tomcat.util.net.JIoEndpoint;
+import org.apache.tomcat.util.net.JIoEndpoint.Handler;
 import org.apache.tomcat.util.net.SocketStatus;
 import org.apache.tomcat.util.net.SocketWrapper;
-import org.apache.tomcat.util.net.AbstractEndpoint.Handler.SocketState;
-import org.apache.tomcat.util.net.JIoEndpoint.Handler;
 
 
 /**
