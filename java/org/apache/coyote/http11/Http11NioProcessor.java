1ccc548885a903a3948ad863cb60b0b2efb7452c
Mark Emlyn David Thomas <markt@apache.org>
2010-10-12 12:10:43 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=50044 for NIO connector
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1021753 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http11/Http11NioProcessor.java
+++ java/org/apache/coyote/http11/Http11NioProcessor.java
@@ -647,7 +647,7 @@ public class Http11NioProcessor extends AbstractHttp11Processor {
             //notify poller if not on a tomcat thread
             RequestInfo rp = request.getRequestProcessor();
             if ( rp.getStage() != org.apache.coyote.Constants.STAGE_SERVICE ) //async handling
-                socket.getPoller().cometInterest(socket);
+                socket.getPoller().add(socket);
         } else if (actionCode == ActionCode.COMET_SETTIMEOUT) {
             if (param==null) return;
             if (socket==null || socket.getAttachment(false)==null) return;
