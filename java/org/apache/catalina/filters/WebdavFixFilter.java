f35853956fcd7868b2fe112c19483327c55ab3b1
Mark Thomas <markt@apache.org>
2010-11-11 10:16:13 +0000

    Typo / line-length
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1033868 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/filters/WebdavFixFilter.java
+++ java/org/apache/catalina/filters/WebdavFixFilter.java
@@ -33,8 +33,9 @@ import javax.servlet.http.HttpServletResponse;
  * a WebDAV client that actually works. Other workarounds that might help
  * include:
  * <ul>
- *   <li>Specifying the port, even if it is port 80, when trying to connect.</li>
- *   <li>Canceling the first authentication dialog box and then trying to
+ *   <li>Specifying the port, even if it is port 80, when trying to
+ *       connect.</li>
+ *   <li>Cancelling the first authentication dialog box and then trying to
  *       reconnect.</li>
  * </ul>
  * 
