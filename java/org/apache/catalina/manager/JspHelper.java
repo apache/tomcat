2f0f80a6c36d6875c64f8c857e2e26661a3865d4
Mark Thomas <markt@apache.org>
2010-11-22 17:19:07 +0000

    Fix CVE-2010-4172. Multiple XSS in Manager web application
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1037778 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/manager/JspHelper.java
+++ java/org/apache/catalina/manager/JspHelper.java
@@ -54,7 +54,7 @@ public class JspHelper {
     }
     private static String localeToString(Locale locale) {
         if (locale != null) {
-            return locale.toString();//locale.getDisplayName();
+            return escapeXml(locale.toString());//locale.getDisplayName();
         } else {
             return "";
         }
