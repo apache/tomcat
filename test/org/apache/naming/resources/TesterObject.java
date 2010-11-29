fed8164c594ede6a7a4fa278bc50cacac32ac54b
Mark Thomas <markt@apache.org>
2010-11-29 15:40:11 +0000

    Add a test case for https://issues.apache.org/bugzilla/show_bug.cgi?id=50351
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1040158 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/naming/resources/TesterObject.java
+++ test/org/apache/naming/resources/TesterObject.java
@@ -18,8 +18,18 @@ package org.apache.naming.resources;
 
 public class TesterObject {
 
+    private String foo;
+
     @Override
     public String toString() {
         return "This is a test object (" + super.toString() + ").";
+    }
+    
+    public void setFoo(String foo) {
+        this.foo = foo;
+    }
+    
+    public String getFoo() {
+        return this.foo;
     }
 }
