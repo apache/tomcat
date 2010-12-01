8fd046d9746cde4aa9df473078672c97be9d292c
Mark Emlyn David Thomas <markt@apache.org>
2010-12-01 18:37:29 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=50360 Bind/release socket on start()/stop() Register/deregister MBeans on init()/destroy()
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1041120 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http11/Http11NioProtocol.java
+++ java/org/apache/coyote/http11/Http11NioProtocol.java
@@ -76,39 +76,16 @@ public class Http11NioProtocol extends AbstractHttp11JsseProtocol {
      */
     @Override
     public void init() throws Exception {
-        endpoint.setName(getName());
-        ((NioEndpoint)endpoint).setHandler(cHandler);
         
-        try {
-            endpoint.init();
-            sslImplementation = new JSSEImplementation();
-        } catch (Exception ex) {
-            log.error(sm.getString("http11protocol.endpoint.initerror"), ex);
-            throw ex;
-        }
-        if(log.isInfoEnabled())
-            log.info(sm.getString("http11protocol.init", getName()));
+        ((NioEndpoint)endpoint).setHandler(cHandler);
 
+        super.init();
     }
 
     @Override
     public void start() throws Exception {
-        if( this.domain != null ) {
-            try {
-                tpOname=new ObjectName
-                    (domain + ":" + "type=ThreadPool,name=" + getName());
-                Registry.getRegistry(null, null)
-                .registerComponent(endpoint, tpOname, null );
-            } catch (Exception e) {
-                log.error("Can't register threadpool" );
-            }
-            rgOname=new ObjectName
-                (domain + ":type=GlobalRequestProcessor,name=" + getName());
-            Registry.getRegistry(null, null).registerComponent
-                ( cHandler.global, rgOname, null );
-        }
-
         try {
+            sslImplementation = new JSSEImplementation();
             endpoint.start();
         } catch (Exception ex) {
             log.error(sm.getString("http11protocol.endpoint.starterror"), ex);
@@ -116,6 +93,12 @@ public class Http11NioProtocol extends AbstractHttp11JsseProtocol {
         }
         if(log.isInfoEnabled())
             log.info(sm.getString("http11protocol.start", getName()));
+    }
+
+
+    @Override
+    protected RequestGroupInfo getRequestGroupInfo() {
+        return cHandler.global;
     }
 
 
