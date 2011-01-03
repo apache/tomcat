73557727428e69a3db2446df622c811324233b06
Rainer Jung <rjung@apache.org>
2011-01-03 14:00:29 +0000

    Update Copyright year.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1054620 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/manager/HTMLManagerServlet.java
+++ java/org/apache/catalina/manager/HTMLManagerServlet.java
@@ -788,7 +788,7 @@ public final class HTMLManagerServlet extends ManagerServlet {
      */
     @Override
     public String getServletInfo() {
-        return "HTMLManagerServlet, Copyright (c) 1999-2010, The Apache Software Foundation";
+        return "HTMLManagerServlet, Copyright (c) 1999-2011, The Apache Software Foundation";
     }   
     
     /**
