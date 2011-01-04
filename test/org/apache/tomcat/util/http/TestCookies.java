280aa62a88f8d871851c18f6c81d95cfeeab8d8b
Mark Thomas <markt@apache.org>
2011-01-04 19:20:28 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=49000 Make accepting name only cookies configurable, defaulting to disabled.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1055143 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/tomcat/util/http/TestCookies.java
+++ test/org/apache/tomcat/util/http/TestCookies.java
@@ -102,12 +102,12 @@ public class TestCookies extends TestCase {
 
     public void testNameOnlyCookies() throws Exception {
         // Bug 49000
-        test("fred=1; jim=2; bob", "fred", "1", "jim", "2", "bob", "");
-        test("fred=1; jim=2; bob; george=3", "fred", "1", "jim", "2", "bob", "",
+        test("fred=1; jim=2; bob", "fred", "1", "jim", "2");
+        test("fred=1; jim=2; bob; george=3", "fred", "1", "jim", "2",
                 "george", "3");
         test("fred=1; jim=2; bob=; george=3", "fred", "1", "jim", "2",
-                "bob", "", "george", "3");
-        test("fred=1; jim=2; bob=", "fred", "1", "jim", "2", "bob", "");
+                "george", "3");
+        test("fred=1; jim=2; bob=", "fred", "1", "jim", "2");
     }
 
 
