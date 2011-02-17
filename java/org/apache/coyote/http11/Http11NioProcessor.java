4061e719df321d059644df157ad6dfa4c958407b
Rainer Jung <rjung@apache.org>
2011-02-17 09:51:17 +0000

    Remove duplicate code.
    The implementation in the base class is identical and all other http11 implementations also inherit it from there.
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1071560 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http11/Http11NioProcessor.java
+++ java/org/apache/coyote/http11/Http11NioProcessor.java
@@ -987,22 +987,6 @@ public class Http11NioProcessor extends AbstractHttp11Processor {
 
     }
 
-    /**
-     * Determine if we must drop the connection because of the HTTP status
-     * code.  Use the same list of codes as Apache/httpd.
-     */
-    @Override
-    protected boolean statusDropsConnection(int status) {
-        return status == 400 /* SC_BAD_REQUEST */ ||
-               status == 408 /* SC_REQUEST_TIMEOUT */ ||
-               status == 411 /* SC_LENGTH_REQUIRED */ ||
-               status == 413 /* SC_REQUEST_ENTITY_TOO_LARGE */ ||
-               status == 414 /* SC_REQUEST_URI_TOO_LONG */ ||
-               status == 500 /* SC_INTERNAL_SERVER_ERROR */ ||
-               status == 503 /* SC_SERVICE_UNAVAILABLE */ ||
-               status == 501 /* SC_NOT_IMPLEMENTED */;
-    }
-     
     @Override
     protected AbstractInputBuffer getInputBuffer() {
         return inputBuffer;
