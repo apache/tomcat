2d7dbfe4c63a4242a9b28fdb662d91ceb7a84630
Mark Emlyn David Thomas <markt@apache.org>
2011-03-03 11:24:35 +0000

    CVE-2011-1088 Fix typo
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1076587 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/authenticator/AuthenticatorBase.java
+++ java/org/apache/catalina/authenticator/AuthenticatorBase.java
@@ -482,7 +482,7 @@ public abstract class AuthenticatorBase extends ValveBase
         // The Servlet may specify security constraints through annotations.
         // Ensure that they have been processed before constraints are checked
         Wrapper wrapper = (Wrapper) request.getMappingData().wrapper; 
-        if (wrapper.getServlet() != null) {
+        if (wrapper.getServlet() == null) {
             wrapper.load();
         }
 
