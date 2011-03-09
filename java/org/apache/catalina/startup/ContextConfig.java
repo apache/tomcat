3e5b0455483eed55752047073e92403bfca8d3ec
Mark Emlyn David Thomas <markt@apache.org>
2011-03-09 12:20:58 +0000

    CVE-2011-1088 Fix unit test failures
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1079769 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/startup/ContextConfig.java
+++ java/org/apache/catalina/startup/ContextConfig.java
@@ -366,11 +366,16 @@ public class ContextConfig
      */
     protected synchronized void authenticatorConfig() {
 
-        // Always need an authenticator to support @ServletSecurity annotations
         LoginConfig loginConfig = context.getLoginConfig();
         if (loginConfig == null) {
-            loginConfig = DUMMY_LOGIN_CONFIG;
-            context.setLoginConfig(loginConfig);
+            if (context.getIgnoreAnnotations())  {
+                return;
+            } else {
+                // Not metadata-complete, need an authenticator to support
+                // @ServletSecurity annotations
+                loginConfig = DUMMY_LOGIN_CONFIG;
+                context.setLoginConfig(loginConfig);
+            }
         }
 
         // Has an authenticator been configured already?
