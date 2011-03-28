9a5edb71159a556ad50070f082a33cc2bd641258
Mark Emlyn David Thomas <markt@apache.org>
2011-03-28 19:15:06 +0000

    Move the processor.recycle calls to just before the point where the processor is returned to the pool. This ensures returned processors are recycled (this could have been skipped on some exception paths) Possible contributing factor to https://issues.apache.org/bugzilla/show_bug.cgi?id=50957 ? This is part of the fix for CVE-2011-1475
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1086349 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/ajp/AjpAprProcessor.java
+++ java/org/apache/coyote/ajp/AjpAprProcessor.java
@@ -316,7 +316,6 @@ public class AjpAprProcessor extends AbstractAjpProcessor {
 
             rp.setStage(org.apache.coyote.Constants.STAGE_KEEPALIVE);
             recycle();
-
         }
 
         // Add the socket to the poller
@@ -329,12 +328,10 @@ public class AjpAprProcessor extends AbstractAjpProcessor {
         rp.setStage(org.apache.coyote.Constants.STAGE_ENDED);
         
         if (error || endpoint.isPaused()) {
-            recycle();
             return SocketState.CLOSED;
         } else if (isAsync()) {
             return SocketState.LONG;
         } else {
-            recycle();
             return SocketState.OPEN;
         }
     }
@@ -369,14 +366,12 @@ public class AjpAprProcessor extends AbstractAjpProcessor {
         if (isAsync()) {
             if (error) {
                 request.updateCounters();
-                recycle();
                 return SocketState.CLOSED;
             } else {
                 return SocketState.LONG;
             }
         } else {
             request.updateCounters();
-            recycle();
             if (error) {
                 return SocketState.CLOSED;
             } else {
--- java/org/apache/coyote/ajp/AjpProtocol.java
+++ java/org/apache/coyote/ajp/AjpProtocol.java
@@ -189,6 +189,7 @@ public class AjpProtocol extends AbstractAjpProtocol {
                     return processor.asyncPostProcess();
                 } else {
                     socket.setAsync(false);
+                    processor.recycle();
                     recycledProcessors.offer(processor);
                 }
                 return state;
@@ -211,6 +212,7 @@ public class AjpProtocol extends AbstractAjpProtocol {
                 // less-than-verbose logs.
                 log.error(sm.getString("ajpprotocol.proto.error"), e);
             }
+            processor.recycle();
             recycledProcessors.offer(processor);
             return SocketState.CLOSED;
         }
