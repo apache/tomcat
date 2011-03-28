57f9f75c5bc11c8090aed54fb74df3b3fad48c29
Mark Thomas <markt@apache.org>
2011-03-28 19:19:23 +0000

    Protect against things going wrong during access logging. An un-handled exception here could trigger some unexpected code paths. Possible contributing factor to https://issues.apache.org/bugzilla/show_bug.cgi?id=50957 ? This is part of the fix for CVE-2011-1475
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1086352 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/connector/CoyoteAdapter.java
+++ java/org/apache/catalina/connector/CoyoteAdapter.java
@@ -482,8 +482,13 @@ public class CoyoteAdapter implements Adapter {
                 (connector.getURIEncoding());
         }
         
-        connector.getService().getContainer().logAccess(
-                request, response, time, true);
+        try {
+            connector.getService().getContainer().logAccess(
+                    request, response, time, true);
+        } catch (Throwable t) {
+            ExceptionUtils.handleThrowable(t);
+            log.warn(sm.getString("coyoteAdapter.accesslogFail"), t);
+        }
         
         if (create) {
             request.recycle();
