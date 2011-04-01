f7c03b74c91dec429eb71b16f52ba6870d3dc8a1
Mark Emlyn David Thomas <markt@apache.org>
2011-04-01 17:19:39 +0000

    Correct a Javadoc mix-up
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1087819 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/Realm.java
+++ java/org/apache/catalina/Realm.java
@@ -108,8 +108,8 @@ public interface Realm {
 
 
     /**
-     * Return the Principal associated with the specified chain of X509
-     * client certificates.  If there is none, return <code>null</code>.
+     * Return the Principal associated with the specified {@link GSSContext}.
+     * If there is none, return <code>null</code>.
      *
      * @param gssContext The gssContext processed by the {@link Authenticator}.
      * @param storeCreds Should the realm attempt to store the delegated
@@ -119,10 +119,10 @@ public interface Realm {
     
     
     /**
-     * Return the Principal associated with the specified {@link GSSContext}.
-     * If there is none, return <code>null</code>.
+     * Return the Principal associated with the specified chain of X509
+     * client certificates.  If there is none, return <code>null</code>.
      *
-     * @param gssContext Array of client certificates, with the first one in
+     * @param certs Array of client certificates, with the first one in
      *  the array being the certificate of the client itself.
      */
     public Principal authenticate(X509Certificate certs[]);
