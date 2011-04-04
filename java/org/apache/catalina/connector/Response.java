apache/tomcat

commit 50a1d0b550e318572636b409e55a4e1329029932

Update for April draft of servlet spec. Some 3.0 stuff that was complete will need to be re-done
git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@767956 13f79535-47bb-0310-9956-ffa450edef68

java/org/apache/catalina/connector/Response.java
@@ -30,6 +30,7 @@ import java.security.PrivilegedExceptionAction;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.Locale;
 import java.util.TimeZone;
 import java.util.Vector;
@@ -883,16 +884,15 @@ public class Response
 
 
     /**
-     * Return an array of all the header names set for this response, or
-     * a zero-length array if no headers have been set.
+     * Return an Iterable of all the header names set for this response.
      */
-    public String[] getHeaderNames() {
+    public Iterable<String> getHeaderNames() {
 
         MimeHeaders headers = coyoteResponse.getMimeHeaders();
         int n = headers.size();
-        String[] result = new String[n];
+        List<String> result = new ArrayList<String>(n);
         for (int i = 0; i < n; i++) {
-            result[i] = headers.getName(i).toString();
+            result.add(headers.getName(i).toString());
         }
         return result;
 
@@ -900,13 +900,12 @@ public class Response
 
 
     /**
-     * Return an array of all the header values associated with the
-     * specified header name, or an zero-length array if there are no such
-     * header values.
+     * Return an Iterable of all the header values associated with the
+     * specified header name.
      *
      * @param name Header name to look up
      */
-    public String[] getHeaderValues(String name) {
+    public Iterable<String> getHeaders(String name) {
 
         Enumeration<String> enumeration =
             coyoteResponse.getMimeHeaders().values(name);
@@ -914,10 +913,7 @@ public class Response
         while (enumeration.hasMoreElements()) {
             result.addElement(enumeration.nextElement());
         }
-        String[] resultArray = new String[result.size()];
-        result.copyInto(resultArray);
-        return resultArray;
-
+        return result;
     }
 
 

java/org/apache/catalina/valves/AccessLogValve.java
@@ -28,6 +28,7 @@ import java.net.InetAddress;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 import java.util.TimeZone;
 
@@ -1284,16 +1285,15 @@ public class AccessLogValve
         public void addElement(StringBuffer buf, Date date, Request request,
                 Response response, long time) {
            if (null != response) {
-                String[] values = response.getHeaderValues(header);
-                if(values.length > 0) {
-                    for (int i = 0; i < values.length; i++) {
-                        String string = values[i];
-                        buf.append(string) ;
-                        if(i+1<values.length)
-                            buf.append(",");
+                Iterator<String> iter = response.getHeaders(header).iterator();
+                boolean first = true;
+                while (iter.hasNext()) {
+                    if (!first) {
+                        buf.append(",");
                     }
-                    return ;
+                    buf.append(iter.next());
                 }
+                return ;
             }
             buf.append("-");
         }

java/javax/servlet/AsyncContext.java
@@ -1,0 +1,66 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package javax.servlet;
+
+/**
+ * @since 3.0
+ * $Id$
+ * TODO SERVLET3
+ */
+public interface AsyncContext {
+    public static final String ASYNC_REQUEST_URI =
+        "javax.servlet.async.request_uri";
+    public static final String ASYNC_CONTEXT_PATH  =
+        "javax.servlet.async.context_path";
+    public static final String ASYNC_PATH_INFO =
+        "javax.servlet.async.path_info";
+    public static final String ASYNC_SERVLET_PATH =
+        "javax.servlet.async.servlet_path";
+    public static final String ASYNC_QUERY_STRING =
+        "javax.servlet.async.query_string";
+    
+    ServletRequest getRequest();
+    
+    ServletResponse getResponse();
+    
+    boolean hasOriginalRequestAndResponse();
+    
+    /**
+     * 
+     * @throws IllegalStateException
+     */
+    void dispatch();
+    
+    /**
+     * 
+     * @param path
+     * @throws IllegalStateException
+     */
+    void dispatch(String path);
+    
+    /**
+     * 
+     * @param context
+     * @param path
+     * @throws IllegalStateException
+     */
+    void dispatch(ServletContext context, String path);
+    
+    void complete();
+    
+    void start(Runnable run);
+}

java/javax/servlet/FilterRegistration.java
@@ -16,15 +16,38 @@
 */
 package javax.servlet;
 
-import java.io.IOException;
-import java.util.EventListener;
+import java.util.EnumSet;
 
 /**
  * @since 3.0
  * $Id$
  * TODO SERVLET3 - Add comments
  */
-public interface AsyncListener extends EventListener {
-    void onComplete(AsyncEvent event) throws IOException;
-    void onTimeout(AsyncEvent event) throws IOException;
+public interface FilterRegistration extends Registration {
+    
+    /**
+     * 
+     * @param dispatcherTypes
+     * @param isMatchAfter
+     * @param servletNames
+     * @throws IllegalArgumentException
+     * @throws IllegalStateException
+     */
+    public void addMappingForServletNames(EnumSet<DispatcherType> dispatcherTypes,
+            boolean isMatchAfter, String... servletNames);
+    
+    /**
+     * 
+     * @param dispatcherTypes
+     * @param isMatchAfter
+     * @param urlPatterns
+     * @throws IllegalArgumentException
+     * @throws IllegalStateException
+     */
+    public void addMappingForUrlPatterns(EnumSet<DispatcherType> dispatcherTypes,
+            boolean isMatchAfter, String... urlPatterns);
+
+    public static interface Dynamic
+    extends FilterRegistration, Registration.Dynamic {
+    }
 }

java/javax/servlet/Registration.java
@@ -16,14 +16,28 @@
 */
 package javax.servlet;
 
+import java.util.Map;
+import java.util.Set;
+
 /**
  * @since 3.0
  * $Id$
+ * TODO SERVLET3 - Add comments
  */
-public enum DispatcherType {
-    FORWARD,
-    INCLUDE,
-    REQUEST,
-    ASYNC,
-    ERROR
+public interface Registration {
+    
+    public boolean setInitParameter(String name, String value)
+    throws IllegalArgumentException, IllegalStateException;
+
+    public Set<String> setInitParameters(Map<String,String> initParameters)
+    throws IllegalArgumentException, IllegalStateException;
+
+    public interface Dynamic {
+        
+        public void setDescription(String description)
+        throws IllegalStateException;
+        
+        public void setAsyncSupported(boolean isAsyncSupported)
+        throws IllegalStateException;
+    }
 }

java/javax/servlet/RequestDispatcher.java
@@ -43,7 +43,38 @@ import java.io.IOException;
  
 public interface RequestDispatcher {
 
-
+    public static final String ERROR_EXCEPTION =
+        "javax.servlet.error.exception";
+    public static final String ERROR_EXCEPTION_TYPE =
+        "javax.servlet.error.exception_type";
+    public static final String ERROR_MESSAGE =
+        "javax.servlet.error.message";
+    public static final String ERROR_REQUEST_URI =
+        "javax.servlet.error.request_uri";
+    public static final String ERROR_SERVLET_NAME =
+        "javax.servlet.error.servlet_name";
+    public static final String ERROR_STATUS_CODE =
+        "javax.servlet.error.status_code";
+    public static final String FORWARD_CONTEXT_PATH =
+        "javax.servlet.forward.context_path";
+    public static final String FORWARD_PATH_INFO =
+        "javax.servlet.forward.path_info";
+    public static final String FORWARD_QUERY_STRING =
+        "javax.servlet.forward.query_string";
+    public static final String FORWARD_REQUEST_URI =
+        "javax.servlet.forward.request_uri";
+    public static final String FORWARD_SERVLET_PATH =
+        "javax.servlet.forward.servlet_path";
+    public static final String INCLUDE_CONTEXT_PATH =
+        "javax.servlet.include.context_path";
+    public static final String INCLUDE_PATH_INFO =
+        "javax.servlet.include.path_info";
+    public static final String INCLUDE_QUERY_STRING =
+        "javax.servlet.include.query_string";
+    public static final String INCLUDE_REQUEST_URI =
+        "javax.servlet.include.request_uri";
+    public static final String INCLUDE_SERVLET_PATH =
+        "javax.servlet.include.servlet_path";
 
 
 

java/javax/servlet/ServletContainerInitializer.java
@@ -19,11 +19,9 @@ package javax.servlet;
 /**
  * @since 3.0
  * $Id$
+ * TODO SERVLET3 - Add comments
  */
-public enum DispatcherType {
-    FORWARD,
-    INCLUDE,
-    REQUEST,
-    ASYNC,
-    ERROR
+public interface ServletContainerInitializer {
+    public void onStartup(java.util.Set<java.lang.Class<?>> c,
+            ServletContext ctx);
 }

java/javax/servlet/ServletContext.java
@@ -21,9 +21,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.EnumSet;
 import java.util.Enumeration;
-import java.util.Map;
 import java.util.Set;
-
 
 /**
  * 
@@ -57,6 +55,7 @@ import java.util.Set;
 
 public interface ServletContext {
 
+    public static final String TEMPDIR = "javax.servlet.context.tempdir";
 
     /**
      * Returns a <code>ServletContext</code> object that 
@@ -527,7 +526,17 @@ public interface ServletContext {
     public Enumeration<String> getInitParameterNames();
     
     
-
+    /**
+     * 
+     * @param name
+     * @param value
+     * @return
+     * @throws IllegalStateException
+     * @since Servlet 3.0
+     */
+    public boolean setInitParameter(String name, String value);
+    
+    
     /**
      * Returns the servlet container attribute with the given name, 
      * or <code>null</code> if there is no attribute by that name.
@@ -648,91 +657,118 @@ public interface ServletContext {
     /**
      * 
      * @param servletName
-     * @param description
      * @param className
-     * @param initParameters
-     * @param loadOnStartup
-     * @throws IllegalArgumentException If the servlet name already exists
      * @throws IllegalStateException    If the context has already been
      *                                  initialised
      * @since 3.0
      */
-    public void addServlet(String servletName, String description,
-            String className, Map<String,String> initParameters,
-            int loadOnStartup)
-            throws IllegalArgumentException, IllegalStateException;
+    public ServletRegistration.Dynamic addServlet(String servletName,
+            String className);
     
     
     /**
      * 
      * @param servletName
-     * @param urlPatterns
-     * @throws IllegalArgumentException If urlPatters is null or empty
+     * @param servlet
+     * @since 3.0
      * @throws IllegalStateException    If the context has already been
      *                                  initialised
-     * 
-     * @since 3.0
      */
-    public void addServletMapping(String servletName, String[] urlPatterns)
-            throws IllegalArgumentException, IllegalStateException;
+    public ServletRegistration.Dynamic addServlet(String servletName,
+            Servlet servlet);
+    
+    
+    /**
+     * 
+     * @param servletName
+     * @param servletClass
+     * @since 3.0
+     * @throws IllegalStateException    If the context has already been
+     *                                  initialised
+     */
+    public ServletRegistration.Dynamic addServlet(String servletName,
+            Class<? extends Servlet> servletClass);
+    
+
+    /**
+     * 
+     * @param c
+     * @return
+     * @throws ServletException
+     * @since Servlet 3.0
+     */
+    public <T extends Servlet> T createServlet(Class<T> c)
+    throws ServletException;
+
+    
+    /**
+     * 
+     * @param servletName
+     * @return
+     * @since Servlet 3.0 
+     */
+    public ServletRegistration findServletRegistration(String servletName);
+
 
     /**
      * 
      * @param filterName
-     * @param description
      * @param className
-     * @param initParameters
-     * @throws IllegalArgumentException If the filter name already exists
      * @throws IllegalStateException    If the context has already been
      *                                  initialised
      * 
      * @since 3.0
      */
-    public void addFilter(String filterName, String description,
-            String className, Map<String,String> initParameters)
-            throws IllegalArgumentException, IllegalStateException;
+    public FilterRegistration.Dynamic  addFilter(String filterName,
+            String className);
+    
     
     /**
      * 
      * @param filterName
-     * @param dispatcherTypes
-     * @param isMatchAfter
-     * @param servletNames
-     * @throws IllegalArgumentException If servletNames is null or empty
+     * @param filter
      * @throws IllegalStateException    If the context has already been
      *                                  initialised
+     * 
      * @since 3.0
      */
-    public void addFilterMappingForServletNames(String filterName,
-            EnumSet<DispatcherType> dispatcherTypes, boolean isMatchAfter,
-            String... servletNames)
-            throws IllegalArgumentException, IllegalStateException;
+    public FilterRegistration.Dynamic  addFilter(String filterName,
+            Filter filter);
+
     
     /**
      * 
      * @param filterName
-     * @param dispatcherTypes
-     * @param isMatchAfter
-     * @param urlPatterns
-4     *
+     * @param filterClass
+     * @throws IllegalStateException    If the context has already been
+     *                                  initialised
+     * 
      * @since 3.0
      */
-    public void addFilterMappingForUrlPatterns(String filterName,
-            EnumSet<DispatcherType> dispatcherTypes, boolean isMatchAfter,
-            String... urlPatterns)
-            throws IllegalArgumentException, IllegalStateException;
+    public FilterRegistration.Dynamic  addFilter(String filterName,
+            Class<? extends Filter> filterClass);
     
 
     /**
      * 
-     * @param sessionCookieConfig
-     * @throws IllegalStateException    If the context has already been
-     *                                  initialised
-     * @since 3.0
+     * @param c
+     * @return
+     * @throws ServletException
+     * @since Servlet 3.0
      */
-    public void setSessionCookieConfig(SessionCookieConfig sessionCookieConfig)
-            throws IllegalStateException;
+    public <T extends Filter> T createFilter(Class<T> c)
+    throws ServletException;
+
     
+    /**
+     * 
+     * @param filterName
+     * @return
+     * @since Servlet 3.0 
+     */
+    public FilterRegistration findFilterRegistration(String filterName);
+
+
     /**
      * 
      * @return
@@ -769,11 +805,4 @@ public interface ServletContext {
      */
     public EnumSet<SessionTrackingMode> getEffectiveSessionTrackingModes();
     
-    /**
-     * 
-     * @param path
-     * @return
-     * @since 3.0
-     */
-    public AsyncDispatcher getAsyncDispatcher(String path);
 }

commit 1c47e5cb8e1c0ab810588266a6a54ea3d792f27c

Fix display of response headers in AccessLogValve: 1. When no headers are there, display "-" instead of empty string 2. Do not forget "," between multiple values TC6 already does that but it was broken here in r767956
git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1088458 13f79535-47bb-0310-9956-ffa450edef68

java/org/apache/catalina/connector/Response.java
@@ -913,7 +913,7 @@ public class Response
 
 
     /**
-     * Return an Iterable of all the header values associated with the
+     * Return a Collection of all the header values associated with the
      * specified header name.
      *
      * @param name Header name to look up

java/org/apache/catalina/valves/AccessLogValve.java
@@ -1290,18 +1290,17 @@ public class AccessLogValve extends ValveBase implements AccessLog {
         @Override
         public void addElement(StringBuilder buf, Date date, Request request,
                 Response response, long time) {
-           if (null != response) {
+            if (null != response) {
                 Iterator<String> iter = response.getHeaders(header).iterator();
-                boolean first = true;
-                while (iter.hasNext()) {
-                    if (!first) {
-                        buf.append(",");
-                    }
+                if (iter.hasNext()) {
                     buf.append(iter.next());
+                    while (iter.hasNext()) {
+                        buf.append(',').append(iter.next());
+                    }
+                    return;
                 }
-                return ;
             }
-            buf.append("-");
+            buf.append('-');
         }
     }
     