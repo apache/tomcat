1d483bfb3db0f4b7eb0e84a74e0ce5894fa4ba96
Mark Thomas <markt@apache.org>
2011-05-05 13:39:09 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=47371 Correctly coerce the empty string to zero when used as an operand in EL arithmetic. Patch provided by gbt.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1099789 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/el/lang/ELArithmetic.java
+++ java/org/apache/el/lang/ELArithmetic.java
@@ -394,13 +394,12 @@ public abstract class ELArithmetic {
         if (isNumber(obj)) {
             return coerce((Number) obj);
         }
-        if (obj instanceof String) {
-            return coerce((String) obj);
-        }
         if (obj == null || "".equals(obj)) {
             return coerce(ZERO);
         }
-
+        if (obj instanceof String) {
+            return coerce((String) obj);
+        }
         if (obj instanceof Character) {
             return coerce(Short.valueOf((short) ((Character) obj).charValue()));
         }
