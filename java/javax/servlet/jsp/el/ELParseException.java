d86139d6c59d67bd858124a437f88cae493e9f4e
Mark Emlyn David Thomas <markt@apache.org>
2011-05-06 08:05:42 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=51155 Add missing comments to @deprecated code Patch provided by sebb
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1100125 13f79535-47bb-0310-9956-ffa450edef68

--- java/javax/servlet/jsp/el/ELParseException.java
+++ java/javax/servlet/jsp/el/ELParseException.java
@@ -22,7 +22,7 @@ package javax.servlet.jsp.el;
  * Represents a parsing error encountered while parsing an EL expression.
  *
  * @since 2.0
- * @deprecated
+ * @deprecated As of JSP 2.1, replaced by javax.el.ELException
  */
 @SuppressWarnings("dep-ann") // TCK signature test fails with annotation
 public class ELParseException extends ELException {
