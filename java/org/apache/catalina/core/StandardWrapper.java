ad1524e6fd1de51e816569c9935403ee074e999f
Mark Emlyn David Thomas <markt@apache.org>
2011-05-08 22:53:32 +0000

    Use the correct classloader This is the fix for CVE-2011-1582
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1100832 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/core/StandardWrapper.java
+++ java/org/apache/catalina/core/StandardWrapper.java
@@ -1136,7 +1136,8 @@ public class StandardWrapper extends ContainerBase
         if (getServlet() == null) {
             Class<?> clazz = null;
             try {
-                clazz = getParentClassLoader().loadClass(getServletClass());
+                clazz = getParent().getLoader().getClassLoader().loadClass(
+                        getServletClass());
                 processServletSecurityAnnotation(clazz);
             } catch (ClassNotFoundException e) {
                 // Safe to ignore. No class means no annotations to process
