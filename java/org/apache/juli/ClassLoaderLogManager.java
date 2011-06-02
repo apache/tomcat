f8d38261bf8feff7de948cac66f45a795bd62449
Mark Thomas <markt@apache.org>
2011-06-02 16:11:16 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=51249 Correct system property replacement code so properties of the form ${...}${...} can be used without error.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1130625 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/juli/ClassLoaderLogManager.java
+++ java/org/apache/juli/ClassLoaderLogManager.java
@@ -568,7 +568,7 @@ public class ClassLoaderLogManager extends LogManager {
         String result = str;
         int pos_start = result.indexOf("${");
         if (pos_start != -1) {
-            int pos_end = result.indexOf('}');
+            int pos_end = result.indexOf('}', pos_start);
             if (pos_end != -1) {
                 String propName = result.substring(pos_start + 2, pos_end);
                 String replacement = System.getProperty(propName);
