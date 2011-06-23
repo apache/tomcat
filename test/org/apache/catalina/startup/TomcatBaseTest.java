e2bc1132e854411114febe1517631bd2fc4aac80
Rainer Jung <rjung@apache.org>
2011-06-23 11:01:02 +0000

    Use system properties loaded from catalina.properties via the class path in unit tests.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1138828 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/catalina/startup/TomcatBaseTest.java
+++ test/org/apache/catalina/startup/TomcatBaseTest.java
@@ -106,37 +106,8 @@ public abstract class TomcatBaseTest extends TestCase {
         }
         
         System.setProperty("catalina.base", tempDir.getAbsolutePath());
-        System.setProperty("tomcat.util.scan.DefaultJarScanner.jarsToSkip",
-                "bootstrap.jar,commons-daemon.jar,tomcat-juli.jar," +
-                "annotations-api.jar,el-api.jar,jsp-api.jar,servlet-api.jar," +
-                "catalina.jar,catalina-ant.jar,catalina-ha.jar," +
-                "catalina-tribes.jar," +
-                "jasper.jar,jasper-el.jar,ecj-*.jar," +
-                "tomcat-api.jar,tomcat-util.jar,tomcat-coyote.jar," +
-                "tomcat-dbcp.jar," +
-                "tomcat-i18n-en.jar,tomcat-i18n-es.jar,tomcat-i18n-fr.jar," +
-                "tomcat-i18n-ja.jar," +
-                "tomcat-juli-adapters.jar,catalina-jmx-remote.jar,catalina-ws.jar," +
-                "commons-beanutils*.jar,commons-collections*.jar," +
-                "commons-dbcp*.jar,commons-digester*.jar," +
-                "commons-fileupload*.jar,commons-io*.jar,commons-lang*.jar," +
-                "commons-logging*.jar," +
-                "commons-math*.jar,commons-pool*.jar," +
-                "jstl.jar," +
-                "geronimo-spec-jaxrpc*.jar,wsdl4j*.jar," +
-                "ant.jar,ant-junit*.jar,jmx.jar,h2*.jar,hibernate*.jar," +
-                "httpclient*.jar," +
-                "jmx-tools.jar,jta*.jar,log4j*.jar,mail*.jar,slf4j*.jar," +
-                "xercesImpl.jar,xmlParserAPIs.jar,xml-apis.jar," +
-                "dnsns.jar,ldapsec.jar,localedata.jar,sunjce_provider.jar," +
-                "sunmscapi.jar," +
-                "sunpkcs11.jar,jhall.jar,tools.jar," +
-                "apple_provider.jar,AppleScriptEngine.jar,CoreAudio.jar," +
-                "dns_sd.jar," +
-                "j3daudio.jar,j3dcore.jar,j3dutils.jar," +
-                "jai_core.jar,jai_codec.jar," +
-                "mlibwrapper_jai.jar,MRJToolkit.jar,vecmath.jar," +
-                "junit.jar,junit-*.jar,ant-launcher.jar");
+        // Trigger loading of catalina.properties
+        CatalinaProperties.getProperty("foo");
         
         File appBase = new File(tempDir, "webapps");
         if (!appBase.exists() && !appBase.mkdir()) {
