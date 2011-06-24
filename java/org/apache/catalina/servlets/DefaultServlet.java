65011b7b025451bf11039350aee0c06f537caa9e
Rainer Jung <rjung@apache.org>
2011-06-24 12:48:20 +0000

    Fix IllegalStateException for JavaScript files when switching from Writer to OutputStream.
    The special handling of this case in the
    DefaultServlet was broken due to a MIME type
    change for JavaScript.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1139280 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/servlets/DefaultServlet.java
+++ java/org/apache/catalina/servlets/DefaultServlet.java
@@ -877,7 +877,8 @@ public class DefaultServlet
                 // trying to serve a text file
                 if ( (contentType == null)
                         || (contentType.startsWith("text"))
-                        || (contentType.endsWith("xml")) ) {
+                        || (contentType.endsWith("xml"))
+                        || (contentType.contains("/javascript")) ) {
                     writer = response.getWriter();
                     // Cannot reliably serve partial content with a Writer
                     ranges = FULL;
