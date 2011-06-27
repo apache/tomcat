5fa3732c0a165d16043d4fa470a3354cd5b09558
Mark Thomas <markt@apache.org>
2011-06-27 09:27:06 +0000

    Fix CVE-2011-2204. Prevent user passwords appearing in log files if a runtime exception (e.g. OOME) occurs while creating a new user for a MemoryUserDatabase via JMX.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1140070 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/users/MemoryUserDatabase.java
+++ java/org/apache/catalina/users/MemoryUserDatabase.java
@@ -585,7 +585,7 @@ public class MemoryUserDatabase implements UserDatabase {
             values = getUsers();
             while (values.hasNext()) {
                 writer.print("  ");
-                writer.println(values.next());
+                writer.println(((MemoryUser) values.next()).toXml());
             }
 
             // Print the file epilog
