7ecd0d7fcdad9e6aa9fc7b5ba406138f03b57112
Mark Emlyn David Thomas <markt@apache.org>
2011-06-29 12:12:48 +0000

    Ensure an access log entry is made if an error occurs during asynchronous request processing and the socket is immediately closed.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1141066 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/ajp/AbstractAjpProcessor.java
+++ java/org/apache/coyote/ajp/AbstractAjpProcessor.java
@@ -465,10 +465,13 @@ public abstract class AbstractAjpProcessor extends AbstractProcessor {
        } catch (Throwable t) {
            ExceptionUtils.handleThrowable(t);
            getLog().error(sm.getString("http11processor.request.process"), t);
-           // 500 - Internal Server Error
-           response.setStatus(500);
-           adapter.log(request, response, 0);
            error = true;
+       } finally {
+           if (error) {
+               // 500 - Internal Server Error
+               response.setStatus(500);
+               adapter.log(request, response, 0);
+           }
        }
 
        rp.setStage(org.apache.coyote.Constants.STAGE_ENDED);
