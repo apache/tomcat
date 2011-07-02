apache/tomcat

commit 3a2c781c75d9ce12887ea5a5433055fae5bcf4cc

When using Servlets that implement the SingleThreadModel interface, add the single instance created to the pool when it is determined that a pool of servlets is required rather than throwing it away.
git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1142112 13f79535-47bb-0310-9956-ffa450edef68

java/org/apache/catalina/core/StandardWrapper.java
@@ -822,9 +822,16 @@ public class StandardWrapper extends ContainerBase
                                 log.debug("Allocating non-STM instance");
 
                             instance = loadServlet();
-                            // For non-STM, increment here to prevent a race
-                            // condition with unload. Bug 43683, test case #3
-                            if (!singleThreadModel) {
+                            if (singleThreadModel) {
+                                // No need to lock pool since until an instance
+                                // is created, no threads will get past this
+                                // point
+                                instancePool.push(instance);
+                                nInstances++;
+                            } else {
+                                // For non-STM, increment here to prevent a race
+                                // condition with unload. Bug 43683, test case
+                                // #3
                                 newInstance = true;
                                 countAllocated.incrementAndGet();
                             }

commit 2a75fdfcbe2caba6367d78428da11ca9fa0005ff

Correct deadlock introduced in r1142112
git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1142205 13f79535-47bb-0310-9956-ffa450edef68

java/org/apache/catalina/core/StandardWrapper.java
@@ -822,13 +822,7 @@ public class StandardWrapper extends ContainerBase
                                 log.debug("Allocating non-STM instance");
 
                             instance = loadServlet();
-                            if (singleThreadModel) {
-                                // No need to lock pool since until an instance
-                                // is created, no threads will get past this
-                                // point
-                                instancePool.push(instance);
-                                nInstances++;
-                            } else {
+                            if (!singleThreadModel) {
                                 // For non-STM, increment here to prevent a race
                                 // condition with unload. Bug 43683, test case
                                 // #3
@@ -850,7 +844,14 @@ public class StandardWrapper extends ContainerBase
                 initServlet(instance);
             }
 
-            if (!singleThreadModel) {
+            if (singleThreadModel) {
+                // Have to do this outside of the sync above to prevent a
+                // possible deadlock
+                synchronized (instancePool) {
+                    instancePool.push(instance);
+                    nInstances++;
+                }
+            } else {
                 if (log.isTraceEnabled())
                     log.trace("  Returning non-STM instance");
                 // For new instances, count will have been incremented at the