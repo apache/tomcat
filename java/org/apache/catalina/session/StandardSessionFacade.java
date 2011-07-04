da638381597617b537e79dffcaaf9b870c1f639c
Mark Emlyn David Thomas <markt@apache.org>
2011-07-04 13:28:11 +0000

    Fix a small number of warnings
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1142655 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/session/StandardSessionFacade.java
+++ java/org/apache/catalina/session/StandardSessionFacade.java
@@ -23,7 +23,6 @@ import java.util.Enumeration;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
-import javax.servlet.http.HttpSessionContext;
 
 
 /**
@@ -113,7 +112,7 @@ public class StandardSessionFacade
      */
     @Override
     @Deprecated
-    public HttpSessionContext getSessionContext() {
+    public javax.servlet.http.HttpSessionContext getSessionContext() {
         return session.getSessionContext();
     }
 
