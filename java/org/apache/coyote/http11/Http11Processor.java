be76a4e0f703fb9fa3b9ba70243276b3eba88617
Mark Emlyn David Thomas <markt@apache.org>
2011-07-08 14:59:43 +0000

    Protocol will call recycle
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1144349 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http11/Http11Processor.java
+++ java/org/apache/coyote/http11/Http11Processor.java
@@ -356,12 +356,10 @@ public class Http11Processor extends AbstractHttp11Processor {
 
         rp.setStage(org.apache.coyote.Constants.STAGE_ENDED);
         if (error || endpoint.isPaused()) {
-            recycle();
             return SocketState.CLOSED;
         } else if (isAsync()) {
             return SocketState.LONG;
         } else {
-            recycle();
             if (!keepAlive) {
                 return SocketState.CLOSED;
             } else {
