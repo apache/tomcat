356946717d880c3e20ee1d3e0f383d048fd000e7
Mark Emlyn David Thomas <markt@apache.org>
2011-07-11 22:27:06 +0000

    Protect against infinite loops in the HTTP NIO connector if sendfile is configured to send more data than is available in the file. (markt) Part of the fix for CVE-2011-2526
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1145383 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/net/NioEndpoint.java
+++ java/org/apache/tomcat/util/net/NioEndpoint.java
@@ -1240,6 +1240,13 @@ public class NioEndpoint extends AbstractEndpoint {
                         sd.pos += written;
                         sd.length -= written;
                         attachment.access();
+                    } else {
+                        // Unusual not to be unable to transfer any bytes
+                        // Check the length was set correctly
+                        if (sd.fchannel.size() <= sd.pos) {
+                            throw new IOException("Sendfile configured to " +
+                                    "send more data than was available");
+                        }
                     }
                 }
                 if ( sd.length <= 0 && sc.getOutboundRemaining()<=0) {
