7276ae0d95ed32b4ac0983a7add905f2d1214e88
Mark Emlyn David Thomas <markt@apache.org>
2011-07-12 08:34:20 +0000

    Correct a comment Part of the fix for CVE-2011-2526
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1145489 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/net/NioEndpoint.java
+++ java/org/apache/tomcat/util/net/NioEndpoint.java
@@ -1241,7 +1241,7 @@ public class NioEndpoint extends AbstractEndpoint {
                         sd.length -= written;
                         attachment.access();
                     } else {
-                        // Unusual not to be unable to transfer any bytes
+                        // Unusual not to be able to transfer any bytes
                         // Check the length was set correctly
                         if (sd.fchannel.size() <= sd.pos) {
                             throw new IOException("Sendfile configured to " +
