f41d4b648431e4167da477eacbcc4b0cde67d83a
Mark Emlyn David Thomas <markt@apache.org>
2011-07-12 12:51:35 +0000

    Socket has been closed, return false so an attempt is not made to re-use the socket Part of the fix for CVE-2011-2526
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1145571 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/net/NioEndpoint.java
+++ java/org/apache/tomcat/util/net/NioEndpoint.java
@@ -1271,6 +1271,7 @@ public class NioEndpoint extends AbstractEndpoint {
                             log.debug("Send file connection is being closed");
                         }
                         cancelledKey(sk,SocketStatus.STOP,false);
+                        return false;
                     }
                 } else if ( attachment.interestOps() == 0 && reg ) {
                     if (log.isDebugEnabled()) {
