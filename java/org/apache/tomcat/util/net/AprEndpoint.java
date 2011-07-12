1d372c881eafd9ffe729996f8560fd5fe50cd39d
Mark Emlyn David Thomas <markt@apache.org>
2011-07-12 17:50:11 +0000

    Protect against crashes in the HTTP APR connector if sendfile is configured to send more data than is available in the file. Part of the fix for CVE-2011-2526
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1145694 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/net/AprEndpoint.java
+++ java/org/apache/tomcat/util/net/AprEndpoint.java
@@ -1460,7 +1460,9 @@ public class AprEndpoint extends AbstractEndpoint {
                                                data.pos, data.end - data.pos, 0);
                     if (nw < 0) {
                         if (!(-nw == Status.EAGAIN)) {
-                            destroySocket(data.socket);
+                            Pool.destroy(data.fdpool);
+                            // No need to close socket, this will be done by
+                            // calling code since data.socket == 0
                             data.socket = 0;
                             return false;
                         } else {
