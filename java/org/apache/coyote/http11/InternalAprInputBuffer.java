9ef3390a66f7febffcc28eaa204b9d503048a8c6
Jean-Frederic Clere <jfclere@apache.org>
2011-07-19 09:36:08 +0000

    Fix for PR50394
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1148254 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http11/InternalAprInputBuffer.java
+++ java/org/apache/coyote/http11/InternalAprInputBuffer.java
@@ -587,6 +587,8 @@ public class InternalAprInputBuffer extends AbstractInputBuffer {
                 bbuf.get(buf, pos, nRead);
                 lastValid = pos + nRead;
             } else {
+                if ((-nRead) == Status.APR_EOF)
+                    return false;
                 if ((-nRead) == Status.ETIMEDOUT || (-nRead) == Status.TIMEUP) {
                     throw new SocketTimeoutException(sm.getString("iib.failedread"));
                 } else {
