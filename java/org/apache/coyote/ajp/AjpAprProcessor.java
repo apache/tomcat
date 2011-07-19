a3f3379508b3c083453f60c4f342ee96c7601171
Jean-Frederic Clere <jfclere@apache.org>
2011-07-19 08:06:46 +0000

    Fix for PR49595
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1148216 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/ajp/AjpAprProcessor.java
+++ java/org/apache/coyote/ajp/AjpAprProcessor.java
@@ -507,13 +507,13 @@ public class AjpAprProcessor extends AbstractAjpProcessor<Long> {
         long socketRef = socket.getSocket().longValue();
         
         if (outputBuffer.position() > 0) {
-            if (Socket.sendbb(socketRef, 0, outputBuffer.position()) < 0) {
+            if ((socketRef != 0) && Socket.sendbb(socketRef, 0, outputBuffer.position()) < 0) {
                 throw new IOException(sm.getString("ajpprocessor.failedsend"));
             }
             outputBuffer.clear();
         }
         // Send explicit flush message
-        if (explicit && !finished) {
+        if (explicit && !finished &&  (socketRef != 0)) {
             if (Socket.send(socketRef, flushMessageArray, 0,
                     flushMessageArray.length) < 0) {
                 throw new IOException(sm.getString("ajpprocessor.failedflush"));
