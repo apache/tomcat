9edd44be97113d49b05937ef5296aacdef8d0136
Konstantin Kolinko <kkolinko@apache.org>
2011-07-25 21:04:30 +0000

    Converted the tests to JUnit 4.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1150907 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/tomcat/util/net/TestXxxEndpoint.java
+++ test/org/apache/tomcat/util/net/TestXxxEndpoint.java
@@ -20,13 +20,17 @@ package org.apache.tomcat.util.net;
 import java.io.File;
 import java.net.ServerSocket;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Tomcat;
-import org.apache.catalina.startup.TomcatBaseTest;
-
+import org.apache.catalina.startup.TomcatBaseTestJUnit4;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
-
 import org.apache.tomcat.jni.Address;
 import org.apache.tomcat.jni.Error;
 import org.apache.tomcat.jni.Library;
@@ -38,7 +42,7 @@ import org.apache.tomcat.jni.Socket;
  * Test case for the Endpoint implementations. The testing framework will ensure
  * that each implementation is tested.
  */
-public class TestXxxEndpoint extends TomcatBaseTest {
+public class TestXxxEndpoint extends TomcatBaseTestJUnit4 {
 
     private static Log log = LogFactory.getLog(TestXxxEndpoint.class);
 
@@ -108,6 +112,7 @@ public class TestXxxEndpoint extends TomcatBaseTest {
         }
     }
 
+    @Test
     public void testStartStopBindOnInit() throws Exception {
         Tomcat tomcat = getTomcatInstance();
         File appDir = new File(getBuildDirectory(), "webapps/examples");
@@ -151,6 +156,7 @@ public class TestXxxEndpoint extends TomcatBaseTest {
         tomcat.getConnector().start();
     }
 
+    @Test
     public void testStartStopBindOnStart() throws Exception {
         Tomcat tomcat = getTomcatInstance();
         Connector c = tomcat.getConnector();
