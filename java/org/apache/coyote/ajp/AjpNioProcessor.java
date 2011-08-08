3b513e1d289be4f6f60fc34eb4307969f705382a
Mark Emlyn David Thomas <markt@apache.org>
2011-08-08 09:57:05 +0000

    Correctly indicate if socket is closing when calling recycle for the AJP NIO processor. Note since the flag is unused in this case there were no bugs triggered by the re-factoring error. (rjung)
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1154897 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/ajp/AjpNioProcessor.java
+++ java/org/apache/coyote/ajp/AjpNioProcessor.java
@@ -128,14 +128,14 @@ public class AjpNioProcessor extends AbstractAjpProcessor<NioChannel> {
                     } catch (IOException e) {
                         error = true;
                     }
-                    recycle(true);
+                    recycle(false);
                     continue;
                 } else if(type != Constants.JK_AJP13_FORWARD_REQUEST) {
                     // Usually the servlet didn't read the previous request body
                     if(log.isDebugEnabled()) {
                         log.debug("Unexpected message: "+type);
                     }
-                    recycle(false);
+                    recycle(true);
                     continue;
                 }
                 request.setStartTime(System.currentTimeMillis());
@@ -217,7 +217,7 @@ public class AjpNioProcessor extends AbstractAjpProcessor<NioChannel> {
                 socket.setTimeout(keepAliveTimeout);
             }
 
-            recycle(true);
+            recycle(false);
         }
         
         rp.setStage(org.apache.coyote.Constants.STAGE_ENDED);
