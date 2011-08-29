7e4eeb98f48639e697bc29b849816965ab6ea676
Mark Emlyn David Thomas <markt@apache.org>
2011-08-29 19:45:13 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=51698 Fix CVE-2011-3190 Prevent AJP request forgery via unread request body packet
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1162958 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/ajp/AjpNioProcessor.java
+++ java/org/apache/coyote/ajp/AjpNioProcessor.java
@@ -126,12 +126,14 @@ public class AjpNioProcessor extends AbstractAjpProcessor<NioChannel> {
                     recycle(false);
                     continue;
                 } else if(type != Constants.JK_AJP13_FORWARD_REQUEST) {
-                    // Usually the servlet didn't read the previous request body
-                    if(log.isDebugEnabled()) {
-                        log.debug("Unexpected message: "+type);
+                    // Unexpected packet type. Unread body packets should have
+                    // been swallowed in finish().
+                    if (log.isDebugEnabled()) {
+                        log.debug("Unexpected message: " + type);
                     }
+                    error = true;
                     recycle(true);
-                    continue;
+                    break;
                 }
                 request.setStartTime(System.currentTimeMillis());
             } catch (IOException e) {
