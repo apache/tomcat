90ec9675fa080e22df5f9e3e7019a19eb2faec14
Mark Emlyn David Thomas <markt@apache.org>
2011-08-29 19:45:13 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=51698 Fix CVE-2011-3190 Prevent AJP request forgery via unread request body packet
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1162958 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/ajp/AbstractAjpProcessor.java
+++ java/org/apache/coyote/ajp/AbstractAjpProcessor.java
@@ -985,6 +985,11 @@ public abstract class AbstractAjpProcessor<S> extends AbstractProcessor<S> {
 
         finished = true;
 
+        // Swallow the unread body packet if present
+        if (first && request.getContentLengthLong() > 0) {
+            receive();
+        }
+
         // Add the end message
         if (error) {
             output(endAndCloseMessageArray, 0, endAndCloseMessageArray.length);
--- java/org/apache/coyote/ajp/AjpProcessor.java
+++ java/org/apache/coyote/ajp/AjpProcessor.java
@@ -143,13 +143,14 @@ public class AjpProcessor extends AbstractAjpProcessor<Socket> {
                     }
                     continue;
                 } else if(type != Constants.JK_AJP13_FORWARD_REQUEST) {
-                    // Usually the servlet didn't read the previous request body
-                    if(log.isDebugEnabled()) {
-                        log.debug("Unexpected message: "+type);
+                    // Unexpected packet type. Unread body packets should have
+                    // been swallowed in finish().
+                    if (log.isDebugEnabled()) {
+                        log.debug("Unexpected message: " + type);
                     }
-                    continue;
+                    error = true;
+                    break;
                 }
-
                 request.setStartTime(System.currentTimeMillis());
             } catch (IOException e) {
                 error = true;
