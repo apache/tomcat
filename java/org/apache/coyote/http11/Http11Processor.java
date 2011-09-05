9f2afd04653755cdd609cc390fef1d470b016d6d
Mark Thomas <markt@apache.org>
2011-09-05 16:59:27 +0000

    Add debug log message to align HTTP connectors
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1165361 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http11/Http11Processor.java
+++ java/org/apache/coyote/http11/Http11Processor.java
@@ -141,7 +141,7 @@ public class Http11Processor extends AbstractHttp11Processor<Socket> {
         inputBuffer.init(socketWrapper, endpoint);
         outputBuffer.init(socketWrapper, endpoint);
 
-        // Error flag
+        // Flags
         error = false;
         keepAlive = true;
         comet = false;
@@ -220,6 +220,9 @@ public class Http11Processor extends AbstractHttp11Processor<Socket> {
                     inputBuffer.parseHeaders();
                 }
             } catch (IOException e) {
+                if (log.isDebugEnabled()) {
+                    log.debug(sm.getString("http11processor.header.parse"), e);
+                }
                 error = true;
                 break;
             } catch (Throwable t) {
