e4b2eeff7a901e928c8a2539b6f717f64ec80aab
Konstantin Kolinko <kkolinko@apache.org>
2011-09-28 09:48:32 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=51905 Merged revision 1176799 from tomcat/trunk: Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=51905 Skip attempt to shutdown AprEndpoint acceptor threads if the server socket has been already forcefully closed. Reduce waiting timeout from 30s to 10s. I think if things go wrong 10s is enough to wait.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1176801 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/net/AprEndpoint.java
+++ java/org/apache/tomcat/util/net/AprEndpoint.java
@@ -652,8 +652,8 @@ public class AprEndpoint extends AbstractEndpoint {
             running = false;
             unlockAccept();
             for (int i = 0; i < acceptors.length; i++) {
-                long s = System.currentTimeMillis() + 30000;
-                while (acceptors[i].isAlive()) {
+                long s = System.currentTimeMillis() + 10000;
+                while (acceptors[i].isAlive() && serverSock != 0) {
                     try {
                         acceptors[i].interrupt();
                         acceptors[i].join(1000);
