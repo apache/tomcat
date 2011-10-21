9e27845515aeb30820f0bcaa1fde5685d58c9364
Keiichi Fujino <kfujino@apache.org>
2011-10-21 01:10:51 +0000

    follow-up r1186631.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1187149 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/ha/session/JvmRouteBinderValve.java
+++ java/org/apache/catalina/ha/session/JvmRouteBinderValve.java
@@ -411,7 +411,7 @@ public class JvmRouteBinderValve extends ValveBase implements ClusterValve {
     protected void sendSessionIDClusterBackup(Request request, String sessionId,
             String newSessionID) {
         CatalinaCluster c = getCluster();
-        if (c != null && getManager(request) instanceof DeltaManager) {
+        if (c != null && !(getManager(request) instanceof BackupManager)) {
             SessionIDMessage msg = new SessionIDMessage();
             msg.setOrignalSessionID(sessionId);
             msg.setBackupSessionID(newSessionID);
