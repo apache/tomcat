95c08f32520d7be8e23c9a62b3a1ef829a76b3b9
Konstantin Kolinko <kkolinko@apache.org>
2011-10-26 12:19:26 +0000

    Remove trailing whitespace from bat, sh, res/tomcat.nsi and textual files in /, /bin, /res
    It is merge of revisions r1187805, r1187827 from tomcat/trunk,
    but with exception of res/checkstyle/checkstyle.xml changes from r1187805
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1189163 13f79535-47bb-0310-9956-ffa450edef68

--- bin/setclasspath.bat
+++ bin/setclasspath.bat
@@ -17,7 +17,7 @@ rem limitations under the License.
 rem ---------------------------------------------------------------------------
 rem Set JAVA_HOME or JRE_HOME if not already set, ensure any provided settings
 rem are valid and consistent with the selected start-up options and set up the
-rem endorsed directory. 
+rem endorsed directory.
 rem
 rem $Id$
 rem ---------------------------------------------------------------------------
