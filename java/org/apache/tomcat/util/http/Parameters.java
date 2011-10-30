d9fcfa52e09bbbddc3a88342122dd370bd20a52b
Mark Emlyn David Thomas <markt@apache.org>
2011-10-30 19:03:41 +0000

    Simplify addition of parameters
    This was part of the fix for CVE-2012-0022
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1195226 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/http/Parameters.java
+++ java/org/apache/tomcat/util/http/Parameters.java
@@ -101,6 +101,7 @@ public final class Parameters {
     // Access to the current name/values, no side effect ( processing ).
     // You must explicitly call handleQueryParameters and the post methods.
     
+    @Deprecated
     public void addParameterValues(String key, String[] newValues) {
         if (key == null) {
             return;
@@ -170,7 +171,7 @@ public final class Parameters {
     }
 
 
-    private void addParam( String key, String value ) {
+    public void addParameter( String key, String value ) {
         if( key==null ) return;
         ArrayList<String> values = paramHashValues.get(key);
         if (values == null) {
@@ -339,7 +340,7 @@ public final class Parameters {
                     value = tmpValue.toString();
                 }
 
-                addParam(name, value);
+                addParameter(name, value);
             } catch (IOException e) {
                 decodeFailCount++;
                 if (decodeFailCount == 1 || log.isDebugEnabled()) {
