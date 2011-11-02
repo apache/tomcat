387bebb3cad310ade96ab8392897c6abde02b004
Konstantin Kolinko <kkolinko@apache.org>
2011-11-02 22:09:42 +0000

    Wrap and format long lines. No functional change.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1196827 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/net/NioEndpoint.java
+++ java/org/apache/tomcat/util/net/NioEndpoint.java
@@ -564,7 +564,10 @@ public class NioEndpoint extends AbstractEndpoint {
                 pollers[i].destroy();
                 pollers[i] = null;
             }
-            try { stopLatch.await(selectorTimeout+100,TimeUnit.MILLISECONDS); } catch (InterruptedException ignore ) {}
+            try {
+                stopLatch.await(selectorTimeout + 100, TimeUnit.MILLISECONDS);
+            } catch (InterruptedException ignore) {
+            }
         }
         eventCache.clear();
         keyCache.clear();
@@ -1272,7 +1275,10 @@ public class NioEndpoint extends AbstractEndpoint {
                         log.debug("Send file complete for:"+sd.fileName);
                     }
                     attachment.setSendfileData(null);
-                    try {sd.fchannel.close();}catch(Exception ignore){}
+                    try {
+                        sd.fchannel.close();
+                    } catch (Exception ignore) {
+                    }
                     if ( sd.keepAlive ) {
                         if (reg) {
                             if (log.isDebugEnabled()) {
@@ -1409,9 +1415,23 @@ public class NioEndpoint extends AbstractEndpoint {
             error = false;
             lastRegistered = 0;
             sendfileData = null;
-            if ( readLatch!=null ) try {for (int i=0; i<(int)readLatch.getCount();i++) readLatch.countDown();}catch (Exception ignore){}
+            if (readLatch != null) {
+                try {
+                    for (int i = 0; i < (int) readLatch.getCount(); i++) {
+                        readLatch.countDown();
+                    }
+                } catch (Exception ignore) {
+                }
+            }
             readLatch = null;
-            if ( writeLatch!=null ) try {for (int i=0; i<(int)writeLatch.getCount();i++) writeLatch.countDown();}catch (Exception ignore){}
+            if (writeLatch != null) {
+                try {
+                    for (int i = 0; i < (int) writeLatch.getCount(); i++) {
+                        writeLatch.countDown();
+                    }
+                } catch (Exception ignore) {
+                }
+            }
             writeLatch = null;
             cometNotify = false;
             cometOps = SelectionKey.OP_READ;
