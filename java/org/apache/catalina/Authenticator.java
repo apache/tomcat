69441a9aba785937444fd97b08553f10510ed3d2
Konstantin Kolinko <kkolinko@apache.org>
2011-11-10 05:30:28 +0000

    Merging revision 1187801 from tomcat/trunk: Whitespace removal from remaining /java/org/apache/catalina Part 1 of n
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1200156 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/Authenticator.java
+++ java/org/apache/catalina/Authenticator.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -36,7 +36,7 @@ import org.apache.catalina.deploy.LoginConfig;
  */
 
 public interface Authenticator {
-    
+
     /**
      * Authenticate the user making this request, based on the specified
      * login configuration.  Return <code>true</code> if any specified
@@ -52,7 +52,7 @@ public interface Authenticator {
      */
     public boolean authenticate(Request request, HttpServletResponse response,
             LoginConfig config) throws IOException;
-    
+
     public void login(String userName, String password, Request request)
             throws ServletException;
 
