2352dcf6a143efab852ca785dd08b28df555ab95
Konstantin Kolinko <kkolinko@apache.org>
2011-11-10 05:31:27 +0000

    Merging revision 1187801 from tomcat/trunk: Whitespace removal from remaining /java/org/apache/catalina Part 2
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1200157 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/comet/CometEvent.java
+++ java/org/apache/catalina/comet/CometEvent.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,39 +26,39 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * The CometEvent interface.
- * 
+ *
  * @author Filip Hanik
  * @author Remy Maucherat
  */
 public interface CometEvent {
 
     /**
-     * Enumeration describing the major events that the container can invoke 
+     * Enumeration describing the major events that the container can invoke
      * the CometProcessors event() method with.<br>
-     * BEGIN - will be called at the beginning 
-     *  of the processing of the connection. It can be used to initialize any relevant 
-     *  fields using the request and response objects. Between the end of the processing 
+     * BEGIN - will be called at the beginning
+     *  of the processing of the connection. It can be used to initialize any relevant
+     *  fields using the request and response objects. Between the end of the processing
      *  of this event, and the beginning of the processing of the end or error events,
      *  it is possible to use the response object to write data on the open connection.
-     *  Note that the response object and dependent OutputStream and Writer are still 
-     *  not synchronized, so when they are accessed by multiple threads, 
-     *  synchronization is mandatory. After processing the initial event, the request 
+     *  Note that the response object and dependent OutputStream and Writer are still
+     *  not synchronized, so when they are accessed by multiple threads,
+     *  synchronization is mandatory. After processing the initial event, the request
      *  is considered to be committed.<br>
      * READ - This indicates that input data is available, and that one read can be made
      *  without blocking. The available and ready methods of the InputStream or
      *  Reader may be used to determine if there is a risk of blocking: the servlet
-     *  should read while data is reported available. When encountering a read error, 
-     *  the servlet should report it by propagating the exception properly. Throwing 
-     *  an exception will cause the error event to be invoked, and the connection 
-     *  will be closed. 
+     *  should read while data is reported available. When encountering a read error,
+     *  the servlet should report it by propagating the exception properly. Throwing
+     *  an exception will cause the error event to be invoked, and the connection
+     *  will be closed.
      *  Alternately, it is also possible to catch any exception, perform clean up
      *  on any data structure the servlet may be using, and using the close method
-     *  of the event. It is not allowed to attempt reading data from the request 
+     *  of the event. It is not allowed to attempt reading data from the request
      *  object outside of the execution of this method.<br>
      * END - End may be called to end the processing of the request. Fields that have
      *  been initialized in the begin method should be reset. After this event has
      *  been processed, the request and response objects, as well as all their dependent
-     *  objects will be recycled and used to process other requests. End will also be 
+     *  objects will be recycled and used to process other requests. End will also be
      *  called when data is available and the end of file is reached on the request input
      *  (this usually indicates the client has pipelined a request).<br>
      * ERROR - Error will be called by the container in the case where an IO exception
@@ -68,8 +68,8 @@ public interface CometEvent {
      *  objects will be recycled and used to process other requests.
      */
     public enum EventType {BEGIN, READ, END, ERROR}
-    
-    
+
+
     /**
      * Event details.<br>
      * TIMEOUT - the connection timed out (sub type of ERROR); note that this ERROR type is not fatal, and
@@ -81,60 +81,60 @@ public interface CometEvent {
      * SESSION_END - the servlet ended the session (sub type of END)
      */
     public enum EventSubType { TIMEOUT, CLIENT_DISCONNECT, IOEXCEPTION, WEBAPP_RELOAD, SERVER_SHUTDOWN, SESSION_END }
-    
-    
+
+
     /**
      * Returns the HttpServletRequest.
-     * 
+     *
      * @return HttpServletRequest
      */
     public HttpServletRequest getHttpServletRequest();
-    
+
     /**
      * Returns the HttpServletResponse.
-     * 
+     *
      * @return HttpServletResponse
      */
     public HttpServletResponse getHttpServletResponse();
-    
+
     /**
      * Returns the event type.
-     * 
+     *
      * @return EventType
      */
     public EventType getEventType();
-    
+
     /**
      * Returns the sub type of this event.
-     * 
+     *
      * @return EventSubType
      */
     public EventSubType getEventSubType();
-    
+
     /**
-     * Ends the Comet session. This signals to the container that 
+     * Ends the Comet session. This signals to the container that
      * the container wants to end the comet session. This will send back to the
      * client a notice that the server has no more data to send as part of this
      * request. The servlet should perform any needed cleanup as if it had received
-     * an END or ERROR event. 
-     * 
+     * an END or ERROR event.
+     *
      * @throws IOException if an IO exception occurs
      */
     public void close() throws IOException;
-    
+
     /**
-     * Sets the timeout for this Comet connection. Please NOTE, that the implementation 
+     * Sets the timeout for this Comet connection. Please NOTE, that the implementation
      * of a per connection timeout is OPTIONAL and MAY NOT be implemented.<br/>
      * This method sets the timeout in milliseconds of idle time on the connection.
      * The timeout is reset every time data is received from the connection or data is flushed
-     * using <code>response.flushBuffer()</code>. If a timeout occurs, the 
-     * <code>error(HttpServletRequest, HttpServletResponse)</code> method is invoked. The 
+     * using <code>response.flushBuffer()</code>. If a timeout occurs, the
+     * <code>error(HttpServletRequest, HttpServletResponse)</code> method is invoked. The
      * web application SHOULD NOT attempt to reuse the request and response objects after a timeout
      * as the <code>error(HttpServletRequest, HttpServletResponse)</code> method indicates.<br/>
      * This method should not be called asynchronously, as that will have no effect.
-     * 
+     *
      * @param timeout The timeout in milliseconds for this connection, must be a positive value, larger than 0
-     * @throws IOException An IOException may be thrown to indicate an IO error, 
+     * @throws IOException An IOException may be thrown to indicate an IO error,
      *         or that the EOF has been reached on the connection
      * @throws ServletException An exception has occurred, as specified by the root
      *         cause
