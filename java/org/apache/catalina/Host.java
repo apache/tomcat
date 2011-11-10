a168c97973320ce4fe9eba268b233bbe366bda54
Konstantin Kolinko <kkolinko@apache.org>
2011-11-10 05:33:31 +0000

    Merging revision 1187801 from tomcat/trunk: Whitespace removal from remaining /java/org/apache/catalina Part 4
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1200159 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/Host.java
+++ java/org/apache/catalina/Host.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -74,7 +74,7 @@ public interface Host extends Container {
      * ${catalina.base}/conf/&lt;engine name&gt;/&lt;host name&gt; directory
      */
     public String getXmlBase();
-    
+
     /**
      * Set the Xml root for this Host.  This can be an absolute
      * pathname, a relative pathname, or a URL.
@@ -101,8 +101,8 @@ public interface Host extends Container {
 
 
     /**
-     * Return the value of the auto deploy flag.  If true, it indicates that 
-     * this host's child webapps should be discovered and automatically 
+     * Return the value of the auto deploy flag.  If true, it indicates that
+     * this host's child webapps should be discovered and automatically
      * deployed dynamically.
      */
     public boolean getAutoDeploy();
@@ -110,7 +110,7 @@ public interface Host extends Container {
 
     /**
      * Set the auto deploy flag value for this host.
-     * 
+     *
      * @param autoDeploy The new auto deploy flag
      */
     public void setAutoDeploy(boolean autoDeploy);
@@ -122,7 +122,7 @@ public interface Host extends Container {
      */
     public String getConfigClass();
 
-    
+
     /**
      * Set the Java class name of the context configuration class
      * for new web applications.
@@ -131,10 +131,10 @@ public interface Host extends Container {
      */
     public void setConfigClass(String configClass);
 
-        
+
     /**
-     * Return the value of the deploy on startup flag.  If true, it indicates 
-     * that this host's child webapps should be discovered and automatically 
+     * Return the value of the deploy on startup flag.  If true, it indicates
+     * that this host's child webapps should be discovered and automatically
      * deployed.
      */
     public boolean getDeployOnStartup();
@@ -142,7 +142,7 @@ public interface Host extends Container {
 
     /**
      * Set the deploy on startup flag value for this host.
-     * 
+     *
      * @param deployOnStartup The new deploy on startup flag
      */
     public void setDeployOnStartup(boolean deployOnStartup);
