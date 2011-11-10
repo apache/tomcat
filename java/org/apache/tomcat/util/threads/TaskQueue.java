d13149f7dc085c9a18994eab25bf05723d10d420
Konstantin Kolinko <kkolinko@apache.org>
2011-11-10 05:55:40 +0000

    Merging revision 1187781 from tomcat/trunk: Whitespace removal from /java/org/apache/tomcat I won't merge all of it (as it spans bcel etc.), but only our code. Part 7, the last one. I am not updating mergeinfo, because not all of the original revision was merged.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1200171 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/threads/TaskQueue.java
+++ java/org/apache/tomcat/util/threads/TaskQueue.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,9 +23,9 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * As task queue specifically designed to run with a thread pool executor.
- * The task queue is optimised to properly utilize threads within 
+ * The task queue is optimised to properly utilize threads within
  * a thread pool executor. If you use a normal queue, the executor will spawn threads
- * when there are idle threads and you wont be able to force items unto the queue itself 
+ * when there are idle threads and you wont be able to force items unto the queue itself
  * @author fhanik
  *
  */
@@ -34,7 +34,7 @@ public class TaskQueue extends LinkedBlockingQueue<Runnable> {
     private static final long serialVersionUID = 1L;
 
     private ThreadPoolExecutor parent = null;
-    
+
     // no need to be volatile, the one times when we change and read it occur in
     // a single thread (the one that did stop a context and fired listeners)
     private Integer forcedRemainingCapacity = null;
@@ -54,7 +54,7 @@ public class TaskQueue extends LinkedBlockingQueue<Runnable> {
     public void setParent(ThreadPoolExecutor tp) {
         parent = tp;
     }
-    
+
     public boolean force(Runnable o) {
         if ( parent.isShutdown() ) throw new RejectedExecutionException("Executor not running, can't force a command into the queue");
         return super.offer(o); //forces the item onto the queue, to be used if the task is rejected
