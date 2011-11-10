00851fd8ab6f1afac70570b566e575255665ef39
Konstantin Kolinko <kkolinko@apache.org>
2011-11-10 05:30:28 +0000

    Merging revision 1187801 from tomcat/trunk: Whitespace removal from remaining /java/org/apache/catalina Part 1 of n
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1200156 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/CatalinaFactory.java
+++ java/org/apache/catalina/CatalinaFactory.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,21 +25,21 @@ import org.apache.catalina.core.StandardPipeline;
  * other components.  The current implementation is as simple as possible. If
  * there is demand it can be extended to support alternative factories and/or
  * alternative defaults.
- * 
+ *
  * TODO: Create the other standard components via this factory
  */
 public class CatalinaFactory {
-    
+
     private static CatalinaFactory factory = new CatalinaFactory();
-    
+
     public static CatalinaFactory getFactory() {
         return factory;
     }
-    
+
     private CatalinaFactory() {
         // Hide the default constructor
     }
-    
+
     public String getDefaultPipelineClassName() {
         return StandardPipeline.class.getName();
     }
