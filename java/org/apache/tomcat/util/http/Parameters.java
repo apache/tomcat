apache/tomcat

commit 4fb4727ac3f2d9c122be5958a9a2b97b46c0dc1f

Merged revision 1187753 from tomcat/trunk: Clean-up. No functional change. Part 10, the last one. Updating mergeinfo.
git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1200184 13f79535-47bb-0310-9956-ffa450edef68

java/org/apache/tomcat/util/http/Parameters.java
@@ -14,7 +14,6 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-
 package org.apache.tomcat.util.http;
 
 import java.io.IOException;
@@ -34,7 +33,7 @@ import org.apache.tomcat.util.buf.UDecoder;
 import org.apache.tomcat.util.res.StringManager;
 
 /**
- * 
+ *
  * @author Costin Manolache
  */
 public final class Parameters {
@@ -49,7 +48,7 @@ public final class Parameters {
         new HashMap<String,ArrayList<String>>();
 
     private boolean didQueryParameters=false;
-    
+
     MessageBytes queryMB;
 
     UDecoder urlDec;
@@ -57,7 +56,7 @@ public final class Parameters {
 
     String encoding=null;
     String queryStringEncoding=null;
-    
+
     private int limit = -1;
     private int parameterCount = 0;
 
@@ -98,11 +97,11 @@ public final class Parameters {
         encoding=null;
         decodedQuery.recycle();
     }
-    
+
     // -------------------- Data access --------------------
     // Access to the current name/values, no side effect ( processing ).
     // You must explicitly call handleQueryParameters and the post methods.
-    
+
     @Deprecated
     public void addParameterValues(String key, String[] newValues) {
         if (key == null) {
@@ -129,7 +128,7 @@ public final class Parameters {
         }
         return values.toArray(new String[values.size()]);
     }
- 
+
     public Enumeration<String> getParameterNames() {
         handleQueryParameters();
         return Collections.enumeration(paramHashValues.keySet());
@@ -151,13 +150,16 @@ public final class Parameters {
     /** Process the query string into parameters
      */
     public void handleQueryParameters() {
-        if( didQueryParameters ) return;
+        if( didQueryParameters ) {
+            return;
+        }
 
         didQueryParameters=true;
 
-        if( queryMB==null || queryMB.isNull() )
+        if( queryMB==null || queryMB.isNull() ) {
             return;
-        
+        }
+
         if(log.isDebugEnabled()) {
             log.debug("Decoding query " + decodedQuery + " " +
                     queryStringEncoding);
@@ -176,7 +178,9 @@ public final class Parameters {
     public void addParameter( String key, String value )
             throws IllegalStateException {
 
-        if( key==null ) return;
+        if( key==null ) {
+            return;
+        }
 
         parameterCount ++;
         if (limit > -1 && parameterCount > limit) {
@@ -203,28 +207,28 @@ public final class Parameters {
     // if needed
     ByteChunk tmpName=new ByteChunk();
     ByteChunk tmpValue=new ByteChunk();
-    private ByteChunk origName=new ByteChunk();
-    private ByteChunk origValue=new ByteChunk();
+    private final ByteChunk origName=new ByteChunk();
+    private final ByteChunk origValue=new ByteChunk();
     CharChunk tmpNameC=new CharChunk(1024);
     public static final String DEFAULT_ENCODING = "ISO-8859-1";
     private static final Charset DEFAULT_CHARSET =
         Charset.forName(DEFAULT_ENCODING);
-    
-    
+
+
     public void processParameters( byte bytes[], int start, int len ) {
         processParameters(bytes, start, len, getCharset(encoding));
     }
 
     private void processParameters(byte bytes[], int start, int len,
                                   Charset charset) {
-        
+
         if(log.isDebugEnabled()) {
             log.debug(sm.getString("parameters.bytes",
                     new String(bytes, start, len, DEFAULT_CHARSET)));
         }
 
         int decodeFailCount = 0;
-            
+
         int pos = start;
         int end = start + len;
 
@@ -277,6 +281,7 @@ public final class Parameters {
                         break;
                 }
             } while (!parameterComplete && pos < end);
+            }
 
             if (pos == end) {
                 if (nameEnd == -1) {
@@ -285,14 +290,14 @@ public final class Parameters {
                     valueEnd = pos;
                 }
             }
-            
+
             if (log.isDebugEnabled() && valueStart == -1) {
                 log.debug(sm.getString("parameters.noequal",
                         Integer.valueOf(nameStart), Integer.valueOf(nameEnd),
                         new String(bytes, nameStart, nameEnd-nameStart,
                                 DEFAULT_CHARSET)));
             }
-            
+
             if (nameEnd <= nameStart ) {
                 if (log.isInfoEnabled()) {
                     String extract;
@@ -313,7 +318,7 @@ public final class Parameters {
                 continue;
                 // invalid chunk - it's better to ignore
             }
-            
+
             tmpName.setBytes(bytes, nameStart, nameEnd - nameStart);
             tmpValue.setBytes(bytes, valueStart, valueEnd - valueStart);
 
@@ -329,7 +334,7 @@ public final class Parameters {
                     log.error(sm.getString("paramerers.copyFail"), ioe);
                 }
             }
-            
+
             try {
                 String name;
                 String value;
@@ -385,13 +390,15 @@ public final class Parameters {
     private void urlDecode(ByteChunk bc)
         throws IOException {
         if( urlDec==null ) {
-            urlDec=new UDecoder();   
+            urlDec=new UDecoder();
         }
         urlDec.convert(bc);
     }
 
     public void processParameters( MessageBytes data, String encoding ) {
-        if( data==null || data.isNull() || data.getLength() <= 0 ) return;
+        if( data==null || data.isNull() || data.getLength() <= 0 ) {
+            return;
+        }
 
         if( data.getType() != MessageBytes.T_BYTES ) {
             data.toBytes();

java/org/apache/tomcat/util/http/FastHttpDateFormat.java
@@ -14,7 +14,6 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-
 package org.apache.tomcat.util.http;
 
 import java.text.DateFormat;
@@ -27,7 +26,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Utility class to generate HTTP dates.
- * 
+ *
  * @author Remy Maucherat
  */
 public final class FastHttpDateFormat {
@@ -36,14 +35,14 @@ public final class FastHttpDateFormat {
     // -------------------------------------------------------------- Variables
 
 
-    private static final int CACHE_SIZE = 
+    private static final int CACHE_SIZE =
         Integer.parseInt(System.getProperty("org.apache.tomcat.util.http.FastHttpDateFormat.CACHE_SIZE", "1000"));
 
-    
+
     /**
      * HTTP date format.
      */
-    private static final SimpleDateFormat format = 
+    private static final SimpleDateFormat format =
         new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz", Locale.US);
 
 
@@ -89,14 +88,14 @@ public final class FastHttpDateFormat {
     /**
      * Formatter cache.
      */
-    private static final ConcurrentHashMap<Long, String> formatCache = 
+    private static final ConcurrentHashMap<Long, String> formatCache =
         new ConcurrentHashMap<Long, String>(CACHE_SIZE);
 
 
     /**
      * Parser cache.
      */
-    private static final ConcurrentHashMap<String, Long> parseCache = 
+    private static final ConcurrentHashMap<String, Long> parseCache =
         new ConcurrentHashMap<String, Long>(CACHE_SIZE);
 
 
@@ -130,8 +129,9 @@ public final class FastHttpDateFormat {
 
         Long longValue = new Long(value);
         String cachedDate = formatCache.get(longValue);
-        if (cachedDate != null)
+        if (cachedDate != null) {
             return cachedDate;
+        }
 
         String newDate = null;
         Date dateValue = new Date(value);
@@ -154,12 +154,13 @@ public final class FastHttpDateFormat {
     /**
      * Try to parse the given date as a HTTP date.
      */
-    public static final long parseDate(String value, 
+    public static final long parseDate(String value,
                                        DateFormat[] threadLocalformats) {
 
         Long cachedDate = parseCache.get(value);
-        if (cachedDate != null)
+        if (cachedDate != null) {
             return cachedDate.longValue();
+        }
 
         Long date = null;
         if (threadLocalformats != null) {

java/org/apache/tomcat/util/http/HttpMessages.java
@@ -14,14 +14,13 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-
 package org.apache.tomcat.util.http;
 
 import org.apache.tomcat.util.res.StringManager;
 
 /**
  * Handle (internationalized) HTTP messages.
- * 
+ *
  * @author James Duncan Davidson [duncan@eng.sun.com]
  * @author James Todd [gonzo@eng.sun.com]
  * @author Jason Hunter [jch@eng.sun.com]
@@ -32,12 +31,12 @@ public class HttpMessages {
     // XXX move message resources in this package
     protected static final StringManager sm =
         StringManager.getManager("org.apache.tomcat.util.http.res");
-        
+
     static String st_200=null;
     static String st_302=null;
     static String st_400=null;
     static String st_404=null;
-    
+
     /** Get the status string associated with a status code.
      *  No I18N - return the messages defined in the HTTP spec.
      *  ( the user isn't supposed to see them, this is the last
@@ -48,21 +47,29 @@ public class HttpMessages {
      */
     public static String getMessage( int status ) {
         // method from Response.
-        
+
         // Does HTTP requires/allow international messages or
         // are pre-defined? The user doesn't see them most of the time
         switch( status ) {
         case 200:
-            if( st_200==null ) st_200=sm.getString( "sc.200");
+            if( st_200==null ) {
+                st_200=sm.getString( "sc.200");
+            }
             return st_200;
         case 302:
-            if( st_302==null ) st_302=sm.getString( "sc.302");
+            if( st_302==null ) {
+                st_302=sm.getString( "sc.302");
+            }
             return st_302;
         case 400:
-            if( st_400==null ) st_400=sm.getString( "sc.400");
+            if( st_400==null ) {
+                st_400=sm.getString( "sc.400");
+            }
             return st_400;
         case 404:
-            if( st_404==null ) st_404=sm.getString( "sc.404");
+            if( st_404==null ) {
+                st_404=sm.getString( "sc.404");
+            }
             return st_404;
         }
         return sm.getString("sc."+ status);
@@ -77,8 +84,9 @@ public class HttpMessages {
      */
     public static String filter(String message) {
 
-        if (message == null)
+        if (message == null) {
             return (null);
+        }
 
         char content[] = new char[message.length()];
         message.getChars(0, message.length(), content, 0);
@@ -107,7 +115,7 @@ public class HttpMessages {
     /**
      * Is the provided message safe to use in an HTTP header. Safe messages must
      * meet the requirements of RFC2616 - i.e. must consist only of TEXT.
-     * 
+     *
      * @param msg   The message to test
      * @return      <code>true</code> if the message is safe to use in an HTTP
      *              header else <code>false</code>

java/org/apache/tomcat/util/http/MimeHeaders.java
@@ -14,7 +14,6 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-
 package org.apache.tomcat.util.http;
 
 import java.io.PrintWriter;
@@ -53,16 +52,16 @@ import org.apache.tomcat.util.buf.MessageBytes;
    directly access all headers, and most headers are single-valued.
    ( the alternative - a hash or similar data structure - will add
    an overhead that is not needed in most cases )
-   
+
    Apache seems to be using a similar method for storing and manipulating
    headers.
-       
+
    Future enhancements:
    - hash the headers the first time a header is requested ( i.e. if the
    servlet needs direct access to headers).
    - scan "common" values ( length, cookies, etc ) during the parse
    ( addHeader hook )
-   
+
 */
 
 
@@ -83,8 +82,8 @@ import org.apache.tomcat.util.buf.MessageBytes;
  *  XXX remove unused methods
  *  XXX External enumerations, with 0 GC.
  *  XXX use HeaderName ID
- *  
- * 
+ *
+ *
  * @author dac@eng.sun.com
  * @author James Todd [gonzo@eng.sun.com]
  * @author Costin Manolache
@@ -95,7 +94,7 @@ public class MimeHeaders {
      *  XXX  make it configurable ( fine-tuning of web-apps )
      */
     public static final int DEFAULT_HEADER_SIZE=8;
-    
+
     /**
      * The header fields.
      */
@@ -154,7 +153,7 @@ public class MimeHeaders {
     }
 
     // -------------------- Idx access to headers ----------
-    
+
     /**
      * Returns the current number of header fields.
      */
@@ -182,7 +181,7 @@ public class MimeHeaders {
      */
     public int findHeader( String name, int starting ) {
         // We can use a hash - but it's not clear how much
-        // benefit you can get - there is an  overhead 
+        // benefit you can get - there is an  overhead
         // and the number of headers is small (4-5 ?)
         // Another problem is that we'll pay the overhead
         // of constructing the hashtable
@@ -195,7 +194,7 @@ public class MimeHeaders {
         }
         return -1;
     }
-    
+
     // -------------------- --------------------
 
     /**
@@ -212,7 +211,7 @@ public class MimeHeaders {
     }
 
     // -------------------- Adding headers --------------------
-    
+
 
     /**
      * Adds a partially constructed field to the header.  This
@@ -263,7 +262,7 @@ public class MimeHeaders {
         return mhf.getValue();
     }
 
-    /** Allow "set" operations - 
+    /** Allow "set" operations -
         return a MessageBytes container for the
         header value ( existing header or new
         if this .
@@ -302,7 +301,7 @@ public class MimeHeaders {
     /**
      * Finds and returns a unique header field with the given name. If no such
      * field exists, null is returned. If the specified header field is not
-     * unique then an {@link IllegalArgumentException} is thrown. 
+     * unique then an {@link IllegalArgumentException} is thrown.
      */
     public MessageBytes getUniqueValue(String name) {
         MessageBytes result = null;
@@ -348,7 +347,7 @@ public class MimeHeaders {
      */
     private void removeHeader(int idx) {
         MimeHeaderField mh = headers[idx];
-        
+
         mh.recycle();
         headers[idx] = headers[count - 1];
         headers[count - 1] = mh;
@@ -397,7 +396,7 @@ class NamesEnumerator implements Enumeration<String> {
         // next element
         pos++;
     }
-    
+
     @Override
     public boolean hasMoreElements() {
         return next!=null;
@@ -440,7 +439,7 @@ class ValuesEnumerator implements Enumeration<String> {
         }
         pos++;
     }
-    
+
     @Override
     public boolean hasMoreElements() {
         return next!=null;
@@ -459,8 +458,8 @@ class MimeHeaderField {
     // speed up name enumerations and search ( both cpu and
     // GC)
     MimeHeaderField next;
-    MimeHeaderField prev; 
-    
+    MimeHeaderField prev;
+
     protected final MessageBytes nameB = MessageBytes.newInstance();
     protected final MessageBytes valueB = MessageBytes.newInstance();
 

commit ce09c9a8467f4623dab9eff63ec0fdef28093737

Fix compilation failure caused by r1200184
git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1200186 13f79535-47bb-0310-9956-ffa450edef68

java/org/apache/tomcat/util/http/Parameters.java
@@ -281,7 +281,6 @@ public final class Parameters {
                         break;
                 }
             } while (!parameterComplete && pos < end);
-            }
 
             if (pos == end) {
                 if (nameEnd == -1) {