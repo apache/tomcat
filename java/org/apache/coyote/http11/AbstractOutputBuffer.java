594bf368ce978803aa293c145bd6351f3ec3b423
Rainer Jung <rjung@apache.org>
2011-11-11 22:18:27 +0000

    Improve multi-byte character handling in Coyote output for HTTP and AJP.
    Backport of r1201069 from trunk.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1201076 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http11/AbstractOutputBuffer.java
+++ java/org/apache/coyote/http11/AbstractOutputBuffer.java
@@ -473,10 +473,8 @@ public abstract class AbstractOutputBuffer<S> implements OutputBuffer{
             // but is the only consistent approach within the current
             // servlet framework.  It must suffice until servlet output
             // streams properly encode their output.
-            if ((c <= 31) && (c != 9)) {
-                c = ' ';
-            } else if (c == 127) {
-                c = ' ';
+            if (((c <= 31) && (c != 9)) || c == 127 || c > 255) {
+              c = ' ';
             }
             buf[pos++] = (byte) c;
         }
@@ -520,10 +518,8 @@ public abstract class AbstractOutputBuffer<S> implements OutputBuffer{
             // but is the only consistent approach within the current
             // servlet framework.  It must suffice until servlet output
             // streams properly encode their output.
-            if ((c <= 31) && (c != 9)) {
-                c = ' ';
-            } else if (c == 127) {
-                c = ' ';
+            if (((c <= 31) && (c != 9)) || c == 127 || c > 255) {
+              c = ' ';
             }
             buf[pos++] = (byte) c;
         }
