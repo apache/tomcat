ac1bd41798ac406a99984e62a6ec9139ac25c8bf
Mark Emlyn David Thomas <markt@apache.org>
2012-01-19 15:21:30 +0000

    s/exipre/expire/g
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1233426 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/ha/session/DeltaManager.java
+++ java/org/apache/catalina/ha/session/DeltaManager.java
@@ -1193,7 +1193,7 @@ public class DeltaManager extends ClusterManagerBase{
             }//end if
         }//for
         long timeEnd = System.currentTimeMillis();
-        if(log.isDebugEnabled()) log.debug("End expire sessions " + getName() + " exipre processingTime " + (timeEnd - timeNow) + " expired direct sessions: " + expireDirect + " expired direct sessions: " + expireIndirect);
+        if(log.isDebugEnabled()) log.debug("End expire sessions " + getName() + " expire processingTime " + (timeEnd - timeNow) + " expired direct sessions: " + expireDirect + " expired direct sessions: " + expireIndirect);
 
     }
 
