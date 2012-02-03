24e4039f8100c0dbea98b38f070252341e67fe24
Mark Emlyn David Thomas <markt@apache.org>
2012-02-03 12:07:04 +0000

    Use final
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1240103 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/mbeans/MBeanUtils.java
+++ java/org/apache/catalina/mbeans/MBeanUtils.java
@@ -79,7 +79,7 @@ public class MBeanUtils {
      * <code>createManagedBean()</code>.  The first element of each pair
      * is a class name, and the second element is the managed bean name.
      */
-    private static String exceptions[][] = {
+    private static final String exceptions[][] = {
         { "org.apache.catalina.users.MemoryGroup",
           "Group" },
         { "org.apache.catalina.users.MemoryRole",
@@ -908,7 +908,7 @@ public class MBeanUtils {
 
     }
 
-    static Hashtable<String,int[]> seq = new Hashtable<String,int[]>();
+    static final Hashtable<String,int[]> seq = new Hashtable<String,int[]>();
     static int getSeq( String key ) {
         int i[]=seq.get( key );
         if (i == null ) {
