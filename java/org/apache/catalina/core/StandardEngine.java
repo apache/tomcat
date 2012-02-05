174a21510e17277f0fb61111f605d0eaf5d0326a
Mark Thomas <markt@apache.org>
2012-02-05 20:12:26 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=52587 Ensure that if it is necessary to fall back to the default NullRealm, the NullRealm instance is created early enough for it to be correctly initialised.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1240795 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/core/StandardEngine.java
+++ java/org/apache/catalina/core/StandardEngine.java
@@ -237,6 +237,15 @@ public class StandardEngine extends ContainerBase implements Engine {
     }
 
 
+    @Override
+    protected void initInternal() throws LifecycleException {
+        // Ensure that a Realm is present before any attempt is made to start
+        // one. This will create the default NullRealm if necessary.
+        getRealm();
+        super.initInternal();
+    }
+
+
     /**
      * Start this component and implement the requirements
      * of {@link org.apache.catalina.util.LifecycleBase#startInternal()}.
