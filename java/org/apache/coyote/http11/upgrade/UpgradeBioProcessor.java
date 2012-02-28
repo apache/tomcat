325b94423d7a3a0c4a76dc2c54fb29474987d134
Mark Thomas <markt@apache.org>
2012-02-28 20:20:12 +0000

    Remove unnecessary comment post refactoring. Fix new lines.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1294817 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http11/upgrade/UpgradeBioProcessor.java
+++ java/org/apache/coyote/http11/upgrade/UpgradeBioProcessor.java
@@ -23,12 +23,6 @@ import java.net.Socket;
 
 import org.apache.tomcat.util.net.SocketWrapper;
 
-/**
- * Implementation note: The need to extend Http11Processor could probably be
- * removed if the Processor interface was expanded to cover all of the methods
- * required by the AbstractProtocol. That would simplify the code and further
- * reduce the size of instances of this class.
- */
 public class UpgradeBioProcessor extends UpgradeProcessor<Socket> {
 
     private final InputStream inputStream;
