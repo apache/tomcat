094bcdcc347cc4d7b2a79d0cb1bcd772ecd3ee74
Filip Hanik <fhanik@apache.org>
2012-03-01 18:27:56 +0000

    Allow the examples to configure the buffer on the fly
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1295724 13f79535-47bb-0310-9956-ffa450edef68

--- webapps/examples/WEB-INF/classes/websocket/EchoMessage.java
+++ webapps/examples/WEB-INF/classes/websocket/EchoMessage.java
@@ -20,6 +20,8 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 
+import javax.servlet.ServletException;
+
 import org.apache.catalina.websocket.MessageInbound;
 import org.apache.catalina.websocket.StreamInbound;
 import org.apache.catalina.websocket.WebSocketServlet;
@@ -28,13 +30,40 @@ import org.apache.catalina.websocket.WebSocketServlet;
 public class EchoMessage extends WebSocketServlet {
 
     private static final long serialVersionUID = 1L;
+    private volatile int byteBufSize;
+    private volatile int charBufSize;
+    
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        byteBufSize = getInitParameterIntValue("byteBufferMaxSize", 2097152);
+        charBufSize = getInitParameterIntValue("charBufferMaxSize", 2097152);
+    }
+    
+    public int getInitParameterIntValue(String name, int defaultValue) {
+        String val = this.getInitParameter(name);
+        int result = defaultValue;
+        try {
+            result = Integer.parseInt(val);
+        }catch (Exception x) {
+        }
+        return result;
+    }
+
+
 
     @Override
     protected StreamInbound createWebSocketInbound(String subProtocol) {
-        return new EchoMessageInbound();
+        return new EchoMessageInbound(byteBufSize,charBufSize);
     }
 
     private static final class EchoMessageInbound extends MessageInbound {
+        
+        public EchoMessageInbound(int byteBufferMaxSize, int charBufferMaxSize) {
+            super();
+            setByteBufferMaxSize(byteBufferMaxSize);
+            setCharBufferMaxSize(charBufferMaxSize);
+        }
 
         @Override
         protected void onBinaryMessage(ByteBuffer message) throws IOException {
