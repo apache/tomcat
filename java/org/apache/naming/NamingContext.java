7cec4ff35ae186f414f7de012ff49f84838ae6dd
Mark Emlyn David Thomas <markt@apache.org>
2012-03-07 21:11:01 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=52792 More informative error message when something in JNDI can't be found.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1298121 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/naming/NamingContext.java
+++ java/org/apache/naming/NamingContext.java
@@ -274,7 +274,7 @@ public class NamingContext implements Context {
 
         if (entry == null) {
             throw new NameNotFoundException
-                (sm.getString("namingContext.nameNotBound", name.get(0)));
+                (sm.getString("namingContext.nameNotBound", name, name.get(0)));
         }
 
         if (name.size() > 1) {
@@ -369,7 +369,7 @@ public class NamingContext implements Context {
 
         if (entry == null) {
             throw new NameNotFoundException
-                (sm.getString("namingContext.nameNotBound", name.get(0)));
+                (sm.getString("namingContext.nameNotBound", name, name.get(0)));
         }
 
         if (entry.type != NamingEntry.CONTEXT) {
@@ -423,7 +423,7 @@ public class NamingContext implements Context {
 
         if (entry == null) {
             throw new NameNotFoundException
-                (sm.getString("namingContext.nameNotBound", name.get(0)));
+                (sm.getString("namingContext.nameNotBound", name, name.get(0)));
         }
 
         if (entry.type != NamingEntry.CONTEXT) {
@@ -492,7 +492,7 @@ public class NamingContext implements Context {
 
         if (entry == null) {
             throw new NameNotFoundException
-                (sm.getString("namingContext.nameNotBound", name.get(0)));
+                (sm.getString("namingContext.nameNotBound", name, name.get(0)));
         }
 
         if (name.size() > 1) {
@@ -818,7 +818,7 @@ public class NamingContext implements Context {
 
         if (entry == null) {
             throw new NameNotFoundException
-                (sm.getString("namingContext.nameNotBound", name.get(0)));
+                (sm.getString("namingContext.nameNotBound", name, name.get(0)));
         }
 
         if (name.size() > 1) {
@@ -897,8 +897,8 @@ public class NamingContext implements Context {
 
         if (name.size() > 1) {
             if (entry == null) {
-                throw new NameNotFoundException
-                    (sm.getString("namingContext.nameNotBound", name.get(0)));
+                throw new NameNotFoundException(sm.getString(
+                        "namingContext.nameNotBound", name, name.get(0)));
             }
             if (entry.type == NamingEntry.CONTEXT) {
                 if (rebind) {
