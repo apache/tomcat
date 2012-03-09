94834f949dbf14452862fc480e2fbc0957d50f8a
Mark Thomas <markt@apache.org>
2012-03-09 19:08:49 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=52850 Enable the memoey leak protection code to play nicely with IBM JVMs as well as Oracle JVMs. Extend test case coverage of memory leak protection. Patch provided by Rohit Kelapure.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1298983 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/loader/WebappClassLoader.java
+++ java/org/apache/catalina/loader/WebappClassLoader.java
@@ -27,6 +27,7 @@ import java.io.InputStream;
 import java.lang.ref.Reference;
 import java.lang.ref.WeakReference;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.net.MalformedURLException;
@@ -2244,8 +2245,7 @@ public class WebappClassLoader
                     }
 
                     // TimerThread can be stopped safely so treat separately
-                    if (thread.getClass().getName().equals(
-                            "java.util.TimerThread") &&
+                    if (thread.getClass().getName().startsWith("java.util.Timer") &&
                             clearReferencesStopTimerThreads) {
                         clearReferencesStopTimerThread(thread);
                         continue;
@@ -2268,20 +2268,28 @@ public class WebappClassLoader
                     // If the thread has been started via an executor, try
                     // shutting down the executor
                     try {
-                        Field targetField =
-                            thread.getClass().getDeclaredField("target");
-                        targetField.setAccessible(true);
-                        Object target = targetField.get(thread);
 
-                        if (target != null &&
-                                target.getClass().getCanonicalName().equals(
-                                "java.util.concurrent.ThreadPoolExecutor.Worker")) {
-                            Field executorField =
-                                target.getClass().getDeclaredField("this$0");
-                            executorField.setAccessible(true);
-                            Object executor = executorField.get(target);
-                            if (executor instanceof ThreadPoolExecutor) {
-                                ((ThreadPoolExecutor) executor).shutdownNow();
+                        Field targetField = null;
+                        try {
+                            targetField = thread.getClass().getDeclaredField("target");
+                        }catch (NoSuchFieldException nfe){
+                            targetField = thread.getClass().getDeclaredField("runnable");
+                        }
+                        if (null != targetField){
+                            targetField.setAccessible(true);
+                            Object target = targetField.get(thread);
+
+                            if (target != null &&
+                                    target.getClass().getCanonicalName() != null
+                                    && target.getClass().getCanonicalName().equals(
+                                    "java.util.concurrent.ThreadPoolExecutor.Worker")) {
+                                Field executorField =
+                                    target.getClass().getDeclaredField("this$0");
+                                executorField.setAccessible(true);
+                                Object executor = executorField.get(target);
+                                if (executor instanceof ThreadPoolExecutor) {
+                                    ((ThreadPoolExecutor) executor).shutdownNow();
+                                }
                             }
                         }
                     } catch (SecurityException e) {
@@ -2350,21 +2358,33 @@ public class WebappClassLoader
         // - queue.clear()
 
         try {
-            Field newTasksMayBeScheduledField =
-                thread.getClass().getDeclaredField("newTasksMayBeScheduled");
-            newTasksMayBeScheduledField.setAccessible(true);
-            Field queueField = thread.getClass().getDeclaredField("queue");
-            queueField.setAccessible(true);
 
-            Object queue = queueField.get(thread);
+            try {
+                Field newTasksMayBeScheduledField =
+                    thread.getClass().getDeclaredField("newTasksMayBeScheduled");
+                newTasksMayBeScheduledField.setAccessible(true);
+                Field queueField = thread.getClass().getDeclaredField("queue");
+                queueField.setAccessible(true);
 
-            Method clearMethod = queue.getClass().getDeclaredMethod("clear");
-            clearMethod.setAccessible(true);
+                Object queue = queueField.get(thread);
 
-            synchronized(queue) {
-                newTasksMayBeScheduledField.setBoolean(thread, false);
-                clearMethod.invoke(queue);
-                queue.notify();  // In case queue was already empty.
+                Method clearMethod = queue.getClass().getDeclaredMethod("clear");
+                clearMethod.setAccessible(true);
+
+                synchronized(queue) {
+                    newTasksMayBeScheduledField.setBoolean(thread, false);
+                    clearMethod.invoke(queue);
+                    queue.notify();  // In case queue was already empty.
+                }
+
+            }catch (NoSuchFieldException nfe){
+                Method cancelMethod = thread.getClass().getDeclaredMethod("cancel");
+                if (null != cancelMethod){
+                    synchronized(thread) {
+                        cancelMethod.setAccessible(true);
+                        cancelMethod.invoke(thread);
+                    }
+                }
             }
 
             log.error(sm.getString("webappClassLoader.warnTimerThread",
@@ -2394,21 +2414,29 @@ public class WebappClassLoader
             inheritableThreadLocalsField.setAccessible(true);
             // Make the underlying array of ThreadLoad.ThreadLocalMap.Entry objects
             // accessible
-            Class<?> tlmClass =
-                Class.forName("java.lang.ThreadLocal$ThreadLocalMap");
+            Class<?> tlmClass = Class.forName("java.lang.ThreadLocal$ThreadLocalMap");
             Field tableField = tlmClass.getDeclaredField("table");
             tableField.setAccessible(true);
+            Method expungeStaleEntriesMethod = tlmClass.getDeclaredMethod("expungeStaleEntries");
+            expungeStaleEntriesMethod.setAccessible(true);
 
             for (int i = 0; i < threads.length; i++) {
                 Object threadLocalMap;
                 if (threads[i] != null) {
+
                     // Clear the first map
                     threadLocalMap = threadLocalsField.get(threads[i]);
-                    checkThreadLocalMapForLeaks(threadLocalMap, tableField);
+                    if (null != threadLocalMap){
+                        expungeStaleEntriesMethod.invoke(threadLocalMap);
+                        checkThreadLocalMapForLeaks(threadLocalMap, tableField);
+                    }
+
                     // Clear the second map
-                    threadLocalMap =
-                        inheritableThreadLocalsField.get(threads[i]);
-                    checkThreadLocalMapForLeaks(threadLocalMap, tableField);
+                    threadLocalMap =inheritableThreadLocalsField.get(threads[i]);
+                    if (null != threadLocalMap){
+                        expungeStaleEntriesMethod.invoke(threadLocalMap);
+                        checkThreadLocalMapForLeaks(threadLocalMap, tableField);
+                    }
                 }
             }
         } catch (SecurityException e) {
@@ -2424,6 +2452,12 @@ public class WebappClassLoader
             log.warn(sm.getString("webappClassLoader.checkThreadLocalsForLeaksFail",
                     contextName), e);
         } catch (IllegalAccessException e) {
+            log.warn(sm.getString("webappClassLoader.checkThreadLocalsForLeaksFail",
+                    contextName), e);
+        } catch (InvocationTargetException e) {
+            log.warn(sm.getString("webappClassLoader.checkThreadLocalsForLeaksFail",
+                    contextName), e);
+        } catch (NoSuchMethodException e) {
             log.warn(sm.getString("webappClassLoader.checkThreadLocalsForLeaksFail",
                     contextName), e);
         }
@@ -2529,7 +2563,7 @@ public class WebappClassLoader
 
         ClassLoader cl = clazz.getClassLoader();
         while (cl != null) {
-            if(cl == this) {
+            if (cl == this) {
                 return true;
             }
             cl = cl.getParent();
