7c7b441ca6379c369efd3d4e1ea61270dfa89702
Konstantin Kolinko <kkolinko@apache.org>
2012-03-20 18:55:40 +0000

    Fix checkstyle issues with trailing spaces and imports formatting.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1303096 13f79535-47bb-0310-9956-ffa450edef68

--- modules/jdbc-pool/src/test/java/org/apache/tomcat/jdbc/test/MultipleCloseTest.java
+++ modules/jdbc-pool/src/test/java/org/apache/tomcat/jdbc/test/MultipleCloseTest.java
@@ -18,6 +18,7 @@
 package org.apache.tomcat.jdbc.test;
 
 import java.sql.Connection;
+
 import org.apache.tomcat.jdbc.test.driver.Driver;
 
 public class MultipleCloseTest extends DefaultTestCase {
