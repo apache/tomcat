2acad4b58b156b8617d2f6e3159ce734f1b398e9
Keiichi Fujino <kfujino@apache.org>
2012-04-23 02:57:35 +0000

    Make sure complete message takes priority over access message.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1329045 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/tribes/tipis/AbstractReplicatedMap.java
+++ java/org/apache/catalina/tribes/tipis/AbstractReplicatedMap.java
@@ -439,19 +439,19 @@ public abstract class AbstractReplicatedMap<K,V>
                 }
 
             }
-            if (msg == null && isAccess) {
-                //construct a access message
-                msg = new MapMessage(mapContextName, MapMessage.MSG_ACCESS,
-                        false, (Serializable) entry.getKey(), null, null, entry.getPrimary(),
-                        entry.getBackupNodes());
-            }
-            if (msg == null) {
+            if (msg == null && complete) {
                 //construct a complete
                 msg = new MapMessage(mapContextName, MapMessage.MSG_BACKUP,
                                      false, (Serializable) entry.getKey(),
                                      (Serializable) entry.getValue(),
                                      null, entry.getPrimary(),entry.getBackupNodes());
 
+            }
+            if (msg == null) {
+                //construct a access message
+                msg = new MapMessage(mapContextName, MapMessage.MSG_ACCESS,
+                        false, (Serializable) entry.getKey(), null, null, entry.getPrimary(),
+                        entry.getBackupNodes());
             }
             try {
                 if ( channel!=null && entry.getBackupNodes()!= null && entry.getBackupNodes().length > 0 ) {
