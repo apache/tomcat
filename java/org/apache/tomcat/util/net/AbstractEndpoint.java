ef90bc27a6d34d9715d6fcac4f455ec17cab7117
Konstantin Kolinko <kkolinko@apache.org>
2012-05-12 17:24:07 +0000

    Merged revisions r1337591 r1337595 from tomcat/trunk: Followup to r1336516 Add Javadoc (as asked by sebb) and avoid looking up a volatile field twice
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1337597 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/net/AbstractEndpoint.java
+++ java/org/apache/tomcat/util/net/AbstractEndpoint.java
@@ -174,9 +174,24 @@ public abstract class AbstractEndpoint {
 
     public int  getMaxConnections() { return this.maxConnections; }
 
+    /**
+     * Return the current count of connections handled by this endpoint, if the
+     * connections are counted (which happens when the maximum count of
+     * connections is limited), or <code>-1</code> if they are not. This
+     * property is added here so that this value can be inspected through JMX.
+     * It is visible on "ThreadPool" MBean.
+     *
+     * <p>The count is incremented by the Acceptor before it tries to accept a
+     * new connection. Until the limit is reached and thus the count cannot be
+     * incremented,  this value is more by 1 (the count of acceptors) than the
+     * actual count of connections that are being served.
+     *
+     * @return The count
+     */
     public long getConnectionCount() {
-        if (connectionLimitLatch != null) {
-            return connectionLimitLatch.getCount();
+        LimitLatch latch = connectionLimitLatch;
+        if (latch != null) {
+            return latch.getCount();
         }
         return -1;
     }
