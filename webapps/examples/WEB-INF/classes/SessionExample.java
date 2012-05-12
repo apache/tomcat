17769595fcad2e7311accc69852554cfe3084d89
Konstantin Kolinko <kkolinko@apache.org>
2012-05-12 23:17:21 +0000

    Merged revision 1337719 from tomcat/trunk: Fix several trivial HTML markup errors in example servlets e.g. duplicate <body> tag preceding <head> one.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1337730 13f79535-47bb-0310-9956-ffa450edef68

--- webapps/examples/WEB-INF/classes/SessionExample.java
+++ webapps/examples/WEB-INF/classes/SessionExample.java
@@ -53,13 +53,12 @@ public class SessionExample extends HttpServlet {
 
         PrintWriter out = response.getWriter();
         out.println("<html>");
-        out.println("<body bgcolor=\"white\">");
         out.println("<head>");
 
         String title = RB.getString("sessions.title");
         out.println("<title>" + title + "</title>");
         out.println("</head>");
-        out.println("<body>");
+        out.println("<body bgcolor=\"white\">");
 
         // img stuff not req'd for source code html showing
         // relative links everywhere!
@@ -130,11 +129,8 @@ public class SessionExample extends HttpServlet {
         out.println("</form>");
 
         out.print("<p><a href=\"");
-        out.print(response.encodeURL("SessionExample?dataname=foo&datavalue=bar"));
+        out.print(response.encodeURL("SessionExample?dataname=foo&amp;datavalue=bar"));
         out.println("\" >URL encoded </a>");
-
-        out.println("</body>");
-        out.println("</html>");
 
         out.println("</body>");
         out.println("</html>");
