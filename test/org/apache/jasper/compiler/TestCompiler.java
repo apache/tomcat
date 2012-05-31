apache/tomcat

commit 5bc141ea954a4415d1290a1493d3e4619a8a1a0a

Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=51584 Ensure file paths are encoded/decoded when translated to/from URLs so special characters don't cause issues.
git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1152593 13f79535-47bb-0310-9956-ffa450edef68

java/org/apache/naming/resources/DirContextURLConnection.java
@@ -22,6 +22,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.security.Permission;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -153,6 +155,7 @@ public class DirContextURLConnection
                         path = path.substring(contextPath.length());
                     }
                 }
+                path = URLDecoder.decode(path, "UTF-8");
                 object = context.lookup(path);
                 attributes = context.getAttributes(path);
                 if (object instanceof Resource)
@@ -385,7 +388,8 @@ public class DirContextURLConnection
 
         // Reopen resource
         try {
-            resource = (Resource) context.lookup(getURL().getFile());
+            resource = (Resource) context.lookup(
+                    URLDecoder.decode(getURL().getFile(), "UTF-8"));
         } catch (NamingException e) {
             // Ignore
         }
@@ -445,7 +449,8 @@ public class DirContextURLConnection
                     context.list(file.substring(start));
                 while (enumeration.hasMoreElements()) {
                     NameClassPair ncp = enumeration.nextElement();
-                    result.addElement(ncp.getName());
+                    result.addElement(
+                            URLEncoder.encode(ncp.getName(), "UTF-8"));
                 }
             } catch (NamingException e) {
                 // Unexpected exception

commit 4b097daec77957d7d359c8ac1ffe4c45382a6da3

Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=53257 BZ 53257 is partially a regression caused by r1152593 (the fix for BZ 51584) and partially an existing issue that had always been present. This patch: - reverts r1152593 - extends the test cases to cover BZ 53257 & BZ 51584 - correctly fixes BZ 53257 & BZ 51584 - fixes the additional issues the unit tests uncovered
git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1344890 13f79535-47bb-0310-9956-ffa450edef68

java/org/apache/catalina/core/ApplicationContext.java
@@ -14,14 +14,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-
 package org.apache.catalina.core;
-
 
 import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -69,7 +67,6 @@ import org.apache.catalina.connector.Connector;
 import org.apache.catalina.deploy.FilterDef;
 import org.apache.catalina.util.ResourceSet;
 import org.apache.catalina.util.ServerInfo;
-import org.apache.naming.resources.DirContextURLStreamHandler;
 import org.apache.naming.resources.Resource;
 import org.apache.tomcat.util.ExceptionUtils;
 import org.apache.tomcat.util.buf.CharChunk;
@@ -520,9 +517,8 @@ public class ApplicationContext
             String hostName = context.getParent().getName();
             try {
                 resources.lookup(normPath);
-                return new URL
-                    ("jndi", "", 0, getJNDIUri(hostName, fullPath),
-                     new DirContextURLStreamHandler(resources));
+                return new URI("jndi",
+                        getJNDIUri(hostName, fullPath), null).toURL();
             } catch (NamingException e) {
                 // Ignore
             } catch (Exception e) {

java/org/apache/naming/resources/DirContextURLConnection.java
@@ -22,8 +22,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
 import java.security.Permission;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -42,6 +40,7 @@ import javax.naming.directory.Attributes;
 import javax.naming.directory.DirContext;
 
 import org.apache.naming.JndiPermission;
+import org.apache.tomcat.util.buf.UDecoder;
 import org.apache.tomcat.util.http.FastHttpDateFormat;
 
 /**
@@ -55,8 +54,9 @@ import org.apache.tomcat.util.http.FastHttpDateFormat;
  * @author <a href="mailto:remm@apache.org">Remy Maucherat</a>
  * @version $Revision$
  */
-public class DirContextURLConnection
-    extends URLConnection {
+public class DirContextURLConnection extends URLConnection {
+
+    private static final UDecoder URL_DECODER = new UDecoder();
 
 
     // ----------------------------------------------------------- Constructors
@@ -123,7 +123,6 @@ public class DirContextURLConnection
 
     // ------------------------------------------------------------- Properties
 
-
     /**
      * Connect to the DirContext, and retrieve the bound object, as well as
      * its attributes. If no object is bound with the name specified in the
@@ -157,7 +156,7 @@ public class DirContextURLConnection
                         path = path.substring(contextPath.length());
                     }
                 }
-                path = URLDecoder.decode(path, "UTF-8");
+                path = URL_DECODER.convert(path, false);
                 object = context.lookup(path);
                 attributes = context.getAttributes(path);
                 if (object instanceof Resource)
@@ -390,8 +389,7 @@ public class DirContextURLConnection
 
         // Reopen resource
         try {
-            resource = (Resource) context.lookup(
-                    URLDecoder.decode(getURL().getFile(), "UTF-8"));
+            resource = (Resource) context.lookup(getURL().getFile());
         } catch (NamingException e) {
             // Ignore
         }
@@ -457,8 +455,7 @@ public class DirContextURLConnection
                     context.list(file.substring(start));
                 while (enumeration.hasMoreElements()) {
                     NameClassPair ncp = enumeration.nextElement();
-                    result.addElement(
-                            URLEncoder.encode(ncp.getName(), "UTF-8"));
+                    result.addElement(ncp.getName());
                 }
             } catch (NamingException e) {
                 // Unexpected exception

res/checkstyle/org-import-control.xml
@@ -106,6 +106,7 @@
     <allow pkg="javax.mail"/>
     <allow pkg="javax.wsdl"/>
     <allow pkg="org.apache.naming"/>
+    <allow class="org.apache.tomcat.util.buf.UDecoder"/>
     <allow class="org.apache.tomcat.util.http.FastHttpDateFormat"/>
     <allow class="org.apache.tomcat.util.http.RequestUtil"/>
     <subpackage name="factory.webservices">

test/org/apache/jasper/compiler/TestCompiler.java
@@ -76,8 +76,101 @@ public class TestCompiler extends TomcatBaseTest {
         assertTrue(headers.get("Content-Type").get(0).startsWith("text/plain"));
     }
 
+    @Test
+    public void testBug53257a() throws Exception {
+        Tomcat tomcat = getTomcatInstance();
+
+        File appDir = new File("test/webapp-3.0");
+        tomcat.addWebapp(null, "/test", appDir.getAbsolutePath());
+        tomcat.start();
+
+        ByteChunk res = getUrl("http://localhost:" + getPort() +
+                "/test/bug53257/foo%3bbar.jsp");
+
+        // Check request completed
+        String result = res.toString();
+        assertEcho(result, "OK");
+    }
+
+    @Test
+    public void testBug53257b() throws Exception {
+        Tomcat tomcat = getTomcatInstance();
+
+        File appDir = new File("test/webapp-3.0");
+        tomcat.addWebapp(null, "/test", appDir.getAbsolutePath());
+        tomcat.start();
+
+        ByteChunk res = getUrl("http://localhost:" + getPort() +
+                "/test/bug53257/foo&bar.jsp");
+
+        // Check request completed
+        String result = res.toString();
+        assertEcho(result, "OK");
+    }
+
+    @Test
+    public void testBug53257c() throws Exception {
+        Tomcat tomcat = getTomcatInstance();
+
+        File appDir = new File("test/webapp-3.0");
+        tomcat.addWebapp(null, "/test", appDir.getAbsolutePath());
+        tomcat.start();
+
+        ByteChunk res = getUrl("http://localhost:" + getPort() +
+                "/test/bug53257/foo%23bar.jsp");
+
+        // Check request completed
+        String result = res.toString();
+        assertEcho(result, "OK");
+    }
+
+    @Test
+    public void testBug53257d() throws Exception {
+        Tomcat tomcat = getTomcatInstance();
+
+        File appDir = new File("test/webapp-3.0");
+        tomcat.addWebapp(null, "/test", appDir.getAbsolutePath());
+        tomcat.start();
+
+        ByteChunk res = getUrl("http://localhost:" + getPort() +
+                "/test/bug53257/foo%25bar.jsp");
+
+        // Check request completed
+        String result = res.toString();
+        assertEcho(result, "OK");
+    }
+
+    @Test
+    public void testBug53257e() throws Exception {
+        Tomcat tomcat = getTomcatInstance();
+
+        File appDir = new File("test/webapp-3.0");
+        tomcat.addWebapp(null, "/test", appDir.getAbsolutePath());
+        tomcat.start();
+
+        ByteChunk res = getUrl("http://localhost:" + getPort() +
+                "/test/bug53257/foo+bar.jsp");
+
+        // Check request completed
+        String result = res.toString();
+        assertEcho(result, "OK");
+    }
+
+    @Test
+    public void testBug51584() throws Exception {
+        Tomcat tomcat = getTomcatInstance();
+
+        File appDir = new File("test/webapp-3.0-fragments");
+        tomcat.addWebapp(null, "/test", appDir.getAbsolutePath());
+        tomcat.start();
+
+        // No further tests required. The bug triggers an infinite loop on
+        // context start so the test will crash before it reaches this point if
+        // it fails
+    }
+
     /** Assertion for text printed by tags:echo */
     private static void assertEcho(String result, String expected) {
-        assertTrue(result.indexOf("<p>" + expected + "</p>") > 0);
+        assertTrue(result, result.indexOf("<p>" + expected + "</p>") > 0);
     }
 }

test/webapp-3.0/bug53257/index.jsp
@@ -15,8 +15,8 @@
   limitations under the License.
 --%><%@page contentType="text/plain; charset=UTF-8"
 %><%@page import="java.net.URL,java.net.URLConnection"%><%
-    String[] testFiles =
-            new String[] {"foo;bar", "foo&bar", "foo#bar", "foo+bar"};
+    String[] testFiles = new String[] {"foo;bar.txt", "foo&bar.txt",
+            "foo#bar.txt", "foo%bar.txt", "foo+bar.txt"};
     for (String testFile : testFiles) {
         URL url = application.getResource("/bug53257/" + testFile);
         if (url == null) {