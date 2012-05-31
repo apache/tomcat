71cf9b6cb7c33d6321fcf5d8d2655c8c360e9d21
Mark Emlyn David Thomas <markt@apache.org>
2012-05-31 20:42:38 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=53257 BZ 53257 is partially a regression caused by r1152593 (the fix for BZ 51584) and partially an existing issue that had always been present. This patch: - reverts r1152593 - extends the test cases to cover BZ 53257 & BZ 51584 - correctly fixes BZ 53257 & BZ 51584 - fixes the additional issues the unit tests uncovered
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1344902 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/naming/resources/DirContextURLConnection.java
+++ java/org/apache/naming/resources/DirContextURLConnection.java
@@ -22,8 +22,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
 import java.security.Permission;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -42,6 +40,8 @@ import javax.naming.directory.Attributes;
 import javax.naming.directory.DirContext;
 
 import org.apache.naming.JndiPermission;
+import org.apache.tomcat.util.buf.UDecoder;
+import org.apache.tomcat.util.buf.UEncoder;
 import org.apache.tomcat.util.http.FastHttpDateFormat;
 
 /**
@@ -55,12 +55,16 @@ import org.apache.tomcat.util.http.FastHttpDateFormat;
  * @author <a href="mailto:remm@apache.org">Remy Maucherat</a>
  * @version $Revision$
  */
-public class DirContextURLConnection 
-    extends URLConnection {
-    
-    
+public class DirContextURLConnection extends URLConnection {
+
+    private static final UDecoder URL_DECODER = new UDecoder();
+    private static final UEncoder URL_ENCODER = new UEncoder();
+
+    static{
+        URL_ENCODER.addSafeCharacter('/');
+    }
+
     // ----------------------------------------------------------- Constructors
-    
     
     public DirContextURLConnection(DirContext context, URL url) {
         super(url);
@@ -120,8 +124,8 @@ public class DirContextURLConnection
 
 
     // ------------------------------------------------------------- Properties
-    
-    
+
+
     /**
      * Connect to the DirContext, and retrieve the bound object, as well as
      * its attributes. If no object is bound with the name specified in the
@@ -155,7 +159,7 @@ public class DirContextURLConnection
                         path = path.substring(contextPath.length());
                     }
                 }
-                path = URLDecoder.decode(path, "UTF-8");
+                path = URL_DECODER.convert(path, false);
                 object = context.lookup(path);
                 attributes = context.getAttributes(path);
                 if (object instanceof Resource)
@@ -389,7 +393,7 @@ public class DirContextURLConnection
         // Reopen resource
         try {
             resource = (Resource) context.lookup(
-                    URLDecoder.decode(getURL().getFile(), "UTF-8"));
+                    URL_DECODER.convert(getURL().getFile(), false));
         } catch (NamingException e) {
             // Ignore
         }
@@ -455,8 +459,7 @@ public class DirContextURLConnection
                     context.list(file.substring(start));
                 while (enumeration.hasMoreElements()) {
                     NameClassPair ncp = enumeration.nextElement();
-                    result.addElement(
-                            URLEncoder.encode(ncp.getName(), "UTF-8"));
+                    result.addElement(URL_ENCODER.encodeURL(ncp.getName()));
                 }
             } catch (NamingException e) {
                 // Unexpected exception
