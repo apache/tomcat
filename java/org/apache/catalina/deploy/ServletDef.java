5cb48baf6c8c8ad34b35cb4e37474d4d0207cda6
Mark Emlyn David Thomas <markt@apache.org>
2012-06-01 09:41:18 +0000

    Remove unnecessary code
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1345030 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/deploy/ServletDef.java
+++ java/org/apache/catalina/deploy/ServletDef.java
@@ -213,20 +213,6 @@ public class ServletDef implements Serializable {
     }
 
     /**
-     * Add a security-role-ref to the set of security-role-refs associated
-     * with this servlet.
-     * @deprecated
-     */
-    @Deprecated
-    public void addSecurityRoleRef(String roleName, String roleLink) {
-        SecurityRoleRef srr = new SecurityRoleRef();
-        srr.setName(roleName);
-        srr.setLink(roleLink);
-        securityRoleRefs.add(srr);
-    }
-
-
-    /**
      * The multipart configuration, if any, for this servlet
      */
     private MultipartDef multipartDef = null;
