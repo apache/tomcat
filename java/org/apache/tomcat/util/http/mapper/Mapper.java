6dc11de2ff3a05c6107c907dd8d163db8ee40c27
Mark Thomas <markt@apache.org>
2012-06-05 20:56:27 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=53356 Additional fix. Correct context & servlet paths
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1346581 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/http/mapper/Mapper.java
+++ java/org/apache/tomcat/util/http/mapper/Mapper.java
@@ -1036,9 +1036,9 @@ public final class Mapper {
             if (path.equals("/")) {
                 // Special handling for Context Root mapped servlet
                 mappingData.pathInfo.setString("/");
-                mappingData.wrapperPath.recycle();
+                mappingData.wrapperPath.setString("");
                 // This seems wrong but it is what the spec says...
-                mappingData.contextPath.recycle();
+                mappingData.contextPath.setString("");
             } else {
                 mappingData.wrapperPath.setString(wrappers[pos].name);
             }
