97ae6a63c0239f59909098d38dbc11b464a90274
Rainer Jung <rjung@apache.org>
2012-06-16 06:28:02 +0000

    Fix checking recommended tcnative version.
    Backport of r1350864 from trunk.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1350865 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/core/AprLifecycleListener.java
+++ java/org/apache/catalina/core/AprLifecycleListener.java
@@ -161,7 +161,7 @@ public class AprLifecycleListener
         int patch = 0;
         int apver = 0;
         int rqver = TCN_REQUIRED_MAJOR * 1000 + TCN_REQUIRED_MINOR * 100 + TCN_REQUIRED_PATCH;
-        int rcver = TCN_REQUIRED_MAJOR * 1000 + TCN_REQUIRED_MINOR * 100 + TCN_RECOMMENDED_PV;
+        int rcver = TCN_REQUIRED_MAJOR * 1000 + TCN_RECOMMENDED_MINOR * 100 + TCN_RECOMMENDED_PV;
 
         if (aprInitialized) {
             return;
