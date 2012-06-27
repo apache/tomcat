af1abbaf5f1b7960ef2d43d84c4f08928889b192
Filip Hanik <fhanik@apache.org>
2012-06-27 17:36:05 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=53416 Make sure we retain the domain if the registration comes from Tomcat
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1354641 13f79535-47bb-0310-9956-ffa450edef68

--- modules/jdbc-pool/src/main/java/org/apache/tomcat/jdbc/pool/DataSource.java
+++ modules/jdbc-pool/src/main/java/org/apache/tomcat/jdbc/pool/DataSource.java
@@ -118,7 +118,9 @@ public class DataSource extends DataSourceProxy implements javax.sql.DataSource,
         String origDomain = original.getDomain();
         properties.put("type", "ConnectionPool");
         properties.put("class", this.getClass().getName());
-        if (original.getKeyProperty("path")!=null) {
+        if (original.getKeyProperty("path")!=null || properties.get("context")!=null) {
+            //this ensures that if the registration came from tomcat, we're not losing
+            //the unique domain, but putting that into as an engine attribute
             properties.put("engine", origDomain);
         }
         ObjectName name = new ObjectName(domain,properties);
