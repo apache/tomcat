c6ea8be09f706f417ddd5036c2d5508873a919bc
Mark Emlyn David Thomas <markt@apache.org>
2012-07-28 18:04:29 +0000

    Don't parse username twice with different methods
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1366723 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/authenticator/DigestAuthenticator.java
+++ java/org/apache/catalina/authenticator/DigestAuthenticator.java
@@ -273,7 +273,7 @@ public class DigestAuthenticator extends AuthenticatorBase {
             }
             
             if (principal != null) {
-                String username = parseUsername(authorization);
+                String username = digestInfo.getUsername();
                 register(request, response, principal,
                         HttpServletRequest.DIGEST_AUTH,
                          username, null);
@@ -310,7 +310,10 @@ public class DigestAuthenticator extends AuthenticatorBase {
      * can be identified, return <code>null</code>
      *
      * @param authorization Authorization string to be parsed
+     *
+     * @deprecated  Unused. Will be removed in Tomcat 8.0.x
      */
+    @Deprecated
     protected String parseUsername(String authorization) {
 
         // Validate the authorization credentials format
@@ -516,6 +519,11 @@ public class DigestAuthenticator extends AuthenticatorBase {
             this.key = key;
             this.cnonces = cnonces;
             this.validateUri = validateUri;
+        }
+
+
+        public String getUsername() {
+            return userName;
         }
 
         public boolean validate(Request request, String authorization,
