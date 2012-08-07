60301e2654ac12ccc7f5ec14c4b3d709a883d8a3
Mark Emlyn David Thomas <markt@apache.org>
2012-08-07 16:58:53 +0000

    Deprecate unused code
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1370367 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/tribes/membership/MemberImpl.java
+++ java/org/apache/catalina/tribes/membership/MemberImpl.java
@@ -41,11 +41,21 @@ public class MemberImpl implements Member, java.io.Externalizable {
      * default is false
      */
     public static final boolean DO_DNS_LOOKUPS = Boolean.parseBoolean(System.getProperty("org.apache.catalina.tribes.dns_lookups","false"));
+
     /**
-     * Public properties specific to this implementation
+     * @deprecated  Unused - will be removed in Tomcat 8.0.x
      */
+    @Deprecated
     public static final transient String TCP_LISTEN_PORT = "tcpListenPort";
+    /**
+     * @deprecated  Unused - will be removed in Tomcat 8.0.x
+     */
+    @Deprecated
     public static final transient String TCP_LISTEN_HOST = "tcpListenHost";
+    /**
+     * @deprecated  Unused - will be removed in Tomcat 8.0.x
+     */
+    @Deprecated
     public static final transient String MEMBER_NAME = "memberName";
 
     public static final transient byte[] TRIBES_MBR_BEGIN = new byte[] {84, 82, 73, 66, 69, 83, 45, 66, 1, 0};
