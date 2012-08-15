ec1710ddec3caf694e6995f95962f399450fa8a8
Mark Emlyn David Thomas <markt@apache.org>
2012-08-15 20:37:06 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=53702 <jsp-property-group> may contain multiple <url-pattern>
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1373621 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/core/ApplicationJspPropertyGroupDescriptor.java
+++ java/org/apache/catalina/core/ApplicationJspPropertyGroupDescriptor.java
@@ -14,11 +14,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.catalina.core;
 
 import java.util.Collection;
-import java.util.HashSet;
 
 import javax.servlet.descriptor.JspPropertyGroupDescriptor;
 
@@ -164,13 +162,6 @@ public class ApplicationJspPropertyGroupDescriptor
     
     @Override
     public Collection<String> getUrlPatterns() {
-        Collection<String> result = new HashSet<String>();
-        
-        if (jspPropertyGroup.getUrlPattern() != null) {
-            result.add(jspPropertyGroup.getUrlPattern());
-        }
-        
-        return result;
+        return jspPropertyGroup.getUrlPatterns();
     }
-
 }
