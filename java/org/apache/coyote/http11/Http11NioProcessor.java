2000d351e9ed07bcedac1cf102fe8c0956db4394
Mark Emlyn David Thomas <markt@apache.org>
2012-08-15 22:56:05 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=53697 Fix a regression in the fix for BZ 51881 that mean that in some circumstances the isComet flag was not reset on the HttpAprProcessor. This caused problems when the Processor was re-used for a new connection.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1373667 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http11/Http11NioProcessor.java
+++ java/org/apache/coyote/http11/Http11NioProcessor.java
@@ -294,7 +294,6 @@ public class Http11NioProcessor extends AbstractHttp11Processor<NioChannel> {
     @Override
     public void recycleInternal() {
         socket = null;
-        comet = false;
         sendfileData = null;
     }
 
