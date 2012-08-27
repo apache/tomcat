0389ee4daf45bc3104298b39ba213b6f52bbf034
Mark Emlyn David Thomas <markt@apache.org>
2012-08-27 20:58:04 +0000

    Better handling of exception
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1377858 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/authenticator/DigestAuthenticator.java
+++ java/org/apache/catalina/authenticator/DigestAuthenticator.java
@@ -88,8 +88,7 @@ public class DigestAuthenticator extends AuthenticatorBase {
             if (md5Helper == null)
                 md5Helper = MessageDigest.getInstance("MD5");
         } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-            throw new IllegalStateException();
+            throw new IllegalStateException(e);
         }
     }
 
@@ -100,7 +99,7 @@ public class DigestAuthenticator extends AuthenticatorBase {
     /**
      * MD5 message digest provider.
      */
-    protected static volatile MessageDigest md5Helper;
+     protected static volatile MessageDigest md5Helper;
 
 
     /**
