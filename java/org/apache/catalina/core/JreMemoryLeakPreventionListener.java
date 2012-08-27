e9818c1b8bdc711778016466db11071b41349c51
Filip Hanik <fhanik@apache.org>
2012-08-27 14:20:55 +0000

    Per http://markmail.org/message/nqnogctvfuyzhtol
    1. Already encountered two users that would like to set this value. There is
    never any need to hard code any value, regardless of its use 
    2. This turns it into a property on the listener 
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1377689 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/core/JreMemoryLeakPreventionListener.java
+++ java/org/apache/catalina/core/JreMemoryLeakPreventionListener.java
@@ -218,6 +218,17 @@ public class JreMemoryLeakPreventionListener implements LifecycleListener {
         this.classesToInitialize = classesToInitialize;
     }
 
+    /**
+     * Sets the time that this listener will request for garbage-collection latency
+     * @see {@link sun.misc.GC#requestLatency(long)}
+     */
+    private long gcDaemonPeriod = Long.MAX_VALUE - 1;
+    public long getGcDaemonPeriod() {
+        return gcDaemonPeriod;
+    }
+    public void setGcDaemonPeriod(long gcDaemonPeriod) {
+        this.gcDaemonPeriod = gcDaemonPeriod;
+    }
 
     @Override
     public void lifecycleEvent(LifecycleEvent event) {
@@ -297,7 +308,7 @@ public class JreMemoryLeakPreventionListener implements LifecycleListener {
                         Method method = clazz.getDeclaredMethod(
                                 "requestLatency",
                                 new Class[] {long.class});
-                        method.invoke(null, Long.getLong("org.apache.catalina.core.jreMemoryLeakPreventionGCDaemonPeriod", Long.valueOf(Long.MAX_VALUE-1)));
+                        method.invoke(null, getGcDaemonPeriod());
                     } catch (ClassNotFoundException e) {
                         if (System.getProperty("java.vendor").startsWith(
                                 "Sun")) {
