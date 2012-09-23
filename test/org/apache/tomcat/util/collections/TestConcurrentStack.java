77c29d404e3cade51b1b47f9972fa24a92df1630
Mark Thomas <markt@apache.org>
2012-09-23 15:20:38 +0000

    ConcurrentLinkedQueue is currently the biggest contributor to garbage in the load test so this is intended as a replacement.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1389076 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test/org/apache/tomcat/util/collections/TestConcurrentStack.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.tomcat.util.collections;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestConcurrentStack {
+
+    @Test
+    public void testPopEmpty() {
+        ConcurrentStack<Object> stack = new ConcurrentStack<>();
+        Assert.assertNull(stack.pop());
+    }
+
+    @Test
+    public void testPushPopOrder() {
+        ConcurrentStack<Object> stack = new ConcurrentStack<>();
+
+        Object o1 = new Object();
+        Object o2 = new Object();
+        Object o3 = new Object();
+        Object o4 = new Object();
+
+        stack.push(o1);
+        stack.push(o2);
+        stack.push(o3);
+        stack.push(o4);
+
+        Assert.assertSame(stack.pop(), o4);
+        Assert.assertSame(stack.pop(), o3);
+        Assert.assertSame(stack.pop(), o2);
+        Assert.assertSame(stack.pop(), o1);
+
+        Assert.assertNull(stack.pop());
+    }
+
+    @Test
+    public void testExpandPushPopOrder() {
+        ConcurrentStack<Object> stack = new ConcurrentStack<>();
+
+        Object o1 = new Object();
+        Object o2 = new Object();
+        Object o3 = new Object();
+        Object o4 = new Object();
+
+        for (int i = 0; i < 300; i++) {
+            stack.push(o1);
+            stack.push(o2);
+            stack.push(o3);
+            stack.push(o4);
+        }
+
+        for (int i = 0; i < 300; i++) {
+            Assert.assertSame(stack.pop(), o4);
+            Assert.assertSame(stack.pop(), o3);
+            Assert.assertSame(stack.pop(), o2);
+            Assert.assertSame(stack.pop(), o1);
+        }
+
+        Assert.assertNull(stack.pop());
+    }
+
+}
