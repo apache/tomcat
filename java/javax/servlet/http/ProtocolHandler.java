0d0e3f0a88bb2cc841b96efec8446f76f53ee6f4
Mark Thomas <markt@apache.org>
2012-10-12 09:19:25 +0000

    Add @since tags.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1397487 13f79535-47bb-0310-9956-ffa450edef68

--- java/javax/servlet/http/ProtocolHandler.java
+++ java/javax/servlet/http/ProtocolHandler.java
@@ -18,16 +18,20 @@ package javax.servlet.http;
 
 /**
  * Interface between the HTTP upgrade process and the new protocol.
+ *
+ * @since Servlet 3.1
  */
 public interface ProtocolHandler {
 
     /**
      * This method is called once the request/response pair where
-     * {@link HttpServletRequest#upgrade(ProtocolHandler) is called has
+     * {@link HttpServletRequest#upgrade(ProtocolHandler)} is called has
      * completed processing and is the point where control of the connection
      * passes from the container to the {@link ProtocolHandler}.
      *
      * @param connection    The connection that has been upgraded
+     *
+     * @since Servlet 3.1
      */
     void init(WebConnection connection);
 }
