e3f60745f41a3652564b05fda3f7ae6fdff18925
Mark Emlyn David Thomas <markt@apache.org>
2012-10-13 14:30:39 +0000

    Fix my errors. Reviewed by kkolinko
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1397839 13f79535-47bb-0310-9956-ffa450edef68

--- java/javax/servlet/http/HttpServlet.java
+++ java/javax/servlet/http/HttpServlet.java
@@ -848,12 +848,12 @@ class NoBodyOutputStream extends ServletOutputStream {
                     lStrings.getString("err.io.nullArray"));
         }
 
-        if (offset < 0 || len < 0 || offset+len < buf.length) {
+        if (offset < 0 || len < 0 || offset+len > buf.length) {
             String msg = lStrings.getString("err.io.indexOutOfBounds");
             Object[] msgArgs = new Object[3];
             msgArgs[0] = Integer.valueOf(offset);
             msgArgs[1] = Integer.valueOf(len);
-            msgArgs[2] = Integer.valueOf(offset + len);
+            msgArgs[2] = Integer.valueOf(buf.length);
             msg = MessageFormat.format(msg, msgArgs);
             throw new IndexOutOfBoundsException(msg);
         }
