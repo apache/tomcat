e502b1fe4d7c5b757e8a2ad00bbe458fa4e64846
Mark Emlyn David Thomas <markt@apache.org>
2012-10-13 20:46:26 +0000

    Fill in some Servlet 3.0 Javadoc
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1397945 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/connector/Response.java
+++ java/org/apache/catalina/connector/Response.java
@@ -896,23 +896,12 @@ public class Response
     // --------------------------------------------------- HttpResponse Methods
 
 
-    /**
-     * Return the value for the specified header, or <code>null</code> if this
-     * header has not been set.  If more than one value was added for this
-     * name, only the first is returned; use {@link #getHeaders(String)} to
-     * retrieve all of them.
-     *
-     * @param name Header name to look up
-     */
     @Override
     public String getHeader(String name) {
         return coyoteResponse.getMimeHeaders().getHeader(name);
     }
 
 
-    /**
-     * Return an Iterable of all the header names set for this response.
-     */
     @Override
     public Collection<String> getHeaderNames() {
 
@@ -927,12 +916,6 @@ public class Response
     }
 
 
-    /**
-     * Return a Collection of all the header values associated with the
-     * specified header name.
-     *
-     * @param name Header name to look up
-     */
     @Override
     public Collection<String> getHeaders(String name) {
 
@@ -955,9 +938,6 @@ public class Response
     }
 
 
-    /**
-     * Return the HTTP status code associated with this Response.
-     */
     @Override
     public int getStatus() {
         return coyoteResponse.getStatus();
