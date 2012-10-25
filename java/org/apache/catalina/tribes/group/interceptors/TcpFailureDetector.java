3542147ac50a834138e603c62ec080732173ab29
Keiichi Fujino <kfujino@apache.org>
2012-10-25 11:54:21 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=54045. Make sure getMembers() returns available member when TcpFailureDetector works in static cluster.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1402110 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/tribes/group/interceptors/TcpFailureDetector.java
+++ java/org/apache/catalina/tribes/group/interceptors/TcpFailureDetector.java
@@ -36,6 +36,7 @@ import org.apache.catalina.tribes.io.ChannelData;
 import org.apache.catalina.tribes.io.XByteBuffer;
 import org.apache.catalina.tribes.membership.MemberImpl;
 import org.apache.catalina.tribes.membership.Membership;
+import org.apache.catalina.tribes.membership.StaticMember;
 
 /**
  * <p>Title: A perfect failure detector </p>
@@ -157,6 +158,9 @@ public class TcpFailureDetector extends ChannelInterceptorBase {
                 //not correct, we need to maintain the map
                 membership.removeMember( (MemberImpl) member);
                 removeSuspects.remove(member);
+                if (member instanceof StaticMember) {
+                    addSuspects.put(member, Long.valueOf(System.currentTimeMillis()));
+                }
                 notify = true;
             } else {
                 //add the member as suspect
@@ -228,6 +232,9 @@ public class TcpFailureDetector extends ChannelInterceptorBase {
                 if (membership.getMember(members[i])!=null) {
                     membership.removeMember((MemberImpl)members[i]);
                     removeSuspects.remove(members[i]);
+                    if (members[i] instanceof StaticMember) {
+                        addSuspects.put(members[i], Long.valueOf(System.currentTimeMillis()));
+                    }
                     super.memberDisappeared(members[i]);
                 }
             } //end if
@@ -239,6 +246,10 @@ public class TcpFailureDetector extends ChannelInterceptorBase {
         //update all alive times
         Member[] members = super.getMembers();
         for (int i = 0; members != null && i < members.length; i++) {
+            if (addSuspects.containsKey(members[i]) && membership.getMember(members[i]) == null) {
+                // avoid temporary adding member.
+                continue;
+            }
             if (membership.memberAlive( (MemberImpl) members[i])) {
                 //we don't have this one in our membership, check to see if he/she is alive
                 if (memberAlive(members[i])) {
