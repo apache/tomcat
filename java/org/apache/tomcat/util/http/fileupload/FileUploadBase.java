da7d55be167eb5ae5986d68f6cd794e31d69a514
Mark Emlyn David Thomas <markt@apache.org>
2012-10-26 21:41:37 +0000

    Don't need 1.3 compatibility. Fix an Eclipse warning.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1402676 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/http/fileupload/FileUploadBase.java
+++ java/org/apache/tomcat/util/http/fileupload/FileUploadBase.java
@@ -958,32 +958,23 @@ public abstract class FileUploadBase {
      * {@link FileUploadException} in an {@link IOException}.
      */
     public static class FileUploadIOException extends IOException {
-        /** The exceptions UID, for serializing an instance.
-         */
-        private static final long serialVersionUID = -7047616958165584154L;
-        /** The exceptions cause; we overwrite the parent
-         * classes field, which is available since Java
-         * 1.4 only.
-         */
-        private final FileUploadException cause;
 
-        /**
-         * Creates a <code>FileUploadIOException</code> with the
-         * given cause.
-         * @param pCause The exceptions cause, if any, or null.
-         */
-        public FileUploadIOException(FileUploadException pCause) {
-            // We're not doing super(pCause) cause of 1.3 compatibility.
-            cause = pCause;
+        private static final long serialVersionUID = -3082868232248803474L;
+
+        public FileUploadIOException() {
+            super();
         }
 
-        /**
-         * Returns the exceptions cause.
-         * @return The exceptions cause, if any, or null.
-         */
-        @Override
-        public Throwable getCause() {
-            return cause;
+        public FileUploadIOException(String message, Throwable cause) {
+            super(message, cause);
+        }
+
+        public FileUploadIOException(String message) {
+            super(message);
+        }
+
+        public FileUploadIOException(Throwable cause) {
+            super(cause);
         }
     }
 
