2eb82f298501167ee6d9dba0a0e44801f0c0196b
Mark Thomas <markt@apache.org>
2012-11-06 22:45:26 +0000

    Add Eclipse generate hashCode() to fix a warning
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1406385 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/jasper/compiler/Mark.java
+++ java/org/apache/jasper/compiler/Mark.java
@@ -247,6 +247,20 @@ final class Mark {
         return false;
     }
 
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + col;
+        result = prime * result + cursor;
+        result = prime * result + fileId;
+        result = prime * result + line;
+        result = prime * result + ((reader == null) ? 0 : reader.hashCode());
+        return result;
+    }
+
+
     /**
      * Keep track of parser before parsing an included file.
      * This class keeps track of the parser before we switch to parsing an
