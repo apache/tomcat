753f48c31c8978e15ff06b098e5e425630e12bf5
Konstantin Kolinko <kkolinko@apache.org>
2012-11-12 20:17:58 +0000

    For https://issues.apache.org/bugzilla/show_bug.cgi?id=54044 Add test for org.apache.juli.DateFormatCache. It is port of the test of AccessLogValve.DateFormatCache. I am using reflection to get interesting fields, so no need to change field visibilities.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1408438 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/juli/TestDateFormatCache.java
+++ test/org/apache/juli/TestDateFormatCache.java
@@ -14,8 +14,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.catalina.valves;
+package org.apache.juli;
 
+import java.lang.reflect.Field;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -24,22 +25,31 @@ import java.util.TimeZone;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class TestAccessLogValve {
+public class TestDateFormatCache {
 
     // Note that there is a similar test:
-    // org.apache.juli.TestDateFormatCache.testBug54044()
+    // org.apache.catalina.valves.TestAccessLogValve.testBug54044()
     @Test
     public void testBug54044() throws Exception {
 
+        final String timeFormat = "dd-MMM-yyyy HH:mm:ss";
         final int cacheSize = 10;
 
-        SimpleDateFormat sdf =
-                new SimpleDateFormat("dd/MMM/yyyy:HH:mm:ss", Locale.US);
+        SimpleDateFormat sdf = new SimpleDateFormat(timeFormat, Locale.US);
         sdf.setTimeZone(TimeZone.getDefault());
 
-        AccessLogValve.DateFormatCache dfc =
-                new AccessLogValve.DateFormatCache(
-                        cacheSize, Locale.US, null);
+        DateFormatCache dfc = new DateFormatCache(cacheSize, timeFormat, null);
+
+        // Get dfc.cache.cache field
+        Object dfcCache;
+        Field dfcCacheArray;
+        {
+            Field dfcCacheField = dfc.getClass().getDeclaredField("cache");
+            dfcCacheField.setAccessible(true);
+            dfcCache = dfcCacheField.get(dfc);
+            dfcCacheArray = dfcCache.getClass().getDeclaredField("cache");
+            dfcCacheArray.setAccessible(true);
+        }
 
         // Create an array to hold the expected values
         String[] expected = new String[cacheSize];
@@ -49,35 +59,40 @@ public class TestAccessLogValve {
             dfc.getFormat(secs * 1000);
             expected[secs] = generateExpected(sdf, secs);
         }
-        Assert.assertArrayEquals(expected, dfc.cLFCache.cache);
-
+        Assert.assertArrayEquals(expected,
+                (String[]) dfcCacheArray.get(dfcCache));
 
         // Cause the cache to roll-around by one and then confirm
         dfc.getFormat(cacheSize * 1000);
         expected[0] = generateExpected(sdf, cacheSize);
-        Assert.assertArrayEquals(expected, dfc.cLFCache.cache);
+        Assert.assertArrayEquals(expected,
+                (String[]) dfcCacheArray.get(dfcCache));
 
         // Jump 2 ahead and then confirm (skipped value should be null)
-        dfc.getFormat((cacheSize + 2)* 1000);
+        dfc.getFormat((cacheSize + 2) * 1000);
         expected[1] = null;
         expected[2] = generateExpected(sdf, cacheSize + 2);
-        Assert.assertArrayEquals(expected, dfc.cLFCache.cache);
+        Assert.assertArrayEquals(expected,
+                (String[]) dfcCacheArray.get(dfcCache));
 
         // Back 1 to fill in the gap
-        dfc.getFormat((cacheSize + 1)* 1000);
+        dfc.getFormat((cacheSize + 1) * 1000);
         expected[1] = generateExpected(sdf, cacheSize + 1);
-        Assert.assertArrayEquals(expected, dfc.cLFCache.cache);
+        Assert.assertArrayEquals(expected,
+                (String[]) dfcCacheArray.get(dfcCache));
 
         // Return to 1 and confirm skipped value is null
         dfc.getFormat(1 * 1000);
         expected[1] = generateExpected(sdf, 1);
         expected[2] = null;
-        Assert.assertArrayEquals(expected, dfc.cLFCache.cache);
+        Assert.assertArrayEquals(expected,
+                (String[]) dfcCacheArray.get(dfcCache));
 
         // Go back one further
         dfc.getFormat(0);
         expected[0] = generateExpected(sdf, 0);
-        Assert.assertArrayEquals(expected, dfc.cLFCache.cache);
+        Assert.assertArrayEquals(expected,
+                (String[]) dfcCacheArray.get(dfcCache));
 
         // Jump ahead far enough that the entire cache will need to be cleared
         dfc.getFormat(42 * 1000);
@@ -85,10 +100,12 @@ public class TestAccessLogValve {
             expected[i] = null;
         }
         expected[0] = generateExpected(sdf, 42);
-        Assert.assertArrayEquals(expected, dfc.cLFCache.cache);
+        Assert.assertArrayEquals(expected,
+                (String[]) dfcCacheArray.get(dfcCache));
     }
 
     private String generateExpected(SimpleDateFormat sdf, long secs) {
-        return "[" + sdf.format(new Date(secs * 1000)) + " +0000]";
+        return sdf.format(new Date(secs * 1000));
     }
+
 }
