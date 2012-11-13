665f2436e45a082e5a1d6eec3cc91315dc99d78f
Konstantin Kolinko <kkolinko@apache.org>
2012-11-13 20:49:01 +0000

    Reduce visibility
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1408950 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/util/URLEncoder.java
+++ java/org/apache/catalina/util/URLEncoder.java
@@ -34,7 +34,7 @@ import java.util.BitSet;
  * @author Remy Maucherat
  */
 public class URLEncoder {
-    protected static final char[] hexadecimal =
+    private static final char[] hexadecimal =
     {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
      'A', 'B', 'C', 'D', 'E', 'F'};
 
