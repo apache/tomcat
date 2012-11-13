767a6e5ba84b2ab9e3a404a92622b9bad9a365f2
Konstantin Kolinko <kkolinko@apache.org>
2012-11-13 20:38:11 +0000

    Review of r1389005 - GC optimization of statusCode display in AccessLogValve Simplify code and do not expose public char[] arrays.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1408946 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/valves/Constants.java
+++ java/org/apache/catalina/valves/Constants.java
@@ -36,44 +36,4 @@ public final class Constants {
         public static final String COMBINED_PATTERN = "%h %l %u %t \"%r\" %s %b \"%{Referer}i\" \"%{User-Agent}i\"";
     }
 
-    public static final char[] SC_CONTINUE_CHAR = new char[] {'1', '0', '0'};
-    public static final char[] SC_SWITCHING_PROTOCOLS_CHAR = new char[] {'1', '0', '1'};
-    public static final char[] SC_OK_CHAR = new char[] {'2', '0', '0'};
-    public static final char[] SC_CREATED_CHAR = new char[] {'2', '0', '1'};
-    public static final char[] SC_ACCEPTED_CHAR = new char[] {'2', '0', '2'};
-    public static final char[] SC_NON_AUTHORITATIVE_INFORMATION_CHAR = new char[] {'2', '0', '3'};
-    public static final char[] SC_NO_CONTENT_CHAR = new char[] {'2', '0', '4'};
-    public static final char[] SC_RESET_CONTENT_CHAR = new char[] {'2', '0', '5'};
-    public static final char[] SC_PARTIAL_CONTENT_CHAR = new char[] {'2', '0', '6'};
-    public static final char[] SC_MULTIPLE_CHOICES_CHAR = new char[] {'3', '0', '0'};
-    public static final char[] SC_MOVED_PERMANENTLY_CHAR = new char[] {'3', '0', '1'};
-    public static final char[] SC_MOVED_TEMPORARILY_CHAR = new char[] {'3', '0', '2'};
-    public static final char[] SC_SEE_OTHER_CHAR = new char[] {'3', '0', '3'};
-    public static final char[] SC_NOT_MODIFIED_CHAR = new char[] {'3', '0', '4'};
-    public static final char[] SC_USE_PROXY_CHAR = new char[] {'3', '0', '5'};
-    public static final char[] SC_TEMPORARY_REDIRECT_CHAR = new char[] {'3', '0', '7'};
-    public static final char[] SC_BAD_REQUEST_CHAR = new char[] {'4', '0', '0'};
-    public static final char[] SC_UNAUTHORIZED_CHAR = new char[] {'4', '0', '1'};
-    public static final char[] SC_PAYMENT_REQUIRED_CHAR = new char[] {'4', '0', '2'};
-    public static final char[] SC_FORBIDDEN_CHAR = new char[] {'4', '0', '3'};
-    public static final char[] SC_NOT_FOUND_CHAR = new char[] {'4', '0', '4'};
-    public static final char[] SC_METHOD_NOT_ALLOWED_CHAR = new char[] {'4', '0', '5'};
-    public static final char[] SC_NOT_ACCEPTABLE_CHAR = new char[] {'4', '0', '6'};
-    public static final char[] SC_PROXY_AUTHENTICATION_REQUIRED_CHAR = new char[] {'4', '0', '7'};
-    public static final char[] SC_REQUEST_TIMEOUT_CHAR = new char[] {'4', '0', '8'};
-    public static final char[] SC_CONFLICT_CHAR = new char[] {'4', '0', '9'};
-    public static final char[] SC_GONE_CHAR = new char[] {'4', '1', '0'};
-    public static final char[] SC_LENGTH_REQUIRED_CHAR = new char[] {'4', '1', '1'};
-    public static final char[] SC_PRECONDITION_FAILED_CHAR = new char[] {'4', '1', '2'};
-    public static final char[] SC_REQUEST_ENTITY_TOO_LARGE_CHAR = new char[] {'4', '1', '3'};
-    public static final char[] SC_REQUEST_URI_TOO_LONG_CHAR = new char[] {'4', '1', '4'};
-    public static final char[] SC_UNSUPPORTED_MEDIA_TYPE_CHAR = new char[] {'4', '1', '5'};
-    public static final char[] SC_REQUESTED_RANGE_NOT_SATISFIABLE_CHAR = new char[] {'4', '1', '6'};
-    public static final char[] SC_EXPECTATION_FAILED_CHAR = new char[] {'4', '1', '7'};
-    public static final char[] SC_INTERNAL_SERVER_ERROR_CHAR = new char[] {'5', '0', '0'};
-    public static final char[] SC_NOT_IMPLEMENTED_CHAR = new char[] {'5', '0', '1'};
-    public static final char[] SC_BAD_GATEWAY_CHAR = new char[] {'5', '0', '2'};
-    public static final char[] SC_SERVICE_UNAVAILABLE_CHAR = new char[] {'5', '0', '3'};
-    public static final char[] SC_GATEWAY_TIMEOUT_CHAR = new char[] {'5', '0', '4'};
-    public static final char[] SC_HTTP_VERSION_NOT_SUPPORTED_CHAR = new char[] {'5', '0', '5'};
 }
