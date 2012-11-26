80d616745acbd7bcd4267a13748f3e9aebf1beb2
Mark Thomas <markt@apache.org>
2012-11-26 11:44:20 +0000

    Follow up to r1413556
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1413562 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/naming/java/javaURLContextFactory.java
+++ java/org/apache/naming/java/javaURLContextFactory.java
@@ -40,7 +40,7 @@ import org.apache.naming.SelectorContext;
  * java.naming.factory.url.pkgs=org.apache.naming property
  * to the JNDI properties file</li>
  * <li>Setting an environment variable named Context.URL_PKG_PREFIXES with
- * its value including the org.apache.catalina.util.naming package name.
+ * its value including the org.apache.naming package name.
  * More detail about this can be found in the JNDI documentation :
  * {@link javax.naming.spi.NamingManager#getURLContext(java.lang.String, java.util.Hashtable)}.</li>
  * </ul>
