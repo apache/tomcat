0e167fad83e6398d635dcddea04c23729e5f82d2
Mark Emlyn David Thomas <markt@apache.org>
2012-11-27 10:06:42 +0000

    Populate the SCI Javadoc
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1414053 13f79535-47bb-0310-9956-ffa450edef68

--- java/javax/servlet/ServletContainerInitializer.java
+++ java/javax/servlet/ServletContainerInitializer.java
@@ -19,18 +19,35 @@ package javax.servlet;
 import java.util.Set;
 
 /**
+ * ServletContainerInitializers (SCIs) are registered via an entry in the
+ * file META-INF/services/javax.servlet.ServletContainerInitializer that must be
+ * included in the JAR file that contains the SCI implementation.
+ * <p>
+ * SCIs processing is performed regardless of the setting of metadata-complete.
+ * SCI processing can be controlled be JAR file via fragment ordering. If an
+ * absolute ordering is defined, the only those fragments included in the
+ * ordering will be scanned. To disable SCI processing completely, an empty
+ * absolute ordering may be defined.
+ * <p>
+ * SCIs register an interest in annotations (class, method or field) or types
+ * via the {@link javax.servlet.annotation.HandlesTypes} annotation which is
+ * added to the {@link #onStartup(Set, ServletContext)} method.
+ *
  * @since Servlet 3.0
- * $Id$
- * TODO SERVLET3 - Add comments
  */
 public interface ServletContainerInitializer {
 
     /**
+     * Receives notification during startup of a web application of the classes
+     * within the web application that matched the criteria defined via the
+     * {@link javax.servlet.annotation.HandlesTypes} annotation.
      *
-     * @param c
-     * @param ctx
-     * @throws ServletException
+     * @param c     The (possibly null) set of classes that met the specified
+     *              criteria
+     * @param ctx   The ServletContext of the web application in which the
+     *              classes were discovered
+     *
+     * @throws ServletException If an error occurs
      */
-    public void onStartup(Set<Class<?>> c, ServletContext ctx)
-    throws ServletException;
+    void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException;
 }
