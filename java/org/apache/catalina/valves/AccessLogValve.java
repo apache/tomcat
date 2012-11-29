42a3bd8cbeb38c04413ca1b59b916170776afb89
Rainer Jung <rjung@apache.org>
2012-11-29 14:44:53 +0000

    Fix javadoc.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1415186 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/valves/AccessLogValve.java
+++ java/org/apache/catalina/valves/AccessLogValve.java
@@ -1018,7 +1018,12 @@ public class AccessLogValve extends ValveBase implements AccessLog {
 
 
     /**
-     * Close the currently open log file (if any)
+     * Create a File object based on the current log file name.
+     * Directories are created as needed but the underlying file
+     * is not created or opened.
+     *
+     * @param useDateStamp include the timestamp in the file name.
+     * @return the log file object
      */
     private File getLogFile(boolean useDateStamp) {
 
