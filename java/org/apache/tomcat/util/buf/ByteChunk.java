ef8c65022a82d1d2927df80f0c19adc9927144cb
Mark Thomas <markt@apache.org>
2012-12-04 21:21:11 +0000

    Replicate the equals() and hashCode() strategy from MessageBytes to silence some FindBugs warnings
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1417194 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/buf/ByteChunk.java
+++ java/org/apache/tomcat/util/buf/ByteChunk.java
@@ -101,6 +101,10 @@ public final class ByteChunk implements Cloneable, Serializable {
     */
     public static final Charset DEFAULT_CHARSET = B2CConverter.ISO_8859_1;
 
+    private int hashCode=0;
+    // did we compute the hashcode ?
+    private boolean hasHashCode = false;
+
     // byte[]
     private byte[] buff;
 
@@ -141,6 +145,7 @@ public final class ByteChunk implements Cloneable, Serializable {
         start=0;
         end=0;
         isSet=false;
+        hasHashCode = false;
     }
 
     public void reset() {
@@ -157,6 +162,7 @@ public final class ByteChunk implements Cloneable, Serializable {
         start=0;
         end=0;
         isSet=true;
+        hasHashCode = false;
     }
 
     /**
@@ -171,6 +177,7 @@ public final class ByteChunk implements Cloneable, Serializable {
         start = off;
         end = start+ len;
         isSet=true;
+        hasHashCode = false;
     }
 
     public void setCharset(Charset charset) {
@@ -488,6 +495,14 @@ public final class ByteChunk implements Cloneable, Serializable {
 
     // -------------------- equals --------------------
 
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof ByteChunk) {
+            return equals((ByteChunk) obj);
+        }
+        return false;
+    }
+
     /**
      * Compares the message bytes to the specified String object.
      * @param s the String to compare
@@ -625,6 +640,19 @@ public final class ByteChunk implements Cloneable, Serializable {
     }
 
     // -------------------- Hash code  --------------------
+
+    @Override
+    public int hashCode() {
+        if (hasHashCode) {
+            return hashCode;
+        }
+        int code = 0;
+
+        code = hash();
+        hashCode = code;
+        hasHashCode = true;
+        return code;
+    }
 
     // normal hash.
     public int hash() {
