7785c5eb04cc89de1372e117567774f208b3773f
Mark Emlyn David Thomas <markt@apache.org>
2012-12-05 19:41:05 +0000

    Fix package
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1417605 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/websocket/Utf8Decoder.java
+++ java/org/apache/tomcat/websocket/Utf8Decoder.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.catalina.websocket;
+package org.apache.tomcat.websocket;
 
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
