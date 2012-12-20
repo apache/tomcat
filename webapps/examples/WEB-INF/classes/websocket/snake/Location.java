9f87d2975451431f7937b8608257b53f13b55bb4
Mark Emlyn David Thomas <markt@apache.org>
2012-12-20 22:17:50 +0000

    Switch the snake WebSocket example to the new API.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1424733 13f79535-47bb-0310-9956-ffa450edef68

--- webapps/examples/WEB-INF/classes/websocket/snake/Location.java
+++ webapps/examples/WEB-INF/classes/websocket/snake/Location.java
@@ -29,13 +29,13 @@ public class Location {
     public Location getAdjacentLocation(Direction direction) {
         switch (direction) {
             case NORTH:
-                return new Location(x, y - SnakeWebSocketServlet.GRID_SIZE);
+                return new Location(x, y - SnakeAnnotation.GRID_SIZE);
             case SOUTH:
-                return new Location(x, y + SnakeWebSocketServlet.GRID_SIZE);
+                return new Location(x, y + SnakeAnnotation.GRID_SIZE);
             case EAST:
-                return new Location(x + SnakeWebSocketServlet.GRID_SIZE, y);
+                return new Location(x + SnakeAnnotation.GRID_SIZE, y);
             case WEST:
-                return new Location(x - SnakeWebSocketServlet.GRID_SIZE, y);
+                return new Location(x - SnakeAnnotation.GRID_SIZE, y);
             case NONE:
                 // fall through
             default:
