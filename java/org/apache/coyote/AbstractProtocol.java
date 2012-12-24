39a30dc66b8fcfca0ba4788153f176de9d6a6a0a
Mark Emlyn David Thomas <markt@apache.org>
2012-12-24 00:27:14 +0000

    Add a debug message I found useful when trying to track down a WebSocket issue
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1425564 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/AbstractProtocol.java
+++ java/org/apache/coyote/AbstractProtocol.java
@@ -633,6 +633,11 @@ public abstract class AbstractProtocol implements ProtocolHandler,
                         processor = createUpgradeProcessor(
                                 socket, httpUpgradeHandler);
                     }
+                    if (getLog().isDebugEnabled()) {
+                        getLog().debug("Socket: [" + socket +
+                                "], Status in: [" + status +
+                                "], State out: [" + state + "]");
+                    }
                 } while (state == SocketState.ASYNC_END ||
                         state == SocketState.UPGRADING);
 
