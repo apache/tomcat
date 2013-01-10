f31003e653d8fd94b85e8b5947604c235266c654
Konstantin Kolinko <kkolinko@apache.org>
2013-01-10 01:24:46 +0000

    Fix release of processors in AjpNioProtocol. Wrong object was used as a key in the connections map.
    "connections" is "Map<S,Processor<S>>", so the key in the map is socket, not its wrapper.
    Found this in a review inspired by a similar fix in r1426662.
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1431171 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/ajp/AjpNioProtocol.java
+++ java/org/apache/coyote/ajp/AjpNioProtocol.java
@@ -146,7 +146,8 @@ public class AjpNioProtocol extends AbstractAjpProtocol {
          */
         @Override
         public void release(SocketWrapper<NioChannel> socket) {
-            Processor<NioChannel> processor = connections.remove(socket);
+            Processor<NioChannel> processor =
+                    connections.remove(socket.getSocket());
             if (processor != null) {
                 processor.recycle(true);
                 recycledProcessors.push(processor);
