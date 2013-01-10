a48f0fb0c26cb55992b685079cabca2099ce189c
Mark Emlyn David Thomas <markt@apache.org>
2013-01-10 12:36:17 +0000

    Follow-up to r1431300. Fix error on reload when postConstruct and preDestroy entries are present in weeb.xml
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1431321 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/core/StandardContext.java
+++ java/org/apache/catalina/core/StandardContext.java
@@ -5622,6 +5622,9 @@ public class StandardContext extends ContainerBase
         
         createdServlets.clear();
 
+        postConstructMethods.clear();
+        preDestroyMethods.clear();
+
         if(log.isDebugEnabled())
             log.debug("resetContext " + getObjectName());
     }
