ff2a783bcad849b704380c7db63db5469d8606fc
Mark Thomas <markt@apache.org>
2013-02-10 20:45:47 +0000

    Fix copy and paste error highlighted by Eclipse NPE warnings
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1444611 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/storeconfig/WebResourceRootSF.java
+++ java/org/apache/catalina/storeconfig/WebResourceRootSF.java
@@ -65,7 +65,7 @@ public class WebResourceRootSF extends StoreFactoryBase {
                             + ".[JarResources]");
             if (jarResourcesElementDesc != null) {
                 for (WebResourceSet jarResources : jarResourcesArray) {
-                    preResourcesElementDesc.getStoreFactory().store(aWriter, indent,
+                    jarResourcesElementDesc.getStoreFactory().store(aWriter, indent,
                             jarResources);
                 }
             }
@@ -77,7 +77,7 @@ public class WebResourceRootSF extends StoreFactoryBase {
                             + ".[PostResources]");
             if (postResourcesElementDesc != null) {
                 for (WebResourceSet postResources : postResourcesArray) {
-                    preResourcesElementDesc.getStoreFactory().store(aWriter, indent,
+                    postResourcesElementDesc.getStoreFactory().store(aWriter, indent,
                             postResources);
                 }
             }
