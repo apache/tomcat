04b4424400072d2cac04071300cc4e7fd242dfe3
Mark Thomas <markt@apache.org>
2013-02-15 17:25:48 +0000

    Rename. All endpoints, not just Pojos will need this.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1446699 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/websocket/pojo/UriTemplate.java
+++ java/org/apache/tomcat/websocket/pojo/UriTemplate.java
@@ -29,7 +29,7 @@ import org.apache.tomcat.util.res.StringManager;
  * Extracts path parameters from URIs used to create web socket connections
  * using the URI template defined for the associated Endpoint.
  */
-public class PojoUriTemplate {
+public class UriTemplate {
 
     private static StringManager sm = StringManager.getManager(Constants.PACKAGE_NAME);
     private final String template;
@@ -37,7 +37,7 @@ public class PojoUriTemplate {
     private final List<String> names = new ArrayList<>();
 
 
-    public PojoUriTemplate(String template) {
+    public UriTemplate(String template) {
         this.template = template;
         // +10 is just a guess at this point
         StringBuilder pattern = new StringBuilder(template.length() + 10);
