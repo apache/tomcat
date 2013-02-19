cc5983a2214aba5940760acb253ea429227aa36d
Keiichi Fujino <kfujino@apache.org>
2013-02-19 05:52:16 +0000

    Improvements of InterceptorSF. - Add support for static membership. - Interceptor except StaticMembershipInterceptor has no child elements.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1447596 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/storeconfig/InterceptorSF.java
+++ java/org/apache/catalina/storeconfig/InterceptorSF.java
@@ -20,11 +20,43 @@ package org.apache.catalina.storeconfig;
 import java.io.PrintWriter;
 
 import org.apache.catalina.tribes.ChannelInterceptor;
+import org.apache.catalina.tribes.group.interceptors.StaticMembershipInterceptor;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
 
 /**
  * Generate Interceptor Element
  */
 public class InterceptorSF extends StoreFactoryBase {
+
+    private static Log log = LogFactory.getLog(InterceptorSF.class);
+
+    @Override
+    public void store(PrintWriter aWriter, int indent, Object aElement)
+            throws Exception {
+        if (aElement instanceof StaticMembershipInterceptor) {
+            StoreDescription elementDesc = getRegistry().findDescription(
+                    aElement.getClass());
+
+            if (elementDesc != null) {
+                if (log.isDebugEnabled())
+                    log.debug(sm.getString("factory.storeTag",
+                            elementDesc.getTag(), aElement));
+                getStoreAppender().printIndent(aWriter, indent + 2);
+                getStoreAppender().printOpenTag(aWriter, indent + 2, aElement,
+                            elementDesc);
+                storeChildren(aWriter, indent + 2, aElement, elementDesc);
+                getStoreAppender().printIndent(aWriter, indent + 2);
+                getStoreAppender().printCloseTag(aWriter, elementDesc);
+            } else {
+                if (log.isWarnEnabled())
+                    log.warn(sm.getString("factory.storeNoDescriptor",
+                            aElement.getClass()));
+            }
+        } else {
+            super.store(aWriter, indent, aElement);
+        }
+    }
 
     /**
      * Store the specified Interceptor child.
@@ -42,7 +74,7 @@ public class InterceptorSF extends StoreFactoryBase {
     @Override
     public void storeChildren(PrintWriter aWriter, int indent, Object aInterceptor,
             StoreDescription parentDesc) throws Exception {
-        if (aInterceptor instanceof ChannelInterceptor) {
+        if (aInterceptor instanceof StaticMembershipInterceptor) {
             ChannelInterceptor interceptor = (ChannelInterceptor) aInterceptor;
             // Store nested <Member> elements
             storeElementArray(aWriter, indent + 2, interceptor.getMembers());
