ab2b7d42643f9646f64b31e5cdc2e8e0d1ae83ea
Konstantin Kolinko <kkolinko@apache.org>
2013-03-06 00:40:04 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=54638 The status page of manager webapp was reporting wrong value for "Used" memory in XML mode.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1453105 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/manager/StatusTransformer.java
+++ java/org/apache/catalina/manager/StatusTransformer.java
@@ -264,7 +264,7 @@ public class StatusTransformer {
                 writer.write(" usageInit='" + usage.getInit() + "'");
                 writer.write(" usageCommitted='" + usage.getCommitted() + "'");
                 writer.write(" usageMax='" + usage.getMax() + "'");
-                writer.write(" usageUsed='" + usage.getInit() + "'/>");
+                writer.write(" usageUsed='" + usage.getUsed() + "'/>");
             }
 
             writer.write("</jvm>");
