9047138c601db8e01d63f5ffb28b13af666f9f20
Mark Emlyn David Thomas <markt@apache.org>
2013-03-07 13:03:58 +0000

    Javadoc updates
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1453842 13f79535-47bb-0310-9956-ffa450edef68

--- java/javax/websocket/server/PathParam.java
+++ java/javax/websocket/server/PathParam.java
@@ -21,6 +21,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Used to annotate method parameters on POJO endpoints the the {@link
+ * ServerEndpoint} has been defined with a {@link ServerEndpoint#value()} that
+ * uses a URI template.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
 public @interface PathParam {
--- java/javax/websocket/server/ServerApplicationConfig.java
+++ java/javax/websocket/server/ServerApplicationConfig.java
@@ -20,10 +20,32 @@ import java.util.Set;
 
 import javax.websocket.Endpoint;
 
+/**
+ * Applications may provide an implementation of this interface to filter the
+ * discovered WebSocket endpoints that are deployed. Implementations of this
+ * class will be discovered via an ServletContainerInitializer scan.
+ */
 public interface ServerApplicationConfig {
 
+    /**
+     * Enables applications to filter the discovered implementations of
+     * {@link ServerEndpointConfig}.
+     *
+     * @param scanned   The {@link Endpoint} implementations found in the
+     *                  application
+     * @return  The set of configurations for the endpoint the application
+     *              wishes to deploy
+     */
     Set<ServerEndpointConfig> getEndpointConfigs(
             Set<Class<? extends Endpoint>> scanned);
 
+    /**
+     * Enables applications to filter the discovered classes annotated with
+     * {@link ServerEndpoint}.
+     *
+     * @param scanned   The POJOs annotated with {@link ServerEndpoint} found in
+     *                  the application
+     * @return  The set of POJOs the application wishes to deploy
+     */
     Set<Class<?>> getAnnotatedEndpointClasses(Set<Class<?>> scanned);
 }
