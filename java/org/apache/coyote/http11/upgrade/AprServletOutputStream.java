96576bb3f8eeb4c2dbf75036f05250c6f0822c6e
Mark Thomas <markt@apache.org>
2013-03-13 20:59:17 +0000

    Check this in before I start to work on the APR/native SSL issue
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1456125 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http11/upgrade/AprServletOutputStream.java
+++ java/org/apache/coyote/http11/upgrade/AprServletOutputStream.java
@@ -47,61 +47,74 @@ public class AprServletOutputStream extends AbstractServletOutputStream {
         Lock readLock = wrapper.getBlockingStatusReadLock();
         WriteLock writeLock = wrapper.getBlockingStatusWriteLock();
 
-        boolean writeDone = false;
-        int result = 0;
         try {
             readLock.lock();
             if (wrapper.getBlockingStatus() == block) {
                 if (closed) {
                     throw new IOException(sm.getString("apr.closed"));
                 }
-                result = Socket.send(socket, b, off, len);
-                writeDone = true;
+                return doWriteInternal(b, off, len);
             }
         } finally {
             readLock.unlock();
         }
 
-        if (!writeDone) {
+        try {
+            writeLock.lock();
+            // Set the current settings for this socket
+            wrapper.setBlockingStatus(block);
+            if (block) {
+                Socket.timeoutSet(socket, endpoint.getSoTimeout() * 1000);
+            } else {
+                Socket.timeoutSet(socket, 0);
+            }
+
+            // Downgrade the lock
             try {
-                writeLock.lock();
-                wrapper.setBlockingStatus(block);
-                // Set the current settings for this socket
-                Socket.optSet(socket, Socket.APR_SO_NONBLOCK, (block ? -1 : 0));
-                // Downgrade the lock
-                try {
-                    readLock.lock();
-                    writeLock.unlock();
-                    if (closed) {
-                        throw new IOException(sm.getString("apr.closed"));
-                    }
-                    result = Socket.send(socket, b, off, len);
-                } finally {
-                    readLock.unlock();
+                readLock.lock();
+                writeLock.unlock();
+                if (closed) {
+                    throw new IOException(sm.getString("apr.closed"));
                 }
+                return doWriteInternal(b, off, len);
             } finally {
-                // Should have been released above but may not have been on some
-                // exception paths
-                if (writeLock.isHeldByCurrentThread()) {
-                    writeLock.unlock();
-                }
+                readLock.unlock();
+            }
+        } finally {
+            // Should have been released above but may not have been on some
+            // exception paths
+            if (writeLock.isHeldByCurrentThread()) {
+                writeLock.unlock();
             }
         }
-
-        if (result >= 0) {
-            if (result < len) {
-                endpoint.getPoller().add(socket, -1, false, true);
-            }
-            return result;
-        } else if (-result == Status.EAGAIN) {
-            endpoint.getPoller().add(socket, -1, false, true);
-            return 0;
-        }
-
-        throw new IOException(sm.getString("apr.write.error",
-                Integer.valueOf(-result)));
-
     }
+
+
+    private int doWriteInternal(byte[] b, int off, int len) throws IOException {
+
+        int start = off;
+        int left = len;
+        int written;
+
+        do {
+            written = Socket.send(socket, b, start, left);
+            if (Status.APR_STATUS_IS_EAGAIN(-written)) {
+                endpoint.getPoller().add(socket, -1, false, true);
+                written = 0;
+            } else if (written < 0) {
+                throw new IOException(sm.getString("apr.write.error",
+                        Integer.valueOf(-written)));
+            }
+            start += written;
+            left -= written;
+        } while (written > 0 && left > 0);
+
+        if (left > 0) {
+            endpoint.getPoller().add(socket, -1, false, true);
+        }
+        return len - left;
+    }
+
 
     @Override
     protected void doFlush() throws IOException {
