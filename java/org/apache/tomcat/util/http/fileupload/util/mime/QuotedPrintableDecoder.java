5283d43020c9ac62121b2100bec0331875d69d52
Mark Emlyn David Thomas <markt@apache.org>
2013-03-19 23:14:02 +0000

    Whitespace police after merge
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1458565 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/http/fileupload/util/mime/QuotedPrintableDecoder.java
+++ java/org/apache/tomcat/util/http/fileupload/util/mime/QuotedPrintableDecoder.java
@@ -95,7 +95,7 @@ final class QuotedPrintableDecoder {
 
     /**
      * Convert a hex digit to the binary value it represents.
-     * 
+     *
      * @param b the ascii hex byte to convert (0-0, A-F, a-f)
      * @return the int value of the hex byte, 0-15
      * @throws IOException if the byte is not a valid hex digit.
