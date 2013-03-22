5ed271247cddc052e78d936ce677d6b317acce67
Konstantin Kolinko <kkolinko@apache.org>
2013-03-22 20:47:21 +0000

    Merged revision 1435760 from tomcat/trunk: Fix broken test (it has been broken for a long time) The timeout needs to be shorter than the sleep time so the connection is abandoned
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1459979 13f79535-47bb-0310-9956-ffa450edef68

--- modules/jdbc-pool/src/test/java/org/apache/tomcat/jdbc/test/TwoDataSources.java
+++ modules/jdbc-pool/src/test/java/org/apache/tomcat/jdbc/test/TwoDataSources.java
@@ -28,7 +28,7 @@ public class TwoDataSources extends DefaultTestCase {
         org.apache.tomcat.jdbc.pool.DataSource d1 = this.createDefaultDataSource();
         org.apache.tomcat.jdbc.pool.DataSource d2 = this.createDefaultDataSource();
         d1.setRemoveAbandoned(true);
-        d1.setRemoveAbandonedTimeout(10);
+        d1.setRemoveAbandonedTimeout(2);
         d1.setTimeBetweenEvictionRunsMillis(1000);
         d2.setRemoveAbandoned(false);
         Connection c1 = d1.getConnection();
