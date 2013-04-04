7c14f19ee5079b0595df56ed47fd7b3adc0aa0aa
Konstantin Kolinko <kkolinko@apache.org>
2013-04-04 23:00:53 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=54802 Provide location information for exceptions thrown by JspDocumentParser
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1464781 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/jasper/compiler/JspDocumentParser.java
+++ java/org/apache/jasper/compiler/JspDocumentParser.java
@@ -1184,8 +1184,9 @@ class JspDocumentParser
         TagInfo tagInfo = tagLibInfo.getTag(localName);
         TagFileInfo tagFileInfo = tagLibInfo.getTagFile(localName);
         if (tagInfo == null && tagFileInfo == null) {
-            throw new SAXException(
-                Localizer.getMessage("jsp.error.xml.bad_tag", localName, uri));
+            throw new SAXParseException(
+                Localizer.getMessage("jsp.error.xml.bad_tag", localName, uri),
+                locator);
         }
         Class<?> tagHandlerClass = null;
         if (tagInfo != null) {
@@ -1194,11 +1195,11 @@ class JspDocumentParser
                 tagHandlerClass =
                     ctxt.getClassLoader().loadClass(handlerClassName);
             } catch (Exception e) {
-                throw new SAXException(
+                throw new SAXParseException(
                     Localizer.getMessage("jsp.error.loadclass.taghandler",
                                          handlerClassName,
                                          qName),
-                    e);
+                    locator, e);
             }
         }
 
@@ -1329,7 +1330,7 @@ class JspDocumentParser
                         Localizer.getMessage(
                             "jsp.error.parse.xml.scripting.invalid.body",
                             elemType);
-                    throw new SAXException(msg);
+                    throw new SAXParseException(msg, locator);
                 }
             }
         }
@@ -1357,7 +1358,7 @@ class JspDocumentParser
                 locator,
                 fnfe);
         } catch (Exception e) {
-            throw new SAXException(e);
+            throw new SAXParseException(e.getMessage(), locator, e);
         }
     }
 
