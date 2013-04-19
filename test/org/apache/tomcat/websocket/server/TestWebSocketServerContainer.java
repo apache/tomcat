0339feceaa7268fde35fa10f1dddec4abc1106c7
Mark Emlyn David Thomas <markt@apache.org>
2013-04-19 19:25:49 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=54807 Forgot to create TreeSet with correct Comparator. Add a test case that checks the context starts correctly.
    Also, TemplatePathMatchComparator is thread-safe so use a single instance rather than creating multiple instances.
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1469997 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test/org/apache/tomcat/websocket/server/TestWebSocketServerContainer.java
@@ -0,0 +1,70 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.tomcat.websocket.server;
+
+import javax.servlet.ServletContextEvent;
+import javax.websocket.DeploymentException;
+import javax.websocket.server.ServerEndpointConfig;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleState;
+import org.apache.catalina.servlets.DefaultServlet;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.startup.TomcatBaseTest;
+import org.apache.tomcat.websocket.TesterEchoServer;
+
+
+public class TestWebSocketServerContainer extends TomcatBaseTest {
+
+    @Test
+    public void testBug54807() throws Exception {
+        Tomcat tomcat = getTomcatInstance();
+        // Must have a real docBase - just use temp
+        Context ctx =
+            tomcat.addContext("", System.getProperty("java.io.tmpdir"));
+        ctx.addApplicationListener(Bug54807Config.class.getName());
+        Tomcat.addServlet(ctx, "default", new DefaultServlet());
+        ctx.addServletMapping("/", "default");
+
+        tomcat.start();
+
+        Assert.assertEquals(LifecycleState.STARTED, ctx.getState());
+    }
+
+
+    public static class Bug54807Config extends WsListener {
+
+        @Override
+        public void contextInitialized(ServletContextEvent sce) {
+            super.contextInitialized(sce);
+
+            WsServerContainer sc = WsServerContainer.getServerContainer();
+
+            ServerEndpointConfig sec = ServerEndpointConfig.Builder.create(
+                    TesterEchoServer.Basic.class, "/{param}").build();
+
+            try {
+                sc.addEndpoint(sec);
+            } catch (DeploymentException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
