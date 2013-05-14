fb7c4ed65aca11e153e83cdf5ac3732039305e65
Mark Emlyn David Thomas <markt@apache.org>
2013-05-14 12:02:30 +0000

    Ensure HostConfig has the latest settings of copyXML, deployXML and unpackWARs before performing automatic deployment.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1482312 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/startup/HostConfig.java
+++ java/org/apache/catalina/startup/HostConfig.java
@@ -297,9 +297,6 @@ public class HostConfig
     @Override
     public void lifecycleEvent(LifecycleEvent event) {
 
-        if (event.getType().equals(Lifecycle.PERIODIC_EVENT))
-            check();
-
         // Identify the host we are associated with
         try {
             host = (Host) event.getLifecycle();
@@ -314,11 +311,13 @@ public class HostConfig
         }
 
         // Process the event that has occurred
-        if (event.getType().equals(Lifecycle.START_EVENT))
+        if (event.getType().equals(Lifecycle.PERIODIC_EVENT)) {
+            check();
+        } else if (event.getType().equals(Lifecycle.START_EVENT)) {
             start();
-        else if (event.getType().equals(Lifecycle.STOP_EVENT))
+        } else if (event.getType().equals(Lifecycle.STOP_EVENT)) {
             stop();
-
+        }
     }
 
 
