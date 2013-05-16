aabef09e48b1f6a4eca7263b999b8f93cc35cb1d
Violeta Georgieva Georgieva <violetagg@apache.org>
2013-05-16 08:07:24 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=54981 ServletContext.getJspConfigDescriptor() must return NULL when there is no jsp configuration
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1483229 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/core/ApplicationContext.java
+++ java/org/apache/catalina/core/ApplicationContext.java
@@ -1418,7 +1418,14 @@ public class ApplicationContext
 
     @Override
     public JspConfigDescriptor getJspConfigDescriptor() {
-        return context.getJspConfigDescriptor();
+        JspConfigDescriptor jspConfigDescriptor = context
+                .getJspConfigDescriptor();
+        if (jspConfigDescriptor.getJspPropertyGroups().isEmpty()
+                && jspConfigDescriptor.getTaglibs().isEmpty()) {
+            return null;
+        } else {
+            return jspConfigDescriptor;
+        }
     }
 
 
