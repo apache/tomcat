23e3a9fb98707bc17ea638e70f48adcfda6cc78c
Mark Thomas <markt@apache.org>
2013-06-02 21:05:15 +0000

    Remove unnecessary Javadoc
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1488789 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http11/InternalAprOutputBuffer.java
+++ java/org/apache/coyote/http11/InternalAprOutputBuffer.java
@@ -219,9 +219,6 @@ public class InternalAprOutputBuffer extends AbstractOutputBuffer<Long> {
     }
 
 
-    /**
-     * Callback to write data from the buffer.
-     */
     @Override
     protected synchronized boolean flushBuffer(boolean block)
             throws IOException {
