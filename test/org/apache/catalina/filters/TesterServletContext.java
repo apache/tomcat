9a66b12cb3adcb23c817b8db8f585515d596a3aa
Mark Thomas <markt@apache.org>
2013-06-04 10:51:31 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=55046 Add a Filter that implements CORS (http://www.w3.org/TR/cors/) Based on a patch by Mohit Soni.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1489385 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test/org/apache/catalina/filters/TesterServletContext.java
@@ -0,0 +1,320 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.catalina.filters;
+
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.EventListener;
+import java.util.Map;
+import java.util.Set;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterRegistration;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.Servlet;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import javax.servlet.ServletRegistration.Dynamic;
+import javax.servlet.SessionCookieConfig;
+import javax.servlet.SessionTrackingMode;
+import javax.servlet.descriptor.JspConfigDescriptor;
+
+public class TesterServletContext implements ServletContext {
+
+    @Override
+    public String getContextPath() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public ServletContext getContext(String uripath) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public int getMajorVersion() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public int getMinorVersion() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public String getMimeType(String file) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public Set<String> getResourcePaths(String path) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public URL getResource(String path) throws MalformedURLException {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public InputStream getResourceAsStream(String path) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public RequestDispatcher getRequestDispatcher(String path) {
+
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public RequestDispatcher getNamedDispatcher(String name) {
+
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public Servlet getServlet(String name) throws ServletException {
+
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public Enumeration<Servlet> getServlets() {
+
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public Enumeration<String> getServletNames() {
+
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public void log(String msg) {
+        // NOOP
+    }
+
+    @Override
+    public void log(Exception exception, String msg) {
+        // NOOP
+    }
+
+    @Override
+    public void log(String message, Throwable throwable) {
+        // NOOP
+    }
+
+    @Override
+    public String getRealPath(String path) {
+
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public String getServerInfo() {
+
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public String getInitParameter(String name) {
+
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public Enumeration<String> getInitParameterNames() {
+
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public Object getAttribute(String name) {
+
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public Enumeration<String> getAttributeNames() {
+
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public void setAttribute(String name, Object object) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public void removeAttribute(String name) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public String getServletContextName() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public int getEffectiveMajorVersion() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public int getEffectiveMinorVersion() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public boolean setInitParameter(String name, String value) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public Dynamic addServlet(String servletName, String className) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public Dynamic addServlet(String servletName, Servlet servlet) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public Dynamic addServlet(String servletName,
+            Class<? extends Servlet> servletClass) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public <T extends Servlet> T createServlet(Class<T> c)
+            throws ServletException {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public ServletRegistration getServletRegistration(String servletName) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public Map<String, ? extends ServletRegistration> getServletRegistrations() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public javax.servlet.FilterRegistration.Dynamic addFilter(
+            String filterName, String className) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public javax.servlet.FilterRegistration.Dynamic addFilter(
+            String filterName, Filter filter) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public javax.servlet.FilterRegistration.Dynamic addFilter(
+            String filterName, Class<? extends Filter> filterClass) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public <T extends Filter> T createFilter(Class<T> c)
+            throws ServletException {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public FilterRegistration getFilterRegistration(String filterName) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public Map<String, ? extends FilterRegistration> getFilterRegistrations() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public SessionCookieConfig getSessionCookieConfig() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public void setSessionTrackingModes(
+            Set<SessionTrackingMode> sessionTrackingModes) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public Set<SessionTrackingMode> getDefaultSessionTrackingModes() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public Set<SessionTrackingMode> getEffectiveSessionTrackingModes() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public void addListener(String className) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public <T extends EventListener> void addListener(T t) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public void addListener(Class<? extends EventListener> listenerClass) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public <T extends EventListener> T createListener(Class<T> c)
+            throws ServletException {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public JspConfigDescriptor getJspConfigDescriptor() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public ClassLoader getClassLoader() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public void declareRoles(String... roleNames) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public String getVirtualServerName() {
+        throw new RuntimeException("Not implemented");
+    }
+
+}
