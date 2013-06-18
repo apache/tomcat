e6165eb03bebb60e0277314b909df6c5acb2a933
Mark Emlyn David Thomas <markt@apache.org>
2013-06-18 22:43:17 +0000

    A unit test for handling the special case of an empty HttpConstraint that does not result in unauthenticated access for all.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1494355 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/catalina/connector/TesterRequest.java
+++ test/org/apache/catalina/connector/TesterRequest.java
@@ -36,4 +36,13 @@ public class TesterRequest extends Request {
     public String getDecodedRequestURI() {
         return "/level1/level2/foo.html";
     }
+
+    private String method;
+    public void setMethod(String method) {
+        this.method = method;
+    }
+    @Override
+    public String getMethod() {
+        return method;
+    }
 }
