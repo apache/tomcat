1be308e9484f09670ab68a1207b1b8ec89fb4330
Mark Emlyn David Thomas <markt@apache.org>
2013-07-03 14:50:38 +0000

    Re-order to simplify checking against EL spec
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1499428 13f79535-47bb-0310-9956-ffa450edef68

--- java/javax/el/Expression.java
+++ java/javax/el/Expression.java
@@ -26,13 +26,13 @@ public abstract class Expression implements Serializable {
 
     private static final long serialVersionUID = -6663767980471823812L;
 
+    public abstract String getExpressionString();
+
     @Override
     public abstract boolean equals(Object obj);
 
     @Override
     public abstract int hashCode();
-
-    public abstract String getExpressionString();
 
     public abstract boolean isLiteralText();
 
