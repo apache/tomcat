252de40e8a482ac36ecb07d5c87b7ce730d28b7f
Mark Emlyn David Thomas <markt@apache.org>
2013-07-15 13:59:51 +0000

    EL 3.0 collections operations. Implement filter
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1503253 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/el/stream/TestCollectionOperations.java
+++ test/org/apache/el/stream/TestCollectionOperations.java
@@ -17,6 +17,7 @@
 package org.apache.el.stream;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import javax.el.ELProcessor;
@@ -24,7 +25,30 @@ import javax.el.ELProcessor;
 import org.junit.Assert;
 import org.junit.Test;
 
+import org.apache.el.TesterBeanA;
+
 public class TestCollectionOperations {
+
+    private static final TesterBeanA bean01 = new TesterBeanA();
+    private static final TesterBeanA bean02 = new TesterBeanA();
+    private static final TesterBeanA bean03 = new TesterBeanA();
+    private static final List<TesterBeanA> beans;
+
+    static {
+        List<TesterBeanA> list = new ArrayList<>();
+
+        bean01.setValLong(1);
+        list.add(bean01);
+
+        bean02.setValLong(2);
+        list.add(bean02);
+
+        bean03.setValLong(3);
+        list.add(bean03);
+
+        beans = Collections.unmodifiableList(list);
+    }
+
 
     @Test
     public void testToList01() {
@@ -55,4 +79,17 @@ public class TestCollectionOperations {
         Assert.assertEquals(expected, result);
     }
 
+
+    @Test
+    public void testFilter01() {
+        ELProcessor processor = new ELProcessor();
+        processor.defineBean("beans", beans);
+        Object result = processor.getValue(
+                "beans.stream().filter(b->b.valLong > 2).toList()",
+                List.class);
+        List<TesterBeanA> expected = new ArrayList<>(1);
+        expected.add(bean03);
+
+        Assert.assertEquals(expected, result);
+    }
 }
