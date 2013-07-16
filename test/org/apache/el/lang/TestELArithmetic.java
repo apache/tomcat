04643087e10b0dfe6d447230218d56f8d4de42c9
Mark Thomas <markt@apache.org>
2013-07-16 20:49:58 +0000

    Add some test cases for null handling
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1503878 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/el/lang/TestELArithmetic.java
+++ test/org/apache/el/lang/TestELArithmetic.java
@@ -19,8 +19,11 @@ package org.apache.el.lang;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
+import javax.el.ELProcessor;
+
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestELArithmetic {
@@ -29,33 +32,75 @@ public class TestELArithmetic {
         new BigInteger("1000000000000000000000");
 
     @Test
-    public void testAdd() throws Exception {
+    public void testAdd01() throws Exception {
         assertEquals("1000000000000000000001.1",
                 String.valueOf(ELArithmetic.add(a, b)));
     }
 
     @Test
-    public void testSubtract() throws Exception {
+    public void testAdd02() {
+        ELProcessor processor = new ELProcessor();
+        Object result = processor.eval("null + null");
+        Assert.assertEquals(Long.valueOf(0), result);
+    }
+
+    @Test
+    public void testSubtract01() throws Exception {
         assertEquals("-999999999999999999998.9",
                 String.valueOf(ELArithmetic.subtract(a, b)));
     }
 
     @Test
-    public void testMultiply() throws Exception {
+    public void testSubtract02() {
+        ELProcessor processor = new ELProcessor();
+        Object result = processor.eval("null - null");
+        Assert.assertEquals(Long.valueOf(0), result);
+    }
+
+    @Test
+    public void testMultiply01() throws Exception {
         assertEquals("1100000000000000000000.0",
                 String.valueOf(ELArithmetic.multiply(a, b)));
     }
 
     @Test
-    public void testDivide() throws Exception {
+    public void testMultiply02() {
+        ELProcessor processor = new ELProcessor();
+        Object result = processor.eval("null * null");
+        Assert.assertEquals(Long.valueOf(0), result);
+    }
+
+    @Test
+    public void testDivide01() throws Exception {
         assertEquals("0.0",
                 String.valueOf(ELArithmetic.divide(a, b)));
     }
 
     @Test
-    public void testMod() throws Exception {
+    public void testDivide02() {
+        ELProcessor processor = new ELProcessor();
+        Object result = processor.eval("null / null");
+        Assert.assertEquals(Long.valueOf(0), result);
+    }
+
+    @Test
+    public void testMod01() throws Exception {
         assertEquals("1.1",
                 String.valueOf(ELArithmetic.mod(a, b)));
+    }
+
+    @Test
+    public void testMod02() {
+        ELProcessor processor = new ELProcessor();
+        Object result = processor.eval("null % null");
+        Assert.assertEquals(Long.valueOf(0), result);
+    }
+
+    @Test
+    public void testUnaryMinus01() {
+        ELProcessor processor = new ELProcessor();
+        Object result = processor.eval("-null");
+        Assert.assertEquals(Long.valueOf(0), result);
     }
 
     @Test
@@ -94,5 +139,4 @@ public class TestELArithmetic {
     public void testBug47371doubleString2() throws Exception {
         assertEquals(Double.valueOf(2), ELArithmetic.add("1.", "1"));
     }
-
 }
