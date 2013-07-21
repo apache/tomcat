c71833010d581899bddabcd492f6423193cd0caf
Mark Emlyn David Thomas <markt@apache.org>
2013-07-21 08:43:56 +0000

    Remove unused imports
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1505308 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/catalina/core/TestApplicationContext.java
+++ test/org/apache/catalina/core/TestApplicationContext.java
@@ -27,7 +27,6 @@ import javax.servlet.descriptor.JspPropertyGroupDescriptor;
 import javax.servlet.http.HttpServletResponse;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import org.apache.catalina.startup.Tomcat;
