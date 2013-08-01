a2f27f74aa5de9edc77b69b795bc898a41d2d496
Mark Emlyn David Thomas <markt@apache.org>
2013-08-01 09:46:15 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=55331 A disatch to an async servlet during the onTimeout method of an async listener should not trigger an ISE.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1509151 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/AsyncStateMachine.java
+++ java/org/apache/coyote/AsyncStateMachine.java
@@ -216,6 +216,10 @@ public class AsyncStateMachine<S> {
         } else if (state == AsyncState.DISPATCHING) {
             state = AsyncState.DISPATCHED;
             return SocketState.ASYNC_END;
+        } else if (state == AsyncState.STARTED) {
+            // This can occur if an async listener does a dispatch to an async
+            // servlet during onTimeout
+            return SocketState.LONG;
         } else {
             throw new IllegalStateException(
                     sm.getString("asyncStateMachine.invalidAsyncState",
