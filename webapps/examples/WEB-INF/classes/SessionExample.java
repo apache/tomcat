81a627ad73b455993c37f872432679a80974cf8a
Mark Thomas <markt@apache.org>
2013-08-07 20:07:15 +0000

    Part of fix for https://issues.apache.org/bugzilla/show_bug.cgi?id=48550 Make sure Servlets that could output UTF-8 mark the content as UTF-8. I think I found all the places where this needed to be changed. The other possible places involve HTTP headers so must be ISO-8859-1.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1511468 13f79535-47bb-0310-9956-ffa450edef68

--- webapps/examples/WEB-INF/classes/SessionExample.java
+++ webapps/examples/WEB-INF/classes/SessionExample.java
@@ -49,7 +49,7 @@ public class SessionExample extends HttpServlet {
                       HttpServletResponse response)
         throws IOException, ServletException
     {
-        response.setContentType("text/html");
+        response.setContentType("text/html;charset=UTF-8");
 
         PrintWriter out = response.getWriter();
         out.println("<html>");
