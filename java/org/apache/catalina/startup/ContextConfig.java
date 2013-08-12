70fcab1c098e2980fbcf9fe749c0d3d4908b416f
Mark Emlyn David Thomas <markt@apache.org>
2013-08-12 14:32:01 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=55404 Log warnings as warnings
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1513151 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/startup/ContextConfig.java
+++ java/org/apache/catalina/startup/ContextConfig.java
@@ -1131,7 +1131,7 @@ public class ContextConfig implements LifecycleListener {
             for (int j = 0; j < roles.length; j++) {
                 if (!"*".equals(roles[j]) &&
                     !context.findSecurityRole(roles[j])) {
-                    log.info(sm.getString("contextConfig.role.auth", roles[j]));
+                    log.warn(sm.getString("contextConfig.role.auth", roles[j]));
                     context.addSecurityRole(roles[j]);
                 }
             }
@@ -1143,14 +1143,14 @@ public class ContextConfig implements LifecycleListener {
             Wrapper wrapper = (Wrapper) wrappers[i];
             String runAs = wrapper.getRunAs();
             if ((runAs != null) && !context.findSecurityRole(runAs)) {
-                log.info(sm.getString("contextConfig.role.runas", runAs));
+                log.warn(sm.getString("contextConfig.role.runas", runAs));
                 context.addSecurityRole(runAs);
             }
             String names[] = wrapper.findSecurityReferences();
             for (int j = 0; j < names.length; j++) {
                 String link = wrapper.findSecurityReference(names[j]);
                 if ((link != null) && !context.findSecurityRole(link)) {
-                    log.info(sm.getString("contextConfig.role.link", link));
+                    log.warn(sm.getString("contextConfig.role.link", link));
                     context.addSecurityRole(link);
                 }
             }
