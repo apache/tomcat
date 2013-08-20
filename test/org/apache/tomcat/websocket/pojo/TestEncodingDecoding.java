91fd30754e376b516a682cc66e41384204bf0dbb
Mark Emlyn David Thomas <markt@apache.org>
2013-08-20 11:13:33 +0000

    Further reduce a potential timing sensitivity
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1515778 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/tomcat/websocket/pojo/TestEncodingDecoding.java
+++ test/org/apache/tomcat/websocket/pojo/TestEncodingDecoding.java
@@ -163,30 +163,27 @@ public class TestEncodingDecoding extends TomcatBaseTest {
         session.close();
 
         // Should not take very long but some failures have been seen
-        i = 0;
-        while (i < 20) {
-            if (Server.isLifeCycleEventCalled(
-                    MsgStringEncoder.class.getName()+":init")) {
+        i = testEvent(MsgStringEncoder.class.getName()+":init", 0);
+        i = testEvent(MsgStringDecoder.class.getName()+":init", i);
+        i = testEvent(MsgByteEncoder.class.getName()+":init", i);
+        i = testEvent(MsgByteDecoder.class.getName()+":init", i);
+        i = testEvent(MsgStringEncoder.class.getName()+":destroy", i);
+        i = testEvent(MsgStringDecoder.class.getName()+":destroy", i);
+        i = testEvent(MsgByteEncoder.class.getName()+":destroy", i);
+        i = testEvent(MsgByteDecoder.class.getName()+":destroy", i);
+    }
+
+
+    private int testEvent(String name, int count) throws InterruptedException {
+        int i = count;
+        while (i < 50) {
+            if (Server.isLifeCycleEventCalled(name)) {
                 break;
             }
             Thread.sleep(100);
         }
-        Assert.assertTrue(Server.isLifeCycleEventCalled(
-                MsgStringEncoder.class.getName()+":init"));
-        Assert.assertTrue(Server.isLifeCycleEventCalled(
-                MsgStringDecoder.class.getName()+":init"));
-        Assert.assertTrue(Server.isLifeCycleEventCalled(
-                MsgByteEncoder.class.getName()+":init"));
-        Assert.assertTrue(Server.isLifeCycleEventCalled(
-                MsgByteDecoder.class.getName()+":init"));
-        Assert.assertTrue(Server.isLifeCycleEventCalled(
-                MsgStringEncoder.class.getName()+":destroy"));
-        Assert.assertTrue(Server.isLifeCycleEventCalled(
-                MsgStringDecoder.class.getName()+":destroy"));
-        Assert.assertTrue(Server.isLifeCycleEventCalled(
-                MsgByteEncoder.class.getName()+":destroy"));
-        Assert.assertTrue(Server.isLifeCycleEventCalled(
-                MsgByteDecoder.class.getName()+":destroy"));
+        Assert.assertTrue(Server.isLifeCycleEventCalled(name));
+        return i;
     }
 
 
