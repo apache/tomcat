ab70f07bbd9715c526e4d91b25348b1ad57994e1
Violeta Georgieva Georgieva <violetagg@apache.org>
2013-08-20 08:38:29 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=55442 Handle calls to a constructor of an imported class or to a static method, imported statically. Unit test based on a patch provided by Daniel Mikusa.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1515744 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test/org/apache/el/parser/TestAstFunction.java
@@ -0,0 +1,41 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.el.parser;
+
+import javax.el.ELProcessor;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestAstFunction {
+
+    @Test
+    public void testImport01() {
+        ELProcessor processor = new ELProcessor();
+        Object result = processor.getValue("Integer(1000)", Integer.class);
+        Assert.assertEquals(Integer.valueOf(1000), result);
+    }
+
+    @Test
+    public void testImport02() {
+        ELProcessor processor = new ELProcessor();
+        processor.getELManager().getELContext().getImportHandler()
+                .importStatic("java.lang.Integer.valueOf");
+        Object result = processor.getValue("valueOf(1000)", Integer.class);
+        Assert.assertEquals(Integer.valueOf(1000), result);
+    }
+}
