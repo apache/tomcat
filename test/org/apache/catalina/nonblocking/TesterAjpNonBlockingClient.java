112bc4f35b15b6cd2c7d9d0af99a9e3646910003
Mark Thomas <markt@apache.org>
2013-09-05 22:40:50 +0000

    Add a simple non-blocking write test that currently fails. This won't impact the CI systems since the class is excluded from the unit tests.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1520447 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/catalina/nonblocking/TesterAjpNonBlockingClient.java
+++ test/org/apache/catalina/nonblocking/TesterAjpNonBlockingClient.java
@@ -16,10 +16,15 @@
  */
 package org.apache.catalina.nonblocking;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.net.SocketFactory;
 import javax.servlet.http.HttpServletResponse;
 
 import org.junit.Assert;
@@ -29,14 +34,14 @@ import org.apache.catalina.nonblocking.TestNonBlockingAPI.DataWriter;
 import org.apache.catalina.startup.TomcatBaseTest;
 import org.apache.tomcat.util.buf.ByteChunk;
 
+/**
+ * This is not a standard set of unit tests. This is a set of test clients for
+ * AJP support of Servlet 3.1 non-blocking IO. It assumes that there is an httpd
+ * instance listening on localhost:80 that is redirecting all traffic to a
+ * default Tomcat 8 instance that includes the examples web application.
+ */
 public class TesterAjpNonBlockingClient extends TomcatBaseTest {
 
-    /**
-     * This is not a standard unit test. This is a test client for AJP
-     * non-blocking reads. It assumes that there is an httpd instance listening
-     * on localhost:80 that is redirecting all traffic to a default Tomcat 8
-     * instance that includes the examples web application.
-     */
     @Test
     public void doTestAJPNonBlockingRead() throws Exception {
 
@@ -49,5 +54,48 @@ public class TesterAjpNonBlockingClient extends TomcatBaseTest {
         System.out.println(out.toString());
 
         Assert.assertEquals(HttpServletResponse.SC_OK, rc);
+    }
+
+
+    @Test
+    public void testNonBlockingWrite() throws Exception {
+
+        SocketFactory factory = SocketFactory.getDefault();
+        Socket s = factory.createSocket("localhost", 80);
+
+        ByteChunk result = new ByteChunk();
+        OutputStream os = s.getOutputStream();
+        os.write(("GET /examples/servlets/nonblocking/numberwriter HTTP/1.1\r\n" +
+                "Host: localhost\r\n" +
+                "Connection: close\r\n" +
+                "\r\n").getBytes(StandardCharsets.ISO_8859_1));
+        os.flush();
+
+        InputStream is = s.getInputStream();
+        byte[] buffer = new byte[8192];
+
+        int read = 0;
+        int readSinceLastPause = 0;
+        while (read != -1) {
+            read = is.read(buffer);
+            if (read > 0) {
+                result.append(buffer, 0, read);
+            }
+            readSinceLastPause += read;
+            if (readSinceLastPause > 40000) {
+                readSinceLastPause = 0;
+                Thread.sleep(500);
+            }
+        }
+
+        os.close();
+        is.close();
+        s.close();
+
+        // Validate the result
+        String resultString = result.toString();
+        log.info("Client read " + resultString.length() + " bytes");
+
+        Assert.assertTrue(resultString.contains("00000000000000010000"));
     }
 }
