3a54975c01b0845ca0afd9a75f9cb333299b786f
Mark Emlyn David Thomas <markt@apache.org>
2013-09-09 11:15:33 +0000

    Whoops. Put the elements in the right annotation.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1521043 13f79535-47bb-0310-9956-ffa450edef68

--- java/javax/annotation/sql/DataSourceDefinitions.java
+++ java/javax/annotation/sql/DataSourceDefinitions.java
@@ -27,22 +27,5 @@ import java.lang.annotation.Target;
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface DataSourceDefinitions {
-    String className();
-    String name();
-    String description() default "";
-    String url() default "";
-    String user() default "";
-    String password() default "";
-    String databaseName() default "";
-    int portNumber() default -1;
-    String serverName() default "localhost";
-    int isolationLevel() default -1;
-    boolean transactional() default true;
-    int initialPoolSize() default -1;
-    int maxPoolSize() default -1;
-    int minPoolSize() default -1;
-    int maxIdleTime() default -1;
-    int maxStatements() default -1;
-    String[] properties() default {};
-    int loginTimeout() default 0;
+    DataSourceDefinition[] value();
 }
