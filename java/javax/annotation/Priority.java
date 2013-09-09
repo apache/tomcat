3ff74d7646e54692c6962460c31d2549ab8c322e
Mark Thomas <markt@apache.org>
2013-09-09 10:51:36 +0000

    Remainder of fix for https://issues.apache.org/bugzilla/show_bug.cgi?id=55534 Commons Annotations 1.2 changes
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1521035 13f79535-47bb-0310-9956-ffa450edef68

--- java/javax/annotation/Priority.java
+++ java/javax/annotation/Priority.java
@@ -22,26 +22,10 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * @since Common Annotations 1.0
+ * @since Common Annotations 1.2
  */
-@Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD})
+@Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-public @interface Resource {
-    public enum AuthenticationType {
-        CONTAINER,
-        APPLICATION
-    }
-    public String name() default "";
-    /**
-     * Uses generics since Common Annotations 1.2.
-     */
-    public Class<?> type() default Object.class;
-    public AuthenticationType authenticationType() default AuthenticationType.CONTAINER;
-    public boolean shareable() default true;
-    public String description() default "";
-    public String mappedName() default "";
-    /**
-     * @since Common Annotations 1.1
-     */
-    public String lookup() default "";
+public @interface Priority {
+    int value();
 }
