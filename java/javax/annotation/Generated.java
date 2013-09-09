b3337ab2f46a4e04a4d02b5ab6e84a015dc29a00
Mark Thomas <markt@apache.org>
2013-09-09 10:11:33 +0000

    Remove some unnecessary whitespace. Add some @since markers prior to fixing bz 55534.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1521025 13f79535-47bb-0310-9956-ffa450edef68

--- java/javax/annotation/Generated.java
+++ java/javax/annotation/Generated.java
@@ -14,8 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-
 package javax.annotation;
 
 import java.lang.annotation.ElementType;
@@ -23,11 +21,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * @since Common Annotations 1.0
+ */
 @Target({ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR,
     ElementType.FIELD, ElementType.LOCAL_VARIABLE, ElementType.METHOD,
     ElementType.PACKAGE, ElementType.PARAMETER, ElementType.TYPE})
 @Retention(RetentionPolicy.SOURCE)
-
 public @interface Generated {
     public String[] value();
     public String date() default "";
