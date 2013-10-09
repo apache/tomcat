af49b44d0992721042ea7e00ade8e5b941abaca3
Mark Emlyn David Thomas <markt@apache.org>
2013-10-09 14:33:39 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=55642 Correct logic error in Node.isLiteral() that was returning the inverse of the desired result when the value was an EL expression.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1530632 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/jasper/compiler/Node.java
+++ java/org/apache/jasper/compiler/Node.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.jasper.compiler;
 
 import java.util.ArrayList;
@@ -2289,7 +2288,7 @@ abstract class Node implements TagConstants {
          *         time.
          */
         public boolean isLiteral() {
-            return !expression && (el != null) && !namedAttribute;
+            return !expression && (el == null) && !namedAttribute;
         }
 
         /**
