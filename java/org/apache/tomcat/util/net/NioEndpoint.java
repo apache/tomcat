apache/tomcat

commit 371cbea0bf0c152d496a95bbe006d0dd7434b44f

First revision of an example non blocking read operation. The servlet specification, and discussions on the expert group are quite contradictory. According to the specification request response object can only live during service() or when an AsyncContext is present. but the NIO api is written with examples of bypassing both.
So for this iteration, we are working with the assumption that 
NIO is only allowed during async operations, otherwise there is no point to NIO.
This is because 'polling' was not allowed, so there is no point to have NIO when the thread is present



git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1357039 13f79535-47bb-0310-9956-ffa450edef68

java/org/apache/tomcat/util/net/NioEndpoint.java
@@ -1244,7 +1244,7 @@ public class NioEndpoint extends AbstractEndpoint {
                                         processSocket(channel, SocketStatus.DISCONNECT, true);
                                 } else {
                                     //future placement of a WRITE notif
-                                    if (!processSocket(channel, SocketStatus.OPEN, true))
+                                    if (!processSocket(channel, SocketStatus.OPEN_WRITE, true))
                                         processSocket(channel, SocketStatus.DISCONNECT, true);
                                 }
                             } else {
@@ -1253,8 +1253,23 @@ public class NioEndpoint extends AbstractEndpoint {
                         } else {
                             //later on, improve latch behavior
                             if ( isWorkerAvailable() ) {
-                                unreg(sk, attachment,sk.readyOps());
-                                boolean close = (!processSocket(channel, null, true));
+
+                                boolean readAndWrite = sk.isReadable() && sk.isWritable();
+                                reg(sk, attachment, 0);
+                                if (readAndWrite) {
+                                    //remember the that we want to know about write too
+                                    attachment.interestOps(SelectionKey.OP_WRITE);
+                                }
+                                //read goes before write
+                                if (sk.isReadable()) {
+                                    //read notification
+                                    if (!processSocket(channel, SocketStatus.OPEN, true))
+                                        close = true;
+                                } else {
+                                    //future placement of a WRITE notif
+                                    if (!processSocket(channel, SocketStatus.OPEN_WRITE, true))
+                                        close = true;
+                                }
                                 if (close) {
                                     cancelledKey(sk,SocketStatus.DISCONNECT);
                                 }
@@ -1652,11 +1667,10 @@ public class NioEndpoint extends AbstractEndpoint {
                                     (KeyAttachment) key.attachment(),
                                     status);
                         }
-
+                        KeyAttachment ka = (KeyAttachment)key.attachment();
                         if (state == SocketState.CLOSED) {
                             // Close socket and pool
                             try {
-                                KeyAttachment ka = (KeyAttachment) key.attachment();
                                 if (ka!=null) ka.setComet(false);
                                 socket.getPoller().cancelledKey(key, SocketStatus.ERROR);
                                 nioChannels.offer(socket);
@@ -1666,6 +1680,9 @@ public class NioEndpoint extends AbstractEndpoint {
                             }catch ( Exception x ) {
                                 log.error("",x);
                             }
+                        } else if (state == SocketState.LONG && ka != null && ka.isAsync() && ka.interestOps() > 0) {
+                            //we are async, and we are interested in operations
+                            ka.getPoller().add(socket, ka.interestOps());
                         }
                     } else if (handshake == -1 ) {
                         KeyAttachment ka = null;

java/javax/servlet/ServletInputStream.java
@@ -94,8 +94,11 @@ public abstract class ServletInputStream extends InputStream {
      * @return
      */
     public abstract boolean isFinished();
+
     /**
      * TODO SERVLET 3.1
+     * If this returns false, the container will invoke
+     * {@link ReadListener#onDataAvailable()} when data is available.
      * @return
      */
     public abstract boolean isReady();

java/javax/servlet/ServletOutputStream.java
@@ -273,6 +273,8 @@ public abstract class ServletOutputStream extends OutputStream {
     }
 
     /**
+     * If this returns false, it will cause a callback to
+     * {@link WriteListener#onWritePossible()} when the buffer has emptied
      * TODO SERVLET 3.1
      * @return
      */

java/org/apache/catalina/connector/CoyoteAdapter.java
@@ -20,6 +20,7 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 import java.util.EnumSet;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.SessionTrackingMode;
@@ -293,6 +294,22 @@ public class CoyoteAdapter implements Adapter {
                     asyncConImpl.setErrorState(null);
                 }
             }
+
+
+            if (!request.isAsyncDispatching() && request.isAsync()) {
+                AtomicBoolean result = new AtomicBoolean(true);
+                req.action(ActionCode.ASYNC_DISPATCH_FOR_OPERATION, this);
+                if (result.get()) {
+                    if (status==SocketStatus.OPEN_WRITE) {
+                        //TODO Notify write listener
+                    } else if (status==SocketStatus.OPEN) {
+                        //TODO Notify read listener
+                        asyncConImpl.canRead();
+                    }
+                    success = true;
+                }
+            }
+
             if (request.isAsyncDispatching()) {
                 success = true;
                 connector.getService().getContainer().getPipeline().getFirst().invoke(request, response);

java/org/apache/catalina/connector/CoyoteInputStream.java
@@ -242,25 +242,20 @@ public class CoyoteInputStream
         }
     }
 
-    /**
-     * TODO SERVLET 3.1
-     */
     @Override
     public boolean isFinished() {
-        // TODO Auto-generated method stub
-        return false;
+        return ib.isFinished();
     }
 
     @Override
     public int dataAvailable() {
-        // TODO Auto-generated method stub
-        return 0;
+        return ib.dataAvailable();
     }
 
 
     @Override
     public boolean isReady() {
-        return dataAvailable()>0;
+        return ib.isReady();
     }
 
 
@@ -269,10 +264,8 @@ public class CoyoteInputStream
      */
     @Override
     public void setReadListener(ReadListener listener) {
-        // TODO Auto-generated method stub
-
+        ib.setReadListener(listener);
     }
-
 
 
 }

java/org/apache/catalina/connector/InputBuffer.java
@@ -23,6 +23,8 @@ import java.security.PrivilegedActionException;
 import java.security.PrivilegedExceptionAction;
 import java.util.HashMap;
 
+import javax.servlet.ReadListener;
+
 import org.apache.catalina.security.SecurityUtil;
 import org.apache.coyote.ActionCode;
 import org.apache.coyote.Request;
@@ -247,6 +249,51 @@ public class InputBuffer extends Reader
         return available;
     }
 
+    private volatile ReadListener listener;
+    public void setReadListener(ReadListener listener) {
+        this.listener = listener;
+        coyoteRequest.action(ActionCode.SET_READ_LISTENER, listener);
+    }
+
+    public ReadListener getReadListener() {
+        return listener;
+    }
+
+    public boolean isFinished() {
+        return dataAvailable()==0;
+    }
+
+    public int dataAvailable() {
+        if (getReadListener()==null) throw new IllegalStateException("not in non blocking mode.");
+        int result = 0;
+        //first check if we have buffered something already
+        result = available();
+
+        if(result <= 0) {
+            //here we can issue a non blocking read
+            //if supported
+            //TODO SERVLET 3.1
+        }
+
+        return result;
+
+    }
+
+
+    public boolean isReady() {
+        if (getReadListener()==null) throw new IllegalStateException("not in non blocking mode.");
+        int available = dataAvailable();
+        boolean result = available>0;
+        if (!result) {
+            coyoteRequest.action(ActionCode.NB_READ_INTEREST, null);
+        }
+        return result;
+    }
+
+
+
+
+
 
     // ------------------------------------------------- Bytes Handling Methods
 
@@ -441,7 +488,9 @@ public class InputBuffer extends Reader
         if (closed) {
             throw new IOException(sm.getString("inputBuffer.streamClosed"));
         }
-
+        if (state == INITIAL_STATE) {
+            state = CHAR_STATE;
+        }
         return (available() > 0);
     }
 

java/org/apache/catalina/core/AsyncContextImpl.java
@@ -105,6 +105,25 @@ public class AsyncContextImpl implements AsyncContext, AsyncContextCallback {
         }
     }
 
+    public boolean canRead() throws IOException {
+        ClassLoader oldCL = Thread.currentThread().getContextClassLoader();
+        ClassLoader newCL = request.getContext().getLoader().getClassLoader();
+        try {
+            Thread.currentThread().setContextClassLoader(newCL);
+            request.getCoyoteRequest().getReadListener().onDataAvailable();
+            if (request.getInputStream().dataAvailable()==0) {
+                request.getCoyoteRequest().getReadListener().onAllDataRead();
+            }
+        }finally {
+            Thread.currentThread().setContextClassLoader(oldCL);
+        }
+        return true;
+    }
+
+    public boolean canWrite() throws IOException {
+        return false;
+    }
+
     public boolean timeout() throws IOException {
         AtomicBoolean result = new AtomicBoolean();
         request.getCoyoteRequest().action(ActionCode.ASYNC_TIMEOUT, result);

java/org/apache/coyote/ActionCode.java
@@ -137,6 +137,13 @@ public enum ActionCode {
     ASYNC_DISPATCH,
 
     /**
+     * Callback when an async call is
+     * {@link javax.servlet.ReadListener#onDataAvailable()} or
+     * {@link javax.servlet.WriteListener#onWritePossible()}
+     */
+    ASYNC_DISPATCH_FOR_OPERATION,
+
+    /**
      * Callback to indicate the the actual dispatch has started and that the
      * async state needs change.
      */
@@ -193,5 +200,27 @@ public enum ActionCode {
     /**
      * Callback to trigger the HTTP upgrade process.
      */
-    UPGRADE
+    UPGRADE,
+
+    /**
+     * Callback to trigger setting the ReadListener
+     */
+    SET_READ_LISTENER,
+
+    /**
+     * Callback to trigger setting the WriteListener
+     */
+    SET_WRITE_LISTENER,
+
+    /**
+     * Indicator that Servlet is interested in being
+     * notified when data is available to be read
+     */
+    NB_READ_INTEREST,
+
+    /**
+     *Indicator that the Servlet is interested
+     *in being notified when it can write data
+     */
+    NB_WRITE_INTEREST
 }

commit 08e5d12865ebe6fce5fd3533458cc75a58897d31

Fix a bug introduced in r1357039 that removed a local close variable which mean that code that should of triggered a socket close, ended up closing the Poller as well.
git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1531087 13f79535-47bb-0310-9956-ffa450edef68

java/org/apache/tomcat/util/net/NioEndpoint.java
@@ -1158,18 +1158,19 @@ public class NioEndpoint extends AbstractEndpoint<NioChannel> {
                         } else {
                             if ( isWorkerAvailable() ) {
                                 unreg(sk, attachment, sk.readyOps());
+                                boolean closeSocket = false;
                                 // Read goes before write
                                 if (sk.isReadable()) {
                                     if (!processSocket(channel, SocketStatus.OPEN_READ, true)) {
-                                        close = true;
+                                        closeSocket = true;
                                     }
                                 }
-                                if (!close && sk.isWritable()) {
+                                if (!closeSocket && sk.isWritable()) {
                                     if (!processSocket(channel, SocketStatus.OPEN_WRITE, true)) {
-                                        close = true;
+                                        closeSocket = true;
                                     }
                                 }
-                                if (close) {
+                                if (closeSocket) {
                                     cancelledKey(sk,SocketStatus.DISCONNECT);
                                 }
                             } else {