706dd19ffb6b32301d81af0dc4f63144803c6515
Mark Thomas <markt@apache.org>
2013-10-16 11:12:46 +0000

    Add a debug message that enables the socket reference (used in most debuig messages) to be matched to the port number (used in tcpdump) to enable unusual behaviour in the debug logs to be easily matched to the corresponding network activity.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1532719 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/net/AprEndpoint.java
+++ java/org/apache/tomcat/util/net/AprEndpoint.java
@@ -1007,6 +1007,13 @@ public class AprEndpoint extends AbstractEndpoint<Long> {
                         // Accept the next incoming connection from the server
                         // socket
                         socket = Socket.accept(serverSock);
+                        if (log.isDebugEnabled()) {
+                            long sa = Address.get(Socket.APR_REMOTE, socket);
+                            Sockaddr addr = Address.getInfo(sa);
+                            log.debug(sm.getString("endpoint.apr.remoteport",
+                                    Long.valueOf(socket),
+                                    Long.valueOf(addr.port)));
+                        }
                     } catch (Exception e) {
                         //we didn't get a socket
                         countDownConnection();
