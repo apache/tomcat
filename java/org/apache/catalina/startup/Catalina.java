8817badb2b62f251829c4a89aaa62e84755defc3
Mark Emlyn David Thomas <markt@apache.org>
2013-10-17 15:28:24 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=55656 Configure the Digester to use the server class loader when parsing server.xml rather than the class loader that loaded StandardServer. Patch provided by Roberto Benedetti.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1533119 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/startup/Catalina.java
+++ java/org/apache/catalina/startup/Catalina.java
@@ -35,7 +35,6 @@ import org.apache.catalina.Globals;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.LifecycleState;
 import org.apache.catalina.Server;
-import org.apache.catalina.core.StandardServer;
 import org.apache.catalina.security.SecurityConfig;
 import org.apache.juli.ClassLoaderLogManager;
 import org.apache.tomcat.util.ExceptionUtils;
@@ -310,7 +309,7 @@ public class Catalina {
         attrs.add("className");
         fakeAttributes.put(Object.class, attrs);
         digester.setFakeAttributes(fakeAttributes);
-        digester.setClassLoader(StandardServer.class.getClassLoader());
+        digester.setUseContextClassLoader(true);
 
         // Configure the actions we will be using
         digester.addObjectCreate("Server",
@@ -430,6 +429,7 @@ public class Catalina {
 
         // Initialize the digester
         Digester digester = new Digester();
+        digester.setUseContextClassLoader(true);
 
         // Configure the rules we need for shutting down
         digester.addObjectCreate("Server",
@@ -459,7 +459,6 @@ public class Catalina {
         if( s == null ) {
             // Create and execute our Digester
             Digester digester = createStopDigester();
-            digester.setClassLoader(Thread.currentThread().getContextClassLoader());
             File file = configFile();
             FileInputStream fis = null;
             try {
