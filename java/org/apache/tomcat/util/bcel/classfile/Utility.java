54b40373c830ce5a7ab8cd0f014432b81b321208
Mark Emlyn David Thomas <markt@apache.org>
2013-11-09 21:39:37 +0000

    chopit parameter is always false in remaining code. Simplify.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1540404 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/bcel/classfile/Utility.java
+++ java/org/apache/tomcat/util/bcel/classfile/Utility.java
@@ -39,35 +39,10 @@ public abstract class Utility {
      * Slashes <em>/</em> are converted to dots <em>.</em>.
      *
      * @param str The long class name
-     * @param prefix The prefix the get rid off
-     * @param chopit Flag that determines whether chopping is executed or not
      * @return Compacted class name
      */
-    public static final String compactClassName( String str, String prefix, boolean chopit ) {
-        int len = prefix.length();
-        str = str.replace('/', '.'); // Is `/' on all systems, even DOS
-        if (chopit) {
-            // If string starts with `prefix' and contains no further dots
-            if (str.startsWith(prefix) && (str.substring(len).indexOf('.') == -1)) {
-                str = str.substring(len);
-            }
-        }
-        return str;
-    }
-
-
-    /**
-     * Shorten long class names, <em>java/lang/String</em> becomes
-     * <em>java.lang.String</em>,
-     * e.g.. If <em>chopit</em> is <em>true</em> the prefix <em>java.lang</em>
-     * is also removed.
-     *
-     * @param str The long class name
-     * @param chopit Flag that determines whether chopping is executed or not
-     * @return Compacted class name
-     */
-    public static final String compactClassName( String str, boolean chopit ) {
-        return compactClassName(str, "java.lang.", chopit);
+    public static final String compactClassName(String str) {
+        return str.replace('/', '.'); // Is `/' on all systems, even DOS
     }
 
 
