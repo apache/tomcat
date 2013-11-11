4f53e576c28319e6aa06307270798b750ed226e1
Konstantin Kolinko <kkolinko@apache.org>
2013-11-11 21:30:58 +0000

    Re: r1540522 Fix unneeded boxing/unboxing.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1540848 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/jasper/servlet/JasperInitializer.java
+++ java/org/apache/jasper/servlet/JasperInitializer.java
@@ -49,7 +49,7 @@ public class JasperInitializer implements ServletContainerInitializer {
             LOG.debug(Localizer.getMessage(MSG + ".onStartup", context.getServletContextName()));
         }
 
-        boolean validate = Boolean.valueOf(context.getInitParameter(VALIDATE));
+        boolean validate = Boolean.parseBoolean(context.getInitParameter(VALIDATE));
 
         // scan the application for TLDs
         TldScanner scanner = new TldScanner(context, true, validate);
