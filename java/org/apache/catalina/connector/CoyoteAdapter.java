16466c42e6fa800a523a369c4806d6bd7f61c5d4
Mark Thomas <markt@apache.org>
2013-11-15 13:39:52 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=55772 Ensure that the request and response are recycled after an error during async processing.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1542267 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/connector/CoyoteAdapter.java
+++ java/org/apache/catalina/connector/CoyoteAdapter.java
@@ -20,6 +20,7 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 import java.util.EnumSet;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.servlet.ReadListener;
 import javax.servlet.RequestDispatcher;
@@ -574,8 +575,10 @@ public class CoyoteAdapter implements Adapter {
             // Ignore
         } finally {
             req.getRequestProcessor().setWorkerThreadName(null);
+            AtomicBoolean error = new AtomicBoolean(false);
+            res.action(ActionCode.IS_ERROR, error);
             // Recycle the wrapper request and response
-            if (!comet && !async) {
+            if (!comet && !async || error.get()) {
                 request.recycle();
                 response.recycle();
             } else {
--- test/org/apache/coyote/http11/TestAbstractHttp11Processor.java
+++ test/org/apache/coyote/http11/TestAbstractHttp11Processor.java
@@ -18,12 +18,19 @@ package org.apache.coyote.http11;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.net.Socket;
 import java.nio.CharBuffer;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 
+import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -32,6 +39,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import org.apache.catalina.Context;
@@ -39,6 +47,7 @@ import org.apache.catalina.startup.SimpleHttpClient;
 import org.apache.catalina.startup.TesterServlet;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.catalina.startup.TomcatBaseTest;
+import org.apache.tomcat.util.buf.B2CConverter;
 import org.apache.tomcat.util.buf.ByteChunk;
 
 public class TestAbstractHttp11Processor extends TomcatBaseTest {
@@ -394,6 +403,115 @@ public class TestAbstractHttp11Processor extends TomcatBaseTest {
             assertFalse(responseBody.toString().contains("FAIL"));
         }
     }
+
+
+    private static CountDownLatch bug55772Latch1 = new CountDownLatch(1);
+    private static CountDownLatch bug55772Latch2 = new CountDownLatch(1);
+    private static CountDownLatch bug55772Latch3 = new CountDownLatch(1);
+    private static boolean bug55772IsSecondRequest = false;
+    private static boolean bug55772RequestStateLeaked = false;
+
+
+    @Test
+    public void testBug55772() throws Exception {
+        Tomcat tomcat = getTomcatInstance();
+        tomcat.getConnector().setProperty("processorCache", "1");
+        tomcat.getConnector().setProperty("maxThreads", "1");
+
+        // Must have a real docBase - just use temp
+        Context ctxt = tomcat.addContext("",
+                System.getProperty("java.io.tmpdir"));
+
+        Tomcat.addServlet(ctxt, "async", new Bug55772Servlet());
+        ctxt.addServletMapping("/*", "async");
+
+        tomcat.start();
+
+        String request1 = "GET /async HTTP/1.1\r\n" +
+                "Host: localhost:" + getPort() + "\r\n" +
+                "Connection: keep-alive\r\n" +
+                "Cache-Control: max-age=0\r\n" +
+                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\n" +
+                "User-Agent: Request1\r\n" +
+                "Accept-Encoding: gzip,deflate,sdch\r\n" +
+                "Accept-Language: en-US,en;q=0.8,fr;q=0.6,es;q=0.4\r\n" +
+                "Cookie: something.that.should.not.leak=true\r\n" +
+                "\r\n";
+
+        String request2 = "GET /async HTTP/1.1\r\n" +
+                "Host: localhost:" + getPort() + "\r\n" +
+                "Connection: keep-alive\r\n" +
+                "Cache-Control: max-age=0\r\n" +
+                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\n" +
+                "User-Agent: Request2\r\n" +
+                "Accept-Encoding: gzip,deflate,sdch\r\n" +
+                "Accept-Language: en-US,en;q=0.8,fr;q=0.6,es;q=0.4\r\n" +
+                "\r\n";
+
+        try (final Socket connection = new Socket("localhost", getPort())) {
+            connection.setSoLinger(true, 0);
+            Writer writer = new OutputStreamWriter(connection.getOutputStream(),
+                    B2CConverter.getCharset("US-ASCII"));
+            writer.write(request1);
+            writer.flush();
+
+            bug55772Latch1.await();
+            connection.close();
+        }
+
+        bug55772Latch2.await();
+        bug55772IsSecondRequest = true;
+
+        try (final Socket connection = new Socket("localhost", getPort())) {
+            connection.setSoLinger(true, 0);
+            Writer writer = new OutputStreamWriter(connection.getOutputStream(),
+                    B2CConverter.getCharset("US-ASCII"));
+            writer.write(request2);
+            writer.flush();
+            connection.getInputStream().read();
+        }
+
+        bug55772Latch3.await();
+        if (bug55772RequestStateLeaked) {
+            Assert.fail("State leaked between requests!");
+        }
+    }
+
+
+    private static class Bug55772Servlet extends HttpServlet {
+
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            if (bug55772IsSecondRequest) {
+                Cookie[] cookies = req.getCookies();
+                if (cookies != null && cookies.length > 0) {
+                    for (Cookie cookie : req.getCookies()) {
+                        if (cookie.getName().equalsIgnoreCase("something.that.should.not.leak")) {
+                            bug55772RequestStateLeaked = true;
+                        }
+                    }
+                }
+                bug55772Latch3.countDown();
+            } else {
+                req.getCookies(); // We have to do this so Tomcat will actually parse the cookies from the request
+            }
+
+            req.setAttribute("org.apache.catalina.ASYNC_SUPPORTED", Boolean.TRUE);
+            AsyncContext asyncContext = req.startAsync();
+            asyncContext.setTimeout(5000);
+
+            bug55772Latch1.countDown();
+
+            PrintWriter writer = asyncContext.getResponse().getWriter();
+            writer.print('\n');
+            writer.flush();
+
+            bug55772Latch2.countDown();
+        }
+    }
+
 
     private static final class LargeHeaderServlet extends HttpServlet {
 
