30869c4d0f77e4fba50adfad01be9bcf48d174ce
Mark Thomas <markt@apache.org>
2013-11-29 19:25:49 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=55804 If the GSSCrednetial for the cached principal expires when using SPNEGO authentication, force a re-authentication.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1546631 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/connector/Request.java
+++ java/org/apache/catalina/connector/Request.java
@@ -96,6 +96,8 @@ import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
 import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
 import org.apache.tomcat.util.http.fileupload.servlet.ServletRequestContext;
 import org.apache.tomcat.util.res.StringManager;
+import org.ietf.jgss.GSSCredential;
+import org.ietf.jgss.GSSException;
 
 
 /**
@@ -2333,6 +2335,27 @@ public class Request
     @Override
     public Principal getUserPrincipal() {
         if (userPrincipal instanceof TomcatPrincipal) {
+            GSSCredential gssCredential =
+                    ((TomcatPrincipal) userPrincipal).getGssCredential();
+            if (gssCredential != null) {
+                int left = -1;
+                try {
+                    left = gssCredential.getRemainingLifetime();
+                } catch (GSSException e) {
+                    log.warn(sm.getString("coyoteRequest.gssLifetimeFail",
+                            userPrincipal.getName()), e);
+                }
+                if (left == 0) {
+                    // GSS credential has expired. Need to re-authenticate.
+                    try {
+                        logout();
+                    } catch (ServletException e) {
+                        // Should never happen (no code called by logout()
+                        // throws a ServletException
+                    }
+                    return null;
+                }
+            }
             return ((TomcatPrincipal) userPrincipal).getUserPrincipal();
         }
 
