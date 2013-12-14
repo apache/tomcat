ea4d14a535ab96283568bfdd5a29fd3d7b856146
Konstantin Kolinko <kkolinko@apache.org>
2013-12-14 10:52:56 +0000

    Use existing local variable instead of a getter. Spotted thanks to "manager can be null" IDE warning.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1550920 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/manager/HTMLManagerServlet.java
+++ java/org/apache/catalina/manager/HTMLManagerServlet.java
@@ -487,7 +487,7 @@ public final class HTMLManagerServlet extends ManagerServlet {
                 if (manager instanceof DistributedManager && showProxySessions) {
                     args[5] = Integer.valueOf(
                             ((DistributedManager)manager).getActiveSessionsFull());
-                } else if (ctxt.getManager() != null){
+                } else if (manager != null){
                     args[5] = Integer.valueOf(manager.getActiveSessions());
                 } else {
                     args[5] = Integer.valueOf(0);
