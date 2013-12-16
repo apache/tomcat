2b08fef845a457e3522e8c855e7f3baf68baaaa7
Mark Thomas <markt@apache.org>
2013-12-16 11:47:02 +0000

    Add a unit test for tag files packaged in JARs
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1551188 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test/org/apache/jasper/TestJspCpompilationContext.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jasper;
+
+import java.io.File;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.startup.TomcatBaseTest;
+import org.apache.tomcat.util.buf.ByteChunk;
+
+public class TestJspCpompilationContext extends TomcatBaseTest {
+
+    @Test
+    public void testTagFileInJar() throws Exception {
+        Tomcat tomcat = getTomcatInstance();
+
+        File appDir = new File("test/webapp");
+        // app dir is relative to server home
+        tomcat.addWebapp(null, "/test", appDir.getAbsolutePath());
+
+        tomcat.start();
+
+        ByteChunk body = new ByteChunk();
+
+        int rc = getUrl("http://localhost:" + getPort() +
+                "/test/jsp/tagFileInJar.jsp", body, null);
+
+        Assert.assertEquals(HttpServletResponse.SC_OK, rc);
+        Assert.assertTrue(body.toString().contains("00 - OK"));
+    }
+}
