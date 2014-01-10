6bb75385ff1bbc8edaf2b62e005b18b110e32419
Jeremy Boynes <jboynes@apache.org>
2014-01-10 03:08:28 +0000

    add tests for setting v1 cookie values
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1557017 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/tomcat/util/http/TestSetCookieSupport.java
+++ test/org/apache/tomcat/util/http/TestSetCookieSupport.java
@@ -33,6 +33,7 @@ public class TestSetCookieSupport {
     @Test
     public void v0NullValue() {
         Cookie cookie = new Cookie("foo", null);
+        // should this throw an IAE?
 //        Assert.assertEquals("foo=", SetCookieSupport.generateHeader(cookie));
         Assert.assertEquals("foo=\"\"", SetCookieSupport.generateHeader(cookie));
     }
@@ -107,6 +108,97 @@ public class TestSetCookieSupport {
     public void v0ValueContainsBackslashAndQuote() {
         Cookie cookie = new Cookie("foo", "a\"b\\c");
 //        Assert.assertEquals("foo=a\"b\\c", SetCookieSupport.generateHeader(cookie));
+        Assert.assertEquals("foo=\"a\\\"b\\\\c\"; Version=1", SetCookieSupport.generateHeader(cookie));
+    }
+
+    @Test
+    public void v1simpleCookie() {
+        Cookie cookie = new Cookie("foo", "bar");
+        cookie.setVersion(1);
+        Assert.assertEquals("foo=bar; Version=1", SetCookieSupport.generateHeader(cookie));
+    }
+
+    @Test
+    public void v1NullValue() {
+        Cookie cookie = new Cookie("foo", null);
+        cookie.setVersion(1);
+        // should this throw an IAE?
+        Assert.assertEquals("foo=\"\"; Version=1", SetCookieSupport.generateHeader(cookie));
+    }
+
+    @Test
+    public void v1QuotedValue() {
+        Cookie cookie = new Cookie("foo", "\"bar\"");
+        cookie.setVersion(1);
+        // should this be escaping the quotes rather than passing through?
+        Assert.assertEquals("foo=\"bar\"; Version=1", SetCookieSupport.generateHeader(cookie));
+    }
+
+    @Test
+    public void v1ValueContainsSemicolon() {
+        Cookie cookie = new Cookie("foo", "a;b");
+        cookie.setVersion(1);
+        // should this be throwing IAE rather than adding quotes?
+        Assert.assertEquals("foo=\"a;b\"; Version=1", SetCookieSupport.generateHeader(cookie));
+    }
+
+    @Test
+    public void v1ValueContainsComma() {
+        Cookie cookie = new Cookie("foo", "a,b");
+        cookie.setVersion(1);
+        // should this be throwing IAE rather than adding quotes?
+        Assert.assertEquals("foo=\"a,b\"; Version=1", SetCookieSupport.generateHeader(cookie));
+    }
+
+    @Test
+    public void v1ValueContainsSpace() {
+        Cookie cookie = new Cookie("foo", "a b");
+        cookie.setVersion(1);
+        // should this be throwing IAE rather than adding quotes?
+        Assert.assertEquals("foo=\"a b\"; Version=1", SetCookieSupport.generateHeader(cookie));
+    }
+
+    @Test
+    public void v1ValueContainsEquals() {
+        Cookie cookie = new Cookie("foo", "a=b");
+        cookie.setVersion(1);
+        // should this be throwing IAE rather than adding quotes?
+        Assert.assertEquals("foo=\"a=b\"; Version=1", SetCookieSupport.generateHeader(cookie));
+    }
+
+    @Test
+    public void v1ValueContainsQuote() {
+        Cookie cookie = new Cookie("foo", "a\"b");
+        cookie.setVersion(1);
+        // should this be throwing IAE rather than adding quotes?
+        Assert.assertEquals("foo=\"a\\\"b\"; Version=1", SetCookieSupport.generateHeader(cookie));
+    }
+
+    @Ignore("bug 55975")
+    @Test
+    public void v1ValueContainsNonV0Separator() {
+        Cookie cookie = new Cookie("foo", "a()<>@,;:\\\"/[]?={}b");
+        cookie.setVersion(1);
+        // should this be throwing IAE rather than adding quotes?
+        Assert.assertEquals("foo=\"a()<>@,;:\\\\\\\"/[]?={}b\"; Version=1", SetCookieSupport.generateHeader(cookie));
+    }
+
+    @Ignore("bug 55975")
+    @Test
+    public void v1ValueContainsBackslash() {
+        Cookie cookie = new Cookie("foo", "a\\b");
+        cookie.setVersion(1);
+        // should this be throwing IAE rather than adding quotes?
+        Assert.assertEquals("foo=\"a\\\\b\"; Version=1", SetCookieSupport.generateHeader(cookie));
+    }
+
+
+    @Ignore("bug 55975")
+    @Test
+    public void v1ValueContainsBackslashAndQuote() {
+        Cookie cookie = new Cookie("foo", "a\"b\\c");
+        cookie.setVersion(1);
+        // should this be throwing IAE rather than adding quotes?
         Assert.assertEquals("foo=\"a\\\"b\\\\c\"; Version=1", SetCookieSupport.generateHeader(cookie));
     }
 }
--- test/org/apache/tomcat/util/http/TestSetCookieSupportSeparatorsAllowed.java
+++ test/org/apache/tomcat/util/http/TestSetCookieSupportSeparatorsAllowed.java
@@ -19,6 +19,7 @@ package org.apache.tomcat.util.http;
 import javax.servlet.http.Cookie;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class TestSetCookieSupportSeparatorsAllowed {
@@ -96,5 +97,98 @@ public class TestSetCookieSupportSeparatorsAllowed {
     public void v0ValueContainsBackslashAndQuote() {
         Cookie cookie = new Cookie("foo", "a\"b\\c");
         Assert.assertEquals("foo=a\"b\\c", SetCookieSupport.generateHeader(cookie));
+    }
+
+    @Test
+    public void v1simpleCookie() {
+        Cookie cookie = new Cookie("foo", "bar");
+        cookie.setVersion(1);
+        Assert.assertEquals("foo=bar; Version=1", SetCookieSupport.generateHeader(cookie));
+    }
+
+    @Test
+    public void v1NullValue() {
+        Cookie cookie = new Cookie("foo", null);
+        cookie.setVersion(1);
+        // should this throw an IAE?
+        Assert.assertEquals("foo=\"\"; Version=1", SetCookieSupport.generateHeader(cookie));
+    }
+
+    @Test
+    public void v1QuotedValue() {
+        Cookie cookie = new Cookie("foo", "\"bar\"");
+        cookie.setVersion(1);
+        // should this be escaping the quotes rather than passing through?
+        Assert.assertEquals("foo=\"bar\"; Version=1", SetCookieSupport.generateHeader(cookie));
+    }
+
+    @Test
+    public void v1ValueContainsSemicolon() {
+        Cookie cookie = new Cookie("foo", "a;b");
+        cookie.setVersion(1);
+        // should this be throwing IAE rather than adding quotes?
+        Assert.assertEquals("foo=\"a;b\"; Version=1", SetCookieSupport.generateHeader(cookie));
+    }
+
+    @Test
+    public void v1ValueContainsComma() {
+        Cookie cookie = new Cookie("foo", "a,b");
+        cookie.setVersion(1);
+        // should this be throwing IAE rather than adding quotes?
+        Assert.assertEquals("foo=\"a,b\"; Version=1", SetCookieSupport.generateHeader(cookie));
+    }
+
+    @Test
+    public void v1ValueContainsSpace() {
+        Cookie cookie = new Cookie("foo", "a b");
+        cookie.setVersion(1);
+        // should this be throwing IAE rather than adding quotes?
+        Assert.assertEquals("foo=\"a b\"; Version=1", SetCookieSupport.generateHeader(cookie));
+    }
+
+    @Ignore("bug 55984")
+    @Test
+    public void v1ValueContainsEquals() {
+        Cookie cookie = new Cookie("foo", "a=b");
+        cookie.setVersion(1);
+        // should this be throwing IAE rather than adding quotes?
+        Assert.assertEquals("foo=\"a=b\"; Version=1", SetCookieSupport.generateHeader(cookie));
+    }
+
+    @Ignore("bug 55984")
+    @Test
+    public void v1ValueContainsQuote() {
+        Cookie cookie = new Cookie("foo", "a\"b");
+        cookie.setVersion(1);
+        // should this be throwing IAE rather than adding quotes?
+        Assert.assertEquals("foo=\"a\\\"b\"; Version=1", SetCookieSupport.generateHeader(cookie));
+    }
+
+    @Ignore("bug 55975")
+    @Test
+    public void v1ValueContainsNonV0Separator() {
+        Cookie cookie = new Cookie("foo", "a()<>@,;:\\\"/[]?={}b");
+        cookie.setVersion(1);
+        // should this be throwing IAE rather than adding quotes?
+        Assert.assertEquals("foo=\"a()<>@,;:\\\\\\\"/[]?={}b\"; Version=1", SetCookieSupport.generateHeader(cookie));
+    }
+
+    @Ignore("bug 55975")
+    @Test
+    public void v1ValueContainsBackslash() {
+        Cookie cookie = new Cookie("foo", "a\\b");
+        cookie.setVersion(1);
+        // should this be throwing IAE rather than adding quotes?
+        Assert.assertEquals("foo=\"a\\\\b\"; Version=1", SetCookieSupport.generateHeader(cookie));
+    }
+
+
+    @Ignore("bug 55975")
+    @Test
+    public void v1ValueContainsBackslashAndQuote() {
+        Cookie cookie = new Cookie("foo", "a\"b\\c");
+        cookie.setVersion(1);
+        // should this be throwing IAE rather than adding quotes?
+        Assert.assertEquals("foo=\"a\\\"b\\\\c\"; Version=1", SetCookieSupport.generateHeader(cookie));
     }
 }
