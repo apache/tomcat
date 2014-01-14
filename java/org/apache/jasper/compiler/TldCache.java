52f85c10427fd9218e2c37e1d512dcdc0d5156f3
Mark Thomas <markt@apache.org>
2014-01-14 12:04:41 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=55995 Fix NPE when using JspC and there are JARs containing TLDs on the classpath
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1558019 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/jasper/compiler/TldCache.java
+++ java/org/apache/jasper/compiler/TldCache.java
@@ -121,14 +121,19 @@ public class TldCache {
         result[0] = -1;
         result[1] = -1;
         try {
-            URL url = servletContext.getResource(tldResourcePath.getWebappPath());
-            URLConnection conn = url.openConnection();
-            result[0] = conn.getLastModified();
-            if ("file".equals(url.getProtocol())) {
-                // Reading the last modified time opens an input stream so we
-                // need to make sure it is closed again otherwise the TLD file
-                // will be locked until GC runs.
-                conn.getInputStream().close();
+            String webappPath = tldResourcePath.getWebappPath();
+            if (webappPath != null) {
+                // webappPath will be null for JARs containing TLDs that are on
+                // the class path but not part of the web application
+                URL url = servletContext.getResource(tldResourcePath.getWebappPath());
+                URLConnection conn = url.openConnection();
+                result[0] = conn.getLastModified();
+                if ("file".equals(url.getProtocol())) {
+                    // Reading the last modified time opens an input stream so we
+                    // need to make sure it is closed again otherwise the TLD file
+                    // will be locked until GC runs.
+                    conn.getInputStream().close();
+                }
             }
             Jar jar = tldResourcePath.getJar();
             if (jar != null) {
