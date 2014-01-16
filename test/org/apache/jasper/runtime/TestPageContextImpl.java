40403e9a62b4ad50398bec490cc376179b21a6c0
Mark Thomas <markt@apache.org>
2014-01-16 14:21:17 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=56010 Don't throw an IllegalArgumentException when JspFactory.getPageContext is used with JspWriter.DEFAULT_BUFFER Based on a patch by Eugene Chung. (markt)
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1558811 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/jasper/runtime/TestPageContextImpl.java
+++ test/org/apache/jasper/runtime/TestPageContextImpl.java
@@ -17,15 +17,24 @@
 package org.apache.jasper.runtime;
 
 import java.io.File;
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.jsp.JspFactory;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.PageContext;
 
 import org.junit.Assert;
 
 import org.junit.Test;
 
+import org.apache.catalina.Context;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.catalina.startup.TomcatBaseTest;
+import org.apache.jasper.Constants;
 import org.apache.tomcat.util.buf.ByteChunk;
 
 public class TestPageContextImpl extends TomcatBaseTest {
@@ -49,5 +58,45 @@ public class TestPageContextImpl extends TomcatBaseTest {
         String body = res.toString();
         Assert.assertTrue(body.contains("OK"));
         Assert.assertFalse(body.contains("FAIL"));
+    }
+
+    @Test
+    public void testDefaultBufferSize() throws Exception {
+        Tomcat tomcat = getTomcatInstance();
+
+        File appDir = new File("test/webapp");
+        // app dir is relative to server home
+        Context ctx = tomcat.addWebapp(null, "/test", appDir.getAbsolutePath());
+
+        // Add the Servlet
+        Tomcat.addServlet(ctx, "bug56010", new Bug56010());
+        ctx.addServletMapping("/bug56010", "bug56010");
+
+        tomcat.start();
+
+        ByteChunk res = getUrl("http://localhost:" + getPort() + "/test/bug56010");
+
+        String result = res.toString();
+        Assert.assertTrue(result.contains("OK"));
+    }
+
+    public static class Bug56010 extends HttpServlet {
+
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+                throws ServletException, IOException {
+
+            PageContext pageContext = JspFactory.getDefaultFactory().getPageContext(
+                    this, req, resp, null, false, JspWriter.DEFAULT_BUFFER, true);
+            JspWriter out = pageContext.getOut();
+            if (Constants.DEFAULT_BUFFER_SIZE == out.getBufferSize()) {
+                resp.getWriter().println("OK");
+            } else {
+                resp.getWriter().println("FAIL");
+            }
+        }
+
     }
 }
