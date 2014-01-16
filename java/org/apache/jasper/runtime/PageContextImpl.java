48650dd3970af2e24ffb0a81b631eed0d7e3a406
Mark Emlyn David Thomas <markt@apache.org>
2014-01-16 14:21:17 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=56010 Don't throw an IllegalArgumentException when JspFactory.getPageContext is used with JspWriter.DEFAULT_BUFFER Based on a patch by Eugene Chung. (markt)
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1558811 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/jasper/runtime/PageContextImpl.java
+++ java/org/apache/jasper/runtime/PageContextImpl.java
@@ -47,6 +47,7 @@ import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.PageContext;
 import javax.servlet.jsp.tagext.BodyContent;
 
+import org.apache.jasper.Constants;
 import org.apache.jasper.compiler.Localizer;
 import org.apache.jasper.el.ELContextImpl;
 import org.apache.jasper.security.SecurityUtil;
@@ -137,6 +138,9 @@ public class PageContextImpl extends PageContext {
 
         // initialize the initial out ...
         depth = -1;
+        if (bufferSize == JspWriter.DEFAULT_BUFFER) {
+            bufferSize = Constants.DEFAULT_BUFFER_SIZE;
+        }
         if (this.baseOut == null) {
             this.baseOut = new JspWriterImpl(response, bufferSize, autoFlush);
         } else {
