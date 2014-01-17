2b22011d9b9e429534d74fd578a8ffcc53581f2c
Mark Emlyn David Thomas <markt@apache.org>
2014-01-17 14:30:34 +0000

    Remove setParentClassLoader() as it could result in odd behaviour if the class loader is constructed with one parent and then this method is called to change the parent. This class will have visibility of the new parent but any code inherited from the super class will still see the old parent.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1559113 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/loader/WebappClassLoader.java
+++ java/org/apache/catalina/loader/WebappClassLoader.java
@@ -513,7 +513,10 @@ public class WebappClassLoader extends URLClassLoader
      /**
       * Utility method for use in subclasses.
       * Must be called before Lifecycle methods to have any effect.
+      *
+      * @deprecated Will be removed in 8.0.x onwards.
       */
+    @Deprecated
      protected void setParentClassLoader(ClassLoader pcl) {
          parent = pcl;
      }
