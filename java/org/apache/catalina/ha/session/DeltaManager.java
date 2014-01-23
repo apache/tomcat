1857f2ad3c1d07b5fa48e436fbad3d6f1a75986e
Mark Thomas <markt@apache.org>
2014-01-23 11:46:30 +0000

    Use try with resoucres
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1560646 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/ha/session/DeltaManager.java
+++ java/org/apache/catalina/ha/session/DeltaManager.java
@@ -647,10 +647,8 @@ public class DeltaManager extends ClusterManagerBase{
     protected void deserializeSessions(byte[] data) throws ClassNotFoundException,IOException {
 
         // Open an input stream to the specified pathname, if any
-        ObjectInputStream ois = null;
         // Load the previously unloaded active sessions
-        try {
-            ois = getReplicationStream(data);
+        try (ObjectInputStream ois = getReplicationStream(data)) {
             Integer count = (Integer) ois.readObject();
             int n = count.intValue();
             for (int i = 0; i < n; i++) {
@@ -691,14 +689,6 @@ public class DeltaManager extends ClusterManagerBase{
         } catch (IOException e) {
             log.error(sm.getString("deltaManager.loading.ioe", e), e);
             throw e;
-        } finally {
-            // Close the input stream
-            try {
-                if (ois != null) ois.close();
-            } catch (IOException f) {
-                // ignored
-            }
-            ois = null;
         }
     }
 
@@ -714,12 +704,8 @@ public class DeltaManager extends ClusterManagerBase{
     protected byte[] serializeSessions(Session[] currentSessions) throws IOException {
 
         // Open an output stream to the specified pathname, if any
-        ByteArrayOutputStream fos = null;
-        ObjectOutputStream oos = null;
-
-        try {
-            fos = new ByteArrayOutputStream();
-            oos = new ObjectOutputStream(new BufferedOutputStream(fos));
+        ByteArrayOutputStream fos = new ByteArrayOutputStream();
+        try (ObjectOutputStream oos = new ObjectOutputStream(new BufferedOutputStream(fos))) {
             oos.writeObject(Integer.valueOf(currentSessions.length));
             for(int i=0 ; i < currentSessions.length;i++) {
                 ((DeltaSession)currentSessions[i]).writeObjectData(oos);
@@ -729,16 +715,8 @@ public class DeltaManager extends ClusterManagerBase{
         } catch (IOException e) {
             log.error(sm.getString("deltaManager.unloading.ioe", e), e);
             throw e;
-        } finally {
-            if (oos != null) {
-                try {
-                    oos.close();
-                } catch (IOException f) {
-                    // Ignore
-                }
-                oos = null;
-            }
         }
+
         // send object data as byte[]
         return fos.toByteArray();
     }
