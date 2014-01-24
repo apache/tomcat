c675d20f8afb9f857981873f85dd0bfac2242dab
Konstantin Kolinko <kkolinko@apache.org>
2014-01-24 16:43:52 +0000

    Remove svn keywords from test/**
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1561054 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/catalina/startup/DuplicateMappingParamServlet.java
+++ test/org/apache/catalina/startup/DuplicateMappingParamServlet.java
@@ -30,7 +30,6 @@ import javax.servlet.http.HttpServletResponse;
  * Test Mock with wrong Annotation!
  *
  * @author Peter Rossbach
- * @version $Revision$
  */
 @WebServlet(value = "/annotation/overwrite", urlPatterns = { "/param2" }, name = "param", initParams = {
         @WebInitParam(name = "foo", value = "Hello"),
