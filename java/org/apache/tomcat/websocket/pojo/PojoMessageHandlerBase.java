e52ad228656f4983f7fadc4a40a813dd3c5134eb
Mark Thomas <markt@apache.org>
2014-01-27 10:05:14 +0000

    Add a TODO to remind me to come back and look at this
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1561623 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/websocket/pojo/PojoMessageHandlerBase.java
+++ java/org/apache/tomcat/websocket/pojo/PojoMessageHandlerBase.java
@@ -49,6 +49,8 @@ public abstract class PojoMessageHandlerBase<T>
             int indexSession, long maxMessageSize) {
         this.pojo = pojo;
         this.method = method;
+        // TODO: The method should already accessible here but the following
+        // code seems to be necessary in some as yet not fully understood cases.
         try {
             this.method.setAccessible(true);
         } catch (Exception e) {
