2a6c5a84435087b318be2622e5e5b659fc920b9e
Mark Emlyn David Thomas <markt@apache.org>
2014-02-12 14:36:26 +0000

    In some circumstances asynchronous requests could time out too soon.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1567634 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http11/AbstractHttp11Processor.java
+++ java/org/apache/coyote/http11/AbstractHttp11Processor.java
@@ -795,6 +795,8 @@ public abstract class AbstractHttp11Processor<S> extends AbstractProcessor<S> {
             internalBuffer.addActiveFilter(savedBody);
         } else if (actionCode == ActionCode.ASYNC_START) {
             asyncStateMachine.asyncStart((AsyncContextCallback) param);
+            // Async time out is based on SocketWrapper access time
+            getSocketWrapper().access();
         } else if (actionCode == ActionCode.ASYNC_DISPATCHED) {
             asyncStateMachine.asyncDispatched();
         } else if (actionCode == ActionCode.ASYNC_TIMEOUT) {
