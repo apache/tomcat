f45f08a360037700189140e5400a2a61e8efdac7
Konstantin Kolinko <kkolinko@apache.org>
2014-02-17 09:38:03 +0000

    Merged revisions r1568779-r1568780, r1568828 from tomcat/trunk:
    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=56143
    Allow service.bat to be launched from a non-UAC console,
    by using arguments instead of env variables to pass options to prunsrv.
    
    Fix regression in r1567742 that values for --StdOutput, --StdError were lost.
    
    Pass --LogPath option to service removal command, to avoid using default value for this option 
    (service uninstallation logs were written to %SystemRoot%\system32\LogFiles\Apache).
    
    Align Commons Daemon options between service.bat and exe installer:
    service.bat changes:
    --DisplayName
    --StartPath, --StopPath (They should be unnecessary in --StartMode jvm, but exe installed sets them and they are visible and settable in configuration dialog)
    --Classpath: Reference tomcat-juli.jar only once when home==base.
    --JvmOptions: Just reordered.
    
    exe installer changes:
    --JvmMs, --JvmMx
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1568920 13f79535-47bb-0310-9956-ffa450edef68

--- bin/service.bat
+++ bin/service.bat
@@ -75,7 +75,7 @@ set "EXECUTABLE=%CATALINA_HOME%\bin\tomcat@VERSION_MAJOR@.exe"
 
 rem Set default Service name
 set SERVICE_NAME=Tomcat@VERSION_MAJOR@
-set PR_DISPLAYNAME=Apache Tomcat @VERSION_MAJOR@
+set DISPLAYNAME=Apache Tomcat @VERSION_MAJOR@ %SERVICE_NAME%
 
 if "x%1x" == "xx" goto displayUsage
 set SERVICE_CMD=%1
@@ -85,7 +85,7 @@ if "x%1x" == "xx" goto checkServiceCmd
 if "x%1x" == "x/userx" goto runAsUser
 if "x%1x" == "x--userx" goto runAsUser
 set SERVICE_NAME=%1
-set PR_DISPLAYNAME=Apache Tomcat %1
+set DISPLAYNAME=Apache Tomcat @VERSION_MAJOR@ %1
 shift
 if "x%1x" == "xx" goto checkServiceCmd
 goto checkUser
@@ -108,7 +108,11 @@ goto end
 
 :doRemove
 rem Remove the service
-"%EXECUTABLE%" //DS//%SERVICE_NAME%
+echo Removing the service '%SERVICE_NAME%' ...
+echo Using CATALINA_BASE:    "%CATALINA_BASE%"
+
+"%EXECUTABLE%" //DS//%SERVICE_NAME% ^
+    --LogPath "%CATALINA_BASE%\logs"
 if not errorlevel 1 goto removed
 echo Failed removing '%SERVICE_NAME%' service
 goto end
@@ -124,45 +128,43 @@ echo Using CATALINA_BASE:    "%CATALINA_BASE%"
 echo Using JAVA_HOME:        "%JAVA_HOME%"
 echo Using JRE_HOME:         "%JRE_HOME%"
 
-rem Use the environment variables as an example
-rem Each command line option is prefixed with PR_
-
-set PR_DESCRIPTION=Apache Tomcat @VERSION@ Server - http://tomcat.apache.org/
-set "PR_INSTALL=%EXECUTABLE%"
-set "PR_LOGPATH=%CATALINA_BASE%\logs"
-set "PR_CLASSPATH=%CATALINA_HOME%\bin\bootstrap.jar;%CATALINA_BASE%\bin\tomcat-juli.jar;%CATALINA_HOME%\bin\tomcat-juli.jar"
 rem Set the server jvm from JAVA_HOME
-set "PR_JVM=%JRE_HOME%\bin\server\jvm.dll"
-if exist "%PR_JVM%" goto foundJvm
+set "JVM=%JRE_HOME%\bin\server\jvm.dll"
+if exist "%JVM%" goto foundJvm
 rem Set the client jvm from JAVA_HOME
-set "PR_JVM=%JRE_HOME%\bin\client\jvm.dll"
-if exist "%PR_JVM%" goto foundJvm
-set PR_JVM=auto
-set PR_STDOUTPUT=auto
-set PR_STDERROR=auto
+set "JVM=%JRE_HOME%\bin\client\jvm.dll"
+if exist "%JVM%" goto foundJvm
+set JVM=auto
 :foundJvm
-echo Using JVM:              "%PR_JVM%"
+echo Using JVM:              "%JVM%"
+
+set "CLASSPATH=%CATALINA_HOME%\bin\bootstrap.jar;%CATALINA_BASE%\bin\tomcat-juli.jar"
+if not "%CATALINA_HOME%" == "%CATALINA_BASE%" set "CLASSPATH=%CLASSPATH%;%CATALINA_HOME%\bin\tomcat-juli.jar"
+
 "%EXECUTABLE%" //IS//%SERVICE_NAME% ^
+    --Description "Apache Tomcat @VERSION@ Server - http://tomcat.apache.org/" ^
+    --DisplayName "%DISPLAYNAME%" ^
+    --Install "%EXECUTABLE%" ^
+    --LogPath "%CATALINA_BASE%\logs" ^
+    --StdOutput auto ^
+    --StdError auto ^
+    --Classpath "%CLASSPATH%" ^
+    --Jvm "%JVM%" ^
+    --StartMode jvm ^
+    --StopMode jvm ^
+    --StartPath "%CATALINA_HOME%" ^
+    --StopPath "%CATALINA_HOME%" ^
     --StartClass org.apache.catalina.startup.Bootstrap ^
     --StopClass org.apache.catalina.startup.Bootstrap ^
     --StartParams start ^
     --StopParams stop ^
-    --JvmOptions "-Dcatalina.base=%CATALINA_BASE%;-Dcatalina.home=%CATALINA_HOME%;-Djava.endorsed.dirs=%CATALINA_HOME%\endorsed;-Djava.io.tmpdir=%CATALINA_BASE%\temp;-Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager;-Djava.util.logging.config.file=%CATALINA_BASE%\conf\logging.properties" ^
-    --StartMode jvm ^
-    --StopMode jvm ^
+    --JvmOptions "-Dcatalina.home=%CATALINA_HOME%;-Dcatalina.base=%CATALINA_BASE%;-Djava.endorsed.dirs=%CATALINA_HOME%\endorsed;-Djava.io.tmpdir=%CATALINA_BASE%\temp;-Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager;-Djava.util.logging.config.file=%CATALINA_BASE%\conf\logging.properties" ^
     --JvmMs 128 ^
     --JvmMx 256
 if not errorlevel 1 goto installed
 echo Failed installing '%SERVICE_NAME%' service
 goto end
 :installed
-rem Clear the environment variables. They are not needed any more.
-set PR_DISPLAYNAME=
-set PR_DESCRIPTION=
-set PR_INSTALL=
-set PR_LOGPATH=
-set PR_CLASSPATH=
-set PR_JVM=
 echo The service '%SERVICE_NAME%' has been installed.
 
 :end
