27ac1263bf25a6af9175afe772416e39b1f51f6a
Remy Maucherat <remm@apache.org>
2014-03-10 11:27:11 +0000

    Add experimental NIO2 connector. Based on code developed by Nabil Benothman.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1575905 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/org/apache/coyote/ajp/AjpNio2Processor.java
@@ -0,0 +1,269 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.coyote.ajp;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.CompletionHandler;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+import org.apache.tomcat.util.net.Nio2Channel;
+import org.apache.tomcat.util.net.Nio2Endpoint;
+import org.apache.tomcat.util.net.SocketStatus;
+import org.apache.tomcat.util.net.SocketWrapper;
+
+/**
+ * Processes AJP requests using NIO2.
+ */
+public class AjpNio2Processor extends AbstractAjpProcessor<Nio2Channel> {
+
+    private static final Log log = LogFactory.getLog(AjpNio2Processor.class);
+    @Override
+    protected Log getLog() {
+        return log;
+    }
+
+    /**
+     * The completion handler used for asynchronous write operations
+     */
+    protected CompletionHandler<Integer, SocketWrapper<Nio2Channel>> writeCompletionHandler;
+
+    /**
+     * Flipped flag for read buffer.
+     */
+    protected boolean flipped = false;
+
+    /**
+     * Write pending flag.
+     */
+    protected volatile boolean writePending = false;
+
+    public AjpNio2Processor(int packetSize, Nio2Endpoint endpoint0) {
+        super(packetSize, endpoint0);
+        response.setOutputBuffer(new SocketOutputBuffer());
+        this.writeCompletionHandler = new CompletionHandler<Integer, SocketWrapper<Nio2Channel>>() {
+            @Override
+            public void completed(Integer nBytes, SocketWrapper<Nio2Channel> attachment) {
+                boolean notify = false;
+                synchronized (writeCompletionHandler) {
+                    if (nBytes < 0) {
+                        failed(new IOException(sm.getString("ajpprocessor.failedsend")), attachment);
+                        return;
+                    }
+                    writePending = false;
+                    if (!Nio2Endpoint.isInline()) {
+                        notify = true;
+                    }
+                }
+                if (notify) {
+                    endpoint.processSocket(attachment, SocketStatus.OPEN_WRITE, false);
+                }
+            }
+            @Override
+            public void failed(Throwable exc, SocketWrapper<Nio2Channel> attachment) {
+                attachment.setError(true);
+                writePending = false;
+                endpoint.processSocket(attachment, SocketStatus.DISCONNECT, true);
+            }
+        };
+    }
+
+    @Override
+    public void recycle(boolean socketClosing) {
+        super.recycle(socketClosing);
+        writePending = false;
+        flipped = false;
+    }
+
+    @Override
+    protected void registerForEvent(boolean read, boolean write) {
+        final Nio2Endpoint.Nio2SocketWrapper attach =
+                (Nio2Endpoint.Nio2SocketWrapper) socketWrapper;
+        if (attach == null) {
+            return;
+        }
+        if (read) {
+            attach.interestOps(attach.interestOps() | Nio2Endpoint.OP_READ);
+        }
+        if (write) {
+            attach.interestOps(attach.interestOps() | Nio2Endpoint.OP_WRITE);
+        }
+    }
+
+    @Override
+    protected void resetTimeouts() {
+        // The NIO connector uses the timeout configured on the wrapper in the
+        // poller. Therefore, it needs to be reset once asycn processing has
+        // finished.
+        if (!error && socketWrapper != null &&
+                asyncStateMachine.isAsyncDispatching()) {
+            long soTimeout = endpoint.getSoTimeout();
+
+            //reset the timeout
+            if (keepAliveTimeout > 0) {
+                socketWrapper.setTimeout(keepAliveTimeout);
+            } else {
+                socketWrapper.setTimeout(soTimeout);
+            }
+        }
+
+    }
+
+
+    @Override
+    protected void setupSocket(SocketWrapper<Nio2Channel> socketWrapper)
+            throws IOException {
+        // NO-OP
+    }
+
+
+    @Override
+    protected void setTimeout(SocketWrapper<Nio2Channel> socketWrapper,
+            int timeout) throws IOException {
+        socketWrapper.setTimeout(timeout);
+    }
+
+
+    @Override
+    protected int output(byte[] src, int offset, int length, boolean block)
+            throws IOException {
+
+        if (socketWrapper == null || socketWrapper.getSocket() == null)
+            return -1;
+
+        ByteBuffer writeBuffer =
+                socketWrapper.getSocket().getBufHandler().getWriteBuffer();
+
+        int result = 0;
+        if (block) {
+            writeBuffer.clear();
+            writeBuffer.put(src, offset, length);
+            writeBuffer.flip();
+            try {
+                result = socketWrapper.getSocket().write(writeBuffer).get(socketWrapper.getTimeout(), TimeUnit.MILLISECONDS);
+            } catch (InterruptedException | ExecutionException
+                    | TimeoutException e) {
+                throw new IOException(sm.getString("ajpprocessor.failedsend"), e);
+            }
+        } else {
+            synchronized (writeCompletionHandler) {
+                if (!writePending) {
+                    writeBuffer.clear();
+                    writeBuffer.put(src, offset, length);
+                    writeBuffer.flip();
+                    writePending = true;
+                    Nio2Endpoint.startInline();
+                    socketWrapper.getSocket().write(writeBuffer, socketWrapper.getTimeout(),
+                            TimeUnit.MILLISECONDS, socketWrapper, writeCompletionHandler);
+                    Nio2Endpoint.endInline();
+                    result = length;
+                }
+            }
+        }
+        return result;
+    }
+
+
+    @Override
+    protected boolean read(byte[] buf, int pos, int n, boolean blockFirstRead)
+        throws IOException {
+
+        int read = 0;
+        int res = 0;
+        boolean block = blockFirstRead;
+
+        while (read < n) {
+            res = readSocket(buf, read + pos, n - read, block);
+            if (res > 0) {
+                read += res;
+            } else if (res == 0 && !block) {
+                return false;
+            } else {
+                throw new IOException(sm.getString("ajpprocessor.failedread"));
+            }
+            block = true;
+        }
+        return true;
+    }
+
+
+    private int readSocket(byte[] buf, int pos, int n, boolean block)
+            throws IOException {
+        int nRead = 0;
+        ByteBuffer readBuffer =
+                socketWrapper.getSocket().getBufHandler().getReadBuffer();
+
+        if (block) {
+            if (!flipped) {
+                readBuffer.flip();
+                flipped = true;
+            }
+            if (readBuffer.remaining() > 0) {
+                nRead = Math.min(n, readBuffer.remaining());
+                readBuffer.get(buf, pos, nRead);
+                if (readBuffer.remaining() == 0) {
+                    readBuffer.clear();
+                    flipped = false;
+                }
+            } else {
+                readBuffer.clear();
+                flipped = false;
+                readBuffer.limit(n);
+                try {
+                    nRead = socketWrapper.getSocket().read(readBuffer).get(socketWrapper.getTimeout(), TimeUnit.MILLISECONDS);
+                } catch (InterruptedException | ExecutionException
+                        | TimeoutException e) {
+                    throw new IOException(sm.getString("ajpprocessor.failedread"), e);
+                }
+                if (nRead > 0) {
+                    if (!flipped) {
+                        readBuffer.flip();
+                        flipped = true;
+                    }
+                    nRead = Math.min(n, readBuffer.remaining());
+                    readBuffer.get(buf, pos, nRead);
+                    if (readBuffer.remaining() == 0) {
+                        readBuffer.clear();
+                        flipped = false;
+                    }
+                }
+            }
+        } else {
+            if (!flipped) {
+                readBuffer.flip();
+                flipped = true;
+            }
+            if (readBuffer.remaining() > 0) {
+                nRead = Math.min(n, readBuffer.remaining());
+                readBuffer.get(buf, pos, nRead);
+                if (readBuffer.remaining() == 0) {
+                    readBuffer.clear();
+                    flipped = false;
+                }
+            } else {
+                readBuffer.clear();
+                flipped = false;
+                readBuffer.limit(n);
+            }
+        }
+        return nRead;
+    }
+}
---
+++ java/org/apache/coyote/http11/Http11Nio2Protocol.java
@@ -0,0 +1,278 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.coyote.http11;
+
+import java.io.IOException;
+
+import javax.net.ssl.SSLEngine;
+import javax.servlet.http.HttpUpgradeHandler;
+
+import org.apache.coyote.AbstractProtocol;
+import org.apache.coyote.Processor;
+import org.apache.coyote.http11.upgrade.Nio2Processor;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+import org.apache.tomcat.util.net.AbstractEndpoint;
+import org.apache.tomcat.util.net.Nio2Channel;
+import org.apache.tomcat.util.net.Nio2Endpoint;
+import org.apache.tomcat.util.net.Nio2Endpoint.Handler;
+import org.apache.tomcat.util.net.Nio2Endpoint.Nio2SocketWrapper;
+import org.apache.tomcat.util.net.SSLImplementation;
+import org.apache.tomcat.util.net.SecureNio2Channel;
+import org.apache.tomcat.util.net.SocketStatus;
+import org.apache.tomcat.util.net.SocketWrapper;
+
+
+/**
+ * HTTP/1.1 protocol implementation using NIO2.
+ */
+public class Http11Nio2Protocol extends AbstractHttp11JsseProtocol<Nio2Channel> {
+
+    private static final Log log = LogFactory.getLog(Http11Nio2Protocol.class);
+
+
+    @Override
+    protected Log getLog() { return log; }
+
+
+    @Override
+    protected AbstractEndpoint.Handler getHandler() {
+        return cHandler;
+    }
+
+
+    public Http11Nio2Protocol() {
+        endpoint=new Nio2Endpoint();
+        cHandler = new Http11ConnectionHandler(this);
+        ((Nio2Endpoint) endpoint).setHandler(cHandler);
+        setSoLinger(Constants.DEFAULT_CONNECTION_LINGER);
+        setSoTimeout(Constants.DEFAULT_CONNECTION_TIMEOUT);
+        setTcpNoDelay(Constants.DEFAULT_TCP_NO_DELAY);
+    }
+
+
+    public Nio2Endpoint getEndpoint() {
+        return ((Nio2Endpoint)endpoint);
+    }
+
+    @Override
+    public void start() throws Exception {
+        super.start();
+        if (npnHandler != null) {
+            npnHandler.init(getEndpoint(), 0, getAdapter());
+        }
+    }
+
+    // -------------------- Properties--------------------
+
+    private final Http11ConnectionHandler cHandler;
+
+    // -------------------- Pool setup --------------------
+
+    public void setAcceptorThreadPriority(int threadPriority) {
+        ((Nio2Endpoint)endpoint).setAcceptorThreadPriority(threadPriority);
+    }
+
+    public void setPollerThreadPriority(int threadPriority) {
+        ((Nio2Endpoint)endpoint).setPollerThreadPriority(threadPriority);
+    }
+
+    public int getAcceptorThreadPriority() {
+      return ((Nio2Endpoint)endpoint).getAcceptorThreadPriority();
+    }
+
+    public int getPollerThreadPriority() {
+      return ((Nio2Endpoint)endpoint).getThreadPriority();
+    }
+
+    public boolean getUseSendfile() {
+        return endpoint.getUseSendfile();
+    }
+
+    public void setUseSendfile(boolean useSendfile) {
+        ((Nio2Endpoint)endpoint).setUseSendfile(useSendfile);
+    }
+
+    // -------------------- Tcp setup --------------------
+
+    public void setOomParachute(int oomParachute) {
+        ((Nio2Endpoint)endpoint).setOomParachute(oomParachute);
+    }
+
+    // ----------------------------------------------------- JMX related methods
+
+    @Override
+    protected String getNamePrefix() {
+        return ("http-nio2");
+    }
+
+
+    // --------------------  Connection handler --------------------
+
+    protected static class Http11ConnectionHandler
+            extends AbstractConnectionHandler<Nio2Channel,Http11Nio2Processor>
+            implements Handler {
+
+        protected Http11Nio2Protocol proto;
+
+        Http11ConnectionHandler(Http11Nio2Protocol proto) {
+            this.proto = proto;
+        }
+
+        @Override
+        protected AbstractProtocol<Nio2Channel> getProtocol() {
+            return proto;
+        }
+
+        @Override
+        protected Log getLog() {
+            return log;
+        }
+
+
+        @Override
+        public SSLImplementation getSslImplementation() {
+            return proto.sslImplementation;
+        }
+
+        /**
+         * Expected to be used by the Poller to release resources on socket
+         * close, errors etc.
+         */
+        @Override
+        public void release(SocketWrapper<Nio2Channel> socket) {
+            Processor<Nio2Channel> processor =
+                connections.remove(socket.getSocket());
+            if (processor != null) {
+                processor.recycle(true);
+                recycledProcessors.push(processor);
+            }
+        }
+
+        @Override
+        public SocketState process(SocketWrapper<Nio2Channel> socket,
+                SocketStatus status) {
+            if (proto.npnHandler != null) {
+                SocketState ss = proto.npnHandler.process(socket, status);
+                if (ss != SocketState.OPEN) {
+                    return ss;
+                }
+            }
+            return super.process(socket, status);
+        }
+
+
+        /**
+         * Expected to be used by the handler once the processor is no longer
+         * required.
+         *
+         * @param socket
+         * @param processor
+         * @param isSocketClosing   Not used in HTTP
+         * @param addToPoller
+         */
+        @Override
+        public void release(SocketWrapper<Nio2Channel> socket,
+                Processor<Nio2Channel> processor, boolean isSocketClosing,
+                boolean addToPoller) {
+            processor.recycle(isSocketClosing);
+            recycledProcessors.push(processor);
+            if (socket.isAsync()) {
+                ((Nio2Endpoint) proto.endpoint).removeTimeout(socket);
+            }
+            if (addToPoller) {
+                ((Nio2Endpoint) proto.endpoint).awaitBytes(socket);
+            }
+        }
+
+
+        @Override
+        protected void initSsl(SocketWrapper<Nio2Channel> socket,
+                Processor<Nio2Channel> processor) {
+            if (proto.isSSLEnabled() &&
+                    (proto.sslImplementation != null)
+                    && (socket.getSocket() instanceof SecureNio2Channel)) {
+                SecureNio2Channel ch = (SecureNio2Channel)socket.getSocket();
+                processor.setSslSupport(
+                        proto.sslImplementation.getSSLSupport(
+                                ch.getSslEngine().getSession()));
+            } else {
+                processor.setSslSupport(null);
+            }
+
+        }
+
+        @Override
+        protected void longPoll(SocketWrapper<Nio2Channel> socket,
+                Processor<Nio2Channel> processor) {
+            if (processor.isAsync()) {
+                socket.setAsync(true);
+                ((Nio2Endpoint) proto.endpoint).addTimeout(socket);
+            } else if (processor.isUpgrade()) {
+                if (((Nio2SocketWrapper) socket).isUpgradeInit()) {
+                    ((Nio2Endpoint) proto.endpoint).awaitBytes(socket);
+                }
+            } else {
+                // Either:
+                //  - this is comet request
+                //  - this is an upgraded connection
+                //  - the request line/headers have not been completely
+                //    read
+                // The completion handlers should be in place,
+                // so nothing to do here
+            }
+        }
+
+        @Override
+        public Http11Nio2Processor createProcessor() {
+            Http11Nio2Processor processor = new Http11Nio2Processor(
+                    proto.getMaxHttpHeaderSize(), (Nio2Endpoint)proto.endpoint,
+                    proto.getMaxTrailerSize(), proto.getMaxExtensionSize());
+            processor.setAdapter(proto.getAdapter());
+            processor.setMaxKeepAliveRequests(proto.getMaxKeepAliveRequests());
+            processor.setKeepAliveTimeout(proto.getKeepAliveTimeout());
+            processor.setConnectionUploadTimeout(
+                    proto.getConnectionUploadTimeout());
+            processor.setDisableUploadTimeout(proto.getDisableUploadTimeout());
+            processor.setCompressionMinSize(proto.getCompressionMinSize());
+            processor.setCompression(proto.getCompression());
+            processor.setNoCompressionUserAgents(proto.getNoCompressionUserAgents());
+            processor.setCompressableMimeTypes(proto.getCompressableMimeTypes());
+            processor.setRestrictedUserAgents(proto.getRestrictedUserAgents());
+            processor.setSocketBuffer(proto.getSocketBuffer());
+            processor.setMaxSavePostSize(proto.getMaxSavePostSize());
+            processor.setServer(proto.getServer());
+            register(processor);
+            return processor;
+        }
+
+        @Override
+        protected Processor<Nio2Channel> createUpgradeProcessor(
+                SocketWrapper<Nio2Channel> socket,
+                HttpUpgradeHandler httpUpgradeProcessor)
+                throws IOException {
+            return new Nio2Processor(socket, httpUpgradeProcessor);
+        }
+
+        @Override
+        public void onCreateSSLEngine(SSLEngine engine) {
+            if (proto.npnHandler != null) {
+                proto.npnHandler.onCreateEngine(engine);
+            }
+        }
+    }
+}
