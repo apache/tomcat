4a7a9be8f4d232af5aa3f43f7f8ebb1b678c147d
Remy Maucherat <remm@apache.org>
2014-03-11 14:30:46 +0000

    Increase the write timeout (5s -> 10s) to see how the test reacts (or if it's a deadlock somewhere), I see some significant spikes in my own testing if I put some load on my computer.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1576363 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/tomcat/websocket/TesterFirehoseServer.java
+++ test/org/apache/tomcat/websocket/TesterFirehoseServer.java
@@ -43,7 +43,7 @@ public class TesterFirehoseServer {
     public static final String MESSAGE;
     public static final int MESSAGE_SIZE = 1024;
     public static final int WAIT_TIME_MILLIS = 60000;
-    public static final int SEND_TIME_OUT_MILLIS = 5000;
+    public static final int SEND_TIME_OUT_MILLIS = 10000;
 
     static {
         StringBuilder sb = new StringBuilder(MESSAGE_SIZE);
