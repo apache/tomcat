8a7744f08051308f548c8759505740546e1f5797
Konstantin Kolinko <kkolinko@apache.org>
2014-03-15 17:00:31 +0000

    Clean an IDE warning about unthrown exception. Since r1577757 the setSSLEngine() method is a pure setter, it no longer calls reset() and no longer throws an IOException.
    (An alternative way to silence such warnings is to document the exception in Javadoc for the method).
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1577902 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/net/SecureNio2Channel.java
+++ java/org/apache/tomcat/util/net/SecureNio2Channel.java
@@ -100,7 +100,7 @@ public class SecureNio2Channel extends Nio2Channel  {
         reset();
     }
 
-    public void setSSLEngine(SSLEngine engine) throws IOException {
+    public void setSSLEngine(SSLEngine engine) {
         this.sslEngine = engine;
     }
 
