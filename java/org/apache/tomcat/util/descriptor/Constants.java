bf08129e491555b27ef665d1d6672867f2a19549
Mark Thomas <markt@apache.org>
2014-04-25 11:10:55 +0000

    Add new constant missed in previous commit This is part 7 of 7 of the fix for CVE-2014-0119
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1589992 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/descriptor/Constants.java
+++ java/org/apache/tomcat/util/descriptor/Constants.java
@@ -21,4 +21,5 @@ public class Constants {
     public static final String PACKAGE_NAME =
             Constants.class.getPackage().getName();
 
+    public static final boolean IS_SECURITY_ENABLED = (System.getSecurityManager() != null);
 }
