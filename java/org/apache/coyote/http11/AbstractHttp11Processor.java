32525e0d08b46ea6601c5423e59401fd562ce3fa
Mark Thomas <markt@apache.org>
2014-05-13 11:25:14 +0000

    Address root cause of ReadPendingException rather than swallowing it.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1594198 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http11/AbstractHttp11Processor.java
+++ java/org/apache/coyote/http11/AbstractHttp11Processor.java
@@ -18,7 +18,6 @@ package org.apache.coyote.http11;
 
 import java.io.IOException;
 import java.io.InterruptedIOException;
-import java.nio.channels.ReadPendingException;
 import java.util.Locale;
 import java.util.StringTokenizer;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -955,7 +954,7 @@ public abstract class AbstractHttp11Processor<S> extends AbstractProcessor<S> {
                         setSocketTimeout(connectionUploadTimeout);
                     }
                 }
-            } catch (IOException | ReadPendingException e) {
+            } catch (IOException e) {
                 if (getLog().isDebugEnabled()) {
                     getLog().debug(
                             sm.getString("http11processor.header.parse"), e);
