f8d813f23278918902ac1458d04feacf94cab18f
Mark Emlyn David Thomas <markt@apache.org>
2014-05-20 10:28:46 +0000

    Experiment with the Simbian duplicate code detection tool. Remove 100+ lines of duplicate code
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1596186 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/naming/factory/EjbFactory.java
+++ java/org/apache/naming/factory/EjbFactory.java
@@ -16,11 +16,7 @@
  */
 package org.apache.naming.factory;
 
-import java.util.Hashtable;
-
-import javax.naming.Context;
 import javax.naming.InitialContext;
-import javax.naming.Name;
 import javax.naming.NamingException;
 import javax.naming.RefAddr;
 import javax.naming.Reference;
@@ -33,96 +29,43 @@ import org.apache.naming.EjbRef;
  *
  * @author Remy Maucherat
  */
-public class EjbFactory implements ObjectFactory {
+public class EjbFactory extends FactoryBase {
 
-    /**
-     * Create a new EJB instance.
-     *
-     * @param obj The reference object describing the DataSource
-     */
     @Override
-    public Object getObjectInstance(Object obj, Name name, Context nameCtx,
-                                    Hashtable<?,?> environment)
-        throws Exception {
+    protected boolean isReferenceTypeSupported(Object obj) {
+        return obj instanceof EjbRef;
+    }
 
-        if (obj instanceof EjbRef) {
-            Reference ref = (Reference) obj;
+    @Override
+    protected ObjectFactory getDefaultFactory(Reference ref) throws NamingException {
 
-            // If ejb-link has been specified, resolving the link using JNDI
-            RefAddr linkRefAddr = ref.get(EjbRef.LINK);
-            if (linkRefAddr != null) {
-                // Retrieving the EJB link
-                String ejbLink = linkRefAddr.getContent().toString();
-                Object beanObj = (new InitialContext()).lookup(ejbLink);
-                return beanObj;
-            }
-
-            ObjectFactory factory = null;
-            RefAddr factoryRefAddr = ref.get(Constants.FACTORY);
-            if (factoryRefAddr != null) {
-                // Using the specified factory
-                String factoryClassName =
-                    factoryRefAddr.getContent().toString();
-                // Loading factory
-                ClassLoader tcl =
-                    Thread.currentThread().getContextClassLoader();
-                Class<?> factoryClass = null;
-                if (tcl != null) {
-                    try {
-                        factoryClass = tcl.loadClass(factoryClassName);
-                    } catch(ClassNotFoundException e) {
-                        NamingException ex = new NamingException
-                            ("Could not load resource factory class");
-                        ex.initCause(e);
-                        throw ex;
-                    }
-                } else {
-                    try {
-                        factoryClass = Class.forName(factoryClassName);
-                    } catch(ClassNotFoundException e) {
-                        NamingException ex = new NamingException
-                            ("Could not load resource factory class");
-                        ex.initCause(e);
-                        throw ex;
-                    }
-                }
-                if (factoryClass != null) {
-                    try {
-                        factory = (ObjectFactory) factoryClass.newInstance();
-                    } catch(Throwable t) {
-                        NamingException ex = new NamingException
-                            ("Could not load resource factory class");
-                        ex.initCause(t);
-                        throw ex;
-                    }
-                }
-            } else {
-                String javaxEjbFactoryClassName =
-                    System.getProperty("javax.ejb.Factory",
-                                       Constants.OPENEJB_EJB_FACTORY);
-                try {
-                    factory = (ObjectFactory)
-                        Class.forName(javaxEjbFactoryClassName).newInstance();
-                } catch(Throwable t) {
-                    if (t instanceof NamingException)
-                        throw (NamingException) t;
-                    NamingException ex = new NamingException
-                        ("Could not create resource factory instance");
-                    ex.initCause(t);
-                    throw ex;
-                }
-            }
-
-            if (factory != null) {
-                return factory.getObjectInstance
-                    (obj, name, nameCtx, environment);
-            } else {
-                throw new NamingException
-                    ("Cannot create resource instance");
-            }
-
+        ObjectFactory factory;
+        String javaxEjbFactoryClassName = System.getProperty(
+                "javax.ejb.Factory", Constants.OPENEJB_EJB_FACTORY);
+        try {
+            factory = (ObjectFactory)
+                Class.forName(javaxEjbFactoryClassName).newInstance();
+        } catch(Throwable t) {
+            if (t instanceof NamingException)
+                throw (NamingException) t;
+            NamingException ex = new NamingException
+                ("Could not create resource factory instance");
+            ex.initCause(t);
+            throw ex;
         }
+        return factory;
+    }
 
+    @Override
+    protected Object getLinked(Reference ref) throws NamingException {
+        // If ejb-link has been specified, resolving the link using JNDI
+        RefAddr linkRefAddr = ref.get(EjbRef.LINK);
+        if (linkRefAddr != null) {
+            // Retrieving the EJB link
+            String ejbLink = linkRefAddr.getContent().toString();
+            Object beanObj = (new InitialContext()).lookup(ejbLink);
+            return beanObj;
+        }
         return null;
     }
 }
--- java/org/apache/naming/factory/ResourceFactory.java
+++ java/org/apache/naming/factory/ResourceFactory.java
@@ -14,16 +14,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-
 package org.apache.naming.factory;
 
-import java.util.Hashtable;
-
-import javax.naming.Context;
-import javax.naming.Name;
 import javax.naming.NamingException;
-import javax.naming.RefAddr;
 import javax.naming.Reference;
 import javax.naming.spi.ObjectFactory;
 
@@ -34,122 +27,52 @@ import org.apache.naming.ResourceRef;
  *
  * @author Remy Maucherat
  */
-public class ResourceFactory
-    implements ObjectFactory {
+public class ResourceFactory extends FactoryBase {
 
-
-    // ----------------------------------------------------------- Constructors
-
-
-    // -------------------------------------------------------------- Constants
-
-
-    // ----------------------------------------------------- Instance Variables
-
-
-    // --------------------------------------------------------- Public Methods
-
-
-    // -------------------------------------------------- ObjectFactory Methods
-
-
-    /**
-     * Crete a new DataSource instance.
-     *
-     * @param obj The reference object describing the DataSource
-     */
     @Override
-    public Object getObjectInstance(Object obj, Name name, Context nameCtx,
-                                    Hashtable<?,?> environment)
-        throws Exception {
+    protected boolean isReferenceTypeSupported(Object obj) {
+        return obj instanceof ResourceRef;
+    }
 
-        if (obj instanceof ResourceRef) {
-            Reference ref = (Reference) obj;
-            ObjectFactory factory = null;
-            RefAddr factoryRefAddr = ref.get(Constants.FACTORY);
-            if (factoryRefAddr != null) {
-                // Using the specified factory
-                String factoryClassName =
-                    factoryRefAddr.getContent().toString();
-                // Loading factory
-                ClassLoader tcl =
-                    Thread.currentThread().getContextClassLoader();
-                Class<?> factoryClass = null;
-                if (tcl != null) {
-                    try {
-                        factoryClass = tcl.loadClass(factoryClassName);
-                    } catch(ClassNotFoundException e) {
-                        NamingException ex = new NamingException
-                            ("Could not load resource factory class");
-                        ex.initCause(e);
-                        throw ex;
-                    }
-                } else {
-                    try {
-                        factoryClass = Class.forName(factoryClassName);
-                    } catch(ClassNotFoundException e) {
-                        NamingException ex = new NamingException
-                            ("Could not load resource factory class");
-                        ex.initCause(e);
-                        throw ex;
-                    }
-                }
-                if (factoryClass != null) {
-                    try {
-                        factory = (ObjectFactory) factoryClass.newInstance();
-                    } catch (Exception e) {
-                        if (e instanceof NamingException)
-                            throw (NamingException) e;
-                        NamingException ex = new NamingException
-                            ("Could not create resource factory instance");
-                        ex.initCause(e);
-                        throw ex;
-                    }
-                }
-            } else {
-                if (ref.getClassName().equals("javax.sql.DataSource")) {
-                    String javaxSqlDataSourceFactoryClassName =
-                        System.getProperty("javax.sql.DataSource.Factory",
-                                           Constants.DBCP_DATASOURCE_FACTORY);
-                    try {
-                        factory = (ObjectFactory)
-                            Class.forName(javaxSqlDataSourceFactoryClassName)
-                            .newInstance();
-                    } catch (Exception e) {
-                        NamingException ex = new NamingException
-                            ("Could not create resource factory instance");
-                        ex.initCause(e);
-                        throw ex;
-                    }
-                } else if (ref.getClassName().equals("javax.mail.Session")) {
-                    String javaxMailSessionFactoryClassName =
-                        System.getProperty("javax.mail.Session.Factory",
-                                           "org.apache.naming.factory.MailSessionFactory");
-                    try {
-                        factory = (ObjectFactory)
-                            Class.forName(javaxMailSessionFactoryClassName)
-                            .newInstance();
-                    } catch(Throwable t) {
-                        NamingException ex = new NamingException
-                            ("Could not create resource factory instance");
-                        ex.initCause(t);
-                        throw ex;
-                    }
-                }
+    @Override
+    protected ObjectFactory getDefaultFactory(Reference ref) throws NamingException {
+
+        ObjectFactory factory = null;
+
+        if (ref.getClassName().equals("javax.sql.DataSource")) {
+            String javaxSqlDataSourceFactoryClassName =
+                System.getProperty("javax.sql.DataSource.Factory",
+                        Constants.DBCP_DATASOURCE_FACTORY);
+            try {
+                factory = (ObjectFactory) Class.forName(
+                        javaxSqlDataSourceFactoryClassName).newInstance();
+            } catch (Exception e) {
+                NamingException ex = new NamingException(
+                        "Could not create resource factory instance");
+                ex.initCause(e);
+                throw ex;
             }
-            if (factory != null) {
-                return factory.getObjectInstance
-                    (obj, name, nameCtx, environment);
-            } else {
-                throw new NamingException
-                    ("Cannot create resource instance");
+        } else if (ref.getClassName().equals("javax.mail.Session")) {
+            String javaxMailSessionFactoryClassName =
+                System.getProperty("javax.mail.Session.Factory",
+                        "org.apache.naming.factory.MailSessionFactory");
+            try {
+                factory = (ObjectFactory)
+                    Class.forName(javaxMailSessionFactoryClassName).newInstance();
+            } catch(Throwable t) {
+                NamingException ex = new NamingException(
+                        "Could not create resource factory instance");
+                ex.initCause(t);
+                throw ex;
             }
         }
 
-        return null;
-
+        return factory;
     }
 
-
+    @Override
+    protected Object getLinked(Reference ref) {
+        // Not supported
+        return null;
+    }
 }
-
