957b062426a1933364e9ef48772d1ca7b2422be5
Mark Emlyn David Thomas <markt@apache.org>
2014-05-20 18:44:52 +0000

    Backport of r1575910 Better align 7.0.x with trunk to aid back-ports
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1596362 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http11/upgrade/AbstractServletOutputStream.java
+++ java/org/apache/coyote/http11/upgrade/AbstractServletOutputStream.java
@@ -155,17 +155,17 @@ public abstract class AbstractServletOutputStream extends ServletOutputStream {
 
 
     protected final void onWritePossible() throws IOException {
-        synchronized (writeLock) {
-            try {
+        try {
+            synchronized (writeLock) {
                 writeInternal(buffer, 0, buffer.length);
-            } catch (Throwable t) {
-                ExceptionUtils.handleThrowable(t);
-                onError(t);
-                if (t instanceof IOException) {
-                    throw (IOException) t;
-                } else {
-                    throw new IOException(t);
-                }
+            }
+        } catch (Throwable t) {
+            ExceptionUtils.handleThrowable(t);
+            onError(t);
+            if (t instanceof IOException) {
+                throw (IOException) t;
+            } else {
+                throw new IOException(t);
             }
         }
 
@@ -210,7 +210,7 @@ public abstract class AbstractServletOutputStream extends ServletOutputStream {
      * Abstract method to be overridden by concrete implementations. The base
      * class will ensure that there are no concurrent calls to this method for
      * the same socket by ensuring that the writeLock is held when making any
-     * calls this method.
+     * calls to this method.
      */
     protected abstract int doWrite(boolean block, byte[] b, int off, int len)
             throws IOException;
