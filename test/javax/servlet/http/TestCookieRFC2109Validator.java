2f4998eeafe4149980bc0bec4091f18b91ec0319
Mark Emlyn David Thomas <markt@apache.org>
2014-05-21 12:20:23 +0000

    Apply patch 02 from jboynes to improve cookie handling. Refactor cookie tests to test each CookieNameValidator directly. I made a few additional changes. The patch should be safe since it only impacts the unit tests.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1596551 13f79535-47bb-0310-9956-ffa450edef68

--- test/javax/servlet/http/TestCookieRFC2109Validator.java
+++ test/javax/servlet/http/TestCookieRFC2109Validator.java
@@ -16,29 +16,20 @@
  */
 package javax.servlet.http;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 /**
- * Basic tests for Cookie in STRICT_SERVLET_COMPLIANCE configuration.
+ * Basic tests for Cookie in default configuration.
  */
-public class TestCookieStrict {
+public class TestCookieRFC2109Validator {
     static {
-        System.setProperty("org.apache.tomcat.util.http.ServerCookie.STRICT_NAMING", "true");
+        System.setProperty("org.apache.tomcat.util.http.ServerCookie.FWD_SLASH_IS_SEPARATOR", "true");
     }
+
+    private RFC2109Validator validator = new RFC2109Validator();
 
     @Test
-    public void testDefaults() {
-        Cookie cookie = new Cookie("strict", null);
-        Assert.assertEquals("strict", cookie.getName());
-        Assert.assertNull(cookie.getValue());
-        Assert.assertEquals(0, cookie.getVersion());
-        Assert.assertEquals(-1, cookie.getMaxAge());
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void strictNamingImpliesRFC2109() {
-        @SuppressWarnings("unused")
-        Cookie cookie = new Cookie("@Foo", null);
+    public void actualCharactersAllowedInName() {
+        TestCookie.checkCharInName(validator, TestCookie.TOKEN);
     }
 }
