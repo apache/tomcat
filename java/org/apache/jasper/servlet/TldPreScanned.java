1338db82257f83132bee1a38b3e9a6cded2b5c87
Mark Emlyn David Thomas <markt@apache.org>
2014-05-26 17:52:57 +0000

    Implement third part of pull request from Greg Wilkins to make Jasper more independent from Tomcat. Add support for using an existing list of TLDs rather than scanning for them This closes #4
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1597617 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/org/apache/jasper/servlet/TldPreScanned.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jasper.servlet;
+
+import java.net.URL;
+import java.util.Collection;
+
+import javax.servlet.ServletContext;
+
+import org.apache.tomcat.util.descriptor.tld.TldResourcePath;
+
+public class TldPreScanned extends TldScanner {
+
+    private final Collection<URL> preScannedURLs;
+
+    public TldPreScanned (ServletContext context, boolean namespaceAware, boolean validation,
+            boolean blockExternal, Collection<URL> preScannedTlds) {
+        super(context, namespaceAware, validation, blockExternal);
+        preScannedURLs = preScannedTlds;
+    }
+
+    @Override
+    public void scanJars() {
+        for (URL url : preScannedURLs){
+            String str = url.toExternalForm();
+            int a = str.indexOf("jar:");
+            int b = str.indexOf("!/");
+            if (a >= 0 && b> 0) {
+                String fileUrl = str.substring(a + 4, b);
+                String path = str.substring(b + 2);
+                try {
+                    parseTld(new TldResourcePath(new URL(fileUrl), null, path));
+                } catch (Exception e) {
+                    throw new IllegalStateException(e);
+                }
+            } else {
+                throw new IllegalStateException("Bad tld url: "+str);
+            }
+        }
+    }
+}
