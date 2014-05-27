aa218528263356d9da798bbd4c2c8804c34bfdf2
Violeta Georgieva Georgieva <violetagg@apache.org>
2014-05-27 19:32:11 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=55282 o.a.t.util.descriptor.web.ApplicationListener overrides equals and hashCode methods.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1597855 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/descriptor/web/ApplicationListener.java
+++ java/org/apache/tomcat/util/descriptor/web/ApplicationListener.java
@@ -40,4 +40,33 @@ public class ApplicationListener {
     public boolean isPluggabilityBlocked() {
         return pluggabilityBlocked;
     }
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((className == null) ? 0 : className.hashCode());
+        return result;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof ApplicationListener)) {
+            return false;
+        }
+        ApplicationListener other = (ApplicationListener) obj;
+        if (className == null) {
+            if (other.className != null) {
+                return false;
+            }
+        } else if (!className.equals(other.className)) {
+            return false;
+        }
+        return true;
+    }
 }
