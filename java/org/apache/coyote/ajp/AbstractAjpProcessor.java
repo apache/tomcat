9f2ab03dd5520fd5a9d8897042afa0db366cc554
Mark Emlyn David Thomas <markt@apache.org>
2014-05-28 11:45:26 +0000

    Fix copy/paste error and return a 500 rather 400 response
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1597982 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/ajp/AbstractAjpProcessor.java
+++ java/org/apache/coyote/ajp/AbstractAjpProcessor.java
@@ -772,8 +772,8 @@ public abstract class AbstractAjpProcessor<S> extends AbstractProcessor<S> {
                 } catch (Throwable t) {
                     ExceptionUtils.handleThrowable(t);
                     getLog().debug(sm.getString("ajpprocessor.request.prepare"), t);
-                    // 400 - Internal Server Error
-                    response.setStatus(400);
+                    // 500 - Internal Server Error
+                    response.setStatus(500);
                     getAdapter().log(request, response, 0);
                     error = true;
                 }
