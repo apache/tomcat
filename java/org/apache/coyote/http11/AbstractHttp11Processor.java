c225e5234600e9334269bf5f94b46a8614d24e19
Mark Emlyn David Thomas <markt@apache.org>
2014-05-28 21:46:55 +0000

    No need for the expectation check in an error state as the connection will be closed any way.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1598153 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http11/AbstractHttp11Processor.java
+++ java/org/apache/coyote/http11/AbstractHttp11Processor.java
@@ -1073,18 +1073,16 @@ public abstract class AbstractHttp11Processor<S> extends AbstractProcessor<S> {
                     // input. This way uploading a 100GB file doesn't tie up the
                     // thread if the servlet has rejected it.
                     getInputBuffer().setSwallowInput(false);
-                }
-                if (response.getStatus() < 200 || response.getStatus() > 299) {
-                    if (expectation) {
-                        // Client sent Expect: 100-continue but received a
-                        // non-2xx response. Disable keep-alive (if enabled) to
-                        // ensure the connection is closed. Some clients may
-                        // still send the body, some may send the next request.
-                        // No way to differentiate, so close the connection to
-                        // force the client to send the next request.
-                        getInputBuffer().setSwallowInput(false);
-                        keepAlive = false;
-                    }
+                } else if (expectation &&
+                        (response.getStatus() < 200 || response.getStatus() > 299)) {
+                    // Client sent Expect: 100-continue but received a
+                    // non-2xx response. Disable keep-alive (if enabled) to
+                    // ensure the connection is closed. Some clients may
+                    // still send the body, some may send the next request.
+                    // No way to differentiate, so close the connection to
+                    // force the client to send the next request.
+                    getInputBuffer().setSwallowInput(false);
+                    keepAlive = false;
                 }
                 endRequest();
             }
