1acf55e934a36a04f048cb019304621fc2b4bbaa
Mark Emlyn David Thomas <markt@apache.org>
2014-06-17 20:31:05 +0000

    Ensure that the asynchronous WebSocket echo endpoint in the examples web application always waits for the previous message to complete before it sends the next. Fixes some failures with this endpoint and the Autobahn tests
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1603287 13f79535-47bb-0310-9956-ffa450edef68

--- webapps/examples/WEB-INF/classes/websocket/echo/EchoAsyncAnnotation.java
+++ webapps/examples/WEB-INF/classes/websocket/echo/EchoAsyncAnnotation.java
@@ -19,6 +19,10 @@ package websocket.echo;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import javax.websocket.OnMessage;
 import javax.websocket.PongMessage;
@@ -36,6 +40,9 @@ import javax.websocket.server.ServerEndpoint;
 @ServerEndpoint("/websocket/echoAsyncAnnotation")
 public class EchoAsyncAnnotation {
 
+    private static final Future<Void> COMPLETED = new CompletedFuture();
+
+    Future<Void> f = COMPLETED;
     StringBuilder sb = null;
     ByteArrayOutputStream bytes = null;
 
@@ -46,8 +53,15 @@ public class EchoAsyncAnnotation {
         }
         sb.append(msg);
         if (last) {
-            //System.out.println("Write: " + sb.length());
-            session.getAsyncRemote().sendText(sb.toString());
+            // Before we send the next message, have to wait for the previous
+            // message to complete
+            try {
+                f.get();
+            } catch (InterruptedException | ExecutionException e) {
+                // Let the container deal with it
+                throw new RuntimeException(e);
+            }
+            f = session.getAsyncRemote().sendText(sb.toString());
             sb = null;
         }
     }
@@ -59,10 +73,16 @@ public class EchoAsyncAnnotation {
             bytes = new ByteArrayOutputStream();
         }
         bytes.write(msg);
-        //System.out.println("Got: " + msg.length + " " + last + " " + bytes.size());
         if (last) {
-            //System.out.println("Write bytes: " + bytes.size());
-            session.getAsyncRemote().sendBinary(ByteBuffer.wrap(bytes.toByteArray()));
+            // Before we send the next message, have to wait for the previous
+            // message to complete
+            try {
+                f.get();
+            } catch (InterruptedException | ExecutionException e) {
+                // Let the container deal with it
+                throw new RuntimeException(e);
+            }
+            f = session.getAsyncRemote().sendBinary(ByteBuffer.wrap(bytes.toByteArray()));
             bytes = null;
         }
     }
@@ -75,5 +95,35 @@ public class EchoAsyncAnnotation {
     @OnMessage
     public void echoPongMessage(PongMessage pm) {
         // NO-OP
+    }
+
+    private static class CompletedFuture implements Future<Void> {
+
+        @Override
+        public boolean cancel(boolean mayInterruptIfRunning) {
+            return false;
+        }
+
+        @Override
+        public boolean isCancelled() {
+            return false;
+        }
+
+        @Override
+        public boolean isDone() {
+            return true;
+        }
+
+        @Override
+        public Void get() throws InterruptedException, ExecutionException {
+            return null;
+        }
+
+        @Override
+        public Void get(long timeout, TimeUnit unit)
+                throws InterruptedException, ExecutionException,
+                TimeoutException {
+            return null;
+        }
     }
 }
