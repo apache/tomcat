c8c7e8d528749ff2968f069e19d19cb0dde07214
Mark Emlyn David Thomas <markt@apache.org>
2014-06-18 20:43:58 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=56334#c15 Handle potential whitespace around input.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1603621 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/jasper/compiler/TestParser.java
+++ test/org/apache/jasper/compiler/TestParser.java
@@ -417,6 +417,13 @@ public class TestParser extends TomcatBaseTest {
         Assert.assertTrue(result, result.contains("<set data-value=\"05c##{&gt;hello&lt;\"/>"));
         Assert.assertTrue(result, result.contains("05x:<set data-value=\"\"/>"));
         Assert.assertTrue(result, result.contains("<set xmlns:foo=\"urn:06a\\bar\\baz\"/>"));
+        Assert.assertTrue(result, result.contains("07a:<set data-value=\"\\?resize\"/>"));
+        Assert.assertTrue(result, result.contains("07b:<set data-content=\"\\?resize=.+\"/>"));
+        Assert.assertTrue(result, result.contains("07c:<set data-content=\"\\?resize=.+\"/>"));
+        Assert.assertTrue(result, result.contains("07d:<set data-content=\"false\"/>"));
+        Assert.assertTrue(result, result.contains("07e:<set data-content=\"false\"/>"));
+        Assert.assertTrue(result, result.contains("07f:<set data-content=\"\\\'something\'\"/>"));
+        /* Assert.assertTrue(result, result.contains("07g:<set data-content=\"\\\'something\'\"/>")); */
     }
 
     /** Assertion for text printed by tags:echo */
