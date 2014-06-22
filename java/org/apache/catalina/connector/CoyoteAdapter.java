9e3deff42e90c7d347c10e5fb7dbd56c7e407a27
Konstantin Kolinko <kkolinko@apache.org>
2014-06-22 16:32:46 +0000

    https://issues.apache.org/bugzilla/show_bug.cgi?id=56657 If sessionid belongs to the latest version of webapp, use the latest version.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1604605 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/connector/CoyoteAdapter.java
+++ java/org/apache/catalina/connector/CoyoteAdapter.java
@@ -909,7 +909,7 @@ public class CoyoteAdapter implements Adapter {
                     for (int i = (contexts.length); i > 0; i--) {
                         Context ctxt = contexts[i - 1];
                         if (ctxt.getManager().findSession(sessionID) != null) {
-                            // Was the correct context already mapped?
+                            // We found a context. Is it the one that has already been mapped?
                             if (!ctxt.equals(request.getMappingData().context)) {
                                 // Set version so second time through mapping the
                                 // correct context is found
@@ -917,8 +917,8 @@ public class CoyoteAdapter implements Adapter {
                                 // Reset mapping
                                 request.getMappingData().recycle();
                                 mapRequired = true;
-                                break;
                             }
+                            break;
                         }
                     }
                 }
