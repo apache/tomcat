53b4bfd85ca18739b45d3295bd07f0434d1256e7
Mark Thomas <markt@apache.org>
2014-07-09 22:48:55 +0000

    Merge BCEL changes to r1609331. No fucntional change. Cosmetic changes only.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1609334 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/bcel/classfile/LocalVariableTypeTable.java
+++ java/org/apache/tomcat/util/bcel/classfile/LocalVariableTypeTable.java
@@ -22,16 +22,16 @@ import java.io.IOException;
 // The new table is used when generic types are about...
 
 //LocalVariableTable_attribute {
-//     u2 attribute_name_index;
-//     u4 attribute_length;
-//     u2 local_variable_table_length;
-//     {  u2 start_pc;
-//        u2 length;
-//        u2 name_index;
-//        u2 descriptor_index;
-//        u2 index;
-//     } local_variable_table[local_variable_table_length];
-//   }
+//       u2 attribute_name_index;
+//       u4 attribute_length;
+//       u2 local_variable_table_length;
+//       {  u2 start_pc;
+//          u2 length;
+//          u2 name_index;
+//          u2 descriptor_index;
+//          u2 index;
+//       } local_variable_table[local_variable_table_length];
+//     }
 
 //LocalVariableTypeTable_attribute {
 //    u2 attribute_name_index;
