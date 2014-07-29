b8f2eb2b19606b44cf1a451f3c0fa54e2b8186f2
Konstantin Kolinko <kkolinko@apache.org>
2014-07-29 21:09:48 +0000

    Annotate a disabled (work-in-progress) test with @Ignore, so that it is recognized and skipped, instead of failing the whole testsuite run with "No runnable methods" error.
    Fix checkstyle errors such as imports order and missing license header.
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1614489 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/tomcat/util/net/jsse/openssl/TestCipher.java
+++ test/org/apache/tomcat/util/net/jsse/openssl/TestCipher.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.tomcat.util.net.jsse.openssl;
 
 import java.io.InputStream;
@@ -7,10 +23,12 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
 import org.apache.catalina.util.IOTools;
 import org.apache.tomcat.util.http.fileupload.ByteArrayOutputStream;
-import org.junit.Assert;
-//import org.junit.Test;
 
 public class TestCipher {
 
@@ -19,7 +37,8 @@ public class TestCipher {
      * one cipher suite that is recognised by JSSE or is a cipher suite known
      * not to be supported by JSSE.
      */
-    //@Test
+    @Test
+    @Ignore //FIXME: enable the test
     public void testAllOpenSSlCiphersMapped() throws Exception {
         Set<String> openSSLCipherSuites = getOpenSSLCiphersAsSet("ALL");
 
