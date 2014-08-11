0a64e36a873a24c5fb835dce957c1b80c4e7e38c
Mark Thomas <markt@apache.org>
2014-08-11 09:44:10 +0000

    Use root just created rather than new object
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1617234 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/catalina/webresources/AbstractTestDirResourceSet.java
+++ test/org/apache/catalina/webresources/AbstractTestDirResourceSet.java
@@ -35,9 +35,7 @@ public abstract class AbstractTestDirResourceSet extends AbstractTestResourceSet
     public WebResourceRoot getWebResourceRoot() {
         File f = new File(getBaseDir());
         TesterWebResourceRoot root = new TesterWebResourceRoot();
-        WebResourceSet webResourceSet =
-                new DirResourceSet(new TesterWebResourceRoot(), "/",
-                        f.getAbsolutePath(), "/");
+        WebResourceSet webResourceSet = new DirResourceSet(root, "/", f.getAbsolutePath(), "/");
         webResourceSet.setReadOnly(readOnly);
         root.setMainResources(webResourceSet);
         return root;
