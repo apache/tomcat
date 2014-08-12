f92e9351a876bcb76c11dd58ecad8e5780c39537
Konstantin Kolinko <kkolinko@apache.org>
2014-08-12 13:49:51 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=56801 Improve performance of org.apache.tomcat.util.file.Matcher which is to filter JARs for scanning during web application start. Based on a patch by Sheldon Shao.
    It is backport of r1615876 and r1617470.
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1617475 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/file/Matcher.java
+++ java/org/apache/tomcat/util/file/Matcher.java
@@ -45,13 +45,15 @@ public final class Matcher {
      *         file name, or <code>false</code> otherwise.
      */
     public static boolean matchName(Set<String> patternSet, String fileName) {
+        char[] fileNameArray = fileName.toCharArray();
         for (String pattern: patternSet) {
-            if (match(pattern, fileName, true)) {
+            if (match(pattern, fileNameArray, true)) {
                 return true;
             }
         }
         return false;
     }
+
 
     /**
      * Tests whether or not a string matches against a pattern.
@@ -61,8 +63,8 @@ public final class Matcher {
      *
      * @param pattern The pattern to match against.
      *                Must not be <code>null</code>.
-     * @param str     The string which must be matched against the pattern.
-     *                Must not be <code>null</code>.
+     * @param str     The string which must be matched against the
+     *                pattern. Must not be <code>null</code>.
      * @param caseSensitive Whether or not matching should be performed
      *                        case sensitively.
      *
@@ -71,9 +73,32 @@ public final class Matcher {
      *         or <code>false</code> otherwise.
      */
     public static boolean match(String pattern, String str,
+            boolean caseSensitive) {
+
+        return match(pattern, str.toCharArray(), caseSensitive);
+    }
+
+
+    /**
+     * Tests whether or not a string matches against a pattern.
+     * The pattern may contain two special characters:<br>
+     * '*' means zero or more characters<br>
+     * '?' means one and only one character
+     *
+     * @param pattern The pattern to match against.
+     *                Must not be <code>null</code>.
+     * @param strArr  The character array which must be matched against the
+     *                pattern. Must not be <code>null</code>.
+     * @param caseSensitive Whether or not matching should be performed
+     *                        case sensitively.
+     *
+     *
+     * @return <code>true</code> if the string matches against the pattern,
+     *         or <code>false</code> otherwise.
+     */
+    private static boolean match(String pattern, char[] strArr,
                                 boolean caseSensitive) {
         char[] patArr = pattern.toCharArray();
-        char[] strArr = str.toCharArray();
         int patIdxStart = 0;
         int patIdxEnd = patArr.length - 1;
         int strIdxStart = 0;
