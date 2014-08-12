4631876cefdc0f142f44091d9388ab284dd4124f
Mark Thomas <markt@apache.org>
2014-08-12 18:26:55 +0000

    Map protocol names to the name used by OpenSSL with ciphers -v
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1617553 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/net/jsse/openssl/Protocol.java
+++ java/org/apache/tomcat/util/net/jsse/openssl/Protocol.java
@@ -18,5 +18,24 @@
 package org.apache.tomcat.util.net.jsse.openssl;
 
 enum Protocol {
-    SSLv3, SSLv2, TLSv1, TLSv1_2;
+
+    SSLv3("SSLv3"),
+    SSLv2("SSLv2"),
+    TLSv1("SSLv3"),
+    TLSv1_2("TLSv1.2");
+
+    private final String openSSLName;
+
+    private Protocol(String openSSLName) {
+        this.openSSLName = openSSLName;
+    }
+
+    /**
+     * The name returned by OpenSSL in the protocol column when using
+     * <code>openssl ciphers -v</code>. This is currently only used by the unit
+     * tests hence it is package private.
+     */
+    String getOpenSSLName() {
+        return openSSLName;
+    }
 }
