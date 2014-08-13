51b73a177f37a34fba5428f12f810a456744e89d
Remy Maucherat <remm@apache.org>
2014-08-13 14:06:43 +0000

    NIO2 seems less likely to detect a disconnect when writing, for some reason. When the test fails (extremely rare for me), no error is actually reported by NIO2 so there's nothing that can really be improved from what I can see. Try to disconnect earlier to try to improve the test's reliability on gump (this also makes the test run faster).
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1617734 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/catalina/nonblocking/TestNonBlockingAPI.java
+++ test/org/apache/catalina/nonblocking/TestNonBlockingAPI.java
@@ -302,7 +302,7 @@ public class TestNonBlockingAPI extends TomcatBaseTest {
         int read = 0;
         int readSinceLastPause = 0;
         int readTotal = 0;
-        while (read != -1 && readTotal < WRITE_SIZE / 2) {
+        while (read != -1 && readTotal < WRITE_SIZE / 8) {
             long start = System.currentTimeMillis();
             read = is.read(buffer);
             long end = System.currentTimeMillis();
@@ -349,8 +349,8 @@ public class TestNonBlockingAPI extends TomcatBaseTest {
 
         // TODO Figure out why non-blocking writes with the NIO connector appear
         // to be slower on Linux
-        alv.validateAccessLog(1, 500, WRITE_PAUSE_MS * 7,
-                WRITE_PAUSE_MS * 7 + 30 * 1000);
+        alv.validateAccessLog(1, 500, WRITE_PAUSE_MS,
+                WRITE_PAUSE_MS + 30 * 1000);
     }
 
 
