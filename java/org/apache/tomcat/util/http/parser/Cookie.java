e22a3f94aa1c2598fdea26220ff113d5596fedea
Mark Emlyn David Thomas <markt@apache.org>
2014-09-01 19:58:37 +0000

    Fix copy/paste error
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1621876 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/http/parser/Cookie.java
+++ java/org/apache/tomcat/util/http/parser/Cookie.java
@@ -205,7 +205,7 @@ public class Cookie {
 
     private static void skipUntilSemiColon(ByteBuffer bb) {
         while(bb.hasRemaining()) {
-            if (bb.get() == EQUALS_BYTE) {
+            if (bb.get() == SEMICOLON_BYTE) {
                 break;
             }
         }
@@ -333,7 +333,7 @@ public class Cookie {
     private static void logInvalidHeader(ByteBuffer bb) {
         UserDataHelper.Mode logMode = invalidCookieLog.getNextMode();
         if (logMode != null) {
-            String headerValue = new String(bb.array(), bb.position(), bb.limit(),
+            String headerValue = new String(bb.array(), bb.position(), bb.limit() - bb.position(),
                         StandardCharsets.UTF_8);
             String message = sm.getString("cookie.invalidCookieValue", headerValue);
             switch (logMode) {
