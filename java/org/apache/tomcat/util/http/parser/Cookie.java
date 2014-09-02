ca6de90ad5ba07a6874985339a5b5d66cd23c554
Mark Thomas <markt@apache.org>
2014-09-02 15:25:34 +0000

    More docs
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1622034 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/http/parser/Cookie.java
+++ java/org/apache/tomcat/util/http/parser/Cookie.java
@@ -27,15 +27,20 @@ import org.apache.tomcat.util.res.StringManager;
 
 
 /**
- * Cookie header parser based on RFC6265 and RFC2109.
- * <br/>
- * The parsing of cookies using RFC6265 is more relaxed that the specification
- * in the following ways:
+ * <p>Cookie header parser based on RFC6265 and RFC2109.</p>
+ * <p>The parsing of cookies using RFC6265 is more relaxed that the
+ * specification in the following ways:</p>
  * <ul>
  *   <li>Values 0x80 to 0xFF are permitted in cookie-octet to support the use of
  *       UTF-8 in cookie values as used by HTML 5.</li>
  *   <li>For cookies without a value, the '=' is not required after the name as
  *       some browsers do not sent it.</li>
+ * </ul>
+ * <p>The parsing of cookies using RFC2109 is more relaxed that the
+ * specification in the following ways:</p>
+ * <ul>
+ *   <li>Values for the path attribute that contain a / character do not have to
+ *       be quoted even though / is not permitted in a token.</li>
  * </ul>
  *
  * <p>Implementation note:<br/>
