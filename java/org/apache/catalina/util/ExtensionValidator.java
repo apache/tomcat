9451769013894261f0aa90e6cf79dbdfae2a3c5f
Mark Emlyn David Thomas <markt@apache.org>
2014-09-03 12:17:18 +0000

    Whitespace police
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1622234 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/util/ExtensionValidator.java
+++ java/org/apache/catalina/util/ExtensionValidator.java
@@ -176,7 +176,7 @@ public final class ExtensionValidator {
                         ManifestResource.SYSTEM);
                 containerManifestResources.add(mre);
             }
-        } 
+        }
     }
 
 
