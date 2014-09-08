d179a73f129560b17f4145df5e8417ac59367fc3
Mark Emlyn David Thomas <markt@apache.org>
2014-09-08 18:34:39 +0000

    Extract common code into a new method to reduce duplication identified by Simian.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1623456 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/el/lang/ELArithmetic.java
+++ java/org/apache/el/lang/ELArithmetic.java
@@ -249,22 +249,10 @@ public abstract class ELArithmetic {
     private static final Long ZERO = Long.valueOf(0);
 
     public static final Number add(final Object obj0, final Object obj1) {
-        if (obj0 == null && obj1 == null) {
+        final ELArithmetic delegate = findDelegate(obj0, obj1);
+        if (delegate == null) {
             return Long.valueOf(0);
         }
-
-        final ELArithmetic delegate;
-        if (BIGDECIMAL.matches(obj0, obj1))
-            delegate = BIGDECIMAL;
-        else if (DOUBLE.matches(obj0, obj1)) {
-            if (BIGINTEGER.matches(obj0, obj1))
-                delegate = BIGDECIMAL;
-            else
-                delegate = DOUBLE;
-        } else if (BIGINTEGER.matches(obj0, obj1))
-            delegate = BIGINTEGER;
-        else
-            delegate = LONG;
 
         Number num0 = delegate.coerce(obj0);
         Number num1 = delegate.coerce(obj1);
@@ -294,22 +282,10 @@ public abstract class ELArithmetic {
     }
 
     public static final Number subtract(final Object obj0, final Object obj1) {
-        if (obj0 == null && obj1 == null) {
+        final ELArithmetic delegate = findDelegate(obj0, obj1);
+        if (delegate == null) {
             return Long.valueOf(0);
         }
-
-        final ELArithmetic delegate;
-        if (BIGDECIMAL.matches(obj0, obj1))
-            delegate = BIGDECIMAL;
-        else if (DOUBLE.matches(obj0, obj1)) {
-            if (BIGINTEGER.matches(obj0, obj1))
-                delegate = BIGDECIMAL;
-            else
-                delegate = DOUBLE;
-        } else if (BIGINTEGER.matches(obj0, obj1))
-            delegate = BIGINTEGER;
-        else
-            delegate = LONG;
 
         Number num0 = delegate.coerce(obj0);
         Number num1 = delegate.coerce(obj1);
@@ -337,27 +313,35 @@ public abstract class ELArithmetic {
     }
 
     public static final Number multiply(final Object obj0, final Object obj1) {
-        if (obj0 == null && obj1 == null) {
+        final ELArithmetic delegate = findDelegate(obj0, obj1);
+        if (delegate == null) {
             return Long.valueOf(0);
         }
-
-        final ELArithmetic delegate;
-        if (BIGDECIMAL.matches(obj0, obj1))
-            delegate = BIGDECIMAL;
-        else if (DOUBLE.matches(obj0, obj1)) {
-            if (BIGINTEGER.matches(obj0, obj1))
-                delegate = BIGDECIMAL;
-            else
-                delegate = DOUBLE;
-        } else if (BIGINTEGER.matches(obj0, obj1))
-            delegate = BIGINTEGER;
-        else
-            delegate = LONG;
 
         Number num0 = delegate.coerce(obj0);
         Number num1 = delegate.coerce(obj1);
 
         return delegate.multiply(num0, num1);
+    }
+
+    private static ELArithmetic findDelegate(final Object obj0, final Object obj1) {
+        if (obj0 == null && obj1 == null) {
+            return null;
+        }
+
+        if (BIGDECIMAL.matches(obj0, obj1)) {
+            return BIGDECIMAL;
+        } else if (DOUBLE.matches(obj0, obj1)) {
+            if (BIGINTEGER.matches(obj0, obj1)) {
+                return BIGDECIMAL;
+            } else {
+                return DOUBLE;
+            }
+        } else if (BIGINTEGER.matches(obj0, obj1)) {
+            return BIGINTEGER;
+        } else {
+            return LONG;
+        }
     }
 
     public static final boolean isNumber(final Object obj) {
