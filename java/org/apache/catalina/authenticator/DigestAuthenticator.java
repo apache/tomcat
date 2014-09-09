c3b939b6cf87bbcef0b4ef624009560396ac037f
Mark Thomas <markt@apache.org>
2014-09-09 08:13:42 +0000

    Remove deprecated code
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1623684 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/authenticator/DigestAuthenticator.java
+++ java/org/apache/catalina/authenticator/DigestAuthenticator.java
@@ -19,8 +19,6 @@ package org.apache.catalina.authenticator;
 import java.io.IOException;
 import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.security.Principal;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -57,32 +55,16 @@ public class DigestAuthenticator extends AuthenticatorBase {
      */
     protected static final String QOP = "auth";
 
-    // ----------------------------------------------------------- Constructors
 
+    // ----------------------------------------------------------- Constructors
 
     public DigestAuthenticator() {
         super();
         setCache(false);
-        try {
-            if (md5Helper == null) {
-                md5Helper = MessageDigest.getInstance("MD5");
-            }
-        } catch (NoSuchAlgorithmException e) {
-            throw new IllegalStateException(e);
-        }
     }
 
 
     // ----------------------------------------------------- Instance Variables
-
-
-    /**
-     * MD5 message digest provider.
-     * @deprecated  Unused - will be removed in Tomcat 8.0.x onwards
-     */
-    @Deprecated
-    protected static volatile MessageDigest md5Helper;
-
 
     /**
      * List of server nonce values currently being tracked
