51415352d34615db317f40bdb0f795500d310dc3
Mark Emlyn David Thomas <markt@apache.org>
2014-09-10 22:27:25 +0000

    Simplify. No functional change.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1624156 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/bcel/classfile/AnnotationDefault.java
+++ java/org/apache/tomcat/util/bcel/classfile/AnnotationDefault.java
@@ -44,25 +44,8 @@ public class AnnotationDefault extends Attribute
             DataInputStream file, ConstantPool constant_pool)
             throws IOException
     {
-        this(name_index, length, (ElementValue) null,
-                constant_pool);
+        super(name_index, length, constant_pool);
         // Default value
         ElementValue.readElementValue(file, constant_pool);
-    }
-
-    /**
-     * @param name_index
-     *            Index pointing to the name <em>Code</em>
-     * @param length
-     *            Content length in bytes
-     * @param defaultValue
-     *            the annotation's default value
-     * @param constant_pool
-     *            Array of constants
-     */
-    public AnnotationDefault(int name_index, int length,
-            ElementValue defaultValue, ConstantPool constant_pool)
-    {
-        super(name_index, length, constant_pool);
     }
 }
