d409b39c00eb82c6e8c002ea17d94c99ca7e7d85
Violeta Georgieva Georgieva <violetagg@apache.org>
2014-09-16 19:37:54 +0000

    Merged revision 1604810 from tomcat/trunk: Add the initial implementation for the permessage-deflate implementation This is a work-in-progress - There are still multiple Autobahn failures (or rather there will be once it is plumbed in) - No support (yet) for compression of outgoing messages
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1625362 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/org/apache/tomcat/websocket/TransformationFactory.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.tomcat.websocket;
+
+import javax.websocket.Extension;
+
+public class TransformationFactory {
+
+    private static final TransformationFactory factory = new TransformationFactory();
+
+    private TransformationFactory() {
+        // Hide default constructor
+    }
+
+    public static TransformationFactory getInstance() {
+        return factory;
+    }
+
+    public Transformation create(Extension ext) {
+        if (PerMessageDeflate.NAME.equals(ext.getName())) {
+            return new PerMessageDeflate(ext.getParameters());
+        }
+        // TODO i18n
+        throw new IllegalArgumentException("Unsupported extension");
+    }
+}
