4b2588c6353cec0c9a2a2b48012d788a84d72b69
Mark Thomas <markt@apache.org>
2014-09-16 13:54:57 +0000

    Fix formatting
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1625288 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/buf/HexUtils.java
+++ java/org/apache/tomcat/util/buf/HexUtils.java
@@ -23,12 +23,10 @@ package org.apache.tomcat.util.buf;
  *
  * @author Craig R. McClanahan
  */
-
 public final class HexUtils {
 
 
     // -------------------------------------------------------------- Constants
-
 
     /**
      *  Table for HEX to DEC byte translation.
@@ -55,10 +53,10 @@ public final class HexUtils {
      */
     private static final char[] hex = "0123456789abcdef".toCharArray();
 
+
     // --------------------------------------------------------- Static Methods
 
-
-    public static int getDec(int index){
+    public static int getDec(int index) {
         // Fast for correct values, slower for incorrect ones
         try {
             return DEC[index - '0'];
@@ -67,19 +65,18 @@ public final class HexUtils {
         }
     }
 
-    public static byte getHex(int index){
+    public static byte getHex(int index) {
         return HEX[index];
     }
 
-    public static String toHexString(byte[] bytes)
-    {
-        if(null == bytes) {
+    public static String toHexString(byte[] bytes) {
+        if (null == bytes) {
             return null;
         }
 
         StringBuilder sb = new StringBuilder(bytes.length << 1);
 
-        for(int i=0; i<bytes.length; ++i) {
+        for(int i = 0; i < bytes.length; ++i) {
             sb.append(hex[(bytes[i] & 0xf0) >> 4])
                 .append(hex[(bytes[i] & 0x0f)])
                 ;
