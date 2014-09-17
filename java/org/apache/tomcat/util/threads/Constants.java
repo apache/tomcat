29816631f71520201c53d5fad51806e1d3b41620
Mark Emlyn David Thomas <markt@apache.org>
2014-09-17 18:43:21 +0000

    Fix cyclic dependency
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1625735 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/threads/Constants.java
+++ java/org/apache/tomcat/util/threads/Constants.java
@@ -24,4 +24,9 @@ public final class Constants {
     public static final String Package = "org.apache.tomcat.util.threads";
 
     public static final long DEFAULT_THREAD_RENEWAL_DELAY = 1000L;
+
+    /**
+     * Has security been turned on?
+     */
+    public static final boolean IS_SECURITY_ENABLED = (System.getSecurityManager() != null);
 }
