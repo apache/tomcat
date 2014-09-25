fdb3e09621ff1380bbf7a3a644b7d5b83ca94015
Mark Thomas <markt@apache.org>
2014-09-25 19:32:53 +0000

    Add docs
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1627604 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/CredentialHandler.java
+++ java/org/apache/catalina/CredentialHandler.java
@@ -16,9 +16,35 @@
  */
 package org.apache.catalina;
 
+/**
+ * This interface is used by the {@link Realm} to compare the user provided
+ * credentials with the credentials stored in the {@link Realm} for that user.
+ */
 public interface CredentialHandler {
 
+    /**
+     * Checks to see if the input credentials match the stored credentials
+     *
+     * @param inputCredentials  User provided credentials
+     * @param storedCredentials Credentials stored in the {@link Realm}
+     *
+     * @return <code>true</code> if the inputCredentials match the
+     *         storedCredentials, otherwise <code>false</code>
+     */
     boolean matches(String inputCredentials, String storedCredentials);
 
+    /**
+     * Generates the equivalent stored credentials for the given input
+     * credentials.
+     *
+     * @param inputCredentials  User provided credentials
+     * @param salt              Salt, if any
+     * @param iterations        Number of iterations of the algorithm associated
+     *                          with this CredentialHandler applied to the
+     *                          inputCredentials to generate the equivalent
+     *                          stored credentials
+     * @return  The equivalent stored credentials for the given input
+     *          credentials
+     */
     String mutate(String inputCredentials, byte[] salt, int iterations);
 }
