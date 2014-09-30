ca29056d24838e67a4f30e7051f5a639e9311353
Mark Emlyn David Thomas <markt@apache.org>
2014-09-30 07:58:26 +0000

    Update comments
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1628369 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/tomcat/util/http/TestCookiesNoStrictNamingSysProps.java
+++ test/org/apache/tomcat/util/http/TestCookiesNoStrictNamingSysProps.java
@@ -14,7 +14,6 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-
 package org.apache.tomcat.util.http;
 
 import static org.junit.Assert.assertEquals;
@@ -25,11 +24,12 @@ import org.apache.catalina.startup.Tomcat;
 import org.apache.tomcat.util.buf.ByteChunk;
 
 /**
- * Test case for {@link LegacyCookieProcessor}. <b>Note</b> because of the use of <code>final
- * static</code> constants in {@link LegacyCookieProcessor}, each of these tests must be
- * executed in a new JVM instance. The tests have been place in separate classes
- * to facilitate this when running the unit tests via Ant.
+ * Test case for {@link LegacyCookieProcessor}. <b>Note</b> because of the use
+ * of <code>final static</code> constants in {@link CookieSupport}, each of
+ * these tests must be executed in a new JVM instance. The tests have been place
+ * in separate classes to facilitate this when running the unit tests via Ant.
  */
+@SuppressWarnings("javadoc")
 public class TestCookiesNoStrictNamingSysProps extends CookiesBaseTest {
 
     @Override
