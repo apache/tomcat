5734c9e57fca677ccd3253ba4bbb395e50569ce2
Mark Emlyn David Thomas <markt@apache.org>
2014-10-03 14:17:47 +0000

    Fix namespace inconsistencies
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1629205 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/users/MemoryUserDatabase.java
+++ java/org/apache/catalina/users/MemoryUserDatabase.java
@@ -546,7 +546,7 @@ public class MemoryUserDatabase implements UserDatabase {
             writer.println("<?xml version='1.0' encoding='utf-8'?>");
             writer.println("<tomcat-users xmlns=\"http://tomcat.apache.org/xml\"");
             writer.println("              xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"");
-            writer.println("              xsi:schemaLocation=\"http://tomcat.apache.org tomcat-users.xsd\"");
+            writer.println("              xsi:schemaLocation=\"http://tomcat.apache.org/xml tomcat-users.xsd\"");
             writer.println("              version=\"1.0\">");
 
             // Print entries for each defined role, group, and user
