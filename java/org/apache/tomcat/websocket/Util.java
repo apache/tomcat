153b0df7d2dba5eeef1917766fc8578387841867
Mark Emlyn David Thomas <markt@apache.org>
2014-10-08 19:01:20 +0000

    NPE protection. Note NPE only possible with test code.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1630199 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/websocket/Util.java
+++ java/org/apache/tomcat/websocket/Util.java
@@ -332,21 +332,23 @@ public class Util {
                     throws DeploymentException{
 
         List<DecoderEntry> result = new ArrayList<>();
-        for (Class<? extends Decoder> decoderClazz : decoderClazzes) {
-            // Need to instantiate decoder to ensure it is valid and that
-            // deployment can be failed if it is not
-            @SuppressWarnings("unused")
-            Decoder instance;
-            try {
-                instance = decoderClazz.newInstance();
-            } catch (InstantiationException | IllegalAccessException e) {
-                throw new DeploymentException(
-                        sm.getString("pojoMethodMapping.invalidDecoder",
-                                decoderClazz.getName()), e);
+        if (decoderClazzes != null) {
+            for (Class<? extends Decoder> decoderClazz : decoderClazzes) {
+                // Need to instantiate decoder to ensure it is valid and that
+                // deployment can be failed if it is not
+                @SuppressWarnings("unused")
+                Decoder instance;
+                try {
+                    instance = decoderClazz.newInstance();
+                } catch (InstantiationException | IllegalAccessException e) {
+                    throw new DeploymentException(
+                            sm.getString("pojoMethodMapping.invalidDecoder",
+                                    decoderClazz.getName()), e);
+                }
+                DecoderEntry entry = new DecoderEntry(
+                        Util.getDecoderType(decoderClazz), decoderClazz);
+                result.add(entry);
             }
-            DecoderEntry entry = new DecoderEntry(
-                    Util.getDecoderType(decoderClazz), decoderClazz);
-            result.add(entry);
         }
 
         return result;
