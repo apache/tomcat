6f9127953c91bd3e5870b3f7c1a7afe692f6bf9f
Mark Emlyn David Thomas <markt@apache.org>
2014-10-13 10:39:09 +0000

    Javadoc. Fix Java 8 warnings for this class.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1631342 13f79535-47bb-0310-9956-ffa450edef68

--- java/javax/servlet/AsyncContext.java
+++ java/javax/servlet/AsyncContext.java
@@ -39,23 +39,41 @@ public interface AsyncContext {
     boolean hasOriginalRequestAndResponse();
 
     /**
-     *
-     * @throws IllegalStateException
+     * @throws IllegalStateException if this method is called when the request
+     * is not in asynchronous mode. The request is in asynchronous mode after
+     * {@link javax.servlet.http.HttpServletRequest#startAsync()} or
+     * {@link javax.servlet.http.HttpServletRequest#startAsync(ServletRequest,
+     * ServletResponse)} has been called and before {@link #complete()} or any
+     * other dispatch() method has been called.
      */
     void dispatch();
 
     /**
+     * @param path The path to which the request/response should be dispatched
+     *             relative to the {@link ServletContext} from which this async
+     *             request was started.
      *
-     * @param path
-     * @throws IllegalStateException
+     * @throws IllegalStateException if this method is called when the request
+     * is not in asynchronous mode. The request is in asynchronous mode after
+     * {@link javax.servlet.http.HttpServletRequest#startAsync()} or
+     * {@link javax.servlet.http.HttpServletRequest#startAsync(ServletRequest,
+     * ServletResponse)} has been called and before {@link #complete()} or any
+     * other dispatch() method has been called.
      */
     void dispatch(String path);
 
     /**
+     * @param path The path to which the request/response should be dispatched
+     *             relative to the specified {@link ServletContext}.
+     * @param context The {@link ServletContext} to which the request/response
+     *                should be dispatched.
      *
-     * @param context
-     * @param path
-     * @throws IllegalStateException
+     * @throws IllegalStateException if this method is called when the request
+     * is not in asynchronous mode. The request is in asynchronous mode after
+     * {@link javax.servlet.http.HttpServletRequest#startAsync()} or
+     * {@link javax.servlet.http.HttpServletRequest#startAsync(ServletRequest,
+     * ServletResponse)} has been called and before {@link #complete()} or any
+     * other dispatch() method has been called.
      */
     void dispatch(ServletContext context, String path);
 
@@ -72,12 +90,17 @@ public interface AsyncContext {
     throws ServletException;
 
     /**
-     * Set timeout in milliseconds. 0 or less indicates no timeout.
+     * Set the timeout.
+     *
+     * @param timeout The timeout in milliseconds. 0 or less indicates no
+     *                timeout.
      */
     void setTimeout(long timeout);
 
     /**
-     * Get timeout in milliseconds. 0 or less indicates no timeout.
+     * Get the current.
+     *
+     * @return The timeout in milliseconds. 0 or less indicates no timeout.
      */
     long getTimeout();
 }
