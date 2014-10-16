a8c43c76d33e17429f5eacbb08fbe235c9910132
Violeta Georgieva Georgieva <violetagg@apache.org>
2014-10-16 07:46:36 +0000

    Merged revision 1631817 from tomcat/trunk: Fix client subprotocol handling (pass protocol names from the annotation).
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1632223 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/websocket/WsWebSocketContainer.java
+++ java/org/apache/tomcat/websocket/WsWebSocketContainer.java
@@ -162,6 +162,7 @@ public class WsWebSocketContainer
         ClientEndpointConfig config = builder.
                 decoders(Arrays.asList(annotation.decoders())).
                 encoders(Arrays.asList(annotation.encoders())).
+                preferredSubprotocols(Arrays.asList(annotation.subprotocols())).
                 build();
         return connectToServer(ep, config, path);
     }
