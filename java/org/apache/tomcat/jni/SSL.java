9ec0336ab3c61a7e8341e02f5a67f52d1998c892
Christopher Schultz <schultz@apache.org>
2014-10-17 13:52:24 +0000

    Maintain SSL_OP parity with OpenSSL: add SSL_OP_NO_TLSv1_1 and SSL_OP_NO_TLSv1_2.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1632577 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/jni/SSL.java
+++ java/org/apache/tomcat/jni/SSL.java
@@ -135,6 +135,9 @@ public final class SSL {
     public static final int SSL_OP_NO_SSLv2                         = 0x01000000;
     public static final int SSL_OP_NO_SSLv3                         = 0x02000000;
     public static final int SSL_OP_NO_TLSv1                         = 0x04000000;
+    public static final int SSL_OP_NO_TLSv1_1                       = 0x08000000;
+    public static final int SSL_OP_NO_TLSv1_2                       = 0x10000000;
+
     public static final int SSL_OP_NO_TICKET                        = 0x00004000;
 
     /* The next flag deliberately changes the ciphertest, this is a check
