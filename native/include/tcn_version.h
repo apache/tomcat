d4e225af4ecbcc546aae025947705382e9e8b147
Mark Thomas <markt@apache.org>
2014-10-17 19:51:18 +0000

    Bump version ready for next release
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/native/branches/1.1.x@1632658 13f79535-47bb-0310-9956-ffa450edef68

--- native/include/tcn_version.h
+++ native/include/tcn_version.h
@@ -69,7 +69,7 @@ extern "C" {
 #define TCN_MINOR_VERSION       1
 
 /** patch level */
-#define TCN_PATCH_VERSION       31
+#define TCN_PATCH_VERSION       32
 
 /**
  *  This symbol is defined for internal, "development" copies of TCN. This
