b0dd053bbeabf13b11be257f441d9f2f87dc3e71
Mark Thomas <markt@apache.org>
2014-10-20 12:41:49 +0000

    Fix typo
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/native/branches/1.1.x@1633136 13f79535-47bb-0310-9956-ffa450edef68

--- jnirelease.sh
+++ jnirelease.sh
@@ -178,7 +178,7 @@ else
    fi
    WIN_VERSION=`grep TCN_VERSION ${JKJNIDIST}/jni/native/os/win32/libtcnative.rc | grep define | awk ' { print $3 } '`
    if [ "x\"$JKJNIVER\"" != "x$WIN_VERSION" ]; then
-     echo "Check: ${JKJNIDIST}/jni/native/os/win32/libtcnative.rc says $WIN_VERSION (FILEVERSION, PRODUCTVERSION, TCN_VERSION"
+     echo "Check: ${JKJNIDIST}/jni/native/os/win32/libtcnative.rc says $WIN_VERSION (FILEVERSION, PRODUCTVERSION, TCN_VERSION)"
      exit 1
    fi
 fi
