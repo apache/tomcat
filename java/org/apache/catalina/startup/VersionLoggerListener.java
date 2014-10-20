08f589ebf7481ee2ec4407c4310d306b26618ce1
Violeta Georgieva Georgieva <violetagg@apache.org>
2014-10-20 14:24:58 +0000

    Fix https://issues.apache.org/bugzilla/show_bug.cgi?id=47919 Merged revisions 1630110, 1630407 from tomcat/trunk: - Extend the information logged when Tomcat starts to optionally include (enabled by default) the values of key environment variables. - Log all environment variables but disable this by default - Log all command line arguments passed to java.exe by default but include an option to exclude them
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1633165 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/startup/VersionLoggerListener.java
+++ java/org/apache/catalina/startup/VersionLoggerListener.java
@@ -16,6 +16,12 @@
  */
 package org.apache.catalina.startup;
 
+import java.lang.management.ManagementFactory;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 import org.apache.catalina.Lifecycle;
 import org.apache.catalina.LifecycleEvent;
 import org.apache.catalina.LifecycleListener;
@@ -36,6 +42,29 @@ public class VersionLoggerListener implements LifecycleListener {
      */
     protected static final StringManager sm = StringManager.getManager(Constants.Package);
 
+    private boolean logArgs = true;
+    private boolean logEnv = false;
+
+
+    public boolean getLogArgs() {
+        return logArgs;
+    }
+
+
+    public void setLogArgs(boolean logArgs) {
+        this.logArgs = logArgs;
+    }
+
+
+    public boolean getLogEnv() {
+        return logEnv;
+    }
+
+
+    public void setLogEnv(boolean logEnv) {
+        this.logEnv = logEnv;
+    }
+
 
     @Override
     public void lifecycleEvent(LifecycleEvent event) {
@@ -52,15 +81,36 @@ public class VersionLoggerListener implements LifecycleListener {
                 ServerInfo.getServerBuilt()));
         log.info(sm.getString("versionLoggerListener.serverInfo.server.number",
                 ServerInfo.getServerNumber()));
-        log.info(sm.getString("versionLoggerListener.serverInfo.os.name",
+        log.info(sm.getString("versionLoggerListener.os.name",
                 System.getProperty("os.name")));
-        log.info(sm.getString("versionLoggerListener.serverInfo.os.version",
+        log.info(sm.getString("versionLoggerListener.os.version",
                 System.getProperty("os.version")));
-        log.info(sm.getString("versionLoggerListener.serverInfo.os.arch",
+        log.info(sm.getString("versionLoggerListener.os.arch",
                 System.getProperty("os.arch")));
-        log.info(sm.getString("versionLoggerListener.serverInfo.vm.version",
+        log.info(sm.getString("versionLoggerListener.java.home",
+                System.getProperty("java.home")));
+        log.info(sm.getString("versionLoggerListener.vm.version",
                 System.getProperty("java.runtime.version")));
-        log.info(sm.getString("versionLoggerListener.serverInfo.vm.vendor",
+        log.info(sm.getString("versionLoggerListener.vm.vendor",
                 System.getProperty("java.vm.vendor")));
+        log.info(sm.getString("versionLoggerListener.catalina.base",
+                System.getProperty("catalina.base")));
+        log.info(sm.getString("versionLoggerListener.catalina.home",
+                System.getProperty("catalina.home")));
+
+        if (logArgs) {
+            List<String> args = ManagementFactory.getRuntimeMXBean().getInputArguments();
+            for (String arg : args) {
+                log.info(sm.getString("versionLoggerListener.arg", arg));
+            }
+        }
+
+        if (logEnv) {
+            Map<String,String> envs = System.getenv();
+            SortedSet<String> keys = new TreeSet<String>(envs.keySet());
+            for (String key : keys) {
+                log.info(sm.getString("versionLoggerListener.env", key, envs.get(key)));
+            }
+        }
     }
 }
