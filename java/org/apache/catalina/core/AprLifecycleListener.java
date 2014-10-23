c917cfbf0bf4ee5b95b61cfe8f560e25f5a4762d
Mark Thomas <markt@apache.org>
2014-10-23 14:47:14 +0000

    Require Tomcat Native 1.1.32
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1633825 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/core/AprLifecycleListener.java
+++ java/org/apache/catalina/core/AprLifecycleListener.java
@@ -66,9 +66,9 @@ public class AprLifecycleListener
 
     protected static final int TCN_REQUIRED_MAJOR = 1;
     protected static final int TCN_REQUIRED_MINOR = 1;
-    protected static final int TCN_REQUIRED_PATCH = 30;
+    protected static final int TCN_REQUIRED_PATCH = 32;
     protected static final int TCN_RECOMMENDED_MINOR = 1;
-    protected static final int TCN_RECOMMENDED_PV = 30;
+    protected static final int TCN_RECOMMENDED_PV = 32;
 
 
     // ---------------------------------------------- Properties
