d8fac528c295bc287dc57157a468355947701de6
Mark Emlyn David Thomas <markt@apache.org>
2014-10-24 18:21:33 +0000

    Simplify
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1634114 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/buf/ByteChunk.java
+++ java/org/apache/tomcat/util/buf/ByteChunk.java
@@ -499,8 +499,7 @@ public final class ByteChunk implements Cloneable, Serializable {
         // new String(byte[], int, int, Charset) takes a defensive copy of the
         // entire byte array. This is expensive if only a small subset of the
         // bytes will be used. The code below is from Apache Harmony.
-        CharBuffer cb;
-        cb = charset.decode(ByteBuffer.wrap(buff, start, end-start));
+        CharBuffer cb = charset.decode(ByteBuffer.wrap(buff, start, end-start));
         return new String(cb.array(), cb.arrayOffset(), cb.length());
     }
 
