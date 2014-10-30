aaa3eac3e7de61f36ab62d924e231198b2991651
Remy Maucherat <remm@apache.org>
2014-10-30 08:37:01 +0000

    Fileupload respects header ordering, but the part implementation doesn't. Also use a linked collection there to fix the discrepancy.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1635429 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/core/ApplicationPart.java
+++ java/org/apache/catalina/core/ApplicationPart.java
@@ -22,8 +22,8 @@ import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Locale;
 import java.util.Map;
 
@@ -69,7 +69,7 @@ public class ApplicationPart implements Part {
     @Override
     public Collection<String> getHeaderNames() {
         if (fileItem instanceof DiskFileItem) {
-            HashSet<String> headerNames = new HashSet<>();
+            LinkedHashSet<String> headerNames = new LinkedHashSet<>();
             Iterator<String> iter =
                 ((DiskFileItem) fileItem).getHeaders().getHeaderNames();
             while (iter.hasNext()) {
@@ -83,7 +83,7 @@ public class ApplicationPart implements Part {
     @Override
     public Collection<String> getHeaders(String name) {
         if (fileItem instanceof DiskFileItem) {
-            HashSet<String> headers = new HashSet<>();
+            LinkedHashSet<String> headers = new LinkedHashSet<>();
             Iterator<String> iter =
                 ((DiskFileItem) fileItem).getHeaders().getHeaders(name);
             while (iter.hasNext()) {
