f16102197705c23e13008556f4cdc0d26e802b31
Mark Thomas <markt@apache.org>
2014-12-11 19:23:27 +0000

    Start to put together some unit tests for the RewriteValve based on Konstantin's concerns raised in https://issues.apache.org/bugzilla/show_bug.cgi?id=57215
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1644730 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test/org/apache/catalina/valves/rewrite/TestRewriteValve.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.catalina.valves.rewrite;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.startup.TomcatBaseTest;
+import org.apache.tomcat.util.buf.ByteChunk;
+
+public class TestRewriteValve extends TomcatBaseTest {
+
+    @Test
+    public void testNoRewrite() throws Exception {
+        doTestRewrite("");
+    }
+
+    @Test
+    @Ignore // getRequestURI is not encoded
+    public void testNoopRewrite() throws Exception {
+        doTestRewrite("RewriteRule ^(.*) $1");
+    }
+
+    private void doTestRewrite(String config) throws Exception {
+        Tomcat tomcat = getTomcatInstance();
+
+        // No file system docBase required
+        Context ctx = tomcat.addContext("", null);
+
+        RewriteValve rewriteValve = new RewriteValve();
+        ctx.getPipeline().addValve(rewriteValve);
+
+        rewriteValve.setConfiguration(config);
+
+        // Note: URLPatterns should be URL encoded
+        //       (http://svn.apache.org/r285186)
+        Tomcat.addServlet(ctx, "snoop", new SnoopServlet());
+        ctx.addServletMapping("/a/%255A", "snoop");
+
+        tomcat.start();
+
+        ByteChunk res = getUrl("http://localhost:" + getPort() + "/a/%255A");
+
+        String body = res.toString();
+        Assert.assertTrue(body, body.contains("/a/%255A"));
+    }
+}
