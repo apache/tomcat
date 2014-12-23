3397e03b84d1e1d602acda0cfba83df73438fd7b
Mark Thomas <markt@apache.org>
2014-12-23 09:02:24 +0000

    Remove duplicate code
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1647508 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/http/HttpMessages.java
+++ java/org/apache/tomcat/util/http/HttpMessages.java
@@ -110,43 +110,6 @@ public class HttpMessages {
 
 
     /**
-     * Filter the specified message string for characters that are sensitive
-     * in HTML.  This avoids potential attacks caused by including JavaScript
-     * codes in the request URL that is often reported in error messages.
-     *
-     * @param message The message string to be filtered
-     */
-    public static String filter(String message) {
-
-        if (message == null) {
-            return (null);
-        }
-
-        char content[] = new char[message.length()];
-        message.getChars(0, message.length(), content, 0);
-        StringBuilder result = new StringBuilder(content.length + 50);
-        for (int i = 0; i < content.length; i++) {
-            switch (content[i]) {
-            case '<':
-                result.append("&lt;");
-                break;
-            case '>':
-                result.append("&gt;");
-                break;
-            case '&':
-                result.append("&amp;");
-                break;
-            case '"':
-                result.append("&quot;");
-                break;
-            default:
-                result.append(content[i]);
-            }
-        }
-        return (result.toString());
-    }
-
-    /**
      * Is the provided message safe to use in an HTTP header. Safe messages must
      * meet the requirements of RFC2616 - i.e. must consist only of TEXT.
      *
