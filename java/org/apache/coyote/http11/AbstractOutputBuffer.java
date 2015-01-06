2cf4c2fa1562975e936e6718599b3f5cb1689902
Mark Thomas <markt@apache.org>
2015-01-06 11:06:13 +0000

    Make final
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1649778 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http11/AbstractOutputBuffer.java
+++ java/org/apache/coyote/http11/AbstractOutputBuffer.java
@@ -93,7 +93,7 @@ public abstract class AbstractOutputBuffer<S> implements OutputBuffer {
     /**
      * Underlying output buffer.
      */
-    protected OutputBuffer outputStreamOutputBuffer;
+    protected final OutputBuffer outputStreamOutputBuffer;
 
     /**
      * Bytes written to client for the current request
