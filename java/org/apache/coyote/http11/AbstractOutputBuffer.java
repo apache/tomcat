86720e2250e5f06ac741c3a1f447f387a214f59a
Mark Thomas <markt@apache.org>
2015-01-06 11:08:23 +0000

    Revert since it breaks a unit test
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1649780 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http11/AbstractOutputBuffer.java
+++ java/org/apache/coyote/http11/AbstractOutputBuffer.java
@@ -93,7 +93,7 @@ public abstract class AbstractOutputBuffer<S> implements OutputBuffer {
     /**
      * Underlying output buffer.
      */
-    protected final OutputBuffer outputStreamOutputBuffer;
+    protected OutputBuffer outputStreamOutputBuffer;
 
     /**
      * Bytes written to client for the current request
