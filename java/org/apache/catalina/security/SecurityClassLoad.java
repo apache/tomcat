ff4f3b6d9f4a36a79023f7e16547c823e064d9bc
Mark Thomas <markt@apache.org>
2015-01-09 10:05:24 +0000

    Remove unnecessary code since o.a.coyote.http11.Constants is now pre-loaded.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1650477 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/security/SecurityClassLoad.java
+++ java/org/apache/catalina/security/SecurityClassLoad.java
@@ -163,7 +163,6 @@ public final class SecurityClassLoad {
     private static final void loadCoyotePackage(ClassLoader loader)
             throws Exception {
         final String basePackage = "org.apache.coyote.";
-        loader.loadClass(basePackage + "http11.Http11OutputBuffer$1");
         loader.loadClass(basePackage + "http11.Constants");
         // Make sure system property is read at this point
         Class<?> clazz = loader.loadClass(basePackage + "Constants");
