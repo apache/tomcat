ddf42b915cb8c87e36cb9d5ca7e9ad245a95b263
Mark Thomas <markt@apache.org>
2015-02-16 17:20:40 +0000

    Add some debug information to memory leak tests that are currently failing on Gump.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1660168 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/tomcat/unittest/TesterCounter.java
+++ test/org/apache/tomcat/unittest/TesterCounter.java
@@ -16,7 +16,18 @@
  */
 package org.apache.tomcat.unittest;
 
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+
 public class TesterCounter {
+
+    private static final Log log = LogFactory.getLog(TesterCounter.class);
+
+    static {
+        log.info("TestCounter loaded by " + TesterCounter.class.getClassLoader() +
+                " in thread " + Thread.currentThread().getName());
+    }
+
     private int count = 0;
 
     public void increment() {
--- test/org/apache/tomcat/unittest/TesterLeakingServlet2.java
+++ test/org/apache/tomcat/unittest/TesterLeakingServlet2.java
@@ -25,10 +25,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+
 
 public class TesterLeakingServlet2 extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
+    private static final Log log = LogFactory.getLog(TesterLeakingServlet2.class);
+
 
     @Override
     protected void doGet(HttpServletRequest request,
@@ -38,6 +43,7 @@ public class TesterLeakingServlet2 extends HttpServlet {
         List<TesterCounter> counterList = TesterThreadScopedHolder.getFromHolder();
         TesterCounter counter;
         if (counterList == null) {
+            log.info("Adding thread local to thread " + Thread.currentThread().getName());
             counter = new TesterCounter();
             TesterThreadScopedHolder.saveInHolder(Arrays.asList(counter));
         } else {
