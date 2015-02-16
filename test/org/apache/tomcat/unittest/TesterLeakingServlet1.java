2b325fe521c2ab7dc68072aee2f48e4fadc0154d
Mark Thomas <markt@apache.org>
2015-02-16 17:20:40 +0000

    Add some debug information to memory leak tests that are currently failing on Gump.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1660168 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/tomcat/unittest/TesterLeakingServlet1.java
+++ test/org/apache/tomcat/unittest/TesterLeakingServlet1.java
@@ -23,7 +23,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+
 public class TesterLeakingServlet1 extends HttpServlet {
+
+    private static final Log log = LogFactory.getLog(TesterLeakingServlet1.class);
 
     private static final long serialVersionUID = 1L;
 
@@ -36,6 +41,7 @@ public class TesterLeakingServlet1 extends HttpServlet {
 
         TesterCounter counter = myThreadLocal.get();
         if (counter == null) {
+            log.info("Adding thread local to thread " + Thread.currentThread().getName());
             counter = new TesterCounter();
             myThreadLocal.set(counter);
         }
