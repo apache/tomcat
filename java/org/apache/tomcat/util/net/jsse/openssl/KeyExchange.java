83c3c37e5455095b3946139083f763f98d27c462
Mark Thomas <markt@apache.org>
2015-02-21 16:25:45 +0000

    For future reference, add the OpenSSL constant names to aid selecting the right values when adding new Ciphers.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1661388 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/net/jsse/openssl/KeyExchange.java
+++ java/org/apache/tomcat/util/net/jsse/openssl/KeyExchange.java
@@ -18,16 +18,16 @@
 package org.apache.tomcat.util.net.jsse.openssl;
 
 enum KeyExchange {
-    EECDH /* ephemeral ECDH */,
-    RSA /* RSA key exchange */,
-    DHr /* DH cert, RSA CA cert */ /* no such ciphersuites supported! */,
-    DHd /* DH cert, DSA CA cert */ /* no such ciphersuite supported! */,
-    EDH /* tmp DH key no DH cert */,
-    PSK /* PSK */,
-    FZA /* Fortezza */  /* no such ciphersuite supported! */,
-    KRB5 /* Kerberos 5 key exchange */,
-    ECDHr /* ECDH cert, RSA CA cert */,
-    ECDHe /* ECDH cert, ECDSA CA cert */,
-    GOST /* GOST key exchange */,
-    SRP /* SRP */;
+    EECDH /* SSL_kECDHE - ephemeral ECDH */,
+    RSA   /* SSL_kRSA   - RSA key exchange */,
+    DHr   /* SSL_kDHr   - DH cert, RSA CA cert */ /* no such ciphersuites supported! */,
+    DHd   /* SSL_kDHd   - DH cert, DSA CA cert */ /* no such ciphersuite supported! */,
+    EDH   /* SSL_kDHE   - tmp DH key no DH cert */,
+    PSK   /* SSK_kPSK   - PSK */,
+    FZA   /* SSL_kFZA   - Fortezza */  /* no such ciphersuite supported! */,
+    KRB5  /* SSL_kKRB5  - Kerberos 5 key exchange */,
+    ECDHr /* SSL_kECDHr - ECDH cert, RSA CA cert */,
+    ECDHe /* SSL_eECDHe - ECDH cert, ECDSA CA cert */,
+    GOST  /* SSL_kGOST  - GOST key exchange */,
+    SRP   /* SSL_kSRP   - SRP */;
 }
