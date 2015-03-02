fa66012c7c655ce0556265660422617864aaa8fc
Mark Thomas <markt@apache.org>
2015-03-02 12:07:11 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=57602 Ensure HEAD requests return the correct content length (i.e. the same as for a GET) when the requested resource includes a resource served by a servlet that extends HttpServlet.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1663279 13f79535-47bb-0310-9956-ffa450edef68

--- test/javax/servlet/http/TestHttpServlet.java
+++ test/javax/servlet/http/TestHttpServlet.java
@@ -17,6 +17,7 @@
 package javax.servlet.http;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -68,6 +69,78 @@ public class TestHttpServlet extends TomcatBaseTest {
         protected void doGet(HttpServletRequest req, HttpServletResponse resp)
                 throws ServletException, IOException {
             resp.setHeader("content-length", RESPONSE_LENGTH);
+        }
+    }
+
+
+    /**
+     * Verifies that the same Content-Length is returned for both GET and HEAD
+     * operations when a Servlet includes content from another Servlet
+     */
+    @Test
+    public void testBug57602() throws Exception {
+        Tomcat tomcat = getTomcatInstance();
+
+        // No file system docBase required
+        StandardContext ctx = (StandardContext) tomcat.addContext("", null);
+
+        Bug57602ServletOuter outer = new Bug57602ServletOuter();
+        Tomcat.addServlet(ctx, "Bug57602ServletOuter", outer);
+        ctx.addServletMapping("/outer", "Bug57602ServletOuter");
+
+        Bug57602ServletInner inner = new Bug57602ServletInner();
+        Tomcat.addServlet(ctx, "Bug57602ServletInner", inner);
+        ctx.addServletMapping("/inner", "Bug57602ServletInner");
+
+        tomcat.start();
+
+        Map<String,List<String>> resHeaders= new HashMap<>();
+        String path = "http://localhost:" + getPort() + "/outer";
+        ByteChunk out = new ByteChunk();
+
+        int rc = getUrl(path, out, resHeaders);
+        Assert.assertEquals(HttpServletResponse.SC_OK, rc);
+        String length = resHeaders.get("Content-Length").get(0);
+        Assert.assertEquals(Long.parseLong(length), out.getLength());
+        out.recycle();
+
+        rc = headUrl(path, out, resHeaders);
+        Assert.assertEquals(HttpServletResponse.SC_OK, rc);
+        Assert.assertEquals(0, out.getLength());
+        Assert.assertEquals(length, resHeaders.get("Content-Length").get(0));
+
+        tomcat.stop();
+    }
+
+
+    private static class Bug57602ServletOuter extends HttpServlet {
+
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+                throws ServletException, IOException {
+            resp.setContentType("text/plain");
+            resp.setCharacterEncoding("UTF-8");
+            PrintWriter pw = resp.getWriter();
+            pw.println("Header");
+            req.getRequestDispatcher("/inner").include(req, resp);
+            pw.println("Footer");
+        }
+    }
+
+
+    private static class Bug57602ServletInner extends HttpServlet {
+
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+                throws ServletException, IOException {
+            resp.setContentType("text/plain");
+            resp.setCharacterEncoding("UTF-8");
+            PrintWriter pw = resp.getWriter();
+            pw.println("Included");
         }
     }
 }
