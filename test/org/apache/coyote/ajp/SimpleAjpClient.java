2f919dfc56c48831cd75f429a7d35bbded3935e2
Mark Thomas <markt@apache.org>
2015-03-07 15:16:50 +0000

    Make the packet size used by the test AJP client configurable
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1664863 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/coyote/ajp/SimpleAjpClient.java
+++ test/org/apache/coyote/ajp/SimpleAjpClient.java
@@ -30,7 +30,7 @@ import javax.net.SocketFactory;
  */
 public class SimpleAjpClient {
 
-    private static final int AJP_PACKET_SIZE = 8192;
+    private static final int DEFAULT_AJP_PACKET_SIZE = 8192;
     private static final byte[] AJP_CPING;
 
     static {
@@ -43,6 +43,7 @@ public class SimpleAjpClient {
                 ajpCping.getLen());
     }
 
+    private final int packetSize;
     private String host = "localhost";
     private int port = -1;
     /* GET == 2 */
@@ -55,6 +56,14 @@ public class SimpleAjpClient {
     private int serverPort = 80;
     private boolean ssl = false;
     private Socket socket = null;
+
+    public SimpleAjpClient() {
+        this(DEFAULT_AJP_PACKET_SIZE);
+    }
+
+    public SimpleAjpClient(int packetSize) {
+        this.packetSize = packetSize;
+    }
 
     public void setPort(int port) {
         this.port = port;
@@ -284,7 +293,7 @@ public class SimpleAjpClient {
      */
     public TesterAjpMessage createForwardMessage() {
 
-        TesterAjpMessage message = new TesterAjpMessage(AJP_PACKET_SIZE);
+        TesterAjpMessage message = new TesterAjpMessage(packetSize);
         message.reset();
 
         // Set the header bytes
@@ -323,7 +332,7 @@ public class SimpleAjpClient {
 
     public TesterAjpMessage createBodyMessage(byte[] data) {
 
-        TesterAjpMessage message = new TesterAjpMessage(AJP_PACKET_SIZE);
+        TesterAjpMessage message = new TesterAjpMessage(packetSize);
         message.reset();
 
         // Set the header bytes
@@ -376,7 +385,7 @@ public class SimpleAjpClient {
 
         InputStream is = socket.getInputStream();
 
-        TesterAjpMessage message = new TesterAjpMessage(AJP_PACKET_SIZE);
+        TesterAjpMessage message = new TesterAjpMessage(packetSize);
 
         byte[] buf = message.getBuffer();
 
