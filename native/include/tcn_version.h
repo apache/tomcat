bdd1b645e6036d10cd5fd7a432f0dd06cb7a3933
Mark Thomas <markt@apache.org>
2015-03-11 10:14:17 +0000

    Update version to 1.1.33
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/native/branches/1.1.x@1665830 13f79535-47bb-0310-9956-ffa450edef68

--- native/include/tcn_version.h
+++ native/include/tcn_version.h
@@ -69,7 +69,7 @@ extern "C" {
 #define TCN_MINOR_VERSION       1
 
 /** patch level */
-#define TCN_PATCH_VERSION       32
+#define TCN_PATCH_VERSION       33
 
 /**
  *  This symbol is defined for internal, "development" copies of TCN. This
