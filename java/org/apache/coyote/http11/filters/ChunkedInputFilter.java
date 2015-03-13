b2f8befea65cdd38079b34cb0606f211cee4c80b
Mark Thomas <markt@apache.org>
2015-03-13 11:28:37 +0000

    Fix Java 6 compatibility
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1666407 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http11/filters/ChunkedInputFilter.java
+++ java/org/apache/coyote/http11/filters/ChunkedInputFilter.java
@@ -600,7 +600,7 @@ public class ChunkedInputFilter implements InputFilter {
         }
     
         String headerName = new String(trailingHeaders.getBytes(), startPos,
-                colonPos - startPos, StandardCharsets.ISO_8859_1);
+                colonPos - startPos, "ISO_8859_1");
     
         if (allowedTrailerHeaders.contains(headerName.trim().toLowerCase(Locale.ENGLISH))) {
             MessageBytes headerValue = headers.addValue(headerName);
