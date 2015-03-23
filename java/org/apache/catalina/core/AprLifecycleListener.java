af82d2f696d293e750db8763255fc867ebc5a424
Mark Thomas <markt@apache.org>
2015-03-23 13:32:19 +0000

    Update tc-native to 1.1.33 and make this the minimum recommended version due to the potential crash on OS X, FreeBSD and possibly other platforms.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1668635 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/core/AprLifecycleListener.java
+++ java/org/apache/catalina/core/AprLifecycleListener.java
@@ -70,7 +70,7 @@ public class AprLifecycleListener
     protected static final int TCN_REQUIRED_MINOR = 1;
     protected static final int TCN_REQUIRED_PATCH = 32;
     protected static final int TCN_RECOMMENDED_MINOR = 1;
-    protected static final int TCN_RECOMMENDED_PV = 32;
+    protected static final int TCN_RECOMMENDED_PV = 33;
 
 
     // ---------------------------------------------- Properties
