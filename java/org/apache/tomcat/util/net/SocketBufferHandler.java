a8a45aa068d2077ed99fd636c87ce8d9bb039a61
Mark Thomas <markt@apache.org>
2015-04-13 13:50:55 +0000

    Fix buffer expansion.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1673189 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/net/SocketBufferHandler.java
+++ java/org/apache/tomcat/util/net/SocketBufferHandler.java
@@ -151,9 +151,9 @@ public class SocketBufferHandler {
 
 
     public void expand(int newSize) {
-        configureReadBufferForRead();
+        configureReadBufferForWrite();
         readBuffer = ByteBufferUtils.expand(readBuffer, newSize);
-        configureWriteBufferForRead();
+        configureWriteBufferForWrite();
         writeBuffer = ByteBufferUtils.expand(writeBuffer, newSize);
     }
 }
