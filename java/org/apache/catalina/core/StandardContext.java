35a14cc5a6b722451202412e292637e294fc271f
Mark Thomas <markt@apache.org>
2015-04-22 20:05:02 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=57841 Improve error logging during context start
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1675486 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/core/StandardContext.java
+++ java/org/apache/catalina/core/StandardContext.java
@@ -4963,7 +4963,7 @@ public class StandardContext extends ContainerBase
             try {
                 setResources(new StandardRoot(this));
             } catch (IllegalArgumentException e) {
-                log.error("Error initializing resources: " + e.getMessage());
+                log.error(sm.getString("standardContext.resourcesInit"), e);
                 ok = false;
             }
         }
@@ -4989,7 +4989,7 @@ public class StandardContext extends ContainerBase
             dependencyCheck = ExtensionValidator.validateApplication
                 (getResources(), this);
         } catch (IOException ioe) {
-            log.error("Error in dependencyCheck", ioe);
+            log.error(sm.getString("standardContext.extensionValidationError"), ioe);
             dependencyCheck = false;
         }
 
@@ -5115,7 +5115,7 @@ public class StandardContext extends ContainerBase
             }
 
             if (!getConfigured()) {
-                log.error( "Error getConfigured");
+                log.error(sm.getString("standardContext.configurationFail"));
                 ok = false;
             }
 
@@ -5164,7 +5164,7 @@ public class StandardContext extends ContainerBase
             // Configure and call application event listeners
             if (ok) {
                 if (!listenerStart()) {
-                    log.error( "Error listenerStart");
+                    log.error(sm.getString("standardContext.listenerFail"));
                     ok = false;
                 }
             }
@@ -5183,14 +5183,14 @@ public class StandardContext extends ContainerBase
                     ((Lifecycle) manager).start();
                 }
             } catch(Exception e) {
-                log.error("Error manager.start()", e);
+                log.error(sm.getString("standardContext.managerFail"), e);
                 ok = false;
             }
 
             // Configure and call application filters
             if (ok) {
                 if (!filterStart()) {
-                    log.error("Error filterStart");
+                    log.error(sm.getString("standardContext.filterFail"));
                     ok = false;
                 }
             }
@@ -5198,7 +5198,7 @@ public class StandardContext extends ContainerBase
             // Load and initialize all "load on startup" servlets
             if (ok) {
                 if (!loadOnStartup(findChildren())){
-                    log.error("Error loadOnStartup");
+                    log.error(sm.getString("standardContext.servletFail"));
                     ok = false;
                 }
             }
