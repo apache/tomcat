f98e103efdae3aee6ec217991093d214f0f9eb50
Mark Thomas <markt@apache.org>
2015-05-06 12:36:50 +0000

    Use the truststoreProvider rather than the keystoreProvider for the trust manager
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1677971 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/net/jsse/JSSESocketFactory.java
+++ java/org/apache/tomcat/util/net/jsse/JSSESocketFactory.java
@@ -545,8 +545,7 @@ public class JSSESocketFactory implements ServerSocketFactory, SSLUtil {
             algorithm = TrustManagerFactory.getDefaultAlgorithm();
         }
 
-        return getTrustManagers(truststoreType, endpoint.getKeystoreProvider(),
-                algorithm);
+        return getTrustManagers(truststoreType, endpoint.getTruststoreProvider(), algorithm);
     }
 
     @Override
