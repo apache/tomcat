08b64181036b94d143a467c4da2a1452d790b27e
Mark Thomas <markt@apache.org>
2015-05-07 10:32:51 +0000

    Fix an i18n TODO
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1678156 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/net/AbstractJsseEndpoint.java
+++ java/org/apache/tomcat/util/net/AbstractJsseEndpoint.java
@@ -67,8 +67,8 @@ public abstract class AbstractJsseEndpoint<S> extends AbstractEndpoint<S> {
         SSLHostConfig sslHostConfig = getSSLHostConfig(sniHostName);
         SSLContextWrapper sslContextWrapper = (SSLContextWrapper) sslHostConfig.getSslContext();
         if (sslContextWrapper == null) {
-            // TODO i18n
-            throw new IllegalStateException();
+            throw new IllegalStateException(
+                    sm.getString("endpoint.jsse.noSslContext", sniHostName));
         }
 
         SSLEngine engine = sslContextWrapper.getSSLContext().createSSLEngine();
