238abfa014230bf77d2bcd0f1b554aa4d69286ee
Mark Thomas <markt@apache.org>
2015-05-12 22:29:45 +0000

    Start to process the initial settings frame sent by the client.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1679116 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http2/Http2UpgradeHandler.java
+++ java/org/apache/coyote/http2/Http2UpgradeHandler.java
@@ -16,6 +16,7 @@
  */
 package org.apache.coyote.http2;
 
+import java.io.EOFException;
 import java.io.IOException;
 
 import javax.servlet.http.WebConnection;
@@ -46,11 +47,16 @@ public class Http2UpgradeHandler implements InternalHttpUpgradeHandler {
     private static final Log log = LogFactory.getLog(Http2UpgradeHandler.class);
     private static final StringManager sm = StringManager.getManager(Http2UpgradeHandler.class);
 
+    private static final int FRAME_SETTINGS = 4;
+
     private volatile SocketWrapperBase<?> socketWrapper;
     private volatile boolean initialized = false;
     private volatile ConnectionPrefaceParser connectionPrefaceParser =
             new ConnectionPrefaceParser();
-    private volatile boolean readFirstFrame = false;
+    private volatile boolean firstFrame = true;
+    private volatile boolean open = true;
+
+    private volatile int settingsMaxFrameSize = 16 * 1024;
 
 
     @Override
@@ -89,10 +95,19 @@ public class Http2UpgradeHandler implements InternalHttpUpgradeHandler {
             }
             connectionPrefaceParser = null;
 
-            while (processFrame()) {
+            try {
+                while (processFrame()) {
+                }
+                // TODO Catch the Http2Exception and reset the stream / close
+                // the connection as appropriate
+            } catch (IOException ioe) {
+                log.error("TODO: i18n - Frame processing error", ioe);
+                open = false;
             }
 
-            // TODO: CLOSED (GO_AWAY + no open streams apart from 0?) vs LONG
+            if (open) {
+                return SocketState.LONG;
+            }
             break;
 
         case OPEN_WRITE:
@@ -124,8 +139,123 @@ public class Http2UpgradeHandler implements InternalHttpUpgradeHandler {
     }
 
 
-    private boolean processFrame() {
+    private boolean processFrame() throws IOException {
+        // TODO: Consider refactoring and making this a field to reduce GC.
+        byte[] frameHeader = new byte[9];
+        if (!getFrameHeader(frameHeader)) {
+            return false;
+        }
+
+        int frameType = getFrameType(frameHeader);
+        int streamId = getStreamIdentifier(frameHeader);
+        int payloadSize = getPayloadSize(streamId, frameHeader);
+
+        switch (frameType) {
+        case FRAME_SETTINGS:
+            processFrameSettings(streamId, payloadSize);
+            break;
+        default:
+            // Unknown frame type.
+            processFrameUnknown(streamId, frameType, payloadSize);
+        }
         return false;
+    }
+
+
+    private void processFrameSettings(int streamId, int payloadSize) throws IOException {
+        if (streamId != 0) {
+            // TODO i18n
+            throw new Http2Exception("", 0, Http2Exception.FRAME_SIZE_ERROR);
+        }
+
+        if (payloadSize % 6 != 0) {
+            // TODO i18n
+            throw new Http2Exception("", 0, Http2Exception.FRAME_SIZE_ERROR);
+        }
+    }
+
+
+    private void processFrameUnknown(int streamId, int type, int payloadSize) throws IOException {
+        // Swallow the payload
+        log.info("Swallowing [" + payloadSize + "] bytes of unknown frame type + [" + type +
+                "] from stream [" + streamId + "]");
+        swallowPayload(payloadSize);
+    }
+
+
+    private void swallowPayload(int payloadSize) throws IOException {
+        int read = 0;
+        byte[] buffer = new byte[8 * 1024];
+        while (read < payloadSize) {
+            int toRead = Math.min(buffer.length, payloadSize - read);
+            int thisTime = socketWrapper.read(true, buffer, 0, toRead);
+            if (thisTime == -1) {
+                throw new IOException("TODO: i18n");
+            }
+            read += thisTime;
+        }
+    }
+
+
+    private boolean getFrameHeader(byte[] frameHeader) throws IOException {
+        // All frames start with a fixed size header.
+        int headerBytesRead = socketWrapper.read(false, frameHeader, 0, frameHeader.length);
+
+        // No frame header read. Non-blocking between frames, so return.
+        if (headerBytesRead == 0) {
+            return false;
+        }
+
+        // Partial header read. Blocking within a frame to block while the
+        // remainder is read.
+        if (headerBytesRead < frameHeader.length) {
+            int read = socketWrapper.read(true, frameHeader, headerBytesRead,
+                    frameHeader.length - headerBytesRead);
+            if (read == -1) {
+                // TODO i18n
+                throw new EOFException();
+            }
+        }
+
+        return true;
+    }
+
+
+    private int getFrameType(byte[] frameHeader) throws IOException {
+        int frameType = frameHeader[3] & 0xFF;
+        // Make sure the first frame is a settings frame
+        if (firstFrame) {
+            if (frameType != FRAME_SETTINGS) {
+                // TODO i18n
+                throw new Http2Exception("", 0, Http2Exception.PROTOCOL_ERROR);
+            } else {
+                firstFrame = false;
+            }
+        }
+        return frameType;
+    }
+
+
+    private int getStreamIdentifier(byte[] frameHeader) {
+        // MSB of [5] is reserved and must be ignored.
+        return ((frameHeader[5] & 0x7F) << 24) + ((frameHeader[6] & 0xFF) << 16) +
+                ((frameHeader[7] & 0xFF) << 6) + (frameHeader[8] & 0xFF);
+    }
+
+
+    private int getPayloadSize(int streamId, byte[] frameHeader) throws IOException {
+        // Make sure the payload size is valid
+        int payloadSize = ((frameHeader[0] & 0xFF) << 16) +
+                ((frameHeader[1] & 0xFF) << 8) +
+                (frameHeader[2] & 0xFF);
+
+        if (payloadSize > settingsMaxFrameSize) {
+            swallowPayload(payloadSize);
+            // TODO i18n
+            throw new Http2Exception("", streamId, Http2Exception.FRAME_SIZE_ERROR);
+        }
+
+        return payloadSize;
     }
 
 
