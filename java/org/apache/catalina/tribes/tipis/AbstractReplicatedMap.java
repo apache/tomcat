9ddfd6107c8b928c2264ede28da2245e42e7309f
Keiichi Fujino <kfujino@apache.org>
2015-05-13 02:04:50 +0000

    Ensure that the state transfer flag is updated to true only when the map states have been transferred correctly from existing map members.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1679126 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/tribes/tipis/AbstractReplicatedMap.java
+++ java/org/apache/catalina/tribes/tipis/AbstractReplicatedMap.java
@@ -495,6 +495,7 @@ public abstract class AbstractReplicatedMap<K,V>
                             messageReceived( (Serializable) list.get(i), resp[0].getSource());
                         } //for
                     }
+                    stateTransferred = true;
                 } else {
                     log.warn("Transfer state, 0 replies, probably a timeout.");
                 }
@@ -506,7 +507,6 @@ public abstract class AbstractReplicatedMap<K,V>
         } catch (ClassNotFoundException x) {
             log.error("Unable to transfer LazyReplicatedMap state.", x);
         }
-        stateTransferred = true;
     }
 
     /**
