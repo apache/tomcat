0b11a7b0ec9fda1aed87a7d1e3684c49007420eb
Keiichi Fujino <kfujino@apache.org>
2015-05-21 09:51:41 +0000

    Use StringManager to provide i18n support in the org.apache.catalina.tribes.io package.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1680790 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/tribes/io/XByteBuffer.java
+++ java/org/apache/catalina/tribes/io/XByteBuffer.java
@@ -27,6 +27,7 @@ import java.io.Serializable;
 import java.nio.ByteBuffer;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.apache.catalina.tribes.util.StringManager;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
 
@@ -49,6 +50,7 @@ import org.apache.juli.logging.LogFactory;
 public class XByteBuffer {
 
     private static final Log log = LogFactory.getLog(XByteBuffer.class);
+    protected static final StringManager sm = StringManager.getManager(XByteBuffer.class);
 
     /**
      * This is a package header, 7 bytes (FLT2002)
@@ -106,13 +108,13 @@ public class XByteBuffer {
     }
 
     public void setLength(int size) {
-        if ( size > buf.length ) throw new ArrayIndexOutOfBoundsException("Size is larger than existing buffer.");
+        if ( size > buf.length ) throw new ArrayIndexOutOfBoundsException(sm.getString("xByteBuffer.size.larger.buffer"));
         bufSize = size;
     }
 
     public void trim(int length) {
         if ( (bufSize - length) < 0 )
-            throw new ArrayIndexOutOfBoundsException("Can't trim more bytes than are available. length:"+bufSize+" trim:"+length);
+            throw new ArrayIndexOutOfBoundsException(sm.getString("xByteBuffer.unableTrim", bufSize, length));
         bufSize -= length;
     }
 
@@ -159,7 +161,7 @@ public class XByteBuffer {
         if ( discard ) {
             if (bufSize > START_DATA.length && (firstIndexOf(buf, 0, START_DATA) == -1)) {
                 bufSize = 0;
-                log.error("Discarded the package, invalid header");
+                log.error(sm.getString("xByteBuffer.discarded.invalidHeader"));
                 return false;
             }
         }
@@ -226,7 +228,7 @@ public class XByteBuffer {
         if ( discard ) {
             if (bufSize > START_DATA.length && (firstIndexOf(buf, 0, START_DATA) == -1)) {
                 bufSize = 0;
-                log.error("Discarded the package, invalid header");
+                log.error(sm.getString("xByteBuffer.discarded.invalidHeader"));
                 return false;
             }
         }
@@ -305,7 +307,7 @@ public class XByteBuffer {
     public XByteBuffer extractDataPackage(boolean clearFromBuffer) {
         int psize = countPackages(true);
         if (psize == 0) {
-            throw new java.lang.IllegalStateException("No package exists in XByteBuffer");
+            throw new java.lang.IllegalStateException(sm.getString("xByteBuffer.no.package"));
         }
         int size = toInt(buf, START_DATA.length);
         XByteBuffer xbuf = BufferPool.getBufferPool().getBuffer(size,false);
@@ -351,7 +353,7 @@ public class XByteBuffer {
 
     public static byte[] createDataPackage(byte[] data, int doff, int dlength, byte[] buffer, int bufoff) {
         if ( (buffer.length-bufoff) > getDataPackageLength(dlength) ) {
-            throw new ArrayIndexOutOfBoundsException("Unable to create data package, buffer is too small.");
+            throw new ArrayIndexOutOfBoundsException(sm.getString("xByteBuffer.unableCreate"));
         }
         System.arraycopy(START_DATA, 0, buffer, bufoff, START_DATA.length);
         toBytes(data.length,buffer, bufoff+START_DATA.length);
@@ -554,7 +556,7 @@ public class XByteBuffer {
         } else if (message instanceof Serializable)
             return (Serializable) message;
         else {
-            throw new ClassCastException("Message has the wrong class. It should implement Serializable, instead it is:"+message.getClass().getName());
+            throw new ClassCastException(sm.getString("xByteBuffer.wrong.class", message.getClass().getName()));
         }
     }
 
