663a4e30cfb605b36bb80d10d865b0ca740feb23
Rainer Jung <rjung@apache.org>
2015-05-26 14:59:04 +0000

    Use constants for SSL/TLS protocol names to reduce chances of fatal consequences of string typos.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1681770 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/net/AprEndpoint.java
+++ java/org/apache/tomcat/util/net/AprEndpoint.java
@@ -378,19 +378,19 @@ public class AprEndpoint extends AbstractEndpoint<Long> implements SNICallBack {
                     value = SSL.SSL_PROTOCOL_ALL;
                 } else {
                     for (String protocol : sslHostConfig.getProtocols()) {
-                        if ("SSLv2Hello".equalsIgnoreCase(protocol)) {
+                        if (Constants.SSL_PROTO_SSLv2Hello.equalsIgnoreCase(protocol)) {
                             // NO-OP. OpenSSL always supports SSLv2Hello
-                        } else if ("SSLv2".equalsIgnoreCase(protocol)) {
+                        } else if (Constants.SSL_PROTO_SSLv2.equalsIgnoreCase(protocol)) {
                             value |= SSL.SSL_PROTOCOL_SSLV2;
-                        } else if ("SSLv3".equalsIgnoreCase(protocol)) {
+                        } else if (Constants.SSL_PROTO_SSLv3.equalsIgnoreCase(protocol)) {
                             value |= SSL.SSL_PROTOCOL_SSLV3;
-                        } else if ("TLSv1".equalsIgnoreCase(protocol)) {
+                        } else if (Constants.SSL_PROTO_TLSv1.equalsIgnoreCase(protocol)) {
                             value |= SSL.SSL_PROTOCOL_TLSV1;
-                        } else if ("TLSv1.1".equalsIgnoreCase(protocol)) {
+                        } else if (Constants.SSL_PROTO_TLSv1_1.equalsIgnoreCase(protocol)) {
                             value |= SSL.SSL_PROTOCOL_TLSV1_1;
-                        } else if ("TLSv1.2".equalsIgnoreCase(protocol)) {
+                        } else if (Constants.SSL_PROTO_TLSv1_2.equalsIgnoreCase(protocol)) {
                             value |= SSL.SSL_PROTOCOL_TLSV1_2;
-                        } else if ("all".equalsIgnoreCase(protocol)) {
+                        } else if (Constants.SSL_PROTO_ALL.equalsIgnoreCase(protocol)) {
                             value |= SSL.SSL_PROTOCOL_ALL;
                         } else {
                             // Protocol not recognized, fail to start as it is safer than
