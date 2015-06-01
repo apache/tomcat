14d7211b9830cb28c28747a4a57057eaaf59163b
Mark Thomas <markt@apache.org>
2015-06-01 20:22:14 +0000

    Various minor clean-ups
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1682994 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/coyote/http2/TestHttp2Section_3_2.java
+++ test/org/apache/coyote/http2/TestHttp2Section_3_2.java
@@ -138,6 +138,6 @@ public class TestHttp2Section_3_2 extends Http2TestBase {
                 return;
             }
         }
-        throw new IOException("No content-length");
+        Assert.fail("No content-length");
     }
 }
