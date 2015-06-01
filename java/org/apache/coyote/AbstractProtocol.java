b1db98bda9335d8b66a767ea4ea110525ef65c50
Mark Thomas <markt@apache.org>
2015-06-01 09:13:00 +0000

    Add handling for the new (and as yet unused) exception.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1682841 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/AbstractProtocol.java
+++ java/org/apache/coyote/AbstractProtocol.java
@@ -804,6 +804,11 @@ public abstract class AbstractProtocol<S> implements ProtocolHandler,
                 // IOExceptions are normal
                 getLog().debug(sm.getString(
                         "abstractConnectionHandler.ioexception.debug"), e);
+            } catch (ProtocolException e) {
+                // Protocol exceptions normally mean the client sent invalid or
+                // incomplete data.
+                getLog().debug(sm.getString(
+                        "abstractConnectionHandler.protocolexception.debug"), e);
             }
             // Future developers: if you discover any other
             // rare-but-nonfatal exceptions, catch them here, and log as
