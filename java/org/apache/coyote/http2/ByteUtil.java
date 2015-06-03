3592641c162001f7490bea4923dd57433bf86ca1
Mark Thomas <markt@apache.org>
2015-06-03 18:44:15 +0000

    Add new method to set four bytes
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1683411 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http2/ByteUtil.java
+++ java/org/apache/coyote/http2/ByteUtil.java
@@ -72,4 +72,12 @@ public class ByteUtil {
         return ((long)(input[firstByte] & 0xFF) << 24) + ((input[firstByte + 1] & 0xFF) << 16) +
                 ((input[firstByte + 2] & 0xFF) << 8) + (input[firstByte + 3] & 0xFF);
     }
+
+
+    public static void setFourBytes(byte[] output, int firstByte, long value) {
+        output[firstByte]     = (byte) ((value & 0xFF000000) >> 24);
+        output[firstByte + 1] = (byte) ((value & 0xFF0000) >> 16);
+        output[firstByte + 2] = (byte) ((value & 0xFF00) >> 8);
+        output[firstByte + 3] = (byte) (value & 0xFF);
+    }
 }
