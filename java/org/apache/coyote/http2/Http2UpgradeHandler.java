6d091bb876936499d5a2d4baa6bf153a510bce01
Mark Thomas <markt@apache.org>
2015-06-09 13:26:54 +0000

    Fix failing tests after merging checks
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1684414 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http2/Http2UpgradeHandler.java
+++ java/org/apache/coyote/http2/Http2UpgradeHandler.java
@@ -172,7 +172,8 @@ public class Http2UpgradeHandler extends AbstractStream implements InternalHttpU
                 String base64Settings = stream.getCoyoteRequest().getHeader(HTTP2_SETTINGS_HEADER);
                 byte[] settings = Base64.decodeBase64(base64Settings);
 
-                FrameType.SETTINGS.check(1, settings.length);
+                // Settings are only valid on stream 0
+                FrameType.SETTINGS.check(0, settings.length);
 
                 for (int i = 0; i < settings.length % 6; i++) {
                     int id = ByteUtil.getTwoBytes(settings, i * 6);
