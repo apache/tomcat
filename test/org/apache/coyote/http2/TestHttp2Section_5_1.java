2f904a1c229813db8e144b87effa3da013536595
Mark Thomas <markt@apache.org>
2015-06-12 12:48:44 +0000

    Fix test error highlighted by fix in window size handling
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1685079 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/coyote/http2/TestHttp2Section_5_1.java
+++ test/org/apache/coyote/http2/TestHttp2Section_5_1.java
@@ -212,10 +212,13 @@ public class TestHttp2Section_5_1 extends Http2TestBase {
 
         sendSimpleRequest(5);
 
+        // Default connection window size is 64k - 1. Initial request will have
+        // used 8k (56k -1).
+        // Expecting
         // 1 * headers
-        // 64k of body (8 * 8k)
+        // 56k-1 of body (7 * ~8k)
         // 1 * error (could be in any order)
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < 8; i++) {
             parser.readFrame(true);
         }
         parser.readFrame(true);
