6c771c092371423ff0cd4e53c0d4443919fbf335
Mark Thomas <markt@apache.org>
2015-06-15 22:11:10 +0000

    Implemented very basic JASPIC support with ability to register providers Patch by fjodorver
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1685678 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/authenticator/jaspic/JaspicAuthenticator.java
+++ java/org/apache/catalina/authenticator/jaspic/JaspicAuthenticator.java
@@ -17,38 +17,120 @@
 package org.apache.catalina.authenticator.jaspic;
 
 import java.io.IOException;
+import java.security.Principal;
+import java.util.Map;
 
+import javax.security.auth.Subject;
+import javax.security.auth.message.AuthException;
+import javax.security.auth.message.AuthStatus;
+import javax.security.auth.message.MessageInfo;
+import javax.security.auth.message.config.AuthConfigFactory;
+import javax.security.auth.message.config.AuthConfigProvider;
+import javax.security.auth.message.config.ServerAuthConfig;
+import javax.security.auth.message.config.ServerAuthContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.catalina.Authenticator;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.authenticator.AuthenticatorBase;
 import org.apache.catalina.connector.Request;
-import org.apache.catalina.connector.Response;
-import org.apache.catalina.valves.ValveBase;
+
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
 
-public class JaspicAuthenticator extends ValveBase implements Authenticator {
+public class JaspicAuthenticator extends AuthenticatorBase {
 
     private static final Log log = LogFactory.getLog(JaspicAuthenticator.class);
 
+    private static final String AUTH_TYPE = "JASPIC";
+    private static final String MESSAGE_LAYER = "HttpServlet";
+
+    private JaspicCallbackHandler callbackHandler;
+    private Subject serviceSubject;
+
+    @SuppressWarnings("rawtypes")
+    private Map authProperties = null;
+
+
+    @Override
+    protected synchronized void startInternal() throws LifecycleException {
+        super.startInternal();
+        callbackHandler = new JaspicCallbackHandler(container.getRealm());
+        serviceSubject = new Subject();
+    }
+
+
     @Override
     public boolean authenticate(Request request, HttpServletResponse response) throws IOException {
-        return true;
+        MessageInfo messageInfo = new MessageInfoImpl(request, response, true);
+        AuthConfigFactory factory = AuthConfigFactory.getFactory();
+        String appContext = request.getLocalName() + " " + request.getContextPath();
+
+        AuthConfigProvider configProvider =
+                factory.getConfigProvider(MESSAGE_LAYER, appContext, null);
+        ServerAuthConfig authConfig = getAuthConfig(appContext, configProvider);
+        String authContextId = authConfig.getAuthContextID(messageInfo);
+
+        ServerAuthContext authContext = null;
+        authContext = getAuthContext(authConfig, authContextId, authProperties, authContext);
+        AuthStatus authStatus = validateRequest(messageInfo, authContext);
+
+        if (authStatus == AuthStatus.SUCCESS) {
+            Principal principal = callbackHandler.getPrincipal();
+            if (principal != null) {
+                register(request, response, principal, AUTH_TYPE, null, null);
+            }
+            return true;
+        }
+
+        return false;
     }
+
+
+    private AuthStatus validateRequest(MessageInfo messageInfo, ServerAuthContext authContext) {
+        Subject clientSubject = new Subject();
+        try {
+            return authContext.validateRequest(messageInfo, clientSubject, serviceSubject);
+        } catch (AuthException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+
+    @SuppressWarnings("rawtypes")
+    private ServerAuthContext getAuthContext(ServerAuthConfig authConfig, String authContextId,
+            Map authProperties, ServerAuthContext authContext) {
+        try {
+            return authConfig.getAuthContext(authContextId, serviceSubject, authProperties);
+        } catch (AuthException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
 
     @Override
     public void login(String userName, String password, Request request) throws ServletException {
-
+        throw new IllegalStateException("not implemented yet!");
     }
+
 
     @Override
     public void logout(Request request) {
-
+        throw new IllegalStateException("not implemented yet!");
     }
 
+
+    private ServerAuthConfig getAuthConfig(String appContext, AuthConfigProvider configProvider) {
+        try {
+            return configProvider.getServerAuthConfig(MESSAGE_LAYER, appContext, callbackHandler);
+        } catch (AuthException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+
     @Override
-    public void invoke(Request request, Response response) throws IOException, ServletException {
-        getNext().invoke(request, response);
+    protected String getAuthMethod() {
+        return AUTH_TYPE;
     }
 }
