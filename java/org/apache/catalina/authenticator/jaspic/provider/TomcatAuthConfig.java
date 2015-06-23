fbd6b3bcd01cf5a49a1f3dc614f288e1a0dd79e5
Mark Thomas <markt@apache.org>
2015-06-23 10:03:20 +0000

    Implemented JASPIC module for DIGEST authentication Patch by fjodorver
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1687016 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/authenticator/jaspic/provider/TomcatAuthConfig.java
+++ java/org/apache/catalina/authenticator/jaspic/provider/TomcatAuthConfig.java
@@ -30,6 +30,7 @@ import javax.security.auth.message.config.ServerAuthContext;
 
 import org.apache.catalina.Realm;
 import org.apache.catalina.authenticator.jaspic.provider.modules.BasicAuthModule;
+import org.apache.catalina.authenticator.jaspic.provider.modules.DigestAuthModule;
 import org.apache.catalina.authenticator.jaspic.provider.modules.TomcatAuthModule;
 
 public class TomcatAuthConfig implements ServerAuthConfig {
@@ -94,6 +95,7 @@ public class TomcatAuthConfig implements ServerAuthConfig {
     private Collection<TomcatAuthModule> getModules() {
         List<TomcatAuthModule> modules = new ArrayList<>();
         modules.add(new BasicAuthModule());
+        modules.add(new DigestAuthModule(realm));
         return modules;
     }
 }
