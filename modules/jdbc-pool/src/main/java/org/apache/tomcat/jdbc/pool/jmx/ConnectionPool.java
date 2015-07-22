a716959cff4b41e20dbe851e5fbee43ba8c86696
Keiichi Fujino <kfujino@apache.org>
2015-07-22 06:28:41 +0000

    The fairness attribute and IgnoreExceptionOnPreLoad attribute do not allow a change via JMX.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1692222 13f79535-47bb-0310-9956-ffa450edef68

--- modules/jdbc-pool/src/main/java/org/apache/tomcat/jdbc/pool/jmx/ConnectionPool.java
+++ modules/jdbc-pool/src/main/java/org/apache/tomcat/jdbc/pool/jmx/ConnectionPool.java
@@ -488,7 +488,8 @@ public class ConnectionPool extends NotificationBroadcasterSupport implements Co
 
     @Override
     public void setFairQueue(boolean fairQueue) {
-        getPoolProperties().setFairQueue(fairQueue);
+        // noop - this pool is already running
+        throw new UnsupportedOperationException();
     }
 
 
@@ -855,7 +856,8 @@ public class ConnectionPool extends NotificationBroadcasterSupport implements Co
      */
     @Override
     public void setIgnoreExceptionOnPreLoad(boolean ignoreExceptionOnPreLoad) {
-        getPoolProperties().setIgnoreExceptionOnPreLoad(ignoreExceptionOnPreLoad);
+        // noop - this pool is already running
+        throw new UnsupportedOperationException();
     }
 
     /**
