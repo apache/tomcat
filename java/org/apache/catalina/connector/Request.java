95545f4d13f6f011db418728363b39c7ff457921
Mark Thomas <markt@apache.org>
2015-08-06 09:51:20 +0000

    Differentiate between failure modes. Handling is currently identical but it won't be once BZ 58031 is addressed.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1694433 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/connector/Request.java
+++ java/org/apache/catalina/connector/Request.java
@@ -3216,8 +3216,17 @@ public class Request
                 byte[] formData = null;
                 try {
                     formData = readChunkedPostBody();
+                } catch (IllegalStateException ise) {
+                    // chunkedPostTooLarge error
+                    Context context = getContext();
+                    if (context != null && context.getLogger().isDebugEnabled()) {
+                        context.getLogger().debug(
+                                sm.getString("coyoteRequest.parseParameters"),
+                                ise);
+                    }
+                    return;
                 } catch (IOException e) {
-                    // Client disconnect or chunkedPostTooLarge error
+                    // Client disconnect
                     if (context.getLogger().isDebugEnabled()) {
                         context.getLogger().debug(
                                 sm.getString("coyoteRequest.parseParameters"), e);
@@ -3272,7 +3281,7 @@ public class Request
                     (body.getLength() + len) > connector.getMaxPostSize()) {
                 // Too much data
                 checkSwallowInput();
-                throw new IOException(
+                throw new IllegalStateException(
                         sm.getString("coyoteRequest.chunkedPostTooLarge"));
             }
             if (len > 0) {
