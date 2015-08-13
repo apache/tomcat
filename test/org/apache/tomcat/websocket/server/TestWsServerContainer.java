a2c70faa95aee3c96467f01ed15d26ed985cb00b
Violeta Georgieva Georgieva <violetagg@apache.org>
2015-08-13 15:07:01 +0000

    Do not use try-with-resources
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1695724 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/tomcat/websocket/server/TestWsServerContainer.java
+++ test/org/apache/tomcat/websocket/server/TestWsServerContainer.java
@@ -79,7 +79,9 @@ public class TestWsServerContainer extends TomcatBaseTest {
         SimpleClient client = new SimpleClient();
         URI uri = new URI("ws://localhost:" + getPort() + "/echoBasic");
 
-        try (Session session = wsContainer.connectToServer(client, uri);) {
+        Session session = null;
+        try {
+            session = wsContainer.connectToServer(client, uri);
             CountDownLatch latch = new CountDownLatch(1);
             BasicText handler = new BasicText(latch);
             session.addMessageHandler(handler);
@@ -92,6 +94,10 @@ public class TestWsServerContainer extends TomcatBaseTest {
             Assert.assertEquals(1, messages.size());
             for (String message : messages) {
                 Assert.assertEquals("echoBasic", message);
+            }
+        } finally {
+            if (session != null) {
+                session.close();
             }
         }
     }
