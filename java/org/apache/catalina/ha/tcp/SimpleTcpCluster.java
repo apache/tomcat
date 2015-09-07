508023f05230af5aacfaefc432325ab1aaa4e009
Keiichi Fujino <kfujino@apache.org>
2015-09-07 05:59:18 +0000

    Fix a default clusterListeners in SimpleTcpCluster. The optimal default value is different for each session manager. ClusterSessionListener is never used in BackupManager.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1701547 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/ha/tcp/SimpleTcpCluster.java
+++ java/org/apache/catalina/ha/tcp/SimpleTcpCluster.java
@@ -553,7 +553,7 @@ public class SimpleTcpCluster extends LifecycleMBeanBase
     }
 
     protected void checkDefaults() {
-        if ( clusterListeners.size() == 0 ) {
+        if ( clusterListeners.size() == 0 && managerTemplate instanceof DeltaManager ) {
             addClusterListener(new ClusterSessionListener());
         }
         if ( valves.size() == 0 ) {
