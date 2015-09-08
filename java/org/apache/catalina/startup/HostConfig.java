7923b1d4a4b3dd2bc2306e76153d2e3cad0627e2
Mark Thomas <markt@apache.org>
2015-09-08 10:23:47 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=58187 When deploying applications via the Manager, skip the unnecessary test of file modification time that delayed deployment until the next automatic deployment check ran.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1701767 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/startup/HostConfig.java
+++ java/org/apache/catalina/startup/HostConfig.java
@@ -1389,8 +1389,27 @@ public class HostConfig
 
     /**
      * Check resources for redeployment and reloading.
+     *
+     * @deprecated Use {@link #checkResources(DeployedApplication, boolean)}.
+     *             Will be removed in Tomcat 9.0.x
      */
+    @Deprecated
     protected synchronized void checkResources(DeployedApplication app) {
+        checkResources(app, false);
+    }
+
+    /**
+     * Check resources for redeployment and reloading.
+     *
+     * @param app   The web application to check
+     * @param skipFileModificationResolutionCheck
+     *              When checking files for modification should the check that
+     *              requires that any file modification must have occurred at
+     *              least as long ago as the resolution of the file time stamp
+     *              be skipped
+     */
+    protected synchronized void checkResources(DeployedApplication app,
+            boolean skipFileModificationResolutionCheck) {
         String[] resources =
             app.redeployResources.keySet().toArray(new String[0]);
         // Offset the current time by the resolution of File.lastModified()
@@ -1409,7 +1428,8 @@ public class HostConfig
                 // modifications that take place in the same second are not
                 // missed. See Bug 57765.
                 if (resource.lastModified() != lastModified && (!host.getAutoDeploy() ||
-                        resource.lastModified() < currentTimeWithResolutionOffset)) {
+                        resource.lastModified() < currentTimeWithResolutionOffset ||
+                        skipFileModificationResolutionCheck)) {
                     if (resource.isDirectory()) {
                         // No action required for modified directory
                         app.redeployResources.put(resources[i],
@@ -1490,7 +1510,8 @@ public class HostConfig
             // missed. See Bug 57765.
             if ((resource.lastModified() != lastModified &&
                     (!host.getAutoDeploy() ||
-                            resource.lastModified() < currentTimeWithResolutionOffset)) ||
+                            resource.lastModified() < currentTimeWithResolutionOffset ||
+                            skipFileModificationResolutionCheck)) ||
                     update) {
                 if (!update) {
                     // Reload application
@@ -1692,7 +1713,7 @@ public class HostConfig
                 deployed.values().toArray(new DeployedApplication[0]);
             for (int i = 0; i < apps.length; i++) {
                 if (!isServiced(apps[i].name))
-                    checkResources(apps[i]);
+                    checkResources(apps[i], false);
             }
 
             // Check for old versions of applications that can now be undeployed
@@ -1707,12 +1728,21 @@ public class HostConfig
 
 
     /**
-     * Check status of a specific webapp, for use with stuff like management webapps.
+     * Check status of a specific web application and reload, redeploy or deploy
+     * it as necessary. This method is for use with functionality such as
+     * management web applications that upload new/updated web applications and
+     * need to trigger the appropriate action to deploy them. This method
+     * assumes that the web application is currently marked as serviced and that
+     * any uploading/updating has been completed before this method is called.
+     * Any action taken as a result of the checks will complete before this
+     * method returns.
+     *
+     * @param name The name of the web application to check
      */
     public void check(String name) {
         DeployedApplication app = deployed.get(name);
         if (app != null) {
-            checkResources(app);
+            checkResources(app, true);
         }
         deployApps(name);
     }
