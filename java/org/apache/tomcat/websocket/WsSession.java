ff4539a4c518462308f43a707ab8aa6ab9705a87
Mark Thomas <markt@apache.org>
2015-09-10 13:35:36 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=58342 Fix copy paste error that meant MessageHandler removal could fail for binary and pong MessageHandlers. Patch provided by DJ.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1702246 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/websocket/WsSession.java
+++ java/org/apache/tomcat/websocket/WsSession.java
@@ -315,13 +315,13 @@ public class WsSession implements Session {
             removed = true;
         }
 
-        if (listener.equals(binaryMessageHandler) ||
+        if (wrapped.equals(binaryMessageHandler) ||
                 listener.equals(binaryMessageHandler)) {
             binaryMessageHandler = null;
             removed = true;
         }
 
-        if (listener.equals(pongMessageHandler) ||
+        if (wrapped.equals(pongMessageHandler) ||
                 listener.equals(pongMessageHandler)) {
             pongMessageHandler = null;
             removed = true;
