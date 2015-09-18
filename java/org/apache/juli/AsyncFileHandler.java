2db254d81bf7523e7130cef8961223d5987ba1d4
Mark Thomas <markt@apache.org>
2015-09-18 13:27:36 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=58412 Ensure correct method and package names are logged.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1703842 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/juli/AsyncFileHandler.java
+++ java/org/apache/juli/AsyncFileHandler.java
@@ -99,6 +99,11 @@ public class AsyncFileHandler extends FileHandler {
         if (!isLoggable(record)) {
             return;
         }
+        if (record != null) {
+            // fill source entries, before we hand the record over to another
+            // thread with another class loader
+            record.getSourceMethodName();
+        }
         LogEntry entry = new LogEntry(record,this);
         boolean added = false;
         try {
