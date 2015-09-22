622ef0349787f2ec88430b911b7e8211922d0870
Mark Thomas <markt@apache.org>
2015-09-22 22:00:26 +0000

    Switch all the DirResource based tests to use dedicated temporary directories for all read/write tests.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1704739 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/catalina/webresources/TestDirResourceSetInternal.java
+++ test/org/apache/catalina/webresources/TestDirResourceSetInternal.java
@@ -17,19 +17,58 @@
 package org.apache.catalina.webresources;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileAttribute;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 
 import org.apache.catalina.WebResourceRoot;
 import org.apache.catalina.WebResourceSet;
+import org.apache.catalina.startup.ExpandWar;
+import org.apache.catalina.startup.TomcatBaseTest;
 
-public class TestDirResourceSetInternal extends TestDirResourceSet {
+public class TestDirResourceSetInternal extends AbstractTestResourceSet {
+
+    private static Path tempDir;
+    private static File dir1;
+
+    @BeforeClass
+    public static void before() throws IOException {
+        tempDir = Files.createTempDirectory("test", new FileAttribute[0]);
+        dir1 = new File(tempDir.toFile(), "dir1");
+        TomcatBaseTest.recurrsiveCopy(new File("test/webresources/dir1").toPath(), dir1.toPath());
+    }
+
+    @AfterClass
+    public static void after() {
+        ExpandWar.delete(tempDir.toFile());
+    }
+
 
     @Override
     public WebResourceRoot getWebResourceRoot() {
-        File f = new File("test/");
         TesterWebResourceRoot root = new TesterWebResourceRoot();
         WebResourceSet webResourceSet =
-                new DirResourceSet(root, "/", f.getAbsolutePath(), "/webresources/dir1");
+                new DirResourceSet(root, "/", tempDir.toAbsolutePath().toString(), "/dir1");
         root.setMainResources(webResourceSet);
         return root;
+    }
+
+    @Override
+    protected boolean isWriteable() {
+        return true;
+    }
+
+    @Override
+    public File getBaseDir() {
+        return dir1;
+    }
+
+    @Override
+    public void testNoArgConstructor() {
+        // NO-OP. Tested in TestDirResource
     }
 }
