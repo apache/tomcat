6a4235d8b92090e9bf0908010c2cc67ae15128a7
Mark Thomas <markt@apache.org>
2015-09-22 21:35:54 +0000

    Use a dedicated temporary directory for resources that will be read/write during tests. Need to expand this to other tests with read/write resources.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1704731 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/catalina/webresources/TestDirResourceSetVirtual.java
+++ test/org/apache/catalina/webresources/TestDirResourceSetVirtual.java
@@ -17,11 +17,36 @@
 package org.apache.catalina.webresources;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileAttribute;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 
 import org.apache.catalina.WebResourceRoot;
 import org.apache.catalina.WebResourceSet;
+import org.apache.catalina.startup.ExpandWar;
+import org.apache.catalina.startup.TomcatBaseTest;
 
 public class TestDirResourceSetVirtual extends TestDirResourceSet {
+
+    private static Path tempDir;
+    private static File dir1;
+
+    @BeforeClass
+    public static void before() throws IOException {
+        tempDir = Files.createTempDirectory("test", new FileAttribute[0]);
+        dir1 = new File(tempDir.toFile(), "dir1");
+        TomcatBaseTest.recurrsiveCopy(new File("test/webresources/dir1").toPath(), dir1.toPath());
+    }
+
+    @AfterClass
+    public static void after() {
+        ExpandWar.delete(tempDir.toFile());
+    }
+
 
     @Override
     public WebResourceRoot getWebResourceRoot() {
@@ -32,19 +57,19 @@ public class TestDirResourceSetVirtual extends TestDirResourceSet {
         root.setMainResources(webResourceSet);
 
         WebResourceSet f1 = new FileResourceSet(root, "/f1.txt",
-                "test/webresources/dir1/f1.txt", "/");
+                dir1.getAbsolutePath() + "/f1.txt", "/");
         root.addPreResources(f1);
 
         WebResourceSet f2 = new FileResourceSet(root, "/f2.txt",
-                "test/webresources/dir1/f2.txt", "/");
+                dir1.getAbsolutePath() + "/f2.txt", "/");
         root.addPreResources(f2);
 
         WebResourceSet d1 = new DirResourceSet(root, "/d1",
-                "test/webresources/dir1/d1", "/");
+                dir1.getAbsolutePath() + "/d1", "/");
         root.addPreResources(d1);
 
         WebResourceSet d2 = new DirResourceSet(root, "/d2",
-                "test/webresources/dir1/d2", "/");
+                dir1.getAbsolutePath() + "/d2", "/");
         root.addPreResources(d2);
 
         return root;
