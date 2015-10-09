11336d58983f5090397fde82619ed3746009cb3a
Mark Thomas <markt@apache.org>
2015-10-09 19:34:08 +0000

    Take advantage of GenericFilter
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1707801 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/catalina/startup/ParamFilter.java
+++ test/org/apache/catalina/startup/ParamFilter.java
@@ -20,9 +20,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.DispatcherType;
-import javax.servlet.Filter;
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
+import javax.servlet.GenericFilter;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -33,27 +32,18 @@ import javax.servlet.annotation.WebInitParam;
  * Test Mock to check Filter Annotations
  * @author Peter Rossbach
  */
-@WebFilter(value = "/param", filterName = "paramFilter", dispatcherTypes = {
-        DispatcherType.ERROR, DispatcherType.ASYNC }, initParams = { @WebInitParam(name = "message", value = "Servlet says: ") })
-public class ParamFilter implements Filter {
+@WebFilter(value = "/param", filterName = "paramFilter",
+        dispatcherTypes = { DispatcherType.ERROR, DispatcherType.ASYNC },
+        initParams = { @WebInitParam(name = "message", value = "Servlet says: ") })
+public class ParamFilter extends GenericFilter {
 
-    private FilterConfig _filterConfig;
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        _filterConfig = filterConfig;
-    }
+    private static final long serialVersionUID = 1L;
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res,
             FilterChain chain) throws ServletException, IOException {
         PrintWriter out = res.getWriter();
-        out.print(_filterConfig.getInitParameter("message"));
+        out.print(getInitParameter("message"));
         chain.doFilter(req, res);
-    }
-
-    @Override
-    public void destroy() {
-        // destroy
     }
 }
