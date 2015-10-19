873a19a78bd40845bb687265bbbe23e9539ce30f
Mark Thomas <markt@apache.org>
2015-10-19 15:44:17 +0000

    Expand the margin of error for Gump
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1709432 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/coyote/http2/TestHttp2Section_5_3.java
+++ test/org/apache/coyote/http2/TestHttp2Section_5_3.java
@@ -163,7 +163,7 @@ public class TestHttp2Section_5_3 extends Http2TestBase {
                 // Unexpected stream
                 Assert.fail("Unexpected stream: [" + output.getTrace() + "]");
             }
-            if (data[1] > 3) {
+            if (data[1] > 10) {
                 // Larger than expected body size
                 Assert.fail("Larger than expected body: [" + output.getTrace() + "]");
             }
@@ -177,7 +177,7 @@ public class TestHttp2Section_5_3 extends Http2TestBase {
         // possible that the timing of the server threads is such that there are
         // still small body frames to read.
         int[] data = parseBodyFrame(output.getTrace());
-        while (data[1] < 4) {
+        while (data[1] < 10) {
             // Debugging Gump failure
             log.info(output.getTrace());
             output.clearTrace();
@@ -193,12 +193,16 @@ public class TestHttp2Section_5_3 extends Http2TestBase {
             log.info(output.getTrace());
             if (data[0] == 19) {
                 seen19 = true;
-                if (data[1] < 256 || data[1] > 260) {
+                // If everything works instantly this should be 256 but allow a
+                // fairly large margin for timing differences
+                if (data[1] < 256 || data[1] > 290) {
                     Assert.fail("Unexpected body size: [" + output.getTrace() + "]");
                 }
             } else if (data[0] == 21) {
                 seen21 = true;
-                if (data[1] < 768 || data[1] > 772) {
+                // If everything works instantly this should be 768 but allow a
+                // fairly large margin for timing differences
+                if (data[1] < 768 || data[1] > 800) {
                     Assert.fail("Unexpected body size: [" + output.getTrace() + "]");
                 }
             } else {
