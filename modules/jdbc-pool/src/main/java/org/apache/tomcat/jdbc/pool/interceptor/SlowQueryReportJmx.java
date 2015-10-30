fbd146e0a93b31bc872bfd6e9b1f1f7905521a73
Keiichi Fujino <kfujino@apache.org>
2015-10-30 06:25:14 +0000

    No-functional changes. Remove TODO.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1711404 13f79535-47bb-0310-9956-ffa450edef68

--- modules/jdbc-pool/src/main/java/org/apache/tomcat/jdbc/pool/interceptor/SlowQueryReportJmx.java
+++ modules/jdbc-pool/src/main/java/org/apache/tomcat/jdbc/pool/interceptor/SlowQueryReportJmx.java
@@ -124,7 +124,6 @@ public class SlowQueryReportJmx extends SlowQueryReport implements NotificationE
 
     @Override
     public void reset(ConnectionPool parent, PooledConnection con) {
-        // TODO Auto-generated method stub
         super.reset(parent, con);
         if (parent!=null) {
             poolName = parent.getName();
