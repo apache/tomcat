e57fe6a26264d494305795350b43e52f5108b9be
Mark Thomas <markt@apache.org>
2015-11-02 14:14:10 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=58058 Ensure only one thread can attempt to initialize the APR library at a time
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1712003 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/jni/Library.java
+++ java/org/apache/tomcat/jni/Library.java
@@ -162,10 +162,13 @@ public final class Library {
      * Setup any APR internal data structures.  This MUST be the first function
      * called for any APR library.
      * @param libraryName the name of the library to load
+     *
+     * @return {@code true} if the native code was initialized successfully
+     *         otherwise {@code false}
+     *
+     * @throws Exception if a problem occurred during initialization
      */
-    public static boolean initialize(String libraryName)
-        throws Exception
-    {
+    public synchronized static boolean initialize(String libraryName) throws Exception {
         if (_instance == null) {
             if (libraryName == null)
                 _instance = new Library();
