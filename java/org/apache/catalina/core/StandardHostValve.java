b405f50cc0947cbc54189ced89e6545509b36d0d
Mark Thomas <markt@apache.org>
2015-11-06 15:13:50 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=58581 If custom error page fails, fall back to standard error page rather than throwing an NPE. Based on a patch by Huxing Zhang.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1712977 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/core/StandardHostValve.java
+++ java/org/apache/catalina/core/StandardHostValve.java
@@ -320,7 +320,7 @@ final class StandardHostValve extends ValveBase {
             // Look for a default error page
             errorPage = context.findErrorPage(0);
         }
-        if (errorPage != null && response.setErrorReported()) {
+        if (errorPage != null && response.isErrorReportRequired()) {
             response.setAppCommitted(false);
             request.setAttribute(RequestDispatcher.ERROR_STATUS_CODE,
                               Integer.valueOf(statusCode));
@@ -342,6 +342,7 @@ final class StandardHostValve extends ValveBase {
             request.setAttribute(RequestDispatcher.ERROR_REQUEST_URI,
                                  request.getRequestURI());
             if (custom(request, response, errorPage)) {
+                response.setErrorReported();
                 try {
                     response.finishResponse();
                 } catch (ClientAbortException e) {
@@ -462,6 +463,12 @@ final class StandardHostValve extends ValveBase {
                 request.getContext().getServletContext();
             RequestDispatcher rd =
                 servletContext.getRequestDispatcher(errorPage.getLocation());
+
+            if (rd == null) {
+                container.getLogger().error(
+                    sm.getString("standardHostValue.customStatusFailed", errorPage.getLocation()));
+                return false;
+            }
 
             if (response.isCommitted()) {
                 // Response is committed - including the error page is the
