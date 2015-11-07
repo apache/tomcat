5775677d3d9e858279499507b8e44b87aa0345ba
Mark Thomas <markt@apache.org>
2015-11-07 21:17:43 +0000

    Javadoc This complete the org.apache.coyote package which is now warning free
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1713182 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http11/InputFilter.java
+++ java/org/apache/coyote/http11/InputFilter.java
@@ -31,9 +31,9 @@ import org.apache.tomcat.util.buf.ByteChunk;
 public interface InputFilter extends InputBuffer {
 
     /**
-     * Some filters need additional parameters from the request. All the
-     * necessary reading can occur in that method, as this method is called
-     * after the request header processing is complete.
+     * Some filters need additional parameters from the request.
+     *
+     * @param request The request to be associated with this filter
      */
     public void setRequest(Request request);
 
@@ -46,12 +46,18 @@ public interface InputFilter extends InputBuffer {
 
     /**
      * Get the name of the encoding handled by this filter.
+     *
+     * @return The encoding name as a byte chunk to facilitate comparison with
+     *         the value read from the HTTP headers which will also be a
+     *         ByteChunk
      */
     public ByteChunk getEncodingName();
 
 
     /**
      * Set the next buffer in the filter pipeline.
+     *
+     * @param buffer The next buffer
      */
     public void setBuffer(InputBuffer buffer);
 
@@ -63,19 +69,25 @@ public interface InputFilter extends InputBuffer {
      * too many bytes were read. This method is allowed to use buffer.doRead
      * to consume extra bytes. The result of this method can't be negative (if
      * an error happens, an IOException should be thrown instead).
+     *
+     * @throws IOException If an error happens
      */
-    public long end()
-        throws IOException;
+    public long end() throws IOException;
 
 
     /**
      * Amount of bytes still available in a buffer.
+     *
+     * @return The number of bytes in the buffer
      */
     public int available();
 
 
     /**
      * Has the request body been read fully?
+     *
+     * @return {@code true} if the request body has been fully read, otherwise
+     *         {@code false}
      */
     public boolean isFinished();
 }
