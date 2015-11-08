8045d8a646a3d5189ff3084abf3ce424d3ba9ce1
Jean-Frederic Clere <jfclere@apache.org>
2015-11-08 10:49:53 +0000

    Add the java of the native setCertificateRaw.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1713221 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/jni/SSLContext.java
+++ java/org/apache/tomcat/jni/SSLContext.java
@@ -509,4 +509,15 @@ public final class SSLContext {
      * @return {@code true} if success, {@code false} otherwise.
      */
     public static native boolean setSessionIdContext(long ctx, byte[] sidCtx);
+    
+    /**
+     * Set CertificateRaw
+     * <br>
+     * Use keystore a certificate and key to fill the BIOP
+     * @param ctx Server or Client context to use.
+     * @param cert Byte array with the certificate in DER encoding.
+     * @param key Byte array with the Private Key file in PEM format.
+     * @param idx Certificate index SSL_AIDX_RSA or SSL_AIDX_DSA.
+     */
+	public static native boolean setCertificateRaw(long ctx, byte[] cert, byte[] key, int sslAidxRsa);
 }
