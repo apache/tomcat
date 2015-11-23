ff31d85a5c5c3f3774ff05bb0864d27379157c1f
Mark Thomas <markt@apache.org>
2015-11-23 22:04:18 +0000

    Fix Javadoc errors Trivial clean-up
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1715976 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/ContextBind.java
+++ java/org/apache/tomcat/ContextBind.java
@@ -23,9 +23,9 @@ public interface ContextBind {
      * class loader. If no web application class loader is defined, or if the
      * current thread is already using the web application class loader then no
      * change will be made. If the class loader is changed and a
-     * {@link ThreadBindingListener} is configured then
-     * {@link ThreadBindingListener#bind()} will be called after the change has
-     * been made.
+     * {@link org.apache.catalina.ThreadBindingListener} is configured then
+     * {@link org.apache.catalina.ThreadBindingListener#bind()} will be called
+     * after the change has been made.
      *
      * @param usePrivilegedAction
      *          Should a {@link java.security.PrivilegedAction} be used when
@@ -39,14 +39,15 @@ public interface ContextBind {
      *         the thread context class loader in use when the method was
      *         called. If no change was made then this method returns null.
      */
-    public ClassLoader bind(boolean usePrivilegedAction, ClassLoader originalClassLoader);
+    ClassLoader bind(boolean usePrivilegedAction, ClassLoader originalClassLoader);
 
     /**
      * Restore the current thread context class loader to the original class
      * loader in used before {@link #bind(boolean, ClassLoader)} was called. If
      * no original class loader is passed to this method then no change will be
-     * made. If the class loader is changed and a {@link ThreadBindingListener}
-     * is configured then {@link ThreadBindingListener#unbind()} will be called
+     * made. If the class loader is changed and a
+     * {@link org.apache.catalina.ThreadBindingListener} is configured then
+     * {@link org.apache.catalina.ThreadBindingListener#unbind()} will be called
      * before the change is made.
      *
      * @param usePrivilegedAction
@@ -55,6 +56,5 @@ public interface ContextBind {
      * @param originalClassLoader
      *          The class loader to restore as the thread context class loader
      */
-    public void unbind(boolean usePrivilegedAction, ClassLoader originalClassLoader);
-
+    void unbind(boolean usePrivilegedAction, ClassLoader originalClassLoader);
 }
