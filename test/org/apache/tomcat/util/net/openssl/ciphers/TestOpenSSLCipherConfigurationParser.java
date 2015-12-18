f9af9143e06903daf6aa80eb9e2ce116ba1ce5dc
Konstantin Kolinko <kkolinko@apache.org>
2015-12-18 02:03:15 +0000

    Fix typo in a comment - followup to r1720640
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1720711 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/tomcat/util/net/openssl/ciphers/TestOpenSSLCipherConfigurationParser.java
+++ test/org/apache/tomcat/util/net/openssl/ciphers/TestOpenSSLCipherConfigurationParser.java
@@ -667,7 +667,7 @@ public class TestOpenSSLCipherConfigurationParser {
         // OpenSSL treats many ciphers as having equal preference. The order
         // returned depends on the order they are requested. The following code
         // checks that the Parser produces a cipher list that is consistent with
-        // OpenSSL's preference order by confirming that running through OPenSSL
+        // OpenSSL's preference order by confirming that running through OpenSSL
         // does not change the order.
         String parserOrderedExpression = listToString(jsseCipherListFromParser, ',');
         Assert.assertEquals(
