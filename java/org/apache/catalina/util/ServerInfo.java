b4a95da4f3927b6f3cd928fbb90fd4f971ae86f7
Konstantin Kolinko <kkolinko@apache.org>
2015-12-27 04:19:17 +0000

    Fix 8.0.x -> 9.0.x typo in the unlikely case when ServerInfo.properties file does not exist.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1721783 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/util/ServerInfo.java
+++ java/org/apache/catalina/util/ServerInfo.java
@@ -69,11 +69,11 @@ public class ServerInfo {
             ExceptionUtils.handleThrowable(t);
         }
         if (info == null)
-            info = "Apache Tomcat 8.0.x-dev";
+            info = "Apache Tomcat 9.0.x-dev";
         if (built == null)
             built = "unknown";
         if (number == null)
-            number = "8.0.x";
+            number = "9.0.x";
 
         serverInfo = info;
         serverBuilt = built;
