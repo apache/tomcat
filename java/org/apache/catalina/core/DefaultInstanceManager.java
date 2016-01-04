629c6cda787e38071b028d8189654c2a40a0691a
Konstantin Kolinko <kkolinko@apache.org>
2016-01-04 16:00:49 +0000

    Simplify. Fix naming of message keys (s/Resources/Resource/). Log name of a missing resource.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1722905 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/core/DefaultInstanceManager.java
+++ java/org/apache/catalina/core/DefaultInstanceManager.java
@@ -88,19 +88,16 @@ public class DefaultInstanceManager implements InstanceManager {
         privileged = catalinaContext.getPrivileged();
         this.containerClassLoader = containerClassLoader;
         ignoreAnnotations = catalinaContext.getIgnoreAnnotations();
-        StringManager sm = StringManager.getManager(Constants.Package);
+        Log log = catalinaContext.getLogger();
         restrictedServlets = loadProperties(
                 "org/apache/catalina/core/RestrictedServlets.properties",
-                sm.getString("defaultInstanceManager.restrictedServletsResource"),
-                catalinaContext.getLogger());
+                "defaultInstanceManager.restrictedServletsResource", log);
         restrictedListeners = loadProperties(
                 "org/apache/catalina/core/RestrictedListeners.properties",
-                sm.getString("defaultInstanceManager.restrictedListenersResources"),
-                catalinaContext.getLogger());
+                "defaultInstanceManager.restrictedListenersResource", log);
         restrictedFilters = loadProperties(
                 "org/apache/catalina/core/RestrictedFilters.properties",
-                sm.getString("defaultInstanceManager.restrictedFiltersResource"),
-                catalinaContext.getLogger());
+                "defaultInstanceManager.restrictedFiltersResource", log);
         this.context = context;
         this.injectionMap = injectionMap;
         this.postConstructMethods = catalinaContext.findPostConstructMethods();
@@ -624,17 +621,17 @@ public class DefaultInstanceManager implements InstanceManager {
         }
     }
 
-    private static Properties loadProperties(String resourceName, String errorString, Log log) {
+    private static Properties loadProperties(String resourceName, String messageKey, Log log) {
         Properties result = new Properties();
         ClassLoader cl = DefaultInstanceManager.class.getClassLoader();
         try (InputStream is = cl.getResourceAsStream(resourceName)) {
-            if (is ==null) {
-                log.error(errorString);
+            if (is == null) {
+                log.error(sm.getString(messageKey, resourceName));
             } else {
                 result.load(is);
             }
         } catch (IOException ioe) {
-            log.error(errorString, ioe);
+            log.error(sm.getString(messageKey, resourceName), ioe);
         }
         return result;
     }
