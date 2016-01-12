eaeccb8578cc862e63e8b38fd1c646e9d32607e9
Mark Thomas <markt@apache.org>
2016-01-12 15:02:52 +0000

    Javadoc
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1724256 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/manager/util/BaseSessionComparator.java
+++ java/org/apache/catalina/manager/util/BaseSessionComparator.java
@@ -22,7 +22,10 @@ import java.util.Comparator;
 import org.apache.catalina.Session;
 
 /**
- * Comparator which permits to compare on a session's content
+ * Comparator which permits to compare on a session's content.
+ *
+ * @param <T> The type of the session content to be compared
+ *
  * @author C&eacute;drik LIME
  */
 public abstract class BaseSessionComparator<T> implements Comparator<Session> {
