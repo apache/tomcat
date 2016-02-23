615a41740ceac9fbe98ae785f3e0023c9051ad72
Mark Thomas <markt@apache.org>
2016-02-23 20:34:39 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=59015 Fix potential cause of endless APR Poller loop during shutdown if the Poller experiences an error during the shutdown process.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1731946 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/net/AprEndpoint.java
+++ java/org/apache/tomcat/util/net/AprEndpoint.java
@@ -1844,7 +1844,7 @@ public class AprEndpoint extends AbstractEndpoint<Long> implements SNICallBack {
                             }
                         }
 
-                        if (reset) {
+                        if (reset && pollerRunning) {
                             // Reallocate the current poller
                             int count = Poll.pollset(pollers[i], desc);
                             long newPoller = allocatePoller(actualPollerSize, pool, -1);
