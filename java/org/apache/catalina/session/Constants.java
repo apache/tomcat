4d560c3fd0a839b3bb3e73e4bac48d096fe4c5a3
Mark Thomas <markt@apache.org>
2016-02-23 15:07:51 +0000

    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1731882 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/session/Constants.java
+++ java/org/apache/catalina/session/Constants.java
@@ -21,6 +21,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.apache.catalina.Globals;
+import org.apache.catalina.valves.CrawlerSessionManagerValve;
 
 /**
  * Manifest constants for the <code>org.apache.catalina.session</code>
@@ -42,6 +43,7 @@ public class Constants {
         Set<String> names = new HashSet<>();
         names.add(Globals.SUBJECT_ATTR);
         names.add(Globals.GSS_CREDENTIAL_ATTR);
+        names.add(CrawlerSessionManagerValve.class.getName());
         excludedAttributeNames = Collections.unmodifiableSet(names);
     }
 }
