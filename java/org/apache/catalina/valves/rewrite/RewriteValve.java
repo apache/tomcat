af77543a9fe90f83628ed2abe0ae334a56e30268
Remy Maucherat <remm@apache.org>
2016-03-01 09:45:27 +0000

    Fix incorrect parsing of the NE and NC flags in rewrite rules.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1733019 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/valves/rewrite/RewriteValve.java
+++ java/org/apache/catalina/valves/rewrite/RewriteValve.java
@@ -689,12 +689,12 @@ public class RewriteValve extends ValveBase {
             rule.setHost(true);
         } else if (flag.startsWith("last") || flag.startsWith("L")) {
             rule.setLast(true);
-        } else if (flag.startsWith("next") || flag.startsWith("N")) {
-            rule.setNext(true);
         } else if (flag.startsWith("nocase") || flag.startsWith("NC")) {
             rule.setNocase(true);
         } else if (flag.startsWith("noescape") || flag.startsWith("NE")) {
             rule.setNoescape(true);
+        } else if (flag.startsWith("next") || flag.startsWith("N")) {
+            rule.setNext(true);
         // FIXME: Proxy not supported, would require proxy capabilities in Tomcat
         /* } else if (flag.startsWith("proxy") || flag.startsWith("P")) {
             rule.setProxy(true);*/
