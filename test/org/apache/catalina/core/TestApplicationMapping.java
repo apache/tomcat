260bd28b83a792d79f7bbc37b05e0c240d48fe83
Mark Thomas <markt@apache.org>
2016-03-08 15:11:05 +0000

    Checkstyle
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1734100 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/catalina/core/TestApplicationMapping.java
+++ test/org/apache/catalina/core/TestApplicationMapping.java
@@ -18,18 +18,20 @@ package org.apache.catalina.core;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Mapping;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import org.apache.catalina.Context;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.catalina.startup.TomcatBaseTest;
 import org.apache.tomcat.util.buf.ByteChunk;
-import org.junit.Assert;
-import org.junit.Test;
 
 public class TestApplicationMapping extends TomcatBaseTest {
 
