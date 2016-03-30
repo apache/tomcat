c76ddd7dfcfd83e4f32236399e677307903f099a
Mark Thomas <markt@apache.org>
2016-03-30 19:30:20 +0000

    Add the code (commented out else it will cause problems on systems without the method) to set the certificate chain when using NIO/NIO2 + OpenSSL + JSSE config
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1737157 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/net/openssl/OpenSSLContext.java
+++ java/org/apache/tomcat/util/net/openssl/OpenSSLContext.java
@@ -322,12 +322,20 @@ public class OpenSSLContext implements org.apache.tomcat.util.net.SSLContext {
             } else {
                 X509KeyManager keyManager = chooseKeyManager(kms);
                 String alias = certificate.getCertificateKeyAlias();
-                X509Certificate certificate = keyManager.getCertificateChain(alias)[0];
+                X509Certificate[] chain = keyManager.getCertificateChain(alias);
                 PrivateKey key = keyManager.getPrivateKey(alias);
                 StringBuilder sb = new StringBuilder(BEGIN_KEY);
                 sb.append(Base64.getMimeEncoder(64, new byte[] {'\n'}).encodeToString(key.getEncoded()));
                 sb.append(END_KEY);
-                SSLContext.setCertificateRaw(ctx, certificate.getEncoded(), sb.toString().getBytes(StandardCharsets.US_ASCII), SSL.SSL_AIDX_RSA);
+                SSLContext.setCertificateRaw(ctx, chain[0].getEncoded(), sb.toString().getBytes(StandardCharsets.US_ASCII), SSL.SSL_AIDX_RSA);
+                /*
+                 * Uncomment the code block below once there has been a tc-native
+                 * release with this method and the minimum tc-native version
+                 * has been incremented.
+                for (int i = 1; i < chain.length; i++) {
+                    SSLContext.addChainCertificateRaw(ctx, chain[i].getEncoded());
+                }
+                */
             }
             // Client certificate verification
             int value = 0;
