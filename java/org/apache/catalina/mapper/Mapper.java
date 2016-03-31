b7d9ef8afa4854c3dbf0a195c74cd2f07c4590e0
Remy Maucherat <remm@apache.org>
2016-03-31 18:55:22 +0000

    59255: Fix posible NPE in mapper, using Konstantin Kolinko's idea.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1737270 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/mapper/Mapper.java
+++ java/org/apache/catalina/mapper/Mapper.java
@@ -879,7 +879,7 @@ public final class Mapper {
         }
 
         if(mappingData.wrapper == null && noServletPath &&
-                mappingData.context.getMapperContextRootRedirectEnabled()) {
+                contextVersion.object.getMapperContextRootRedirectEnabled()) {
             // The path is empty, redirect to "/"
             path.append('/');
             pathEnd = path.getEnd();
@@ -1008,7 +1008,7 @@ public final class Mapper {
                     file = contextVersion.resources.getResource(pathStr);
                 }
                 if (file != null && file.isDirectory() &&
-                        mappingData.context.getMapperDirectoryRedirectEnabled()) {
+                        contextVersion.object.getMapperDirectoryRedirectEnabled()) {
                     // Note: this mutates the path: do not do any processing
                     // after this (since we set the redirectPath, there
                     // shouldn't be any)
