c49b5f9c34f6d772d4d8c6e27a499ef31c3e67c9
Violeta Georgieva Georgieva <violetagg@apache.org>
2016-04-04 08:24:24 +0000

    Merged revision 1737632 from tomcat/trunk: Clarify in the log message that specifying both urlPatterns and value attributes in WebServlet and WebFilter annotations is not allowed.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1737635 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/startup/ContextConfig.java
+++ java/org/apache/catalina/startup/ContextConfig.java
@@ -2399,7 +2399,7 @@ public class ContextConfig implements LifecycleListener {
             if ("value".equals(name) || "urlPatterns".equals(name)) {
                 if (urlPatternsSet) {
                     throw new IllegalArgumentException(sm.getString(
-                            "contextConfig.urlPatternValue", className));
+                            "contextConfig.urlPatternValue", "WebServlet", className));
                 }
                 urlPatternsSet = true;
                 urlPatterns = processAnnotationsStringArray(evp.getValue());
@@ -2511,7 +2511,7 @@ public class ContextConfig implements LifecycleListener {
             if ("value".equals(name) || "urlPatterns".equals(name)) {
                 if (urlPatternsSet) {
                     throw new IllegalArgumentException(sm.getString(
-                            "contextConfig.urlPatternValue", className));
+                            "contextConfig.urlPatternValue", "WebFilter", className));
                 }
                 urlPatterns = processAnnotationsStringArray(evp.getValue());
                 urlPatternsSet = urlPatterns.length > 0;
