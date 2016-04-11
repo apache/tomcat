be7e11ef3e7d0cac6daee6852c01596b248f4f1d
Mark Thomas <markt@apache.org>
2016-04-11 21:33:56 +0000

    Avoid NPE in some error conditions / edge cases
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1738650 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/core/ApplicationMapping.java
+++ java/org/apache/catalina/core/ApplicationMapping.java
@@ -33,35 +33,36 @@ public class ApplicationMapping {
 
     public Mapping getMapping() {
         if (mapping == null) {
+            String servletName;
+            if (mappingData.wrapper == null) {
+                servletName = "";
+            } else {
+                servletName = mappingData.wrapper.getName();
+            }
             switch (mappingData.matchType) {
                 case CONTEXT_ROOT:
-                    mapping = new MappingImpl("", "", mappingData.matchType,
-                            mappingData.wrapper.getName());
+                    mapping = new MappingImpl("", "", mappingData.matchType, servletName);
                     break;
                 case DEFAULT:
-                    mapping = new MappingImpl("/", "/", mappingData.matchType,
-                            mappingData.wrapper.getName());
+                    mapping = new MappingImpl("/", "/", mappingData.matchType, servletName);
                     break;
                 case EXACT:
                     mapping = new MappingImpl(mappingData.wrapperPath.toString(),
-                            mappingData.wrapperPath.toString(), mappingData.matchType,
-                            mappingData.wrapper.getName());
+                            mappingData.wrapperPath.toString(), mappingData.matchType, servletName);
                     break;
                 case EXTENSION:
                     String path = mappingData.wrapperPath.toString();
                     int extIndex = path.lastIndexOf('.');
                     mapping = new MappingImpl(path.substring(0, extIndex),
-                            "*" + path.substring(extIndex), mappingData.matchType,
-                            mappingData.wrapper.getName());
+                            "*" + path.substring(extIndex), mappingData.matchType, servletName);
                     break;
                 case PATH:
                     mapping = new MappingImpl(mappingData.pathInfo.toString(),
                             mappingData.wrapperPath.toString() + "/*",
-                            mappingData.matchType, mappingData.wrapper.getName());
+                            mappingData.matchType, servletName);
                     break;
                 case UNKNOWN:
-                    mapping = new MappingImpl("", "", mappingData.matchType,
-                            mappingData.wrapper.getName());
+                    mapping = new MappingImpl("", "", mappingData.matchType, servletName);
                     break;
             }
         }
