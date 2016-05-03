apache/tomcat

commit 561ad2cb5ad1bc8e24d991f9da5657f57b17555b

Javadoc for scanWebInfClasses() explicitly states that access should only be via the ServletContext so access the files that way.
git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1742105 13f79535-47bb-0310-9956-ffa450edef68

java/org/apache/jasper/servlet/TldScanner.java
@@ -394,13 +394,18 @@ public class TldScanner {
             // JARs have been unpacked into WEB-INF/classes as happens with some
             // IDEs.
 
-            // We know that WEB-INF/classes/META-INF must be a directory on disk
-            String webappPath = WEB_INF + "classes";
-            String realPath = context.getRealPath(webappPath);
+            Set<String> paths = context.getResourcePaths(WEB_INF + "classes/META-INF");
 
-            File webInfClasses = new File(realPath);
-
-            scan(webInfClasses, webappPath, true);
+            for (String path : paths) {
+                if (path.endsWith(TLD_EXT)) {
+                    String webappPath = WEB_INF + "classes/META-INF" + path;
+                    try {
+                        parseTld(webappPath);
+                    } catch (SAXException e) {
+                        throw new IOException(e);
+                    }
+                }
+            }
         }
 
 

commit 2ce1826c179509da12fcc691c539ac34c743dd6f

Correct regression in r1742105.
git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1742111 13f79535-47bb-0310-9956-ffa450edef68

java/org/apache/jasper/servlet/TldScanner.java
@@ -398,9 +398,8 @@ public class TldScanner {
 
             for (String path : paths) {
                 if (path.endsWith(TLD_EXT)) {
-                    String webappPath = WEB_INF + "classes/META-INF" + path;
                     try {
-                        parseTld(webappPath);
+                        parseTld(path);
                     } catch (SAXException e) {
                         throw new IOException(e);
                     }