b50d3e52230df0b7dd3be60cdc26099bf618afaf
Mark Thomas <markt@apache.org>
2016-05-04 12:41:23 +0000

    Javadoc fixes
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1742263 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/Jar.java
+++ java/org/apache/tomcat/Jar.java
@@ -21,16 +21,16 @@ import java.io.InputStream;
 import java.net.URL;
 import java.util.jar.Manifest;
 
-import org.apache.tomcat.util.scan.JarFactory;
-
 /**
  * Provides an abstraction for use by the various classes that need to scan
- * JARs. The classes provided by the JRE for accessing JARs ({@link java.util.jar.JarFile} and
- * {@link java.util.jar.JarInputStream}) have significantly different performance
- * characteristics depending on the form of the URL used to access the JAR.
- * For file based JAR {@link java.net.URL}s, {@link java.util.jar.JarFile} is faster but for non-file
- * based {@link java.net.URL}s, {@link java.util.jar.JarFile} creates a copy of the JAR in the
- * temporary directory so {@link java.util.jar.JarInputStream} is faster.
+ * JARs. The classes provided by the JRE for accessing JARs
+ * ({@link java.util.jar.JarFile} and {@link java.util.jar.JarInputStream}) have
+ * significantly different performance characteristics depending on the form of
+ * the URL used to access the JAR. For file based JAR {@link java.net.URL}s,
+ * {@link java.util.jar.JarFile} is faster but for non-file based
+ * {@link java.net.URL}s, {@link java.util.jar.JarFile} creates a copy of the
+ * JAR in the temporary directory so {@link java.util.jar.JarInputStream} is
+ * faster.
  */
 public interface Jar extends AutoCloseable {
 
@@ -109,7 +109,8 @@ public interface Jar extends AutoCloseable {
      * be used, rather the jar:jar:file:... form (that the JRE does not
      * understand will be used). Note that this means that any code using these
      * URLs will need to understand the jar:jar:file:... form and use the
-     * {@link JarFactory} to ensure resources are accessed correctly.
+     * {@link org.apache.tomcat.util.scan.JarFactory} to ensure resources are
+     * accessed correctly.
      *
      * @param entry The entry to generate the URL for
      *
