4e234e7fee87cda1cdef8399125b0fb2c39e5fe2
Keiichi Fujino <kfujino@apache.org>
2016-05-09 04:48:21 +0000

    Implement map state in the replication map. Ensure that the ping is not executed during the start/stop of the replication map.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1742878 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/tribes/tipis/AbstractReplicatedMap.java
+++ java/org/apache/catalina/tribes/tipis/AbstractReplicatedMap.java
@@ -149,6 +149,11 @@ public abstract class AbstractReplicatedMap<K,V>
      */
     protected transient String mapname = "";
 
+    /**
+     * State of this map
+     */
+    private volatile transient State state = State.NEW;
+
 //------------------------------------------------------------------------------
 //              map owner interface
 //------------------------------------------------------------------------------
@@ -229,7 +234,6 @@ public abstract class AbstractReplicatedMap<K,V>
         //listen for membership notifications
         this.channel.addMembershipListener(this);
 
-
         try {
             //broadcast our map, this just notifies other members of our existence
             broadcast(MapMessage.MSG_INIT, true);
@@ -244,6 +248,7 @@ public abstract class AbstractReplicatedMap<K,V>
                 throw new RuntimeException("Unable to start replicated map.",x);
             }
         }
+        this.state = State.INITIALIZED;
         long complete = System.currentTimeMillis() - start;
         if (log.isInfoEnabled())
             log.info("AbstractReplicatedMap[" +mapContextName + "] initialization was completed in " + complete + " ms.");
@@ -339,6 +344,7 @@ public abstract class AbstractReplicatedMap<K,V>
     }
 
     public void breakdown() {
+        this.state = State.DESTROYED;
         if (this.rpcChannel != null) {
             this.rpcChannel.breakdown();
         }
@@ -887,7 +893,7 @@ public abstract class AbstractReplicatedMap<K,V>
     @Override
     public void heartbeat() {
         try {
-            ping(accessTimeout);
+            if (this.state.isAvailable()) ping(accessTimeout);
         }catch ( Exception x ) {
             log.error("Unable to send AbstractReplicatedMap.ping message",x);
         }
@@ -1624,4 +1630,19 @@ public abstract class AbstractReplicatedMap<K,V>
         this.accessTimeout = accessTimeout;
     }
 
+    private static enum State {
+        NEW(false),
+        INITIALIZED(true),
+        DESTROYED(false);
+
+        private final boolean available;
+
+        private State(boolean available) {
+            this.available = available;
+        }
+
+        public boolean isAvailable() {
+            return available;
+        }
+    }
 }
