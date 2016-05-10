0e5b4ac801f51b1362cf354d25b9eb04e53a8cfd
Mark Thomas <markt@apache.org>
2016-05-10 08:47:46 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=59449 Ensure that the process to remove a child container is the reverse of the process to add one. Patch provided by Huxing Zhang.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1743129 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/core/ContainerBase.java
+++ java/org/apache/catalina/core/ContainerBase.java
@@ -1014,12 +1014,6 @@ public abstract class ContainerBase extends LifecycleMBeanBase
             return;
         }
         
-        synchronized(children) {
-            if (children.get(child.getName()) == null)
-                return;
-            children.remove(child.getName());
-        }
-        
         try {
             if (child.getState().isAvailable()) {
                 child.stop();
@@ -1027,8 +1021,6 @@ public abstract class ContainerBase extends LifecycleMBeanBase
         } catch (LifecycleException e) {
             log.error("ContainerBase.removeChild: stop: ", e);
         }
-        
-        fireContainerEvent(REMOVE_CHILD_EVENT, child);
         
         try {
             // child.destroy() may have already been called which would have
@@ -1041,6 +1033,13 @@ public abstract class ContainerBase extends LifecycleMBeanBase
             log.error("ContainerBase.removeChild: destroy: ", e);
         }
 
+        synchronized(children) {
+            if (children.get(child.getName()) == null)
+                return;
+            children.remove(child.getName());
+        }
+
+        fireContainerEvent(REMOVE_CHILD_EVENT, child);
     }
 
 
