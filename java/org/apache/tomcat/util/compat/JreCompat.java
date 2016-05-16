6d96e3149d6e35c9241c87bf709ac8393475e146
Mark Thomas <markt@apache.org>
2016-05-16 21:48:27 +0000

    Back-port Java 9 support to compatibility util class
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1744155 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/compat/JreCompat.java
+++ java/org/apache/tomcat/util/compat/JreCompat.java
@@ -33,26 +33,36 @@ public class JreCompat {
     private static final JreCompat instance;
     private static StringManager sm =
             StringManager.getManager(JreCompat.class.getPackage().getName());
-    private static final boolean jre7Available;
+    private static final boolean jre9Available;
     private static final boolean jre8Available;
+    private static final boolean jre7Available;
     
     
     static {
         // This is Tomcat 7 with a minimum Java version of Java 6. The latest
-        // Java version the optional features require is Java 8.
+        // Java version the optional features require is Java 9.
         // Look for the highest supported JVM first
-        if (Jre8Compat.isSupported()) {
-            instance = new Jre8Compat();
-            jre7Available = true;
+        if (Jre9Compat.isSupported()) {
+            instance = new Jre9Compat();
+            jre9Available = true;
             jre8Available = true;
+            jre7Available = true;
+        }
+        else if (Jre8Compat.isSupported()) {
+            instance = new Jre8Compat();
+            jre9Available = false;
+            jre8Available = true;
+            jre7Available = true;
         } else if (Jre7Compat.isSupported()) {
             instance = new Jre7Compat();
-            jre7Available = true;
+            jre9Available = false;
             jre8Available = false;
+            jre7Available = true;
         } else {
             instance = new JreCompat();
-            jre7Available = false;
+            jre9Available = false;
             jre8Available = false;
+            jre7Available = false;
         }
     }
     
@@ -130,4 +140,25 @@ public class JreCompat {
         throw new UnsupportedOperationException(sm.getString("jreCompat.noServerCipherSuiteOrder"));
     }
 
+
+    // Java 6 implementation of Java 9 methods
+
+    public static boolean isJre9Available() {
+        return jre9Available;
+    }
+
+
+    /**
+     * Test if the provided exception is an instance of
+     * java.lang.reflect.InaccessibleObjectException.
+     *
+     * @param e The exception to test
+     *
+     * @return {@code true} if the exception is an instance of
+     *         InaccessibleObjectException, otherwise {@code false}
+     */
+    public boolean isInstanceOfInaccessibleObjectException(Exception e) {
+        // Exception does not exist prior to Java 9
+        return false;
+    }
 }
