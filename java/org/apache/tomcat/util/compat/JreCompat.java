4a690294ba71aed953ae2c9d5cc5850dae509b58
Mark Thomas <markt@apache.org>
2016-05-16 21:43:56 +0000

    Back-port Java 9 support to compatibility util class
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.5.x/trunk@1744151 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/compat/JreCompat.java
+++ java/org/apache/tomcat/util/compat/JreCompat.java
@@ -30,18 +30,26 @@ public class JreCompat {
     private static final JreCompat instance;
     private static StringManager sm =
             StringManager.getManager(JreCompat.class.getPackage().getName());
+    private static final boolean jre9Available;
     private static final boolean jre8Available;
 
 
     static {
         // This is Tomcat 8 with a minimum Java version of Java 7. The latest
-        // Java version the optional features require is Java 8.
+        // Java version the optional features require is Java 9.
         // Look for the highest supported JVM first
-        if (Jre8Compat.isSupported()) {
+        if (Jre9Compat.isSupported()) {
+            instance = new Jre9Compat();
+            jre9Available = true;
+            jre8Available = true;
+        }
+        else if (Jre8Compat.isSupported()) {
             instance = new Jre8Compat();
+            jre9Available = false;
             jre8Available = true;
         } else {
             instance = new JreCompat();
+            jre9Available = false;
             jre8Available = false;
         }
     }
@@ -62,5 +70,27 @@ public class JreCompat {
     @SuppressWarnings("unused")
     public void setUseServerCipherSuitesOrder(SSLEngine engine, boolean useCipherSuitesOrder) {
         throw new UnsupportedOperationException(sm.getString("jreCompat.noServerCipherSuiteOrder"));
+    }
+
+
+    // Java 7 implementation of Java 9 methods
+
+    public static boolean isJre9Available() {
+        return jre9Available;
+    }
+
+
+    /**
+     * Test if the provided exception is an instance of
+     * java.lang.reflect.InaccessibleObjectException.
+     *
+     * @param e The exception to test
+     *
+     * @return {@code true} if the exception is an instance of
+     *         InaccessibleObjectException, otherwise {@code false}
+     */
+    public boolean isInstanceOfInaccessibleObjectException(Exception e) {
+        // Exception does not exist prior to Java 9
+        return false;
     }
 }
