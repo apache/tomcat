05f900ca5ed7cbccb62ea76f703c391a89bf8358
Mark Thomas <markt@apache.org>
2016-05-17 20:54:41 +0000

    Remove unnecessary field.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1744324 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/jasper/servlet/JasperLoader.java
+++ java/org/apache/jasper/servlet/JasperLoader.java
@@ -36,14 +36,12 @@ import org.apache.jasper.Constants;
 public class JasperLoader extends URLClassLoader {
 
     private final PermissionCollection permissionCollection;
-    private final ClassLoader parent;
     private final SecurityManager securityManager;
 
     public JasperLoader(URL[] urls, ClassLoader parent,
                         PermissionCollection permissionCollection) {
         super(urls, parent);
         this.permissionCollection = permissionCollection;
-        this.parent = parent;
         this.securityManager = System.getSecurityManager();
     }
 
@@ -122,7 +120,7 @@ public class JasperLoader extends URLClassLoader {
         if( !name.startsWith(Constants.JSP_PACKAGE_NAME + '.') ) {
             // Class is not in org.apache.jsp, therefore, have our
             // parent load it
-            clazz = parent.loadClass(name);
+            clazz = getParent().loadClass(name);
             if( resolve )
                 resolveClass(clazz);
             return clazz;
@@ -139,7 +137,7 @@ public class JasperLoader extends URLClassLoader {
      */
     @Override
     public InputStream getResourceAsStream(String name) {
-        InputStream is = parent.getResourceAsStream(name);
+        InputStream is = getParent().getResourceAsStream(name);
         if (is == null) {
             URL url = findResource(name);
             if (url != null) {
