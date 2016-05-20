7ab5377e062525c37af1ea23d065ec998a676f95
Mark Thomas <markt@apache.org>
2016-05-20 11:04:47 +0000

    Deprecate clearReferencesStatic
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1744705 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/catalina/loader/TestWebappClassLoaderWeaving.java
+++ test/org/apache/catalina/loader/TestWebappClassLoaderWeaving.java
@@ -239,6 +239,7 @@ public class TestWebappClassLoaderWeaving extends TomcatBaseTest {
 
     }
 
+    @SuppressWarnings("deprecation")
     @Test
     public void testCopiedClassLoaderExcludesResourcesAndTransformers() throws Exception {
 
