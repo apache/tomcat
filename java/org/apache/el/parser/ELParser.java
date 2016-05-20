49c16faa0370dbf5d7481ac85f678908a2f5f948
Mark Thomas <markt@apache.org>
2016-05-20 10:34:50 +0000

    Fix an EL memory leak. Note: This fix requires changing generated code. A bug has been raised with JavaCC: https://java.net/jira/browse/JAVACC-293
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1744689 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/el/parser/ELParser.java
+++ java/org/apache/el/parser/ELParser.java
@@ -2099,7 +2099,16 @@ public class ELParser/*@bgen(jjtree)*/implements ELParserTreeConstants, ELParser
     throw generateParseException();
   }
 
-  static private final class LookaheadSuccess extends java.lang.Error { }
+  static private final class LookaheadSuccess extends java.lang.Error {
+      /*
+       * Over-ridden to avoid memory leak via Throwable.backtrace
+       * https://java.net/jira/browse/JAVACC-293
+       */
+    @Override
+    public synchronized Throwable fillInStackTrace() {
+        return this;
+    }
+  }
   final private LookaheadSuccess jj_ls = new LookaheadSuccess();
   private boolean jj_scan_token(int kind) {
     if (jj_scanpos == jj_lastpos) {
