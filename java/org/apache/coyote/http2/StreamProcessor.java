b7963f175e9182cada6eaca88cf0e1c880910efa
Mark Thomas <markt@apache.org>
2016-05-31 21:04:50 +0000

    Be consistent in ordering of method calls.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.5.x/trunk@1746346 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http2/StreamProcessor.java
+++ java/org/apache/coyote/http2/StreamProcessor.java
@@ -130,8 +130,8 @@ public class StreamProcessor extends AbstractProcessor implements Runnable {
             try {
                 stream.flushData();
             } catch (IOException ioe) {
-                response.setErrorException(ioe);
                 setErrorState(ErrorState.CLOSE_CONNECTION_NOW, ioe);
+                response.setErrorException(ioe);
             }
             break;
         }
@@ -367,8 +367,8 @@ public class StreamProcessor extends AbstractProcessor implements Runnable {
                 PushToken pushToken = (PushToken) param;
                 pushToken.setResult(stream.push(pushToken.getPushTarget()));
             } catch (IOException ioe) {
-                response.setErrorException(ioe);
                 setErrorState(ErrorState.CLOSE_CONNECTION_NOW, ioe);
+                response.setErrorException(ioe);
             }
             break;
         }
