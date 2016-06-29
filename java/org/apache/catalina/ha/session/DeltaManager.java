b942efd692c1a39ea9d401ae75494e001e11c89f
Mark Thomas <markt@apache.org>
2016-06-29 19:27:50 +0000

    Deprecate unused constant
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1750697 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/ha/session/DeltaManager.java
+++ java/org/apache/catalina/ha/session/DeltaManager.java
@@ -70,7 +70,9 @@ public class DeltaManager extends ClusterManagerBase{
 
     /**
      * The descriptive name of this Manager implementation (for logging).
+     * @deprecated Unused. Will be removed in Tomcat 9
      */
+    @Deprecated
     protected static final String managerName = "DeltaManager";
     protected String name = null;
 
