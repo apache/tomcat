9f7c171c4cfc44249d368cc732c9a013a275b519
Mark Thomas <markt@apache.org>
2016-07-02 21:16:48 +0000

    Update the packaged version of the Tomcat Native Library to 1.2.8 to pick up the latest fixes and make 1.2.8 the minimum recommended version.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1751100 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/core/AprLifecycleListener.java
+++ java/org/apache/catalina/core/AprLifecycleListener.java
@@ -65,12 +65,11 @@ public class AprLifecycleListener
 
     // ---------------------------------------------- Constants
 
-
     protected static final int TCN_REQUIRED_MAJOR = 1;
     protected static final int TCN_REQUIRED_MINOR = 1;
     protected static final int TCN_REQUIRED_PATCH = 32;
-    protected static final int TCN_RECOMMENDED_MINOR = 1;
-    protected static final int TCN_RECOMMENDED_PV = 33;
+    protected static final int TCN_RECOMMENDED_MINOR = 2;
+    protected static final int TCN_RECOMMENDED_PV = 8;
 
 
     // ---------------------------------------------- Properties
