6526f98de41df662c9618926455c75f2f541d266
Mark Thomas <markt@apache.org>
2016-08-04 20:40:16 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=59899 Update Tomcat's copy of the Java Persistence annotations to include the changes made in 2.1 / JavaEE 7.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1755230 13f79535-47bb-0310-9956-ffa450edef68

--- java/javax/persistence/SynchronizationType.java
+++ java/javax/persistence/SynchronizationType.java
@@ -16,18 +16,7 @@
  */
 package javax.persistence;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
-@Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD})
-@Retention(RetentionPolicy.RUNTIME)
-
-public @interface PersistenceContext {
-   String name() default "";
-   String unitName() default "";
-   PersistenceContextType type() default PersistenceContextType.TRANSACTION;
-   PersistenceProperty[] properties() default {};
-   SynchronizationType synchronization() default SynchronizationType.SYNCHRONIZED;
+public enum SynchronizationType {
+    SYNCHRONIZED,
+    UNSYNCHRONIZED
 }
