7286bd91d04061d3e35306b042d8becf0d6bfa38
Mark Thomas <markt@apache.org>
2016-08-30 19:40:04 +0000

    Add a feature to the Digester that enables it to pass the encoding of the source XML document to the object being populated.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.5.x/trunk@1758449 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/org/apache/tomcat/util/digester/DocumentProperties.java
@@ -0,0 +1,34 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.tomcat.util.digester;
+
+/**
+ *
+ * A collection of interfaces, one per property, that enables the object being
+ * populated by the digester to signal to the digester that it supports the
+ * given property and that the digester should populate that property if
+ * available.
+ */
+public interface DocumentProperties {
+
+    /**
+     * The encoding used by the source XMl document.
+     */
+    public interface Encoding {
+        public void setEncoding(String encoding);
+    }
+}
