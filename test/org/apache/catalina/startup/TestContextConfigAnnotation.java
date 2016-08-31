ca1269f752a6ce96402fd7fbc64f6e2775d8782f
Violeta Georgieva Georgieva <violetagg@apache.org>
2016-08-31 14:47:51 +0000

    Fix the creation of the file in the test. The test was failing when there is a space in the path.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.5.x/trunk@1758613 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/catalina/startup/TestContextConfigAnnotation.java
+++ test/org/apache/catalina/startup/TestContextConfigAnnotation.java
@@ -18,6 +18,7 @@ package org.apache.catalina.startup;
 
 import java.beans.PropertyChangeListener;
 import java.io.File;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -364,12 +365,13 @@ public class TestContextConfigAnnotation {
      *
      * @param className
      * @return File Resource
+     * @throws URISyntaxException
      */
-    private File paramClassResource(String className) {
+    private File paramClassResource(String className) throws URISyntaxException {
         URL url = getClass().getClassLoader().getResource(className + ".class");
         assertNotNull(url);
 
-        File file = new File(url.getPath());
+        File file = new File(url.toURI());
         return file;
     }
 }
