0bd181890ea544a9a97398c597d8a0c9076dd74b
Keiichi Fujino <kfujino@apache.org>
2016-09-15 07:48:23 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=60117 Ensure that the name of LogLevel is localized when using OneLineFormatter. Patch provided by Tatsuya Bessho.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1760890 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/juli/OneLineFormatter.java
+++ java/org/apache/juli/OneLineFormatter.java
@@ -74,7 +74,7 @@ public class OneLineFormatter extends Formatter {
 
         // Severity
         sb.append(' ');
-        sb.append(record.getLevel());
+        sb.append(record.getLevel().getLocalizedName());
 
         // Thread
         sb.append(' ');
