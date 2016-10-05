0e7b5b7834731ef24e8828f0a8795cbb7eb94f1c
Mark Thomas <markt@apache.org>
2016-10-05 11:02:45 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=60192 Correct a typo in the status output of the Manager application. Patch provided by  Radhakrishna Pemmasani.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.5.x/trunk@1763413 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/manager/StatusTransformer.java
+++ java/org/apache/catalina/manager/StatusTransformer.java
@@ -305,7 +305,7 @@ public class StatusTransformer {
             writer.print(mBeanServer.getAttribute(tpName, "currentThreadsBusy"));
             try {
                 Object value = mBeanServer.getAttribute(tpName, "keepAliveCount");
-                writer.print(" Keeped alive sockets count: ");
+                writer.print(" Keep alive sockets count: ");
                 writer.print(value);
             } catch (Exception e) {
                 // Ignore
