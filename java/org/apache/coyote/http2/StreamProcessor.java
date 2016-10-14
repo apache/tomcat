966c5fd75d67f6072a2e10c0f6a7565d6a28bfb5
Mark Thomas <markt@apache.org>
2016-10-14 10:49:19 +0000

    Align with HTTP/1.1. An unhandled exception should trigger a 500 response
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1764867 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http2/StreamProcessor.java
+++ java/org/apache/coyote/http2/StreamProcessor.java
@@ -221,6 +221,7 @@ class StreamProcessor extends AbstractProcessor implements Runnable {
             if (log.isDebugEnabled()) {
                 log.debug(sm.getString("streamProcessor.service.error"), e);
             }
+            response.setStatus(500);
             setErrorState(ErrorState.CLOSE_NOW, e);
         }
 
