85c63227edabbfb4f2f500fc557480a190135d21
Mark Thomas <markt@apache.org>
2016-10-20 10:09:51 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=60232
    The header read buffer needs to be at least the size of the largest header.
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.5.x/trunk@1765798 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http2/Constants.java
+++ java/org/apache/coyote/http2/Constants.java
@@ -20,4 +20,7 @@ public class Constants {
 
     // Prioritisation
     public static final int DEFAULT_WEIGHT = 16;
+
+    // Parsing
+    static final int DEFAULT_HEADER_READ_BUFFER_SIZE = 1024;
 }
