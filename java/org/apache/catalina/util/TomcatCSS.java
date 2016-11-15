de277d4e4c2319baeb7dbb0e268794cd40185936
Violeta Georgieva Georgieva <violetagg@apache.org>
2016-11-15 13:58:33 +0000

    Readability improvements for CSS used in DefaultServlet and ErrorReportValve. Patch provided by Michael Osipov.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1769833 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/util/TomcatCSS.java
+++ java/org/apache/catalina/util/TomcatCSS.java
@@ -22,16 +22,15 @@ package org.apache.catalina.util;
 public class TomcatCSS {
 
     public static final String TOMCAT_CSS =
-        "H1 {font-family:Tahoma,Arial,sans-serif;color:white;background-color:#525D76;font-size:22px;} " +
-        "H2 {font-family:Tahoma,Arial,sans-serif;color:white;background-color:#525D76;font-size:16px;} " +
-        "H3 {font-family:Tahoma,Arial,sans-serif;color:white;background-color:#525D76;font-size:14px;} " +
-        "BODY {font-family:Tahoma,Arial,sans-serif;color:black;background-color:white;} " +
-        "B {font-family:Tahoma,Arial,sans-serif;color:white;background-color:#525D76;} " +
-        "P {font-family:Tahoma,Arial,sans-serif;background:white;color:black;font-size:12px;}" +
-        "A {color : black;}" +
-        "A.name {color : black;}" +
-        ".line {height: 1px; background-color: #525D76; border: none;}";
+        "h1 {font-family:Tahoma,Arial,sans-serif;color:white;background-color:#525D76;font-size:22px;} " +
+        "h2 {font-family:Tahoma,Arial,sans-serif;color:white;background-color:#525D76;font-size:16px;} " +
+        "h3 {font-family:Tahoma,Arial,sans-serif;color:white;background-color:#525D76;font-size:14px;} " +
+        "body {font-family:Tahoma,Arial,sans-serif;color:black;background-color:white;} " +
+        "b {font-family:Tahoma,Arial,sans-serif;color:white;background-color:#525D76;} " +
+        "p {font-family:Tahoma,Arial,sans-serif;background:white;color:black;font-size:12px;} " +
+        "a {color:black;} " +
+        "a.name {color:black;} " +
+        ".line {height:1px;background-color:#525D76;border:none;}";
 
 
 }
-
