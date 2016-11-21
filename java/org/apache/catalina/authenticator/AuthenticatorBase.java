40ffdf6d79ae30e441be5201973e1cdcbdbbb7ee
Mark Thomas <markt@apache.org>
2016-11-21 21:09:09 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=60380 Ensure that a call to HttpServletRequest#logout() triggers a call to TomcatPrincipal#logout(). Based on a patch by Michael Osipov.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1770762 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/authenticator/AuthenticatorBase.java
+++ java/org/apache/catalina/authenticator/AuthenticatorBase.java
@@ -48,6 +48,7 @@ import org.apache.catalina.LifecycleException;
 import org.apache.catalina.Manager;
 import org.apache.catalina.Realm;
 import org.apache.catalina.Session;
+import org.apache.catalina.TomcatPrincipal;
 import org.apache.catalina.Valve;
 import org.apache.catalina.Wrapper;
 import org.apache.catalina.authenticator.jaspic.CallbackHandlerImpl;
@@ -61,6 +62,7 @@ import org.apache.catalina.valves.ValveBase;
 import org.apache.coyote.ActionCode;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
+import org.apache.tomcat.util.ExceptionUtils;
 import org.apache.tomcat.util.descriptor.web.LoginConfig;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.apache.tomcat.util.http.FastHttpDateFormat;
@@ -1104,6 +1106,16 @@ public abstract class AuthenticatorBase extends ValveBase
                 serverAuthContext.cleanSubject(messageInfo, client);
             } catch (AuthException e) {
                 log.debug(sm.getString("authenticator.jaspicCleanSubjectFail"), e);
+            }
+        }
+
+        Principal p = request.getPrincipal();
+        if (p instanceof TomcatPrincipal) {
+            try {
+                ((TomcatPrincipal) p).logout();
+            } catch (Throwable t) {
+                ExceptionUtils.handleThrowable(t);
+                log.debug(sm.getString("authenticator.tomcatPrincipalLogoutFail"), t);
             }
         }
 
