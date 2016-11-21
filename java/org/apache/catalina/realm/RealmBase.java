07744b492142d42a67b30f995dcc76c7c472594b
Mark Thomas <markt@apache.org>
2016-11-21 11:43:35 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=60393 Use consistent parameter naming in implementations of Realm#authenticate(GSSContext, boolean)
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1770656 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/realm/RealmBase.java
+++ java/org/apache/catalina/realm/RealmBase.java
@@ -475,7 +475,7 @@ public abstract class RealmBase extends LifecycleMBeanBase implements Realm {
      * {@inheritDoc}
      */
     @Override
-    public Principal authenticate(GSSContext gssContext, boolean storeCred) {
+    public Principal authenticate(GSSContext gssContext, boolean storeCreds) {
         if (gssContext.isEstablished()) {
             GSSName gssName = null;
             try {
@@ -495,7 +495,7 @@ public abstract class RealmBase extends LifecycleMBeanBase implements Realm {
                     }
                 }
                 GSSCredential gssCredential = null;
-                if (storeCred && gssContext.getCredDelegState()) {
+                if (storeCreds && gssContext.getCredDelegState()) {
                     try {
                         gssCredential = gssContext.getDelegCred();
                     } catch (GSSException e) {
