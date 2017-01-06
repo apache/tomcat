c51e3a3ae9ecf953dea203e0c4378dd8bff5f128
Remy Maucherat <remm@apache.org>
2017-01-06 13:45:42 +0000

    60497: Follow up fix using a better variable name for the tag reuse flag.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1777608 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/jasper/compiler/Generator.java
+++ java/org/apache/jasper/compiler/Generator.java
@@ -2332,7 +2332,7 @@ class Generator {
                 out.print(tagHandlerClassName);
                 out.println(".class);");
                 out.printin("boolean ");
-                out.print(n.getTagHandlerPoolName());
+                out.print(tagHandlerVar);
                 out.println("_reused = false;");
             } else {
                 writeNewInstance(tagHandlerVar, tagHandlerClassName);
@@ -2545,7 +2545,7 @@ class Generator {
                 out.print(".reuse(");
                 out.print(tagHandlerVar);
                 out.println(");");
-                out.printin(n.getTagHandlerPoolName());
+                out.printin(tagHandlerVar);
                 out.println("_reused = true;");
             }
 
@@ -2555,7 +2555,7 @@ class Generator {
             out.pushIndent();
             if (isPoolingEnabled && !(n.implementsJspIdConsumer())) {
                 out.printin("if (!");
-                out.print(n.getTagHandlerPoolName());
+                out.print(tagHandlerVar);
                 out.println("_reused) {");
                 out.pushIndent();
             }
