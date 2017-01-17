2078622e701984acdc03705ce806a6c815da393b
kfujino <kfujino@unknown>
2017-01-17 08:37:33 +0000

    Make the accessTimeout configurable in BackupManager. The accessTimeout is used as a timeout period for PING in replication map.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.5.x/trunk@1779150 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/ha/session/BackupManager.java
+++ java/org/apache/catalina/ha/session/BackupManager.java
@@ -69,6 +69,11 @@ public class BackupManager extends ClusterManagerBase
     private boolean terminateOnStartFailure = false;
 
     /**
+     * The timeout for a ping message in replication map.
+     */
+    private long accessTimeout = 5000;
+
+    /**
      * Constructor, just calls super()
      *
      */
@@ -144,6 +149,7 @@ public class BackupManager extends ClusterManagerBase
                     this, cluster.getChannel(), rpcTimeout, getMapName(),
                     getClassLoaders(), terminateOnStartFailure);
             map.setChannelSendOptions(mapSendOptions);
+            map.setAccessTimeout(accessTimeout);
             this.sessions = map;
         }  catch ( Exception x ) {
             log.error(sm.getString("backupManager.startUnable", getName()),x);
@@ -215,6 +221,14 @@ public class BackupManager extends ClusterManagerBase
         return terminateOnStartFailure;
     }
 
+    public long getAccessTimeout() {
+        return accessTimeout;
+    }
+
+    public void setAccessTimeout(long accessTimeout) {
+        this.accessTimeout = accessTimeout;
+    }
+
     @Override
     public String[] getInvalidatedSessions() {
         return new String[0];
@@ -227,6 +241,7 @@ public class BackupManager extends ClusterManagerBase
         result.mapSendOptions = mapSendOptions;
         result.rpcTimeout = rpcTimeout;
         result.terminateOnStartFailure = terminateOnStartFailure;
+        result.accessTimeout = accessTimeout;
         return result;
     }
 
