2ff56724e9eab31ae7859a6c8c824c5cee584959
Mark Thomas <markt@apache.org>
2017-01-31 15:15:43 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=55938 Resolve remaining issues Note that the use of -1 to indicate the full array in File.(read|write)[Full] has been removed since it was only partially implemented and the implementation was faulty.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1781102 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/jni/File.java
+++ java/org/apache/tomcat/jni/File.java
@@ -359,7 +359,7 @@ public class File {
      * @param thefile The file descriptor to write to.
      * @param buf The buffer which contains the data.
      * @param offset Start offset in buf
-     * @param nbytes The number of bytes to write; (-1) for full array.
+     * @param nbytes The number of bytes to write
      * @return The number of bytes written.
      */
     public static native int write(long thefile, byte[] buf, int offset, int nbytes);
@@ -400,7 +400,7 @@ public class File {
      * @param thefile The file descriptor to write to.
      * @param buf The buffer which contains the data.
      * @param offset Start offset in buf
-     * @param nbytes The number of bytes to write; (-1) for full array.
+     * @param nbytes The number of bytes to write
      * @return The number of bytes written.
      */
     public static native int writeFull(long thefile, byte[] buf, int offset, int nbytes);
@@ -469,7 +469,7 @@ public class File {
      * @param thefile The file descriptor to read from.
      * @param buf The buffer to store the data to.
      * @param offset Start offset in buf
-     * @param nbytes The number of bytes to read (-1) for full array.
+     * @param nbytes The number of bytes to read
      * @return the number of bytes read.
      */
     public static native int read(long thefile, byte[] buf,  int offset, int nbytes);
@@ -512,7 +512,7 @@ public class File {
      * @param thefile The file descriptor to read from.
      * @param buf The buffer to store the data to.
      * @param offset Start offset in buf
-     * @param nbytes The number of bytes to read (-1) for full array.
+     * @param nbytes The number of bytes to read
      * @return the number of bytes read.
      */
     public static native int readFull(long thefile, byte[] buf,  int offset, int nbytes);
