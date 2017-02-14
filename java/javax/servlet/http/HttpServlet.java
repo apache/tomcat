e06b22eef68315afe4815339b6112242beb1cae5
Mark Thomas <markt@apache.org>
2017-02-14 11:07:17 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=60697 Correct OPTIONS response for custom servlets so TRACE is not included when it is disabled
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.5.x/trunk@1782951 13f79535-47bb-0310-9956-ffa450edef68

--- java/javax/servlet/http/HttpServlet.java
+++ java/javax/servlet/http/HttpServlet.java
@@ -20,6 +20,7 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.text.MessageFormat;
 import java.util.Enumeration;
@@ -488,6 +489,18 @@ public abstract class HttpServlet extends GenericServlet {
         boolean ALLOW_DELETE = false;
         boolean ALLOW_TRACE = true;
         boolean ALLOW_OPTIONS = true;
+
+        // Tomcat specific hack to see if TRACE is allowed
+        Class<?> clazz = null;
+        try {
+            clazz = Class.forName("org.apache.catalina.connector.RequestFacade");
+            Method getAllowTrace = clazz.getMethod("getAllowTrace", (Class<?>[]) null);
+            ALLOW_TRACE = ((Boolean) getAllowTrace.invoke(req, (Object[]) null)).booleanValue();
+        } catch (ClassNotFoundException | NoSuchMethodException | SecurityException |
+                IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+            // Ignore. Not running on Tomcat. TRACE is always allowed.
+        }
+        // End of Tomcat specific hack
 
         for (int i=0; i<methods.length; i++) {
             Method m = methods[i];
