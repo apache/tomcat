dca8bb02f6a86e046d9f0545a5e7fb71ee12b301
Keiichi Fujino <kfujino@apache.org>
2017-03-06 08:47:13 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=60806 To avoid ClassNotFoundException, make sure that the web application class loader is passed to ReplicatedContext.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.5.x/trunk@1785608 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/ha/context/ReplicatedContext.java
+++ java/org/apache/catalina/ha/context/ReplicatedContext.java
@@ -56,19 +56,16 @@ public class ReplicatedContext extends StandardContext implements MapOwner {
      */
     @Override
     protected synchronized void startInternal() throws LifecycleException {
-
+        super.startInternal();
         try {
             CatalinaCluster catclust = (CatalinaCluster)this.getCluster();
-            if (this.context == null) this.context = new ReplApplContext(this);
             if ( catclust != null ) {
                 ReplicatedMap<String,Object> map = new ReplicatedMap<>(
                         this, catclust.getChannel(),DEFAULT_REPL_TIMEOUT,
                         getName(),getClassLoaders());
                 map.setChannelSendOptions(mapSendOptions);
                 ((ReplApplContext)this.context).setAttributeMap(map);
-                if (getAltDDName() != null) context.setAttribute(Globals.ALT_DD_ATTR, getAltDDName());
             }
-            super.startInternal();
         }  catch ( Exception x ) {
             log.error(sm.getString("replicatedContext.startUnable", getName()),x);
             throw new LifecycleException(sm.getString("replicatedContext.startFailed", getName()),x);
