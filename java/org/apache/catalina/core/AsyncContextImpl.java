dfa40863421d7681fed893b4256666491887e38c
Mark Thomas <markt@apache.org>
2017-03-06 22:14:05 +0000

    Ensure request and response facades are used when firing application listeners.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1785774 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/core/AsyncContextImpl.java
+++ java/org/apache/catalina/core/AsyncContextImpl.java
@@ -112,7 +112,7 @@ public class AsyncContextImpl implements AsyncContext, AsyncContextCallback {
                 }
             }
         } finally {
-            context.fireRequestDestroyEvent(request);
+            context.fireRequestDestroyEvent(request.getRequest());
             clearServletRequestResponse();
             context.unbind(Globals.IS_SECURITY_ENABLED, oldCL);
         }
