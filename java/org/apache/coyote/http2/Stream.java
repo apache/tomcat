4a6709edf8f61799cf1347c87cf645fa316908b3
Remy Maucherat <remm@apache.org>
2017-03-08 09:13:25 +0000

    Prevent a blocking read after a stream exception.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1785935 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http2/Stream.java
+++ java/org/apache/coyote/http2/Stream.java
@@ -683,7 +683,8 @@ class Stream extends AbstractStream implements HeaderEmitter {
 
             // Ensure that only one thread accesses inBuffer at a time
             synchronized (inBuffer) {
-                while (inBuffer.position() == 0 && !isInputFinished()) {
+                boolean canRead = isActive() && !isInputFinished();
+                while (inBuffer.position() == 0 && canRead) {
                     // Need to block until some data is written
                     try {
                         if (log.isDebugEnabled()) {
@@ -712,7 +713,7 @@ class Stream extends AbstractStream implements HeaderEmitter {
                     }
                     inBuffer.get(outBuffer, 0, written);
                     inBuffer.clear();
-                } else if (isInputFinished()) {
+                } else if (!canRead) {
                     return -1;
                 } else {
                     // Should never happen
