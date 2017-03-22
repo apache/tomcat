4ff7c435562332595af7c22db4acc165340c8800
Mark Thomas <markt@apache.org>
2017-03-22 10:34:57 +0000

    Trivial commit to test CI system. Experimenting with automatic formatting. No functional change.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1788085 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/ant/AbstractCatalinaCommandTask.java
+++ java/org/apache/catalina/ant/AbstractCatalinaCommandTask.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,17 +21,18 @@ import java.net.URLEncoder;
 
 import org.apache.tools.ant.BuildException;
 
-public abstract class AbstractCatalinaCommandTask extends
-        AbstractCatalinaTask {
+public abstract class AbstractCatalinaCommandTask extends AbstractCatalinaTask {
 
     /**
      * The context path of the web application we are managing.
      */
     protected String path = null;
 
+
     public String getPath() {
         return (this.path);
     }
+
 
     public void setPath(String path) {
         this.path = path;
@@ -42,26 +43,29 @@ public abstract class AbstractCatalinaCommandTask extends
      */
     protected String version = null;
 
+
     public String getVersion() {
         return (this.version);
     }
 
+
     public void setVersion(String version) {
         this.version = version;
     }
-    
-    // --------------------------------------------------------- Public Methods
 
+
+    // --------------------------------------------------------- Public Methods
     /**
      * Create query string for the specified command.
-     * 
+     *
      * @param command Command to be executed
+     *
+     * @return The generated query string
      *
      * @exception BuildException if an error occurs
      */
     public StringBuilder createQueryString(String command) throws BuildException {
         StringBuilder buffer = new StringBuilder();
-
         try {
             buffer.append(command);
             if (path == null) {
@@ -75,10 +79,8 @@ public abstract class AbstractCatalinaCommandTask extends
                 }
             }
         } catch (UnsupportedEncodingException e) {
-            throw new BuildException
-                ("Invalid 'charset' attribute: " + getCharset());
+            throw new BuildException("Invalid 'charset' attribute: " + getCharset());
         }
         return buffer;
     }
-
 }
