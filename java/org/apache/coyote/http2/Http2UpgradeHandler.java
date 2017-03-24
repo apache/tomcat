2cb9c724e6a2d15a5bc909c4bf1ab9dfc26fa362
Mark Thomas <markt@apache.org>
2017-03-24 15:42:39 +0000

    Close the streams as soon as we know the connection is closing
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.5.x/trunk@1788480 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http2/Http2UpgradeHandler.java
+++ java/org/apache/coyote/http2/Http2UpgradeHandler.java
@@ -983,6 +983,11 @@ public class Http2UpgradeHandler extends AbstractStream implements InternalHttpU
 
     private void close() {
         connectionState.set(ConnectionState.CLOSED);
+        for (Stream stream : streams.values()) {
+            // The connection is closing. Close the associated streams as no
+            // longer required.
+            stream.receiveReset(Http2Error.CANCEL.getCode());
+        }
         try {
             socketWrapper.close();
         } catch (IOException ioe) {
