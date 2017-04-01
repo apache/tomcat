603efb41f77f045105c69dcd2f8e18abaeae9944
Mark Thomas <markt@apache.org>
2017-04-01 20:42:05 +0000

    Reformat. No functional changed.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1789842 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/jasper/security/SecurityClassLoad.java
+++ java/org/apache/jasper/security/SecurityClassLoad.java
@@ -14,8 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-
 package org.apache.jasper.security;
 
 import org.apache.juli.logging.Log;
@@ -26,7 +24,6 @@ import org.apache.juli.logging.LogFactory;
  * Java SecurityManager so that the defineClassInPackage
  * RuntimePermission does not trigger an AccessControlException.
  */
-
 public final class SecurityClassLoad {
 
     private static final Log log = LogFactory.getLog(SecurityClassLoad.class);
@@ -40,65 +37,41 @@ public final class SecurityClassLoad {
         final String basePackage = "org.apache.jasper.";
         try {
             // Ensure XMLInputFactory is loaded with Tomcat's class loader
-            loader.loadClass( basePackage +
-                    "compiler.EncodingDetector");
+            loader.loadClass( basePackage + "compiler.EncodingDetector");
 
-            loader.loadClass( basePackage +
-                "runtime.JspFactoryImpl$PrivilegedGetPageContext");
-            loader.loadClass( basePackage +
-                "runtime.JspFactoryImpl$PrivilegedReleasePageContext");
+            loader.loadClass( basePackage + "runtime.JspFactoryImpl$PrivilegedGetPageContext");
+            loader.loadClass( basePackage + "runtime.JspFactoryImpl$PrivilegedReleasePageContext");
 
-            loader.loadClass( basePackage +
-                "runtime.JspRuntimeLibrary");
+            loader.loadClass( basePackage + "runtime.JspRuntimeLibrary");
 
-            loader.loadClass( basePackage +
-                "runtime.ServletResponseWrapperInclude");
-            loader.loadClass( basePackage +
-                "runtime.TagHandlerPool");
-            loader.loadClass( basePackage +
-                "runtime.JspFragmentHelper");
+            loader.loadClass( basePackage + "runtime.ServletResponseWrapperInclude");
+            loader.loadClass( basePackage + "runtime.TagHandlerPool");
+            loader.loadClass( basePackage + "runtime.JspFragmentHelper");
 
-            loader.loadClass( basePackage +
-                "runtime.ProtectedFunctionMapper");
+            loader.loadClass( basePackage + "runtime.ProtectedFunctionMapper");
 
-            loader.loadClass( basePackage +
-                "runtime.PageContextImpl");
-            loader.loadClass( basePackage +
-                "runtime.PageContextImpl$1");
-            loader.loadClass( basePackage +
-                "runtime.PageContextImpl$2");
-            loader.loadClass( basePackage +
-                "runtime.PageContextImpl$3");
-            loader.loadClass( basePackage +
-                "runtime.PageContextImpl$4");
-            loader.loadClass( basePackage +
-                "runtime.PageContextImpl$5");
-            loader.loadClass( basePackage +
-                "runtime.PageContextImpl$6");
-            loader.loadClass( basePackage +
-                "runtime.PageContextImpl$7");
-            loader.loadClass( basePackage +
-                "runtime.PageContextImpl$8");
-            loader.loadClass( basePackage +
-                "runtime.PageContextImpl$9");
-            loader.loadClass( basePackage +
-                "runtime.PageContextImpl$10");
-            loader.loadClass( basePackage +
-                "runtime.PageContextImpl$11");
-            loader.loadClass( basePackage +
-                "runtime.PageContextImpl$12");
+            loader.loadClass( basePackage + "runtime.PageContextImpl");
+            loader.loadClass( basePackage + "runtime.PageContextImpl$1");
+            loader.loadClass( basePackage + "runtime.PageContextImpl$2");
+            loader.loadClass( basePackage + "runtime.PageContextImpl$3");
+            loader.loadClass( basePackage + "runtime.PageContextImpl$4");
+            loader.loadClass( basePackage + "runtime.PageContextImpl$5");
+            loader.loadClass( basePackage + "runtime.PageContextImpl$6");
+            loader.loadClass( basePackage + "runtime.PageContextImpl$7");
+            loader.loadClass( basePackage + "runtime.PageContextImpl$8");
+            loader.loadClass( basePackage + "runtime.PageContextImpl$9");
+            loader.loadClass( basePackage + "runtime.PageContextImpl$10");
+            loader.loadClass( basePackage + "runtime.PageContextImpl$11");
+            loader.loadClass( basePackage + "runtime.PageContextImpl$12");
 
-            loader.loadClass( basePackage +
-                "runtime.JspContextWrapper");
+            loader.loadClass( basePackage + "runtime.JspContextWrapper");
 
             // Trigger loading of class and reading of property
             SecurityUtil.isPackageProtectionEnabled();
 
-            loader.loadClass( basePackage +
-                "servlet.JspServletWrapper");
+            loader.loadClass( basePackage + "servlet.JspServletWrapper");
 
-            loader.loadClass( basePackage +
-                "runtime.JspWriterImpl$1");
+            loader.loadClass( basePackage + "runtime.JspWriterImpl$1");
         } catch (ClassNotFoundException ex) {
             log.error("SecurityClassLoad", ex);
         }
