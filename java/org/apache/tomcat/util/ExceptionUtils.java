14bbceacabfa9c60551e3a9a28dcdd968a8a5b35
Mark Thomas <markt@apache.org>
2017-04-06 11:01:42 +0000

    Pre-load the ExceptionUtils class. Since the class is used extensively in error handling, it is prudent to pre-load it to avoid any failure to load this class masking the true problem during error handling.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1790376 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/ExceptionUtils.java
+++ java/org/apache/tomcat/util/ExceptionUtils.java
@@ -57,4 +57,15 @@ public class ExceptionUtils {
         }
         return t;
     }
+
+
+    /**
+     * NO-OP method provided to enable simple pre-loading of this class. Since
+     * the class is used extensively in error handling, it is prudent to
+     * pre-load it to avoid any failure to load this class masking the true
+     * problem during error handling.
+     */
+    public static void preload() {
+        // NO-OP
+    }
 }
