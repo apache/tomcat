7716ae60d58183d7e17d96431b5bfbfa16294d1f
simondi <simondi88@hotmail.com>
2017-04-09 13:56:35 -0700

    Commit the added comment on Application.java

--- java/javax/mail/Authenticator.java
+++ java/javax/mail/Authenticator.java
@@ -1,4 +1,5 @@
 /*
+ *  Modified by Simn Di on Apr 9.
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
