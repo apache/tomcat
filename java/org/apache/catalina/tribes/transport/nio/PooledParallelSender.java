c45f473f4fc887415759bd9ed62eb2b562a8d0dc
Keiichi Fujino <kfujino@apache.org>
2017-04-11 08:29:24 +0000

    Add MBean for Sender component.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1790939 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/tribes/transport/nio/PooledParallelSender.java
+++ java/org/apache/catalina/tribes/transport/nio/PooledParallelSender.java
@@ -26,7 +26,7 @@ import org.apache.catalina.tribes.transport.DataSender;
 import org.apache.catalina.tribes.transport.PooledSender;
 import org.apache.catalina.tribes.util.StringManager;
 
-public class PooledParallelSender extends PooledSender {
+public class PooledParallelSender extends PooledSender implements PooledParallelSenderMBean {
     protected static final StringManager sm = StringManager.getManager(PooledParallelSender.class);
 
     @Override
