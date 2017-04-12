8f6c4d212851e5b690bbbc290d4707076466fcf9
Mark Thomas <markt@apache.org>
2017-04-12 10:22:14 +0000

    Fix back-port
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.5.x/trunk@1791105 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/tomcat/util/net/openssl/ciphers/TesterOpenSSL.java
+++ test/org/apache/tomcat/util/net/openssl/ciphers/TesterOpenSSL.java
@@ -23,6 +23,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.apache.catalina.util.IOTools;
