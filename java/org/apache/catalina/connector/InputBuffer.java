adea5724a0d654c93bbe84841652685387e8a73b
Mark Thomas <markt@apache.org>
2017-05-04 14:03:09 +0000

    Remove unused deprecated code
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1793806 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/connector/InputBuffer.java
+++ java/org/apache/catalina/connector/InputBuffer.java
@@ -386,18 +386,6 @@ public class InputBuffer extends Reader
 
     // ------------------------------------------------- Chars Handling Methods
 
-
-    /**
-     * @param s     New encoding value
-     *
-     * @deprecated This method will be removed in Tomcat 9.0.x
-     */
-    @Deprecated
-    public void setEncoding(String s) {
-        enc = s;
-    }
-
-
     public int realReadChars() throws IOException {
         checkConverter();
 
