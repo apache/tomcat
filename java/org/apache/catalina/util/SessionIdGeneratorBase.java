47cbfa4f798b09f310384f03e7d65facf881df5f
Remy Maucherat <remm@apache.org>
2017-05-05 15:29:05 +0000

    61072: Respect what the doc says about using the platform default secure random.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1794066 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/util/SessionIdGeneratorBase.java
+++ java/org/apache/catalina/util/SessionIdGeneratorBase.java
@@ -198,6 +198,7 @@ public abstract class SessionIdGeneratorBase extends LifecycleBase
             }
         }
 
+        boolean error = false;
         if (result == null) {
             // No secureRandomClass or creation failed. Use SecureRandom.
             try {
@@ -210,15 +211,17 @@ public abstract class SessionIdGeneratorBase extends LifecycleBase
                     result = SecureRandom.getInstance(secureRandomAlgorithm);
                 }
             } catch (NoSuchAlgorithmException e) {
+                error = true;
                 log.error(sm.getString("sessionIdGeneratorBase.randomAlgorithm",
                         secureRandomAlgorithm), e);
             } catch (NoSuchProviderException e) {
+                error = true;
                 log.error(sm.getString("sessionIdGeneratorBase.randomProvider",
                         secureRandomProvider), e);
             }
         }
 
-        if (result == null) {
+        if (result == null && error) {
             // Invalid provider / algorithm
             try {
                 result = SecureRandom.getInstance("SHA1PRNG");
