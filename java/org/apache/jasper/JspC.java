4e5b1a8b355887589ed4a5f5264a5fc74238d600
Mark Thomas <markt@apache.org>
2017-05-10 19:57:33 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=53011 When pre-compiling with JspC, report all compilation errors rather than stopping after the first error. A new option -failFast can be used to restore the previous behaviour of stopping after the first error. Based on a patch provided by Marc Pompl.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1794752 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/jasper/JspC.java
+++ java/org/apache/jasper/JspC.java
@@ -114,6 +114,7 @@ public class JspC extends Task implements Options {
     protected static final String SWITCH_CLASS_NAME = "-c";
     protected static final String SWITCH_FULL_STOP = "--";
     protected static final String SWITCH_COMPILE = "-compile";
+    protected static final String SWITCH_FAIL_FAST = "-failFast";
     protected static final String SWITCH_SOURCE = "-source";
     protected static final String SWITCH_TARGET = "-target";
     protected static final String SWITCH_URI_BASE = "-uribase";
@@ -185,6 +186,7 @@ public class JspC extends Task implements Options {
     protected int dieLevel;
     protected boolean helpNeeded = false;
     protected boolean compile = false;
+    protected boolean failFast = false;
     protected boolean smapSuppressed = true;
     protected boolean smapDumped = false;
     protected boolean caching = true;
@@ -313,6 +315,8 @@ public class JspC extends Task implements Options {
                 targetPackage = nextArg();
             } else if (tok.equals(SWITCH_COMPILE)) {
                 compile=true;
+            } else if (tok.equals(SWITCH_FAIL_FAST)) {
+                failFast = true;
             } else if (tok.equals(SWITCH_CLASS_NAME)) {
                 targetClassName = nextArg();
             } else if (tok.equals(SWITCH_URI_BASE)) {
@@ -1279,14 +1283,7 @@ public class JspC extends Task implements Options {
                                                file),
                           rootCause);
             }
-
-            // Bugzilla 35114.
-            if(getFailOnError()) {
-                throw je;
-            } else {
-                log.error(je.getMessage());
-            }
-
+            throw je;
         } catch (Exception e) {
             if ((e instanceof FileNotFoundException) && log.isWarnEnabled()) {
                 log.warn(Localizer.getMessage("jspc.error.fileDoesNotExist",
@@ -1389,6 +1386,9 @@ public class JspC extends Task implements Options {
 
             initWebXml();
 
+            int errorCount = 0;
+            long start = System.currentTimeMillis();
+
             for (String nextjsp : pages) {
                 File fjsp = new File(nextjsp);
                 if (!fjsp.isAbsolute()) {
@@ -1409,7 +1409,24 @@ public class JspC extends Task implements Options {
                 if (nextjsp.startsWith("." + File.separatorChar)) {
                     nextjsp = nextjsp.substring(2);
                 }
-                processFile(nextjsp);
+                try {
+                    processFile(nextjsp);
+                } catch (JasperException e) {
+                    if (failFast) {
+                        throw e;
+                    }
+                    errorCount++;
+                    log.error(nextjsp + ":" + e.getMessage());
+                }
+            }
+
+            long time = System.currentTimeMillis() - start;
+            String msg = Localizer.getMessage("jspc.compilation.result",
+                    Integer.toString(errorCount), Long.toString(time));
+            if (failOnError && errorCount > 0) {
+                throw new BuildException(msg);
+            } else {
+                log.info(msg);
             }
 
             completeWebXml();
@@ -1422,15 +1439,9 @@ public class JspC extends Task implements Options {
             throw new BuildException(ioe);
 
         } catch (JasperException je) {
-            Throwable rootCause = je;
-            while (rootCause instanceof JasperException
-                    && ((JasperException) rootCause).getRootCause() != null) {
-                rootCause = ((JasperException) rootCause).getRootCause();
+            if (failOnError) {
+                throw new BuildException(je);
             }
-            if (rootCause != je) {
-                rootCause.printStackTrace();
-            }
-            throw new BuildException(je);
         } finally {
             if (loader != null) {
                 LogFactory.release(loader);
