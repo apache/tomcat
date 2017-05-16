db5d6cee02935fe4738474d3ca43af86af2f5110
Mark Thomas <markt@apache.org>
2017-05-16 10:57:30 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=61086 Explicitly signal am empty request body for HTTP 205 responses.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1795297 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http11/AbstractHttp11Processor.java
+++ java/org/apache/coyote/http11/AbstractHttp11Processor.java
@@ -1479,6 +1479,11 @@ public abstract class AbstractHttp11Processor<S> extends AbstractProcessor<S> {
                 (outputFilters[Constants.VOID_FILTER]);
             entityBody = false;
             contentDelimitation = true;
+            if (statusCode == 205) {
+                // RFC 7231 requires the server to explicitly signal an empty
+                // response in this case
+                response.setContentLength(0);
+            }
         }
 
         MessageBytes methodMB = request.method();
