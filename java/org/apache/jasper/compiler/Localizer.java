2681825c4ae59c7eda46ae30c77d626868d7f470
Mark Thomas <markt@apache.org>
2017-05-30 08:27:23 +0000

    Refactor - simplify
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1796787 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/jasper/compiler/Localizer.java
+++ java/org/apache/jasper/compiler/Localizer.java
@@ -148,14 +148,11 @@ public class Localizer {
      * @return Localized error message
      */
     public static String getMessage(String errCode, Object[] args) {
-        String errMsg = errCode;
-        try {
-            errMsg = bundle.getString(errCode);
-            if (args != null && args.length > 0) {
-                MessageFormat formatter = new MessageFormat(errMsg);
-                errMsg = formatter.format(args);
-            }
-        } catch (MissingResourceException e) {
+        String errMsg = getMessage(errCode);
+
+        if (args != null && args.length > 0) {
+            MessageFormat formatter = new MessageFormat(errMsg);
+            errMsg = formatter.format(args);
         }
 
         return errMsg;
