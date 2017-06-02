fd16c2805163588c04482b77d104276aee016ce1
Mark Thomas <markt@apache.org>
2017-06-02 09:53:41 +0000

    Correct import order
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1797361 13f79535-47bb-0310-9956-ffa450edef68

--- java/javax/annotation/Generated.java
+++ java/javax/annotation/Generated.java
@@ -16,16 +16,16 @@
  */
 package javax.annotation;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.lang.annotation.Documented;
 
 /**
  * @since Common Annotations 1.0
  */
-@Documented 
+@Documented
 @Target({ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR,
     ElementType.FIELD, ElementType.LOCAL_VARIABLE, ElementType.METHOD,
     ElementType.PACKAGE, ElementType.PARAMETER, ElementType.TYPE})
--- java/javax/annotation/Resources.java
+++ java/javax/annotation/Resources.java
@@ -16,11 +16,11 @@
  */
 package javax.annotation;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.lang.annotation.Documented;
 
 /**
  * @since Common Annotations 1.0
