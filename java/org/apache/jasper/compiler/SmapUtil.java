90e9b18214360dbb359cc778f04ad8a82d9e52ca
Mark Thomas <markt@apache.org>
2017-06-03 19:38:21 +0000

    Remove commented out code. Tomcat doesn't support something to JSP.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1797527 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/jasper/compiler/SmapUtil.java
+++ java/org/apache/jasper/compiler/SmapUtil.java
@@ -79,22 +79,6 @@ public class SmapUtil {
         // set up our SMAP generator
         SmapGenerator g = new SmapGenerator();
 
-        /** Disable reading of input SMAP because:
-            1. There is a bug here: getRealPath() is null if .jsp is in a jar
-               Bugzilla 14660.
-            2. Mappings from other sources into .jsp files are not supported.
-            TODO: fix 1. if 2. is not true.
-        // determine if we have an input SMAP
-        String smapPath = inputSmapPath(ctxt.getRealPath(ctxt.getJspFile()));
-            File inputSmap = new File(smapPath);
-            if (inputSmap.exists()) {
-                byte[] embeddedSmap = null;
-            byte[] subSmap = SDEInstaller.readWhole(inputSmap);
-            String subSmapString = new String(subSmap, SMAP_ENCODING);
-            g.addSmap(subSmapString, "JSP");
-        }
-        **/
-
         // now, assemble info about our own stratum (JSP) using JspLineMap
         SmapStratum s = new SmapStratum("JSP");
 
