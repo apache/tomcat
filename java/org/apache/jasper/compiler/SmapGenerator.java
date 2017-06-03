a4a22af1655e0a3db927ffde8bbcfce6bc1f5da9
Mark Thomas <markt@apache.org>
2017-06-03 20:07:09 +0000

    Move test code to Unit test Remove testing of embedded SMAPs since only the test code uses this (so I want to remove it in 9.0.x)
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1797531 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/jasper/compiler/SmapGenerator.java
+++ java/org/apache/jasper/compiler/SmapGenerator.java
@@ -130,32 +130,4 @@ public class SmapGenerator {
 
     @Override
     public String toString() { return getString(); }
-
-    //*********************************************************************
-    // For testing (and as an example of use)...
-
-    public static void main(String args[]) {
-        SmapGenerator g = new SmapGenerator();
-        g.setOutputFileName("foo.java");
-        SmapStratum s = new SmapStratum("JSP");
-        s.addFile("foo.jsp");
-        s.addFile("bar.jsp", "/foo/foo/bar.jsp");
-        s.addLineData(1, "foo.jsp", 1, 1, 1);
-        s.addLineData(2, "foo.jsp", 1, 6, 1);
-        s.addLineData(3, "foo.jsp", 2, 10, 5);
-        s.addLineData(20, "bar.jsp", 1, 30, 1);
-        g.addStratum(s, true);
-        System.out.print(g);
-
-        System.out.println("---");
-
-        SmapGenerator embedded = new SmapGenerator();
-        embedded.setOutputFileName("blargh.tier2");
-        s = new SmapStratum("Tier2");
-        s.addFile("1.tier2");
-        s.addLineData(1, "1.tier2", 1, 1, 1);
-        embedded.addStratum(s, true);
-        g.addSmap(embedded.toString(), "JSP");
-        System.out.println(g);
-    }
 }
---
+++ test/org/apache/jasper/compiler/TestSmapGenerator.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jasper.compiler;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSmapGenerator {
+
+    @Test
+    public void test01() {
+        // Formerly part of the main() method in SmapGenerator
+
+        SmapGenerator g = new SmapGenerator();
+        g.setOutputFileName("foo.java");
+        SmapStratum s = new SmapStratum("JSP");
+        s.addFile("foo.jsp");
+        s.addFile("bar.jsp", "/foo/foo/bar.jsp");
+        s.addLineData(1, "foo.jsp", 1, 1, 1);
+        s.addLineData(2, "foo.jsp", 1, 6, 1);
+        s.addLineData(3, "foo.jsp", 2, 10, 5);
+        s.addLineData(20, "/foo/foo/bar.jsp", 1, 30, 1);
+        g.addStratum(s, true);
+
+        Assert.assertEquals(
+                "SMAP\n" +
+                "foo.java\n" +
+                "JSP\n" +
+                "*S JSP\n" +
+                "*F\n" +
+                "+ 0 foo.jsp\n" +
+                "foo.jsp\n" +
+                "+ 1 bar.jsp\n" +
+                "foo/foo/bar.jsp\n" +
+                "*L\n" +
+                "1:1\n" +
+                "2:6\n" +
+                "3,2:10,5\n" +
+                "20#1:30\n" +
+                "*E\n",
+                g.getString());
+    }
+}
