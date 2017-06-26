4ce1f2094d75af63c82d1f4c7d8b742795ef0db7
Mark Thomas <markt@apache.org>
2017-06-26 20:39:16 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=61224 Make the GlobalRequestProcessor MBean attributes read-only. Patch provided by Alexis Hassler. This closes #68
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1799986 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/RequestGroupInfo.java
+++ java/org/apache/coyote/RequestGroupInfo.java
@@ -19,11 +19,13 @@ package org.apache.coyote;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.tomcat.util.modeler.BaseModelMBean;
+
 /** This can be moved to top level ( eventually with a better name ).
  *  It is currently used only as a JMX artifact, to aggregate the data
  *  collected from each RequestProcessor thread.
  */
-public class RequestGroupInfo {
+public class RequestGroupInfo extends BaseModelMBean {
     private final List<RequestInfo> processors = new ArrayList<>();
     private long deadMaxTime = 0;
     private long deadProcessingTime = 0;
