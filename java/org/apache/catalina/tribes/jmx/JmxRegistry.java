b7deb05027496bed0b37c084cb53e5b2b75652f2
Mark Thomas <markt@apache.org>
2017-07-06 08:42:56 +0000

    Fix a FindBugs nag and add an explanatory comment.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.5.x/trunk@1800993 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/tribes/jmx/JmxRegistry.java
+++ java/org/apache/catalina/tribes/jmx/JmxRegistry.java
@@ -60,7 +60,10 @@ public class JmxRegistry {
         // create registry
         registry = new JmxRegistry();
         registry.baseOname = baseOn;
-        registryCache.putIfAbsent(channel.getName(), registry);
+        // It doesn't matter if existing object gets over-written. This object
+        // holds minimal state and that state will be the same for all objects
+        // created for the same channel.
+        registryCache.put(channel.getName(), registry);
         return registry;
     }
 
