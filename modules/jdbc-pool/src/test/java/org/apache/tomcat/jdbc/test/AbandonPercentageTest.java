9ffcee48f378220b0c775d2df3f6c23e3703f95d
Mark Thomas <markt@apache.org>
2017-07-30 18:54:57 +0000

    Fix some scary but low confidence Findbug warnings (and other warnings in those files)
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1803455 13f79535-47bb-0310-9956-ffa450edef68

--- modules/jdbc-pool/src/test/java/org/apache/tomcat/jdbc/test/AbandonPercentageTest.java
+++ modules/jdbc-pool/src/test/java/org/apache/tomcat/jdbc/test/AbandonPercentageTest.java
@@ -34,11 +34,17 @@ public class AbandonPercentageTest extends DefaultTestCase {
         this.datasource.getPoolProperties().setTimeBetweenEvictionRunsMillis(100);
         this.datasource.getPoolProperties().setRemoveAbandoned(true);
         this.datasource.getPoolProperties().setRemoveAbandonedTimeout(1);
-        Connection con = datasource.getConnection();
-        Assert.assertEquals("Number of connections active/busy should be 1",1,datasource.getPool().getActive());
-        Thread.sleep(2000);
-        Assert.assertEquals("Number of connections active/busy should be 0",0,datasource.getPool().getActive());
-        con.close();
+        Connection con = null;
+        try {
+            con = datasource.getConnection();
+            Assert.assertEquals("Number of connections active/busy should be 1",1,datasource.getPool().getActive());
+            Thread.sleep(2000);
+            Assert.assertEquals("Number of connections active/busy should be 0",0,datasource.getPool().getActive());
+        } finally {
+            if (con != null) {
+                con.close();
+            }
+        }
     }
 
     @Test
@@ -51,11 +57,17 @@ public class AbandonPercentageTest extends DefaultTestCase {
         this.datasource.getPoolProperties().setTimeBetweenEvictionRunsMillis(100);
         this.datasource.getPoolProperties().setRemoveAbandoned(true);
         this.datasource.getPoolProperties().setRemoveAbandonedTimeout(1);
-        Connection con = datasource.getConnection();
-        Assert.assertEquals("Number of connections active/busy should be 1",1,datasource.getPool().getActive());
-        Thread.sleep(2000);
-        Assert.assertEquals("Number of connections active/busy should be 1",1,datasource.getPool().getActive());
-        con.close();
+        Connection con = null;
+        try {
+            con = datasource.getConnection();
+            Assert.assertEquals("Number of connections active/busy should be 1",1,datasource.getPool().getActive());
+            Thread.sleep(2000);
+            Assert.assertEquals("Number of connections active/busy should be 1",1,datasource.getPool().getActive());
+        } finally {
+            if (con != null) {
+                con.close();
+            }
+        }
     }
 
     @Test
@@ -69,14 +81,22 @@ public class AbandonPercentageTest extends DefaultTestCase {
         this.datasource.getPoolProperties().setRemoveAbandoned(true);
         this.datasource.getPoolProperties().setRemoveAbandonedTimeout(1);
         this.datasource.getPoolProperties().setJdbcInterceptors(ResetAbandonedTimer.class.getName());
-        Connection con = datasource.getConnection();
-        Assert.assertEquals("Number of connections active/busy should be 1",1,datasource.getPool().getActive());
-        for (int i=0; i<20; i++) {
-            Thread.sleep(200);
-            con.isClosed();
+        Connection con = null;
+        try {
+            con = datasource.getConnection();
+            Assert.assertEquals("Number of connections active/busy should be 1",1,datasource.getPool().getActive());
+            for (int i=0; i<20; i++) {
+                Thread.sleep(200);
+                // This call is here to ensure the pool thinks the connection
+                // is being used.
+                con.isClosed();
+            }
+            Assert.assertEquals("Number of connections active/busy should be 1",1,datasource.getPool().getActive());
+        } finally {
+            if (con != null) {
+                con.close();
+            }
         }
-        Assert.assertEquals("Number of connections active/busy should be 1",1,datasource.getPool().getActive());
-        con.close();
     }
 
     @Test
