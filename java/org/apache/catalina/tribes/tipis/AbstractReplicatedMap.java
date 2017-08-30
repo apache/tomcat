cce2158fe0e4370a257e8ff4a41ffc5527cc9f63
Keiichi Fujino <kfujino@apache.org>
2017-08-30 07:43:35 +0000

    Correct the backup node selection logic that node 0 is returned twice consecutively.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.5.x/trunk@1806671 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/tribes/tipis/AbstractReplicatedMap.java
+++ java/org/apache/catalina/tribes/tipis/AbstractReplicatedMap.java
@@ -923,7 +923,7 @@ public abstract class AbstractReplicatedMap<K,V>
         int node = currentNode++;
         if (node >= size) {
             node = 0;
-            currentNode = 0;
+            currentNode = 1;
         }
         return node;
     }
