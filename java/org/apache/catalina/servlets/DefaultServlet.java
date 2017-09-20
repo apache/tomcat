b577f9a7996b92b650b1649af3c3bae11c120db9
Mark Thomas <markt@apache.org>
2017-09-20 12:23:44 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=61542 Partial fix for CVE-2017-12617 This moves a check from the Default servlet where it applied to GET, POST, HEAD and OPTIONS to the resources implementation where it applies to any method that expects the resource to exist (e.g.DELETE) Still need to address the case where the resource does not exist (e.g. PUT)
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1809011 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/servlets/DefaultServlet.java
+++ java/org/apache/catalina/servlets/DefaultServlet.java
@@ -820,20 +820,6 @@ public class DefaultServlet extends HttpServlet {
             return;
         }
 
-        // If the resource is not a collection, and the resource path
-        // ends with "/" or "\", return NOT FOUND
-        if (resource.isFile() && (path.endsWith("/") || path.endsWith("\\"))) {
-            // Check if we're included so we can return the appropriate
-            // missing resource name in the error
-            String requestUri = (String) request.getAttribute(
-                    RequestDispatcher.INCLUDE_REQUEST_URI);
-            if (requestUri == null) {
-                requestUri = request.getRequestURI();
-            }
-            response.sendError(HttpServletResponse.SC_NOT_FOUND, requestUri);
-            return;
-        }
-
         boolean included = false;
         // Check if the conditions specified in the optional If headers are
         // satisfied.
--- java/org/apache/catalina/webresources/AbstractFileResourceSet.java
+++ java/org/apache/catalina/webresources/AbstractFileResourceSet.java
@@ -57,6 +57,14 @@ public abstract class AbstractFileResourceSet extends AbstractResourceSet {
             name = "";
         }
         File file = new File(fileBase, name);
+
+        // If the requested names ends in '/', the Java File API will return a
+        // matching file if one exists. This isn't what we want as it is not
+        // consistent with the Servlet spec rules for request mapping.
+        if (file.isFile() && name.endsWith("/")) {
+            return null;
+        }
+
         if (!mustExist || file.canRead()) {
 
             if (getRoot().getAllowLinking()) {
