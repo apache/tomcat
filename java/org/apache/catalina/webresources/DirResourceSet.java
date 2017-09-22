4cf7dab88282c8f3c92f0b961cdb0096e1d63e88
Mark Thomas <markt@apache.org>
2017-09-22 07:54:31 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=61542 Remainder of fix for CVE-2017-12617 This ensures that a path specified for creation of a file does not end in '/' since that is dropped by the File API.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.5.x/trunk@1809274 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/webresources/DirResourceSet.java
+++ java/org/apache/catalina/webresources/DirResourceSet.java
@@ -217,6 +217,12 @@ public class DirResourceSet extends AbstractFileResourceSet {
             return false;
         }
 
+        // write() is meant to create a file so ensure that the path doesn't
+        // end in '/'
+        if (path.endsWith("/")) {
+            return false;
+        }
+
         File dest = null;
         String webAppMount = getWebAppMount();
         if (path.startsWith(webAppMount)) {
--- test/org/apache/catalina/webresources/AbstractTestResourceSet.java
+++ test/org/apache/catalina/webresources/AbstractTestResourceSet.java
@@ -447,14 +447,8 @@ public abstract class AbstractTestResourceSet {
     public final void testWriteDirB() {
         WebResource d1 = resourceRoot.getResource(getMount() + "/d1/");
         InputStream is = new ByteArrayInputStream("test".getBytes());
-        if (d1.exists()) {
+        if (d1.exists() || d1.isVirtual()) {
             Assert.assertFalse(resourceRoot.write(getMount() + "/d1/", is, false));
-        } else if (d1.isVirtual()) {
-            Assert.assertTrue(resourceRoot.write(
-                    getMount() + "/d1/", is, false));
-            File file = new File(getBaseDir(), "d1");
-            Assert.assertTrue(file.exists());
-            Assert.assertTrue(file.delete());
         } else {
             Assert.fail("Unhandled condition in unit test");
         }
@@ -488,6 +482,14 @@ public abstract class AbstractTestResourceSet {
             Assert.assertFalse(resourceRoot.write(
                     getMount() + "/" + newFileName, is, false));
         }
+    }
+
+    @Test
+    public final void testWriteWithTrailingSlash() {
+        String newFileName = getNewFileName() + "/";
+        InputStream is = new ByteArrayInputStream("test".getBytes());
+        Assert.assertFalse(resourceRoot.write(
+                getMount() + "/" + newFileName, is, false));
     }
 
     protected abstract String getNewFileName();
