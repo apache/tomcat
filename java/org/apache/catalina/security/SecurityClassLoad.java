8c48df0ccc484ba369635fde601523cb47a9cb44
Mark Thomas <markt@apache.org>
2017-10-06 13:11:12 +0000

    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1811327 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/security/SecurityClassLoad.java
+++ java/org/apache/catalina/security/SecurityClassLoad.java
@@ -107,6 +107,7 @@ public final class SecurityClassLoad {
         final String basePackage = "org.apache.catalina.util.";
         loader.loadClass(basePackage + "ParameterMap");
         loader.loadClass(basePackage + "RequestUtil");
+        loader.loadClass(basePackage + "TLSUtil");
     }
 
 
