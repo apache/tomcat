73380ed6968a91a1d983d0050349dcaf51870ad8
Mark Thomas <markt@apache.org>
2017-11-11 20:58:15 +0000

    Fix back-port. No default methods so need init() and destroy()
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.5.x/trunk@1814975 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/filters/SessionInitializerFilter.java
+++ java/org/apache/catalina/filters/SessionInitializerFilter.java
@@ -20,6 +20,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -58,4 +59,14 @@ public class SessionInitializerFilter implements Filter {
         chain.doFilter(request, response);
     }
 
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // NO-OP
+    }
+
+    @Override
+    public void destroy() {
+        // NO-OP
+    }
 }
