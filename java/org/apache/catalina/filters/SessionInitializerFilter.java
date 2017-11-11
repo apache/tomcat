8266f738dd25011429b8c70849e81d5ba216652d
Mark Thomas <markt@apache.org>
2017-11-11 21:01:26 +0000

    Provide the SessionInitializerFilter that can be used to ensure that an HTTP session exists when initiating a WebSocket connection. Patch provided by isapir.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1814977 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/org/apache/catalina/filters/SessionInitializerFilter.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.catalina.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * A {@link javax.servlet.Filter} that initializes the {@link
+ * javax.servlet.http.HttpSession} for the {@link HttpServletRequest} by calling
+ * its getSession() method.
+ * <p>
+ * This is required for some operations with WebSocket requests, where it is
+ * too late to initialize the HttpSession object, and the current Java WebSocket
+ * specification does not provide a way to do so.
+ */
+public class SessionInitializerFilter implements Filter {
+
+    /**
+     * Calls {@link HttpServletRequest}'s getSession() to initialize the
+     * HttpSession and continues processing the chain.
+     *
+     * @param request  The request to process
+     * @param response The response associated with the request
+     * @param chain    Provides access to the next filter in the chain for this
+     *                 filter to pass the request and response to for further
+     *                 processing
+     * @throws IOException      if an I/O error occurs during this filter's
+     *                          processing of the request
+     * @throws ServletException if the processing fails for any other reason
+     */
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        ((HttpServletRequest)request).getSession();
+
+        chain.doFilter(request, response);
+    }
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // NO-OP
+    }
+
+    @Override
+    public void destroy() {
+        // NO-OP
+    }
+}
