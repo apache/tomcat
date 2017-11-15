5ccbc59f1823af22338f0efd9da32abe6f1919df
Mark Thomas <markt@apache.org>
2017-11-15 15:40:50 +0000

    Correct failure message
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1815318 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/juli/TestFileHandler.java
+++ test/org/apache/juli/TestFileHandler.java
@@ -67,7 +67,7 @@ public class TestFileHandler {
         File[] files = logsDir.listFiles();
         if (files != null) {
             for (File file : files) {
-                Assert.assertTrue("Failed to create [" + file + "]", file.delete());
+                Assert.assertTrue("Failed to delete [" + file + "]", file.delete());
             }
             Assert.assertTrue("Failed to create [" + logsDir + "]", logsDir.delete());
         }
