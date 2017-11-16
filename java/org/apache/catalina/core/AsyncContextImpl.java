860a0e6dde5639dba70d53fa706b8dc65340c902
Mark Thomas <markt@apache.org>
2017-11-16 20:01:12 +0000

    Avoid a possible NullPointerException when timing out AsyncContext instances during shut down.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1815505 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/core/AsyncContextImpl.java
+++ java/org/apache/catalina/core/AsyncContextImpl.java
@@ -123,6 +123,8 @@ public class AsyncContextImpl implements AsyncContext, AsyncContextCallback {
     public boolean timeout() {
         AtomicBoolean result = new AtomicBoolean();
         request.getCoyoteRequest().action(ActionCode.ASYNC_TIMEOUT, result);
+        // Avoids NPEs during shutdown. A call to recycle will null this field.
+        Context context = this.context;
 
         if (result.get()) {
             ClassLoader oldCL = context.bind(false, null);
