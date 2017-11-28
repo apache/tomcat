bc36a76c431a021420729a8ee88e6db72a456dd9
Mark Thomas <markt@apache.org>
2017-11-28 14:22:22 +0000

    Add compression config to HTTP/2 protocol
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1816550 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http2/Http2Protocol.java
+++ java/org/apache/coyote/http2/Http2Protocol.java
@@ -25,10 +25,13 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.regex.Pattern;
 
 import org.apache.coyote.Adapter;
+import org.apache.coyote.CompressionConfig;
 import org.apache.coyote.Processor;
 import org.apache.coyote.Request;
+import org.apache.coyote.Response;
 import org.apache.coyote.UpgradeProtocol;
 import org.apache.coyote.UpgradeToken;
 import org.apache.coyote.http11.upgrade.InternalHttpUpgradeHandler;
@@ -71,6 +74,8 @@ public class Http2Protocol implements UpgradeProtocol {
     private int maxTrailerSize = Constants.DEFAULT_MAX_TRAILER_SIZE;
     private boolean initiatePingDisabled = false;
     private boolean useSendfile = true;
+    // Compression
+    private final CompressionConfig compressionConfig = new CompressionConfig();
 
     @Override
     public String getHttpUpgradeName(boolean isSSLEnabled) {
@@ -281,5 +286,51 @@ public class Http2Protocol implements UpgradeProtocol {
 
     public boolean getInitiatePingDisabled() {
         return initiatePingDisabled;
+    }
+
+
+    public void setCompression(String compression) {
+        compressionConfig.setCompression(compression);
+    }
+    public String getCompression() {
+        return compressionConfig.getCompression();
+    }
+    protected int getCompressionLevel() {
+        return compressionConfig.getCompressionLevel();
+    }
+
+
+    public String getNoCompressionUserAgents() {
+        return compressionConfig.getNoCompressionUserAgents();
+    }
+    protected Pattern getNoCompressionUserAgentsPattern() {
+        return compressionConfig.getNoCompressionUserAgentsPattern();
+    }
+    public void setNoCompressionUserAgents(String noCompressionUserAgents) {
+        compressionConfig.setNoCompressionUserAgents(noCompressionUserAgents);
+    }
+
+
+    public String getCompressibleMimeType() {
+        return compressionConfig.getCompressibleMimeType();
+    }
+    public void setCompressibleMimeType(String valueS) {
+        compressionConfig.setCompressibleMimeType(valueS);
+    }
+    public String[] getCompressibleMimeTypes() {
+        return compressionConfig.getCompressibleMimeTypes();
+    }
+
+
+    public int getCompressionMinSize() {
+        return compressionConfig.getCompressionMinSize();
+    }
+    public void setCompressionMinSize(int compressionMinSize) {
+        compressionConfig.setCompressionMinSize(compressionMinSize);
+    }
+
+
+    public boolean useCompression(Request request, Response response) {
+        return compressionConfig.useCompression(request, response);
     }
 }
