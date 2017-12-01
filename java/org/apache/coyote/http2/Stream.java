eb3d4dd8acf772dfd78f3cac5403c06c8b220132
Mark Thomas <markt@apache.org>
2017-12-01 10:26:39 +0000

    Refactor header preparation with adding compression support in mind.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.5.x/trunk@1816822 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http2/Stream.java
+++ java/org/apache/coyote/http2/Stream.java
@@ -35,7 +35,6 @@ import org.apache.coyote.http2.HpackDecoder.HeaderEmitter;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
 import org.apache.tomcat.util.buf.ByteChunk;
-import org.apache.tomcat.util.http.FastHttpDateFormat;
 import org.apache.tomcat.util.http.MimeHeaders;
 import org.apache.tomcat.util.net.ApplicationBufferHandler;
 import org.apache.tomcat.util.res.StringManager;
@@ -55,7 +54,7 @@ public class Stream extends AbstractStream implements HeaderEmitter {
     static {
         Response response =  new Response();
         response.setStatus(100);
-        prepareHeaders(response);
+        StreamProcessor.prepareHeaders(response);
         ACK_HEADERS = response.getMimeHeaders();
     }
 
@@ -411,7 +410,6 @@ public class Stream extends AbstractStream implements HeaderEmitter {
 
 
     void writeHeaders() throws IOException {
-        prepareHeaders(coyoteResponse);
         boolean endOfStream = getOutputBuffer().hasNoBody();
         // TODO: Is 1k the optimal value?
         handler.writeHeaders(this, 0, coyoteResponse.getMimeHeaders(), endOfStream, 1024);
@@ -623,33 +621,6 @@ public class Stream extends AbstractStream implements HeaderEmitter {
 
         } else {
             handler.push(request, stream);
-        }
-    }
-
-
-    private static void prepareHeaders(Response coyoteResponse) {
-        MimeHeaders headers = coyoteResponse.getMimeHeaders();
-        int statusCode = coyoteResponse.getStatus();
-
-        // Add the pseudo header for status
-        headers.addValue(":status").setString(Integer.toString(statusCode));
-
-        // Check to see if a response body is present
-        if (!(statusCode < 200 || statusCode == 205 || statusCode == 304)) {
-            String contentType = coyoteResponse.getContentType();
-            if (contentType != null) {
-                headers.setValue("content-type").setString(contentType);
-            }
-            String contentLanguage = coyoteResponse.getContentLanguage();
-            if (contentLanguage != null) {
-                headers.setValue("content-language").setString(contentLanguage);
-            }
-        }
-
-        // Add date header unless it is an informational response or the
-        // application has already set one
-        if (statusCode >= 200 && headers.getValue("date") == null) {
-            headers.addValue("date").setString(FastHttpDateFormat.getCurrentDate());
         }
     }
 
