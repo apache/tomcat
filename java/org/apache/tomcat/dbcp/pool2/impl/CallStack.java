3e8c569ac18391d80e7e8ba029831701af5a4fb9
Mark Thomas <markt@apache.org>
2017-12-04 16:55:22 +0000

    Fix Javadoc errors reported by CI
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1817104 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/dbcp/pool2/impl/CallStack.java
+++ java/org/apache/tomcat/dbcp/pool2/impl/CallStack.java
@@ -20,7 +20,8 @@ import java.io.PrintWriter;
 
 /**
  * Strategy for obtaining and printing the current call stack. This is primarily useful for
- * {@linkplain UsageTracking usage tracking} so that different JVMs and configurations can use more efficient strategies
+ * {@link org.apache.tomcat.dbcp.pool2.UsageTracking usage tracking} so
+ * that different JVMs and configurations can use more efficient strategies
  * for obtaining the current call stack depending on metadata needs.
  *
  * @see CallStackUtils
@@ -30,7 +31,7 @@ public interface CallStack {
 
     /**
      * Prints the current stack trace if available to a PrintWriter. The format is undefined and is primarily useful
-     * for debugging issues with {@link PooledObject} usage in user code.
+     * for debugging issues with {@link org.apache.tomcat.dbcp.pool2.PooledObject} usage in user code.
      *
      * @param writer a PrintWriter to write the current stack trace to if available
      * @return true if a stack trace was available to print or false if nothing was printed
