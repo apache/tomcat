3f8317a5c3ba624072250cbddfff9a5f6ccc1d3e
Rainer Jung <rjung@apache.org>
2018-01-04 20:39:44 +0000

    Happy New Year 2018
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1820206 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/manager/HTMLManagerServlet.java
+++ java/org/apache/catalina/manager/HTMLManagerServlet.java
@@ -786,7 +786,7 @@ public final class HTMLManagerServlet extends ManagerServlet {
      */
     @Override
     public String getServletInfo() {
-        return "HTMLManagerServlet, Copyright (c) 1999-2017, The Apache Software Foundation";
+        return "HTMLManagerServlet, Copyright (c) 1999-2018, The Apache Software Foundation";
     }
 
     /**
