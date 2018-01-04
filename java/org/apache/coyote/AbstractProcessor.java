3dd871c5d43c9c5313f5cbc45b74e6c5636579a3
Mark Thomas <markt@apache.org>
2018-01-04 20:23:26 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=61886 Log errors on non-container threads at DEBUG rather than INFO. The exception will be made available to the application via the asynchronous error handling mechanism. Ensure that multiple threads do not attempt to complete the AsyncContext if an I/O error occurs in the stock ticker example Servlet.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1820201 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/AbstractProcessor.java
+++ java/org/apache/coyote/AbstractProcessor.java
@@ -92,7 +92,9 @@ public abstract class AbstractProcessor<S> implements ActionHook, Processor<S> {
             // clean-up. Need to do it this way to ensure that all the necessary
             // clean-up is performed.
             asyncStateMachine.asyncMustError();
-            getLog().info(sm.getString("abstractProcessor.nonContainerThreadError"), t);
+            if (getLog().isDebugEnabled()) {
+                getLog().debug(sm.getString("abstractProcessor.nonContainerThreadError"), t);
+            }
             getEndpoint().processSocketAsync(socketWrapper, SocketStatus.ERROR);
         }
     }
