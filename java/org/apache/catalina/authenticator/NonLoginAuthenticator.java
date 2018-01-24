d6945a8dd9916e68dcb2b92d055af8aae1bd3c8d
Mark Thomas <markt@apache.org>
2018-01-24 16:32:31 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=62036 When caching an authenticated user Principal in the session when the web application is configured with the NonLoginAuthenticator, cache the internal Principal object rather than the user facing Principal object as Tomcat requires the internal object to correctly process later authorization checks.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1822116 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/authenticator/NonLoginAuthenticator.java
+++ java/org/apache/catalina/authenticator/NonLoginAuthenticator.java
@@ -80,10 +80,10 @@ public final class NonLoginAuthenticator extends AuthenticatorBase {
         // Don't try and use SSO to authenticate since there is no auth
         // configured for this web application
         if (checkForCachedAuthentication(request, response, true)) {
-            // save the inherited Principal in this session so it can remain
-            // authenticated until it expires
+            // Save the inherited Principal in this session so it can remain
+            // authenticated until it expires.
             if (cache) {
-                request.getSessionInternal(true).setPrincipal(request.getUserPrincipal());
+                request.getSessionInternal(true).setPrincipal(request.getPrincipal());
             }
             return true;
         }
