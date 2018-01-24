41c19cbde385643de99943015556b1366b466c4c
Remy Maucherat <remm@apache.org>
2018-01-24 15:13:11 +0000

    Avoid duplicate load calls (which can happen in some cases like a malformed server.xml).
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1822109 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/startup/Catalina.java
+++ java/org/apache/catalina/startup/Catalina.java
@@ -122,6 +122,12 @@ public class Catalina {
     protected boolean useNaming = true;
 
 
+    /**
+     * Prevent duplicate loads.
+     */
+    protected boolean loaded = false;
+
+
     // ----------------------------------------------------------- Constructors
 
     public Catalina() {
@@ -517,6 +523,11 @@ public class Catalina {
      * Start a new server instance.
      */
     public void load() {
+
+        if (loaded) {
+            return;
+        }
+        loaded = true;
 
         long t1 = System.nanoTime();
 
