f984ba89453d48e677c9c15bda6727438cafd39b
Mark Thomas <markt@apache.org>
2018-02-02 11:06:31 +0000

    When using Tomcat embedded, only perform Authenticator configuration once during web application start.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1822945 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/startup/Tomcat.java
+++ java/org/apache/catalina/startup/Tomcat.java
@@ -966,18 +966,17 @@ public class Tomcat {
                 Context context = (Context) event.getLifecycle();
                 if (event.getType().equals(Lifecycle.CONFIGURE_START_EVENT)) {
                     context.setConfigured(true);
-                }
-                // LoginConfig is required to process @ServletSecurity
-                // annotations
-                if (context.getLoginConfig() == null) {
-                    context.setLoginConfig(
-                            new LoginConfig("NONE", null, null, null));
-                    context.getPipeline().addValve(new NonLoginAuthenticator());
+
+                    // LoginConfig is required to process @ServletSecurity
+                    // annotations
+                    if (context.getLoginConfig() == null) {
+                        context.setLoginConfig(new LoginConfig("NONE", null, null, null));
+                        context.getPipeline().addValve(new NonLoginAuthenticator());
+                    }
                 }
             } catch (ClassCastException e) {
             }
         }
-
     }
 
 
