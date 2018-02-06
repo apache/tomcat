723ea6a5bc5e7bc49e5ef84273c3b3c164a6a4fd
Mark Thomas <markt@apache.org>
2018-02-06 11:38:09 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=62067 Correctly apply security constraints mapped to the context root using a URL pattern of ""
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1823306 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/realm/RealmBase.java
+++ java/org/apache/catalina/realm/RealmBase.java
@@ -546,9 +546,9 @@ public abstract class RealmBase extends LifecycleMBeanBase implements Realm {
 
         // Check each defined security constraint
         String uri = request.getRequestPathMB().toString();
-        // Bug47080 - in rare cases this may be null
+        // Bug47080 - in rare cases this may be null or ""
         // Mapper treats as '/' do the same to prevent NPE
-        if (uri == null) {
+        if (uri == null || uri.length() == 0) {
             uri = "/";
         }
 
@@ -580,7 +580,8 @@ public abstract class RealmBase extends LifecycleMBeanBase implements Realm {
                 }
 
                 for(int k=0; k < patterns.length; k++) {
-                    if(uri.equals(patterns[k])) {
+                    // Exact match including special case for the context root.
+                    if(uri.equals(patterns[k]) || patterns[k].length() == 0 && uri.equals("/")) {
                         found = true;
                         if(collection[j].findMethod(method)) {
                             if(results == null) {
