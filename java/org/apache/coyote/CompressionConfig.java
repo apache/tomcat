apache/tomcat

commit 2d9c0fc0ab740954babff315f9998fd521f4fc9f

Prevent Tomcat from applying gzip compression to content that is already compressed with brotli compression. Patch provided by burka. This closes #99
git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1824297 13f79535-47bb-0310-9956-ffa450edef68

java/org/apache/coyote/CompressionConfig.java
@@ -186,9 +186,10 @@ public class CompressionConfig {
 
         MimeHeaders responseHeaders = response.getMimeHeaders();
 
-        // Check if content is not already gzipped
+        // Check if content is not already compressed
         MessageBytes contentEncodingMB = responseHeaders.getValue("Content-Encoding");
-        if ((contentEncodingMB != null) && (contentEncodingMB.indexOf("gzip") != -1)) {
+        if ((contentEncodingMB != null) && (contentEncodingMB.indexOf("gzip") != -1) &&
+                    (contentEncodingMB.indexOf("br") != -1)) {
             return false;
         }
 

commit 68f6c471f06d1fe29f05c34e95c50f96a885ed0d

Correct regression in r1824297
git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1824311 13f79535-47bb-0310-9956-ffa450edef68

java/org/apache/coyote/CompressionConfig.java
@@ -188,8 +188,9 @@ public class CompressionConfig {
 
         // Check if content is not already compressed
         MessageBytes contentEncodingMB = responseHeaders.getValue("Content-Encoding");
-        if ((contentEncodingMB != null) && (contentEncodingMB.indexOf("gzip") != -1) &&
-                    (contentEncodingMB.indexOf("br") != -1)) {
+        if (contentEncodingMB != null &&
+                (contentEncodingMB.indexOf("gzip") != -1 ||
+                        contentEncodingMB.indexOf("br") != -1)) {
             return false;
         }
 