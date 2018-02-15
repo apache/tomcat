apache/tomcat

commit f7c0e9812f2c7c133019adc8aff3cc9216dec2f9

Prevent Tomcat from applying gzip compression to content that is already compressed with brotli compression. Patch provided by burka.
git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1824300 13f79535-47bb-0310-9956-ffa450edef68

java/org/apache/coyote/http11/AbstractHttp11Processor.java
@@ -579,12 +579,11 @@ public abstract class AbstractHttp11Processor<S> extends AbstractProcessor<S> {
      */
     private boolean isCompressible() {
 
-        // Check if content is not already gzipped
-        MessageBytes contentEncodingMB =
-            response.getMimeHeaders().getValue("Content-Encoding");
+        // Check if content is not already compressed
+        MessageBytes contentEncodingMB = response.getMimeHeaders().getValue("Content-Encoding");
 
-        if ((contentEncodingMB != null)
-            && (contentEncodingMB.indexOf("gzip") != -1)) {
+        if ((contentEncodingMB != null) && (contentEncodingMB.indexOf("gzip") != -1) &&
+                (contentEncodingMB.indexOf("br") != -1)) {
             return false;
         }
 

commit 588d458e457dcb3c4e3ca81f683ec4e2bf2f4aa7

Correct regression in r1824300
git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1824315 13f79535-47bb-0310-9956-ffa450edef68

java/org/apache/coyote/http11/AbstractHttp11Processor.java
@@ -582,8 +582,9 @@ public abstract class AbstractHttp11Processor<S> extends AbstractProcessor<S> {
         // Check if content is not already compressed
         MessageBytes contentEncodingMB = response.getMimeHeaders().getValue("Content-Encoding");
 
-        if ((contentEncodingMB != null) && (contentEncodingMB.indexOf("gzip") != -1) &&
-                (contentEncodingMB.indexOf("br") != -1)) {
+        if ((contentEncodingMB != null) &&
+                (contentEncodingMB.indexOf("gzip") != -1 ||
+                        contentEncodingMB.indexOf("br") != -1)) {
             return false;
         }
 