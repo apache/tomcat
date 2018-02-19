db65572976cb2f9645c687f48fbb095f3e0f59a0
Mark Thomas <markt@apache.org>
2018-02-19 16:33:43 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=62218 Correctly create a JNDI ServiceRef using the specified interface rather than the concrete type. Based on a suggestion by Ángel Álvarez Páscua.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1824774 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/core/NamingContextListener.java
+++ java/org/apache/catalina/core/NamingContextListener.java
@@ -998,7 +998,7 @@ public class NamingContextListener
 
         // Create a reference to the resource.
         Reference ref = new ServiceRef
-            (service.getName(), service.getType(), service.getServiceqname(),
+            (service.getName(), service.getInterface(), service.getServiceqname(),
              service.getWsdlfile(), service.getJaxrpcmappingfile());
         // Adding the additional port-component-ref, if any
         Iterator<String> portcomponent = service.getServiceendpoints();
