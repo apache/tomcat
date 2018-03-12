65db87946ff2cfa2101c39671fa8304db8fe3955
Keiichi Fujino <kfujino@apache.org>
2018-03-12 07:13:58 +0000

    Ensure that parameters have been cleared when PreparedStatement and/or CallableStatement are cached.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc7.0.x/trunk@1826515 13f79535-47bb-0310-9956-ffa450edef68

--- modules/jdbc-pool/src/main/java/org/apache/tomcat/jdbc/pool/interceptor/StatementCache.java
+++ modules/jdbc-pool/src/main/java/org/apache/tomcat/jdbc/pool/interceptor/StatementCache.java
@@ -18,8 +18,8 @@ package org.apache.tomcat.jdbc.pool.interceptor;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -158,7 +158,7 @@ public class StatementCache extends StatementDecoratorInterceptor {
         boolean process = process(this.types, method, false);
         if (process) {
             Object result = null;
-            CachedStatement statementProxy = new CachedStatement((Statement)statement,sql);
+            CachedStatement statementProxy = new CachedStatement((PreparedStatement)statement,sql);
             result = constructor.newInstance(new Object[] { statementProxy });
             statementProxy.setActualProxy(result);
             statementProxy.setConnection(proxy);
@@ -248,10 +248,10 @@ public class StatementCache extends StatementDecoratorInterceptor {
         return cache;
     }
 
-    protected class CachedStatement extends StatementDecoratorInterceptor.StatementProxy<Statement> {
+    protected class CachedStatement extends StatementDecoratorInterceptor.StatementProxy<PreparedStatement> {
         boolean cached = false;
         CacheKey key;
-        public CachedStatement(Statement parent, String sql) {
+        public CachedStatement(PreparedStatement parent, String sql) {
             super(parent, sql);
         }
 
@@ -269,6 +269,9 @@ public class StatementCache extends StatementDecoratorInterceptor {
                     if (result != null && !result.isClosed()) {
                         result.close();
                     }
+                    // clear parameter
+                    getDelegate().clearParameters();
+
                     //create a new facade
                     Object actualProxy = getConstructor().newInstance(new Object[] { proxy });
                     proxy.setActualProxy(actualProxy);
