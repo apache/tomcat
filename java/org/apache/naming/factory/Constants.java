ed8f94b98442b356d329dc1fa549c015afc32da3
Mark Thomas <markt@apache.org>
2018-03-14 16:11:30 +0000

    Clean-up prior to looking at BZ 50019
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1826731 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/naming/factory/Constants.java
+++ java/org/apache/naming/factory/Constants.java
@@ -14,45 +14,33 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-
 package org.apache.naming.factory;
-
 
 /**
  * Static constants for this package.
  */
-
 public final class Constants {
 
     public static final String Package = "org.apache.naming.factory";
 
-    public static final String DEFAULT_RESOURCE_FACTORY =
-        Package + ".ResourceFactory";
+    public static final String DEFAULT_RESOURCE_FACTORY = Package + ".ResourceFactory";
 
-    public static final String DEFAULT_RESOURCE_LINK_FACTORY =
-        Package + ".ResourceLinkFactory";
+    public static final String DEFAULT_RESOURCE_LINK_FACTORY = Package + ".ResourceLinkFactory";
 
-    public static final String DEFAULT_TRANSACTION_FACTORY =
-        Package + ".TransactionFactory";
+    public static final String DEFAULT_TRANSACTION_FACTORY = Package + ".TransactionFactory";
 
-    public static final String DEFAULT_RESOURCE_ENV_FACTORY =
-        Package + ".ResourceEnvFactory";
+    public static final String DEFAULT_RESOURCE_ENV_FACTORY = Package + ".ResourceEnvFactory";
 
-    public static final String DEFAULT_EJB_FACTORY =
-        Package + ".EjbFactory";
+    public static final String DEFAULT_EJB_FACTORY = Package + ".EjbFactory";
 
-    public static final String DEFAULT_SERVICE_FACTORY =
-        Package + ".webservices.ServiceRefFactory";
+    public static final String DEFAULT_SERVICE_FACTORY = Package + ".webservices.ServiceRefFactory";
 
-    public static final String DEFAULT_HANDLER_FACTORY =
-        Package + ".HandlerFactory";
+    public static final String DEFAULT_HANDLER_FACTORY = Package + ".HandlerFactory";
 
     public static final String DBCP_DATASOURCE_FACTORY =
-        "org.apache.tomcat.dbcp.dbcp2.BasicDataSourceFactory";
+            "org.apache.tomcat.dbcp.dbcp2.BasicDataSourceFactory";
 
-    public static final String OPENEJB_EJB_FACTORY =
-        Package + ".OpenEjbFactory";
+    public static final String OPENEJB_EJB_FACTORY = Package + ".OpenEjbFactory";
 
     public static final String FACTORY = "factory";
 
