8308e4df2a374f706086444334554aea2555ce6a
Remy Maucherat <remm@apache.org>
2018-03-15 15:20:26 +0000

    62177 for NIO2, as I suppose the sync didn't guarantee the ordered writing that is needed with push.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1826832 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http2/Http2AsyncUpgradeHandler.java
+++ java/org/apache/coyote/http2/Http2AsyncUpgradeHandler.java
@@ -164,12 +164,13 @@ public class Http2AsyncUpgradeHandler extends Http2UpgradeHandler {
             if (headerFrameBuffers != null) {
                 bufs = headerFrameBuffers.bufs.toArray(BYTEBUFFER_ARRAY);
             }
-        }
-        if (bufs != null) {
-            socketWrapper.write(BlockingMode.SEMI_BLOCK, protocol.getWriteTimeout(),
-                    TimeUnit.MILLISECONDS, null, SocketWrapperBase.COMPLETE_WRITE,
-                    applicationErrorCompletion, bufs);
-            handleAsyncException();
+            // FIXME: look again at more optimized syncs, remove due to the need to write header frames in order with push
+            if (bufs != null) {
+                socketWrapper.write(BlockingMode.SEMI_BLOCK, protocol.getWriteTimeout(),
+                        TimeUnit.MILLISECONDS, null, SocketWrapperBase.COMPLETE_WRITE,
+                        applicationErrorCompletion, bufs);
+                handleAsyncException();
+            }
         }
         if (endOfStream) {
             stream.sentEndOfStream();
