a96f85265bdc583dd60d5d4910fe9ace08efc48b
Mark Thomas <markt@apache.org>
2018-03-20 12:08:01 +0000

    Fix test failure when OpenSSL version that supports TLS 1.3 is used.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.5.x/trunk@1827301 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/tomcat/util/net/openssl/TestOpenSSLConf.java
+++ test/org/apache/tomcat/util/net/openssl/TestOpenSSLConf.java
@@ -37,7 +37,7 @@ public class TestOpenSSLConf extends TomcatBaseTest {
     private static final String ENABLED_CIPHER = "AES256-SHA256";
     private static final String[] EXPECTED_CIPHERS = {"AES256-SHA256"};
     private static final String[] ENABLED_PROTOCOLS = {"TLSv1.1"};
-    private static final String[] DISABLED_PROTOCOLS = {"SSLv3", "TLSv1", "TLSv1.2"};
+    private static final String[] DISABLED_PROTOCOLS = {"SSLv3", "TLSv1", "TLSv1.2", "TLSv1.3"};
 
     public SSLHostConfig initOpenSSLConfCmdCipher(String... commands) throws Exception {
         Assert.assertNotNull(commands);
@@ -78,7 +78,9 @@ public class TestOpenSSLConf extends TomcatBaseTest {
 
     @Test
     public void testOpenSSLConfCmdCipher() throws Exception {
-        SSLHostConfig sslHostConfig = initOpenSSLConfCmdCipher("CipherString", ENABLED_CIPHER);
+        // Ensure TLSv1.3 ciphers aren't returned
+        SSLHostConfig sslHostConfig = initOpenSSLConfCmdCipher("CipherString", ENABLED_CIPHER,
+                "MaxProtocol", "TLSv1.2");
         String[] ciphers = sslHostConfig.getEnabledCiphers();
         Assert.assertThat("Wrong HostConfig ciphers", ciphers,
                 CoreMatchers.is(EXPECTED_CIPHERS));
