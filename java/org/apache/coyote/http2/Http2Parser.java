a06294c7fdf893e1b405bd3248cd98aacd02decb
Remy Maucherat <remm@apache.org>
2018-03-21 19:39:47 +0000

    i18n
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.5.x/trunk@1827435 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http2/Http2Parser.java
+++ java/org/apache/coyote/http2/Http2Parser.java
@@ -180,7 +180,8 @@ class Http2Parser {
                 if (dest.remaining() < dataLength) {
                     swallow(streamId, dataLength, false);
                     // Client has sent more data than permitted by Window size
-                    throw new StreamException("Client sent more data than stream window allowed", Http2Error.FLOW_CONTROL_ERROR, streamId);
+                    throw new StreamException(sm.getString("http2Parser.processFrameData.window", connectionId),
+                            Http2Error.FLOW_CONTROL_ERROR, streamId);
                 }
                 input.fill(true, dest, dataLength);
                 // Process padding before sending any notifications in case
