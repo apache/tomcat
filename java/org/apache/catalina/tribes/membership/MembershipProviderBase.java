67952727b7ee5da88c2a0ead3f0263630c810ede
Keiichi Fujino <kfujino@apache.org>
2018-05-09 04:32:23 +0000

    Implements the base class of MembershipProvider.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1831225 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/org/apache/catalina/tribes/membership/MembershipProviderBase.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.catalina.tribes.membership;
+
+import java.util.Properties;
+
+import org.apache.catalina.tribes.Member;
+import org.apache.catalina.tribes.MembershipListener;
+import org.apache.catalina.tribes.MembershipProvider;
+
+public abstract class MembershipProviderBase implements MembershipProvider {
+
+    protected Membership membership;
+    protected MembershipListener membershipListener;
+
+    @Override
+    public void init(Properties properties) throws Exception {
+    }
+
+    @Override
+    public boolean hasMembers() {
+        if (membership == null ) return false;
+        return membership.hasMembers();
+    }
+
+    @Override
+    public Member getMember(Member mbr) {
+        if (membership.getMembers() == null) return null;
+        return membership.getMember(mbr);
+    }
+
+    @Override
+    public Member[] getMembers() {
+        if (membership.getMembers() == null) return Membership.EMPTY_MEMBERS;
+        return membership.getMembers();
+    }
+
+    @Override
+    public void setMembershipListener(MembershipListener listener) {
+        this.membershipListener = listener;
+    }
+}
