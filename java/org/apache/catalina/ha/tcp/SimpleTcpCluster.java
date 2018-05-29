32b18b5935ff299166c7e8b4a59287b15c5177b7
Keiichi Fujino <kfujino@apache.org>
2018-05-29 06:45:46 +0000

    Simplify codes. remove duplicate codes.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1832430 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/ha/tcp/SimpleTcpCluster.java
+++ java/org/apache/catalina/ha/tcp/SimpleTcpCluster.java
@@ -680,24 +680,7 @@ public class SimpleTcpCluster extends LifecycleMBeanBase
      */
     @Override
     public void send(ClusterMessage msg, Member dest) {
-        try {
-            msg.setAddress(getLocalMember());
-            int sendOptions = channelSendOptions;
-            if (dest != null) {
-                if (!getLocalMember().equals(dest)) {
-                    channel.send(new Member[] {dest}, msg, sendOptions);
-                } else
-                    log.error(sm.getString("simpleTcpCluster.unableSend.localMember", msg));
-            } else {
-                Member[] destmembers = channel.getMembers();
-                if (destmembers.length>0)
-                    channel.send(destmembers,msg, sendOptions);
-                else if (log.isDebugEnabled())
-                    log.debug("No members in cluster, ignoring message:"+msg);
-            }
-        } catch (Exception x) {
-            log.error(sm.getString("simpleTcpCluster.sendFailed"), x);
-        }
+        send(msg, dest, this.channelSendOptions);
     }
 
     @Override
