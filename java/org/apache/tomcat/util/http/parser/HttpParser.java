903232070376a37c84f48ff6f8a386837a62ba50
Mark Thomas <markt@apache.org>
2018-05-30 10:09:56 +0000

    Correctly handle a digest authorization header when the user name contains an escaped character.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.5.x/trunk@1832528 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/http/parser/HttpParser.java
+++ java/org/apache/tomcat/util/http/parser/HttpParser.java
@@ -429,7 +429,7 @@ public class HttpParser {
                 if (returnQuoted) {
                     result.append('\\');
                 }
-                result.append(c);
+                result.append((char) c);
             } else {
                 result.append((char) c);
             }
