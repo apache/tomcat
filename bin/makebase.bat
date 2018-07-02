443e71a320b3c4af23876df5e709dcaeb29873aa
Mark Thomas <markt@apache.org>
2018-07-02 15:24:23 +0000

    Replace '::' with 'rem' for consistency with other .bat files. Also, research suggests '::' is not an official comment marker but a malformed label which causes the line to be ignored. The end result is generally the same but there are some edge cases with '::'
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1834853 13f79535-47bb-0310-9956-ffa450edef68

--- bin/makebase.bat
+++ bin/makebase.bat
@@ -1,35 +1,35 @@
-:: Licensed to the Apache Software Foundation (ASF) under one or more
-:: contributor license agreements.  See the NOTICE file distributed with
-:: this work for additional information regarding copyright ownership.
-:: The ASF licenses this file to You under the Apache License, Version 2.0
-:: (the "License"); you may not use this file except in compliance with
-:: the License.  You may obtain a copy of the License at
-::
-::     http://www.apache.org/licenses/LICENSE-2.0
-::
-:: Unless required by applicable law or agreed to in writing, software
-:: distributed under the License is distributed on an "AS IS" BASIS,
-:: WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-:: See the License for the specific language governing permissions and
-:: limitations under the License.
+rem Licensed to the Apache Software Foundation (ASF) under one or more
+rem contributor license agreements.  See the NOTICE file distributed with
+rem this work for additional information regarding copyright ownership.
+rem The ASF licenses this file to You under the Apache License, Version 2.0
+rem (the "License"); you may not use this file except in compliance with
+rem the License.  You may obtain a copy of the License at
+rem
+rem     http://www.apache.org/licenses/LICENSE-2.0
+rem
+rem Unless required by applicable law or agreed to in writing, software
+rem distributed under the License is distributed on an "AS IS" BASIS,
+rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem See the License for the specific language governing permissions and
+rem limitations under the License.
 
-:: This script creates the directory structure required for running Tomcat
-:: in a separate directory by pointing %CATALINA_BASE% to it. It copies the
-:: conf directory from %CATALINA_HOME%, and creates empty directories for
-:: bin, lib, logs, temp, webapps, and work.
-::
-:: If the file %CATALINA_HOME%/bin/setenv.sh exists then it is copied to
-:: the target directory as well.
-::
-:: Usage: makebase <path-to-target-directory>
+rem This script creates the directory structure required for running Tomcat
+rem in a separate directory by pointing %CATALINA_BASE% to it. It copies the
+rem conf directory from %CATALINA_HOME%, and creates empty directories for
+rem bin, lib, logs, temp, webapps, and work.
+rem
+rem If the file %CATALINA_HOME%/bin/setenv.sh exists then it is copied to
+rem the target directory as well.
+rem
+rem Usage: makebase <path-to-target-directory>
 
 @echo off
 
-:: first arg is the target directory
+rem first arg is the target directory
 set BASE_TGT=%1
 
 if %BASE_TGT%.==. (
-    :: target directory not provided; exit
+    rem target directory not provided; exit
     echo "Usage: makebase <path-to-target-directory>"
     goto :EOF
 )
@@ -38,28 +38,28 @@ set CURR_DIR=%~dp0
 set HOME_DIR=%CURR_DIR%..\
 
 if exist %BASE_TGT% (
-  :: target directory exists
+  rem target directory exists
   echo "Target directory exists"
 
-    :: exit if target directory is not empty
+    rem exit if target directory is not empty
     for /F %%i in ('dir /b "%BASE_TGT%\*.*"') do (
         echo "Target directory is not empty"
         goto :EOF
     )
 ) else (
-    :: create the target directory
+    rem create the target directory
     mkdir %BASE_TGT%
 )
 
-:: create empty directories for bin, lib, logs, temp, webapps, and work
+rem create empty directories for bin, lib, logs, temp, webapps, and work
 for %%d in (bin, lib, logs, temp, webapps, work) do (
     mkdir "%BASE_TGT%\%%d"
 )
 
-:: copy conf directory
+rem copy conf directory
 robocopy %HOME_DIR%\conf %BASE_TGT%\conf > nul
 
-:: copy setenv.bat if exists
+rem copy setenv.bat if exists
 robocopy %HOME_DIR%\bin %BASE_TGT%\bin setenv.bat > nul
 
 echo "Created CATALINA_BASE directory at $BASE_TGT"
