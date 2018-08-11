bb3191d937193171439ac0d5d97e45e565326766
Mark Thomas <markt@apache.org>
2018-08-11 18:42:48 +0000

    Fix broken tests
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1837878 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/coyote/http2/Http2UpgradeHandler.java
+++ java/org/apache/coyote/http2/Http2UpgradeHandler.java
@@ -810,6 +810,12 @@ class Http2UpgradeHandler extends AbstractStream implements InternalHttpUpgradeH
                     log.debug(sm.getString("upgradeHandler.releaseBacklog",
                             connectionId, stream.getIdentifier()));
                 }
+                // There is never any O/P on stream zero but it is included in
+                // the backlog as it simplifies the code. Skip it if it appears
+                // here.
+                if (this == stream) {
+                    continue;
+                }
                 Response coyoteResponse = ((Stream) stream).getCoyoteResponse();
                 if (coyoteResponse.getWriteListener() == null) {
                     // Blocking, so use notify to release StreamOutputBuffer
