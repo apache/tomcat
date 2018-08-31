9b7f5b7f40ca585aa43fe6ff46deba78d6451baa
Mark Thomas <markt@apache.org>
2018-08-31 10:03:40 +0000

    Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=62662 Fix the corruption of web.xml output during JSP compilation caused by the fix for 53492. Patch provided by Bernhard Frauendienst.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1839741 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/jasper/JspC.java
+++ java/org/apache/jasper/JspC.java
@@ -1167,19 +1167,22 @@ public class JspC extends Task implements Options {
         }
 
         if (servletout != null) {
-            servletout.write("\n    <servlet>\n        <servlet-name>");
-            servletout.write(thisServletName);
-            servletout.write("</servlet-name>\n        <servlet-class>");
-            servletout.write(thisServletName);
-            servletout.write("</servlet-class>\n    </servlet>\n");
+            synchronized(servletout) {
+                servletout.write("\n    <servlet>\n        <servlet-name>");
+                servletout.write(thisServletName);
+                servletout.write("</servlet-name>\n        <servlet-class>");
+                servletout.write(thisServletName);
+                servletout.write("</servlet-class>\n    </servlet>\n");
+            }
         }
         if (mappingout != null) {
-            mappingout.write("\n    <servlet-mapping>\n        <servlet-name>");
-            mappingout.write(thisServletName);
-            mappingout.write("</servlet-name>\n        <url-pattern>");
-            mappingout.write(file.replace('\\', '/'));
-            mappingout.write("</url-pattern>\n    </servlet-mapping>\n");
-
+            synchronized(mappingout) {
+                mappingout.write("\n    <servlet-mapping>\n        <servlet-name>");
+                mappingout.write(thisServletName);
+                mappingout.write("</servlet-name>\n        <url-pattern>");
+                mappingout.write(file.replace('\\', '/'));
+                mappingout.write("</url-pattern>\n    </servlet-mapping>\n");
+            }
         }
     }
 
