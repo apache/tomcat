a91c514796d0f6ec026feb72b1643d83da334786
Mark Thomas <markt@apache.org>
2018-09-03 19:12:26 +0000

    Remove / deprecate code originally added to enable the CGI Servlet to work as an include with SSI. The CGI servlet has generic handling for the include case and this flag is no longer required.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.5.x/trunk@1839985 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/catalina/ssi/SSIFilter.java
+++ java/org/apache/catalina/ssi/SSIFilter.java
@@ -36,8 +36,6 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.apache.catalina.Globals;
 /**
  * Filter to process SSI requests within a webpage. Mapped to a content types
  * from within web.xml.
@@ -95,9 +93,6 @@ public class SSIFilter implements Filter {
         // cast once
         HttpServletRequest req = (HttpServletRequest)request;
         HttpServletResponse res = (HttpServletResponse)response;
-
-        // indicate that we're in SSI processing
-        req.setAttribute(Globals.SSI_FLAG_ATTR, "true");
 
         // setup to capture output
         ByteArrayServletOutputStream basos = new ByteArrayServletOutputStream();
