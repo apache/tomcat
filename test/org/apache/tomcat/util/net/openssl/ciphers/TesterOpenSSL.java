bfd88d375291ad6658572d8bf8c6f7bb0a9ebeb1
Mark Thomas <markt@apache.org>
2018-10-03 09:07:15 +0000

    Back-port fix from trunk that is required by Gump so the required OpenSSL libraries are on the path.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.5.x/trunk@1842692 13f79535-47bb-0310-9956-ffa450edef68

--- test/org/apache/tomcat/util/net/openssl/ciphers/TesterOpenSSL.java
+++ test/org/apache/tomcat/util/net/openssl/ciphers/TesterOpenSSL.java
@@ -422,8 +422,14 @@ public class TesterOpenSSL {
 
     private static String executeOpenSSLCommand(String... args) throws IOException {
         String openSSLPath = System.getProperty("tomcat.test.openssl.path");
+        String openSSLLibPath = null;
         if (openSSLPath == null || openSSLPath.length() == 0) {
             openSSLPath = "openssl";
+        } else {
+            // Explicit OpenSSL path may also need explicit lib path
+            // (e.g. Gump needs this)
+            openSSLLibPath = openSSLPath.substring(0, openSSLPath.lastIndexOf('/'));
+            openSSLLibPath = openSSLLibPath + "/../lib";
         }
         List<String> cmd = new ArrayList<>();
         cmd.add(openSSLPath);
@@ -432,6 +438,18 @@ public class TesterOpenSSL {
         }
 
         ProcessBuilder pb = new ProcessBuilder(cmd.toArray(new String[cmd.size()]));
+
+        if (openSSLLibPath != null) {
+            Map<String,String> env = pb.environment();
+            String libraryPath = env.get("LD_LIBRARY_PATH");
+            if (libraryPath == null) {
+                libraryPath = openSSLLibPath;
+            } else {
+                libraryPath = libraryPath + ":" + openSSLLibPath;
+            }
+            env.put("LD_LIBRARY_PATH", libraryPath);
+        }
+
         Process p = pb.start();
 
         InputStreamToText stdout = new InputStreamToText(p.getInputStream());
