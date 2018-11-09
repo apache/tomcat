883a3a874a29d4c6150d44e7a1ce6cb592f013bb
Mark Thomas <markt@apache.org>
2018-11-09 10:16:18 +0000

    Add the code I've been using to export translations to a single file per language. Plenty of scope for improvement.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1846226 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/org/apache/tomcat/buildutil/translate/Export.java
@@ -0,0 +1,162 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package org.apache.tomcat.buildutil.translate;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.regex.Pattern;
+
+/**
+ * Generates a single properties file per language for import into a translation
+ * tool.
+ */
+public class Export {
+
+    private static final Map<String,Properties> translations = new HashMap<>();
+    private static final Pattern ADD_CONTINUATION = Pattern.compile("\\n", Pattern.MULTILINE);
+    private static final Pattern ESCAPE_LEADING_SPACE = Pattern.compile("^(\\s)", Pattern.MULTILINE);
+
+    public static void main(String... args) {
+        for (String dir : Constants.SEARCH_DIRS) {
+            processRoot(dir);
+        }
+
+        outputTranslations();
+    }
+
+
+    private static void processRoot(String dir) {
+        // Run from within IDE so working dir is root of project.
+        File root = new File(dir);
+
+        // Assumes no l18n files directly in roots
+        for (File f : root.listFiles()) {
+            if (f.isDirectory()) {
+                processDirectory(f);
+            }
+        }
+    }
+
+
+    private static void processDirectory(File dir) {
+        for (File f : dir.listFiles()) {
+            if (f.isDirectory()) {
+                processDirectory(f);
+            } else if (f.isFile()) {
+                processFile(f);
+            }
+        }
+    }
+
+
+    private static void processFile(File f) {
+        String name = f.getName();
+
+        // non-l10n files
+        if (!name.startsWith(Constants.L10N_PREFIX)) {
+            return;
+        }
+
+        // Determine language
+        String language = name.substring(Constants.L10N_PREFIX.length(), name.length() - Constants.L10N_SUFFIX.length());
+        if (language.length() == 0) {
+            // Default
+        } else if (language.length() == 3) {
+            language = language.substring(1);
+        }
+
+        String keyPrefix = getKeyPrefix(f);
+
+        Properties props = new Properties();
+
+        try (FileInputStream fis = new FileInputStream(f);
+                Reader r = new InputStreamReader(fis, StandardCharsets.UTF_8)) {
+            props.load(r);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Create a Map for the language if one does not exist.
+        Properties translation = translations.get(language);
+        if (translation == null) {
+            translation = new Properties();
+            translations.put(language, translation);
+        }
+
+        // Add the properties from this file to the combined file, prefixing the
+        // key with the package name to ensure uniqueness.
+        for (Object obj : props.keySet()) {
+            String key = (String) obj;
+            String value = props.getProperty(key);
+
+            translation.put(keyPrefix + key, value);
+        }
+    }
+
+
+    private static String getKeyPrefix(File f) {
+        File wd = new File(".");
+        String prefix = f.getParentFile().getAbsolutePath();
+        prefix = prefix.substring(wd.getAbsolutePath().length() - 1);
+        prefix = prefix.replace(File.separatorChar, '.');
+        prefix = prefix + Constants.END_PACKAGE_MARKER;
+        return prefix;
+    }
+
+
+    private static void outputTranslations() {
+        for (Map.Entry<String,Properties> translationEntry : translations.entrySet()) {
+            Properties translation = translationEntry.getValue();
+
+            String language = translationEntry.getKey();
+            if (language.length() > 0) {
+                language = "_" + language;
+            }
+
+            File out = new File(Constants.L10N_PREFIX + language + Constants.L10N_SUFFIX);
+            try (FileOutputStream fos = new FileOutputStream(out);
+                    Writer w = new OutputStreamWriter(fos, StandardCharsets.UTF_8)) {
+                for (Object key : translation.keySet()) {
+                    w.write(key + "=" + formatValue(translation.getProperty((String) key)) + "\n");
+                }
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+        }
+    }
+
+
+    private static String formatValue(String in) {
+        String result = ADD_CONTINUATION.matcher(in).replaceAll("\\\\n\\\\\n");
+        if (result.endsWith("\\\n")) {
+            result = result.substring(0, result.length() - 2);
+        }
+        result = ESCAPE_LEADING_SPACE.matcher(result).replaceAll("\\\\$1");
+        return result;
+    }
+}
+
