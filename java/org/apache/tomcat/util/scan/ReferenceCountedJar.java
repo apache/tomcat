ad03fb99caaff9bfa56159b0f2528ae72870a203
Mark Thomas <markt@apache.org>
2018-12-06 09:35:42 +0000

    Add additional method present in 8.5.x
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/tc8.5.x/trunk@1848275 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/scan/ReferenceCountedJar.java
+++ java/org/apache/tomcat/util/scan/ReferenceCountedJar.java
@@ -143,4 +143,13 @@ public class ReferenceCountedJar implements Jar {
             jar.wrappedJar.reset();
         }
     }
+
+
+    @Override
+    @Deprecated
+    public boolean entryExists(String name) throws IOException {
+        try (ReferenceCountedJar jar = open()) {
+            return jar.wrappedJar.entryExists(name);
+        }
+    }
 }
