3a89851eb7a5a0e18c3e01689eeab6f88e4aebc0
Mark Thomas <markt@apache.org>
2018-12-12 21:18:43 +0000

    Fix an IDE nag (only fix one to trigger a CI build)
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1848797 13f79535-47bb-0310-9956-ffa450edef68

--- java/org/apache/tomcat/util/buf/ByteChunk.java
+++ java/org/apache/tomcat/util/buf/ByteChunk.java
@@ -512,7 +512,8 @@ public final class ByteChunk extends AbstractChunk {
     public void flushBuffer() throws IOException {
         // assert out!=null
         if (out == null) {
-            throw new IOException(sm.getString("chunk.overflow", getLimit(), buff.length));
+            throw new IOException(sm.getString(
+                    "chunk.overflow", Integer.valueOf(getLimit()), Integer.valueOf(buff.length)));
         }
         out.realWriteBytes(buff, start, end - start);
         end = start;
