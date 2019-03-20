a8b43c40dabd98c0c3f375a307ea09c9007cedb7
Mark Thomas <markt@apache.org>
2019-03-20 15:05:05 +0000

    Add support for Java 11 as the source/target for JSP compilation

--- java/org/apache/jasper/compiler/JDTCompiler.java
+++ java/org/apache/jasper/compiler/JDTCompiler.java
@@ -348,6 +348,10 @@ public class JDTCompiler extends org.apache.jasper.compiler.Compiler {
                 // Constant not available in latest ECJ version that runs on
                 // Java 7
                 settings.put(CompilerOptions.OPTION_Source, "10");
+            } else if(opt.equals("11")) {
+                // Constant not available in latest ECJ version that runs on
+                // Java 7
+                settings.put(CompilerOptions.OPTION_Source, "11");
             } else {
                 log.warn("Unknown source VM " + opt + " ignored.");
                 settings.put(CompilerOptions.OPTION_Source,
@@ -404,6 +408,11 @@ public class JDTCompiler extends org.apache.jasper.compiler.Compiler {
                 // Java 7
                 settings.put(CompilerOptions.OPTION_TargetPlatform, "10");
                 settings.put(CompilerOptions.OPTION_Compliance, "10");
+            } else if(opt.equals("11")) {
+                // Constant not available in latest ECJ version that runs on
+                // Java 7
+                settings.put(CompilerOptions.OPTION_TargetPlatform, "11");
+                settings.put(CompilerOptions.OPTION_Compliance, "11");
             } else {
                 log.warn("Unknown target VM " + opt + " ignored.");
                 settings.put(CompilerOptions.OPTION_TargetPlatform,
