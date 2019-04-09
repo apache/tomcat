2448177c3fcc0b2c8025f219d9309e4eef24e80a
Martin Lemanski <martin.lemanski@willhaben.at>
2019-04-09 14:56:14 +0200

    adds braces to if statement
    can't remove with `Map.remove(key, value)`, compiler complains about types
    
    fix 63324

--- java/org/apache/catalina/valves/CrawlerSessionManagerValve.java
+++ java/org/apache/catalina/valves/CrawlerSessionManagerValve.java
@@ -88,8 +88,8 @@ public class CrawlerSessionManagerValve extends ValveBase {
     }
 
     /**
+     * @return The current regular expression being used to match user agents.
      * @see #setCrawlerUserAgents(String)
-     * @return  The current regular expression being used to match user agents.
      */
     public String getCrawlerUserAgents() {
         return crawlerUserAgents;
@@ -113,8 +113,8 @@ public class CrawlerSessionManagerValve extends ValveBase {
     }
 
     /**
-     * @see #setCrawlerIps(String)
      * @return The current regular expression being used to match IP addresses.
+     * @see #setCrawlerIps(String)
      */
     public String getCrawlerIps() {
         return crawlerIps;
@@ -125,15 +125,15 @@ public class CrawlerSessionManagerValve extends ValveBase {
      * Specify the session timeout (in seconds) for a crawler's session. This is
      * typically lower than that for a user session. The default is 60 seconds.
      *
-     * @param sessionInactiveInterval   The new timeout for crawler sessions
+     * @param sessionInactiveInterval The new timeout for crawler sessions
      */
     public void setSessionInactiveInterval(int sessionInactiveInterval) {
         this.sessionInactiveInterval = sessionInactiveInterval;
     }
 
     /**
+     * @return The current timeout in seconds
      * @see #setSessionInactiveInterval(int)
-     * @return  The current timeout in seconds
      */
     public int getSessionInactiveInterval() {
         return sessionInactiveInterval;
@@ -287,8 +287,9 @@ public class CrawlerSessionManagerValve extends ValveBase {
         @Override
         public void valueUnbound(HttpSessionBindingEvent event) {
             if (clientIdentifier != null && clientIdSessionId != null) {
-                if(clientIdSessionId.get(clientIdentifier).equals(event.getSession().getId()))
-                clientIdSessionId.remove(clientIdentifier);
+                if (clientIdSessionId.get(clientIdentifier).equals(event.getSession().getId())) {
+                    clientIdSessionId.remove(clientIdentifier);
+                }
             }
         }
     }
