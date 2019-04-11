c8748aaf9f3f7bc9f38c5805ed80e1a333696216
Coty Sutherland <csutherl@apache.org>
2019-04-11 15:45:55 -0400

    Update ServerInfo to reflect actual information instead of placeholders when running development builds

--- java/org/apache/catalina/util/ServerInfo.java
+++ java/org/apache/catalina/util/ServerInfo.java
@@ -68,11 +68,11 @@ public class ServerInfo {
         } catch (Throwable t) {
             ExceptionUtils.handleThrowable(t);
         }
-        if (info == null)
-            info = "Apache Tomcat 9.0.x-dev";
-        if (built == null)
+        if (info == null || info.equals("Apache Tomcat/@VERSION@"))
+            info = "Apache Tomcat/9.0.x-dev";
+        if (built == null || built.equals("@VERSION_BUILT@"))
             built = "unknown";
-        if (number == null)
+        if (number == null || number.equals("@VERSION_NUMBER@"))
             number = "9.0.x";
 
         serverInfo = info;
