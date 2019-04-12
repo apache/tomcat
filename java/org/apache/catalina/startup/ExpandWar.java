a2b111678c37d8a0a6ba841737a1668d381077b5
Mark Thomas <markt@apache.org>
2019-04-12 11:36:57 +0100

    Fix typo

--- java/org/apache/catalina/startup/ExpandWar.java
+++ java/org/apache/catalina/startup/ExpandWar.java
@@ -68,7 +68,7 @@ public class ExpandWar {
      * @exception IOException if an input/output error was encountered
      *  during expansion
      *
-     * @return The absolute path to the expanded directory foe the given WAR
+     * @return The absolute path to the expanded directory for the given WAR
      */
     public static String expand(Host host, URL war, String pathname)
         throws IOException {
