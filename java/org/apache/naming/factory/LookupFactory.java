c575781797adffbd82f261c250cf0e22834361f5
Mark Thomas <markt@apache.org>
2019-04-18 23:25:37 +0100

    Make clean-up fix compatible with Java 6

--- java/org/apache/naming/factory/LookupFactory.java
+++ java/org/apache/naming/factory/LookupFactory.java
@@ -16,6 +16,7 @@
  */
 package org.apache.naming.factory;
 
+import java.lang.reflect.Method;
 import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.Set;
@@ -135,9 +136,10 @@ public class LookupFactory implements ObjectFactory {
                     NamingException ne = new NamingException(msg);
                     log.warn(msg, ne);
                     // Close the resource we no longer need if we know how to do so
-                    if (result instanceof AutoCloseable) {
+                    if (isInstance(result.getClass(), "java.lang.AutoCloseable")) {
                         try {
-                            ((AutoCloseable) result).close();
+                            Method m = result.getClass().getMethod("close");
+                            m.invoke(result);
                         } catch (Exception e) {
                             // Ignore
                         }
@@ -151,5 +153,26 @@ public class LookupFactory implements ObjectFactory {
 
 
         return result;
+    }
+
+
+    private static boolean isInstance(Class<?> clazz, String type) {
+        if (type.equals(clazz.getName())) {
+            return true;
+        }
+
+        Class<?>[] ifaces = clazz.getInterfaces();
+        for (Class<?> iface : ifaces) {
+            if (isInstance(iface, type)) {
+                return true;
+            }
+        }
+
+        Class<?> superClazz = clazz.getSuperclass();
+        if (superClazz == null) {
+            return false;
+        } else {
+            return isInstance(superClazz, type);
+        }
     }
 }
