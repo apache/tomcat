263ed702b57027c84b5828e835fd3a933b166355
Mark Thomas <markt@apache.org>
2019-04-23 11:41:44 +0100

    Fix build warnings
    Use Tomcat format license text (longer lines)
    Correct package name
    Re-order imports
    Remove static import
    Ignore sync-override warning
    Add missing serialVersionUID

--- java/org/apache/tomcat/util/modeler/NoDescriptorRegistry.java
+++ java/org/apache/tomcat/util/modeler/NoDescriptorRegistry.java
@@ -1,25 +1,25 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-package org.apache.meecrowave.tomcat;
+package org.apache.tomcat.util.modeler;
 
-import org.apache.tomcat.util.modeler.ManagedBean;
-import org.apache.tomcat.util.modeler.Registry;
+import java.io.ObjectInputStream;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
 
 import javax.management.Attribute;
 import javax.management.AttributeList;
@@ -43,11 +43,6 @@ import javax.management.OperationsException;
 import javax.management.QueryExp;
 import javax.management.ReflectionException;
 import javax.management.loading.ClassLoaderRepository;
-import java.io.ObjectInputStream;
-import java.util.List;
-import java.util.Set;
-
-import static java.util.Collections.emptyList;
 
 // skip Tomcat MBeans, goal is really to skip mbean-descriptors.xml reading which is slow for small apps.
 public class NoDescriptorRegistry extends Registry {
@@ -69,6 +64,7 @@ public class NoDescriptorRegistry extends Registry {
         // no-op
     }
 
+    @SuppressWarnings("sync-override")
     @Override
     public int getId(final String domain, final String name) {
         return 0;
@@ -101,7 +97,7 @@ public class NoDescriptorRegistry extends Registry {
 
     @Override
     public List<ObjectName> load(final String sourceType, final Object source, final String param) throws Exception {
-        return emptyList();
+        return Collections.emptyList();
     }
 
     @Override
@@ -307,5 +303,7 @@ public class NoDescriptorRegistry extends Registry {
     }
 
     private static class PassthroughMBean extends ManagedBean {
+
+        private static final long serialVersionUID = 1L;
     }
 }
