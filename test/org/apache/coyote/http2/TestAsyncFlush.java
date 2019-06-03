311a6096a20746f599a4ebe35cea0b56803356c0
Mark Thomas <markt@apache.org>
2019-06-03 16:18:33 +0100

    Fix typo

--- test/org/apache/coyote/http2/TestAsyncFlush.java
+++ test/org/apache/coyote/http2/TestAsyncFlush.java
@@ -41,7 +41,7 @@ import org.apache.catalina.startup.Tomcat;
  *       non-blocking I/O is explicitly called out as illegal in the Servlet
  *       specification but also goes on to say the behaviour if such a call is
  *       made is undefined. Which means it is OK if the call works as expected
- *       (a non-blcoking flush is triggered) :).
+ *       (a non-blocking flush is triggered) :).
  *       If any of these tests fail, that should not block a release since -
  *       while the specification allows this to work - it doesn't require that
  *       it does work.
