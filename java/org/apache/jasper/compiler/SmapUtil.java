817583187593dec89b47d5e2df83226d3943a52f
Mark Thomas <markt@apache.org>
2019-06-10 14:33:06 +0100

    Better error messages for SMAP generation

--- java/org/apache/jasper/compiler/SmapUtil.java
+++ java/org/apache/jasper/compiler/SmapUtil.java
@@ -177,10 +177,12 @@ public class SmapUtil {
             SDEInstaller installer = new SDEInstaller(classFile, smap);
             installer.install(tmpFile);
             if (!classFile.delete()) {
-                throw new IOException(Localizer.getMessage("jsp.error.unable.deleteClassFile"));
+                throw new IOException(Localizer.getMessage("jsp.error.unable.deleteClassFile",
+                        classFile.getAbsolutePath()));
             }
             if (!tmpFile.renameTo(classFile)) {
-                throw new IOException(Localizer.getMessage("jsp.error.unable.renameClassFile"));
+                throw new IOException(Localizer.getMessage("jsp.error.unable.renameClassFile",
+                        tmpFile.getAbsolutePath(), classFile.getAbsolutePath()));
             }
         }
 
