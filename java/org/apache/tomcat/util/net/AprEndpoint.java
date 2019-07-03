60564acc0d6b5b739024a540f44d07f04b417f67
Mark Thomas <markt@apache.org>
2019-07-03 20:44:41 +0100

    Quick fix for poller issue reported on users list

--- java/org/apache/tomcat/util/net/AprEndpoint.java
+++ java/org/apache/tomcat/util/net/AprEndpoint.java
@@ -1534,6 +1534,8 @@ public class AprEndpoint extends AbstractEndpoint<Long> implements SNICallBack {
                         boolean reset = false;
 
                         int rv = 0;
+                        // Reset the nextPollerTime
+                        nextPollerTime = pollerTime;
                         // Iterate on each pollers, but no need to poll empty pollers
                         if (pollerSpace[i] < actualPollerSize) {
                             rv = Poll.poll(pollers[i], nextPollerTime, desc, true);
