52c14e393d3b9348575f1b45ff96b79f68cbbbc1
remm <remm@apache.org>
2019-07-16 18:23:10 +0200

    Test CI again.

--- java/org/apache/tomcat/util/compat/GraalCompat.java
+++ java/org/apache/tomcat/util/compat/GraalCompat.java
@@ -35,12 +35,10 @@ class GraalCompat extends JreCompat {
         GRAAL = result;
     }
 
-
     static boolean isSupported() {
         // This property does not exist for a native image
         return GRAAL;
     }
-
 
     @Override
     public void disableCachingForJarUrlConnections() throws IOException {
