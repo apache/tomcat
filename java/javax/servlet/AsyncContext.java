4e1459f207ac8081d2420d2c1407f5d7d82d70b0
Mark Thomas <markt@apache.org>
2019-07-22 09:44:46 +0100

    Polish. Fix indent.
    Trivial commit to trigger a CI build

--- java/javax/servlet/AsyncContext.java
+++ java/javax/servlet/AsyncContext.java
@@ -22,17 +22,17 @@ package javax.servlet;
  */
 public interface AsyncContext {
     public static final String ASYNC_REQUEST_URI =
-        "javax.servlet.async.request_uri";
+            "javax.servlet.async.request_uri";
     public static final String ASYNC_CONTEXT_PATH  =
-        "javax.servlet.async.context_path";
+            "javax.servlet.async.context_path";
     public static final String ASYNC_MAPPING =
             "javax.servlet.async.mapping";
     public static final String ASYNC_PATH_INFO =
-        "javax.servlet.async.path_info";
+            "javax.servlet.async.path_info";
     public static final String ASYNC_SERVLET_PATH =
-        "javax.servlet.async.servlet_path";
+            "javax.servlet.async.servlet_path";
     public static final String ASYNC_QUERY_STRING =
-        "javax.servlet.async.query_string";
+            "javax.servlet.async.query_string";
 
     ServletRequest getRequest();
 
