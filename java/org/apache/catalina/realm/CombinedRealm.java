c9e9b5d7f88307713c27128d12890daf1c047cc3
Michael Osipov <michaelo@apache.org>
2019-08-02 14:09:02 +0200

    BZ 63627: Implement more fine-grained handling in RealmBase#authenticate(GSSContext, boolean)

--- java/org/apache/catalina/realm/CombinedRealm.java
+++ java/org/apache/catalina/realm/CombinedRealm.java
@@ -331,7 +331,7 @@ public class CombinedRealm extends RealmBase {
      * {@inheritDoc}
      */
     @Override
-    public Principal authenticate(GSSContext gssContext, boolean storeCreds) {
+    public Principal authenticate(GSSContext gssContext, boolean storeCred) {
         if (gssContext.isEstablished()) {
             Principal authenticatedUser = null;
             String username = null;
@@ -352,7 +352,7 @@ public class CombinedRealm extends RealmBase {
                             username, realm.getInfo()));
                 }
 
-                authenticatedUser = realm.authenticate(gssContext, storeCreds);
+                authenticatedUser = realm.authenticate(gssContext, storeCred);
 
                 if (authenticatedUser == null) {
                     if (log.isDebugEnabled()) {
