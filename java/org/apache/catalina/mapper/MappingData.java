335da9218d4c1832c6e66d4f74bf04df589962bb
康智冬 <worldkzd@gmail.com>
2019-08-17 15:40:18 +0800

    fix typo

--- java/org/apache/catalina/mapper/MappingData.java
+++ java/org/apache/catalina/mapper/MappingData.java
@@ -45,7 +45,7 @@ public class MappingData {
 
     public final MessageBytes redirectPath = MessageBytes.newInstance();
 
-    // Fields used by ApplicationMapping to implement javax.servlet.http.Mapping
+    // Fields used by ApplicationMapping to implement javax.servlet.http.HttpServletMapping
     public MappingMatch matchType = null;
 
     public void recycle() {
