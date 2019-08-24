9aa02209c9d0b3fa56c7bd96dd918cea9e229806
kangzhidong <kzd666@163.com>
2019-08-25 00:12:22 +0800

    fix typo

--- java/org/apache/tomcat/util/descriptor/tagplugin/TagPluginParser.java
+++ java/org/apache/tomcat/util/descriptor/tagplugin/TagPluginParser.java
@@ -61,7 +61,7 @@ public class TagPluginParser {
             if (!handler.getWarnings().isEmpty() || !handler.getErrors().isEmpty()) {
                 handler.logFindings(log, source.getSystemId());
                 if (!handler.getErrors().isEmpty()) {
-                    // throw the first to indicate there was a error during processing
+                    // throw the first to indicate there was an error during processing
                     throw handler.getErrors().iterator().next();
                 }
             }
