2015090e55abc9272dd8b6f010a1a568c8263398
psteitz <psteitz@apache.org>
2019-10-20 12:02:46 -0700

    Remove unused imports.

--- test/org/apache/tomcat/dbcp/dbcp/TestBasicDataSource.java
+++ test/org/apache/tomcat/dbcp/dbcp/TestBasicDataSource.java
@@ -27,10 +27,6 @@ import javax.sql.DataSource;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertNotNull;
 
 /**
  * TestSuite for BasicDataSource
