340bb1715d0f0bf274f2d2d24fdbeb45dcccce2c
Phil Steitz <phil.steitz@gmail.com>
2019-10-20 09:37:12 -0700

    Repackaging.

--- test/org/apache/tomcat/dbcp/dbcp/TesterClassLoader.java
+++ test/org/apache/tomcat/dbcp/dbcp/TesterClassLoader.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.commons.dbcp;
+package org.apache.tomcat.dbcp.dbcp;
 
 import java.util.HashSet;
 import java.util.Set;
--- test/org/apache/tomcat/dbcp/dbcp/TesterConnection.java
+++ test/org/apache/tomcat/dbcp/dbcp/TesterConnection.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.commons.dbcp;
+package org.apache.tomcat.dbcp.dbcp;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
