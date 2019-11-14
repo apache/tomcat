ee6ad621bb4609ac513f0bcc40818ad86840d326
Mark Thomas <markt@apache.org>
2019-11-14 10:33:17 +0000

    Align with 9.0.x

--- java/org/apache/juli/logging/LogFactory.java
+++ java/org/apache/juli/logging/LogFactory.java
@@ -17,7 +17,6 @@
 package org.apache.juli.logging;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.nio.file.FileSystems;
 import java.util.ServiceLoader;
 import java.util.logging.LogManager;
@@ -133,8 +132,7 @@ public class LogFactory {
 
         try {
             return discoveredLogConstructor.newInstance(name);
-        } catch (InstantiationException | IllegalAccessException | IllegalArgumentException |
-                InvocationTargetException e) {
+        } catch (ReflectiveOperationException | IllegalArgumentException e) {
             throw new LogConfigurationException(e);
         }
     }
