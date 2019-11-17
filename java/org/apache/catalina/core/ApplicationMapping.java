apache/tomcat

commit 6c03319f992d8a123ce2f47281c5956e5a7c1cfd

Update the Servlet 4 preview API to reflect the final release.
Note that this preview API has deprecated for over a year and may be
removed as soon as the next 8.5.x release.

java/org/apache/catalina/core/ApplicationMapping.java
@@ -17,30 +17,30 @@
 package org.apache.catalina.core;
 
 import org.apache.catalina.mapper.MappingData;
+import org.apache.catalina.servlet4preview.http.HttpServletMapping;
 import org.apache.catalina.servlet4preview.http.MappingMatch;
-import org.apache.catalina.servlet4preview.http.ServletMapping;
 
 public class ApplicationMapping {
 
     private final MappingData mappingData;
 
-    private volatile ServletMapping mapping = null;
+    private volatile HttpServletMapping mapping = null;
 
     public ApplicationMapping(MappingData mappingData) {
         this.mappingData = mappingData;
     }
 
-    public ServletMapping getServletMapping() {
+    public HttpServletMapping getHttpServletMapping() {
         if (mapping == null) {
-            if (mappingData == null) {
-                mapping = new MappingImpl("", "", MappingMatch.UNKNOWN, "");
+            String servletName;
+            if (mappingData.wrapper == null) {
+                servletName = "";
             } else {
-                String servletName;
-                if (mappingData.wrapper == null) {
-                    servletName = "";
-                } else {
-                    servletName = mappingData.wrapper.getName();
-                }
+                servletName = mappingData.wrapper.getName();
+            }
+            if (mappingData.matchType == null) {
+                mapping = new MappingImpl("", "", null, servletName);
+            } else {
                 switch (mappingData.matchType) {
                     case CONTEXT_ROOT:
                         mapping = new MappingImpl("", "", mappingData.matchType, servletName);
@@ -65,12 +65,8 @@ public class ApplicationMapping {
                         } else {
                             matchValue = mappingData.pathInfo.toString().substring(1);
                         }
-                        mapping = new MappingImpl(matchValue,
-                                mappingData.wrapperPath.toString() + "/*",
+                        mapping = new MappingImpl(matchValue, mappingData.wrapperPath.toString() + "/*",
                                 mappingData.matchType, servletName);
-                        break;
-                    case UNKNOWN:
-                        mapping = new MappingImpl("", "", mappingData.matchType, servletName);
                         break;
                 }
             }
@@ -83,7 +79,7 @@ public class ApplicationMapping {
         mapping = null;
     }
 
-    private static class MappingImpl implements ServletMapping {
+    private static class MappingImpl implements HttpServletMapping {
 
         private final String matchValue;
         private final String pattern;

java/org/apache/catalina/connector/Request.java
@@ -81,8 +81,8 @@ import org.apache.catalina.core.ApplicationPushBuilder;
 import org.apache.catalina.core.ApplicationSessionCookieConfig;
 import org.apache.catalina.core.AsyncContextImpl;
 import org.apache.catalina.mapper.MappingData;
+import org.apache.catalina.servlet4preview.http.HttpServletMapping;
 import org.apache.catalina.servlet4preview.http.PushBuilder;
-import org.apache.catalina.servlet4preview.http.ServletMapping;
 import org.apache.catalina.util.ParameterMap;
 import org.apache.catalina.util.TLSUtil;
 import org.apache.catalina.util.URLEncoder;
@@ -2298,8 +2298,8 @@ public class Request implements org.apache.catalina.servlet4preview.http.HttpSer
 
 
     @Override
-    public ServletMapping getServletMapping() {
-        return applicationMapping.getServletMapping();
+    public HttpServletMapping getHttpServletMapping() {
+        return applicationMapping.getHttpServletMapping();
     }
 
 

java/org/apache/catalina/connector/RequestFacade.java
@@ -41,9 +41,9 @@ import javax.servlet.http.Part;
 
 import org.apache.catalina.Globals;
 import org.apache.catalina.security.SecurityUtil;
+import org.apache.catalina.servlet4preview.http.HttpServletMapping;
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.apache.catalina.servlet4preview.http.PushBuilder;
-import org.apache.catalina.servlet4preview.http.ServletMapping;
 import org.apache.tomcat.util.res.StringManager;
 
 /**
@@ -1123,8 +1123,8 @@ public class RequestFacade implements HttpServletRequest {
      * removed or replaced at any time until Servlet 4.0 becomes final.
      */
     @Override
-    public ServletMapping getServletMapping() {
-        return request.getServletMapping();
+    public HttpServletMapping getHttpServletMapping() {
+        return request.getHttpServletMapping();
     }
 
 

java/org/apache/catalina/core/ApplicationContext.java
@@ -67,7 +67,7 @@ import org.apache.catalina.WebResourceRoot;
 import org.apache.catalina.Wrapper;
 import org.apache.catalina.connector.Connector;
 import org.apache.catalina.mapper.MappingData;
-import org.apache.catalina.servlet4preview.http.ServletMapping;
+import org.apache.catalina.servlet4preview.http.HttpServletMapping;
 import org.apache.catalina.util.Introspection;
 import org.apache.catalina.util.ServerInfo;
 import org.apache.catalina.util.URLEncoder;
@@ -483,7 +483,7 @@ public class ApplicationContext implements org.apache.catalina.servlet4preview.S
             Wrapper wrapper = mappingData.wrapper;
             String wrapperPath = mappingData.wrapperPath.toString();
             String pathInfo = mappingData.pathInfo.toString();
-            ServletMapping mapping = new ApplicationMapping(mappingData).getServletMapping();
+            HttpServletMapping mapping = new ApplicationMapping(mappingData).getHttpServletMapping();
 
             // Construct a RequestDispatcher to process this request
             return new ApplicationDispatcher(wrapper, uri, wrapperPath, pathInfo,

java/org/apache/catalina/core/ApplicationDispatcher.java
@@ -44,7 +44,7 @@ import org.apache.catalina.connector.Request;
 import org.apache.catalina.connector.RequestFacade;
 import org.apache.catalina.connector.Response;
 import org.apache.catalina.connector.ResponseFacade;
-import org.apache.catalina.servlet4preview.http.ServletMapping;
+import org.apache.catalina.servlet4preview.http.HttpServletMapping;
 import org.apache.tomcat.util.ExceptionUtils;
 import org.apache.tomcat.util.res.StringManager;
 
@@ -211,7 +211,7 @@ final class ApplicationDispatcher implements AsyncDispatcher, RequestDispatcher 
      */
     public ApplicationDispatcher
         (Wrapper wrapper, String requestURI, String servletPath,
-         String pathInfo, String queryString, ServletMapping mapping, String name) {
+         String pathInfo, String queryString, HttpServletMapping mapping, String name) {
 
         super();
 
@@ -268,7 +268,7 @@ final class ApplicationDispatcher implements AsyncDispatcher, RequestDispatcher 
     /**
      * The mapping for this RequestDispatcher.
      */
-    private final ServletMapping mapping;
+    private final HttpServletMapping mapping;
 
 
     /**
@@ -372,12 +372,12 @@ final class ApplicationDispatcher implements AsyncDispatcher, RequestDispatcher 
                                       hrequest.getPathInfo());
                 wrequest.setAttribute(RequestDispatcher.FORWARD_QUERY_STRING,
                                       hrequest.getQueryString());
-                ServletMapping mapping;
+                HttpServletMapping mapping;
                 if (hrequest instanceof org.apache.catalina.servlet4preview.http.HttpServletRequest) {
                     mapping = ((org.apache.catalina.servlet4preview.http.HttpServletRequest)
-                            hrequest).getServletMapping();
+                            hrequest).getHttpServletMapping();
                 } else {
-                    mapping = (new ApplicationMapping(null)).getServletMapping();
+                    mapping = (new ApplicationMapping(null)).getHttpServletMapping();
                 }
                 wrequest.setAttribute(FORWARD_MAPPING, mapping);
             }
@@ -627,12 +627,12 @@ final class ApplicationDispatcher implements AsyncDispatcher, RequestDispatcher 
 
         wrequest.setAttribute(Globals.DISPATCHER_TYPE_ATTR, DispatcherType.ASYNC);
         wrequest.setAttribute(Globals.DISPATCHER_REQUEST_PATH_ATTR, getCombinedPath());
-        ServletMapping mapping;
+        HttpServletMapping mapping;
         if (hrequest instanceof org.apache.catalina.servlet4preview.http.HttpServletRequest) {
             mapping = ((org.apache.catalina.servlet4preview.http.HttpServletRequest)
-                    hrequest).getServletMapping();
+                    hrequest).getHttpServletMapping();
         } else {
-            mapping = (new ApplicationMapping(null)).getServletMapping();
+            mapping = (new ApplicationMapping(null)).getHttpServletMapping();
         }
         wrequest.setAttribute(ASYNC_MAPPING, mapping);
 

java/org/apache/catalina/core/ApplicationHttpRequest.java
@@ -42,8 +42,8 @@ import org.apache.catalina.Globals;
 import org.apache.catalina.Manager;
 import org.apache.catalina.Session;
 import org.apache.catalina.connector.RequestFacade;
+import org.apache.catalina.servlet4preview.http.HttpServletMapping;
 import org.apache.catalina.servlet4preview.http.PushBuilder;
-import org.apache.catalina.servlet4preview.http.ServletMapping;
 import org.apache.catalina.util.ParameterMap;
 import org.apache.catalina.util.URLEncoder;
 import org.apache.tomcat.util.buf.B2CConverter;
@@ -194,7 +194,7 @@ class ApplicationHttpRequest
     /**
      * The mapping for this request.
      */
-    private ServletMapping mapping = null;
+    private HttpServletMapping mapping = null;
 
 
     /**
@@ -529,7 +529,7 @@ class ApplicationHttpRequest
 
 
     @Override
-    public ServletMapping getServletMapping() {
+    public HttpServletMapping getHttpServletMapping() {
         return mapping;
     }
 
@@ -721,9 +721,9 @@ class ApplicationHttpRequest
         requestURI = request.getRequestURI();
         servletPath = request.getServletPath();
         if (request instanceof org.apache.catalina.servlet4preview.http.HttpServletRequest) {
-            mapping = ((org.apache.catalina.servlet4preview.http.HttpServletRequest) request).getServletMapping();
+            mapping = ((org.apache.catalina.servlet4preview.http.HttpServletRequest) request).getHttpServletMapping();
         } else {
-            mapping = (new ApplicationMapping(null)).getServletMapping();
+            mapping = (new ApplicationMapping(null)).getHttpServletMapping();
         }
     }
 
@@ -783,7 +783,7 @@ class ApplicationHttpRequest
     }
 
 
-    void setMapping(ServletMapping mapping) {
+    void setMapping(HttpServletMapping mapping) {
         this.mapping = mapping;
     }
 

java/org/apache/catalina/filters/ExpiresFilter.java
@@ -1296,7 +1296,7 @@ public class ExpiresFilter extends FilterBase {
                 org.apache.catalina.servlet4preview.http.HttpServletRequest servlet4Request =
                         (org.apache.catalina.servlet4preview.http.HttpServletRequest) request;
 
-                if (servlet4Request.getServletMapping().getMappingMatch() == MappingMatch.DEFAULT &&
+                if (servlet4Request.getHttpServletMapping().getMappingMatch() == MappingMatch.DEFAULT &&
                         response.getStatus() == HttpServletResponse.SC_NOT_MODIFIED) {
                     // Default servlet normally sets the content type but does not for
                     // 304 responses. Look it up.

java/org/apache/catalina/mapper/MappingData.java
@@ -45,7 +45,7 @@ public class MappingData {
     public final MessageBytes redirectPath = MessageBytes.newInstance();
 
     // Fields used by ApplicationMapping to implement javax.servlet.http.HttpServletMapping
-    public MappingMatch matchType = MappingMatch.UNKNOWN;
+    public MappingMatch matchType = null;
 
     public void recycle() {
         host = null;
@@ -59,6 +59,6 @@ public class MappingData {
         wrapperPath.recycle();
         pathInfo.recycle();
         redirectPath.recycle();
-        matchType = MappingMatch.UNKNOWN;
+        matchType = null;
     }
 }

commit 27adc252a0fc8ad254a053c8c8ce0e0af8e4f73d

Fix NPE regression in 6c03319

java/org/apache/catalina/core/ApplicationMapping.java
@@ -32,42 +32,49 @@ public class ApplicationMapping {
 
     public HttpServletMapping getHttpServletMapping() {
         if (mapping == null) {
-            String servletName;
-            if (mappingData.wrapper == null) {
-                servletName = "";
+            if (mappingData == null) {
+                // This can happen when dispatching from an application provided
+                // request object that does not provide the Servlet 4.0 mapping
+                // data.
+                mapping = new MappingImpl("", "", null, "");
             } else {
-                servletName = mappingData.wrapper.getName();
-            }
-            if (mappingData.matchType == null) {
-                mapping = new MappingImpl("", "", null, servletName);
-            } else {
-                switch (mappingData.matchType) {
-                    case CONTEXT_ROOT:
-                        mapping = new MappingImpl("", "", mappingData.matchType, servletName);
-                        break;
-                    case DEFAULT:
-                        mapping = new MappingImpl("", "/", mappingData.matchType, servletName);
-                        break;
-                    case EXACT:
-                        mapping = new MappingImpl(mappingData.wrapperPath.toString().substring(1),
-                                mappingData.wrapperPath.toString(), mappingData.matchType, servletName);
-                        break;
-                    case EXTENSION:
-                        String path = mappingData.wrapperPath.toString();
-                        int extIndex = path.lastIndexOf('.');
-                        mapping = new MappingImpl(path.substring(1, extIndex),
-                                "*" + path.substring(extIndex), mappingData.matchType, servletName);
-                        break;
-                    case PATH:
-                        String matchValue;
-                        if (mappingData.pathInfo.isNull()) {
-                            matchValue = null;
-                        } else {
-                            matchValue = mappingData.pathInfo.toString().substring(1);
-                        }
-                        mapping = new MappingImpl(matchValue, mappingData.wrapperPath.toString() + "/*",
-                                mappingData.matchType, servletName);
-                        break;
+                String servletName;
+                if (mappingData.wrapper == null) {
+                    servletName = "";
+                } else {
+                    servletName = mappingData.wrapper.getName();
+                }
+                if (mappingData.matchType == null) {
+                    mapping = new MappingImpl("", "", null, servletName);
+                } else {
+                    switch (mappingData.matchType) {
+                        case CONTEXT_ROOT:
+                            mapping = new MappingImpl("", "", mappingData.matchType, servletName);
+                            break;
+                        case DEFAULT:
+                            mapping = new MappingImpl("", "/", mappingData.matchType, servletName);
+                            break;
+                        case EXACT:
+                            mapping = new MappingImpl(mappingData.wrapperPath.toString().substring(1),
+                                    mappingData.wrapperPath.toString(), mappingData.matchType, servletName);
+                            break;
+                        case EXTENSION:
+                            String path = mappingData.wrapperPath.toString();
+                            int extIndex = path.lastIndexOf('.');
+                            mapping = new MappingImpl(path.substring(1, extIndex),
+                                    "*" + path.substring(extIndex), mappingData.matchType, servletName);
+                            break;
+                        case PATH:
+                            String matchValue;
+                            if (mappingData.pathInfo.isNull()) {
+                                matchValue = null;
+                            } else {
+                                matchValue = mappingData.pathInfo.toString().substring(1);
+                            }
+                            mapping = new MappingImpl(matchValue, mappingData.wrapperPath.toString() + "/*",
+                                    mappingData.matchType, servletName);
+                            break;
+                    }
                 }
             }
         }