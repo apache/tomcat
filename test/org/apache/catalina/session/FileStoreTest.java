bf074ae239b15594ace6671668c9d797536cffe8
govi20 <govindasakhare20@gmail.com>
2019-12-01 17:45:24 +0530

    PR #227 Unit test FileStore utility methods

---
+++ test/org/apache/catalina/session/FileStoreTest.java
@@ -0,0 +1,95 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.catalina.session;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.catalina.Manager;
+import org.apache.tomcat.unittest.TesterContext;
+import org.apache.tomcat.unittest.TesterServletContext;
+import org.apache.tomcat.util.http.fileupload.FileUtils;
+import org.junit.*;
+
+/**
+ * Test utility methods of FileStore class
+ *
+ * @author Govinda Sakhare
+ */
+public class FileStoreTest {
+
+    private static final String SESS_TEMPPATH = "SESS_TEMP";
+    private static final File dir = new File(SESS_TEMPPATH);
+    private static FileStore fileStore;
+    private static File file1 = new File(SESS_TEMPPATH + "/tmp1.session");
+    private static File file2 = new File(SESS_TEMPPATH + "/tmp2.session");
+    private static Manager manager = new StandardManager();
+
+
+    @BeforeClass
+    public static void setup() throws IOException {
+        TesterContext testerContext = new TesterContext();
+        testerContext.setServletContext(new TesterServletContext());
+        manager.setContext(testerContext);
+        fileStore = new FileStore();
+        fileStore.setManager(manager);
+    }
+
+
+    @AfterClass
+    public static void cleanup() throws IOException {
+        FileUtils.cleanDirectory(dir);
+        FileUtils.deleteDirectory(dir);
+    }
+
+
+    @Before
+    public void beforeEachTest() throws IOException {
+        fileStore.setDirectory(SESS_TEMPPATH);
+        dir.mkdir();
+        file1.createNewFile();
+        file2.createNewFile();
+    }
+
+
+    @Test
+    public void getSize() throws Exception {
+        Assert.assertEquals(2, fileStore.getSize());
+    }
+
+
+    @Test
+    public void clear() throws Exception {
+        fileStore.clear();
+        Assert.assertEquals(0, fileStore.getSize());
+    }
+
+
+    @Test
+    public void keys() throws Exception {
+        Assert.assertArrayEquals(new String[]{"tmp1", "tmp2"}, fileStore.keys());
+        fileStore.clear();
+        Assert.assertArrayEquals(new String[]{}, fileStore.keys());
+    }
+
+
+    @Test
+    public void removeTest() throws Exception {
+        fileStore.remove("tmp1");
+        Assert.assertEquals(1, fileStore.getSize());
+    }
+}
