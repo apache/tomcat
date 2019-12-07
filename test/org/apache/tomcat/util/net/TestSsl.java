1f9d3e6500fb8543493f20c958dc59cbb3415dfa
god <god@win-10-x64-dev.homeinbox.net>
2019-12-07 13:27:54 +0000

    Refactor wait to make test failure due to timing issues less likely

--- test/org/apache/tomcat/util/net/TestSsl.java
+++ test/org/apache/tomcat/util/net/TestSsl.java
@@ -136,28 +136,17 @@ public class TestSsl extends TomcatBaseTest {
 
         socket.startHandshake();
 
-        // One request should be sufficient
-        int requestCount = 0;
-        int listenerComplete = 0;
-        try {
-            while (requestCount < 10) {
-                requestCount++;
-                doRequest(os, r);
-                Assert.assertTrue("Checking no client issuer has been requested",
-                        TesterSupport.getLastClientAuthRequestedIssuerCount() == 0);
-                if (listener.isComplete() && listenerComplete == 0) {
-                    listenerComplete = requestCount;
-                }
-            }
-        } catch (AssertionError | IOException e) {
-            String message = "Failed on request number " + requestCount
-                    + " after startHandshake(). " + e.getMessage();
-            log.error(message, e);
-            Assert.fail(message);
+        doRequest(os, r);
+        // Handshake complete appears to be called asynchronously
+        int wait = 0;
+        while (wait < 5000 && !listener.isComplete()) {
+            wait += 50;
+            Thread.sleep(50);
         }
-
+        Assert.assertTrue("Checking no client issuer has been requested",
+                TesterSupport.getLastClientAuthRequestedIssuerCount() == 0);
         Assert.assertTrue(listener.isComplete());
-        System.out.println("Renegotiation completed after " + listenerComplete + " requests");
+        System.out.println("Renegotiation completed after " + wait + " ms");
     }
 
     private void doRequest(OutputStream os, Reader r) throws IOException {
