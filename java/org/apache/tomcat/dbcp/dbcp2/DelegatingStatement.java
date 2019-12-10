5cf5c1d6e981d2f9faa4ae3806e84df68a02c99e
Mark Thomas <markt@apache.org>
2019-12-10 11:02:34 +0000

    Additional fix for BZ 63995
    User error in diff tool meant I missed this

--- java/org/apache/tomcat/dbcp/dbcp2/DelegatingStatement.java
+++ java/org/apache/tomcat/dbcp/dbcp2/DelegatingStatement.java
@@ -153,7 +153,6 @@ public class DelegatingStatement extends AbandonedTrace implements Statement {
                             thrownList.add(e);
                         }
                     }
-                    clearTrace();
                 }
                 clearTrace();
             }
