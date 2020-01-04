a00e86375e597f6633039248e13b777fd6e6ecfc
Michael Osipov <michaelo@apache.org>
2020-01-04 14:44:52 +0100

    BZ 64011: JNDIRealm no longer authenticates to LDAP

--- java/org/apache/catalina/realm/RealmBase.java
+++ java/org/apache/catalina/realm/RealmBase.java
@@ -1274,7 +1274,8 @@ public abstract class RealmBase extends LifecycleMBeanBase implements org.apache
      * @param gssCredential the GSS credential of the principal
      * @return the principal associated with the given user name.
      */
-    protected Principal getPrincipal(GSSName gssName, GSSCredential gssCredential) {
+    protected Principal getPrincipal(GSSName gssName,
+            GSSCredential gssCredential) {
         String name = gssName.toString();
 
         if (isStripRealmForGss()) {
