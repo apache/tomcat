75cec48b92d593b0beed19d3befedf86d497edfa
Mark Thomas <markt@apache.org>
2020-02-17 16:57:57 +0000

    Fix (hopefully) intermittent Gump failure
    result needs to be volatile because it is written from the container
    thread and read from the test thread (like done)

--- test/org/apache/catalina/core/TestAsyncContextImpl.java
+++ test/org/apache/catalina/core/TestAsyncContextImpl.java
@@ -307,7 +307,7 @@ public class TestAsyncContextImpl extends TomcatBaseTest {
 
         private volatile boolean done = false;
 
-        private StringBuilder result;
+        private volatile StringBuilder result;
 
         public static final long THREAD_SLEEP_TIME = 1000;
 
