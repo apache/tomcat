9ff6b12c3d7c99b4efc05a686f53e46aa24ad8a1
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2020-02-19 14:35:51 +0200

    Check for non-null 'methods'
    ... because jakarta.servlet.http.HttpServlet#getAllDeclaredMethods(Class) returns 'null' when the class is j.servlet.http.HttpServlet

--- java/jakarta/servlet/http/HttpServlet.java
+++ java/jakarta/servlet/http/HttpServlet.java
@@ -443,29 +443,31 @@ public abstract class HttpServlet extends GenericServlet {
                     boolean allowPut = false;
                     boolean allowDelete = false;
 
-                    for (int i = 0; i < methods.length; i++) {
-                        switch (methods[i].getName()) {
-                            case "doGet": {
-                                allowGet = true;
-                                allowHead = true;
-                                break;
+                    if (methods != null) {
+                        for (final Method method : methods) {
+                            switch (method.getName()) {
+                                case "doGet": {
+                                    allowGet = true;
+                                    allowHead = true;
+                                    break;
+                                }
+                                case "doPost": {
+                                    allowPost = true;
+                                    break;
+                                }
+                                case "doPut": {
+                                    allowPut = true;
+                                    break;
+                                }
+                                case "doDelete": {
+                                    allowDelete = true;
+                                    break;
+                                }
+                                default:
+                                    // NO-OP
                             }
-                            case "doPost": {
-                                allowPost = true;
-                                break;
-                            }
-                            case "doPut": {
-                                allowPut = true;
-                                break;
-                            }
-                            case "doDelete": {
-                                allowDelete = true;
-                                break;
-                            }
-                            default:
-                                // NO-OP
-                        }
 
+                        }
                     }
 
                     StringBuilder allow = new StringBuilder();
