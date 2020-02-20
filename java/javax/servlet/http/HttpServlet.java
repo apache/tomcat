42a5701727cdcebb9e8e8346941d679bcda60eac
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2020-02-20 13:24:48 +0200

    Revert "Check for non-null 'methods'"
    This reverts commit 84abf86f24142f1790b9cf5a31b8cce94822c8ce.

--- java/javax/servlet/http/HttpServlet.java
+++ java/javax/servlet/http/HttpServlet.java
@@ -502,21 +502,19 @@ public abstract class HttpServlet extends GenericServlet {
         }
         // End of Tomcat specific hack
 
-        if (methods != null) {
-            for (int i = 0; i < methods.length; i++) {
-                Method m = methods[i];
+        for (int i=0; i<methods.length; i++) {
+            Method m = methods[i];
 
-                if (m.getName().equals("doGet")) {
-                    ALLOW_GET = true;
-                    ALLOW_HEAD = true;
-                }
-                if (m.getName().equals("doPost"))
-                    ALLOW_POST = true;
-                if (m.getName().equals("doPut"))
-                    ALLOW_PUT = true;
-                if (m.getName().equals("doDelete"))
-                    ALLOW_DELETE = true;
+            if (m.getName().equals("doGet")) {
+                ALLOW_GET = true;
+                ALLOW_HEAD = true;
             }
+            if (m.getName().equals("doPost"))
+                ALLOW_POST = true;
+            if (m.getName().equals("doPut"))
+                ALLOW_PUT = true;
+            if (m.getName().equals("doDelete"))
+                ALLOW_DELETE = true;
         }
 
         String allow = null;
