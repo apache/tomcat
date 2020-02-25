19be442046f3fb4fcf18315d460a17bd433bb9f5
Mark Thomas <markt@apache.org>
2020-02-25 14:16:44 +0000

    BZ 64166. HttpServletResponse.getHeaderNames() now returns unique names
    https://bz.apache.org/bugzilla/show_bug.cgi?id=64166

--- java/org/apache/catalina/connector/Response.java
+++ java/org/apache/catalina/connector/Response.java
@@ -30,8 +30,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Enumeration;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Locale;
+import java.util.Set;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.ServletResponse;
@@ -882,9 +884,8 @@ public class Response implements HttpServletResponse {
 
     @Override
     public Collection<String> getHeaders(String name) {
-        Enumeration<String> enumeration =
-                getCoyoteResponse().getMimeHeaders().values(name);
-        List<String> result = new ArrayList<>();
+        Enumeration<String> enumeration = getCoyoteResponse().getMimeHeaders().values(name);
+        Set<String> result = new LinkedHashSet<>();
         while (enumeration.hasMoreElements()) {
             result.add(enumeration.nextElement());
         }
