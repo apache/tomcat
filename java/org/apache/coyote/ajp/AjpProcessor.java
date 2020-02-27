f30d7cd8979d340d062d26a9b1dc896b51f481f9
remm <remm@apache.org>
2020-02-27 10:27:20 +0100

    Ignore zero length secret
    No doc fix needed, secretRequired was already well described.

--- java/org/apache/coyote/ajp/AjpProcessor.java
+++ java/org/apache/coyote/ajp/AjpProcessor.java
@@ -851,7 +851,7 @@ public class AjpProcessor extends AbstractProcessor {
 
             case Constants.SC_A_SECRET:
                 requestHeaderMessage.getBytes(tmpMB);
-                if (secret != null) {
+                if (secret != null && secret.length() > 0) {
                     secretPresentInRequest = true;
                     if (!tmpMB.equals(secret)) {
                         response.setStatus(403);
@@ -869,7 +869,7 @@ public class AjpProcessor extends AbstractProcessor {
         }
 
         // Check if secret was submitted if required
-        if ((secret != null) && !secretPresentInRequest) {
+        if (secret != null && secret.length() > 0 && !secretPresentInRequest) {
             response.setStatus(403);
             setErrorState(ErrorState.CLOSE_CLEAN, null);
         }
