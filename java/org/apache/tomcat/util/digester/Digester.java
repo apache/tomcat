5ee4720ea9ca5701ac306caafa56ea41ec23372f
Mark Thomas <markt@apache.org>
2020-02-28 11:16:18 +0000

    Remove deprecated code

--- java/org/apache/tomcat/util/digester/Digester.java
+++ java/org/apache/tomcat/util/digester/Digester.java
@@ -145,18 +145,6 @@ public class Digester extends DefaultHandler2 {
         }
     }
 
-    /**
-     * A {@link org.apache.tomcat.util.IntrospectionUtils.SecurePropertySource}
-     * that uses environment variables to resolve expressions. Still available
-     * for backwards compatibility.
-     *
-     * @deprecated Use {@link org.apache.tomcat.util.digester.EnvironmentPropertySource}
-     *             This will be removed in Tomcat 10 onwards.
-     */
-    @Deprecated
-    public static class EnvironmentPropertySource extends org.apache.tomcat.util.digester.EnvironmentPropertySource {
-    }
-
 
     protected IntrospectionUtils.PropertySource source[] = new IntrospectionUtils.PropertySource[] {
             new SystemPropertySource() };
