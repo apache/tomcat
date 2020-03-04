b896830c9035b044d2b0b1274b4737b07a40da31
remm <remm@apache.org>
2020-03-04 22:33:46 +0100

    Jakarta to javax

--- test/org/apache/tomcat/util/net/TestSsl.java
+++ test/org/apache/tomcat/util/net/TestSsl.java
@@ -32,10 +32,10 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.junit.Assert;
 import org.junit.Assume;
