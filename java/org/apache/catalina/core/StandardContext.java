6e0ae64989d9f71176503f48382c736eee76f7f6
Mark Thomas <markt@apache.org>
2020-03-04 19:11:14 +0000

    Provide an accessor for inProgressAsyncCount. Helps embedded scenarios.
    Implementing clean shutdown in embedded scenarios is aided by easier
    access to this value.

--- java/org/apache/catalina/core/StandardContext.java
+++ java/org/apache/catalina/core/StandardContext.java
@@ -854,6 +854,11 @@ public class StandardContext extends ContainerBase
     }
 
 
+    public long getInProgressAsyncCount() {
+        return inProgressAsyncCount.get();
+    }
+
+
     @Override
     public void setAllowMultipleLeadingForwardSlashInPath(
             boolean allowMultipleLeadingForwardSlashInPath) {
