9cb1998dc42dc575fc680d1a4b1e029b02d7a582
Mark Thomas <markt@apache.org>
2020-03-10 12:43:46 +0000

    Update test client to handle HTTP 0.9 requests and responses

--- test/org/apache/catalina/startup/SimpleHttpClient.java
+++ test/org/apache/catalina/startup/SimpleHttpClient.java
@@ -90,6 +90,7 @@ public abstract class SimpleHttpClient {
     private String[] request;
     private boolean useContinue = false;
     private boolean useCookies = true;
+    private boolean useHttp09 = false;
     private int requestPause = 1000;
 
     private String responseLine;
@@ -127,6 +128,10 @@ public abstract class SimpleHttpClient {
 
     public boolean getUseCookies() {
         return useCookies;
+    }
+
+    public void setUseHttp09(boolean theUseHttp09Flag) {
+        useHttp09 = theUseHttp09Flag;
     }
 
     public void setRequestPause(int theRequestPause) {
@@ -237,23 +242,26 @@ public abstract class SimpleHttpClient {
             bodyUriElements.clear();
         }
 
-        // Read the response status line
-        responseLine = readLine();
+        // HTTP 0.9 has neither response line nor headers
+        if (!useHttp09) {
+            // Read the response status line
+            responseLine = readLine();
 
-        // Is a 100 continue response expected?
-        if (useContinue) {
-            if (isResponse100()) {
-                // Skip the blank after the 100 Continue response
-                readLine();
-                // Now get the final response
-                responseLine = readLine();
-            } else {
-                throw new IOException("No 100 Continue response");
+            // Is a 100 continue response expected?
+            if (useContinue) {
+                if (isResponse100()) {
+                    // Skip the blank after the 100 Continue response
+                    readLine();
+                    // Now get the final response
+                    responseLine = readLine();
+                } else {
+                    throw new IOException("No 100 Continue response");
+                }
             }
-        }
 
-        // Put the headers into a map, and process interesting ones
-        processHeaders();
+            // Put the headers into a map, and process interesting ones
+            processHeaders();
+        }
 
         // Read the body, if requested and if one exists
         processBody(wantBody);
