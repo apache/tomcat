c515f52cfbc2c46fd4aedafd7d995775240f2675
Mark Thomas <markt@apache.org>
2020-03-16 16:03:12 +0000

    Deprecated unused method

--- java/org/apache/tomcat/util/buf/UDecoder.java
+++ java/org/apache/tomcat/util/buf/UDecoder.java
@@ -298,7 +298,10 @@ public final class UDecoder {
      * @return the decoded string
      * @exception IllegalArgumentException if a '%' character is not followed
      * by a valid 2-digit hexadecimal number
+     *
+     * @deprecated Unused. This will be removed in Tomcat 10 onwards
      */
+    @Deprecated
     public static String URLDecode(String str) {
         return URLDecode(str, StandardCharsets.UTF_8);
     }
