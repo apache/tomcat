a02375cfe67eb8b7ce6f71a5f5e052a5f3755f73
Mark Thomas <markt@apache.org>
2020-05-05 09:48:23 +0100

    Deprecate get/set attribute

--- java/org/apache/catalina/connector/Connector.java
+++ java/org/apache/catalina/connector/Connector.java
@@ -314,7 +314,11 @@ public class Connector extends LifecycleMBeanBase  {
      *
      * @param name the property name
      * @return the property value
+     *
+     * @deprecated Use {@link #getProperty(String)}. This will be removed in
+     *             Tomcat 10 onwards.
      */
+    @Deprecated
     public Object getAttribute(String name) {
         return getProperty(name);
     }
@@ -325,7 +329,11 @@ public class Connector extends LifecycleMBeanBase  {
      *
      * @param name the property name
      * @param value the property value
+     *
+     * @deprecated Use {@link #setAttribute(String, Object)}. This will be
+     *             removed in Tomcat 10 onwards.
      */
+    @Deprecated
     public void setAttribute(String name, Object value) {
         setProperty(name, String.valueOf(value));
     }
