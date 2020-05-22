9a0231683a77e2957cea0fdee88b193b30b0c976
Mark Thomas <markt@apache.org>
2020-05-22 11:27:49 +0100

    Fix BZ 64467. Improve performance of closing idle streams

--- java/org/apache/coyote/http2/Http2UpgradeHandler.java
+++ java/org/apache/coyote/http2/Http2UpgradeHandler.java
@@ -1473,11 +1473,11 @@ class Http2UpgradeHandler extends AbstractStream implements InternalHttpUpgradeH
     }
 
 
-    private void closeIdleStreams(int newMaxActiveRemoteStreamId) throws Http2Exception {
-        for (int i = maxActiveRemoteStreamId + 2; i < newMaxActiveRemoteStreamId; i += 2) {
-            Stream stream = getStream(i, false);
-            if (stream != null) {
-                stream.closeIfIdle();
+    private void closeIdleStreams(int newMaxActiveRemoteStreamId) {
+        for (Entry<Integer,Stream> entry : streams.entrySet()) {
+            if (entry.getKey().intValue() > maxActiveRemoteStreamId &&
+                    entry.getKey().intValue() < newMaxActiveRemoteStreamId) {
+                entry.getValue().closeIfIdle();
             }
         }
         maxActiveRemoteStreamId = newMaxActiveRemoteStreamId;
