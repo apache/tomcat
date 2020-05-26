7447876b5ac2e63b7e8bc0e50d040ec811bfa4c2
Mark Thomas <markt@apache.org>
2020-05-26 17:24:00 +0100

    Remove explicit exception declaration not in spec API.
    When testing with Java 8 this doesn't matter but when testing with Java
    11 this triggers a TCK failure

--- java/javax/servlet/jsp/JspApplicationContext.java
+++ java/javax/servlet/jsp/JspApplicationContext.java
@@ -65,7 +65,7 @@ public interface JspApplicationContext {
      *             if called after the application's
      *             <code>ServletContextListeners</code> have been initialized.
      */
-    public void addELResolver(ELResolver resolver) throws IllegalStateException;
+    public void addELResolver(ELResolver resolver);
 
     /**
      * <p>
