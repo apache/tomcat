34341e447e872add752c2aa0605ddef4e622b839
remm <remm@apache.org>
2020-06-04 17:05:22 +0200

    Fix Tomcat 8.5 issue

--- test/org/apache/tomcat/util/net/TesterSupport.java
+++ test/org/apache/tomcat/util/net/TesterSupport.java
@@ -220,7 +220,7 @@ public final class TesterSupport {
         return ks;
     }
 
-    protected static boolean isRenegotiationSupported(Tomcat tomcat) {
+    public static boolean isRenegotiationSupported(Tomcat tomcat) {
         String protocol = tomcat.getConnector().getProtocolHandlerClassName();
         if (protocol.contains("Apr")) {
             // Disabled by default in 1.1.20 windows binary (2010-07-27)
