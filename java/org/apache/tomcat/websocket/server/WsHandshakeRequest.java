apache/tomcat

commit e09e6b04e41d3dae27fcfe060e6f8f89f1c0d6b5

Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=62731 Make the URI returned by HandshakeRequest.getRequestURI() and Session.getRequestURI() absolute so that the scheme, host and port are accessible.
git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1842203 13f79535-47bb-0310-9956-ffa450edef68

java/org/apache/tomcat/websocket/server/WsHandshakeRequest.java
@@ -31,11 +31,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.websocket.server.HandshakeRequest;
 
 import org.apache.tomcat.util.collections.CaseInsensitiveKeyMap;
+import org.apache.tomcat.util.res.StringManager;
 
 /**
  * Represents the request that this session was opened under.
  */
 public class WsHandshakeRequest implements HandshakeRequest {
+
+    private static final StringManager sm = StringManager.getManager(WsHandshakeRequest.class);
 
     private final URI requestUri;
     private final Map<String,List<String>> parameterMap;
@@ -54,18 +57,7 @@ public class WsHandshakeRequest implements HandshakeRequest {
         queryString = request.getQueryString();
         userPrincipal = request.getUserPrincipal();
         httpSession = request.getSession(false);
-
-        // URI
-        StringBuilder sb = new StringBuilder(request.getRequestURI());
-        if (queryString != null) {
-            sb.append("?");
-            sb.append(queryString);
-        }
-        try {
-            requestUri = new URI(sb.toString());
-        } catch (URISyntaxException e) {
-            throw new IllegalArgumentException(e);
-        }
+        requestUri = buildRequestUri(request);
 
         // ParameterMap
         Map<String,String[]> originalParameters = request.getParameterMap();
@@ -146,5 +138,54 @@ public class WsHandshakeRequest implements HandshakeRequest {
      */
     void finished() {
         request = null;
+    }
+
+
+    /*
+     * See RequestUtil.getRequestURL()
+     */
+    private static URI buildRequestUri(HttpServletRequest req) {
+
+        StringBuffer uri = new StringBuffer();
+        String scheme = req.getScheme();
+        int port = req.getServerPort();
+        if (port < 0) {
+            // Work around java.net.URL bug
+            port = 80;
+        }
+
+        if ("http".equals(scheme)) {
+            uri.append("ws");
+        } else if ("https".equals(scheme)) {
+            uri.append("wss");
+        } else {
+            // Should never happen
+            throw new IllegalArgumentException(
+                    sm.getString("wsHandshakeRequest.unknownScheme", scheme));
+        }
+
+        uri.append("://");
+        uri.append(req.getServerName());
+
+        if ((scheme.equals("http") && (port != 80))
+            || (scheme.equals("https") && (port != 443))) {
+            uri.append(':');
+            uri.append(port);
+        }
+
+        uri.append(req.getRequestURI());
+
+        if (req.getQueryString() != null) {
+            uri.append("?");
+            uri.append(req.getQueryString());
+        }
+
+        try {
+            return new URI(uri.toString());
+        } catch (URISyntaxException e) {
+            // Should never happen
+            throw new IllegalArgumentException(
+                    sm.getString("wsHandshakeRequest.invalidUri", uri.toString()), e);
+        }
     }
 }

java/org/apache/tomcat/websocket/LocalStrings.properties
@@ -77,6 +77,9 @@ wsFrame.wrongRsv=The client frame set the reserved bits to [{0}] for a message w
 
 wsFrameClient.ioe=Failure while reading data sent by server
 
+wsHandshakeRequest.invalidUri=The string [{0}] cannot be used to construct a valid URI
+wsHandshakeRequest.unknownScheme=The scheme [{0}] in the request is not recognised
+
 wsRemoteEndpoint.acquireTimeout=The current message was not fully sent within the specified timeout
 wsRemoteEndpoint.closed=Message will not be sent because the WebSocket session has been closed
 wsRemoteEndpoint.closedDuringMessage=The remainder of the message will not be sent because the WebSocket session has been closed

commit ac169e0f45d21bcd29f387a275f479b7d267909b

support of schemes "wss" and "ws" in WsHandshakeRequest.buildRequestUri() requests with scheme "wss" or "ws", as outlined in [RFC 6455 section 11.1](https://tools.ietf.org/html/rfc6455#section-11.1), will cause an `IllegalArgumentException` with message "wsHandshakeRequest.unknownScheme"
this fixes a regression from e09e6b04e41d3dae27fcfe060e6f8f89f1c0d6b5 / BZ 62731

java/org/apache/tomcat/websocket/server/WsHandshakeRequest.java
@@ -156,6 +156,8 @@ public class WsHandshakeRequest implements HandshakeRequest {
             uri.append("ws");
         } else if ("https".equals(scheme)) {
             uri.append("wss");
+        } else if ("wss".equals(scheme) || "ws".equals(scheme)) {
+            uri.append(scheme);
         } else {
             // Should never happen
             throw new IllegalArgumentException(
@@ -166,6 +168,8 @@ public class WsHandshakeRequest implements HandshakeRequest {
         uri.append(req.getServerName());
 
         if ((scheme.equals("http") && (port != 80))
+            || (scheme.equals("ws") && (port != 80))
+            || (scheme.equals("wss") && (port != 443))
             || (scheme.equals("https") && (port != 443))) {
             uri.append(':');
             uri.append(port);