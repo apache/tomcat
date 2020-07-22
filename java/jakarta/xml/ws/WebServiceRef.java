f5efdadaf9c468c452dbee99831b20021f37495c
Mark Thomas <markt@apache.org>
2020-07-22 13:17:23 +0100

    Complete javax -> jakarta rename.

--- java/jakarta/xml/ws/WebServiceRef.java
+++ java/jakarta/xml/ws/WebServiceRef.java
@@ -14,9 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-
-package javax.xml.ws;
+package jakarta.xml.ws;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
