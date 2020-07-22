87cbed1244205fb112f421449482d02b56f6167b
Filip Hanik <fhanik@pivotal.io>
2020-07-22 11:40:14 -0700

    Revert "Simpler way to determine Graal runtime"
    This reverts commit 6a73695fa6d024ed9fc4adeb32073cbd94309c51.
    https://tomcat.markmail.org/search/?q=#query:%20list%3Aorg.apache.tomcat.dev+page:1+mid:7vo7ugmqjz2z7x5f+state:results

--- java/org/apache/tomcat/util/compat/GraalCompat.java
+++ java/org/apache/tomcat/util/compat/GraalCompat.java
@@ -20,7 +20,20 @@ import java.io.IOException;
 
 class GraalCompat extends Jre9Compat {
 
-    private static final boolean GRAAL = System.getProperty("org.graalvm.nativeimage.imagecode") != null;
+    private static final boolean GRAAL;
+
+    static {
+        boolean result = false;
+        try {
+            Class<?> nativeImageClazz = Class.forName("org.graalvm.nativeimage.ImageInfo");
+            result = Boolean.TRUE.equals(nativeImageClazz.getMethod("inImageCode").invoke(null));
+        } catch (ClassNotFoundException e) {
+            // Must be Graal
+        } catch (ReflectiveOperationException | IllegalArgumentException e) {
+            // Should never happen
+        }
+        GRAAL = result;
+    }
 
     static boolean isSupported() {
         // This property does not exist for a native image
