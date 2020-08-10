bf3bff96e7e2977565c8e7509aaf133c2d044787
Mark Thomas <markt@apache.org>
2020-08-10 17:02:47 +0100

    Add some Javadoc to silence a handful of IDE warnings

--- java/org/apache/tomcat/util/XReflectionIntrospectionUtils.java
+++ java/org/apache/tomcat/util/XReflectionIntrospectionUtils.java
@@ -22,10 +22,28 @@ final class XReflectionIntrospectionUtils {
         return false;
     }
 
+    /**
+     * Always throws {@link UnsupportedOperationException}
+     *
+     * @param o     Unused
+     * @param name  Unused
+     *
+     * @return Never returns normally
+     */
     static Object getPropertyInternal(Object o, String name) {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Always throws {@link UnsupportedOperationException}
+     *
+     * @param o                 Unused
+     * @param name              Unused
+     * @param value             Unused
+     * @param invokeSetProperty Unused
+     *
+     * @return Never returns normally
+     */
     static boolean setPropertyInternal(Object o, String name, String value, boolean invokeSetProperty) {
         throw new UnsupportedOperationException();
     }
