2f19d166d4f095d0715bcefd294e87a83f7661ac
Mark Thomas <markt@apache.org>
2020-08-10 17:19:52 +0100

    Refactor Default servlet to simplify support for custom ETag formats

--- java/org/apache/catalina/servlets/WebdavServlet.java
+++ java/org/apache/catalina/servlets/WebdavServlet.java
@@ -1994,7 +1994,7 @@ public class WebdavServlet extends DefaultServlet {
         generatePropFindResponse(generatedXML, rewrittenUrl, path, type, propertiesVector,
                 resource.isFile(), false, resource.getCreation(), resource.getLastModified(),
                 resource.getContentLength(), getServletContext().getMimeType(resource.getName()),
-                resource.getETag());
+                generateETag(resource));
     }
 
 
