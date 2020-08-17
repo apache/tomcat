7e65db453b8a7740de48343c12a8a91db96ad79b
Mark Thomas <markt@apache.org>
2020-08-17 15:32:10 +0100

    Remove the debug code. Add explicit mapping for jsp-file defined servlet

--- test/org/apache/jasper/compiler/TestParserNoStrictWhitespace.java
+++ test/org/apache/jasper/compiler/TestParserNoStrictWhitespace.java
@@ -14,18 +14,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.jasper.compiler;
-
-import java.io.File;
-
-import jakarta.servlet.ServletContext;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-import org.apache.catalina.core.StandardContext;
-import org.apache.catalina.core.StandardWrapper;
 import org.apache.catalina.startup.TomcatBaseTest;
 import org.apache.tomcat.util.buf.ByteChunk;
 
@@ -40,33 +33,13 @@ public class TestParserNoStrictWhitespace extends TomcatBaseTest {
     public void testBug49297NoSpaceNotStrict() throws Exception {
         getTomcatInstanceTestWebapp(false, true);
 
-        // Github / Travis / s390x debug code
-        StandardContext ctxt = (StandardContext) getTomcatInstance().getHost().findChild("/test");
-        StandardWrapper w = (StandardWrapper) ctxt.findChild("bug49297NoSpace");
-        System.out.println("JSP strict whitespace: [" +  w.getInitParameter("strictWhitespace") + "]");
-
-        File tmpJsp = new File("test/webapp/bug49nnn/bug49297NoSpace.jsp");
-        System.out.println(".jsp [" + tmpJsp.lastModified() + "]");
-
-        File tmp = (File) ctxt.getServletContext().getAttribute(ServletContext.TEMPDIR);
-        File tmpJava = new File(tmp, "org/apache/jsp/bug49nnn/bug49297NoSpace_jsp.java");
-        File tmpClass = new File(tmp, "org/apache/jsp/bug49nnn/bug49297NoSpace_jsp.class");
-        System.out.println("before request  .java [" + tmpJava.lastModified() + "]");
-        System.out.println("before request .class [" + tmpClass.lastModified() + "]");
-        // debug code end
-
         ByteChunk res = new ByteChunk();
-        int sc = getUrl("http://localhost:" + getPort() +
-                "/test/bug49nnn/bug49297NoSpace.jsp", res, null);
-
-        // Github / Travis / s390x debug code
-        System.out.println("after request  .java [" + tmpJava.lastModified() + "]");
-        System.out.println("after request .class [" + tmpClass.lastModified() + "]");
-        // debug code end
+        int sc = getUrl("http://localhost:" + getPort() + "/test/bug49nnn/bug49297NoSpace.jsp", res, null);
 
         Assert.assertEquals(200, sc);
         assertEcho(res.toString(), "Hello World");
     }
+
 
     /** Assertion for text printed by tags:echo */
     private static void assertEcho(String result, String expected) {
