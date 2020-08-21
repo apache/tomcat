198d63b2ad2ce564c06fb597304d85ab9e350a8c
Mark Thomas <markt@apache.org>
2020-08-21 16:34:10 +0100

    Avoid possible NPE when stream is closed

--- java/org/apache/coyote/http2/Stream.java
+++ java/org/apache/coyote/http2/Stream.java
@@ -594,16 +594,30 @@ public class Stream extends AbstractStream implements HeaderEmitter {
 
 
     final void receivedEndOfStream() throws ConnectionException {
-        long contentLengthHeader = coyoteRequest.getContentLengthLong();
-        if (contentLengthHeader > -1 && contentLengthReceived != contentLengthHeader) {
+        if (isContentLengthInconsistent()) {
             throw new ConnectionException(sm.getString("stream.header.contentLength",
-                    getConnectionId(), getIdentifier(), Long.valueOf(contentLengthHeader),
+                    getConnectionId(), getIdentifier(),
+                    Long.valueOf(coyoteRequest.getContentLengthLong()),
                     Long.valueOf(contentLengthReceived)), Http2Error.PROTOCOL_ERROR);
         }
         state.receivedEndOfStream();
         if (inputBuffer != null) {
             inputBuffer.notifyEof();
         }
+    }
+
+
+    final boolean isContentLengthInconsistent() {
+        Request coyoteRequest = this.coyoteRequest;
+        // May be null when processing trailer headers after stream has been
+        // closed.
+        if (coyoteRequest != null) {
+            long contentLengthHeader = coyoteRequest.getContentLengthLong();
+            if (contentLengthHeader > -1 && contentLengthReceived != contentLengthHeader) {
+                return true;
+            }
+        }
+        return false;
     }
 
 
