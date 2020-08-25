91fac278b92ad33136ec6825fa557a19f426e808
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2020-08-25 14:28:11 +0300

    Check earlier for valid first character
    Move the check for the possible characters in the first position

--- java/org/apache/catalina/connector/CoyoteAdapter.java
+++ java/org/apache/catalina/connector/CoyoteAdapter.java
@@ -1272,6 +1272,12 @@ public class CoyoteAdapter implements Adapter {
         int pos = 0;
         int index = 0;
 
+
+        // The URL must start with '/' (or '\' that will be replaced soon)
+        if (b[start] != (byte) '/' && b[start] != (byte) '\\') {
+            return false;
+        }
+
         // Replace '\' with '/'
         // Check for null byte
         for (pos = start; pos < end; pos++) {
@@ -1285,11 +1291,6 @@ public class CoyoteAdapter implements Adapter {
             if (b[pos] == (byte) 0) {
                 return false;
             }
-        }
-
-        // The URL must start with '/'
-        if (b[start] != (byte) '/') {
-            return false;
         }
 
         // Replace "//" with "/"
