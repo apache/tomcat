20181bb9b7dc6d9407dd3c9430c4d79f880c1027
Mark Thomas <markt@apache.org>
2020-09-15 14:44:40 +0100

    Deprecate JDBCRealm

--- java/org/apache/catalina/realm/DataSourceRealm.java
+++ java/org/apache/catalina/realm/DataSourceRealm.java
@@ -33,7 +33,7 @@ import org.apache.naming.ContextBindings;
 /**
 *
 * Implementation of <b>Realm</b> that works with any JDBC JNDI DataSource.
-* See the JDBCRealm.howto for more details on how to set up the database and
+* See the Realm How-To for more details on how to set up the database and
 * for configuration options.
 *
 * @author Glenn L. Nielsen
