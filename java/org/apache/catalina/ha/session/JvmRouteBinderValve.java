d7c4d4652c4a136a6bd26245273c9bf572406b9e
Mark Thomas <markt@apache.org>
2020-09-16 19:25:33 +0100

    Fix spelling
    No need for backwards compatibility changes. The new name is now
    consistent with the documentation. Any users relying on the old spelling
    can configure the attribute name explicitly.

--- java/org/apache/catalina/ha/session/JvmRouteBinderValve.java
+++ java/org/apache/catalina/ha/session/JvmRouteBinderValve.java
@@ -116,7 +116,7 @@ public class JvmRouteBinderValve extends ValveBase implements ClusterValve {
      */
     protected long numberOfSessions = 0;
 
-    protected String sessionIdAttribute = "org.apache.catalina.ha.session.JvmRouteOrignalSessionID";
+    protected String sessionIdAttribute = "org.apache.catalina.ha.session.JvmRouteOriginalSessionID";
 
 
     /*--Logic---------------------------------------------------*/
