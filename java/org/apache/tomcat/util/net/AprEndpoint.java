71bb6d546927212224204eef4cd4ef911b469d97
Mark Thomas <markt@apache.org>
2020-09-16 18:04:39 +0100

    Fix spellings in docs
    Based on a PR provided by John Bampton

--- java/org/apache/tomcat/util/net/AprEndpoint.java
+++ java/org/apache/tomcat/util/net/AprEndpoint.java
@@ -2169,7 +2169,7 @@ public class AprEndpoint extends AbstractEndpoint<Long> {
          * will be handled asynchronously inside the kernel. As a result,
          * the poller will never be used.
          *
-         * @param data containing the reference to the data which should be snet
+         * @param data containing the reference to the data which should be sent
          * @return true if all the data has been sent right away, and false
          *              otherwise
          */
