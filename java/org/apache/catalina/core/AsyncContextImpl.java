78d9c76333214c752ebdd108677925dad6bdc585
Mark Thomas <markt@apache.org>
2020-09-22 14:40:58 +0100

    Fix double counting in tracking of in-flight async requests

--- java/org/apache/catalina/core/AsyncContextImpl.java
+++ java/org/apache/catalina/core/AsyncContextImpl.java
@@ -113,7 +113,6 @@ public class AsyncContextImpl implements AsyncContext, AsyncContextCallback {
         } finally {
             context.fireRequestDestroyEvent(request.getRequest());
             clearServletRequestResponse();
-            this.context.decrementInProgressAsyncCount();
             context.unbind(Globals.IS_SECURITY_ENABLED, oldCL);
         }
     }
@@ -206,16 +205,10 @@ public class AsyncContextImpl implements AsyncContext, AsyncContextCallback {
                     (AsyncDispatcher) requestDispatcher;
             final ServletRequest servletRequest = getRequest();
             final ServletResponse servletResponse = getResponse();
-            // https://bz.apache.org/bugzilla/show_bug.cgi?id=63246
-            // Take a local copy as the dispatch may complete the
-            // request/response and that in turn may trigger recycling of this
-            // object before the in-progress count can be decremented
-            final Context context = this.context;
             this.dispatch = new AsyncRunnable(
                     request, applicationDispatcher, servletRequest, servletResponse);
             this.request.getCoyoteRequest().action(ActionCode.ASYNC_DISPATCH, null);
             clearServletRequestResponse();
-            context.decrementInProgressAsyncCount();
         }
     }
 
@@ -454,6 +447,18 @@ public class AsyncContextImpl implements AsyncContext, AsyncContextCallback {
                 complete();
             }
         }
+    }
+
+
+    @Override
+    public void incrementInProgressAsyncCount() {
+        context.incrementInProgressAsyncCount();
+    }
+
+
+    @Override
+    public void decrementInProgressAsyncCount() {
+        context.decrementInProgressAsyncCount();
     }
 
 
--- java/org/apache/coyote/AsyncStateMachine.java
+++ java/org/apache/coyote/AsyncStateMachine.java
@@ -235,6 +235,9 @@ public class AsyncStateMachine {
         if (state == AsyncState.DISPATCHED) {
             generation.incrementAndGet();
             state = AsyncState.STARTING;
+            // Note: In this instance, caller is responsible for calling
+            // asyncCtxt.incrementInProgressAsyncCount() as that allows simpler
+            // error handling.
             this.asyncCtxt = asyncCtxt;
             lastAsyncStart = System.currentTimeMillis();
         } else {
@@ -274,12 +277,14 @@ public class AsyncStateMachine {
         } else if (state == AsyncState.MUST_COMPLETE || state == AsyncState.COMPLETING) {
             asyncCtxt.fireOnComplete();
             state = AsyncState.DISPATCHED;
+            asyncCtxt.decrementInProgressAsyncCount();
             return SocketState.ASYNC_END;
         } else if (state == AsyncState.MUST_DISPATCH) {
             state = AsyncState.DISPATCHING;
             return SocketState.ASYNC_END;
         } else if (state == AsyncState.DISPATCHING) {
             state = AsyncState.DISPATCHED;
+            asyncCtxt.decrementInProgressAsyncCount();
             return SocketState.ASYNC_END;
         } else if (state == AsyncState.STARTED) {
             // This can occur if an async listener does a dispatch to an async
@@ -401,6 +406,7 @@ public class AsyncStateMachine {
         if (state == AsyncState.DISPATCHING ||
                 state == AsyncState.MUST_DISPATCH) {
             state = AsyncState.DISPATCHED;
+            asyncCtxt.decrementInProgressAsyncCount();
         } else {
             throw new IllegalStateException(
                     sm.getString("asyncStateMachine.invalidAsyncState",
@@ -413,6 +419,12 @@ public class AsyncStateMachine {
         clearNonBlockingListeners();
         if (state == AsyncState.STARTING) {
             state = AsyncState.MUST_ERROR;
+        } else if (state == AsyncState.DISPATCHED) {
+            // Async error handling has moved processing back into an async
+            // state. Need to increment in progress count as it will decrement
+            // when the async state is exited again.
+            asyncCtxt.incrementInProgressAsyncCount();
+            state = AsyncState.ERROR;
         } else {
             state = AsyncState.ERROR;
         }
