e92a2d469ce9a44401c836d61e77d80474bafec9
Igal Sapir <isapir@apache.org>
2020-10-03 11:24:58 -0700

    Added Javadoc comments for Java 15

--- java/javax/annotation/Priority.java
+++ java/javax/annotation/Priority.java
@@ -29,5 +29,9 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Priority {
+
+    /**
+     * @return the int value
+     */
     int value();
 }
--- java/javax/annotation/security/RolesAllowed.java
+++ java/javax/annotation/security/RolesAllowed.java
@@ -29,5 +29,9 @@ import java.lang.annotation.Target;
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RolesAllowed {
+
+    /**
+     * @return a String[] of the allowed roles
+     */
     public String[] value();
 }
