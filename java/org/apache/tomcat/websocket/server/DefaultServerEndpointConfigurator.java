d0b4638662b6d0d87b6f137823e4155025e748f8
Mark Thomas <markt@apache.org>
2020-11-06 10:01:49 +0000

    Add JPMS metadata for use of ServiceProvider API by WebSocket

--- java/org/apache/tomcat/websocket/server/DefaultServerEndpointConfigurator.java
+++ java/org/apache/tomcat/websocket/server/DefaultServerEndpointConfigurator.java
@@ -26,6 +26,7 @@ import javax.websocket.HandshakeResponse;
 import javax.websocket.server.HandshakeRequest;
 import javax.websocket.server.ServerEndpointConfig;
 
+@aQute.bnd.annotation.spi.ServiceProvider(value=ServerEndpointConfig.Configurator.class)
 public class DefaultServerEndpointConfigurator
         extends ServerEndpointConfig.Configurator {
 
