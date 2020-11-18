3285734aa74f470047a502cee85487c6431d2863
Mark Thomas <markt@apache.org>
2020-11-18 17:10:49 +0000

    Switch to new, non-deprecated method

--- java/org/apache/tomcat/util/net/NioEndpoint.java
+++ java/org/apache/tomcat/util/net/NioEndpoint.java
@@ -238,7 +238,7 @@ public class NioEndpoint extends AbstractJsseEndpoint<NioChannel,SocketChannel> 
         }
         if (running) {
             running = false;
-            acceptor.stop();
+            acceptor.stop(10);
             if (poller != null) {
                 poller.destroy();
                 poller = null;
