a6b67811ef62d475d095166c63f4d9bcab54f15b
Mark Thomas <markt@apache.org>
2020-12-10 13:53:17 +0000

    Update deprecation as code is shared between Tomcat 10 and Tomcat Native

--- java/org/apache/tomcat/jni/File.java
+++ java/org/apache/tomcat/jni/File.java
@@ -24,8 +24,9 @@ import java.nio.ByteBuffer;
  * @author Mladen Turk
  *
  * @deprecated  The scope of the APR/Native Library will be reduced in Tomcat
- *              10.1.x onwards to only those components required to provide
- *              OpenSSL integration with the NIO and NIO2 connectors.
+ *              10.1.x / Tomcat Native 2.x onwards to only include those
+ *              components required to provide OpenSSL integration with the NIO
+ *              and NIO2 connectors.
  */
 @Deprecated
 public class File {
--- java/org/apache/tomcat/jni/PoolCallback.java
+++ java/org/apache/tomcat/jni/PoolCallback.java
@@ -22,8 +22,9 @@ package org.apache.tomcat.jni;
  * @author Mladen Turk
  *
  * @deprecated  The scope of the APR/Native Library will be reduced in Tomcat
- *              10.1.x onwards to only those components required to provide
- *              OpenSSL integration with the NIO and NIO2 connectors.
+ *              10.1.x / Tomcat Native 2.x onwards to only include those
+ *              components required to provide OpenSSL integration with the NIO
+ *              and NIO2 connectors.
  */
 @Deprecated
 public interface PoolCallback {
