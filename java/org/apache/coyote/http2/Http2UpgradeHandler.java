fcdbc690558db734952a5d93fa422dd02da6cfb6
Mark Thomas <markt@apache.org>
2021-02-01 10:19:32 +0000

    Fix comment typo

--- java/org/apache/coyote/http2/Http2UpgradeHandler.java
+++ java/org/apache/coyote/http2/Http2UpgradeHandler.java
@@ -1226,7 +1226,7 @@ public class Http2UpgradeHandler extends AbstractStream implements InternalHttpU
             if (stream.isClosedFinal()) {
                 // This stream went from IDLE to CLOSED and is likely to have
                 // been created by the client as part of the priority tree.
-                // Candidate for steo 3.
+                // Candidate for step 3.
                 candidatesStepThree.add(stream.getIdentifier());
             } else if (stream.getChildStreams().size() == 0) {
                 // Prune it
