7dcf54b7a7f5fdd9560241d25427aba3e8899a84
Mark Thomas <markt@apache.org>
2021-04-21 12:51:04 +0100

    Remove unused code

--- java/org/apache/jasper/compiler/Generator.java
+++ java/org/apache/jasper/compiler/Generator.java
@@ -2334,7 +2334,9 @@ class Generator {
         public void visit(Node.AttributeGenerator n) throws JasperException {
             Node.CustomTag tag = n.getTag();
             Node.JspAttribute[] attrs = tag.getJspAttributes();
-            for (int i = 0; attrs != null && i < attrs.length; i++) {
+            // The TagPluginManager only creates AttributeGenerator nodes for
+            // attributes that are present.
+            for (int i = 0; i < attrs.length; i++) {
                 if (attrs[i].getName().equals(n.getName())) {
                     out.print(evaluateAttribute(getTagHandlerInfo(tag),
                             attrs[i], tag, null));
