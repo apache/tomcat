e498a7dc8e6d3db852199288013ac58d6263f93d
Mark Thomas <markt@apache.org>
2021-04-30 11:13:35 +0100

    Refactor passing of path parameters to POJO end points
    I don't recall why path parameters were originally passed via user
    properties (possibly so the wrapper had a 0-arg constructor?) but the
    refactoring removes the need for this and simplifies the code.
    This is technically a public API change but it is primarily Tomcat
    internals and I can't see a reason that users or frameworks would be
    using it therefore I have made a breaking change.

--- java/org/apache/tomcat/websocket/pojo/PojoEndpointClient.java
+++ java/org/apache/tomcat/websocket/pojo/PojoEndpointClient.java
@@ -24,7 +24,6 @@ import jakarta.websocket.DeploymentException;
 import jakarta.websocket.EndpointConfig;
 import jakarta.websocket.Session;
 
-
 /**
  * Wrapper class for instances of POJOs annotated with
  * {@link jakarta.websocket.ClientEndpoint} so they appear as standard
@@ -34,10 +33,9 @@ public class PojoEndpointClient extends PojoEndpointBase {
 
     public PojoEndpointClient(Object pojo,
             List<Class<? extends Decoder>> decoders) throws DeploymentException {
+        super(Collections.<String,String>emptyMap());
         setPojo(pojo);
-        setMethodMapping(
-                new PojoMethodMapping(pojo.getClass(), decoders, null));
-        setPathParameters(Collections.<String,String>emptyMap());
+        setMethodMapping(new PojoMethodMapping(pojo.getClass(), decoders, null));
     }
 
     @Override
