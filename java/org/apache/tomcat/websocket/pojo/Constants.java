81e49cee6c2b9e453e2f4b5e1ae44ba13dc6797f
Mark Thomas <markt@apache.org>
2021-04-30 11:13:35 +0100

    Refactor passing of path parameters to POJO end points
    I don't recall why path parameters were originally passed via user
    properties (possibly so the wrapper had a 0-arg constructor?) but the
    refactoring removes the need for this and simplifies the code.
    This is technically a public API change but it is primarily Tomcat
    internals and I can't see a reason that users or frameworks would be
    using it therefore I have made a breaking change.

--- java/org/apache/tomcat/websocket/pojo/Constants.java
+++ java/org/apache/tomcat/websocket/pojo/Constants.java
@@ -21,8 +21,6 @@ package org.apache.tomcat.websocket.pojo;
  */
 public class Constants {
 
-    public static final String POJO_PATH_PARAM_KEY =
-            "org.apache.tomcat.websocket.pojo.PojoEndpoint.pathParams";
     public static final String POJO_METHOD_MAPPING_KEY =
             "org.apache.tomcat.websocket.pojo.PojoEndpoint.methodMapping";
 
