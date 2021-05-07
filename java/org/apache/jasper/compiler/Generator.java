50577a12614b42623e8f08f63f101647b0d5d085
Mark Thomas <markt@apache.org>
2021-05-07 18:07:14 +0100

    Remove unnecessary code
    Validator ensures alias is present if name-from-attribute is present

--- java/org/apache/jasper/compiler/Generator.java
+++ java/org/apache/jasper/compiler/Generator.java
@@ -3120,9 +3120,6 @@ class Generator {
                 String nameFrom = tagVar.getNameFromAttribute();
                 if (nameFrom != null) {
                     String aliasedName = n.getAttributeValue(nameFrom);
-                    if (aliasedName == null)
-                        continue;
-
                     if (!aliasSeen) {
                         out.printin("java.util.HashMap ");
                         aliasMapVar = tagHandlerVar + "_aliasMap";
