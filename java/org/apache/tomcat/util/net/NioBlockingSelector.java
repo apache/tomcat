b42b6fedefea8f1a8f26bc006e8aeee6497c7411
Mark Thomas <markt@apache.org>
2021-05-18 10:56:49 +0100

    Fix BZ 65311. Correct race condition that could lead to short delay
    https://bz.apache.org/bugzilla/show_bug.cgi?id=65311

--- java/org/apache/tomcat/util/net/NioBlockingSelector.java
+++ java/org/apache/tomcat/util/net/NioBlockingSelector.java
@@ -320,11 +320,9 @@ public class NioBlockingSelector {
                     events();
                     int keyCount = 0;
                     try {
-                        int i = wakeupCounter.get();
-                        if (i>0)
+                        if (wakeupCounter.getAndSet(-1) > 0) {
                             keyCount = selector.selectNow();
-                        else {
-                            wakeupCounter.set(-1);
+                        } else {
                             keyCount = selector.select(1000);
                         }
                         wakeupCounter.set(0);
