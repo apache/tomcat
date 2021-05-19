c099d68e48bcbb0e8fd54a6ac9bccfcb7432cb07
Mark Thomas <markt@apache.org>
2021-05-19 19:59:46 +0100

    Remove unnecessary code - align with 9.0.x/10.0.x

--- java/org/apache/catalina/connector/CoyoteInputStream.java
+++ java/org/apache/catalina/connector/CoyoteInputStream.java
@@ -125,32 +125,7 @@ public class CoyoteInputStream extends ServletInputStream {
 
     @Override
     public int read(final byte[] b) throws IOException {
-        checkNonBlockingRead();
-
-        if (SecurityUtil.isPackageProtectionEnabled()) {
-            try {
-                Integer result = AccessController
-                        .doPrivileged(new PrivilegedExceptionAction<Integer>() {
-
-                            @Override
-                            public Integer run() throws IOException {
-                                Integer integer = Integer.valueOf(ib.read(b, 0, b.length));
-                                return integer;
-                            }
-
-                        });
-                return result.intValue();
-            } catch (PrivilegedActionException pae) {
-                Exception e = pae.getException();
-                if (e instanceof IOException) {
-                    throw (IOException) e;
-                } else {
-                    throw new RuntimeException(e.getMessage(), e);
-                }
-            }
-        } else {
-            return ib.read(b, 0, b.length);
-        }
+        return read(b, 0, b.length);
     }
 
 
@@ -223,12 +198,6 @@ public class CoyoteInputStream extends ServletInputStream {
         } else {
             return ib.read(b);
         }
-    }
-
-
-    @Override
-    public int readLine(byte[] b, int off, int len) throws IOException {
-        return super.readLine(b, off, len);
     }
 
 
