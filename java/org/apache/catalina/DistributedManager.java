744f17d5458686f0bfb22d210797f29cb99eb332
Mark Thomas <markt@apache.org>
2021-05-23 19:43:15 +0100

    Language improvements
    Identified by https://clcdemo.net/analysis.html?project=tomcat.git

--- java/org/apache/catalina/DistributedManager.java
+++ java/org/apache/catalina/DistributedManager.java
@@ -25,7 +25,7 @@ import java.util.Set;
  * BackupManager is an example of such a Manager as are implementations of the
  * StoreManager interface.
  * <p>
- * With the BackupManager, sessions can be primary (master copy on this node),
+ * With the BackupManager, sessions can be primary (primary copy on this node),
  * backup (backup copy on this node) or proxy (only the session ID on this
  * node). The identity of the primary and backup nodes are known for all
  * sessions, including proxy sessions.
