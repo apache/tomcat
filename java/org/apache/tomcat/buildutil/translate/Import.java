43e919f90952356513443c4ce804fe80c711c72f
Mark Thomas <markt@apache.org>
2021-05-25 22:02:42 +0100

    Update import/export to handle POEditor behaviours
    The end results for
    Tomcat-Export->Tomcat-Import
    and
    Tomcat-Export->POEditor-Import->POEditor-Export->Tomcat-Import
    are now much more similar

--- java/org/apache/tomcat/buildutil/translate/Import.java
+++ java/org/apache/tomcat/buildutil/translate/Import.java
@@ -82,7 +82,7 @@ public class Import {
                 w.write(System.lineSeparator());
             }
 
-            w.write(cKey.key + "=" + Utils.formatValue(value));
+            w.write(cKey.key + "=" + Utils.formatValueImport(value));
             w.write(System.lineSeparator());
         }
         if (w != null) {
