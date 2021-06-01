f5a732e74e2a36442b2bf562c665917c4bb1167a
Mark Thomas <markt@apache.org>
2021-06-01 19:46:52 +0100

    Remove blank lines between license header and package declaration

--- java/org/apache/catalina/session/DataSourceStore.java
+++ java/org/apache/catalina/session/DataSourceStore.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.catalina.session;
 
 import java.io.BufferedInputStream;
