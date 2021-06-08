556b37a57adb8066878358d0831c48ae7c822fae
Mark Thomas <markt@apache.org>
2021-06-08 09:28:26 +0100

    Update to Tomcat Native 1.2.30

--- java/org/apache/catalina/core/AprLifecycleListener.java
+++ java/org/apache/catalina/core/AprLifecycleListener.java
@@ -66,7 +66,7 @@ public class AprLifecycleListener
     protected static final int TCN_REQUIRED_MINOR = 2;
     protected static final int TCN_REQUIRED_PATCH = 14;
     protected static final int TCN_RECOMMENDED_MINOR = 2;
-    protected static final int TCN_RECOMMENDED_PV = 23;
+    protected static final int TCN_RECOMMENDED_PV = 30;
 
 
     // ---------------------------------------------- Properties
