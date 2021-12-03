b68865aa7e85f6e3532716ae10abeda43593b734
Mark Thomas <markt@apache.org>
2021-12-03 07:19:06 +0000

    Fix copied/pasted comment

--- java/org/apache/tomcat/util/collections/SynchronizedStack.java
+++ java/org/apache/tomcat/util/collections/SynchronizedStack.java
@@ -18,10 +18,10 @@ package org.apache.tomcat.util.collections;
 
 /**
  * This is intended as a (mostly) GC-free alternative to
- * {@link java.util.concurrent.ConcurrentLinkedQueue} when the requirement is to
- * create a pool of re-usable objects with no requirement to shrink the pool.
- * The aim is to provide the bare minimum of required functionality as quickly
- * as possible with minimum garbage.
+ * {@link java.util.Stack} when the requirement is to create a pool of re-usable
+ * objects with no requirement to shrink the pool. The aim is to provide the
+ * bare minimum of required functionality as quickly as possible with minimum
+ * garbage.
  *
  * @param <T> The type of object managed by this stack
  */
