549f23562209cf1e68da4c17fbda4112429f2c8c
Piotr P. Karwasz <piotr.github@karwasz.org>
2022-01-28 09:33:04 +0100

    Adds "javax.annotation" to filter
    Some users from mistakenly add `javax.annotation-api` to their
    applications, which prevents Tomcat from finding @Resource and similar
    annotations on the servlets on Java 9+.
    
    To prevent this "javax.annotation" should also be always loaded from
    the common classloader.
    
    On Java 8 and earlier classes from `javax.annotation` where served from
    the system classloader, so the change should be a no-op.

--- java/org/apache/catalina/loader/WebappClassLoaderBase.java
+++ java/org/apache/catalina/loader/WebappClassLoaderBase.java
@@ -2605,7 +2605,8 @@ public abstract class WebappClassLoaderBase extends URLClassLoader
                 if (name.startsWith("servlet.jsp.jstl.", 6)) {
                     return false;
                 }
-                if (name.startsWith("el.", 6) ||
+                if (name.startsWith("annotation.", 6) ||
+                    name.startsWith("el.", 6) ||
                     name.startsWith("servlet.", 6) ||
                     name.startsWith("websocket.", 6) ||
                     name.startsWith("security.auth.message.", 6)) {
@@ -2616,7 +2617,8 @@ public abstract class WebappClassLoaderBase extends URLClassLoader
                 if (name.startsWith("servlet/jsp/jstl/", 6)) {
                     return false;
                 }
-                if (name.startsWith("el/", 6) ||
+                if (name.startsWith("annotation/", 6) ||
+                    name.startsWith("el/", 6) ||
                     name.startsWith("servlet/", 6) ||
                     name.startsWith("websocket/", 6) ||
                     name.startsWith("security/auth/message/", 6)) {
