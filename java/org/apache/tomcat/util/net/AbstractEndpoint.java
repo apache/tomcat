d28bb22ca076fceb275c748b0d511ed9bf12e8c2
Mark Thomas <markt@apache.org>
2022-02-02 19:34:39 +0000

    Correct a regression in the fix for BZ 65454
    Additional updates for new package for ThreadPoolExecutor
    https://bz.apache.org/bugzilla/show_bug.cgi?id=65454

--- java/org/apache/tomcat/util/net/AbstractEndpoint.java
+++ java/org/apache/tomcat/util/net/AbstractEndpoint.java
@@ -688,12 +688,12 @@ public abstract class AbstractEndpoint<S> {
     public void setMinSpareThreads(int minSpareThreads) {
         this.minSpareThreads = minSpareThreads;
         Executor executor = this.executor;
-        if (internalExecutor && executor instanceof java.util.concurrent.ThreadPoolExecutor) {
+        if (internalExecutor && executor instanceof ThreadPoolExecutor) {
             // The internal executor should always be an instance of
-            // j.u.c.ThreadPoolExecutor but it may be null if the endpoint is
-            // not running.
+            // org.apache.tomcat.util.threads.ThreadPoolExecutor but it may be
+            // null if the endpoint is not running.
             // This check also avoids various threading issues.
-            ((java.util.concurrent.ThreadPoolExecutor) executor).setCorePoolSize(minSpareThreads);
+            ((ThreadPoolExecutor) executor).setCorePoolSize(minSpareThreads);
         }
     }
     public int getMinSpareThreads() {
@@ -715,12 +715,12 @@ public abstract class AbstractEndpoint<S> {
     public void setMaxThreads(int maxThreads) {
         this.maxThreads = maxThreads;
         Executor executor = this.executor;
-        if (internalExecutor && executor instanceof java.util.concurrent.ThreadPoolExecutor) {
+        if (internalExecutor && executor instanceof ThreadPoolExecutor) {
             // The internal executor should always be an instance of
-            // j.u.c.ThreadPoolExecutor but it may be null if the endpoint is
-            // not running.
+            // org.apache.tomcat.util.threads.ThreadPoolExecutor but it may be
+            // null if the endpoint is not running.
             // This check also avoids various threading issues.
-            ((java.util.concurrent.ThreadPoolExecutor) executor).setMaximumPoolSize(maxThreads);
+            ((ThreadPoolExecutor) executor).setMaximumPoolSize(maxThreads);
         }
     }
     public int getMaxThreads() {
@@ -903,6 +903,8 @@ public abstract class AbstractEndpoint<S> {
         if (executor != null) {
             if (executor instanceof ThreadPoolExecutor) {
                 return ((ThreadPoolExecutor) executor).getPoolSize();
+            } else if (executor instanceof java.util.concurrent.ThreadPoolExecutor) {
+                return ((java.util.concurrent.ThreadPoolExecutor) executor).getPoolSize();
             } else if (executor instanceof ResizableExecutor) {
                 return ((ResizableExecutor) executor).getPoolSize();
             } else {
@@ -923,6 +925,8 @@ public abstract class AbstractEndpoint<S> {
         if (executor != null) {
             if (executor instanceof ThreadPoolExecutor) {
                 return ((ThreadPoolExecutor) executor).getActiveCount();
+            } else if (executor instanceof java.util.concurrent.ThreadPoolExecutor) {
+                return ((java.util.concurrent.ThreadPoolExecutor) executor).getActiveCount();
             } else if (executor instanceof ResizableExecutor) {
                 return ((ResizableExecutor) executor).getActiveCount();
             } else {
