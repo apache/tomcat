69c7588e1880135843950bb61af01dbaf552e6c4
remm <remm@apache.org>
2022-04-05 16:24:04 +0200

    Fix missing strings

--- java/org/apache/catalina/core/DefaultInstanceManager.java
+++ java/org/apache/catalina/core/DefaultInstanceManager.java
@@ -713,7 +713,7 @@ public class DefaultInstanceManager implements InstanceManager {
             if (method.getName().equals(methodNameFromXml)) {
                 if (!Introspection.isValidLifecycleCallback(method)) {
                     throw new IllegalArgumentException(
-                            "Invalid " + annotation.getName() + " annotation");
+                            sm.getString("defaultInstanceManager.invalidAnnotation", annotation.getName()));
                 }
                 result = method;
             }
@@ -721,7 +721,7 @@ public class DefaultInstanceManager implements InstanceManager {
             if (method.isAnnotationPresent(annotation)) {
                 if (currentMethod != null || !Introspection.isValidLifecycleCallback(method)) {
                     throw new IllegalArgumentException(
-                            "Invalid " + annotation.getName() + " annotation");
+                            sm.getString("defaultInstanceManager.invalidAnnotation", annotation.getName()));
                 }
                 result = method;
             }
