205efe8659d727114fa61c17bd82a6b28b74cf9f
Mark Thomas <markt@apache.org>
2022-05-17 11:11:38 +0100

    Improve handling of errors associated with --enable-preview

--- test/javax/el/TestImportHandlerStandardPackages.java
+++ test/javax/el/TestImportHandlerStandardPackages.java
@@ -70,7 +70,15 @@ public class TestImportHandlerStandardPackages {
                 }
                 // Extract class name
                 String className = fileName.substring(10, fileName.length() - 6);
-                Class<?> clazz = Class.forName("java.lang." + className);
+                Class<?> clazz = null;
+                try {
+                    clazz = Class.forName("java.lang." + className);
+                } catch (Throwable t) {
+                    swallowEnablePreview(className, t);
+                }
+                if (clazz == null) {
+                    continue;
+                }
                 if (!Modifier.isPublic(clazz.getModifiers())) {
                     // Exclude non-public classes
                     continue;
@@ -173,5 +181,26 @@ public class TestImportHandlerStandardPackages {
             result[i] = (String) names[i];
         }
         return result;
+    }
+
+
+    /*
+     * This is a bit of a hack but there isn't a specific exception that can be
+     * caught.
+     */
+    private void swallowEnablePreview(String className, Throwable t) {
+        while (t != null) {
+            if (t.getMessage() != null && t.getMessage().contains("--enable-preview")) {
+                return;
+            }
+
+            Throwable cause = t.getCause();
+            if (t == cause) {
+                break;
+            }
+            t = cause;
+        }
+
+        throw new RuntimeException(className, t);
     }
 }
