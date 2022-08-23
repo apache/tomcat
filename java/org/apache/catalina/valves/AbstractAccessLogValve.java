86d6af94b4389f72b638409226a99caeea7e1bac
Mark Thomas <markt@apache.org>
2022-08-23 08:36:34 +0100

    Improve naming. No functional change.

--- java/org/apache/catalina/valves/AbstractAccessLogValve.java
+++ java/org/apache/catalina/valves/AbstractAccessLogValve.java
@@ -1536,10 +1536,10 @@ public abstract class AbstractAccessLogValve extends ValveBase implements Access
      * write a specific cookie - %{xxx}c
      */
     protected static class CookieElement implements AccessLogElement {
-        private final String header;
+        private final String cookieNameToLog;
 
-        public CookieElement(String header) {
-            this.header = header;
+        public CookieElement(String cookieNameToLog) {
+            this.cookieNameToLog = cookieNameToLog;
         }
 
         @Override
@@ -1549,7 +1549,7 @@ public abstract class AbstractAccessLogValve extends ValveBase implements Access
             Cookie[] cookies = request.getCookies();
             if (cookies != null) {
                 for (Cookie cookie : cookies) {
-                    if (header.equals(cookie.getName())) {
+                    if (cookieNameToLog.equals(cookie.getName())) {
                         value = cookie.getValue();
                         break;
                     }
