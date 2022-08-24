5c7a6dfac2a055a57fdf4eac5cd13946f8d5fda4
Mark Thomas <markt@apache.org>
2022-08-24 11:50:06 +0100

    Fix 66233 - include error message with 400 response for too many cookies
    https://bz.apache.org/bugzilla/show_bug.cgi?id=66233

--- java/org/apache/catalina/connector/CoyoteAdapter.java
+++ java/org/apache/catalina/connector/CoyoteAdapter.java
@@ -744,7 +744,7 @@ public class CoyoteAdapter implements Adapter {
                 // Too many cookies
                 if (!response.isError()) {
                     response.setError();
-                    response.sendError(400);
+                    response.sendError(400, e.getMessage());
                 }
                 return true;
             }
