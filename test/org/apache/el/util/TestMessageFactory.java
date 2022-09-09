b69af64cc90ed82b49666a1fe5168516459f07b0
Mark Thomas <markt@apache.org>
2022-09-09 19:03:31 +0100

    Silence IDE warnings

--- test/org/apache/el/util/TestMessageFactory.java
+++ test/org/apache/el/util/TestMessageFactory.java
@@ -24,6 +24,8 @@ import org.junit.Test;
 
 public class TestMessageFactory {
 
+    private static final Integer ZERO = Integer.valueOf(0);
+
     MessageFactory messageFactory = new MessageFactory(ResourceBundle.getBundle("org.apache.el.util.TestStrings"));
 
     @Test
@@ -32,7 +34,8 @@ public class TestMessageFactory {
         // Trailing '0" is an extra Number argument, not printed by the
         // message pattern. It reflects the case when a translation has not
         // been updated with new arguments.
-        String result = messageFactory.getInternal("messageFactory.formatNone", new BigDecimal(input), 0 /*ignored*/);
+        String result =
+                messageFactory.getInternal("messageFactory.formatNone", new BigDecimal(input), ZERO /*ignored*/);
         // Should be unchanged
         Assert.assertEquals(input, result);
     }
@@ -48,7 +51,8 @@ public class TestMessageFactory {
     @Test
     public void testFormatChoice() {
         String input = "1E+2";
-        String result = messageFactory.getInternal("messageFactory.formatChoice", new BigDecimal(input), 0 /*ignored*/);
+        String result =
+                messageFactory.getInternal("messageFactory.formatChoice", new BigDecimal(input), ZERO /*ignored*/);
         // Should be formatted as an integer
         Assert.assertEquals("100 is enough", result);
     }
@@ -56,7 +60,8 @@ public class TestMessageFactory {
     @Test
     public void testFormatNoArguments() {
         String input = "1E+2";
-        String result = messageFactory.getInternal("messageFactory.formatNoArguments", new BigDecimal(input), 0 /*ignored*/);
+        String result =
+                messageFactory.getInternal("messageFactory.formatNoArguments", new BigDecimal(input), ZERO /*ignored*/);
         Assert.assertEquals("A message", result);
     }
 }
