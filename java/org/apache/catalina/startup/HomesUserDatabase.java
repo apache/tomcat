317afcf5b874b0a6d4445f620ba2331124c17e76
Mark Thomas <markt@apache.org>
2022-09-14 19:29:18 +0100

    Refactor to avoid use of Hashtable. No functional change.

--- java/org/apache/catalina/startup/HomesUserDatabase.java
+++ java/org/apache/catalina/startup/HomesUserDatabase.java
@@ -18,8 +18,10 @@ package org.apache.catalina.startup;
 
 
 import java.io.File;
+import java.util.Collections;
 import java.util.Enumeration;
-import java.util.Hashtable;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -52,7 +54,7 @@ public final class HomesUserDatabase
     /**
      * The set of home directories for all defined users, keyed by username.
      */
-    private final Hashtable<String,String> homes = new Hashtable<>();
+    private final Map<String,String> homes = new HashMap<>();
 
 
     /**
@@ -100,11 +102,11 @@ public final class HomesUserDatabase
 
 
     /**
-     * Return an enumeration of the usernames defined on this server.
+     * Return an enumeration of the user names defined on this server.
      */
     @Override
     public Enumeration<String> getUsers() {
-        return homes.keys();
+        return Collections.enumeration(homes.keySet());
     }
 
 
