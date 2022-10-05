370b73e8a715ec6ef5bddad53bba0edd658d1f80
Mark Thomas <markt@apache.org>
2022-10-05 17:47:56 +0100

    Refactor. Using List interface so use ArrayList rather than LinkedList

--- java/org/apache/catalina/tribes/transport/RxTaskPool.java
+++ java/org/apache/catalina/tribes/transport/RxTaskPool.java
@@ -15,8 +15,8 @@
  * limitations under the License.
  */
 package org.apache.catalina.tribes.transport;
+import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -27,8 +27,8 @@ import java.util.List;
  */
 public class RxTaskPool {
 
-    final List<AbstractRxTask> idle = new LinkedList<>();
-    final List<AbstractRxTask> used = new LinkedList<>();
+    final List<AbstractRxTask> idle = new ArrayList<>();
+    final List<AbstractRxTask> used = new ArrayList<>();
 
     final Object mutex = new Object();
     boolean running = true;
