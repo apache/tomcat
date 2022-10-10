49e2f72e9b5247509566a1090a7bdd91c615de16
Mark Thomas <markt@apache.org>
2022-10-10 11:16:08 +0100

    Checkstyle - import order

--- java/org/apache/tomcat/util/modeler/modules/MbeansDescriptorsIntrospectionSource.java
+++ java/org/apache/tomcat/util/modeler/modules/MbeansDescriptorsIntrospectionSource.java
@@ -24,9 +24,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 import javax.management.ObjectName;
 
