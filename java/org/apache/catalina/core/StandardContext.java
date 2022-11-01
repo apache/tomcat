1883de7bbaf8e3da5c57529a0e8fe88f766d817c
Mark Thomas <markt@apache.org>
2022-11-01 12:30:11 +0000

    Fix BZ 66330
    Correct a regression introduced when fixing bug 62897 that meant any
    value configured for skipMemoryLeakChecksOnJvmShutdown on the Context
    was ignored and the default was always used.
    
    https://bz.apache.org/bugzilla/show_bug.cgi?id=66330

--- java/org/apache/catalina/core/StandardContext.java
+++ java/org/apache/catalina/core/StandardContext.java
@@ -5020,6 +5020,7 @@ public class StandardContext extends ContainerBase
                     cl.setClearReferencesHttpClientKeepAliveThread(getClearReferencesHttpClientKeepAliveThread());
                     cl.setClearReferencesObjectStreamClassCaches(getClearReferencesObjectStreamClassCaches());
                     cl.setClearReferencesThreadLocals(getClearReferencesThreadLocals());
+                    cl.setSkipMemoryLeakChecksOnJvmShutdown(getSkipMemoryLeakChecksOnJvmShutdown());
                 }
 
                 // By calling unbindThread and bindThread in a row, we setup the
