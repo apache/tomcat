60718deae7532dcb47337782b12f41ac3c91a56d
Mark Thomas <markt@apache.org>
2022-11-25 20:47:43 +0000

    Fix license comment indent

--- java/org/apache/catalina/realm/DataSourceRealm.java
+++ java/org/apache/catalina/realm/DataSourceRealm.java
@@ -31,16 +31,16 @@ import org.apache.catalina.LifecycleException;
 import org.apache.naming.ContextBindings;
 
 /**
-*
-* Implementation of <b>Realm</b> that works with any JDBC JNDI DataSource.
-* See the Realm How-To for more details on how to set up the database and
-* for configuration options.
-*
-* @author Glenn L. Nielsen
-* @author Craig R. McClanahan
-* @author Carson McDonald
-* @author Ignacio Ortega
-*/
+ *
+ * Implementation of <b>Realm</b> that works with any JDBC JNDI DataSource.
+ * See the Realm How-To for more details on how to set up the database and
+ * for configuration options.
+ *
+ * @author Glenn L. Nielsen
+ * @author Craig R. McClanahan
+ * @author Carson McDonald
+ * @author Ignacio Ortega
+ */
 public class DataSourceRealm extends RealmBase {
 
 
