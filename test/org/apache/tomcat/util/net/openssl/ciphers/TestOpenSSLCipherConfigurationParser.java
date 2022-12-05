3ddbadbc7a542c9735f79bd107d7436b22bc531b
Mark Thomas <markt@apache.org>
2022-12-05 14:59:59 +0000

    Fix test failures with OpenSSL 3.1.x
    The move of CCM8 ciphers from HIGH to medium has been deferred to 3.2.x.

--- test/org/apache/tomcat/util/net/openssl/ciphers/TestOpenSSLCipherConfigurationParser.java
+++ test/org/apache/tomcat/util/net/openssl/ciphers/TestOpenSSLCipherConfigurationParser.java
@@ -62,8 +62,8 @@ public class TestOpenSSLCipherConfigurationParser {
 
     @Test
     public void testHIGH() throws Exception {
-        if (TesterOpenSSL.VERSION < 30100) {
-            // OpenSSL 3.1.x moved the CCM8 ciphers from high to medium
+        if (TesterOpenSSL.VERSION < 30200) {
+            // OpenSSL 3.2.x moved the CCM8 ciphers from high to medium
             testSpecification("HIGH:!AESCCM8");
         } else {
             testSpecification("HIGH");
@@ -73,8 +73,8 @@ public class TestOpenSSLCipherConfigurationParser {
 
     @Test
     public void testMEDIUM() throws Exception {
-        if (TesterOpenSSL.VERSION < 30100) {
-            // OpenSSL 3.1.x moved the CCM8 ciphers from high to medium
+        if (TesterOpenSSL.VERSION < 30200) {
+            // OpenSSL 3.2.x moved the CCM8 ciphers from high to medium
             testSpecification("MEDIUM:AESCCM8");
         } else {
             testSpecification("MEDIUM");
@@ -533,8 +533,8 @@ public class TestOpenSSLCipherConfigurationParser {
         // Tomcat 8 default as of 2014-08-04
         // This gets an A- from https://www.ssllabs.com/ssltest with no FS for
         // a number of the reference browsers
-        if (TesterOpenSSL.VERSION < 30100) {
-            // OpenSSL 3.1.x moved the CCM8 ciphers from high to medium
+        if (TesterOpenSSL.VERSION < 30200) {
+            // OpenSSL 3.2.x moved the CCM8 ciphers from high to medium
             testSpecification("HIGH:!AESCCM8:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5");
         } else {
             testSpecification("HIGH:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5");
