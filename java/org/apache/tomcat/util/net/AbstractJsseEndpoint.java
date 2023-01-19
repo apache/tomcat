dfb802cd317445868b9d50237cf73becb5828fc8
Mark Thomas <markt@apache.org>
2023-01-19 18:08:10 +0000

    Log basic information for each configured TLS cert when Tomcat starts

--- java/org/apache/tomcat/util/net/AbstractJsseEndpoint.java
+++ java/org/apache/tomcat/util/net/AbstractJsseEndpoint.java
@@ -106,6 +106,7 @@ public abstract class AbstractJsseEndpoint<S,U> extends AbstractEndpoint<S,U> {
                 throw new IllegalArgumentException(e.getMessage(), e);
             }
 
+            logCertificate(certificate);
             certificate.setSslContext(sslContext);
         }
     }
