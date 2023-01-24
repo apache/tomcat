256f3df18757488bbfef58e3763b676af3c35334
Mark Thomas <markt@apache.org>
2023-01-24 11:10:27 +0000

    Code cleanup (format). No functional change.

--- java/org/apache/catalina/tribes/util/UUIDGenerator.java
+++ java/org/apache/catalina/tribes/util/UUIDGenerator.java
@@ -27,8 +27,7 @@ import org.apache.juli.logging.LogFactory;
  */
 public class UUIDGenerator {
     private static final Log log = LogFactory.getLog(UUIDGenerator.class);
-    protected static final StringManager sm =
-        StringManager.getManager("org.apache.catalina.tribes.util");
+    protected static final StringManager sm = StringManager.getManager("org.apache.catalina.tribes.util");
 
     public static final int UUID_LENGTH = 16;
     public static final int UUID_VERSION = 4;
@@ -45,37 +44,37 @@ public class UUIDGenerator {
         secrand.nextInt();
         long time = System.currentTimeMillis() - start;
         if (time > 100) {
-            log.info(sm.getString("uuidGenerator.createRandom",
-                    secrand.getAlgorithm(), Long.valueOf(time)));
+            log.info(sm.getString("uuidGenerator.createRandom", secrand.getAlgorithm(), Long.valueOf(time)));
         }
     }
 
     public static byte[] randomUUID(boolean secure) {
         byte[] result = new byte[UUID_LENGTH];
-        return randomUUID(secure,result,0);
+        return randomUUID(secure, result, 0);
     }
 
     public static byte[] randomUUID(boolean secure, byte[] into, int offset) {
-        if ( (offset+UUID_LENGTH)>into.length ) {
-            throw new ArrayIndexOutOfBoundsException(sm.getString("uuidGenerator.unable.fit",
-                    Integer.toString(UUID_LENGTH), Integer.toString(into.length),
-                    Integer.toString(offset+UUID_LENGTH)));
+        if ((offset + UUID_LENGTH) > into.length) {
+            throw new ArrayIndexOutOfBoundsException(
+                    sm.getString("uuidGenerator.unable.fit", Integer.toString(UUID_LENGTH),
+                            Integer.toString(into.length), Integer.toString(offset + UUID_LENGTH)));
         }
-        Random r = (secure&&(secrand!=null))?secrand:rand;
-        nextBytes(into,offset,UUID_LENGTH,r);
-        into[6+offset] &= 0x0F;
-        into[6+offset] |= (UUID_VERSION << 4);
-        into[8+offset] &= 0x3F; //0011 1111
-        into[8+offset] |= 0x80; //1000 0000
+        Random r = (secure && (secrand != null)) ? secrand : rand;
+        nextBytes(into, offset, UUID_LENGTH, r);
+        into[6 + offset] &= 0x0F;
+        into[6 + offset] |= (UUID_VERSION << 4);
+        into[8 + offset] &= 0x3F; // 0011 1111
+        into[8 + offset] |= 0x80; // 1000 0000
         return into;
     }
 
     /**
      * Same as java.util.Random.nextBytes except this one we don't have to allocate a new byte array
-     * @param into byte[]
+     *
+     * @param into   byte[]
      * @param offset int
      * @param length int
-     * @param r Random
+     * @param r      Random
      */
     public static void nextBytes(byte[] into, int offset, int length, Random r) {
         int numRequested = length;
@@ -86,7 +85,7 @@ public class UUIDGenerator {
                     return;
                 }
                 rnd = (i == 0 ? r.nextInt() : rnd >> BITS_PER_BYTE);
-                into[offset+numGot] = (byte) rnd;
+                into[offset + numGot] = (byte) rnd;
                 numGot++;
             }
         }
