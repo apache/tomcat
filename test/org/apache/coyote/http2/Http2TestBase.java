519403fb8aace2c64e1ee3999511719418dc6968
Mark Thomas <markt@apache.org>
2023-01-25 16:28:34 +0000

    Fix BZ 66442 - h2 responses without bodies should not use DATA frames
    https://bz.apache.org/bugzilla/show_bug.cgi?id=66442

--- test/org/apache/coyote/http2/Http2TestBase.java
+++ test/org/apache/coyote/http2/Http2TestBase.java
@@ -1339,6 +1339,18 @@ public abstract class Http2TestBase extends TomcatBaseTest {
     }
 
 
+    public static class NoContentServlet extends HttpServlet {
+
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+                throws ServletException, IOException {
+            resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
+        }
+    }
+
+
     public static class SimpleServlet extends HttpServlet {
 
         private static final long serialVersionUID = 1L;
