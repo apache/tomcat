6863a909fd15eed3079dd7de8bb8877e1b39b6d1
remm <remm@apache.org>
2023-02-27 13:24:49 +0100

    66482: Restore inline state after async operation in NIO2
    Patch submitted by zhougang.

--- java/org/apache/tomcat/util/net/Nio2Endpoint.java
+++ java/org/apache/tomcat/util/net/Nio2Endpoint.java
@@ -1028,8 +1028,11 @@ public class Nio2Endpoint extends AbstractJsseEndpoint<Nio2Channel,AsynchronousS
                     writeNotify = true;
                 }
                 Nio2Endpoint.startInline();
-                run();
-                Nio2Endpoint.endInline();
+                try {
+                    run();
+                } finally {
+                    Nio2Endpoint.endInline();
+                }
             }
 
             @Override
