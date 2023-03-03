2784ea307b98e4e3371d97f8752e6b85805ea1c5
Mark Thomas <markt@apache.org>
2023-03-03 13:53:22 +0000

    Update format

--- java/org/apache/catalina/authenticator/AuthenticatorBase.java
+++ java/org/apache/catalina/authenticator/AuthenticatorBase.java
@@ -1334,6 +1334,8 @@ public abstract class AuthenticatorBase extends ValveBase implements Authenticat
 
 
     protected enum AllowCorsPreflight {
-        NEVER, FILTER, ALWAYS
+        NEVER,
+        FILTER,
+        ALWAYS
     }
 }
