7ed92614933fcaecacf647e6bfbe8064e34adca1
lihan <lihan@apache.org>
2023-03-10 15:22:50 +0800

    Improved regexp performance: "a-zA-Z0-9_" -> "\w" (#592)
    Submitted by Andrei Briukhov

--- java/org/apache/catalina/servlets/CGIServlet.java
+++ java/org/apache/catalina/servlets/CGIServlet.java
@@ -255,7 +255,7 @@ public final class CGIServlet extends HttpServlet {
         DEFAULT_SUPER_METHODS.add("TRACE");
 
         if (JrePlatform.IS_WINDOWS) {
-            DEFAULT_CMD_LINE_ARGUMENTS_DECODED_PATTERN = Pattern.compile("[a-zA-Z0-9\\Q-_.\\/:\\E]+");
+            DEFAULT_CMD_LINE_ARGUMENTS_DECODED_PATTERN = Pattern.compile("[\\w\\Q-.\\/:\\E]+");
         } else {
             // No restrictions
             DEFAULT_CMD_LINE_ARGUMENTS_DECODED_PATTERN = null;
@@ -324,7 +324,7 @@ public final class CGIServlet extends HttpServlet {
      * Uses \Q...\E to avoid individual quoting.
      */
     private Pattern cmdLineArgumentsEncodedPattern =
-            Pattern.compile("[a-zA-Z0-9\\Q%;/?:@&,$-_.!~*'()\\E]+");
+            Pattern.compile("[\\w\\Q%;/?:@&,$-.!~*'()\\E]+");
 
     /**
      * Limits the decoded form of individual command line arguments. Default
