e7c450e99409cbee20ac5389e80f49594342cff6
Felix Schumacher <fschumacher@apache.org>
2023-03-25 13:12:38 +0100

    Fix Bug 66543 -  Wrong log message used in StandardContext#fireRequestDestroyEvent
    Give it its own log message instead of re-using the one from
    fireRequestInitEvent.

--- java/org/apache/catalina/core/StandardContext.java
+++ java/org/apache/catalina/core/StandardContext.java
@@ -5643,7 +5643,7 @@ public class StandardContext extends ContainerBase implements Context, Notificat
                     listener.requestDestroyed(event);
                 } catch (Throwable t) {
                     ExceptionUtils.handleThrowable(t);
-                    getLogger().error(sm.getString("standardContext.requestListener.requestInit",
+                    getLogger().error(sm.getString("standardContext.requestListener.requestDestroyed",
                             instances[j].getClass().getName()), t);
                     request.setAttribute(RequestDispatcher.ERROR_EXCEPTION, t);
                     return false;
