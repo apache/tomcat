28253ccaf4b607db168e64c4bf7c9f1179a98806
Rainer Jung <rainer.jung@kippdata.de>
2023-04-27 09:32:44 +0200

    Escape timestamp output in AccessLog.
    It is needed if a SimpleDateFormat is used which contains
    verbatim characters that need escaping.

--- java/org/apache/catalina/valves/AbstractAccessLogValve.java
+++ java/org/apache/catalina/valves/AbstractAccessLogValve.java
@@ -1047,6 +1047,8 @@ public abstract class AbstractAccessLogValve extends ValveBase implements Access
 
         /* Our format description string, null if CLF */
         private final String format;
+        /* Does the format string contain characters we need to escape */
+        private final boolean needsEscaping;
         /* Whether to use begin of request or end of response as the timestamp */
         private final boolean usesBegin;
         /* The format type */
@@ -1085,6 +1087,16 @@ public abstract class AbstractAccessLogValve extends ValveBase implements Access
 
         protected DateAndTimeElement(String sdf) {
             String format = sdf;
+            boolean needsEscaping = false;
+            if (sdf != null) {
+                CharArrayWriter writer = new CharArrayWriter();
+                escapeAndAppend(sdf, writer);
+                String escaped = writer.toString();
+                if (!escaped.equals(sdf)) {
+                    needsEscaping = true;
+                }
+            }
+            this.needsEscaping = needsEscaping;
             boolean usesBegin = false;
             FormatType type = FormatType.CLF;
 
@@ -1169,7 +1181,11 @@ public abstract class AbstractAccessLogValve extends ValveBase implements Access
                     temp = temp.replace(tripleMsecPattern, tripleMsec);
                     temp = temp.replace(msecPattern, Long.toString(frac));
                 }
-                buf.append(temp);
+                if (needsEscaping) {
+                    escapeAndAppend(temp, buf);
+                } else {
+                    buf.append(temp);
+                }
             }
         }
     }
