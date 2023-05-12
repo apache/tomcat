2fb202967203446c401bf0d5bdaa05ccba1a3541
Mark Thomas <markt@apache.org>
2023-05-12 09:51:43 +0100

    Move the virtual thread executor from modules to the main code

--- java/org/apache/catalina/core/VirtualThreadExecutor.java
+++ java/org/apache/catalina/core/VirtualThreadExecutor.java
@@ -1,18 +1,18 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package org.apache.catalina.core;
 
@@ -24,7 +24,7 @@ import org.apache.catalina.util.LifecycleMBeanBase;
 /**
  * An executor that uses a new virtual thread for each task.
  */
-public class LoomExecutor extends LifecycleMBeanBase implements Executor {
+public class VirtualThreadExecutor extends LifecycleMBeanBase implements Executor {
 
     private String name;
     private Thread.Builder threadBuilder;
@@ -42,6 +42,7 @@ public class LoomExecutor extends LifecycleMBeanBase implements Executor {
     public void execute(Runnable command) {
         threadBuilder.start(command);
     }
+
 
     @Override
     protected void startInternal() throws LifecycleException {
