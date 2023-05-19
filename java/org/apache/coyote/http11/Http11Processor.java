2cb6f8e4681359087a2a52b6950a315383a8d422
lihan <lihan@apache.org>
2023-05-19 11:24:27 +0800

    Clear SocketWrapper reference to help GC
    (cherry picked from commit 10492dd22bd64ff63cf77786fa67d45cdc2a54b3)

--- java/org/apache/coyote/http11/Http11Processor.java
+++ java/org/apache/coyote/http11/Http11Processor.java
@@ -1419,7 +1419,6 @@ public class Http11Processor extends AbstractProcessor {
         inputBuffer.recycle();
         outputBuffer.recycle();
         upgradeToken = null;
-        socketWrapper = null;
         sendfileData = null;
         sslSupport = null;
     }
