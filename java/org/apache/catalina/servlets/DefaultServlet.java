8f46ae849dfe21ea768ed73e496b34542b6c3ec2
alexkachanov <alexkachanov@yandex.ru>
2023-05-28 17:17:40 +0900

    Bug 66618: add longDate attribute to XML in directory listing

--- java/org/apache/catalina/servlets/DefaultServlet.java
+++ java/org/apache/catalina/servlets/DefaultServlet.java
@@ -1691,6 +1691,9 @@ public class DefaultServlet extends HttpServlet {
             sb.append(" date='")
               .append(childResource.getLastModifiedHttp())
               .append('\'');
+            sb.append(" longDate='")
+              .append(childResource.getLastModified())
+              .append('\'');
 
             sb.append('>');
             sb.append(Escape.htmlElementContent(entry));
