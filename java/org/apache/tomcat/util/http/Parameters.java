866e44ee4bbd0209c5e061aa2542703761af734e
Mark Thomas <markt@apache.org>
2023-06-14 12:14:52 +0100

    Code clean-up - formatting. No functional change.

--- java/org/apache/tomcat/util/http/Parameters.java
+++ java/org/apache/tomcat/util/http/Parameters.java
@@ -44,7 +44,7 @@ public final class Parameters {
 
     private static final StringManager sm = StringManager.getManager("org.apache.tomcat.util.http");
 
-    private final Map<String, ArrayList<String>> paramHashValues = new LinkedHashMap<>();
+    private final Map<String,ArrayList<String>> paramHashValues = new LinkedHashMap<>();
     private boolean didQueryParameters = false;
 
     private MessageBytes queryMB;
@@ -418,8 +418,8 @@ public final class Parameters {
                     } else if (log.isInfoEnabled()) {
                         UserDataHelper.Mode logMode = userDataLog.getNextMode();
                         if (logMode != null) {
-                            String message = sm.getString("parameters.decodeFail.info", tmpName.toString(),
-                                    tmpValue.toString());
+                            String message =
+                                    sm.getString("parameters.decodeFail.info", tmpName.toString(), tmpValue.toString());
                             switch (logMode) {
                                 case INFO_THEN_DEBUG:
                                     message += sm.getString("parameters.fallToDebug");
@@ -487,7 +487,7 @@ public final class Parameters {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        for (Map.Entry<String, ArrayList<String>> e : paramHashValues.entrySet()) {
+        for (Map.Entry<String,ArrayList<String>> e : paramHashValues.entrySet()) {
             sb.append(e.getKey()).append('=');
             StringUtils.join(e.getValue(), ',', sb);
             sb.append('\n');
