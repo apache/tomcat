b0702119b613b3f705e4e552e84d76ad3bcf236a
Mark Thomas <markt@apache.org>
2023-07-04 20:21:11 +0100

    Fix potential resource leaks

--- java/org/apache/catalina/users/DataSourceUserDatabase.java
+++ java/org/apache/catalina/users/DataSourceUserDatabase.java
@@ -465,27 +465,26 @@ public class DataSourceUserDatabase extends SparseUserDatabase {
                 groups.putAll(createdGroups);
                 groups.putAll(modifiedGroups);
 
-                Connection dbConnection = openConnection();
-                if (dbConnection != null && preparedAllGroups != null) {
-                    try (PreparedStatement stmt = dbConnection.prepareStatement(preparedAllGroups)) {
-                        try (ResultSet rs = stmt.executeQuery()) {
-                            while (rs.next()) {
-                                String groupName = rs.getString(1);
-                                if (groupName != null) {
-                                    if (!groups.containsKey(groupName) && !removedGroups.containsKey(groupName)) {
-                                        Group group = findGroupInternal(dbConnection, groupName);
-                                        if (group != null) {
-                                            groups.put(groupName, group);
+                try (Connection dbConnection = openConnection()) {
+                    if (dbConnection != null && preparedAllGroups != null) {
+                        try (PreparedStatement stmt = dbConnection.prepareStatement(preparedAllGroups)) {
+                            try (ResultSet rs = stmt.executeQuery()) {
+                                while (rs.next()) {
+                                    String groupName = rs.getString(1);
+                                    if (groupName != null) {
+                                        if (!groups.containsKey(groupName) && !removedGroups.containsKey(groupName)) {
+                                            Group group = findGroupInternal(dbConnection, groupName);
+                                            if (group != null) {
+                                                groups.put(groupName, group);
+                                            }
                                         }
                                     }
                                 }
                             }
                         }
-                    } catch (SQLException e) {
-                        log.error(sm.getString("dataSourceUserDatabase.exception"), e);
-                    } finally {
-                        closeConnection(dbConnection);
                     }
+                } catch (SQLException e) {
+                    log.error(sm.getString("dataSourceUserDatabase.exception"), e);
                 }
                 return groups.values().iterator();
             } finally {
@@ -506,27 +505,26 @@ public class DataSourceUserDatabase extends SparseUserDatabase {
                 roles.putAll(createdRoles);
                 roles.putAll(modifiedRoles);
 
-                Connection dbConnection = openConnection();
-                if (dbConnection != null && preparedAllRoles != null) {
-                    try (PreparedStatement stmt = dbConnection.prepareStatement(preparedAllRoles)) {
-                        try (ResultSet rs = stmt.executeQuery()) {
-                            while (rs.next()) {
-                                String roleName = rs.getString(1);
-                                if (roleName != null) {
-                                    if (!roles.containsKey(roleName) && !removedRoles.containsKey(roleName)) {
-                                        Role role = findRoleInternal(dbConnection, roleName);
-                                        if (role != null) {
-                                            roles.put(roleName, role);
+                try (Connection dbConnection = openConnection()) {
+                    if (dbConnection != null && preparedAllRoles != null) {
+                        try (PreparedStatement stmt = dbConnection.prepareStatement(preparedAllRoles)) {
+                            try (ResultSet rs = stmt.executeQuery()) {
+                                while (rs.next()) {
+                                    String roleName = rs.getString(1);
+                                    if (roleName != null) {
+                                        if (!roles.containsKey(roleName) && !removedRoles.containsKey(roleName)) {
+                                            Role role = findRoleInternal(dbConnection, roleName);
+                                            if (role != null) {
+                                                roles.put(roleName, role);
+                                            }
                                         }
                                     }
                                 }
                             }
                         }
-                    } catch (SQLException e) {
-                        log.error(sm.getString("dataSourceUserDatabase.exception"), e);
-                    } finally {
-                        closeConnection(dbConnection);
                     }
+                } catch (SQLException e) {
+                    log.error(sm.getString("dataSourceUserDatabase.exception"), e);
                 }
                 return roles.values().iterator();
             } finally {
