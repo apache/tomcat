d3f3359d2ba3abdd8b9719ab9e3c45c51610305d
Mark Thomas <markt@apache.org>
2023-08-07 17:13:55 +0100

    Fix race condition that was causing intermittent CI failures
    Ensure that, if there is no request body, the stream is notified that
    end-of-stream has been received before passing processing of the request
    to a container thread.

--- java/org/apache/coyote/http2/Http2Parser.java
+++ java/org/apache/coyote/http2/Http2Parser.java
@@ -626,10 +626,9 @@ class Http2Parser {
         hpackDecoder.getHeaderEmitter().validateHeaders();
 
         synchronized (output) {
-            output.headersEnd(streamId);
+            output.headersEnd(streamId, headersEndStream);
 
             if (headersEndStream) {
-                output.receivedEndOfStream(streamId);
                 headersEndStream = false;
             }
         }
@@ -777,7 +776,7 @@ class Http2Parser {
 
         void headersContinue(int payloadSize, boolean endOfHeaders);
 
-        void headersEnd(int streamId) throws Http2Exception;
+        void headersEnd(int streamId, boolean endOfStream) throws Http2Exception;
 
         // Reset frames
         void reset(int streamId, long errorCode) throws Http2Exception;
