apache/tomcat

commit 6353e78658ea09c5056943901877f0b6ed31faae

Minor optimization

java/org/apache/catalina/mapper/Mapper.java
@@ -1337,16 +1337,18 @@ public final class Mapper {
             len = end - start;
         }
         for (int i = 0; (i < len) && (result == 0); i++) {
-            if (c[i + start] > compareTo.charAt(i)) {
+            char nameChar = c[i + start];
+            char compareToChar = compareTo.charAt(i);
+            if (nameChar > compareToChar) {
                 result = 1;
-            } else if (c[i + start] < compareTo.charAt(i)) {
+            } else if (nameChar < compareToChar) {
                 result = -1;
             }
         }
         if (result == 0) {
-            if (compareTo.length() > (end - start)) {
+            if (len > (end - start)) {
                 result = -1;
-            } else if (compareTo.length() < (end - start)) {
+            } else if (len < (end - start)) {
                 result = 1;
             }
         }
@@ -1366,16 +1368,18 @@ public final class Mapper {
             len = end - start;
         }
         for (int i = 0; (i < len) && (result == 0); i++) {
-            if (Ascii.toLower(c[i + start]) > Ascii.toLower(compareTo.charAt(i))) {
+            int nameLower = Ascii.toLower(c[i + start]);
+            int compareLower = Ascii.toLower(compareTo.charAt(i));
+            if (nameLower > compareLower) {
                 result = 1;
-            } else if (Ascii.toLower(c[i + start]) < Ascii.toLower(compareTo.charAt(i))) {
+            } else if (nameLower < compareLower) {
                 result = -1;
             }
         }
         if (result == 0) {
-            if (compareTo.length() > (end - start)) {
+            if (len > (end - start)) {
                 result = -1;
-            } else if (compareTo.length() < (end - start)) {
+            } else if (len < (end - start)) {
                 result = 1;
             }
         }

commit 4d3f1bdbf3e7a626edfea97f1b2853ee27076264

Fix a regression in 6353e786

java/org/apache/catalina/mapper/Mapper.java
@@ -1314,7 +1314,8 @@ public final class Mapper {
     private static int compare(CharChunk name, int start, int end, String compareTo) {
         int result = 0;
         char[] c = name.getBuffer();
-        int len = compareTo.length();
+        int compareLen = compareTo.length();
+        int len = compareLen;
         if ((end - start) < len) {
             len = end - start;
         }
@@ -1328,9 +1329,9 @@ public final class Mapper {
             }
         }
         if (result == 0) {
-            if (len > (end - start)) {
+            if (compareLen > (end - start)) {
                 result = -1;
-            } else if (len < (end - start)) {
+            } else if (compareLen < (end - start)) {
                 result = 1;
             }
         }
@@ -1345,7 +1346,8 @@ public final class Mapper {
     private static int compareIgnoreCase(CharChunk name, int start, int end, String compareTo) {
         int result = 0;
         char[] c = name.getBuffer();
-        int len = compareTo.length();
+        int compareLen = compareTo.length();
+        int len = compareLen;
         if ((end - start) < len) {
             len = end - start;
         }
@@ -1359,9 +1361,9 @@ public final class Mapper {
             }
         }
         if (result == 0) {
-            if (len > (end - start)) {
+            if (compareLen > (end - start)) {
                 result = -1;
-            } else if (len < (end - start)) {
+            } else if (compareLen < (end - start)) {
                 result = 1;
             }
         }