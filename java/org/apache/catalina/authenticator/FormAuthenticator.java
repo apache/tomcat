e3703c9abb8fe0d5602f6ba8a8f11d4b6940815a
Mark Thomas <markt@apache.org>
2023-08-22 11:31:23 -0700

    Avoid protocol relative redirects

--- java/org/apache/catalina/authenticator/FormAuthenticator.java
+++ java/org/apache/catalina/authenticator/FormAuthenticator.java
@@ -742,6 +742,12 @@ public class FormAuthenticator extends AuthenticatorBase {
             sb.append('?');
             sb.append(saved.getQueryString());
         }
+
+        // Avoid protocol relative redirects
+        while (sb.length() > 1 && sb.charAt(1) == '/') {
+            sb.deleteCharAt(0);
+        }
+
         return sb.toString();
     }
 }
