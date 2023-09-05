09f6abfa7145f44ec7a71acae9815d0b1d773d7e
greeng00se <thegreengoose7@gmail.com>
2023-09-05 16:15:04 +0900

    Removed unnecessary brackets

--- java/org/apache/coyote/Request.java
+++ java/org/apache/coyote/Request.java
@@ -437,7 +437,7 @@ public final class Request {
 
     public String getContentType() {
         contentType();
-        if ((contentTypeMB == null) || contentTypeMB.isNull()) {
+        if (contentTypeMB == null || contentTypeMB.isNull()) {
             return null;
         }
         return contentTypeMB.toString();
