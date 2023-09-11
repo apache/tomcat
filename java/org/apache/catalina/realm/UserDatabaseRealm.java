8cb1f07ac9e935b6144ac55e3ce4b5b576697128
remm <remm@apache.org>
2023-09-11 12:59:15 +0200

    Fix possible NPE
    Found by coverity.

--- java/org/apache/catalina/realm/UserDatabaseRealm.java
+++ java/org/apache/catalina/realm/UserDatabaseRealm.java
@@ -28,6 +28,7 @@ import javax.naming.Context;
 import org.apache.catalina.Group;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.Role;
+import org.apache.catalina.Server;
 import org.apache.catalina.User;
 import org.apache.catalina.UserDatabase;
 import org.apache.catalina.users.MemoryUserDatabase;
@@ -184,6 +185,11 @@ public class UserDatabaseRealm extends RealmBase {
                             context = ContextBindings.getClassLoader();
                             context = (Context) context.lookup("comp/env");
                         } else {
+                            Server server = getServer();
+                            if (server == null) {
+                                containerLog.error(sm.getString("userDatabaseRealm.noNamingContext"));
+                                return null;
+                            }
                             context = getServer().getGlobalNamingContext();
                         }
                         database = (UserDatabase) context.lookup(resourceName);
