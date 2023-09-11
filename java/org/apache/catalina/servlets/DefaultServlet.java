6dd84aa08dff75e2251c478455244604b49ac32f
remm <remm@apache.org>
2023-09-11 16:11:25 +0200

    Cleanup
    Found by coverity.

--- java/org/apache/catalina/servlets/DefaultServlet.java
+++ java/org/apache/catalina/servlets/DefaultServlet.java
@@ -1317,7 +1317,10 @@ public class DefaultServlet extends HttpServlet {
         is.reset();
         if (stripBom) {
             while (skip-- > 0) {
-                is.read();
+                if (is.read() < 0) {
+                    // Ignore since included
+                    break;
+                }
             }
         }
     }
