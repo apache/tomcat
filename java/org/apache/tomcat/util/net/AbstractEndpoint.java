c1fc8ce3a50ef7e88a635e11377757a80254c452
remm <remm@apache.org>
2023-09-19 14:09:17 +0200

    Avoid needless exception wrapping

--- java/org/apache/tomcat/util/net/AbstractEndpoint.java
+++ java/org/apache/tomcat/util/net/AbstractEndpoint.java
@@ -279,6 +279,9 @@ public abstract class AbstractEndpoint<S,U> {
             try {
                 createSSLContext(sslHostConfig);
             } catch (Exception e) {
+                if (e instanceof IllegalArgumentException) {
+                    throw e;
+                }
                 throw new IllegalArgumentException(e);
             }
         }
