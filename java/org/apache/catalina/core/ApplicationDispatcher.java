abdf1d8e8a8f12d1956f2828bf22b9846a6dcdef
Mark Thomas <markt@apache.org>
2023-11-08 14:59:41 +0000

    Differentiate request cancellation from a bad request
    Introduce BadRequestException as a sub-class of IOException and the
    super class of ClientAbortException. This allows Tomcat to differentiate
    between an invalid request (e.g. invalid HTTP headers) and a request
    that the user cancelled before it completed.

--- java/org/apache/catalina/core/ApplicationDispatcher.java
+++ java/org/apache/catalina/core/ApplicationDispatcher.java
@@ -39,7 +39,7 @@ import org.apache.catalina.AsyncDispatcher;
 import org.apache.catalina.Context;
 import org.apache.catalina.Globals;
 import org.apache.catalina.Wrapper;
-import org.apache.catalina.connector.ClientAbortException;
+import org.apache.catalina.connector.BadRequestException;
 import org.apache.catalina.connector.Request;
 import org.apache.catalina.connector.RequestFacade;
 import org.apache.catalina.connector.Response;
@@ -662,7 +662,7 @@ final class ApplicationDispatcher implements AsyncDispatcher, RequestDispatcher 
                 filterChain.doFilter(request, response);
             }
             // Servlet Service Method is called by the FilterChain
-        } catch (ClientAbortException e) {
+        } catch (BadRequestException e) {
             ioException = e;
         } catch (IOException e) {
             wrapper.getLogger().error(sm.getString("applicationDispatcher.serviceException", wrapper.getName()), e);
@@ -673,7 +673,7 @@ final class ApplicationDispatcher implements AsyncDispatcher, RequestDispatcher 
             wrapper.unavailable(e);
         } catch (ServletException e) {
             Throwable rootCause = StandardWrapper.getRootCause(e);
-            if (!(rootCause instanceof ClientAbortException)) {
+            if (!(rootCause instanceof BadRequestException)) {
                 wrapper.getLogger().error(sm.getString("applicationDispatcher.serviceException", wrapper.getName()),
                         rootCause);
             }
