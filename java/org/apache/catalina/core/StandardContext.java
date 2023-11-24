8649a89e1b6170b73bca01d60d6425e9b59bd9f1
Mark Thomas <markt@apache.org>
2023-11-24 17:15:16 +0000

    Fix warnings / widen background process / lifecycle fix

--- java/org/apache/catalina/core/StandardContext.java
+++ java/org/apache/catalina/core/StandardContext.java
@@ -5264,7 +5264,7 @@ public class StandardContext extends ContainerBase implements Context, Notificat
 
 
     @Override
-    public void backgroundProcess() {
+    public synchronized void backgroundProcess() {
 
         if (!getState().isAvailable()) {
             return;
