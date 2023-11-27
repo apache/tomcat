fc8288fd66e17866a1dd449524069936b7f0834b
Mark Thomas <markt@apache.org>
2023-11-27 13:34:48 +0000

    Test case for BZ 68119
    https://bz.apache.org/bugzilla/show_bug.cgi?id=68119

---
+++ test/javax/el/TestCompositeELResolverPerformance.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package javax.el;
+
+import org.junit.Test;
+
+public class TestCompositeELResolverPerformance {
+
+    /*
+     * https://bz.apache.org/bugzilla/show_bug.cgi?id=68119
+     */
+    @Test
+    public void testConvertToType() throws Exception {
+        ELManager manager = new ELManager();
+        ELContext context = manager.getELContext();
+        ELResolver resolver = context.getELResolver();
+
+        // Warm-up
+        doConversion(context, resolver);
+
+        long start = System.nanoTime();
+        doConversion(context, resolver);
+        long duration = System.nanoTime() - start;
+
+        System.out.println("convertToType performance test complete in " + duration + "ns");
+    }
+
+
+    private void doConversion(ELContext context, ELResolver resolver) {
+        for (int i = 0; i < 10000000; i++) {
+            resolver.convertToType(context, "This is a String", String.class);
+        }
+    }
+}
