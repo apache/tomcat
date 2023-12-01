f33ee501c309d57e2f168af8b5e67d9823c78c2b
remm <remm@apache.org>
2023-12-01 16:52:04 +0100

    Move around some declarations after some symbol visibility changes
    Update jextract.
    Remove callback helpers that are not actually used (they seem to use
    bind).
    No actual change to symbols or deprecation status.

--- java/org/apache/tomcat/util/openssl/openssl_h_Macros.java
+++ java/org/apache/tomcat/util/openssl/openssl_h_Macros.java
@@ -17,7 +17,10 @@
 
 package org.apache.tomcat.util.openssl;
 
+import java.lang.foreign.FunctionDescriptor;
 import java.lang.foreign.MemorySegment;
+import java.lang.foreign.ValueLayout;
+import java.lang.invoke.MethodHandle;
 
 import static org.apache.tomcat.util.openssl.openssl_h.*;
 
@@ -59,7 +62,7 @@ public class openssl_h_Macros {
      * Get the session cache size. # define SSL_CTX_sess_get_cache_size(sslCtx) \ SSL_CTX_ctrl(sslCtx,
      * SSL_CTRL_GET_SESS_CACHE_SIZE, 0, NULL)
      *
-     * @param sslCtx    the SSL context
+     * @param sslCtx the SSL context
      *
      * @return > 0 if successful
      */
@@ -244,5 +247,352 @@ public class openssl_h_Macros {
         return X509_LOOKUP_ctrl(x509Lookup, X509_L_FILE_LOAD(), name, X509_FILETYPE_PEM(), MemorySegment.NULL);
     }
 
+    // Other symbols that have questionable visibility from jextract
+
+    /**
+     * {@snippet lang = c : * void X509_free(X509* a);
+     * }
+     */
+    public static MethodHandle X509_free$MH() {
+        class Holder {
+            static final FunctionDescriptor DESC = FunctionDescriptor.ofVoid(RuntimeHelper.POINTER);
+
+            static final MethodHandle MH = RuntimeHelper.downcallHandle("X509_free", DESC);
+        }
+        return RuntimeHelper.requireNonNull(Holder.MH, "X509_free");
+    }
+
+    public static void X509_free(MemorySegment a) {
+        var mh$ = X509_free$MH();
+        try {
+            mh$.invokeExact(a);
+        } catch (Throwable ex$) {
+            throw new AssertionError("should not reach here", ex$);
+        }
+    }
+
+    /**
+     * {@snippet lang = c : * X509* d2i_X509(X509** a, unsigned char** in, long len);
+     * }
+     */
+    public static MethodHandle d2i_X509$MH() {
+        class Holder {
+            static final FunctionDescriptor DESC = FunctionDescriptor.of(RuntimeHelper.POINTER, RuntimeHelper.POINTER,
+                    RuntimeHelper.POINTER, ValueLayout.JAVA_LONG);
+
+            static final MethodHandle MH = RuntimeHelper.downcallHandle("d2i_X509", DESC);
+        }
+        return RuntimeHelper.requireNonNull(Holder.MH, "d2i_X509");
+    }
+
+    public static MemorySegment d2i_X509(MemorySegment a, MemorySegment in, long len) {
+        var mh$ = d2i_X509$MH();
+        try {
+            return (MemorySegment) mh$.invokeExact(a, in, len);
+        } catch (Throwable ex$) {
+            throw new AssertionError("should not reach here", ex$);
+        }
+    }
+
+    /**
+     * {@snippet lang = c : * int i2d_X509(const X509* a, unsigned char** out);
+     * }
+     */
+    public static MethodHandle i2d_X509$MH() {
+        class Holder {
+            static final FunctionDescriptor DESC = FunctionDescriptor.of(ValueLayout.JAVA_INT, RuntimeHelper.POINTER,
+                    RuntimeHelper.POINTER);
+
+            static final MethodHandle MH = RuntimeHelper.downcallHandle("i2d_X509", DESC);
+        }
+        return RuntimeHelper.requireNonNull(Holder.MH, "i2d_X509");
+    }
+
+    public static int i2d_X509(MemorySegment a, MemorySegment out) {
+        var mh$ = i2d_X509$MH();
+        try {
+            return (int) mh$.invokeExact(a, out);
+        } catch (Throwable ex$) {
+            throw new AssertionError("should not reach here", ex$);
+        }
+    }
+
+    /**
+     * {@snippet lang = c : * X509* PEM_read_bio_X509_AUX(BIO* out, X509** x, pem_password_cb* cb, void* u);
+     * }
+     */
+    public static MethodHandle PEM_read_bio_X509_AUX$MH() {
+        class Holder {
+            static final FunctionDescriptor DESC = FunctionDescriptor.of(RuntimeHelper.POINTER, RuntimeHelper.POINTER,
+                    RuntimeHelper.POINTER, RuntimeHelper.POINTER, RuntimeHelper.POINTER);
+
+            static final MethodHandle MH = RuntimeHelper.downcallHandle("PEM_read_bio_X509_AUX", DESC);
+        }
+        return RuntimeHelper.requireNonNull(Holder.MH, "PEM_read_bio_X509_AUX");
+    }
+
+    public static MemorySegment PEM_read_bio_X509_AUX(MemorySegment out, MemorySegment x, MemorySegment cb,
+            MemorySegment u) {
+        var mh$ = PEM_read_bio_X509_AUX$MH();
+        try {
+            return (MemorySegment) mh$.invokeExact(out, x, cb, u);
+        } catch (Throwable ex$) {
+            throw new AssertionError("should not reach here", ex$);
+        }
+    }
+
+    /**
+     * {@snippet lang = c
+     * : * EC_GROUP* PEM_read_bio_ECPKParameters(BIO* out, EC_GROUP** x, pem_password_cb* cb, void* u);
+     * }
+     */
+    public static MethodHandle PEM_read_bio_ECPKParameters$MH() {
+        class Holder {
+            static final FunctionDescriptor DESC = FunctionDescriptor.of(RuntimeHelper.POINTER, RuntimeHelper.POINTER,
+                    RuntimeHelper.POINTER, RuntimeHelper.POINTER, RuntimeHelper.POINTER);
+
+            static final MethodHandle MH = RuntimeHelper.downcallHandle("PEM_read_bio_ECPKParameters", DESC);
+        }
+        return RuntimeHelper.requireNonNull(Holder.MH, "PEM_read_bio_ECPKParameters");
+    }
+
+    @Deprecated
+    public static MemorySegment PEM_read_bio_ECPKParameters(MemorySegment out, MemorySegment x, MemorySegment cb,
+            MemorySegment u) {
+        var mh$ = PEM_read_bio_ECPKParameters$MH();
+        try {
+            return (MemorySegment) mh$.invokeExact(out, x, cb, u);
+        } catch (Throwable ex$) {
+            throw new AssertionError("should not reach here", ex$);
+        }
+    }
+
+    /**
+     * {@snippet lang = c : * DH* PEM_read_bio_DHparams(BIO* out, DH** x, pem_password_cb* cb, void* u);
+     * }
+     */
+    public static MethodHandle PEM_read_bio_DHparams$MH() {
+        class Holder {
+            static final FunctionDescriptor DESC = FunctionDescriptor.of(RuntimeHelper.POINTER, RuntimeHelper.POINTER,
+                    RuntimeHelper.POINTER, RuntimeHelper.POINTER, RuntimeHelper.POINTER);
+
+            static final MethodHandle MH = RuntimeHelper.downcallHandle("PEM_read_bio_DHparams", DESC);
+        }
+        return RuntimeHelper.requireNonNull(Holder.MH, "PEM_read_bio_DHparams");
+    }
+
+    @Deprecated
+    public static MemorySegment PEM_read_bio_DHparams(MemorySegment out, MemorySegment x, MemorySegment cb,
+            MemorySegment u) {
+        var mh$ = PEM_read_bio_DHparams$MH();
+        try {
+            return (MemorySegment) mh$.invokeExact(out, x, cb, u);
+        } catch (Throwable ex$) {
+            throw new AssertionError("should not reach here", ex$);
+        }
+    }
+
+    /**
+     * {@snippet lang = c : * EVP_PKEY* PEM_read_bio_PrivateKey(BIO* out, EVP_PKEY** x, pem_password_cb* cb, void* u);
+     * }
+     */
+    public static MethodHandle PEM_read_bio_PrivateKey$MH() {
+        class Holder {
+            static final FunctionDescriptor DESC = FunctionDescriptor.of(RuntimeHelper.POINTER, RuntimeHelper.POINTER,
+                    RuntimeHelper.POINTER, RuntimeHelper.POINTER, RuntimeHelper.POINTER);
+
+            static final MethodHandle MH = RuntimeHelper.downcallHandle("PEM_read_bio_PrivateKey", DESC);
+        }
+        return RuntimeHelper.requireNonNull(Holder.MH, "PEM_read_bio_PrivateKey");
+    }
+
+    public static MemorySegment PEM_read_bio_PrivateKey(MemorySegment out, MemorySegment x, MemorySegment cb,
+            MemorySegment u) {
+        var mh$ = PEM_read_bio_PrivateKey$MH();
+        try {
+            return (MemorySegment) mh$.invokeExact(out, x, cb, u);
+        } catch (Throwable ex$) {
+            throw new AssertionError("should not reach here", ex$);
+        }
+    }
+
+    /**
+     * {@snippet lang = c : * void PKCS12_free(PKCS12* a);
+     * }
+     */
+    public static MethodHandle PKCS12_free$MH() {
+        class Holder {
+            static final FunctionDescriptor DESC = FunctionDescriptor.ofVoid(RuntimeHelper.POINTER);
+
+            static final MethodHandle MH = RuntimeHelper.downcallHandle("PKCS12_free", DESC);
+        }
+        return RuntimeHelper.requireNonNull(Holder.MH, "PKCS12_free");
+    }
+
+    public static void PKCS12_free(MemorySegment a) {
+        var mh$ = PKCS12_free$MH();
+        try {
+            mh$.invokeExact(a);
+        } catch (Throwable ex$) {
+            throw new AssertionError("should not reach here", ex$);
+        }
+    }
+
+
+    /**
+     * {@snippet lang = c : * void OCSP_BASICRESP_free(OCSP_BASICRESP* a);
+     * }
+     */
+    public static MethodHandle OCSP_BASICRESP_free$MH() {
+        class Holder {
+            static final FunctionDescriptor DESC = FunctionDescriptor.ofVoid(RuntimeHelper.POINTER);
+
+            static final MethodHandle MH = RuntimeHelper.downcallHandle("OCSP_BASICRESP_free", DESC);
+        }
+        return RuntimeHelper.requireNonNull(Holder.MH, "OCSP_BASICRESP_free");
+    }
+
+    public static void OCSP_BASICRESP_free(MemorySegment a) {
+        var mh$ = OCSP_BASICRESP_free$MH();
+        try {
+            mh$.invokeExact(a);
+        } catch (Throwable ex$) {
+            throw new AssertionError("should not reach here", ex$);
+        }
+    }
+
+    /**
+     * {@snippet lang = c : * void OCSP_RESPONSE_free(OCSP_RESPONSE* a);
+     * }
+     */
+    public static MethodHandle OCSP_RESPONSE_free$MH() {
+        class Holder {
+            static final FunctionDescriptor DESC = FunctionDescriptor.ofVoid(RuntimeHelper.POINTER);
+
+            static final MethodHandle MH = RuntimeHelper.downcallHandle("OCSP_RESPONSE_free", DESC);
+        }
+        return RuntimeHelper.requireNonNull(Holder.MH, "OCSP_RESPONSE_free");
+    }
+
+    public static void OCSP_RESPONSE_free(MemorySegment a) {
+        var mh$ = OCSP_RESPONSE_free$MH();
+        try {
+            mh$.invokeExact(a);
+        } catch (Throwable ex$) {
+            throw new AssertionError("should not reach here", ex$);
+        }
+    }
+
+    /**
+     * {@snippet lang = c : * OCSP_RESPONSE* d2i_OCSP_RESPONSE(OCSP_RESPONSE** a, unsigned char** in, long len);
+     * }
+     */
+    public static MethodHandle d2i_OCSP_RESPONSE$MH() {
+        class Holder {
+            static final FunctionDescriptor DESC = FunctionDescriptor.of(RuntimeHelper.POINTER, RuntimeHelper.POINTER,
+                    RuntimeHelper.POINTER, ValueLayout.JAVA_LONG);
+
+            static final MethodHandle MH = RuntimeHelper.downcallHandle("d2i_OCSP_RESPONSE", DESC);
+        }
+        return RuntimeHelper.requireNonNull(Holder.MH, "d2i_OCSP_RESPONSE");
+    }
+
+    public static MemorySegment d2i_OCSP_RESPONSE(MemorySegment a, MemorySegment in, long len) {
+        var mh$ = d2i_OCSP_RESPONSE$MH();
+        try {
+            return (MemorySegment) mh$.invokeExact(a, in, len);
+        } catch (Throwable ex$) {
+            throw new AssertionError("should not reach here", ex$);
+        }
+    }
+
+    /**
+     * {@snippet lang = c : * void OCSP_CERTID_free(OCSP_CERTID* a);
+     * }
+     */
+    public static MethodHandle OCSP_CERTID_free$MH() {
+        class Holder {
+            static final FunctionDescriptor DESC = FunctionDescriptor.ofVoid(RuntimeHelper.POINTER);
+
+            static final MethodHandle MH = RuntimeHelper.downcallHandle("OCSP_CERTID_free", DESC);
+        }
+        return RuntimeHelper.requireNonNull(Holder.MH, "OCSP_CERTID_free");
+    }
+
+    public static void OCSP_CERTID_free(MemorySegment a) {
+        var mh$ = OCSP_CERTID_free$MH();
+        try {
+            mh$.invokeExact(a);
+        } catch (Throwable ex$) {
+            throw new AssertionError("should not reach here", ex$);
+        }
+    }
+
+    /**
+     * {@snippet lang = c : * OCSP_REQUEST* OCSP_REQUEST_new();
+     * }
+     */
+    public static MethodHandle OCSP_REQUEST_new$MH() {
+        class Holder {
+            static final FunctionDescriptor DESC = FunctionDescriptor.of(RuntimeHelper.POINTER);
+
+            static final MethodHandle MH = RuntimeHelper.downcallHandle("OCSP_REQUEST_new", DESC);
+        }
+        return RuntimeHelper.requireNonNull(Holder.MH, "OCSP_REQUEST_new");
+    }
+
+    public static MemorySegment OCSP_REQUEST_new() {
+        var mh$ = OCSP_REQUEST_new$MH();
+        try {
+            return (MemorySegment) mh$.invokeExact();
+        } catch (Throwable ex$) {
+            throw new AssertionError("should not reach here", ex$);
+        }
+    }
+
+    /**
+     * {@snippet lang = c : * void OCSP_REQUEST_free(OCSP_REQUEST* a);
+     * }
+     */
+    public static MethodHandle OCSP_REQUEST_free$MH() {
+        class Holder {
+            static final FunctionDescriptor DESC = FunctionDescriptor.ofVoid(RuntimeHelper.POINTER);
+
+            static final MethodHandle MH = RuntimeHelper.downcallHandle("OCSP_REQUEST_free", DESC);
+        }
+        return RuntimeHelper.requireNonNull(Holder.MH, "OCSP_REQUEST_free");
+    }
+
+    public static void OCSP_REQUEST_free(MemorySegment a) {
+        var mh$ = OCSP_REQUEST_free$MH();
+        try {
+            mh$.invokeExact(a);
+        } catch (Throwable ex$) {
+            throw new AssertionError("should not reach here", ex$);
+        }
+    }
+
+    /**
+     * {@snippet lang = c : * int i2d_OCSP_REQUEST(const OCSP_REQUEST* a, unsigned char** out);
+     * }
+     */
+    public static MethodHandle i2d_OCSP_REQUEST$MH() {
+        class Holder {
+            static final FunctionDescriptor DESC = FunctionDescriptor.of(ValueLayout.JAVA_INT, RuntimeHelper.POINTER,
+                    RuntimeHelper.POINTER);
+
+            static final MethodHandle MH = RuntimeHelper.downcallHandle("i2d_OCSP_REQUEST", DESC);
+        }
+        return RuntimeHelper.requireNonNull(Holder.MH, "i2d_OCSP_REQUEST");
+    }
+
+    public static int i2d_OCSP_REQUEST(MemorySegment a, MemorySegment out) {
+        var mh$ = i2d_OCSP_REQUEST$MH();
+        try {
+            return (int) mh$.invokeExact(a, out);
+        } catch (Throwable ex$) {
+            throw new AssertionError("should not reach here", ex$);
+        }
+    }
 }
 
