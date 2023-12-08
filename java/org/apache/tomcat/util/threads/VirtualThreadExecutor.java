2b3f0f09641e0d8504a114cf296a18d66039266b
Mark Thomas <markt@apache.org>
2023-12-08 10:26:49 +0000

    Improvements to French translations. (remm)

--- java/org/apache/tomcat/util/threads/VirtualThreadExecutor.java
+++ java/org/apache/tomcat/util/threads/VirtualThreadExecutor.java
@@ -16,14 +16,24 @@
  */
 package org.apache.tomcat.util.threads;
 
-import java.util.concurrent.Executor;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.AbstractExecutorService;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.tomcat.util.compat.JreCompat;
+import org.apache.tomcat.util.res.StringManager;
 
 /**
  * An executor that uses a new virtual thread for each task.
  */
-public class VirtualThreadExecutor implements Executor {
+public class VirtualThreadExecutor extends AbstractExecutorService {
+
+    private static final StringManager sm = StringManager.getManager(VirtualThreadExecutor.class);
+
+    private CountDownLatch shutdown = new CountDownLatch(1);
 
     private final JreCompat jreCompat = JreCompat.getInstance();
 
@@ -35,6 +45,54 @@ public class VirtualThreadExecutor implements Executor {
 
     @Override
     public void execute(Runnable command) {
+        if (isShutdown()) {
+            throw new RejectedExecutionException(
+                    sm.getString("virtualThreadExecutor.taskRejected", command.toString(), this.toString()));
+        }
         jreCompat.threadBuilderStart(threadBuilder, command);
+    }
+
+    @Override
+    public void shutdown() {
+        shutdown.countDown();
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * The VirtualThreadExecutor does not track in-progress tasks so calling this method is equivalent to calling
+     * {@link #shutdown()}.
+     */
+    @Override
+    public List<Runnable> shutdownNow() {
+        shutdown();
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean isShutdown() {
+        return shutdown.getCount() == 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * The VirtualThreadExecutor does not track in-progress tasks so calling this method is equivalent to calling
+     * {@link #isShutdown()}.
+     */
+    @Override
+    public boolean isTerminated() {
+        return isShutdown();
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * The VirtualThreadExecutor does not track in-progress tasks so calling this method is effectively waiting for
+     * {@link #shutdown()} to be called.
+     */
+    @Override
+    public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
+        return shutdown.await(timeout, unit);
     }
 }
