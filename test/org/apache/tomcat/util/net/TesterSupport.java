c0630c9ab7c0aa794698cdd6f5187864702f8aee
Mark Thomas <markt@apache.org>
2024-01-03 19:45:15 +0000

    Test is skipped due to JSSE vs Apr, not renegotiation
    Don't report a test as skipped if it can never pass

--- test/org/apache/tomcat/util/net/TesterSupport.java
+++ test/org/apache/tomcat/util/net/TesterSupport.java
@@ -231,16 +231,6 @@ public final class TesterSupport {
         return ks;
     }
 
-    public static boolean isRenegotiationSupported(Tomcat tomcat) {
-        String protocol = tomcat.getConnector().getProtocolHandlerClassName();
-        if (protocol.contains("Apr")) {
-            // Disabled by default in 1.1.20 windows binary (2010-07-27)
-            return false;
-        }
-
-        return true;
-    }
-
     protected static boolean isClientRenegotiationSupported(Tomcat tomcat) {
         String protocol = tomcat.getConnector().getProtocolHandlerClassName();
         if (protocol.contains("Apr")) {
