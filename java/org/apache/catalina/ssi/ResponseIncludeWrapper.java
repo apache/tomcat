87f2834c256a26323a6a53d5aa3e10168e073540
Mark Thomas <markt@apache.org>
2024-01-15 13:32:45 +0000

    Fix typo

--- java/org/apache/catalina/ssi/ResponseIncludeWrapper.java
+++ java/org/apache/catalina/ssi/ResponseIncludeWrapper.java
@@ -77,7 +77,7 @@ public class ResponseIncludeWrapper extends HttpServletResponseWrapper {
 
 
     /**
-     * Return a printwriter, throws and exception if an OutputStream already been returned.
+     * Return a printwriter, throws an exception if an OutputStream already been returned.
      *
      * @return a PrintWriter object
      *
@@ -98,7 +98,7 @@ public class ResponseIncludeWrapper extends HttpServletResponseWrapper {
 
 
     /**
-     * Return an OutputStream, throws and exception if a printwriter already been returned.
+     * Return an OutputStream, throws an exception if a printwriter already been returned.
      *
      * @return an OutputStream object
      *
