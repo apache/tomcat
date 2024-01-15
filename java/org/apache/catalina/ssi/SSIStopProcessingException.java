99754a3941769ff6b89fd530c63571ea1fba6641
Mark Thomas <markt@apache.org>
2024-01-15 13:03:24 +0000

    Code clean-up. Formatting. No functional change.

--- java/org/apache/catalina/ssi/SSIStopProcessingException.java
+++ java/org/apache/catalina/ssi/SSIStopProcessingException.java
@@ -18,9 +18,8 @@ package org.apache.catalina.ssi;
 
 
 /**
- * Exception used to tell SSIProcessor that it should stop processing SSI
- * commands. This is used to mimic the Apache behavior in #set with invalid
- * attributes.
+ * Exception used to tell SSIProcessor that it should stop processing SSI commands. This is used to mimic the Apache
+ * behavior in #set with invalid attributes.
  *
  * @author Paul Speed
  * @author Dan Sandberg
