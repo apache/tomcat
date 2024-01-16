3fe81486eff27d54face3310ba28cc4b5ee576e0
remm <remm@apache.org>
2024-01-16 16:38:13 +0100

    Throw an ISE instead of return null
    Found by coverity.

--- java/org/apache/catalina/startup/Tomcat.java
+++ java/org/apache/catalina/startup/Tomcat.java
@@ -385,7 +385,7 @@ public class Tomcat {
         // will do class for name and set init params
         Wrapper sw = ctx.createWrapper();
         if (sw == null) {
-            return null;
+            throw new IllegalStateException(sm.getString("tomcat.noWrapper"));
         }
         sw.setServletClass(servletClass);
         sw.setName(servletName);
