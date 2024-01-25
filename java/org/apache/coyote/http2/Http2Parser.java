810f49d5ff6d64b704af85d5b8d0aab9ec3c83f5
Mark Thomas <markt@apache.org>
2024-01-25 12:13:55 +0000

    Report HTTP/2 header parsing errors earlier

--- java/org/apache/coyote/http2/Http2Parser.java
+++ java/org/apache/coyote/http2/Http2Parser.java
@@ -266,6 +266,9 @@ class Http2Parser {
 
         swallowPayload(streamId, FrameType.HEADERS.getId(), padLength, true, buffer);
 
+        // Validate the headers so far
+        hpackDecoder.getHeaderEmitter().validateHeaders();
+
         if (Flags.isEndOfHeaders(flags)) {
             onHeadersComplete(streamId);
         } else {
@@ -428,6 +431,9 @@ class Http2Parser {
         output.headersContinue(payloadSize, endOfHeaders);
 
         readHeaderPayload(streamId, payloadSize, buffer);
+
+        // Validate the headers so far
+        hpackDecoder.getHeaderEmitter().validateHeaders();
 
         if (endOfHeaders) {
             headersCurrentStream = -1;
@@ -619,11 +625,6 @@ class Http2Parser {
             throw new ConnectionException(sm.getString("http2Parser.processFrameHeaders.decodingDataLeft"),
                     Http2Error.COMPRESSION_ERROR);
         }
-
-        // Delay validation (and triggering any exception) until this point
-        // since all the headers still have to be read if a StreamException is
-        // going to be thrown.
-        hpackDecoder.getHeaderEmitter().validateHeaders();
 
         synchronized (output) {
             output.headersEnd(streamId, headersEndStream);
