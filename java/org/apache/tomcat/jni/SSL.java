95ca454cc2c1757c3a40d191b19ad029d6e70cff
Mark Thomas <markt@apache.org>
2024-02-07 19:45:25 +0000

    Align with 11.0.x

--- java/org/apache/tomcat/jni/SSL.java
+++ java/org/apache/tomcat/jni/SSL.java
@@ -241,7 +241,7 @@ public final class SSL {
     public static final int SSL_SELECTOR_FAILURE_NO_ADVERTISE = 0;
     public static final int SSL_SELECTOR_FAILURE_CHOOSE_MY_LAST_PROTOCOL = 1;
 
-    /* Return OpenSSL version number (compile time version, if version < 1.1.0) */
+    /* Return OpenSSL version number (run time version) */
     public static native int version();
 
     /* Return OpenSSL version string (run time version) */
