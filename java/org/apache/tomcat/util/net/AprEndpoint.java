517e0966eb438cd3e2cd41e178c17933fbb855ab
remm <remm@apache.org>
2024-02-09 14:16:31 +0100

    Should fix CI running TestSSLHostConfigCompat

--- java/org/apache/tomcat/util/net/AprEndpoint.java
+++ java/org/apache/tomcat/util/net/AprEndpoint.java
@@ -413,7 +413,7 @@ public class AprEndpoint extends AbstractEndpoint<Long,Long> implements SNICallB
                 sslContext.addCertificate(certificate);
             }
 
-            certificate.setSslContext(sslContext);
+            certificate.setSslContextGenerated(sslContext);
             logCertificate(certificate);
         }
 
