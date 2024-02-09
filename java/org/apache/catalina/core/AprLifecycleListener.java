b06eb25ffa6cc7bb25611de19ac9e5e287815c49
Mark Thomas <markt@apache.org>
2024-02-09 12:35:59 +0000

    Rework Native library stability fix.
    There are two aspects to this:
    - Don't let the library temerinate while finalize is clearing
      OpenSSLContext instances
    - Don't allow finalize to clear OpenSSLContext instances once the
      library generaiton that created that instance has terminated

--- java/org/apache/catalina/core/AprLifecycleListener.java
+++ java/org/apache/catalina/core/AprLifecycleListener.java
@@ -169,14 +169,11 @@ public class AprLifecycleListener implements LifecycleListener {
     }
 
     private static void terminateAPR() {
-        Library.terminatePrepare();
-        // Need to force GC here as some components do APR clean-up in finalize()
-        System.gc();
         AprStatus.setAprInitialized(false);
         AprStatus.setAprAvailable(false);
         fipsModeActive = false;
         sslInitialized = false; // Well we cleaned the pool in terminate.
-        Library.terminate();
+        Library.threadSafeTerminate();
     }
 
     @SuppressWarnings("deprecation")
