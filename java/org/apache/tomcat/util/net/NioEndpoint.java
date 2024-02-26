3d665c04d3ca2bab35e002f68073b1ca08bcd235
remm <remm@apache.org>
2024-02-26 16:26:24 +0100

    Remove old javadoc

--- java/org/apache/tomcat/util/net/NioEndpoint.java
+++ java/org/apache/tomcat/util/net/NioEndpoint.java
@@ -64,17 +64,9 @@ import org.apache.tomcat.util.net.Acceptor.AcceptorState;
 import org.apache.tomcat.util.net.jsse.JSSESupport;
 
 /**
- * NIO tailored thread pool, providing the following services:
- * <ul>
- * <li>Socket acceptor thread</li>
- * <li>Socket poller thread</li>
- * <li>Worker threads pool</li>
- * </ul>
- *
- * TODO: Consider using the virtual machine's thread pool.
+ * NIO endpoint.
  *
  * @author Mladen Turk
- * @author Remy Maucherat
  */
 public class NioEndpoint extends AbstractJsseEndpoint<NioChannel,SocketChannel> {
 
