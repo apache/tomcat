3fab83693bb868a46eba20474631d80c93c05e7c
Mark Thomas <markt@apache.org>
2024-03-12 11:37:15 +0000

    Fix IDE warnings

--- java/org/apache/catalina/realm/RealmBase.java
+++ java/org/apache/catalina/realm/RealmBase.java
@@ -1288,6 +1288,8 @@ public abstract class RealmBase extends LifecycleMBeanBase implements Realm {
      * </ul>
      *
      * @param args The parameters passed on the command line
+     *
+     * @throws IOException If an error occurs reading the password file
      */
     public static void main(String args[]) throws IOException {
 
