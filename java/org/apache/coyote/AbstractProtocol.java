9c6580765951a52ea37e585f0f0a5900d7aa74b5
Romain Manni-Bucau <rmannibucau@gmail.com>
2024-03-23 21:19:31 +0100

    Make (Connection)Handler accessible through API from AbstractProtocol#getHandler to ultimately access RequestGroupInfo (getGlobal)

--- java/org/apache/coyote/AbstractProtocol.java
+++ java/org/apache/coyote/AbstractProtocol.java
@@ -466,7 +466,7 @@ public abstract class AbstractProtocol<S> implements ProtocolHandler, MBeanRegis
     }
 
 
-    protected Handler<S> getHandler() {
+    public Handler<S> getHandler() {
         return handler;
     }
 
