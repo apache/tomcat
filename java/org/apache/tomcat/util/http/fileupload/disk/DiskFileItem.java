8d22121fa1121142f8fba95ee330ef42515c047d
Mark Thomas <markt@apache.org>
2024-04-07 22:01:20 +0200

    Improve handling of overflow

--- java/org/apache/tomcat/util/http/fileupload/disk/DiskFileItem.java
+++ java/org/apache/tomcat/util/http/fileupload/disk/DiskFileItem.java
@@ -291,6 +291,7 @@ public class DiskFileItem
      * or {@code null} if the data cannot be read
      *
      * @throws UncheckedIOException if an I/O error occurs
+     * @throws ArithmeticException if the file {@code size} overflows an int
      */
     @Override
     public byte[] get() throws UncheckedIOException {
@@ -301,7 +302,7 @@ public class DiskFileItem
             return cachedContent != null ? cachedContent.clone() : new byte[0];
         }
 
-        final byte[] fileData = new byte[(int) getSize()];
+        final byte[] fileData = new byte[Math.toIntExact(getSize())];
 
         try (InputStream fis = Files.newInputStream(dfos.getFile().toPath())) {
             IOUtils.readFully(fis, fileData);
