72823b4ea06b68ece3663d8d8d998359967673b0
Mark Thomas <markt@apache.org>
2024-04-17 10:42:29 +0100

    Align NioChannel and SecureNioChannel
    Skipping empty writes can provide a performance improvement for some
    frameworks - typically those that call flush() more than they need to

--- java/org/apache/tomcat/util/net/NioChannel.java
+++ java/org/apache/tomcat/util/net/NioChannel.java
@@ -133,6 +133,10 @@ public class NioChannel implements ByteChannel, ScatteringByteChannel, Gathering
     @Override
     public int write(ByteBuffer src) throws IOException {
         checkInterruptStatus();
+        if (!src.hasRemaining()) {
+            // Nothing left to write
+            return 0;
+        }
         return sc.write(src);
     }
 
