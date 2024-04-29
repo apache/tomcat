27a703266f17b9fc4c9f68bdcd0f190f7dd0ba60
Mark Thomas <markt@apache.org>
2024-04-29 12:59:25 +0100

    Align with Commons DBCP 2.12.0
    Based on review of diff

--- java/org/apache/tomcat/dbcp/dbcp2/cpdsadapter/DriverAdapterCPDS.java
+++ java/org/apache/tomcat/dbcp/dbcp2/cpdsadapter/DriverAdapterCPDS.java
@@ -573,11 +573,13 @@ public class DriverAdapterCPDS implements ConnectionPoolDataSource, Referenceabl
         assertInitializationAllowed();
         connectionProperties = props;
         if (connectionProperties != null) {
-            if (connectionProperties.containsKey(Constants.KEY_USER)) {
-                setUser(connectionProperties.getProperty(Constants.KEY_USER));
+            final String user = connectionProperties.getProperty(Constants.KEY_USER);
+            if (user != null) {
+                setUser(user);
             }
-            if (connectionProperties.containsKey(Constants.KEY_PASSWORD)) {
-                setPassword(connectionProperties.getProperty(Constants.KEY_PASSWORD));
+            final String password = connectionProperties.getProperty(Constants.KEY_PASSWORD);
+            if (password != null) {
+                setPassword(password);
             }
         }
     }
