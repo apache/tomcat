f8ea6383c2e9c93f2e2bd748331a55524ac98374
Mark Thomas <markt@apache.org>
2024-04-29 09:03:32 +0100

    Add support for Java 23

--- java/org/apache/jasper/compiler/JDTCompiler.java
+++ java/org/apache/jasper/compiler/JDTCompiler.java
@@ -332,6 +332,11 @@ public class JDTCompiler extends org.apache.jasper.compiler.Compiler {
                 // Java 11.
                 // This is checked against the actual version below.
                 settings.put(CompilerOptions.OPTION_Source, "22");
+            } else if (opt.equals("23")) {
+                // Constant not available in latest ECJ version that runs on
+                // Java 11.
+                // This is checked against the actual version below.
+                settings.put(CompilerOptions.OPTION_Source, "23");
             } else {
                 log.warn(Localizer.getMessage("jsp.warning.unknown.sourceVM", opt));
                 settings.put(CompilerOptions.OPTION_Source, CompilerOptions.VERSION_11);
@@ -417,6 +422,12 @@ public class JDTCompiler extends org.apache.jasper.compiler.Compiler {
                 // This is checked against the actual version below.
                 settings.put(CompilerOptions.OPTION_TargetPlatform, "22");
                 settings.put(CompilerOptions.OPTION_Compliance, "22");
+            } else if (opt.equals("23")) {
+                // Constant not available in latest ECJ version shipped with
+                // Tomcat. May be supported in a snapshot build.
+                // This is checked against the actual version below.
+                settings.put(CompilerOptions.OPTION_TargetPlatform, "23");
+                settings.put(CompilerOptions.OPTION_Compliance, "23");
             } else {
                 log.warn(Localizer.getMessage("jsp.warning.unknown.targetVM", opt));
                 settings.put(CompilerOptions.OPTION_TargetPlatform, CompilerOptions.VERSION_11);
