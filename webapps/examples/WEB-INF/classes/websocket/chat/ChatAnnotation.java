b779a10c6a45c1531690fe2f67e9e7722c35cade
Mark Thomas <markt@apache.org>
2024-05-01 12:31:07 +0100

    Improve handling of concurrent disconnects

--- webapps/examples/WEB-INF/classes/websocket/chat/ChatAnnotation.java
+++ webapps/examples/WEB-INF/classes/websocket/chat/ChatAnnotation.java
@@ -17,6 +17,8 @@
 package websocket.chat;
 
 import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -40,11 +42,16 @@ public class ChatAnnotation {
 
     private static final String GUEST_PREFIX = "Guest";
     private static final AtomicInteger connectionIds = new AtomicInteger(0);
-    private static final Set<ChatAnnotation> connections =
-            new CopyOnWriteArraySet<>();
+    private static final Set<ChatAnnotation> connections = new CopyOnWriteArraySet<>();
 
     private final String nickname;
     private Session session;
+    /*
+     * The queue of messages that may build up while another message is being sent. The thread that sends a message is
+     * responsible for clearing any queue that builds up while that message is being sent.
+     */
+    private Queue<String> messageBacklog = new ArrayDeque<>();
+    private boolean messageInProgress = false;
 
     public ChatAnnotation() {
         nickname = GUEST_PREFIX + connectionIds.getAndIncrement();
@@ -63,8 +70,7 @@ public class ChatAnnotation {
     @OnClose
     public void end() {
         connections.remove(this);
-        String message = String.format("* %s %s",
-                nickname, "has disconnected.");
+        String message = String.format("* %s %s", nickname, "has disconnected.");
         broadcast(message);
     }
 
@@ -72,12 +78,9 @@ public class ChatAnnotation {
     @OnMessage
     public void incoming(String message) {
         // Never trust the client
-        String filteredMessage = String.format("%s: %s",
-                nickname, HTMLFilter.filter(message.toString()));
+        String filteredMessage = String.format("%s: %s", nickname, HTMLFilter.filter(message.toString()));
         broadcast(filteredMessage);
     }
-
-
 
 
     @OnError
@@ -86,23 +89,53 @@ public class ChatAnnotation {
     }
 
 
+    /*
+     * synchronized blocks are limited to operations that are expected to be quick. More specifically, messages are not
+     * sent from within a synchronized block.
+     */
+    private void sendMessage(String msg) throws IOException {
+
+        synchronized (this) {
+            if (messageInProgress) {
+                messageBacklog.add(msg);
+                return;
+            } else {
+                messageInProgress = true;
+            }
+        }
+
+        boolean queueHasMessagesToBeSent = true;
+
+        String messageToSend = msg;
+        do {
+            session.getBasicRemote().sendText(messageToSend);
+            synchronized (this) {
+                messageToSend = messageBacklog.poll();
+                if (messageToSend == null) {
+                    messageInProgress = false;
+                    queueHasMessagesToBeSent = false;
+                }
+            }
+
+        } while (queueHasMessagesToBeSent);
+     }
+
+
     private static void broadcast(String msg) {
         for (ChatAnnotation client : connections) {
             try {
-                synchronized (client) {
-                    client.session.getBasicRemote().sendText(msg);
-                }
+                client.sendMessage(msg);
             } catch (IOException e) {
                 log.debug("Chat Error: Failed to send message to client", e);
-                connections.remove(client);
-                try {
-                    client.session.close();
-                } catch (IOException e1) {
-                    // Ignore
+                if (connections.remove(client)) {
+                    try {
+                        client.session.close();
+                    } catch (IOException e1) {
+                        // Ignore
+                    }
+                    String message = String.format("* %s %s", client.nickname, "has been disconnected.");
+                    broadcast(message);
                 }
-                String message = String.format("* %s %s",
-                        client.nickname, "has been disconnected.");
-                broadcast(message);
             }
         }
     }
