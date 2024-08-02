00da7f8a9da36c4877126cc94be3d4e58c7e9585
Mark Thomas <markt@apache.org>
2024-08-02 10:36:54 +0100

    Revert unnecessary changes for HTTP/2 request/response recycling

--- java/org/apache/catalina/connector/CoyoteAdapter.java
+++ java/org/apache/catalina/connector/CoyoteAdapter.java
@@ -311,19 +311,20 @@ public class CoyoteAdapter implements Adapter {
             // Create objects
             request = connector.createRequest();
             request.setCoyoteRequest(req);
-            req.setNote(ADAPTER_NOTES, request);
-            req.getParameters().setQueryStringCharset(connector.getURICharset());
-        }
-
-        if (response == null) {
             response = connector.createResponse();
             response.setCoyoteResponse(res);
-            res.setNote(ADAPTER_NOTES, response);
-        }
 
-        // Link objects
-        request.setResponse(response);
-        response.setRequest(request);
+            // Link objects
+            request.setResponse(response);
+            response.setRequest(request);
+
+            // Set as notes
+            req.setNote(ADAPTER_NOTES, request);
+            res.setNote(ADAPTER_NOTES, response);
+
+            // Set query string encoding
+            req.getParameters().setQueryStringCharset(connector.getURICharset());
+        }
 
         if (connector.getXpoweredBy()) {
             response.addHeader("X-Powered-By", POWERED_BY);
