9a8848a10d1fb2c166c00372ee2233a7cc252303
remm <remm@apache.org>
2024-09-01 10:21:50 +0200

    Fix imports

--- test/org/apache/coyote/http2/TestHttp2AccessLogs.java
+++ test/org/apache/coyote/http2/TestHttp2AccessLogs.java
@@ -22,10 +22,10 @@ import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Map;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.junit.Assert;
 import org.junit.Test;
