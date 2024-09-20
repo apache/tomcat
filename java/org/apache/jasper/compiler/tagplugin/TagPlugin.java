2681cfc34a945df92e2cd596134d0363f2424249
Mark Thomas <markt@apache.org>
2024-09-20 15:13:42 +0100

    Code clean-up. Formatting. No functional change.

--- java/org/apache/jasper/compiler/tagplugin/TagPlugin.java
+++ java/org/apache/jasper/compiler/tagplugin/TagPlugin.java
@@ -17,18 +17,17 @@
 package org.apache.jasper.compiler.tagplugin;
 
 /**
- * This interface is to be implemented by the plugin author, to supply
- * an alternate implementation of the tag handlers.  It can be used to
- * specify the Java codes to be generated when a tag is invoked.
- *
- * An implementation of this interface must be registered in a file
- * named "tagPlugins.xml" under WEB-INF.
+ * This interface is to be implemented by the plugin author, to supply an alternate implementation of the tag handlers.
+ * It can be used to specify the Java codes to be generated when a tag is invoked.
+ * <p>
+ * An implementation of this interface must be registered in a file named "tagPlugins.xml" under WEB-INF.
  */
 
 public interface TagPlugin {
 
     /**
      * Generate codes for a custom tag.
+     *
      * @param ctxt a TagPluginContext for accessing Jasper functions
      */
     void doTag(TagPluginContext ctxt);
