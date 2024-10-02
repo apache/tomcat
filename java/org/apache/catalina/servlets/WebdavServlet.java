b03fde03837da631c69226d19d67b751f5b05919
Michael Osipov <michaelo@apache.org>
2024-10-02 13:48:45 +0200

    Make WebdavServlet properly return the Allow header on NOT_ALLOWED status

--- java/org/apache/catalina/servlets/WebdavServlet.java
+++ java/org/apache/catalina/servlets/WebdavServlet.java
@@ -1695,7 +1695,7 @@ public class WebdavServlet extends DefaultServlet implements PeriodicEventListen
 
         if (!resource.isDirectory()) {
             if (!resource.delete()) {
-                resp.sendError(WebdavStatus.SC_METHOD_NOT_ALLOWED);
+                sendNotAllowed(req, resp);
                 return false;
             }
         } else {
