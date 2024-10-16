baa77ba7faa4eee524c7cc2eb7d15610dd9c335e
remm <remm@apache.org>
2024-10-16 11:54:48 +0200

    Delete should remove any existing locks

--- java/org/apache/catalina/servlets/WebdavServlet.java
+++ java/org/apache/catalina/servlets/WebdavServlet.java
@@ -1182,9 +1182,6 @@ public class WebdavServlet extends DefaultServlet implements PeriodicEventListen
 
                 } else {
 
-                    lock.tokens.add(lockToken);
-                    resourceLocks.put(lock.path, lock);
-
                     // Checking if a resource exists at this path
                     if (!resource.exists()) {
 
@@ -1194,6 +1191,9 @@ public class WebdavServlet extends DefaultServlet implements PeriodicEventListen
 
                         lockNullResources.computeIfAbsent(parentPath, k -> new CopyOnWriteArrayList<>()).add(lock.path);
                     }
+
+                    lock.tokens.add(lockToken);
+                    resourceLocks.put(lock.path, lock);
 
                     // Add the Lock-Token header as by RFC 2518 8.10.1
                     // - only do this for newly created locks
@@ -1731,6 +1731,7 @@ public class WebdavServlet extends DefaultServlet implements PeriodicEventListen
                 sendNotAllowed(req, resp);
                 return false;
             }
+            resourceLocks.remove(path);
         } else {
 
             Map<String,Integer> errorList = new LinkedHashMap<>();
@@ -1752,6 +1753,16 @@ public class WebdavServlet extends DefaultServlet implements PeriodicEventListen
                      */
                     errorList.put(path, Integer.valueOf(WebdavStatus.SC_METHOD_NOT_ALLOWED));
                 }
+            } else {
+                Iterator<LockInfo> collectionLocksList = collectionLocks.iterator();
+                while (collectionLocksList.hasNext()) {
+                    LockInfo lock = collectionLocksList.next();
+                    if (path.equals(lock.path)) {
+                        collectionLocks.remove(lock);
+                        break;
+                    }
+                }
+                resourceLocks.remove(path);
             }
 
             if (!errorList.isEmpty()) {
@@ -1830,6 +1841,18 @@ public class WebdavServlet extends DefaultServlet implements PeriodicEventListen
                          */
                         errorList.put(childName, Integer.valueOf(WebdavStatus.SC_METHOD_NOT_ALLOWED));
                     }
+                } else {
+                    if (childResource.isDirectory()) {
+                        Iterator<LockInfo> collectionLocksList = collectionLocks.iterator();
+                        while (collectionLocksList.hasNext()) {
+                            LockInfo lock = collectionLocksList.next();
+                            if (childName.equals(lock.path)) {
+                                collectionLocks.remove(lock);
+                                break;
+                            }
+                        }
+                    }
+                    resourceLocks.remove(childName);
                 }
             }
         }
