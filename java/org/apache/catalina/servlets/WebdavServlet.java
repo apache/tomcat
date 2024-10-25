40d50e1080d479836339cdb2a9c6c97fb4d0c999
remm <remm@apache.org>
2024-10-25 10:51:51 +0200

    Return 400 for unlock without lock token

--- java/org/apache/catalina/servlets/WebdavServlet.java
+++ java/org/apache/catalina/servlets/WebdavServlet.java
@@ -1719,7 +1719,8 @@ public class WebdavServlet extends DefaultServlet implements PeriodicEventListen
 
         String lockTokenHeader = req.getHeader("Lock-Token");
         if (lockTokenHeader == null) {
-            lockTokenHeader = "";
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
         }
 
         boolean unlocked = false;
