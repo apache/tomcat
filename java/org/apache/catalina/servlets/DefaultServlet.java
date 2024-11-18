382872c6d317128f1f4f64eb34ef2b0c27ff6b4a
remm <remm@apache.org>
2024-11-18 14:22:19 +0100

    Fix content-range header length
    Submitted by Justin Chen.

--- java/org/apache/catalina/servlets/DefaultServlet.java
+++ java/org/apache/catalina/servlets/DefaultServlet.java
@@ -2356,7 +2356,7 @@ public class DefaultServlet extends HttpServlet {
                 }
                 long start = getStart(range, length);
                 long end = getEnd(range, length);
-                ostream.println("Content-Range: bytes " + start + "-" + end + "/" + (end - start));
+                ostream.println("Content-Range: bytes " + start + "-" + end + "/" + length);
                 ostream.println();
 
                 // Printing content
