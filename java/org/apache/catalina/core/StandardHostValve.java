eca49efd30847beee6a006ecca8e44e6206fbfd1
Mark Thomas <markt@apache.org>
2024-11-18 15:49:49 +0000

    Fix BZ 69444 - set jakarta.servlet.error.message for error pages
    Ensure the attribute is always set even if empty
    https://bz.apache.org/bugzilla/show_bug.cgi?id=69444

--- java/org/apache/catalina/core/StandardHostValve.java
+++ java/org/apache/catalina/core/StandardHostValve.java
@@ -203,22 +203,8 @@ final class StandardHostValve extends ValveBase {
         }
         if (errorPage != null && response.isErrorReportRequired()) {
             response.setAppCommitted(false);
-            request.setAttribute(RequestDispatcher.ERROR_STATUS_CODE, Integer.valueOf(statusCode));
+            setRequestErrorAttributes(request, statusCode, null, response.getMessage(), null, errorPage.getLocation());
 
-            String message = response.getMessage();
-            if (message == null) {
-                message = "";
-            }
-            request.setAttribute(RequestDispatcher.ERROR_MESSAGE, message);
-            request.setAttribute(Globals.DISPATCHER_REQUEST_PATH_ATTR, errorPage.getLocation());
-            request.setAttribute(Globals.DISPATCHER_TYPE_ATTR, DispatcherType.ERROR);
-
-
-            Wrapper wrapper = request.getWrapper();
-            if (wrapper != null) {
-                request.setAttribute(RequestDispatcher.ERROR_SERVLET_NAME, wrapper.getName());
-            }
-            request.setAttribute(RequestDispatcher.ERROR_REQUEST_URI, request.getRequestURI());
             if (custom(request, response, errorPage)) {
                 response.setErrorReported();
                 try {
@@ -273,18 +259,9 @@ final class StandardHostValve extends ValveBase {
         if (errorPage != null) {
             if (response.setErrorReported()) {
                 response.setAppCommitted(false);
-                request.setAttribute(Globals.DISPATCHER_REQUEST_PATH_ATTR, errorPage.getLocation());
-                request.setAttribute(Globals.DISPATCHER_TYPE_ATTR, DispatcherType.ERROR);
-                request.setAttribute(RequestDispatcher.ERROR_STATUS_CODE,
-                        Integer.valueOf(HttpServletResponse.SC_INTERNAL_SERVER_ERROR));
-                request.setAttribute(RequestDispatcher.ERROR_MESSAGE, throwable.getMessage());
-                request.setAttribute(RequestDispatcher.ERROR_EXCEPTION, realError);
-                Wrapper wrapper = request.getWrapper();
-                if (wrapper != null) {
-                    request.setAttribute(RequestDispatcher.ERROR_SERVLET_NAME, wrapper.getName());
-                }
-                request.setAttribute(RequestDispatcher.ERROR_REQUEST_URI, request.getRequestURI());
-                request.setAttribute(RequestDispatcher.ERROR_EXCEPTION_TYPE, realError.getClass());
+                setRequestErrorAttributes(request, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, realError.getClass(),
+                        throwable.getMessage(), realError, errorPage.getLocation());
+
                 if (custom(request, response, errorPage)) {
                     try {
                         response.finishResponse();
@@ -307,6 +284,47 @@ final class StandardHostValve extends ValveBase {
 
             status(request, response);
         }
+    }
+
+
+    private void setRequestErrorAttributes(Request request, int statusCode, Class<?> exceptionType, String message,
+            Throwable exception, String location) {
+        /*
+         * Generally, don't set attributes to null as that will trigger an unnecessary (in this case) call to
+         * removeAttribute().
+         */
+
+        request.setAttribute(RequestDispatcher.ERROR_STATUS_CODE, Integer.valueOf(statusCode));
+
+        if (exceptionType != null) {
+            request.setAttribute(RequestDispatcher.ERROR_EXCEPTION_TYPE, exceptionType);
+        }
+
+        /*
+         * https://bz.apache.org/bugzilla/show_bug.cgi?id=69444
+         *
+         * Need to ensure message attribute is set even if there is no message (e.g. if error was triggered by an
+         * exception with a null message).
+         */
+        if (message == null) {
+            request.setAttribute(RequestDispatcher.ERROR_MESSAGE, "");
+        } else {
+            request.setAttribute(RequestDispatcher.ERROR_MESSAGE, message);
+        }
+
+        if (exception != null) {
+            request.setAttribute(RequestDispatcher.ERROR_EXCEPTION, exception);
+        }
+
+        request.setAttribute(RequestDispatcher.ERROR_REQUEST_URI, request.getRequestURI());
+
+        Wrapper wrapper = request.getWrapper();
+        if (wrapper != null) {
+            request.setAttribute(RequestDispatcher.ERROR_SERVLET_NAME, wrapper.getName());
+        }
+
+        request.setAttribute(Globals.DISPATCHER_REQUEST_PATH_ATTR, location);
+        request.setAttribute(Globals.DISPATCHER_TYPE_ATTR, DispatcherType.ERROR);
     }
 
 
