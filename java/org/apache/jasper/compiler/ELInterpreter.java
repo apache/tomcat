f9597b644a7c7ac7326c78e123be252fb237660b
Mark Thomas <markt@apache.org>
2024-11-22 22:37:30 +0000

    Code clean-up - formatting. No functional change.

--- java/org/apache/jasper/compiler/ELInterpreter.java
+++ java/org/apache/jasper/compiler/ELInterpreter.java
@@ -19,28 +19,26 @@ package org.apache.jasper.compiler;
 import org.apache.jasper.JspCompilationContext;
 
 /**
- * Defines the interface for the expression language interpreter. This allows
- * users to provide custom EL interpreter implementations that can optimise
- * EL processing for an application by , for example, performing code generation
- * for simple expressions.
+ * Defines the interface for the expression language interpreter. This allows users to provide custom EL interpreter
+ * implementations that can optimise EL processing for an application by , for example, performing code generation for
+ * simple expressions.
  */
 public interface ELInterpreter {
 
     /**
-     * Returns the string representing the code that will be inserted into the
-     * servlet generated for JSP. The default implementation creates a call to
-     * {@link org.apache.jasper.runtime.PageContextImpl#proprietaryEvaluate(
-     * String, Class, jakarta.servlet.jsp.PageContext,
-     * org.apache.jasper.runtime.ProtectedFunctionMapper)} but other
-     * implementations may produce more optimised code.
-     * @param context The compilation context
-     * @param isTagFile <code>true</code> if in a tag file rather than a JSP
-     * @param expression a String containing zero or more "${}" expressions
+     * Returns the string representing the code that will be inserted into the servlet generated for JSP. The default
+     * implementation creates a call to
+     * {@link org.apache.jasper.runtime.PageContextImpl#proprietaryEvaluate( String, Class, jakarta.servlet.jsp.PageContext, org.apache.jasper.runtime.ProtectedFunctionMapper)}
+     * but other implementations may produce more optimised code.
+     *
+     * @param context      The compilation context
+     * @param isTagFile    <code>true</code> if in a tag file rather than a JSP
+     * @param expression   a String containing zero or more "${}" expressions
      * @param expectedType the expected type of the interpreted result
-     * @param fnmapvar Variable pointing to a function map.
+     * @param fnmapvar     Variable pointing to a function map.
+     *
      * @return a String representing a call to the EL interpreter.
      */
-    String interpreterCall(JspCompilationContext context,
-            boolean isTagFile, String expression,
-            Class<?> expectedType, String fnmapvar);
+    String interpreterCall(JspCompilationContext context, boolean isTagFile, String expression, Class<?> expectedType,
+            String fnmapvar);
 }
--- java/org/apache/jasper/compiler/ErrorHandler.java
+++ java/org/apache/jasper/compiler/ErrorHandler.java
@@ -19,12 +19,9 @@ package org.apache.jasper.compiler;
 import org.apache.jasper.JasperException;
 
 /**
- * Interface for handling JSP parse and javac compilation errors.
- *
- * An implementation of this interface may be registered with the
- * ErrorDispatcher by setting the XXX initialization parameter in the JSP
- * page compiler and execution servlet in Catalina's web.xml file to the
- * implementation's fully qualified class name.
+ * Interface for handling JSP parse and javac compilation errors. An implementation of this interface may be registered
+ * with the ErrorDispatcher by setting the XXX initialization parameter in the JSP page compiler and execution servlet
+ * in Catalina's web.xml file to the implementation's fully qualified class name.
  *
  * @author Jan Luehe
  * @author Kin-man Chung
@@ -34,43 +31,42 @@ public interface ErrorHandler {
     /**
      * Processes the given JSP parse error.
      *
-     * @param fname Name of the JSP file in which the parse error occurred
-     * @param line Parse error line number
-     * @param column Parse error column number
-     * @param msg Parse error message
+     * @param fname     Name of the JSP file in which the parse error occurred
+     * @param line      Parse error line number
+     * @param column    Parse error column number
+     * @param msg       Parse error message
      * @param exception Parse exception
+     *
      * @throws JasperException An error occurred
      */
-    void jspError(String fname, int line, int column, String msg,
-            Exception exception) throws JasperException;
+    void jspError(String fname, int line, int column, String msg, Exception exception) throws JasperException;
 
     /**
      * Processes the given JSP parse error.
      *
-     * @param msg Parse error message
+     * @param msg       Parse error message
      * @param exception Parse exception
+     *
      * @throws JasperException An error occurred
      */
-    void jspError(String msg, Exception exception)
-            throws JasperException;
+    void jspError(String msg, Exception exception) throws JasperException;
 
     /**
      * Processes the given javac compilation errors.
      *
-     * @param details Array of JavacErrorDetail instances corresponding to the
-     * compilation errors
+     * @param details Array of JavacErrorDetail instances corresponding to the compilation errors
+     *
      * @throws JasperException An error occurred
      */
-    void javacError(JavacErrorDetail[] details)
-            throws JasperException;
+    void javacError(JavacErrorDetail[] details) throws JasperException;
 
     /**
      * Processes the given javac error report and exception.
      *
      * @param errorReport Compilation error report
-     * @param exception Compilation exception
+     * @param exception   Compilation exception
+     *
      * @throws JasperException An error occurred
      */
-    void javacError(String errorReport, Exception exception)
-            throws JasperException;
+    void javacError(String errorReport, Exception exception) throws JasperException;
 }
