7c5018acc553c0b461fd2ed283c02ca586207508
Mark Thomas <markt@apache.org>
2024-11-28 09:12:56 +0000

    Fix typo

--- java/org/apache/catalina/servlets/DefaultServlet.java
+++ java/org/apache/catalina/servlets/DefaultServlet.java
@@ -1497,7 +1497,7 @@ public class DefaultServlet extends HttpServlet {
             return FULL;
         }
 
-        // Retrieving the range header (if any is specified
+        // Retrieving the range header (if any is specified)
         String rangeHeader = request.getHeader("Range");
 
         if (rangeHeader == null) {
