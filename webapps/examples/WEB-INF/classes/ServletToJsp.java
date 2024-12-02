5adf342f83eebd5a5e0a83141c56955a25f3a0c3
Mark Thomas <markt@apache.org>
2024-12-02 14:19:14 +0000

    Code clean-up - formatting. No functional change

--- webapps/examples/WEB-INF/classes/ServletToJsp.java
+++ webapps/examples/WEB-INF/classes/ServletToJsp.java
@@ -24,16 +24,15 @@ public class ServletToJsp extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     @Override
-    public void doGet (HttpServletRequest request,
-            HttpServletResponse response) {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
 
-       try {
-           // Set the attribute and Forward to hello.jsp
-           request.setAttribute ("servletName", "servletToJsp");
-           getServletConfig().getServletContext().getRequestDispatcher(
-                   "/jsp/jsptoserv/hello.jsp").forward(request, response);
-       } catch (Exception ex) {
-           ex.printStackTrace ();
-       }
+        try {
+            // Set the attribute and Forward to hello.jsp
+            request.setAttribute("servletName", "servletToJsp");
+            getServletConfig().getServletContext().getRequestDispatcher("/jsp/jsptoserv/hello.jsp").forward(request,
+                    response);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
