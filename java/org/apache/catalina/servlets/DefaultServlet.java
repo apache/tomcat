fd2ad0a4bd322ded8a7be29c6cec94147459916d
Mark Thomas <markt@apache.org>
2024-12-13 17:03:27 +0000

    0 - also indicates no modification date is available

--- java/org/apache/catalina/servlets/DefaultServlet.java
+++ java/org/apache/catalina/servlets/DefaultServlet.java
@@ -2242,7 +2242,7 @@ public class DefaultServlet extends HttpServlet {
         }
 
         long resourceLastModified = resource.getLastModified();
-        if (resourceLastModified <= -1) {
+        if (resourceLastModified <= 0) {
             // MUST ignore if the resource does not have a modification date available.
             return true;
         }
@@ -2374,7 +2374,7 @@ public class DefaultServlet extends HttpServlet {
             WebResource resource) throws IOException {
 
         long resourceLastModified = resource.getLastModified();
-        if (resourceLastModified <= -1) {
+        if (resourceLastModified <= 0) {
             // MUST ignore if the resource does not have a modification date available.
             return true;
         }
