17735c794d7d2bf60c68a02c811f62787e83bf20
Rainer Jung <rainer.jung@kippdata.de>
2025-01-01 10:52:16 +0100

    Happy New Year 2025

--- java/org/apache/catalina/manager/host/Constants.java
+++ java/org/apache/catalina/manager/host/Constants.java
@@ -80,7 +80,7 @@ public class Constants {
     public static final String HTML_TAIL_SECTION =
         "<hr size=\"1\" noshade=\"noshade\">\n" +
         "<center><font size=\"-1\" color=\"#525D76\">\n" +
-        " <em>Copyright &copy; 1999-2024, Apache Software Foundation</em>" +
+        " <em>Copyright &copy; 1999-2025, Apache Software Foundation</em>" +
         "</font></center>\n" +
         "\n" +
         "</body>\n" +
