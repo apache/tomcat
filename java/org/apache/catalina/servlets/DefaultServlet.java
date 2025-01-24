be8c59972afbfdc25e3e8e00fe0c86aef626ca77
remm <remm@apache.org>
2025-01-24 15:06:02 +0100

    Enhance lifecycle of temporary files used by partial PUT
    Delete temporary file right after finishing request processing.
    Simplify using createTempFile.

--- java/org/apache/catalina/servlets/DefaultServlet.java
+++ java/org/apache/catalina/servlets/DefaultServlet.java
@@ -615,7 +615,7 @@ public class DefaultServlet extends HttpServlet {
         }
 
         InputStream resourceInputStream = null;
-
+        File tempContentFile = null;
         try {
             // Append data specified in ranges to existing content for this
             // resource - create a temp. file on the local filesystem to
@@ -624,8 +624,8 @@ public class DefaultServlet extends HttpServlet {
             if (range == IGNORE) {
                 resourceInputStream = req.getInputStream();
             } else {
-                File contentFile = executePartialPut(req, range, path);
-                resourceInputStream = new FileInputStream(contentFile);
+                tempContentFile = executePartialPut(req, range, path);
+                resourceInputStream = new FileInputStream(tempContentFile);
             }
 
             if (resources.write(path, resourceInputStream, true)) {
@@ -649,6 +649,9 @@ public class DefaultServlet extends HttpServlet {
                     // Ignore
                 }
             }
+            if (tempContentFile != null) {
+                tempContentFile.delete();
+            }
         }
     }
 
@@ -671,13 +674,7 @@ public class DefaultServlet extends HttpServlet {
         // resource - create a temp. file on the local filesystem to
         // perform this operation
         File tempDir = (File) getServletContext().getAttribute(ServletContext.TEMPDIR);
-        // Convert all '/' characters to '.' in resourcePath
-        String convertedResourcePath = path.replace('/', '.');
-        File contentFile = new File(tempDir, convertedResourcePath);
-        if (contentFile.createNewFile()) {
-            // Clean up contentFile when Tomcat is terminated
-            contentFile.deleteOnExit();
-        }
+        File contentFile = File.createTempFile("put-part-", null, tempDir);
 
         try (RandomAccessFile randAccessContentFile = new RandomAccessFile(contentFile, "rw")) {
 
