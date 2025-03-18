495d9e45191c9503bc283d3457af3527caa7f44c
Mark Thomas <markt@apache.org>
2025-03-18 12:35:54 +0000

    Update OpenSSL version info - OpenSSl HEAD is now 3.6.x

--- test/org/apache/tomcat/util/net/openssl/ciphers/TesterOpenSSL.java
+++ test/org/apache/tomcat/util/net/openssl/ciphers/TesterOpenSSL.java
@@ -49,8 +49,10 @@ public class TesterOpenSSL {
         } catch (IOException e) {
             versionString = "";
         }
-        if (versionString.startsWith("OpenSSL 3.5.")) {
-            // Note: Gump currently tests 12.x with OpenSSL HEAD which is current 3.4.x
+        if (versionString.startsWith("OpenSSL 3.6.")) {
+            // Note: Gump currently tests 12.x with OpenSSL HEAD which is current 3.6.x
+            VERSION = 30600;
+        } else if (versionString.startsWith("OpenSSL 3.5.")) {
             VERSION = 30500;
         } else if (versionString.startsWith("OpenSSL 3.4.")) {
             VERSION = 30400;
@@ -58,18 +60,12 @@ public class TesterOpenSSL {
             VERSION = 30300;
         } else if (versionString.startsWith("OpenSSL 3.2.")) {
             VERSION = 30200;
-        } else if (versionString.startsWith("OpenSSL 3.1.")) {
-            VERSION = 30100;
         } else if (versionString.startsWith("OpenSSL 3.0.")) {
             VERSION = 30000;
-        } else if (versionString.startsWith("OpenSSL 1.1.1")) {
-            // LTS
-            // Supported until at least 2023-09-11
-            // Note: Gump currently tests 9.x and earlier with OpenSSL 1.1.1[x]
-            VERSION = 10101;
-        // Note: Release branches 1.1.0 and earlier are no longer supported by
-        //       the OpenSSL team so these tests don't support them either.
         } else {
+            // Note: 3.1.x is no longer supported by OpenSSL
+            // Note: Release branches 1.1.1 and earlier are no longer supported by
+            //       the OpenSSL team so these tests don't support them either.
             VERSION = -1;
         }
 
