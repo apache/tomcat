e5de08a56913479d024b00ff58eea7896025f103
remm <remm@apache.org>
2025-03-19 10:50:58 +0100

    Add new registry factory that does not return null
    Adding null checks everywhere is not worth it.

--- java/org/apache/tomcat/util/modeler/Registry.java
+++ java/org/apache/tomcat/util/modeler/Registry.java
@@ -132,6 +132,29 @@ public class Registry implements RegistryMBean, MBeanRegistration {
     }
 
 
+    /**
+     * Factory method to create (if necessary) and return our
+     * <code>Registry</code> instance.
+     *
+     * @param key Unused
+     * @param guard Prevent access to the registry by untrusted components
+     *
+     * @return the registry
+     * @throws IllegalArgumentException if the guard object does not allow access
+     * @since 1.1
+     */
+    public static synchronized Registry getRegistryNonNull(Object key, Object guard) {
+        if (registry == null) {
+            registry = new Registry();
+            registry.guard = guard;
+        }
+        if (registry.guard != null && registry.guard != guard) {
+            throw new IllegalArgumentException(sm.getString("registry.cannotAccessRegistry", guard));
+        }
+        return registry;
+    }
+
+
     public static synchronized void disableRegistry() {
         if (registry == null) {
             registry = new NoDescriptorRegistry();
