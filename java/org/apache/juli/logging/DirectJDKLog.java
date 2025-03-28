81c0da073db5ef66e9253a2fce4c89bfac9c3801
Mark Thomas <markt@apache.org>
2025-03-28 19:27:49 +0000

    Code clean-up - formatting. No functional change.

--- java/org/apache/juli/logging/DirectJDKLog.java
+++ java/org/apache/juli/logging/DirectJDKLog.java
@@ -30,23 +30,23 @@ class DirectJDKLog implements Log {
     public final Logger logger;
 
     // Alternate config reader and console format
-    private static final String SIMPLE_FMT="java.util.logging.SimpleFormatter";
-    private static final String FORMATTER="org.apache.juli.formatter";
+    private static final String SIMPLE_FMT = "java.util.logging.SimpleFormatter";
+    private static final String FORMATTER = "org.apache.juli.formatter";
 
     static {
-        if (System.getProperty("java.util.logging.config.class") == null  &&
+        if (System.getProperty("java.util.logging.config.class") == null &&
                 System.getProperty("java.util.logging.config.file") == null) {
             // default configuration - it sucks. Let's override at least the
             // formatter for the console
             try {
-                Formatter fmt= (Formatter) Class.forName(System.getProperty(
-                        FORMATTER, SIMPLE_FMT)).getConstructor().newInstance();
+                Formatter fmt = (Formatter) Class.forName(System.getProperty(FORMATTER, SIMPLE_FMT)).getConstructor()
+                        .newInstance();
                 // it is also possible that the user modified jre/lib/logging.properties -
                 // but that's really stupid in most cases
-                Logger root=Logger.getLogger("");
+                Logger root = Logger.getLogger("");
                 for (Handler handler : root.getHandlers()) {
                     // I only care about console - that's what's used in default config anyway
-                    if (handler instanceof  ConsoleHandler) {
+                    if (handler instanceof ConsoleHandler) {
                         handler.setFormatter(fmt);
                     }
                 }
@@ -57,8 +57,8 @@ class DirectJDKLog implements Log {
         }
     }
 
-    DirectJDKLog(String name ) {
-        logger=Logger.getLogger(name);
+    DirectJDKLog(String name) {
+        logger = Logger.getLogger(name);
     }
 
     @Override
@@ -154,17 +154,17 @@ class DirectJDKLog implements Log {
     private void log(Level level, String msg, Throwable ex) {
         if (logger.isLoggable(level)) {
             // Hack (?) to get the stack trace.
-            Throwable dummyException=new Throwable();
+            Throwable dummyException = new Throwable();
             StackTraceElement[] locations = dummyException.getStackTrace();
             // Caller will be the third element
             String cname = "unknown";
             String method = "unknown";
-            if (locations != null && locations.length >2) {
+            if (locations != null && locations.length > 2) {
                 StackTraceElement caller = locations[2];
                 cname = caller.getClassName();
                 method = caller.getMethodName();
             }
-            if (ex==null) {
+            if (ex == null) {
                 logger.logp(level, cname, method, msg);
             } else {
                 logger.logp(level, cname, method, msg, ex);
@@ -173,8 +173,7 @@ class DirectJDKLog implements Log {
     }
 
     static Log getInstance(String name) {
-        return new DirectJDKLog( name );
+        return new DirectJDKLog(name);
     }
 }
-
 
--- java/org/apache/juli/logging/Log.java
+++ java/org/apache/juli/logging/Log.java
@@ -17,12 +17,14 @@
 package org.apache.juli.logging;
 
 /**
- * <p>A simple logging interface abstracting logging APIs.  In order to be
- * instantiated successfully by {@link LogFactory}, classes that implement
- * this interface must have a constructor that takes a single String
- * parameter representing the "name" of this Log.</p>
- *
- * <p> The six logging levels used by <code>Log</code> are (in order):</p>
+ * <p>
+ * A simple logging interface abstracting logging APIs. In order to be instantiated successfully by {@link LogFactory},
+ * classes that implement this interface must have a constructor that takes a single String parameter representing the
+ * "name" of this Log.
+ * </p>
+ * <p>
+ * The six logging levels used by <code>Log</code> are (in order):
+ * </p>
  * <ol>
  * <li>trace (the least serious)</li>
  * <li>debug</li>
@@ -31,28 +33,26 @@ package org.apache.juli.logging;
  * <li>error</li>
  * <li>fatal (the most serious)</li>
  * </ol>
- * <p>The mapping of these log levels to the concepts used by the underlying
- * logging system is implementation dependent.
- * The implementation should ensure, though, that this ordering behaves
- * as expected.</p>
- *
- * <p>Performance is often a logging concern.
- * By examining the appropriate property,
- * a component can avoid expensive operations (producing information
- * to be logged).</p>
- *
- * <p> For example,
- * <code>
+ * <p>
+ * The mapping of these log levels to the concepts used by the underlying logging system is implementation dependent.
+ * The implementation should ensure, though, that this ordering behaves as expected.
+ * </p>
+ * <p>
+ * Performance is often a logging concern. By examining the appropriate property, a component can avoid expensive
+ * operations (producing information to be logged).
+ * </p>
+ * <p>
+ * For example, <code>
  *    if (log.isDebugEnabled()) {
  *        ... do something expensive ...
  *        log.debug(theResult);
  *    }
  * </code>
  * </p>
- *
- * <p>Configuration of the underlying logging system will generally be done
- * external to the Logging APIs, through whatever mechanism is supported by
- * that system.</p>
+ * <p>
+ * Configuration of the underlying logging system will generally be done external to the Logging APIs, through whatever
+ * mechanism is supported by that system.
+ * </p>
  *
  * @author <a href="mailto:sanders@apache.org">Scott Sanders</a>
  * @author Rod Waldhoff
@@ -64,79 +64,85 @@ public interface Log {
 
 
     /**
-     * <p> Is debug logging currently enabled? </p>
+     * <p>
+     * Is debug logging currently enabled?
+     * </p>
+     * <p>
+     * Call this method to prevent having to perform expensive operations (for example, <code>String</code>
+     * concatenation) when the log level is more than debug.
+     * </p>
      *
-     * <p> Call this method to prevent having to perform expensive operations
-     * (for example, <code>String</code> concatenation)
-     * when the log level is more than debug. </p>
-     *
-     * @return <code>true</code> if debug level logging is enabled, otherwise
-     *         <code>false</code>
+     * @return <code>true</code> if debug level logging is enabled, otherwise <code>false</code>
      */
     boolean isDebugEnabled();
 
 
     /**
-     * <p> Is error logging currently enabled? </p>
+     * <p>
+     * Is error logging currently enabled?
+     * </p>
+     * <p>
+     * Call this method to prevent having to perform expensive operations (for example, <code>String</code>
+     * concatenation) when the log level is more than error.
+     * </p>
      *
-     * <p> Call this method to prevent having to perform expensive operations
-     * (for example, <code>String</code> concatenation)
-     * when the log level is more than error. </p>
-     *
-     * @return <code>true</code> if error level logging is enabled, otherwise
-     *         <code>false</code>
+     * @return <code>true</code> if error level logging is enabled, otherwise <code>false</code>
      */
     boolean isErrorEnabled();
 
 
     /**
-     * <p> Is fatal logging currently enabled? </p>
+     * <p>
+     * Is fatal logging currently enabled?
+     * </p>
+     * <p>
+     * Call this method to prevent having to perform expensive operations (for example, <code>String</code>
+     * concatenation) when the log level is more than fatal.
+     * </p>
      *
-     * <p> Call this method to prevent having to perform expensive operations
-     * (for example, <code>String</code> concatenation)
-     * when the log level is more than fatal. </p>
-     *
-     * @return <code>true</code> if fatal level logging is enabled, otherwise
-     *         <code>false</code>
+     * @return <code>true</code> if fatal level logging is enabled, otherwise <code>false</code>
      */
     boolean isFatalEnabled();
 
 
     /**
-     * <p> Is info logging currently enabled? </p>
+     * <p>
+     * Is info logging currently enabled?
+     * </p>
+     * <p>
+     * Call this method to prevent having to perform expensive operations (for example, <code>String</code>
+     * concatenation) when the log level is more than info.
+     * </p>
      *
-     * <p> Call this method to prevent having to perform expensive operations
-     * (for example, <code>String</code> concatenation)
-     * when the log level is more than info. </p>
-     *
-     * @return <code>true</code> if info level logging is enabled, otherwise
-     *         <code>false</code>
+     * @return <code>true</code> if info level logging is enabled, otherwise <code>false</code>
      */
     boolean isInfoEnabled();
 
 
     /**
-     * <p> Is trace logging currently enabled? </p>
+     * <p>
+     * Is trace logging currently enabled?
+     * </p>
+     * <p>
+     * Call this method to prevent having to perform expensive operations (for example, <code>String</code>
+     * concatenation) when the log level is more than trace.
+     * </p>
      *
-     * <p> Call this method to prevent having to perform expensive operations
-     * (for example, <code>String</code> concatenation)
-     * when the log level is more than trace. </p>
-     *
-     * @return <code>true</code> if trace level logging is enabled, otherwise
-     *         <code>false</code>
+     * @return <code>true</code> if trace level logging is enabled, otherwise <code>false</code>
      */
     boolean isTraceEnabled();
 
 
     /**
-     * <p> Is warn logging currently enabled? </p>
+     * <p>
+     * Is warn logging currently enabled?
+     * </p>
+     * <p>
+     * Call this method to prevent having to perform expensive operations (for example, <code>String</code>
+     * concatenation) when the log level is more than warn.
+     * </p>
      *
-     * <p> Call this method to prevent having to perform expensive operations
-     * (for example, <code>String</code> concatenation)
-     * when the log level is more than warn. </p>
-     *
-     * @return <code>true</code> if warn level logging is enabled, otherwise
-     *         <code>false</code>
+     * @return <code>true</code> if warn level logging is enabled, otherwise <code>false</code>
      */
     boolean isWarnEnabled();
 
@@ -145,7 +151,9 @@ public interface Log {
 
 
     /**
-     * <p> Log a message with trace log level. </p>
+     * <p>
+     * Log a message with trace log level.
+     * </p>
      *
      * @param message log this message
      */
@@ -153,16 +161,20 @@ public interface Log {
 
 
     /**
-     * <p> Log an error with trace log level. </p>
+     * <p>
+     * Log an error with trace log level.
+     * </p>
      *
      * @param message log this message
-     * @param t log this cause
+     * @param t       log this cause
      */
     void trace(Object message, Throwable t);
 
 
     /**
-     * <p> Log a message with debug log level. </p>
+     * <p>
+     * Log a message with debug log level.
+     * </p>
      *
      * @param message log this message
      */
@@ -170,16 +182,20 @@ public interface Log {
 
 
     /**
-     * <p> Log an error with debug log level. </p>
+     * <p>
+     * Log an error with debug log level.
+     * </p>
      *
      * @param message log this message
-     * @param t log this cause
+     * @param t       log this cause
      */
     void debug(Object message, Throwable t);
 
 
     /**
-     * <p> Log a message with info log level. </p>
+     * <p>
+     * Log a message with info log level.
+     * </p>
      *
      * @param message log this message
      */
@@ -187,16 +203,20 @@ public interface Log {
 
 
     /**
-     * <p> Log an error with info log level. </p>
+     * <p>
+     * Log an error with info log level.
+     * </p>
      *
      * @param message log this message
-     * @param t log this cause
+     * @param t       log this cause
      */
     void info(Object message, Throwable t);
 
 
     /**
-     * <p> Log a message with warn log level. </p>
+     * <p>
+     * Log a message with warn log level.
+     * </p>
      *
      * @param message log this message
      */
@@ -204,16 +224,20 @@ public interface Log {
 
 
     /**
-     * <p> Log an error with warn log level. </p>
+     * <p>
+     * Log an error with warn log level.
+     * </p>
      *
      * @param message log this message
-     * @param t log this cause
+     * @param t       log this cause
      */
     void warn(Object message, Throwable t);
 
 
     /**
-     * <p> Log a message with error log level. </p>
+     * <p>
+     * Log a message with error log level.
+     * </p>
      *
      * @param message log this message
      */
@@ -221,16 +245,20 @@ public interface Log {
 
 
     /**
-     * <p> Log an error with error log level. </p>
+     * <p>
+     * Log an error with error log level.
+     * </p>
      *
      * @param message log this message
-     * @param t log this cause
+     * @param t       log this cause
      */
     void error(Object message, Throwable t);
 
 
     /**
-     * <p> Log a message with fatal log level. </p>
+     * <p>
+     * Log a message with fatal log level.
+     * </p>
      *
      * @param message log this message
      */
@@ -238,10 +266,12 @@ public interface Log {
 
 
     /**
-     * <p> Log an error with fatal log level. </p>
+     * <p>
+     * Log an error with fatal log level.
+     * </p>
      *
      * @param message log this message
-     * @param t log this cause
+     * @param t       log this cause
      */
     void fatal(Object message, Throwable t);
 
