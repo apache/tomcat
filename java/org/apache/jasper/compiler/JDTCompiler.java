85800a173e2eb7b9c9d0c303f4cd221e272d43d6
Mark Thomas <markt@apache.org>
2025-03-28 11:50:36 +0000

    Use available constant

--- java/org/apache/jasper/compiler/JDTCompiler.java
+++ java/org/apache/jasper/compiler/JDTCompiler.java
@@ -312,10 +312,7 @@ public class JDTCompiler extends org.apache.jasper.compiler.Compiler {
             } else if (opt.equals("22")) {
                 settings.put(CompilerOptions.OPTION_Source, CompilerOptions.VERSION_22);
             } else if (opt.equals("23")) {
-                // Constant not available in latest ECJ version shipped with
-                // Tomcat. May be supported in a snapshot build.
-                // This is checked against the actual version below.
-                settings.put(CompilerOptions.OPTION_Source, "23");
+                settings.put(CompilerOptions.OPTION_Source, CompilerOptions.VERSION_23);
             } else if (opt.equals("24")) {
                 // Constant not available in latest ECJ version shipped with
                 // Tomcat. May be supported in a snapshot build.
@@ -398,11 +395,8 @@ public class JDTCompiler extends org.apache.jasper.compiler.Compiler {
                 settings.put(CompilerOptions.OPTION_TargetPlatform, CompilerOptions.VERSION_22);
                 settings.put(CompilerOptions.OPTION_Compliance, CompilerOptions.VERSION_22);
             } else if (opt.equals("23")) {
-                // Constant not available in latest ECJ version shipped with
-                // Tomcat. May be supported in a snapshot build.
-                // This is checked against the actual version below.
-                settings.put(CompilerOptions.OPTION_TargetPlatform, "23");
-                settings.put(CompilerOptions.OPTION_Compliance, "23");
+                settings.put(CompilerOptions.OPTION_TargetPlatform, CompilerOptions.VERSION_23);
+                settings.put(CompilerOptions.OPTION_Compliance, CompilerOptions.VERSION_23);
             } else if (opt.equals("24")) {
                 // Constant not available in latest ECJ version shipped with
                 // Tomcat. May be supported in a snapshot build.
