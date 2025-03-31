c748f2fb3f07211fa88f0dedb8032c9c0ad19e4b
Mark Thomas <markt@apache.org>
2025-03-31 17:29:51 +0100

    Restore final keywords
    Removing final broke the signature tests for the pages TCK. final does
    have a meaning for static methods - it prevents sub-classes re-defining
    them.

--- java/jakarta/servlet/jsp/tagext/TagSupport.java
+++ java/jakarta/servlet/jsp/tagext/TagSupport.java
@@ -61,7 +61,7 @@ public class TagSupport implements IterationTag, Serializable {
      *
      * @return the nearest ancestor that implements the interface or is an instance of the class specified
      */
-    public static Tag findAncestorWithClass(Tag from, Class<?> klass) {
+    public static final Tag findAncestorWithClass(Tag from, Class<?> klass) {
         boolean isInterface = false;
 
         if (from == null || klass == null ||
