50cfeb7b4f6facca1a30049373eae6672d83ce2d
Mark Thomas <markt@apache.org>
2025-05-07 15:09:35 +0100

    Further backport fix

--- test/org/apache/catalina/webresources/TestWebResourceContentType.java
+++ test/org/apache/catalina/webresources/TestWebResourceContentType.java
@@ -26,10 +26,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.junit.Assert;
 import org.junit.Test;
