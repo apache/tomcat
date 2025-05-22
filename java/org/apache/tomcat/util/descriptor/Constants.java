eb1dee49a4667053014384aeffc483a07e26dc04
Mark Thomas <markt@apache.org>
2025-05-22 14:35:59 +0100

    Code clean-up - formatting. No functional change.

--- java/org/apache/tomcat/util/descriptor/Constants.java
+++ java/org/apache/tomcat/util/descriptor/Constants.java
@@ -18,8 +18,7 @@ package org.apache.tomcat.util.descriptor;
 
 public class Constants {
 
-    public static final String PACKAGE_NAME =
-            Constants.class.getPackage().getName();
+    public static final String PACKAGE_NAME = Constants.class.getPackage().getName();
 
     public static final boolean IS_SECURITY_ENABLED = (System.getSecurityManager() != null);
 }
