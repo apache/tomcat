382369d3d921ce3507a9f737f04add5e8f946451
Mark Thomas <markt@apache.org>
2025-05-22 11:58:12 +0100

    Code clean-up - formatting. No fucntional change.

--- java/org/apache/tomcat/jni/LibraryNotFoundError.java
+++ java/org/apache/tomcat/jni/LibraryNotFoundError.java
@@ -23,17 +23,15 @@ public class LibraryNotFoundError extends UnsatisfiedLinkError {
     private final String libraryNames;
 
     /**
-     * @param libraryNames A list of the file names of the native libraries that
-     *                     failed to load
-     * @param errors A list of the error messages received when trying to load
-     *               each of the libraries
+     * @param libraryNames A list of the file names of the native libraries that failed to load
+     * @param errors       A list of the error messages received when trying to load each of the libraries
      */
-    public LibraryNotFoundError(String libraryNames, String errors){
+    public LibraryNotFoundError(String libraryNames, String errors) {
         super(errors);
         this.libraryNames = libraryNames;
     }
 
-    public String getLibraryNames(){
+    public String getLibraryNames() {
         return libraryNames;
     }
 }
