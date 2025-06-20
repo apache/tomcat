5d8cc738eeb5e3e7b155a0a17d4e16b7b201a0cf
Mark Thomas <markt@apache.org>
2025-06-20 14:22:24 +0100

    Fix BZ 69717 - expand test cases

--- test/org/apache/catalina/webresources/TestJarResourceSetMountTrailing.java
+++ test/org/apache/catalina/webresources/TestJarResourceSetMountTrailing.java
@@ -17,69 +17,44 @@
 package org.apache.catalina.webresources;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.attribute.FileAttribute;
-
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 
 import org.apache.catalina.WebResourceRoot;
 import org.apache.catalina.WebResourceSet;
-import org.apache.catalina.startup.ExpandWar;
-import org.apache.catalina.startup.TomcatBaseTest;
 
-public class TestDirResourceSetMount extends AbstractTestResourceSetMount {
-
-    private static Path tempDir;
-    private static File dir1;
-
-    @BeforeClass
-    public static void before() throws IOException {
-        tempDir = Files.createTempDirectory("test", new FileAttribute[0]);
-        dir1 = new File(tempDir.toFile(), "dir1");
-        TomcatBaseTest.recursiveCopy(new File("test/webresources/dir1").toPath(), dir1.toPath());
-    }
-
-    @AfterClass
-    public static void after() {
-        ExpandWar.delete(tempDir.toFile());
-    }
-
+public class TestJarResourceSetMountTrailing extends AbstractTestResourceSetMount {
 
     @Override
     public WebResourceRoot getWebResourceRoot() {
+        File f = new File("test/webresources/dir1.jar");
         TesterWebResourceRoot root = new TesterWebResourceRoot();
         WebResourceSet webResourceSet =
-                new DirResourceSet(new TesterWebResourceRoot(), getMount(),
-                        getBaseDir().getAbsolutePath(), "/");
+                new JarResourceSet(root, getMount(), f.getAbsolutePath(), "/");
         root.setMainResources(webResourceSet);
         return root;
     }
 
     @Override
     protected boolean isWritable() {
-        return true;
+        return false;
     }
 
     @Override
     public File getBaseDir() {
-        return dir1;
+        return new File("test/webresources");
     }
 
     @Override
     protected String getNewDirName() {
-        return "test-dir-03";
+        return "test-dir-10";
     }
 
     @Override
     protected String getNewFileNameNull() {
-        return "test-null-03";
+        return "test-null-10";
     }
 
     @Override
     protected String getNewFileName() {
-        return "test-file-03";
+        return "test-file-10";
     }
 }
