abd5aeb24d1edeb22b531648d08969bf3f1319c9
Mark Thomas <markt@apache.org>
2025-06-27 15:32:52 +0100

    Align with 12.0.x

--- java/org/apache/juli/OneLineFormatter.java
+++ java/org/apache/juli/OneLineFormatter.java
@@ -25,7 +25,6 @@ import java.lang.management.ThreadInfo;
 import java.lang.management.ThreadMXBean;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.logging.Formatter;
 import java.util.logging.LogManager;
 import java.util.logging.LogRecord;
@@ -259,7 +258,7 @@ public class OneLineFormatter extends Formatter {
         }
 
         @Override
-        protected boolean removeEldestEntry(Entry<Long,String> eldest) {
+        protected boolean removeEldestEntry(Map.Entry<Long,String> eldest) {
             return (size() > cacheSize);
         }
     }
