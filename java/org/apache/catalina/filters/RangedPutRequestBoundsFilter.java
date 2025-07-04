ba5ce6758691349dbab1f2c2628108343a8b9526
Chenjp <ch_jp@msn.com>
2025-07-04 11:32:12 +0800

    Enhance
    Enhance for javadoc, code sytle, and filter init

--- java/org/apache/catalina/filters/RangedPutRequestBoundsFilter.java
+++ java/org/apache/catalina/filters/RangedPutRequestBoundsFilter.java
@@ -19,7 +19,6 @@ package org.apache.catalina.filters;
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.Enumeration;
-import java.util.regex.Pattern;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.FilterConfig;
@@ -45,23 +44,39 @@ public class RangedPutRequestBoundsFilter extends FilterBase {
     /**
      * default value for maximum size of a ranged PUT target resource.
      */
-    private static final long DEFAULT_MAX_BOUND_UP = 1L << 32;
+    private static final long DEFAULT_MAX = 1L << 32;
     private static final String PARAMETER_MAX_SIZE = "maxSize";
 
     /**
-     * Maximum size of a ranged PUT target resource. 4GB by default.
+     * Maximum size of a ranged PUT target resource, 4GB by default. A value of -1 indicates no maximum.
      */
-    private long maxSize = DEFAULT_MAX_BOUND_UP;
+    private long maxSize = DEFAULT_MAX;
 
+    /**
+     * Gets the maximum size of destination resource that is allowed.
+     *
+     * @return The maximum size of destination resource that is allowed in bytes. A value of -1 indicates no maximum.
+     */
     public long getMaxSize() {
         return maxSize;
     }
 
+    /**
+     * Sets the maximum size of destination resource that is allowed.
+     *
+     * @param maxSize The maximum size of destination resource that is allowed in bytes. A value of -1 indicates no
+     *                    maximum.
+     *
+     * @throws IllegalArgumentException if the maxSize is unsupported
+     */
     public void setMaxSize(long maxSize) {
+        if (maxSize < 0 && maxSize != -1) {
+            throw new IllegalArgumentException("Unsupported value: " + maxSize);
+        }
         this.maxSize = maxSize;
     }
 
-    private final transient Log log = LogFactory.getLog(RangedPutRequestBoundsFilter.class);
+    private transient Log log = LogFactory.getLog(RangedPutRequestBoundsFilter.class);
     private String filterName = null;
 
     @Override
@@ -71,7 +86,15 @@ public class RangedPutRequestBoundsFilter extends FilterBase {
             String name = names.nextElement();
             String value = filterConfig.getInitParameter(name);
             if (PARAMETER_MAX_SIZE.equals(name)) {
-                setMaxSize(Long.valueOf(value));
+                try {
+                    setMaxSize(Long.valueOf(value));
+                } catch (IllegalArgumentException e) {
+                    throw new ServletException(sm.getString("rangedRequestBoundsFilter.invalidParameter", filterName,
+                            PARAMETER_MAX_SIZE, value), e);
+                }
+            } else {
+                throw new ServletException(
+                        sm.getString("rangedRequestBoundsFilter.noSuchParameter", filterName, PARAMETER_MAX_SIZE));
             }
         }
     }
@@ -79,13 +102,12 @@ public class RangedPutRequestBoundsFilter extends FilterBase {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        if (request instanceof HttpServletRequest req) {
+        if (getMaxSize() != -1 && request instanceof HttpServletRequest req) {
             if ("PUT".equals(req.getMethod())) {
                 Enumeration<String> values = req.getHeaders("Content-Range");
                 while (values.hasMoreElements()) {
                     String cr = values.nextElement();
                     if (!checkContentRangeBounds(req, cr)) {
-
                         ((HttpServletResponse) response).sendError(HttpServletResponse.SC_REQUEST_ENTITY_TOO_LARGE);
                         return;
                     }
@@ -107,7 +129,7 @@ public class RangedPutRequestBoundsFilter extends FilterBase {
     private boolean checkContentRangeBounds(HttpServletRequest req, String contentRangeHeader) {
         try {
             ContentRange contentRange = ContentRange.parse(new StringReader(contentRangeHeader));
-            if (contentRange != null && getMaxSize() >= 0 &&
+            if (contentRange != null && getMaxSize() != -1 &&
                     (getMaxSize() <= contentRange.getEnd() || getMaxSize() < (contentRange.getLength()))) {
                 if (log.isWarnEnabled()) {
                     log.warn(sm.getString("rangedRequestBoundsFilter.maxSizeExceeded", filterName, req.getRequestURI(),
