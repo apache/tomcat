6f30237942f818b8248ec3baedfa25928afc9e4b
Mark Thomas <markt@apache.org>
2025-08-18 13:47:58 +0100

    Formatting - fix line length

--- java/org/apache/catalina/connector/Request.java
+++ java/org/apache/catalina/connector/Request.java
@@ -957,8 +957,8 @@ public class Request implements HttpServletRequest {
      * {@inheritDoc}
      * <p>
      * The attribute names returned will only be those for the attributes set via {@link #setAttribute(String, Object)}.
-     * Tomcat internal attributes will not be included even though they are accessible via {@link #getAttribute(String)}.
-     * The Tomcat internal attributes include:
+     * Tomcat internal attributes will not be included even though they are accessible via
+     * {@link #getAttribute(String)}. The Tomcat internal attributes include:
      * <ul>
      * <li>{@link Globals#DISPATCHER_TYPE_ATTR}</li>
      * <li>{@link Globals#DISPATCHER_REQUEST_PATH_ATTR}</li>
@@ -2444,7 +2444,8 @@ public class Request implements HttpServletRequest {
         if (partsParseException != null) {
             Context context = getContext();
             if (context != null && context.getLogger().isDebugEnabled()) {
-                context.getLogger().debug(sm.getString("coyoteRequest.partsParseException", partsParseException.getMessage()));
+                context.getLogger()
+                        .debug(sm.getString("coyoteRequest.partsParseException", partsParseException.getMessage()));
             }
             if (partsParseException instanceof IOException) {
                 throw (IOException) partsParseException;
@@ -2846,7 +2847,8 @@ public class Request implements HttpServletRequest {
         if (parametersParseException != null) {
             Context context = getContext();
             if (context != null && context.getLogger().isDebugEnabled()) {
-                context.getLogger().debug(sm.getString("coyoteRequest.parametersParseException", parametersParseException.getMessage()));
+                context.getLogger().debug(
+                        sm.getString("coyoteRequest.parametersParseException", parametersParseException.getMessage()));
             }
             throw parametersParseException;
         }
