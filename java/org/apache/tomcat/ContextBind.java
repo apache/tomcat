42dbc1fb87c1078ac882b5fd8f9e7de1ad0637f6
Mark Thomas <markt@apache.org>
2025-08-29 12:04:56 +0100

    Code clean-up - formatting. No functional change.

--- java/org/apache/tomcat/ContextBind.java
+++ java/org/apache/tomcat/ContextBind.java
@@ -19,34 +19,25 @@ package org.apache.tomcat;
 public interface ContextBind {
 
     /**
-     * Change the current thread context class loader to the web application
-     * class loader. If no web application class loader is defined, or if the
-     * current thread is already using the web application class loader then no
-     * change will be made. If the class loader is changed and a
-     * {@link org.apache.catalina.ThreadBindingListener} is configured then
-     * {@link org.apache.catalina.ThreadBindingListener#bind()} will be called
-     * after the change has been made.
+     * Change the current thread context class loader to the web application class loader. If no web application class
+     * loader is defined, or if the current thread is already using the web application class loader then no change will
+     * be made. If the class loader is changed and a {@link org.apache.catalina.ThreadBindingListener} is configured
+     * then {@link org.apache.catalina.ThreadBindingListener#bind()} will be called after the change has been made.
      *
-     * @param originalClassLoader The current class loader if known to save this
-     *     method having to look it up
+     * @param originalClassLoader The current class loader if known to save this method having to look it up
      *
-     * @return If the class loader has been changed by the method it will return
-     *     the thread context class loader in use when the method was called. If
-     *     no change was made then this method returns null.
+     * @return If the class loader has been changed by the method it will return the thread context class loader in use
+     *             when the method was called. If no change was made then this method returns null.
      */
     ClassLoader bind(ClassLoader originalClassLoader);
 
     /**
-     * Restore the current thread context class loader to the original class
-     * loader in use before {@link #bind(ClassLoader)} was called. If
-     * no original class loader is passed to this method then no change will be
-     * made. If the class loader is changed and a
-     * {@link org.apache.catalina.ThreadBindingListener} is configured then
-     * {@link org.apache.catalina.ThreadBindingListener#unbind()} will be called
-     * before the change is made.
+     * Restore the current thread context class loader to the original class loader in use before
+     * {@link #bind(ClassLoader)} was called. If no original class loader is passed to this method then no change will
+     * be made. If the class loader is changed and a {@link org.apache.catalina.ThreadBindingListener} is configured
+     * then {@link org.apache.catalina.ThreadBindingListener#unbind()} will be called before the change is made.
      *
-     * @param originalClassLoader
-     *          The class loader to restore as the thread context class loader
+     * @param originalClassLoader The class loader to restore as the thread context class loader
      */
     void unbind(ClassLoader originalClassLoader);
 }
--- java/org/apache/tomcat/JarScannerCallback.java
+++ java/org/apache/tomcat/JarScannerCallback.java
@@ -20,35 +20,31 @@ import java.io.File;
 import java.io.IOException;
 
 /**
- * This interface is implemented by clients of the {@link JarScanner} to enable
- * them to receive notification of a discovered JAR.
+ * This interface is implemented by clients of the {@link JarScanner} to enable them to receive notification of a
+ * discovered JAR.
  */
 public interface JarScannerCallback {
 
     /**
-     * A JAR was found and may be accessed for further processing via the
-     * provided URL connection. The caller is responsible for closing the JAR.
+     * A JAR was found and may be accessed for further processing via the provided URL connection. The caller is
+     * responsible for closing the JAR.
      *
      * @param jar        The JAR to process
      * @param webappPath The path, if any, to the JAR within the web application
-     * @param isWebapp   Indicates if the JAR was found within a web
-     *                       application. If <code>false</code> the JAR should
+     * @param isWebapp   Indicates if the JAR was found within a web application. If <code>false</code> the JAR should
      *                       be treated as being provided by the container
      *
      * @throws IOException if an I/O error occurs while scanning the JAR
      */
-    void scan(Jar jar, String webappPath, boolean isWebapp)
-            throws IOException;
+    void scan(Jar jar, String webappPath, boolean isWebapp) throws IOException;
 
     /**
-     * A directory was found that is to be treated as an unpacked JAR. The
-     * directory may be accessed for further processing via the provided file.
+     * A directory was found that is to be treated as an unpacked JAR. The directory may be accessed for further
+     * processing via the provided file.
      *
      * @param file       The directory containing the unpacked JAR.
-     * @param webappPath The path, if any, to the file within the web
-     *                       application
-     * @param isWebapp   Indicates if the JAR was found within a web
-     *                       application. If <code>false</code> the JAR should
+     * @param webappPath The path, if any, to the file within the web application
+     * @param isWebapp   Indicates if the JAR was found within a web application. If <code>false</code> the JAR should
      *                       be treated as being provided by the container
      *
      * @throws IOException if an I/O error occurs while scanning the JAR
@@ -56,10 +52,9 @@ public interface JarScannerCallback {
     void scan(File file, String webappPath, boolean isWebapp) throws IOException;
 
     /**
-     * A directory structure was found within the web application at
-     * /WEB-INF/classes that should be handled as an unpacked JAR. Note that all
-     * resource access must be via the ServletContext to ensure that any
-     * additional resources are visible.
+     * A directory structure was found within the web application at /WEB-INF/classes that should be handled as an
+     * unpacked JAR. Note that all resource access must be via the ServletContext to ensure that any additional
+     * resources are visible.
      *
      * @throws IOException if an I/O error occurs while scanning WEB-INF/classes
      */
