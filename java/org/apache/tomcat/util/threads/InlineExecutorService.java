7618b88ca652e5faf2ae095cec6164fe84864bfc
Mark Thomas <markt@apache.org>
2025-08-30 14:58:16 +0100

    Fix spurious wait. Code is unused in normal Tomcat usage.
    Identified by Coverity scan

--- java/org/apache/tomcat/util/threads/InlineExecutorService.java
+++ java/org/apache/tomcat/util/threads/InlineExecutorService.java
@@ -55,11 +55,17 @@ public class InlineExecutorService extends AbstractExecutorService {
 
     @Override
     public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
+    	long timeoutExpiry = System.nanoTime() + unit.toNanos(timeout);
+    	long timeoutMillis = unit.toMillis(timeout);
         synchronized (lock) {
-            if (terminated) {
-                return true;
-            }
-            lock.wait(unit.toMillis(timeout));
+        	/*
+        	 * Spurious wake-ups are possible. Keep waiting until the service has been terminated or the timeout has
+        	 * expired. 
+        	 */
+        	while (!terminated && timeoutMillis > 0) {
+        		lock.wait(timeoutMillis);
+        		timeoutMillis = (timeoutExpiry - System.nanoTime()) / 1_000_000;
+        	}
             return terminated;
         }
     }
