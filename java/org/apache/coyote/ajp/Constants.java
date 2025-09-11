68baf12a2f77b5cc40a9a66a0f52cd8e9d1b8ec5
Mark Thomas <markt@apache.org>
2025-09-11 12:32:28 +0100

    Use constant for UNLOCK

--- java/org/apache/coyote/ajp/Constants.java
+++ java/org/apache/coyote/ajp/Constants.java
@@ -108,7 +108,7 @@ public final class Constants {
     // Translates integer codes to names of HTTP methods
     private static final String[] methodTransArray =
             { Method.OPTIONS, Method.GET, Method.HEAD, Method.POST, Method.PUT, Method.OPTIONS, Method.TRACE, Method.TRACE, Method.PROPPATCH, Method.MKCOL, Method.COPY,
-                    Method.MOVE, Method.LOCK, "UNLOCK", "ACL", "REPORT", "VERSION-CONTROL", "CHECKIN", "CHECKOUT", "UNCHECKOUT",
+                    Method.MOVE, Method.LOCK, Method.UNLOCK, "ACL", "REPORT", "VERSION-CONTROL", "CHECKIN", "CHECKOUT", "UNCHECKOUT",
                     "SEARCH", "MKWORKSPACE", "UPDATE", "LABEL", "MERGE", "BASELINE-CONTROL", "MKACTIVITY" };
 
     /**
