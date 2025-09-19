41739883d9b3c28dd137517709fe185ef409be62
Mark Thomas <markt@apache.org>
2025-09-19 16:25:44 +0100

    Fix back-port

--- test/org/apache/coyote/http2/TestStandardSessionIntegrationHttp2.java
+++ test/org/apache/coyote/http2/TestStandardSessionIntegrationHttp2.java
@@ -21,11 +21,11 @@ import java.io.PrintWriter;
 import java.nio.ByteBuffer;
 import java.util.concurrent.CountDownLatch;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.junit.Assert;
 import org.junit.Test;
