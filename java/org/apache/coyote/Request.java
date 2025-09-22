50462fd6c0296f672a9e2aa843be66b24357f0de
Mark Thomas <markt@apache.org>
2025-09-22 21:00:43 +0100

    Correct back-port

--- java/org/apache/coyote/Request.java
+++ java/org/apache/coyote/Request.java
@@ -696,11 +696,9 @@ public final class Request {
     }
 
     /**
-     * Set the start time using the value provided by {@code System.currentTimeMillis()}.
+     * @param startTime time
      *
-     * @param startTime The value returned from {@code System.currentTimeMillis()} at the point the requests started.
-     *
-     * @deprecated Unused. Will be removed in Tomcat 12 onwards. Use {@link #markStartTime()}.
+     * @deprecated This setter will be removed in Tomcat 11. Use {@link #markStartTime()}.
      */
     @Deprecated
     public void setStartTime(long startTime) {
