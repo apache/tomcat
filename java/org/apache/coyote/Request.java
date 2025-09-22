58fbfb3ba096d75093aacf8c5d52be514d280f62
Mark Thomas <markt@apache.org>
2025-09-22 21:17:29 +0100

    Use nanoseconds rather than milliseconds internally. Aligns with 10.0.x+

--- java/org/apache/coyote/Request.java
+++ java/org/apache/coyote/Request.java
@@ -23,6 +23,7 @@ import java.nio.charset.Charset;
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.servlet.ReadListener;
@@ -149,7 +150,7 @@ public final class Request {
 
     private long bytesRead = 0;
     // Time of the request - useful to avoid repeated calls to System.currentTime
-    private long startTime = -1;
+    private long startTimeNanos = -1;
     private Instant startInstant = null;
     private long threadId = 0;
     private int available = 0;
@@ -692,7 +693,11 @@ public final class Request {
     }
 
     public long getStartTime() {
-        return startTime;
+        return System.currentTimeMillis() - TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startTimeNanos);
+    }
+
+    public long getStartTimeNanos() {
+        return startTimeNanos;
     }
 
     /**
@@ -702,12 +707,12 @@ public final class Request {
      */
     @Deprecated
     public void setStartTime(long startTime) {
-        this.startTime = startTime;
+        startTimeNanos = System.nanoTime() + TimeUnit.MILLISECONDS.toNanos(startTime - System.currentTimeMillis());
         startInstant = Instant.now();
     }
 
     public void markStartTime() {
-        startTime = System.currentTimeMillis();
+        startTimeNanos = System.nanoTime();
         startInstant = Instant.now();
     }
 
@@ -813,7 +818,7 @@ public final class Request {
         }
         allDataReadEventSent.set(false);
 
-        startTime = -1;
+        startTimeNanos = -1;
         startInstant = null;
         threadId = 0;
 
