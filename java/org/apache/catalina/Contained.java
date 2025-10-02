e28551b582820feb437e76195118fb01015c51b1
Mark Thomas <markt@apache.org>
2025-10-02 10:39:34 +0100

    Remove @author tags for Peter Donald

--- java/org/apache/catalina/Contained.java
+++ java/org/apache/catalina/Contained.java
@@ -17,11 +17,8 @@
 package org.apache.catalina;
 
 /**
- * <p>
  * Decoupling interface which specifies that an implementing class is associated with at most one
  * <strong>Container</strong> instance.
- * </p>
- * @author Peter Donald
  */
 public interface Contained {
 
