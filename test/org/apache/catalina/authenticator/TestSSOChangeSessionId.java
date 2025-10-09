2893a36945209de1eedbae4a619009c2a0ac7ded
Mark Thomas <markt@apache.org>
2025-10-09 15:33:30 +0100

    Fix back-port

--- test/org/apache/catalina/authenticator/TestSSOChangeSessionId.java
+++ test/org/apache/catalina/authenticator/TestSSOChangeSessionId.java
@@ -25,10 +25,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.junit.After;
 import org.junit.Assert;
