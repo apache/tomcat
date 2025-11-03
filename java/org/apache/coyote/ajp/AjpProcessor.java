36b1a02763f29e95651dd499fb3423f658dbaa75
remm <remm@apache.org>
2025-11-03 10:03:23 +0100

    Fix AJP message length check
    PR#916 by Joshua Rogers.

--- java/org/apache/coyote/ajp/AjpProcessor.java
+++ java/org/apache/coyote/ajp/AjpProcessor.java
@@ -569,7 +569,7 @@ public class AjpProcessor extends AbstractProcessor {
             // Zero length message.
             return true;
         } else {
-            if (messageLength > message.getBuffer().length) {
+            if (messageLength > (buf.length - Constants.H_SIZE)) {
                 // Message too long for the buffer
                 // Need to trigger a 400 response
                 String msg = sm.getString("ajpprocessor.header.tooLong", Integer.valueOf(messageLength),
