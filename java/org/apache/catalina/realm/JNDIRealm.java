97a803bed018478fb082d6fd8530078d1136e3f2
remm <remm@apache.org>
2025-11-03 10:10:44 +0100

    Fix SSL socket factory configuration in the JNDI realm
    Based on PR#915 by Joshua Rogers.

--- java/org/apache/catalina/realm/JNDIRealm.java
+++ java/org/apache/catalina/realm/JNDIRealm.java
@@ -2582,7 +2582,7 @@ public class JNDIRealm extends RealmBase {
         try {
             Object o = constructInstance(className);
             if (o instanceof SSLSocketFactory) {
-                return sslSocketFactory;
+                return (SSLSocketFactory) o;
             } else {
                 throw new IllegalArgumentException(sm.getString("jndiRealm.invalidSslSocketFactory", className));
             }
