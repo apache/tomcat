604a82584a824119ef9f0cdac54ed3b3b7623c12
Mark Thomas <markt@apache.org>
2025-12-02 07:57:00 +0000

    Change SSO reauthentication behaviour for SPNEGO
    Perform full SPNEGO authentication rather than re-using cached user name
    and password (if any)

--- java/org/apache/catalina/authenticator/SingleSignOn.java
+++ java/org/apache/catalina/authenticator/SingleSignOn.java
@@ -74,11 +74,10 @@ import org.apache.tomcat.util.res.StringManager;
  * from the user agent.</li>
  * <li>CLIENT-CERT - call the realm using the credentials present in the TLS connection. If not present in the TLS
  * connection, request them from the user agent.</li>
- * <li>SPNEGO - call the realm using the plain text user name and password cached by the SSO Valve if available. If not
- * cached, request authentication credentials from the user agent.</li>
+ * <li>SPNEGO - request authentication credentials from the user agent.</li>
  * </ul>
  * Note that this means that enabling reauthentication only makes sense if there are two or more web applications in the
- * Host that use BASIC, FORM or SPNEGO. If that is not the case, the SSO Valve will just add processing overhead.
+ * Host that use BASIC or FORM. If that is not the case, the SSO Valve will just add processing overhead.
  */
 public class SingleSignOn extends ValveBase {
 
