f34886c7a5bf42aa93298a5b2f6c44538166f6cf
Mark Thomas <markt@apache.org>
2025-12-04 17:19:03 +0000

    Enable OCSP on the client side (to verify server certs)

--- test/org/apache/tomcat/util/net/ocsp/TestOcspEnabled.java
+++ test/org/apache/tomcat/util/net/ocsp/TestOcspEnabled.java
@@ -30,6 +30,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -121,39 +122,133 @@ public class TestOcspEnabled extends TomcatBaseTest {
 
 
     @Test
-    public void testValidClientValidServerVerifyNone() throws Exception {
-        doTest(true, true, false, false, HttpServletResponse.SC_OK);
+    public void testRevokedClientRevokedServerVerifyNone() throws Exception {
+        doTest(false, false, ClientCertificateVerification.DISABLED, false);
+    }
+
+    @Test(expected = SSLHandshakeException.class)
+    public void testRevokedClientRevokedServerVerifyServer() throws Exception {
+        doTest(false, false, ClientCertificateVerification.DISABLED, true);
     }
 
     @Test
-    public void testValidClientRevokedServerVerifyNone() throws Exception {
-        doTest(true, false, false, false, HttpServletResponse.SC_OK);
+    public void testRevokedClientRevokedServerVerifyClientOptional() throws Exception {
+        doTest(false, false, ClientCertificateVerification.OPTIONAL_NO_CA, false);
+    }
+
+    @Test(expected = SSLHandshakeException.class)
+    public void testRevokedClientRevokedServerVerifyClientOpionalVerifyServer() throws Exception {
+        // Same as false, false, false, true since server certificate is verified before client certificate
+        doTest(false, false, ClientCertificateVerification.OPTIONAL_NO_CA, true);
+    }
+
+    @Test(expected = SSLHandshakeException.class)
+    public void testRevokedClientRevokedServerVerifyClient() throws Exception {
+        doTest(false, false, ClientCertificateVerification.ENABLED, false);
+    }
+
+    @Test(expected = SSLHandshakeException.class)
+    public void testRevokedClientRevokedServerVerifyBoth() throws Exception {
+        // Same as false, false, false, true since server certificate is verified before client certificate
+        doTest(false, false, ClientCertificateVerification.ENABLED, true);
     }
 
     @Test
     public void testRevokedClientValidServerVerifyNone() throws Exception {
-        doTest(false, true, false, false, HttpServletResponse.SC_OK);
+        doTest(false, true, ClientCertificateVerification.DISABLED, false);
     }
 
     @Test
-    public void testRevokedClientRevokedServerVerifyNone() throws Exception {
-        doTest(false, false, false, false, HttpServletResponse.SC_OK);
+    public void testRevokedClientValidServerVerifyServer() throws Exception {
+        doTest(false, true, ClientCertificateVerification.DISABLED, true);
     }
 
+    @Test
+    public void testRevokedClientValidServerVerifyClientOptional() throws Exception {
+        doTest(false, true, ClientCertificateVerification.OPTIONAL_NO_CA, false);
+    }
 
     @Test
-    public void testValidClientValidServerVerifyClient() throws Exception {
-        doTest(true, true, true, false, HttpServletResponse.SC_OK);
+    public void testRevokedClientValidServerVerifyClientOptionalVerifyServer() throws Exception {
+        doTest(false, true, ClientCertificateVerification.OPTIONAL_NO_CA, true);
     }
 
     @Test(expected = SSLHandshakeException.class)
     public void testRevokedClientValidServerVerifyClient() throws Exception {
-        doTest(false, true, true, false, HttpServletResponse.SC_OK);
+        doTest(false, true, ClientCertificateVerification.ENABLED, false);
+    }
+
+    @Test(expected = SSLHandshakeException.class)
+    public void testRevokedClientValidServerVerifyBoth() throws Exception {
+        doTest(false, true, ClientCertificateVerification.ENABLED, true);
+    }
+
+    @Test
+    public void testValidClientRevokedServerVerifyNone() throws Exception {
+        doTest(true, false, ClientCertificateVerification.DISABLED, false);
+    }
+
+    @Test(expected = SSLHandshakeException.class)
+    public void testValidClientRevokedServerVerifyServer() throws Exception {
+        doTest(true, false, ClientCertificateVerification.DISABLED, true);
+    }
+
+    @Test
+    public void testValidClientRevokedServerVerifyClientOptional() throws Exception {
+        doTest(true, false, ClientCertificateVerification.OPTIONAL_NO_CA, false);
+    }
+
+    @Test(expected = SSLHandshakeException.class)
+    public void testValidClientRevokedServerVerifyClientOptionalVerifyServer() throws Exception {
+        doTest(true, false, ClientCertificateVerification.OPTIONAL_NO_CA, true);
+    }
+
+    @Test
+    public void testValidClientRevokedServerVerifyClient() throws Exception {
+        doTest(true, false, ClientCertificateVerification.ENABLED, false);
+    }
+
+    @Test(expected = SSLHandshakeException.class)
+    public void testValidClientRevokedServerVerifyBoth() throws Exception {
+        doTest(true, false, ClientCertificateVerification.ENABLED, true);
+    }
+
+    @Test
+    public void testValidClientValidServerVerifyNone() throws Exception {
+        doTest(true, true, ClientCertificateVerification.DISABLED, false);
+    }
+
+    @Test
+    public void testValidClientValidServerVerifyServer() throws Exception {
+        doTest(true, true, ClientCertificateVerification.DISABLED, true);
+    }
+
+    @Test
+    public void testValidClientValidServerVerifyClientOptional() throws Exception {
+        doTest(true, true, ClientCertificateVerification.OPTIONAL_NO_CA, false);
+    }
+
+    @Test
+    public void testValidClientValidServerVerifyClientOptionalVerifyServer() throws Exception {
+        doTest(true, true, ClientCertificateVerification.OPTIONAL_NO_CA, true);
+    }
+
+    @Test
+    public void testValidClientValidServerVerifyClient() throws Exception {
+        doTest(true, true, ClientCertificateVerification.ENABLED, false);
+    }
+
+    @Test
+    public void testValidClientValidServerVerifyBoth() throws Exception {
+        doTest(true, true, ClientCertificateVerification.ENABLED, true);
     }
 
 
-    private void doTest(boolean clientCertValid, boolean serverCertValid, boolean verifyClientCert,
-            boolean verifyServerCert, int expectedStatusCode) throws Exception {
+    private void doTest(boolean clientCertValid, boolean serverCertValid, ClientCertificateVerification verifyClientCert,
+            boolean verifyServerCert) throws Exception {
+
+        Assume.assumeFalse(!useOpenSSLTrust && verifyClientCert == ClientCertificateVerification.OPTIONAL_NO_CA);
+
         Tomcat tomcat = getTomcatInstance();
 
         // No file system docBase required
@@ -168,20 +263,40 @@ public class TestOcspEnabled extends TomcatBaseTest {
             TesterSupport.initSsl(tomcat, TesterSupport.LOCALHOST_CRL_RSA_JKS, useOpenSSLTrust);
         }
         SSLHostConfig sslHostConfig = tomcat.getConnector().findSslHostConfigs()[0];
-        sslHostConfig.setCertificateVerification("required");
-        sslHostConfig.setOcspEnabled(verifyClientCert);
+        switch (verifyClientCert) {
+            case DISABLED:
+                sslHostConfig.setOcspEnabled(false);
+                sslHostConfig.setCertificateVerification("required");
+                break;
+            case ENABLED:
+                sslHostConfig.setOcspEnabled(true);
+                sslHostConfig.setCertificateVerification("required");
+                break;
+            case OPTIONAL_NO_CA:
+                sslHostConfig.setOcspEnabled(true);
+                sslHostConfig.setCertificateVerification("optionalNoCA");
+                break;
+
+        }
 
         if (clientCertValid) {
-            TesterSupport.configureClientSsl(TesterSupport.CLIENT_JKS);
+            TesterSupport.configureClientSsl(verifyServerCert, TesterSupport.CLIENT_JKS);
         } else {
-            TesterSupport.configureClientSsl(TesterSupport.CLIENT_CRL_JKS);
+            TesterSupport.configureClientSsl(verifyServerCert, TesterSupport.CLIENT_CRL_JKS);
         }
-        // TODO enable client-side OCSP checks
 
         tomcat.start();
 
         int rc = getUrl("https://localhost:" + getPort() + "/simple", new ByteChunk(), false);
 
-        Assert.assertEquals(expectedStatusCode, rc);
+        // If the TLS handshake fails, the test won't get this far.
+        Assert.assertEquals(HttpServletResponse.SC_OK, rc);
+    }
+
+
+    private enum ClientCertificateVerification {
+        ENABLED,
+        OPTIONAL_NO_CA,
+        DISABLED
     }
 }
