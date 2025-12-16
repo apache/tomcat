625b04bdb2bffb738af5988c568ade43a04f4b29
Mark Thomas <markt@apache.org>
2025-12-16 17:38:17 +0000

    Fix Javadoc

--- java/org/apache/tomcat/dbcp/pool2/impl/BaseGenericObjectPool.java
+++ java/org/apache/tomcat/dbcp/pool2/impl/BaseGenericObjectPool.java
@@ -1014,7 +1014,6 @@ public abstract class BaseGenericObjectPool<T> extends BaseObject implements Aut
      *         removal is configured for this pool; Integer.MAX_VALUE otherwise.
      *
      * @see AbandonedConfig#getRemoveAbandonedTimeoutDuration()
-     * @see AbandonedConfig#getRemoveAbandonedTimeoutDuration()
      * @deprecated Use {@link #getRemoveAbandonedTimeoutDuration()}.
      * @since 2.11.0
      */
