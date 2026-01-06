fe5d9d06d063b3133320bfc90f223e2b40572a3f
Mark Thomas <markt@apache.org>
2026-01-06 08:57:06 +0000

    Improve warning for using OpenSSLConf with JSSE implementations

--- java/org/apache/tomcat/util/net/SSLHostConfig.java
+++ java/org/apache/tomcat/util/net/SSLHostConfig.java
@@ -308,7 +308,6 @@ public class SSLHostConfig implements Serializable {
         } else if (openSslConf != null) {
             throw new IllegalArgumentException(sm.getString("sslHostConfig.opensslconf.alreadySet"));
         }
-        setProperty("<OpenSSLConf>", Type.OPENSSL);
         openSslConf = conf;
     }
 
