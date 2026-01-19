dd09bad57945d42d90ddd2e2d2de743484d37fc9
Mark Thomas <markt@apache.org>
2026-01-19 15:53:07 +0000

    Fix potential test failure

--- test/org/apache/tomcat/util/net/ocsp/TestOcspTimeout.java
+++ test/org/apache/tomcat/util/net/ocsp/TestOcspTimeout.java
@@ -16,6 +16,8 @@
  */
 package org.apache.tomcat.util.net.ocsp;
 
+import java.net.SocketException;
+
 import javax.net.ssl.SSLHandshakeException;
 
 import org.junit.AfterClass;
@@ -60,6 +62,15 @@ public class TestOcspTimeout extends OcspBaseTest {
 
     @Test(expected = SSLHandshakeException.class)
     public void testTimeoutWithoutSoftFail() throws Exception {
-        doTest(false, false, ClientCertificateVerification.ENABLED, false, Boolean.FALSE);
+        try {
+            doTest(false, false, ClientCertificateVerification.ENABLED, false, Boolean.FALSE);
+        } catch (SocketException se) {
+            // NIO2 may throw a SocketException rather than a SSLHandshakeException
+            if (getTomcatInstance().getConnector().getProtocolHandlerClassName().contains("Nio2")) {
+                throw new SSLHandshakeException(se.getMessage());
+            } else {
+                throw se;
+            }
+        }
     }
 }
