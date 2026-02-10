e60aa5f1ef958c8d1f05d37fd2f4c57bf7ff6cfa
Mark Thomas <markt@apache.org>
2026-02-10 19:49:28 +0000

    Update the internal fork of Apache Commons BCEL to 6.12.0

--- java/org/apache/tomcat/util/bcel/classfile/ConstantDouble.java
+++ java/org/apache/tomcat/util/bcel/classfile/ConstantDouble.java
@@ -34,7 +34,7 @@ public final class ConstantDouble extends Constant {
     /**
      * Initialize instance from file data.
      *
-     * @param file Input stream
+     * @param file Input stream.
      * @throws IOException if an I/O error occurs.
      */
     ConstantDouble(final DataInput file) throws IOException {
