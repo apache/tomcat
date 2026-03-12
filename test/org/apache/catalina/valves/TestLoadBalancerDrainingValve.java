d72433a3f73358182f99c8d9cd9bbefc59bc72bd
Mark Thomas <markt@apache.org>
2026-03-12 13:15:31 +0000

    Align test with updated redirect code

--- test/org/apache/catalina/valves/TestLoadBalancerDrainingValve.java
+++ test/org/apache/catalina/valves/TestLoadBalancerDrainingValve.java
@@ -162,8 +162,7 @@ public class TestLoadBalancerDrainingValve {
                 if (null != queryString) {
                     expectedRequestURI = expectedRequestURI + '?' + queryString;
                 }
-                response.setHeader("Location", expectedRequestURI);
-                response.setStatus(307);
+                response.sendRedirect(expectedRequestURI, 307);
             }
         }
 
