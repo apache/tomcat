0f83b200ee6d64a7c4fee410efe1a15746fdd0d6
remm <remm@apache.org>
2026-03-17 15:02:59 +0100

    Fix NPE
    Based on patch submitted by Shirsendu Mondal.

--- java/org/apache/catalina/servlets/WebdavServlet.java
+++ java/org/apache/catalina/servlets/WebdavServlet.java
@@ -1882,6 +1882,9 @@ public class WebdavServlet extends DefaultServlet implements PeriodicEventListen
         }
 
         String hrefPath = hrefUri.getPath();
+        if (hrefPath == null) {
+            return null;
+        }
 
         // Avoid path traversals
         if (!hrefPath.equals(RequestUtil.normalize(hrefPath))) {
@@ -2044,6 +2047,10 @@ public class WebdavServlet extends DefaultServlet implements PeriodicEventListen
         }
 
         String destinationPath = destinationUri.getPath();
+        if (destinationPath == null) {
+            resp.sendError(WebdavStatus.SC_BAD_REQUEST);
+            return false;
+        }
 
         // Destination isn't allowed to use '.' or '..' segments
         if (!destinationPath.equals(RequestUtil.normalize(destinationPath))) {
