045db5fa92324ea8ce7c98ae8540e3fb6cf1759b
YCHARAN <Ycharan@gmail.com>
2026-03-20 16:26:18 +0530

    Fix splitCommaSeparated behavior and add unit tests

--- test/org/apache/tomcat/util/buf/TestStringUtils.java
+++ test/org/apache/tomcat/util/buf/TestStringUtils.java
@@ -1,19 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.apache.tomcat.util.buf;
 
 import java.util.Collection;
@@ -22,7 +6,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 /*
- * None of these tests should throw a NPE.
+ *
  */
 public class TestStringUtils {
 
@@ -31,7 +15,6 @@ public class TestStringUtils {
         Assert.assertEquals("", StringUtils.join((String[]) null));
     }
 
-
     @Test
     public void testNullArrayCharStringBuilder() {
         StringBuilder sb = new StringBuilder();
@@ -39,18 +22,15 @@ public class TestStringUtils {
         Assert.assertEquals("", sb.toString());
     }
 
-
     @Test
     public void testNullCollection() {
         Assert.assertEquals("", StringUtils.join((Collection<String>) null));
     }
 
-
     @Test
     public void testNullCollectionChar() {
         Assert.assertEquals("", StringUtils.join(null, ','));
     }
-
 
     @Test
     public void testNullIterableCharStringBuilder() {
@@ -59,7 +39,6 @@ public class TestStringUtils {
         Assert.assertEquals("", sb.toString());
     }
 
-
     @Test
     public void testNullArrayCharFunctionStringBuilder() {
         StringBuilder sb = new StringBuilder();
@@ -67,11 +46,34 @@ public class TestStringUtils {
         Assert.assertEquals("", sb.toString());
     }
 
-
     @Test
     public void testNullIterableCharFunctionStringBuilder() {
         StringBuilder sb = new StringBuilder();
         StringUtils.join((Iterable<String>) null, ',', null, sb);
         Assert.assertEquals("", sb.toString());
+    }
+
+    // ✅ Added tests by Y Charan
+
+    @Test
+    public void testSplitCommaSeparatedOnlyCommas() {
+        String[] result = StringUtils.splitCommaSeparated(",,,");
+        Assert.assertArrayEquals(new String[]{}, result);
+    }
+
+    @Test
+    public void testSplitCommaSeparatedMultipleCommas() {
+        String[] result = StringUtils.splitCommaSeparated("a,,b");
+        Assert.assertArrayEquals(new String[]{"a", "", "b"}, result);
+    }
+
+    // 🔥
+
+    @Test
+    public void testJoinCollectionWithSeparator() {
+        Collection<String> input = java.util.Arrays.asList("a", "b", "c");
+        String result = StringUtils.join(input, ',');
+
+        Assert.assertEquals("a,b,c", result);
     }
 }
