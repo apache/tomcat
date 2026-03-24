9f6075bfb47568eec17c0e0c7e853bd902a13fef
remm <remm@apache.org>
2026-03-24 15:15:03 +0100

    Improve exception for unexpected error

--- java/org/apache/catalina/mapper/Mapper.java
+++ java/org/apache/catalina/mapper/Mapper.java
@@ -710,7 +710,7 @@ public final class Mapper {
             // skipped all mapping work in this case. That behaviour has a risk
             // of returning an inconsistent result.
             // I do not see a valid use case for it.
-            throw new AssertionError();
+            throw new IllegalStateException(sm.getString("mapper.alreadyDone", mappingData.host));
         }
 
         // Virtual host mapping
