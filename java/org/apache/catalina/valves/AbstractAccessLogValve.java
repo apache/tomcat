3aaad27fe235a62f6e151a92254817663fdf8af3
Mark Thomas <markt@apache.org>
2026-04-01 14:30:31 +0100

    Fix a regression in the previous release when logging the query string
    "?-"could appear in the access log rather than "?" when the query string
    was present but empty

--- java/org/apache/catalina/valves/AbstractAccessLogValve.java
+++ java/org/apache/catalina/valves/AbstractAccessLogValve.java
@@ -1119,9 +1119,13 @@ public abstract class AbstractAccessLogValve extends ValveBase implements Access
                     buf.append(request.getMethod());
                     buf.append(' ');
                     escapeAndAppend(request.getRequestURI(), buf);
-                    if (request.getQueryString() != null) {
+                    String query = request.getQueryString();
+                    if (query != null) {
                         buf.append('?');
-                        escapeAndAppend(request.getQueryString(), buf);
+                        // Don't want to write "-" if the query string is empty
+                        if (!query.isEmpty()) {
+                            escapeAndAppend(query, buf);
+                        }
                     }
                     buf.append(' ');
                     buf.append(request.getProtocol());
@@ -1366,7 +1370,12 @@ public abstract class AbstractAccessLogValve extends ValveBase implements Access
             }
             if (query != null) {
                 buf.append('?');
-                escapeAndAppend(query, buf);
+                // Don't want to write "-" if the query string is empty
+                if (!query.isEmpty()) {
+                    escapeAndAppend(query, buf);
+                }
+            } else {
+                buf.append('-');
             }
         }
     }
