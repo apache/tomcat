c547dcd9fa0c061e7ea94d0fdefeaf765848c585
Mark Thomas <markt@apache.org>
2026-04-17 10:04:00 +0100

    Fix an edge case HTTP/2 HPACK header decoding bug

---
+++ test/org/apache/coyote/http2/TestHPackHuffman.java
@@ -0,0 +1,45 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.coyote.http2;
+
+import java.nio.ByteBuffer;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class TestHPackHuffman {
+
+    /*
+     * This specific String exposed an edge case that triggered an unexpected HPACK parsing failure.
+     */
+    @Test
+    public void testValueLeftBrace() throws Exception {
+        ByteBuffer buf = ByteBuffer.allocate(10);
+        String data = "x-value{";
+        HPackHuffman.encode(buf, data, false);
+
+        buf.flip();
+        // Remove the header byte (in Tomcat this is parsed before the bytes are passed to the HPACK decoder)
+        buf.get();
+
+        StringBuilder target = new StringBuilder();
+        HPackHuffman.decode(buf, buf.remaining(), target);
+
+        Assert.assertEquals("Value changed after encode/decode roundtrip", data, target.toString());
+    }
+}
