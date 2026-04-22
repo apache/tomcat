83f3e51df7b87f5f6e626951c575ded1a512e8ef
Mark Thomas <markt@apache.org>
2026-04-22 14:18:52 +0100

    Add case sensitive attribute to LockOutRealm
    # Conflicts:
    #	webapps/docs/changelog.xml
    # Conflicts:
    #	webapps/docs/changelog.xml

--- java/org/apache/catalina/realm/LockOutRealm.java
+++ java/org/apache/catalina/realm/LockOutRealm.java
@@ -20,6 +20,7 @@ import java.io.Serial;
 import java.security.Principal;
 import java.security.cert.X509Certificate;
 import java.util.LinkedHashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -71,6 +72,11 @@ public class LockOutRealm extends CombinedRealm {
      * recent.
      */
     protected Map<String,LockRecord> failedUsers = null;
+
+    /*
+     * Should user names (the keys) in the failedUsers Map be treated in a case sensitive manner. The default is false.
+     */
+    private boolean caseSensitive = false;
 
 
     @Override
@@ -207,6 +213,9 @@ public class LockOutRealm extends CombinedRealm {
      * time will be recorded and any attempt to authenticate a locked user will log a warning.
      */
     public boolean isLocked(String username) {
+        if (!getCaseSensitive()) {
+            username = username.toLowerCase(Locale.ROOT);
+        }
         LockRecord lockRecord;
         synchronized (this) {
             lockRecord = failedUsers.get(username);
@@ -229,6 +238,9 @@ public class LockOutRealm extends CombinedRealm {
      * After successful authentication, any record of previous authentication failure is removed.
      */
     private synchronized void registerAuthSuccess(String username) {
+        if (!getCaseSensitive()) {
+            username = username.toLowerCase(Locale.ROOT);
+        }
         // Successful authentication means removal from the list of failed users
         failedUsers.remove(username);
     }
@@ -238,6 +250,9 @@ public class LockOutRealm extends CombinedRealm {
      * After a failed authentication, add the record of the failed authentication.
      */
     private void registerAuthFailure(String username) {
+        if (!getCaseSensitive()) {
+            username = username.toLowerCase(Locale.ROOT);
+        }
         LockRecord lockRecord;
         synchronized (this) {
             if (!failedUsers.containsKey(username)) {
@@ -336,6 +351,16 @@ public class LockOutRealm extends CombinedRealm {
      */
     public void setCacheRemovalWarningTime(int cacheRemovalWarningTime) {
         this.cacheRemovalWarningTime = cacheRemovalWarningTime;
+    }
+
+
+    public boolean getCaseSensitive() {
+        return caseSensitive;
+    }
+
+
+    public void setCaseSensitive(boolean caseSensitive) {
+        this.caseSensitive = caseSensitive;
     }
 
 
