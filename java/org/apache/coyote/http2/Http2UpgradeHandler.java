b96cfe2078e45d9ede8137d3fa8307f2505948ed
Mark Thomas <markt@apache.org>
2026-05-19 08:55:58 +0100

    Trigger stream reset rather than connection close where appropriate
    RFC 9113 requires a stream reset (protocol error) in each of these error
    cases

--- java/org/apache/coyote/http2/Http2UpgradeHandler.java
+++ java/org/apache/coyote/http2/Http2UpgradeHandler.java
@@ -1857,7 +1857,7 @@ class Http2UpgradeHandler extends AbstractStream implements InternalHttpUpgradeH
 
 
     @Override
-    public void receivedEndOfStream(int streamId) throws ConnectionException {
+    public void receivedEndOfStream(int streamId) throws Http2Exception {
         AbstractNonZeroStream abstractNonZeroStream =
                 getAbstractNonZeroStream(streamId, connectionState.get().isNewStreamAllowed());
         if (abstractNonZeroStream instanceof Stream) {
@@ -1867,7 +1867,7 @@ class Http2UpgradeHandler extends AbstractStream implements InternalHttpUpgradeH
     }
 
 
-    private void receivedEndOfStream(Stream stream) throws ConnectionException {
+    private void receivedEndOfStream(Stream stream) throws Http2Exception {
         stream.receivedEndOfStream();
         if (!stream.isActive()) {
             decrementActiveRemoteStreamCount(stream);
