e92f2c570a3f61b910dee9e0f3873d4607deb020
remm <remm@apache.org>
2026-05-21 10:56:42 +0200

    Fix container cleanup edge cases
    The remove event should be sent if the child container is actually
    removed, not based on some other factor.
    The framework listener should self clean a bit more when removing a
    container. Normally the children would be removed first, but doing this
    does not hurt.

--- java/org/apache/catalina/core/FrameworkListener.java
+++ java/org/apache/catalina/core/FrameworkListener.java
@@ -154,6 +154,10 @@ public abstract class FrameworkListener implements LifecycleListener, ContainerL
             }
         } else if (child instanceof Host || child instanceof Engine) {
             child.removeContainerListener(this);
+            Container[] children = child.findChildren();
+            for (Container current : children) {
+                processContainerRemoveChild(current);
+            }
         }
     }
 
