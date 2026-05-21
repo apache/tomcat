3a7e32711177ffa6dc63edf38170d09f9f5f4f02
remm <remm@apache.org>
2026-05-21 12:13:12 +0200

    Remove confusing dead code

--- java/org/apache/catalina/filters/RemoteIpFilter.java
+++ java/org/apache/catalina/filters/RemoteIpFilter.java
@@ -497,11 +497,6 @@ public class RemoteIpFilter extends GenericFilter {
         protected String scheme;
 
         /**
-         * Whether the request is secure.
-         */
-        protected boolean secure;
-
-        /**
          * The server name.
          */
         protected String serverName;
@@ -523,7 +518,6 @@ public class RemoteIpFilter extends GenericFilter {
             this.remoteAddr = request.getRemoteAddr();
             this.remoteHost = request.getRemoteHost();
             this.scheme = request.getScheme();
-            this.secure = request.isSecure();
             this.serverName = request.getServerName();
             this.serverPort = request.getServerPort();
 
