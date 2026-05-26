76a28a742bc96acabfa2a3b563f2eee6de75c1e7
remm <remm@apache.org>
2026-05-26 15:12:06 +0200

    Code review items for DS user database
    The most significant seems to be the missed modifiedUsers.clear() which
    would cause an update loop.

--- java/org/apache/catalina/users/GenericRole.java
+++ java/org/apache/catalina/users/GenericRole.java
@@ -86,7 +86,8 @@ public class GenericRole<UD extends UserDatabase> extends AbstractRole {
     public boolean equals(Object obj) {
         if (obj instanceof GenericRole) {
             GenericRole<?> role = (GenericRole<?>) obj;
-            return role.database == database && rolename.equals(role.getRolename());
+            return role.database == database &&
+                    ((rolename == null && role.getRolename() == null) || rolename.equals(role.getRolename()));
         }
         return super.equals(obj);
     }
