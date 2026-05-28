8464be56217b51631d428b826860fe1156d4a090
Mark Thomas <markt@apache.org>
2026-05-28 09:25:30 +0100

    Sigh. More of these than I expected.

--- java/org/apache/catalina/servlets/WebdavServlet.java
+++ java/org/apache/catalina/servlets/WebdavServlet.java
@@ -1751,7 +1751,7 @@ public class WebdavServlet extends DefaultServlet implements PeriodicEventListen
                     if (parentLock.hasExpired()) {
                         resourceLocks.remove(parentPath);
                     } else {
-                        // parentPath == currentPath is a check for the first loop
+                        // parentPath == path is a check for the first loop
                         if (parentPath == path || parentLock.depth > 0) {
                             if (parentLock.isExclusive()) {
                                 if (ifHeader.contains(":" + parentLock.token + ">") && (parentLock.principal == null ||
@@ -1853,7 +1853,7 @@ public class WebdavServlet extends DefaultServlet implements PeriodicEventListen
                 if (parentLock.hasExpired()) {
                     resourceLocks.remove(parentPath);
                 } else {
-                    // parentPath == currentPath is a check for the first loop
+                    // parentPath == path is a check for the first loop
                     if (parentPath == path || parentLock.depth > 0) {
                         if (parentLock.isExclusive()) {
                             if (lockTokenHeader.contains(":" + parentLock.token + ">") &&
