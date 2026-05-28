3d79a95aae3d9fb5606a6c8b0a0faab287cf3a42
Mark Thomas <markt@apache.org>
2026-05-28 09:23:01 +0100

    Update comment to align with code

--- java/org/apache/catalina/servlets/WebdavServlet.java
+++ java/org/apache/catalina/servlets/WebdavServlet.java
@@ -2759,7 +2759,7 @@ public class WebdavServlet extends DefaultServlet implements PeriodicEventListen
                 if (parentLock.hasExpired()) {
                     resourceLocks.remove(parentPath);
                 } else {
-                    // parentPath == currentPath is a check for the first loop
+                    // parentPath == path is a check for the first loop
                     if (parentPath == path || parentLock.depth > 0) {
                         if (parentLock.isExclusive()) {
                             parentLock.toXML(generatedXML);
