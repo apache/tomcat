38f7bed26819f08efe49c07432bb9b0f608c0c86
remm <remm@apache.org>
2026-06-01 09:02:18 +0200

    NPE check

--- java/org/apache/coyote/Request.java
+++ java/org/apache/coyote/Request.java
@@ -1034,9 +1034,13 @@ public final class Request {
      * @return the protocol request ID
      */
     public String getProtocolRequestId() {
-        AtomicReference<String> ref = new AtomicReference<>();
-        hook.action(ActionCode.PROTOCOL_REQUEST_ID, ref);
-        return ref.get();
+        if (hook != null) {
+            AtomicReference<String> ref = new AtomicReference<>();
+            hook.action(ActionCode.PROTOCOL_REQUEST_ID, ref);
+            return ref.get();
+        } else {
+            return null;
+        }
     }
 
 
@@ -1046,9 +1050,13 @@ public final class Request {
      * @return the servlet connection
      */
     public ServletConnection getServletConnection() {
-        AtomicReference<ServletConnection> ref = new AtomicReference<>();
-        hook.action(ActionCode.SERVLET_CONNECTION, ref);
-        return ref.get();
+        if (hook != null) {
+            AtomicReference<ServletConnection> ref = new AtomicReference<>();
+            hook.action(ActionCode.SERVLET_CONNECTION, ref);
+            return ref.get();
+        } else {
+            return null;
+        }
     }
 
 
