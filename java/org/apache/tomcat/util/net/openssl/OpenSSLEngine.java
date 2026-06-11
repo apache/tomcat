7692c869766ccd343c639c534dd7556774d6e369
Mark Thomas <markt@apache.org>
2026-06-11 15:22:23 +0100

    Add explanatory comment

--- java/org/apache/tomcat/util/net/openssl/OpenSSLEngine.java
+++ java/org/apache/tomcat/util/net/openssl/OpenSSLEngine.java
@@ -940,6 +940,10 @@ public final class OpenSSLEngine extends SSLEngine implements SSLUtil.ProtocolIn
         int code;
         if (SSL.getVersion(ssl).equals(Constants.SSL_PROTO_TLSv1_3)) {
             code = SSL.verifyClientPostHandshake(ssl);
+            /*
+             * PHA doesn't increment the handshake count with OpenSSL 3.0.x so make it look like the handshake count has
+             * been incremented.
+             */
             currentHandshake = SSL.getHandshakeCount(ssl) - 1;
         } else {
             code = SSL.renegotiate(ssl);
