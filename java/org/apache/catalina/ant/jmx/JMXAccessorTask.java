6b2cc770b018853d2799858ae7efb907373a99e0
Mark Thomas <markt@apache.org>
2026-06-12 17:58:19 +0100

    Treat TabularDataSupport similarly to an array of CompositeData
    This ignores the calculated key but I think that is OK
    Authored by CoPilot / GPT-5.4

--- java/org/apache/catalina/ant/jmx/JMXAccessorTask.java
+++ java/org/apache/catalina/ant/jmx/JMXAccessorTask.java
@@ -22,7 +22,6 @@ import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.UnknownHostException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
@@ -32,7 +31,6 @@ import javax.management.MBeanServerConnection;
 import javax.management.MalformedObjectNameException;
 import javax.management.ObjectName;
 import javax.management.openmbean.CompositeData;
-import javax.management.openmbean.CompositeDataSupport;
 import javax.management.openmbean.CompositeType;
 import javax.management.openmbean.OpenType;
 import javax.management.openmbean.SimpleType;
@@ -672,7 +670,7 @@ public class JMXAccessorTask extends BaseRedirectorHelperTask {
         if (propertyPrefix == null) {
             propertyPrefix = "";
         }
-        if (result instanceof CompositeDataSupport data) {
+        if (result instanceof CompositeData data) {
             CompositeType compositeType = data.getCompositeType();
             Set<String> keys = compositeType.keySet();
             for (String key : keys) {
@@ -685,17 +683,10 @@ public class JMXAccessorTask extends BaseRedirectorHelperTask {
                 }
             }
         } else if (result instanceof TabularDataSupport data) {
-            for (Object key : data.keySet()) {
-                for (Object key1 : ((List<?>) key)) {
-                    CompositeData valuedata = data.get(new Object[] { key1 });
-                    Object value = valuedata.get("value");
-                    OpenType<?> type = valuedata.getCompositeType().getType("value");
-                    if (type instanceof SimpleType<?>) {
-                        setProperty(propertyPrefix + "." + key1, value);
-                    } else {
-                        createProperty(propertyPrefix + "." + key1, value);
-                    }
-                }
+            int rowIndex = 0;
+            for (Object value : data.values()) {
+                createProperty(propertyPrefix + "." + rowIndex, value);
+                rowIndex++;
             }
         } else if (result.getClass().isArray()) {
             if (isSeparatearrayresults()) {
