0e2099f14046b747f9ab63d75be23e83941f9af2
Mark Thomas <markt@apache.org>
2026-06-30 20:11:04 +0100

    Fix additional potential leak

--- java/org/apache/catalina/startup/ContextConfig.java
+++ java/org/apache/catalina/startup/ContextConfig.java
@@ -1748,6 +1748,8 @@ public class ContextConfig implements LifecycleListener {
             entry = hostWebXmlCache.get(host);
             if (entry != null && entry.getGlobalTimeStamp() == globalTimeStamp &&
                     entry.getHostTimeStamp() == hostTimeStamp) {
+                InputSourceUtil.close(globalWebXml);
+                InputSourceUtil.close(hostWebXml);
                 return entry.getWebXml();
             }
 
