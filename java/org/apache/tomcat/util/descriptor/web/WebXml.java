6ec5f1575293cbbd400da55006b6f086e6aa3daf
Mark Thomas <markt@apache.org>
2026-07-06 11:21:54 +0100

    Fix handling of login-config merge conflict

--- java/org/apache/tomcat/util/descriptor/web/WebXml.java
+++ java/org/apache/tomcat/util/descriptor/web/WebXml.java
@@ -2241,6 +2241,7 @@ public class WebXml extends XmlEncodingBase implements DocumentProperties.Charse
                     } else {
                         log.error(
                                 sm.getString("webXml.mergeConflictLoginConfig", fragment.getName(), fragment.getURL()));
+                        return false;
                     }
                 }
             }
