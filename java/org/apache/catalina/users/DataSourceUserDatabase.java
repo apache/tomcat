28ae3e12170e7289c5a8f85d3efe765866013a1b
Mark Thomas <markt@apache.org>
2026-07-20 10:07:49 +0100

    Always call ExceptionUtils.handleThrowable first in a catch block

--- java/org/apache/catalina/users/DataSourceUserDatabase.java
+++ java/org/apache/catalina/users/DataSourceUserDatabase.java
@@ -1250,8 +1250,8 @@ public class DataSourceUserDatabase extends SparseUserDatabase {
                 dbConnection.commit();
                 success = true;
             } catch (Throwable t) {
-                log.error(sm.getString("dataSourceUserDatabase.exception"), t);
                 ExceptionUtils.handleThrowable(t);
+                log.error(sm.getString("dataSourceUserDatabase.exception"), t);
                 // Some exception occurred so rollback everything
                 try {
                     dbConnection.rollback();
