80415d66bff616a708f95723a0210ca0a3669389
Mark Thomas <markt@apache.org>
2026-07-27 08:27:20 +0100

    Simplify

--- java/org/apache/catalina/core/StandardContext.java
+++ java/org/apache/catalina/core/StandardContext.java
@@ -4733,10 +4733,7 @@ public class StandardContext extends ContainerBase implements Context, Notificat
     private void mergeParameters() {
         Map<String,String> mergedParams = new HashMap<>();
 
-        String[] names = findParameters();
-        for (String s : names) {
-            mergedParams.put(s, findParameter(s));
-        }
+        mergedParams.putAll(parameters);
 
         ApplicationParameter[] params = findApplicationParameters();
         for (ApplicationParameter param : params) {
@@ -4751,7 +4748,6 @@ public class StandardContext extends ContainerBase implements Context, Notificat
         for (Map.Entry<String,String> entry : mergedParams.entrySet()) {
             sc.setInitParameter(entry.getKey(), entry.getValue());
         }
-
     }
 
 
