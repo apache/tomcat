e70460d5ec337720d169cb8f8ac2498e9d1738c9
Mark Thomas <markt@apache.org>
2026-08-28 14:11:43 +0100

    Follow-up "Avoid some scenarios of expiration of never expire sessions"
    getMaxInactiveInterval == 0 is documented as never expire.

--- java/org/apache/catalina/session/DataSourceStore.java
+++ java/org/apache/catalina/session/DataSourceStore.java
@@ -356,7 +356,7 @@ public class DataSourceStore extends StoreBase {
     private String[] keys(boolean expiredOnly) throws IOException {
         String sqlTmp = "SELECT " + sessionIdCol + " FROM " + sessionTable + " WHERE " + sessionAppCol + " = ?";
         if (expiredOnly) {
-            sqlTmp += " AND " + sessionMaxInactiveCol + " >= 0";
+            sqlTmp += " AND " + sessionMaxInactiveCol + " > 0";
             sqlTmp += " AND (" + sessionLastAccessedCol + " + " + sessionMaxInactiveCol + " * 1000 < ?)";
         }
         final String keysSql = sqlTmp;
