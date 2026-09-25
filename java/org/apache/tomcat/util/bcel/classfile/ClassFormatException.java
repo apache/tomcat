0c4a542e45f81d386250165ba7ed27ad65c8d058
Mark Thomas <markt@apache.org>
2026-09-25 17:47:53 +0100

    Update the internal fork of Apache Commons BCEL to 6.13.0

--- java/org/apache/tomcat/util/bcel/classfile/ClassFormatException.java
+++ java/org/apache/tomcat/util/bcel/classfile/ClassFormatException.java
@@ -34,7 +34,7 @@ public class ClassFormatException extends RuntimeException {
      * Constructs a new instance with the specified detail message. The cause is not initialized, and may subsequently be initialized by a call to
      * {@link #initCause}.
      *
-     * @param message the detail message. The detail message is saved for later retrieval by the {@link #getMessage()} method.
+     * @param message The detail message. The detail message is saved for later retrieval by the {@link #getMessage()} method.
      */
     public ClassFormatException(final String message) {
         super(message);
